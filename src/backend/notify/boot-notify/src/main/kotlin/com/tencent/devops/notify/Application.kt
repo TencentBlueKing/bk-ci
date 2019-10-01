fa19d7b130781683bf6de9edf3bbd6c42c78d0dd
fitzcao <fitzcao@tencent.com>
2019-10-01 20:03:41 +0800

    notity first merge

--- src/backend/notify/boot-notify/src/main/kotlin/com/tencent/devops/notify/Application.kt
+++ src/backend/notify/boot-notify/src/main/kotlin/com/tencent/devops/notify/Application.kt
@@ -30,8 +30,15 @@ import com.tencent.devops.common.service.MicroService
 import com.tencent.devops.common.service.MicroServiceApplication
 import org.springframework.context.annotation.ComponentScan
 
+//@MicroService
+//@ComponentScan("com.tencent.devops.notify")
+//class Application
+//
+//fun main(args: Array<String>) {
+//    MicroServiceApplication.run(Application::class, args)
+//}
+
 @MicroService
-@ComponentScan("com.tencent.devops.notify")
 class Application
 
 fun main(args: Array<String>) {
