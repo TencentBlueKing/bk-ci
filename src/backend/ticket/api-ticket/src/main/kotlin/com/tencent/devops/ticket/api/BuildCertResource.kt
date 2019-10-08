77fef4c50769dc4d19202047a6a683bcd54e89c0
ddlin <ddlin@tencent.com>
2019-10-08 21:29:11 +0800

    fix compile error

--- src/backend/ticket/api-ticket/src/main/kotlin/com/tencent/devops/ticket/api/BuildCertResource.kt
+++ src/backend/ticket/api-ticket/src/main/kotlin/com/tencent/devops/ticket/api/BuildCertResource.kt
@@ -24,7 +24,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.tencent.devops.ticket.api.v2
+package com.tencent.devops.ticket.api
 
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BUILD_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_VM_NAME
