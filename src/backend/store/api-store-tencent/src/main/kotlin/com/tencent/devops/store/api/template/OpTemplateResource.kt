14a951d0ce10626a71e128c6d6de9f0e1b7d5d8b
jsonwan <jsonwan@tencent.com>
2019-10-25 20:01:49 +0800

    openapi迁移：去除不需要的DB配置依赖，其余微服务接口包结构更新后随之更新引用

--- src/backend/store/api-store-tencent/src/main/kotlin/com/tencent/devops/store/api/template/OpTemplateResource.kt
+++ src/backend/store/api-store-tencent/src/main/kotlin/com/tencent/devops/store/api/template/OpTemplateResource.kt
@@ -1,11 +1,11 @@
 package com.tencent.devops.store.api.template
 
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
+import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.store.pojo.common.StoreVisibleDeptResp
 import com.tencent.devops.store.pojo.common.VisibleApproveReq
 import com.tencent.devops.store.pojo.common.enums.TemplateStatusEnum
 import com.tencent.devops.store.pojo.template.ApproveReq
-import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
-import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.store.pojo.template.OpTemplateResp
 import com.tencent.devops.store.pojo.template.enums.OpTemplateSortTypeEnum
 import com.tencent.devops.store.pojo.template.enums.TemplateTypeEnum
@@ -58,7 +58,7 @@ interface OpTemplateResource {
         latestFlag: Boolean?,
         @ApiParam("排序", required = false)
         @QueryParam("sortType")
-        sortType: OpTemplateSortTypeEnum ? = OpTemplateSortTypeEnum.updateTime,
+        sortType: OpTemplateSortTypeEnum ? = OpTemplateSortTypeEnum.UpdateTime,
         @ApiParam("排序", required = false)
         @QueryParam("desc")
         desc: Boolean?,
