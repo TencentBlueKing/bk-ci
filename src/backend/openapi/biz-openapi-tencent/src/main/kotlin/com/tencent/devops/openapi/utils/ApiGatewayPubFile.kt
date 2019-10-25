482535ede19b08e052da7b2c8430d6e4dc8e5483
jsonwan <jsonwan@tencent.com>
2019-10-25 15:06:39 +0800

    openapi迁移：增加@RefreshScope

--- src/backend/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/utils/ApiGatewayPubFile.kt
+++ src/backend/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/utils/ApiGatewayPubFile.kt
@@ -2,10 +2,12 @@ package com.tencent.devops.openapi.utils
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
 import java.io.File
 
 @Component
+@RefreshScope
 class ApiGatewayPubFile {
 
     @Value("\${api.gateway.pub.file.outer:#{null}}")
