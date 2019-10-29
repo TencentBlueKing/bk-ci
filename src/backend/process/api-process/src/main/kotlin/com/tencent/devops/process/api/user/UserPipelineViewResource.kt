ce702a1b273667fc309f1744276675cb451b8c22
rdeng <rdeng@tencent.com>
2019-10-29 23:26:59 +0800

    Update process

--- src/backend/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineViewResource.kt
+++ src/backend/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineViewResource.kt
@@ -56,8 +56,8 @@ import javax.ws.rs.core.MediaType
 interface UserPipelineViewResource {
     @ApiOperation("获取视图设置")
     @GET
-    @Path("/projects/{projectId}/settings")
-    //@Path("/{projectId}/settings")
+    //@Path("/projects/{projectId}/settings")
+    @Path("/{projectId}/settings")
     fun getViewSettings(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -69,8 +69,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("更新视图设置")
     @POST
-    @Path("/projects/{projectId}/settings")
-    //@Path("/{projectId}/settings")
+    //@Path("/projects/{projectId}/settings")
+    @Path("/{projectId}/settings")
     fun updateViewSettings(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -83,8 +83,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("获取所有视图")
     @GET
-    @Path("/projects/{projectId}/")
-    //@Path("/{projectId}/")
+    //@Path("/projects/{projectId}/")
+    @Path("/{projectId}/")
     fun getViews(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -96,8 +96,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("添加视图")
     @POST
-    @Path("/projects/{projectId}/")
-    //@Path("/{projectId}/")
+    //@Path("/projects/{projectId}/")
+    @Path("/{projectId}/")
     fun addView(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -110,8 +110,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("获取视图")
     @GET
-    @Path("/projects/{projectId}/views/{viewId}")
-    //@Path("/{projectId}/{viewId}")
+    //@Path("/projects/{projectId}/views/{viewId}")
+    @Path("/{projectId}/{viewId}")
     fun getView(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -125,8 +125,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("删除视图")
     @DELETE
-    @Path("/projects/{projectId}/views/{viewId}")
-    //@Path("/{projectId}/{viewId}")
+    //@Path("/projects/{projectId}/views/{viewId}")
+    @Path("/{projectId}/{viewId}")
     fun deleteView(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -141,8 +141,8 @@ interface UserPipelineViewResource {
 
     @ApiOperation("更改视图")
     @PUT
-    @Path("/projects/{projectId}/views/{viewId}")
-    //@Path("/{projectId}/{viewId}")
+    //@Path("/projects/{projectId}/views/{viewId}")
+    @Path("/{projectId}/{viewId}")
     fun updateView(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
