615e8bce1089271ed1c4279802083a4f081f9511
fitzcao <fitzcao@tencent.com>
2019-10-29 22:48:05 +0800

    project bug

--- src/backend/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/ServiceTxProjectResource.kt
+++ src/backend/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/ServiceTxProjectResource.kt
@@ -84,22 +84,6 @@ interface ServiceTxProjectResource {
     ): Result<List<String>>
 
     @GET
-    @Path("/{projectCode}/users/{userId}/verify")
-    @ApiOperation(" 校验用户是否项目成员")
-    fun verifyUserProjectPermission(
-            @ApiParam("PAAS_CC Token", required = true)
-            @HeaderParam(AUTH_HEADER_DEVOPS_ACCESS_TOKEN)
-            accessToken: String,
-            @ApiParam("项目代码", required = true)
-            @PathParam("projectCode")
-            projectCode: String,
-            @ApiParam("用户ID", required = true)
-            @PathParam("userId")
-            userId: String
-    ): Result<Boolean>
-
-
-    @GET
 //    @Path("/preBuild/userProject/{userId}")
     @Path("/preBuild/userProject/userId/{userId}")
     @ApiOperation("查询用户项目")
--- src/backend/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/ProjectPermissionServiceImpl.kt
+++ src/backend/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/ProjectPermissionServiceImpl.kt
@@ -27,43 +27,27 @@
 package com.tencent.devops.project.service.impl
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.exception.OperationException
 import com.tencent.devops.common.api.util.OkhttpUtils
+import com.tencent.devops.common.auth.api.BSAuthProjectApi
 import com.tencent.devops.common.auth.api.pojo.ResourceRegisterInfo
-import com.tencent.devops.project.pojo.AuthProjectForCreateResult
-import com.tencent.devops.project.pojo.Result
+import com.tencent.devops.common.auth.code.BkProjectAuthServiceCode
 import com.tencent.devops.project.service.ProjectPermissionService
-import okhttp3.MediaType
 import okhttp3.Request
-import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.core.MessageProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ProjectPermissionServiceImpl @Autowired constructor(
-        private val objectMapper: ObjectMapper
+        private val objectMapper: ObjectMapper,
+        private val authProjectApi: BSAuthProjectApi,
+        private val bkProjectAuthServiceCode: BkProjectAuthServiceCode
 ) : ProjectPermissionService {
 
     @Value("\${auth.url}")
     private lateinit var authUrl: String
-
-    override fun verifyUserProjectPermission(accessToken: String, projectCode: String, userId: String): Boolean {
-        val url = "$authUrl/$projectCode/users/$userId/verfiy?access_token=$accessToken"
-        logger.info("the verifyUserProjectPermission url is:$url")
-        val body = RequestBody.create(MediaType.parse(MessageProperties.CONTENT_TYPE_JSON), "{}")
-        val request = Request.Builder().url(url).post(body).build()
-        val responseContent = request(request, "verifyUserProjectPermission error")
-        val result = objectMapper.readValue<Result<Any?>>(responseContent)
-        logger.info("the verifyUserProjectPermission result is:$result")
-        if (result.isOk()) {
-            return true
-        }
-        return false
-    }
 
     override fun createResources(userId: String, projectList: List<ResourceRegisterInfo>) {
 
@@ -99,7 +83,7 @@ class ProjectPermissionServiceImpl @Autowired constructor(
     }
 
     override fun verifyUserProjectPermission(projectCode: String, userId: String): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return authProjectApi.isProjectUser(userId,bkProjectAuthServiceCode, projectCode, null)
     }
 
     companion object{
