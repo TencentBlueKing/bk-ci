5e9395e739f02dfb0e7747ed769c425238286a17
freyzheng <freyzheng@tencent.com>
2019-10-30 18:02:52 +0800

    修改render脚本

--- render_gateway.sh
+++ render_gateway.sh
@@ -1,8 +1,11 @@
+pwd
 rm -rf ./ci
 mkdir -p ./ci/gateway
 cp -r ./src/gateway/* ./ci/gateway
 cd ./scripts/
+pwd
 sh ./render_dev -m ci ../support-files/templates/*
 cd ./ci/gateway
+pwd
 cp ./lua/auth_user_oa.lua ./lua/auth_user.lua
 cp ./lua/oauth_util_oa.lua ./lua/oauth_util.lua
