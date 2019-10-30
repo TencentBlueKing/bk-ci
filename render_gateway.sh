85406c7f828a133fb83180651773f417d0d95073
freyzheng <freyzheng@tencent.com>
2019-10-30 18:11:52 +0800

    修改render

--- render_gateway.sh
+++ render_gateway.sh
@@ -3,7 +3,7 @@ rm -rf ${WORKSPACE}/ci
 mkdir -p ${WORKSPACE}/ci/gateway
 cp -r ${WORKSPACE}/src/gateway/* ${WORKSPACE}/ci/gateway
 cd ${WORKSPACE}/scripts/
-sh ./render_dev -m ci ../support-files/templates/*
+sh ./render_local -m ci ../support-files/templates/*
 cd ${WORKSPACE}/ci/gateway
 cp ./lua/auth_user_oa.lua ./lua/auth_user.lua
 cp ./lua/oauth_util_oa.lua ./lua/oauth_util.lua
