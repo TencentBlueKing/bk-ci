eb527f729ac050b16301ff2616c1603ceb329b91
irwinsun <irwin.ypsun@gmail.com>
2019-10-31 10:40:28 +0800

    移除相同文件

--- src/backend/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/Url.kt
+++ src/backend/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/Url.kt
@@ -32,5 +32,7 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel("版本仓库-下载信息")
 data class Url(
     @ApiModelProperty("下载链接", required = true)
-    val url: String
+    val url: String,
+    @ApiModelProperty("下载链接2", required = false)
+    val url2: String? = null
 )
--- src/backend/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/enums/Platform.kt
+++ src/backend/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/pojo/enums/Platform.kt
@@ -24,34 +24,9 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.tencent.devops.artifactory.pojo
+package com.tencent.devops.artifactory.pojo.enums
 
-import com.tencent.devops.artifactory.pojo.enums.ArtifactoryType
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
-
-@ApiModel("版本仓库-文件信息")
-data class AppFileInfo(
-    @ApiModelProperty("文件名", required = true)
-    val name: String,
-    @ApiModelProperty("文件全名", required = true)
-    val fullName: String,
-    @ApiModelProperty("文件路径", required = true)
-    val path: String,
-    @ApiModelProperty("文件全路径", required = true)
-    val fullPath: String,
-    @ApiModelProperty("文件大小(byte)", required = true)
-    val size: Long,
-    @ApiModelProperty("是否文件夹", required = true)
-    val folder: Boolean,
-    @ApiModelProperty("更新时间", required = true)
-    val modifiedTime: Long,
-    @ApiModelProperty("仓库类型", required = true)
-    val artifactoryType: ArtifactoryType,
-    @ApiModelProperty("是否显示", required = true)
-    val show: Boolean,
-    @ApiModelProperty("是否可下载", required = true)
-    val canDownload: Boolean,
-    @ApiModelProperty("版本信息", required = true)
-    val version: String? = null
-)
+enum class Platform {
+    IOS,
+    ANDROID
+}
