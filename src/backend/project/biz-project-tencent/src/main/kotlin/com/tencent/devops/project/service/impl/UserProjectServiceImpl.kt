6470c9c0a706174824330082bea8af5f62890554
fitzcao <fitzcao@tencent.com>
2019-10-31 22:21:14 +0800

    AccessService 脱敏

--- src/backend/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/UserProjectServiceImpl.kt
+++ src/backend/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/impl/UserProjectServiceImpl.kt
@@ -21,7 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class UserProjectServiceServiceImpl @Autowired constructor(
+class UserProjectServiceImpl @Autowired constructor(
         private val dslContext: DSLContext,
         private val serviceTypeDao: ServiceTypeDao,
         private val serviceDao: ServiceDao,
@@ -259,6 +259,6 @@ class UserProjectServiceServiceImpl @Autowired constructor(
     }
 
     companion object {
-        private val logger = LoggerFactory.getLogger(UserProjectServiceServiceImpl::class.java)
+        private val logger = LoggerFactory.getLogger(UserProjectServiceImpl::class.java)
     }
 }
