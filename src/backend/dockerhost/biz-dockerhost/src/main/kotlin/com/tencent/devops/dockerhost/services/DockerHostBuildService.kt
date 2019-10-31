30523df16400e3160bae730f3db674d5514e7701
rdeng <rdeng@tencent.com>
2019-10-31 22:31:48 +0800

    Update docker host

--- src/backend/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/services/DockerHostBuildService.kt
+++ src/backend/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/services/DockerHostBuildService.kt
@@ -236,9 +236,7 @@ class DockerHostBuildService(
 
             return container.id
         } catch (er: Throwable) {
-            logger.error(er.toString())
-            logger.error(er.cause.toString())
-            logger.error(er.message)
+            logger.warn("Fail to start up docker host - ($dockerBuildInfo)", er)
             log(dockerBuildInfo.buildId, true, "启动构建环境失败，错误信息:${er.message}")
             if (er is NotFoundException) {
                 throw NoSuchImageException("Create container failed: ${er.message}")
