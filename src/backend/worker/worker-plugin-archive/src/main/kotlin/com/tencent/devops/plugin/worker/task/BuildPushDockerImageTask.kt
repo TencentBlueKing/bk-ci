97f01eff9a94ba2bf49a1954a437ae4df1efe455
rdeng <rdeng@tencent.com>
2019-10-31 11:34:25 +0800

    Update worker

--- src/backend/worker/worker-plugin-archive/src/main/kotlin/com/tencent/devops/plugin/worker/task/BuildPushDockerImageTask.kt
+++ src/backend/worker/worker-plugin-archive/src/main/kotlin/com/tencent/devops/plugin/worker/task/BuildPushDockerImageTask.kt
@@ -29,9 +29,9 @@ package com.tencent.devops.plugin.worker.task
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.util.OkhttpUtils
-import com.tencent.devops.common.archive.element.BuildPushDockerImageElement
 import com.tencent.devops.common.log.Ansi
 import com.tencent.devops.common.pipeline.enums.BuildScriptType
+import com.tencent.devops.common.pipeline.pojo.element.atom.BuildPushDockerImageElement
 import com.tencent.devops.dockerhost.pojo.DockerBuildParam
 import com.tencent.devops.dockerhost.pojo.Status
 import com.tencent.devops.process.pojo.BuildTask
