c61359b38716f6f80092600a8b0fccaa701b2d38
TENCENT\carlyin <carlyin@tencent.com>
2019-11-04 11:17:14 +0800

    store代码合并

--- src/backend/store/biz-store-sample/src/main/kotlin/com/tencent/devops/store/service/atom/impl/SampleAtomReleaseServiceImpl.kt
+++ src/backend/store/biz-store-sample/src/main/kotlin/com/tencent/devops/store/service/atom/impl/SampleAtomReleaseServiceImpl.kt
@@ -54,12 +54,15 @@ class SampleAtomReleaseServiceImpl : SampleAtomReleaseService, AtomReleaseServic
     private val logger = LoggerFactory.getLogger(SampleAtomReleaseServiceImpl::class.java)
 
     override fun handleAtomPackage(
-        atomPackageSourceType: AtomPackageSourceTypeEnum,
         marketAtomCreateRequest: MarketAtomCreateRequest,
         userId: String,
         atomCode: String
     ): Result<Map<String, String>?> {
         return Result(data = null)
+    }
+
+    override fun getAtomPackageSourceType(atomCode: String): AtomPackageSourceTypeEnum {
+        return AtomPackageSourceTypeEnum.UPLOAD
     }
 
     override fun getFileStr(
