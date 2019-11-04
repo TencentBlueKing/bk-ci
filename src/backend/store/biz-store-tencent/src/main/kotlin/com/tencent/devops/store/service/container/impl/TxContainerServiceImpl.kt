43c3004b8205810a5d1152882bd629d5bc3466ea
sawyersong <sawyersong@tencent.com>
2019-11-04 11:57:41 +0800

    【增加IDC构建类型】

--- src/backend/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/container/impl/TxContainerServiceImpl.kt
+++ src/backend/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/container/impl/TxContainerServiceImpl.kt
@@ -40,6 +40,7 @@ import com.tencent.devops.environment.pojo.enums.NodeStatus
 import com.tencent.devops.environment.pojo.enums.NodeType
 import com.tencent.devops.image.api.ServiceDevCloudImageResource
 import com.tencent.devops.image.api.ServiceImageResource
+import com.tencent.devops.project.api.service.ServiceProjectResource
 import com.tencent.devops.store.pojo.app.ContainerResourceItem
 import com.tencent.devops.store.pojo.container.ContainerResource
 import com.tencent.devops.store.pojo.container.ContainerResourceValue
@@ -59,13 +60,25 @@ class TxContainerServiceImpl @Autowired constructor() : ContainerServiceImpl() {
     private lateinit var pcgImageServiceImpl: PCGImageServiceImpl
 
     override fun buildTypeEnable(buildType: BuildType, projectCode: String): Boolean {
-        logger.info("buildTypeEnable buildType is :$buildType,projectCode is :$projectCode")
-        return true
+        return when (buildType) {
+            BuildType.THIRD_PARTY_PCG -> {
+                pcgImageServiceImpl.projectEnable(projectCode)
+            }
+            else -> true
+        }
     }
 
     override fun clickable(buildType: BuildType, projectCode: String): Boolean {
-        logger.info("clickable buildType is :$buildType,projectCode is :$projectCode")
-        return true
+        return when (buildType) {
+            BuildType.IDC -> {
+                val projectVO = client.get(ServiceProjectResource::class).get(projectCode).data
+                if (projectVO?.enableIdc != null) {
+                    projectVO.enableIdc ?: false
+                } else {
+                    false
+                }
+            } else -> buildType.clickable
+        }
     }
 
     override fun getResource(
