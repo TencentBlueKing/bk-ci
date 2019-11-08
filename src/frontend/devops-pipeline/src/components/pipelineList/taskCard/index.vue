bed8684d4dbd4e97581195acf4c019dcdfc69af1
terlinhe <312592676@qq.com>
2019-11-08 19:50:13 +0800

    fix

--- src/frontend/devops-pipeline/src/components/pipelineList/taskCard/index.vue
+++ src/frontend/devops-pipeline/src/components/pipelineList/taskCard/index.vue
@@ -83,7 +83,7 @@
                 @click.stop="emitEventHandler('title-click', config.pipelineId)"
                 v-if="config.runningInfo.buildCount > 1"
             >
-                {{ $t('newlist.multipleBulids') }}
+                {{ $t('newlist.multipleBuilds') }}
             </div>
             <div class="task-card-running" @click.stop="cardContentClick" v-else>
                 <div class="running-detail clearfix">
