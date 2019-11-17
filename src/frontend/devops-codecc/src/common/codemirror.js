1fd4164fc4f18d338dabdec4b831d5fd2628cd95
irwinsun <irwin.ypsun@gmail.com>
2019-11-17 12:39:50 +0800

    remove unused module

--- src/frontend/devops-codecc/src/common/codemirror.js
+++
@@ -1,9 +0,0 @@
-import CodeMirror from 'codemirror'
-import 'codemirror/addon/scroll/simplescrollbars'
-import 'codemirror/addon/display/placeholder'
-import 'codemirror/addon/display/autorefresh'
-import 'codemirror/mode/meta'
-
-global.CodeMirror = CodeMirror
-
-export default CodeMirror
