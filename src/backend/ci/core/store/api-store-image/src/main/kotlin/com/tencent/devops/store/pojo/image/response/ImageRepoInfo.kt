e25f640474cba609e5db3937538e3a44eed33f6f
jsonwan <jsonwan@tencent.com>
2019-11-24 19:28:15 +0800

    --story=855816927 【研发商店】支持CI镜像市场 - 工作台支持关联镜像到商店
    store镜像代码迁移

--- src/backend/ci/core/store/api-store-image/src/main/kotlin/com/tencent/devops/store/pojo/image/response/ImageRepoInfo.kt
+++ src/backend/ci/core/store/api-store-image/src/main/kotlin/com/tencent/devops/store/pojo/image/response/ImageRepoInfo.kt
@@ -1,5 +1,6 @@
 package com.tencent.devops.store.pojo.image.response
 
+import com.tencent.devops.common.pipeline.type.docker.ImageType
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
@@ -11,27 +12,15 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel("镜像详情")
 data class ImageRepoInfo(
 
-    @ApiModelProperty("镜像ID", required = true)
-    val id: String,
-
-    @ApiModelProperty("镜像代码", required = true)
-    val code: String,
-
-    @ApiModelProperty("镜像名称", required = true)
-    val name: String,
-
     @ApiModelProperty("镜像来源 BKDEVOPS:蓝盾，THIRD:第三方", required = true)
-    val sourceType: String,
+    val sourceType: ImageType,
 
-    @ApiModelProperty("镜像仓库Url", required = true)
-    val repoUrl: String,
-
-    @ApiModelProperty("镜像仓库名称", required = true)
-    val repoName: String,
-
-    @ApiModelProperty("镜像tag", required = true)
-    val tag: String,
+    @ApiModelProperty("镜像完整路径", required = true)
+    val completeImageName: String,
 
     @ApiModelProperty("凭证Id", required = true)
-    val ticketId: String
+    val ticketId: String,
+
+    @ApiModelProperty("初始化凭证的项目", required = true)
+    val ticketProject: String
 )
