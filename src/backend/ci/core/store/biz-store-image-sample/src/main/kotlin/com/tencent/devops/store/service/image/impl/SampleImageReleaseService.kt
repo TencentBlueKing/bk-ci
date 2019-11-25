3722939f02a32a10e44bbb56b72c4ea283b26e41
jsonwan <jsonwan@tencent.com>
2019-11-25 22:41:42 +0800

    --story=855816927 【研发商店】支持CI镜像市场 - 工作台支持关联镜像到商店
    构造函数统一改为lateinit

--- src/backend/ci/core/store/biz-store-image-sample/src/main/kotlin/com/tencent/devops/store/service/image/impl/SampleImageReleaseService.kt
+++ src/backend/ci/core/store/biz-store-image-sample/src/main/kotlin/com/tencent/devops/store/service/image/impl/SampleImageReleaseService.kt
@@ -14,68 +14,14 @@ import com.tencent.devops.common.api.constant.NUM_TWO
 import com.tencent.devops.common.api.constant.SUCCESS
 import com.tencent.devops.common.api.constant.TEST
 import com.tencent.devops.common.api.constant.UNDO
-import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.service.utils.MessageCodeUtil
-import com.tencent.devops.store.dao.common.StoreMemberDao
-import com.tencent.devops.store.dao.common.StorePipelineBuildRelDao
-import com.tencent.devops.store.dao.common.StorePipelineRelDao
-import com.tencent.devops.store.dao.common.StoreProjectRelDao
-import com.tencent.devops.store.dao.common.StoreReleaseDao
-import com.tencent.devops.store.dao.image.ImageAgentTypeDao
-import com.tencent.devops.store.dao.image.ImageCategoryRelDao
-import com.tencent.devops.store.dao.image.ImageDao
-import com.tencent.devops.store.dao.image.ImageLabelRelDao
-import com.tencent.devops.store.dao.image.MarketImageDao
-import com.tencent.devops.store.dao.image.MarketImageFeatureDao
-import com.tencent.devops.store.dao.image.MarketImageVersionLogDao
 import com.tencent.devops.store.pojo.common.ReleaseProcessItem
 import com.tencent.devops.store.pojo.image.enums.ImageStatusEnum
-import com.tencent.devops.store.service.common.StoreCommonService
-import com.tencent.devops.store.service.image.ImageNotifyService
 import com.tencent.devops.store.service.image.ImageReleaseService
-import com.tencent.devops.store.service.image.SupportService
-import org.jooq.DSLContext
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class SampleImageReleaseService @Autowired constructor(
-    private val dslContext: DSLContext,
-    private val storeProjectRelDao: StoreProjectRelDao,
-    private val imageDao: ImageDao,
-    private val imageAgentTypeDao: ImageAgentTypeDao,
-    private val marketImageDao: MarketImageDao,
-    private val imageCategoryRelDao: ImageCategoryRelDao,
-    private val marketImageFeatureDao: MarketImageFeatureDao,
-    private val marketImageVersionLogDao: MarketImageVersionLogDao,
-    private val imageLabelRelDao: ImageLabelRelDao,
-    private val storeMemberDao: StoreMemberDao,
-    private val storePipelineRelDao: StorePipelineRelDao,
-    private val storePipelineBuildRelDao: StorePipelineBuildRelDao,
-    private val storeReleaseDao: StoreReleaseDao,
-    private val storeCommonService: StoreCommonService,
-    private val imageNotifyService: ImageNotifyService,
-    private val supportService: SupportService,
-    private val client: Client
-) : ImageReleaseService(
-    dslContext = dslContext,
-    storeProjectRelDao = storeProjectRelDao,
-    imageDao = imageDao,
-    imageAgentTypeDao = imageAgentTypeDao,
-    marketImageDao = marketImageDao,
-    imageCategoryRelDao = imageCategoryRelDao,
-    marketImageFeatureDao = marketImageFeatureDao,
-    marketImageVersionLogDao = marketImageVersionLogDao,
-    imageLabelRelDao = imageLabelRelDao,
-    storeMemberDao = storeMemberDao,
-    storePipelineRelDao = storePipelineRelDao,
-    storePipelineBuildRelDao = storePipelineBuildRelDao,
-    storeReleaseDao = storeReleaseDao,
-    storeCommonService = storeCommonService,
-    imageNotifyService = imageNotifyService,
-    supportService = supportService,
-    client = client
-) {
+class SampleImageReleaseService : ImageReleaseService() {
     override fun getPassTestStatus(isNormalUpgrade: Boolean): Byte {
         //开源版不审核直接发布
         return ImageStatusEnum.RELEASED.status.toByte()
