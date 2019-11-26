0a8b233b5ff752784320d74a0d3218359cdf748d
TENCENT\carlyin <carlyin@tencent.com>
2019-11-26 18:05:01 +0800

    --story=856530903 镜像市场代码迁移

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/service/ServicePipelineInitResourceImpl.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/service/ServicePipelineInitResourceImpl.kt
@@ -30,7 +30,6 @@ import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.pipeline.pojo.AtomMarketInitPipelineReq
 import com.tencent.devops.common.pipeline.pojo.CheckImageInitPipelineReq
 import com.tencent.devops.common.web.RestResource
-import com.tencent.devops.process.api.service.ServicePipelineInitResource
 import com.tencent.devops.process.pojo.AtomMarketInitPipelineResp
 import com.tencent.devops.process.pojo.CheckImageInitPipelineResp
 import com.tencent.devops.process.service.AtomMarketInitPipelineService
