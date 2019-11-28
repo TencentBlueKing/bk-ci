e4f2eb92abf44c6788651f8a6b1d570528390b67
freyzheng <freyzheng@tencent.com>
2019-11-28 11:48:17 +0800

    --story=856602853 【网关】新网关需要兼容旧的soda头部

--- src/gateway/lua/auth/auth_op_tencent.lua
+++ src/gateway/lua/auth/auth_op_tencent.lua
@@ -18,33 +18,34 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 ]]
 
 -- 判断是否是白名单
+local service_ip_whitelist = config.service_ip_whitelist
 local isInServiceWhitelist = false
-
-if next(config.service_ip_whitelist) ~= nil then 
-  -- 白名单为空的时候不为空的时候
-  local service_ip_whitelist = consulUtil.getAllWhitelistIp()
-  if next(service_ip_whitelist) ~= nil then
-    isInServiceWhitelist, err = ipUtil:isInWhiteList(service_ip_whitelist)
-  else
-    -- 白名单为空的时候
-    isInServiceWhitelist = true
-  end
-else
-  -- 白名单为空的时候
+-- 白名单为空的时候
+if next(service_ip_whitelist) ~= nil then
+  isInServiceWhitelist, err = ipUtil:isInWhiteList(service_ip_whitelist)
+else 
   isInServiceWhitelist = true
 end
-
 if not isInServiceWhitelist then
   ngx.log(ngx.ERR, "client ip do not in service_ip_whitelist: ", err)
   ngx.exit(403)
 end 
-local userId = ""
-if ngx.var.http_x_soda_uid ~= nil and ngx.var.http_x_soda_uid ~= "" then
-  userId = ngx.var.http_x_soda_uid
-end
 
-if ngx.var.http_x_devops_uid ~= nil and ngx.var.http_x_devops_uid ~= "" then
-  userId = ngx.var.http_x_devops_uid
+--- 获取Cookie中bk_token
+local bk_token, err = cookieUtil:get_cookie("bk_ticket")
+if not bk_token then
+  ngx.log(ngx.ERR, "failed to read user request bk_token: ", err)
+  ngx.exit(401)
 end
-ngx.header["x-devops-uid"] = userId
+local ticket = oauthUtil:get_ticket(bk_token)
+
+
+
+-- -- 记录用户的访问情况
+-- ngx.log(ngx.ERR, "access user‘s rtx :", ticket.identity.username)
+
+--- 设置sid
+ngx.header["x-devops-uid"] = ticket.user_id
+ngx.header["x-devops-bk-token"] = bk_token
+ngx.header["x-devops-access-token"] = ticket.access_token
 ngx.exit(200)
