dec48f2016ba05f3e86a503f376a952836c4727c
freyzheng <lovely_zheng@qq.com>
2019-12-06 16:03:24 +0800

    bug_76411179=解决获取代码库的冲突

--- src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/resources/ServiceRepositoryResourceImpl.kt
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/resources/ServiceRepositoryResourceImpl.kt
@@ -124,6 +124,23 @@ class ServiceRepositoryResourceImpl @Autowired constructor(
         return Result(Page(0, 9999, result.count, result.records))
     }
 
+    override fun listByProject(
+        projectId: String,
+        repositoryType: ScmType?,
+        page: Int?,
+        pageSize: Int?
+    ): Result<Page<RepositoryInfo>> {
+        if (projectId.isBlank()) {
+            throw ParamBlankException("Invalid projectId")
+        }
+        val pageNotNull = page ?: 0
+        val pageSizeNotNull = pageSize ?: 20
+
+        val limit = PageUtil.convertPageSizeToSQLLimit(pageNotNull, pageSizeNotNull)
+        val result = repositoryService.listByProject(projectId, repositoryType, limit.offset, limit.limit)
+        return Result(Page(pageNotNull, pageSizeNotNull, result.count, result.records))
+    }
+
     override fun listByProjects(projectIds: Set<String>, page: Int?, pageSize: Int?): Result<Page<RepositoryInfo>> {
         val pageNotNull = page ?: 0
         val pageSizeNotNull = pageSize ?: 20
@@ -131,4 +148,6 @@ class ServiceRepositoryResourceImpl @Autowired constructor(
         val result = repositoryService.listByProject(projectIds, null, limit.offset, limit.limit)
         return Result(Page(pageNotNull, pageSizeNotNull, result.count, result.records))
     }
+
+
 }
