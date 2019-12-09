7d2df59a8cbb5a8c14396aebc774a3de37c62357
johuang <huangjiao511@163.com>
2019-12-09 11:17:19 +0800

    --story=856700685 工蜂CI，yaml增加schema校验

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/resources/old/OldServiceNodeResourceImpl.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/resources/old/OldServiceNodeResourceImpl.kt
@@ -30,7 +30,6 @@ import com.tencent.devops.common.api.constant.CommonMessageCode
 import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
-import com.tencent.devops.environment.api.ServiceNodeResource
 import com.tencent.devops.environment.api.old.OldServiceNodeResource
 import com.tencent.devops.environment.pojo.NodeBaseInfo
 import com.tencent.devops.environment.pojo.NodeWithPermission
