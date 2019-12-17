9ccd11a36bc6f569a1f2948a98a986253212e3bf
xuzhan <xuzhan@tencent.com>
2019-12-17 22:59:14 +0800

    修改不推荐的展示

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/index.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/index.vue
@@ -26,7 +26,7 @@
                         <div class="bk-form-content">
                             <div class="atom-select-entry">
                                 <template v-if="atom">
-                                    <span :title="atom.name" class="atom-selected-name">{{ atom.name }}</span>
+                                    <span :title="atom.recommendFlag === false ? $t('editPage.notRecomendPlugin') : atom.name" :class="[{ 'not-recommend': atom.recommendFlag === false }, 'atom-selected-name']">{{ atom.name }}</span>
                                     <bk-button theme="primary" class="atom-select-btn reselect-btn" :disabled="!editable" @click.stop="toggleAtomSelectorPopup(true)">{{ $t('editPage.reSelect') }}</bk-button>
                                 </template>
                                 <template v-else-if="!atomCode">
