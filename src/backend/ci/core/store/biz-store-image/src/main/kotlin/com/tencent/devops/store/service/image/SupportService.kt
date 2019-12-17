f1df7c154e8073210c74c66fe1c2bc2d44ea7fea
wanxu <1334793794@qq.com>
2019-12-17 21:48:58 +0800

    feature:增加镜像执行拉取异常情况兜底与通知

--- src/backend/ci/core/store/biz-store-image/src/main/kotlin/com/tencent/devops/store/service/image/SupportService.kt
+++ src/backend/ci/core/store/biz-store-image/src/main/kotlin/com/tencent/devops/store/service/image/SupportService.kt
@@ -2,8 +2,12 @@ package com.tencent.devops.store.service.image
 
 import com.tencent.devops.artifactory.api.service.ServiceImageManageResource
 import com.tencent.devops.common.client.Client
+import com.tencent.devops.notify.api.service.ServiceNotifyMessageTemplateResource
+import com.tencent.devops.notify.pojo.SendNotifyMessageTemplateRequest
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Service
 
 /**
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service
  * @Date 2019/11/21
  * @Version 1.0
  */
+
+@RefreshScope
 @Service
 class SupportService @Autowired constructor(
     private val client: Client
@@ -28,5 +34,26 @@ class SupportService @Autowired constructor(
             logger.error("compressImage error is :$e", e)
         }
         return null
+    }
+
+    @Value("\${store.imageExecuteNullNotifyTplCode}")
+    private lateinit var imageExecuteNullNotifyTplCode: String
+
+    @Value("\${store.imageAdminUsers}")
+    private val imageAdminUsersStr: String? = null
+
+    fun sendImageExecuteNullToManagers(titleParams: Map<String, String>?, bodyParams: Map<String, String>?) {
+        val receivers = imageAdminUsersStr!!.trim().split(";").toMutableSet()
+        val sendNotifyMessageTemplateRequest = SendNotifyMessageTemplateRequest(
+            templateCode = imageExecuteNullNotifyTplCode,
+            sender = "DevOps",
+            receivers = receivers,
+            cc = receivers,
+            titleParams = titleParams,
+            bodyParams = bodyParams
+        )
+        val sendNotifyResult = client.get(ServiceNotifyMessageTemplateResource::class)
+            .sendNotifyMessageByTemplate(sendNotifyMessageTemplateRequest)
+        logger.info("sendNotifyResult is:$sendNotifyResult")
     }
 }
