9b4cdadd0da0b491591108506ffe83a459f39536
cao150260 <fitzcao@tencent.com>
2019-12-17 21:54:48 +0800

    --story=856453975 【OpenAPI】流水线可查看/执行权限API

---
+++ src/backend/ci/core/common/common-auth/common-auth-api/src/main/kotlin/com/tencent/devops/common/auth/api/pojo/BKAuthProjectRolesResources.kt
@@ -0,0 +1,8 @@
+package com.tencent.devops.common.auth.api.pojo
+
+data class BKAuthProjectRolesResources (
+    val displayName: String,
+    val roleId: Int,
+    val roleName: String,
+    val type: String
+)
