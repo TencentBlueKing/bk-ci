6944a6a414d0a74528def43609d1005a97d84481
xuzhan <xuzhan@tencent.com>
2019-12-17 17:50:59 +0800

    修改镜像不推荐展示

--- src/frontend/devops-pipeline/src/components/AtomSelector/index.vue
+++ src/frontend/devops-pipeline/src/components/AtomSelector/index.vue
@@ -387,6 +387,9 @@
             right: -6px;
             top: 136px;
         }
+        .not-recommend {
+            text-decoration: line-through;
+        }
         .atom-selector-header {
             position: relative;
             height: 36px;
