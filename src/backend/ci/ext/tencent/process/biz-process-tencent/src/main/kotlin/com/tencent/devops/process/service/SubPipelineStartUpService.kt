95e11d33451ad9c3dc3cb937aca65aa6db70509e
warmli <957569558@qq.com>
2019-12-18 21:59:06 +0800

    --story=856094089 【流水线插件】支持获取用户名下的工蜂代码库/分支/tag列表接口 - 用户态

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/SubPipelineStartUpService.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/SubPipelineStartUpService.kt
@@ -189,7 +189,7 @@ class SubPipelineStartUpService(
                         val map = element.data
                         val msg = map["input"] as? Map<*, *> ?: return@element
                         val subPip = msg["subPip"]
-                        val subPro = msg["projectId"]
+                        val subPro = if (msg["projectId"].toString().isBlank())projectId else msg["projectId"]
                         val exist = HashSet(currentExistPipelines)
                         checkSubpipeline(atomCode, subPro as String, subPip as String, exist)
                         existPipelines.addAll(exist)
