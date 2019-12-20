9c83bf9bcb7e4b0e27bb18d86f54565ae18230ec
sawyersong2 <wudiduomjimo@gmail.com>
2019-12-20 17:15:15 +0800

    --bug=76734921 $env替换失败

--- src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/GitCIRequestService.kt
+++ src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/GitCIRequestService.kt
@@ -247,6 +247,7 @@ class GitCIRequestService @Autowired constructor(
             return yaml
         }
         val gitProjectConf = gitCISettingDao.getSetting(dslContext, gitProjectId) ?: return yaml
+        logger.info("gitProjectConf: $gitProjectConf")
         if (null == gitProjectConf.env) {
             return yaml
         }
@@ -257,20 +258,25 @@ class GitCIRequestService @Autowired constructor(
         var line: String? = br.readLine()
         while (line != null) {
             val envMatches = envRegex.find(line)
-            if (null != envMatches) {
-                val envKeyPrefix = envMatches.groupValues[0]
+            envMatches?.groupValues?.forEach {
+                logger.info("envKeyPrefix: $it")
+                val envKeyPrefix = it
                 val envKey = envKeyPrefix.removePrefix("\$env:")
                 val envValue = getEnvValue(gitProjectConf.env!!, envKey)
-                if (null != envValue) {
-                    line = envRegex.replace(line, envValue)
+                logger.info("envKey: $envKey, envValue: $envValue")
+                line = if (null != envValue) {
+                    envRegex.replace(line!!, envValue)
+                } else {
+                    envRegex.replace(line!!, "null")
                 }
+                logger.info("line: $line")
             }
 
             sb.append(line).append("\n")
             line = br.readLine()
         }
 
-        return yaml
+        return sb.toString()
     }
 
     private fun getEnvValue(env: List<EnvironmentVariables>, key: String): String? {
