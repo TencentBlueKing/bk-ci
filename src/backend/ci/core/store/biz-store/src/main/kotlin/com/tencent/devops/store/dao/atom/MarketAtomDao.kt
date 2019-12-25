7d80a5e1ba464b87e34b73e0b41f57fde369e59d
carlyin <449036249@qq.com>
2019-12-25 17:00:42 +0800

    story: 流水线插件支持定制UI issue  #336

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/atom/MarketAtomDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/atom/MarketAtomDao.kt
@@ -321,6 +321,7 @@ class MarketAtomDao : AtomBaseDao() {
                 DOCS_LINK,
                 VISIBILITY_LEVEL,
                 PRIVATE_REASON,
+                HTML_TEMPLATE_VERSION,
                 PUBLISHER,
                 CREATOR,
                 MODIFIER
@@ -344,6 +345,7 @@ class MarketAtomDao : AtomBaseDao() {
                     docsLink,
                     marketAtomCreateRequest.visibilityLevel?.level,
                     marketAtomCreateRequest.privateReason,
+                    marketAtomCreateRequest.frontendType.typeVersion,
                     userId,
                     userId,
                     userId
@@ -380,6 +382,7 @@ class MarketAtomDao : AtomBaseDao() {
                 .set(PROPS, props)
                 .set(LOGO_URL, marketAtomUpdateRequest.logoUrl)
                 .set(ICON, iconData)
+                .set(HTML_TEMPLATE_VERSION, marketAtomUpdateRequest.frontendType.typeVersion)
                 .set(UPDATE_TIME, LocalDateTime.now())
                 .set(MODIFIER, userId)
                 .where(ID.eq(id))
@@ -460,7 +463,7 @@ class MarketAtomDao : AtomBaseDao() {
                     atomRequest.logoUrl,
                     classType,
                     atomRecord.buildLessRunFlag,
-                    atomRecord.htmlTemplateVersion,
+                    atomRequest.frontendType.typeVersion,
                     atomRecord.visibilityLevel,
                     atomRequest.publisher,
                     userId,
