5ab50763e6a98a5fe840a2b7090a00c10bf4cc44
cao150260 <fitzcao@tencent.com>
2019-12-26 14:39:26 +0800

    --bug=76753789 作业平台-构件分发 权限不足bug

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceOperationResourceImpl.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceOperationResourceImpl.kt
@@ -3,7 +3,6 @@ package com.tencent.devops.process.api.service
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.process.engine.service.PipelineRepositoryService
-import com.tencent.devops.process.service.PipelineUserService
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
@@ -13,7 +12,7 @@ class ServiceOperationResourceImpl @Autowired constructor(
 
     override fun getUpdateUser(pipelineId: String): Result<String> {
         val pipelineInfo = pipelineRepositoryService.getPipelineInfo(pipelineId)
-        if(pipelineInfo != null) {
+        if (pipelineInfo != null) {
             return Result(pipelineInfo!!.lastModifyUser)
         }
         return Result("")
