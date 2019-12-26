0d93bc407e6a959df8cf768216504c69fad8e6d0
freyzheng <freyzheng@tencent.com>
2019-12-26 22:31:23 +0800

    --story=856872287 【webhook】实现跨网络区域的webhook回调 修改参数来源。

--- src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/v2/ApigwCallBackResourceV2.kt
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/v2/ApigwCallBackResourceV2.kt
@@ -37,6 +37,7 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
@@ -55,7 +56,7 @@ interface ApigwCallBackResourceV2 {
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
         @ApiParam("projectId", required = true)
-        @QueryParam("projectId")
+        @PathParam("projectId")
         projectId: String,
         @ApiParam("url", required = true)
         @QueryParam("url")
--- src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/user/UserCallBackResource.kt
+++ src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/user/UserCallBackResource.kt
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
@@ -29,7 +30,7 @@ interface UserCallBackResource {
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
         @ApiParam("projectId", required = true)
-        @QueryParam("projectId")
+        @PathParam("projectId")
         projectId: String,
         @ApiParam("url", required = true)
         @QueryParam("url")
