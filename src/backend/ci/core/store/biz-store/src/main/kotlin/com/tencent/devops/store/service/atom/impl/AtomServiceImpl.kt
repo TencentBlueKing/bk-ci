8641c78f37a6538e146a857e633a71806b794392
carlyin <449036249@qq.com>
2019-12-26 11:35:31 +0800

    story: 流水线插件支持定制UI issue  #336

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/AtomServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/AtomServiceImpl.kt
@@ -29,6 +29,7 @@ package com.tencent.devops.store.service.atom.impl
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.tencent.devops.common.api.constant.CommonMessageCode
+import com.tencent.devops.common.api.enums.FrontendTypeEnum
 import com.tencent.devops.common.api.pojo.Page
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.api.util.DateTimeUtil
@@ -326,7 +327,8 @@ abstract class AtomServiceImpl @Autowired constructor() : AtomService {
                     weight = pipelineAtomRecord.weight,
                     props = atomDao.convertString(pipelineAtomRecord.props),
                     data = atomDao.convertString(pipelineAtomRecord.data),
-                    recommendFlag = atomFeature?.recommendFlag
+                    recommendFlag = atomFeature?.recommendFlag,
+                    frontendType = FrontendTypeEnum.getFrontendTypeObj(pipelineAtomRecord.htmlTemplateVersion)
                 )
             }
         )
