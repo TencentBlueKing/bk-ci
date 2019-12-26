fc7e31fa663957e807cada555e816aacca2c4a6b
freyzheng <freyzheng@tencent.com>
2019-12-26 22:17:44 +0800

    --story=856872287 【webhook】实现跨网络区域的webhook回调

--- src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/user/UserCallBackResource.kt
+++ src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/user/UserCallBackResource.kt
@@ -3,8 +3,8 @@ package com.tencent.devops.process.api.user
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.process.pojo.AccessRepository
-import com.tencent.devops.process.pojo.PipelineId
+import com.tencent.devops.common.pipeline.event.CallBackEvent
+import com.tencent.devops.process.pojo.pipeline.enums.CallBackNetWorkRegionType
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
@@ -21,18 +21,27 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 interface UserCallBackResource {
-    @ApiOperation("创建快速接入流水线")
+    @ApiOperation("创建callback回调")
     @POST
     @Path("/projects/{projectId}")
     fun create(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
+        @ApiParam("projectId", required = true)
+        @QueryParam("projectId")
+        projectId: String,
         @ApiParam("url", required = true)
         @QueryParam("url")
         url: String,
+        @ApiParam("region", required = true)
+        @QueryParam("region")
+        region: CallBackNetWorkRegionType,
         @ApiParam("event", required = true)
         @QueryParam("event")
-        event: String,
-    ): Result<PipelineId>
+        event: CallBackEvent,
+        @ApiParam("secretToken", required = false)
+        @QueryParam("secretToken")
+        secretToken: String?
+    ): Result<Boolean>
 }
---
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/api/user/UserCallBackResourceImpl.kt
@@ -0,0 +1,82 @@
+package com.tencent.devops.process.api.user
+
+import com.tencent.devops.common.api.exception.InvalidParamException
+import com.tencent.devops.common.api.exception.OperationException
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.BSAuthProjectApi
+import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
+import com.tencent.devops.common.auth.code.BSPipelineAuthServiceCode
+import com.tencent.devops.common.pipeline.event.CallBackEvent
+import com.tencent.devops.common.service.utils.MessageCodeUtil
+import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.process.engine.service.ProjectPipelineCallBackService
+import com.tencent.devops.process.pojo.ProjectPipelineCallBack
+import com.tencent.devops.process.pojo.pipeline.enums.CallBackNetWorkRegionType
+import com.tencent.devops.project.constant.ProjectMessageCode
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import java.net.URLEncoder
+import kotlin.text.Regex
+
+@RestResource
+class UserCallBackResourceImpl @Autowired constructor(
+    val projectPipelineCallBackService: ProjectPipelineCallBackService,
+    val bkAuthProjectApi: BSAuthProjectApi,
+    private val bsPipelineAuthServiceCode: BSPipelineAuthServiceCode
+) : UserCallBackResource {
+    companion object {
+        val logger = LoggerFactory.getLogger(this::class.java)
+    }
+
+    @Value("\${devopsGateway.idcProxy:}")
+    private var gatewayIDCProxy: String = ""
+
+    override fun create(
+        userId: String,
+        projectId: String,
+        url: String,
+        region: CallBackNetWorkRegionType,
+        event: CallBackEvent,
+        secretToken: String?
+    ): Result<Boolean> {
+        // 验证用户是否为管理员
+        if (!bkAuthProjectApi.isProjectUser(userId, bsPipelineAuthServiceCode, projectId, BkAuthGroup.MANAGER)) {
+            logger.info("create Project callback createUser is not project manager,createUser[$userId] projectId[$projectId]")
+            throw OperationException((MessageCodeUtil.getCodeLanMessage(ProjectMessageCode.NOT_MANAGER)))
+        }
+        // 验证url的合法性
+        val regex = Regex("^(?=^.{3,255}\$)(http(s)?:\\/\\/)?(www\\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+(:\\d+)*(\\/\\w+\\.\\w+)*([\\?&]\\w+=\\w*)*\$", RegexOption.IGNORE_CASE)
+        val regexResult = url.matches(regex)
+        if (!regexResult) {
+            throw InvalidParamException("URL param is invalid.")
+        }
+
+        val urlEncode = URLEncoder.encode(url, "UTF-8")
+        val callBackUrl = when (region) {
+            CallBackNetWorkRegionType.IDC -> url
+            CallBackNetWorkRegionType.OSS -> {
+                if (gatewayIDCProxy.isNotBlank()) {
+                    "$gatewayIDCProxy/proxy-oss?url=$urlEncode"
+                } else {
+                    url
+                }
+            }
+            CallBackNetWorkRegionType.DEVNET -> {
+                if (gatewayIDCProxy.isNotBlank()) {
+                    "$gatewayIDCProxy/proxy-devnet?url=$urlEncode"
+                } else {
+                    url
+                }
+            }
+        }
+        val projectPipelineCallBack = ProjectPipelineCallBack(
+            projectId = projectId,
+            callBackUrl = callBackUrl,
+            events = event.name,
+            secretToken = secretToken
+        )
+        projectPipelineCallBackService.createCallBack(userId, projectPipelineCallBack)
+        return Result(true)
+    }
+}
