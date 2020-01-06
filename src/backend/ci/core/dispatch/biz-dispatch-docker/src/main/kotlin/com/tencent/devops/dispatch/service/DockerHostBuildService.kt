198fa641cfcdd7c0febc12e3b8ac3e4a7d701767
rdeng <rdeng@tencent.com>
2020-01-06 16:05:51 +0800

    bugfix: Remove the useless logs

--- src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/service/DockerHostBuildService.kt
+++ src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/service/DockerHostBuildService.kt
@@ -291,7 +291,6 @@ class DockerHostBuildService @Autowired constructor(
         try {
             val gray = !grayFlag.isNullOrBlank() && grayFlag!!.toBoolean()
             val grayProjectSet = this.gray.grayProjectSet(redisOperation)
-            logger.info("Get the redis project set: $grayProjectSet")
             redisLock.lock()
             if (gray) {
                 // 优先取设置了IP的任务（可能是固定构建机，也可能是上次用的构建机）
