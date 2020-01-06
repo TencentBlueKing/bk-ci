a574be0c2775b421ed81cbebfecb2f8c39137a68
freyzheng <freyzheng@tencent.com>
2020-01-06 21:46:18 +0800

    --bug=77008377 【插件】bugly和UNITY3D旧插件缺少文件 修改buglyjar包的名字。

--- src/backend/ci/ext/tencent/worker/worker-api-tencent/src/main/kotlin/com/tencent/devops/plugin/worker/task/bugly/BuglyTask.kt
+++ src/backend/ci/ext/tencent/worker/worker-api-tencent/src/main/kotlin/com/tencent/devops/plugin/worker/task/bugly/BuglyTask.kt
@@ -316,7 +316,7 @@ class BuglyTask : ITask() {
 
     // 转化so文件
     private fun outputBuglyTool(jarName: String): URL {
-        val buglyToolInputMD5Stream = Thread.currentThread().contextClassLoader.getResourceAsStream(jarName)
+        val buglyToolInputMD5Stream = Thread.currentThread().contextClassLoader.getResourceAsStream("$jarName.file")
         val inputMD5 = DigestUtils.md5Hex(buglyToolInputMD5Stream)
         val outputFile = File(jarName)
         // 是否执行输出
