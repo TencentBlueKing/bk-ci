3f35e5429c6b1a8a92609b67f449023e13a5bb22
Royalhuang <royalhuang@tencent.com>
2020-01-06 17:52:14 +0800

    bugfix: 修复部分不应出现的系统错误信息 #507

--- src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/pojo/ErrorCode.kt
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/pojo/ErrorCode.kt
@@ -57,4 +57,8 @@ object ErrorCode {
     const val USER_TASK_OPERATE_FAIL = 16015102 // 插件执行过程出错
     const val USER_RESOURCE_NOT_FOUND = 16015103 // 找不到对应系统资源
     const val USER_JOB_OUTTIME_LIMIT = 16015104 // 用户Job排队超时（自行限制）
+
+    // 未知默认错误
+    const val UNKNOWN_DEFAULT_ERROR = 16015200 // 默认错误
+
 }
