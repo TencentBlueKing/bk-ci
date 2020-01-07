da8076ddbc431decda325ab8e6b9690c0f3eceed
rdeng <rdeng@tencent.com>
2020-01-07 13:12:47 +0800

    Revert

--- src/backend/ci/ext/tencent/log/biz-log-tencent/src/main/kotlin/com/tencent/devops/log/configuration/LogESAutoConfiguration.kt
+++ src/backend/ci/ext/tencent/log/biz-log-tencent/src/main/kotlin/com/tencent/devops/log/configuration/LogESAutoConfiguration.kt
@@ -26,14 +26,11 @@
 
 package com.tencent.devops.log.configuration
 
-import com.floragunn.searchguard.ssl.SearchGuardSSLPlugin
-import com.floragunn.searchguard.ssl.util.SSLConfigConstants
 import com.tencent.devops.common.es.ESProperties
 import com.tencent.devops.common.web.WebAutoConfiguration
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
-import org.elasticsearch.plugins.Plugin
 import org.elasticsearch.transport.client.PreBuiltTransportClient
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -55,64 +52,28 @@ class LogESAutoConfiguration {
     private val ip: String? = null
     @Value("\${elasticsearch.port}")
     private val port: Int? = 0
-    @Value("\${elasticsearch.cluster:#{null}}")
+    @Value("\${elasticsearch.cluster}")
     private val cluster: String? = null
-    @Value("\${elasticsearch.keystore.filePath:#{null}}")
-    private val keystoreFilePath: String? = null
-    @Value("\${elasticsearch.keystore.password:#{null}}")
-    private val keystorePassword: String? = null
-    @Value("\${elasticsearch.truststore.filePath:#{null}}")
-    private val truststoreFilePath: String? = null
-    @Value("\${elasticsearch.truststore.password:#{null}}")
-    private val truststorePassword: String? = null
 
     @Bean
     @Primary
     fun transportClient(): TransportClient {
-        if (ip.isNullOrBlank()) {
+        if (ip == null || ip!!.isBlank()) {
             throw IllegalArgumentException("ES集群地址尚未配置")
         }
         if (port == null || port!! <= 0) {
             throw IllegalArgumentException("ES集群端口尚未配置")
         }
-
-        val builder = Settings.builder()
-
-        var plugin: Class<out Plugin>? = null
-
-        if (!cluster.isNullOrBlank()) {
-            builder.put("cluster.name", cluster)
+        if (cluster == null || cluster!!.isBlank()) {
+            throw IllegalArgumentException("ES集群名称尚未配置")
         }
-
-        if (!keystoreFilePath.isNullOrBlank()) {
-            builder.put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_KEYSTORE_FILEPATH, keystoreFilePath)
-                .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_ENFORCE_HOSTNAME_VERIFICATION, false)
-                .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_ENABLED, true)
-                .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_ENFORCE_HOSTNAME_VERIFICATION_RESOLVE_HOST_NAME, true)
-        }
-        if (!truststoreFilePath.isNullOrBlank()) {
-            builder.put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_TRUSTSTORE_FILEPATH, truststoreFilePath)
-        }
-        if (!keystorePassword.isNullOrBlank()) {
-            builder.put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_KEYSTORE_PASSWORD, keystorePassword)
-        }
-        if (!truststorePassword.isNullOrBlank()) {
-            builder.put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_TRUSTSTORE_PASSWORD, truststorePassword)
-            plugin = SearchGuardSSLPlugin::class.java
-        }
-
-        val settings = builder.build()
+        val settings = Settings.builder().put("cluster.name", cluster).build()
         val ips = ip!!.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-
-        val client = if (plugin != null) {
-            PreBuiltTransportClient(settings, plugin)
-        } else {
-            PreBuiltTransportClient(settings)
-        }
+        val client = PreBuiltTransportClient(settings)
         for (ipAddress in ips) {
             client.addTransportAddress(InetSocketTransportAddress(InetAddress.getByName(ipAddress), port!!))
         }
-        logger.info("Init the log es transport client with host($ip:$port), cluster($cluster), keystore($keystoreFilePath|$keystorePassword), truststore($truststoreFilePath|$truststorePassword)")
+        logger.info("Init ES transport client with host($ip:$port) and cluster($cluster)")
         return client
     }
     companion object {
