433f23af55a8fde866272314126c8329e07b462a
cao150260 <fitzcao@tencent.com>
2020-01-08 15:16:40 +0800

    feature:实例维护session，timeout数据结构调整

--- src/backend/ci/core/websocket/biz-websocket/src/main/kotlin/com/tencent/devops/websocket/keys/WebsocketKeys.kt
+++ src/backend/ci/core/websocket/biz-websocket/src/main/kotlin/com/tencent/devops/websocket/keys/WebsocketKeys.kt
@@ -4,7 +4,7 @@ object WebsocketKeys {
     // session超时 redis桶数量
     const val REDIS_MO:Long = 1000
     // 记录登录态超时hash redis桶， BK:webSocket:hash:sessionId:timeOut:key:1-1000,所有的登录态被打散到1000个桶内
-    const val HASH_USER_TIMEOUT_REDIS_KEY = "BK:ws:sessionId:timeOut:hashBucket:key:"
+    const val HASH_USER_TIMEOUT_REDIS_KEY = "BK:wsSessionId:timeOut:hashBucket:key:"
     // 用户项目redis key
     const val PROJECT_USER_REDIS_KEY = "BK:websocket:project:user:key:"
 }
