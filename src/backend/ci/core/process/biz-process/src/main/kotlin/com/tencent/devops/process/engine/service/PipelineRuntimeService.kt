838538d5622d6d5102ba192c340ea667600e43cc
Royalhuang <royalhuang@tencent.com>
2020-01-10 15:32:13 +0800

    bugfix: 插件重试会导致构建重试出错 #556

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
@@ -1939,6 +1939,9 @@ class PipelineRuntimeService @Autowired constructor(
     }
 
     fun writeStartParam(projectId: String, pipelineId: String, buildId: String, model: Model) {
+        val allVariable = getAllVariable(buildId)
+        if (allVariable[PIPELINE_RETRY_COUNT] == null) return
+
         val triggerContainer = model.stages[0].containers[0] as TriggerContainer
 
         if (triggerContainer.buildNo != null) {
@@ -1948,25 +1951,23 @@ class PipelineRuntimeService @Autowired constructor(
                 buildId = buildId, varName = BUILD_NO, varValue = buildNo
             )
         }
-        // 写
-        if (triggerContainer.params.isNotEmpty()) {
-            // 只有在构建参数中的才设置
-            val allVariable = getAllVariable(buildId)
-            val params = allVariable.filter {
-                it.key.startsWith(SkipElementUtils.prefix) || it.key == BUILD_NO || it.key == PIPELINE_RETRY_COUNT
-            }.plus(triggerContainer.params.map {
+        // 只有在构建参数中的才设置
+        val params = allVariable.filter {
+            it.key.startsWith(SkipElementUtils.prefix) || it.key == BUILD_NO || it.key == PIPELINE_RETRY_COUNT
+        }
+        if (triggerContainer.params.isNotEmpty())
+            params.plus(triggerContainer.params.map {
                 if (allVariable.containsKey(it.id)) { // 做下真实传值的替换
                     it.id to allVariable[it.id]
                 } else {
                     it.id to it.defaultValue
                 }
             }.toMap())
-            buildStartupParamService.addParam(
-                projectId = projectId,
-                pipelineId = pipelineId,
-                buildId = buildId,
-                param = JsonUtil.getObjectMapper().writeValueAsString(params)
-            )
-        }
+        buildStartupParamService.addParam(
+            projectId = projectId,
+            pipelineId = pipelineId,
+            buildId = buildId,
+            param = JsonUtil.getObjectMapper().writeValueAsString(params)
+        )
     }
 }
