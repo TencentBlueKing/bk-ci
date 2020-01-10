14f700f045a80bd770377f1628425ac5189b1eea
xuzhan <xuzhan@tencent.com>
2020-01-10 15:41:18 +0800

    历史镜像版本和选择镜像版本不同

--- src/frontend/devops-pipeline/src/components/ContainerPropertyPanel/index.vue
+++ src/frontend/devops-pipeline/src/components/ContainerPropertyPanel/index.vue
@@ -500,10 +500,11 @@
                     imageName: card.name
                 }))
                 return this.getVersionList(card.code).then(() => {
-                    const firstVersion = this.versionList[0] || {}
+                    let chooseVersion = this.versionList[0] || {}
+                    if (card.version) chooseVersion = this.versionList.find(x => x.versionValue === card.version) || {}
                     this.handleContainerChange('dispatchType', Object.assign({
                         ...this.container.dispatchType,
-                        imageVersion: firstVersion.versionValue,
+                        imageVersion: chooseVersion.versionValue,
                         value: card.code
                     }))
                 })
