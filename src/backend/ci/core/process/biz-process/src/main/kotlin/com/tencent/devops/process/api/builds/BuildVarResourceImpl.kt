6c1013b147bffb466a30641ed393b1ae822d9c63
warmli <957569558@qq.com>
2020-01-10 20:57:46 +0800

    feature:支持获取构建参数

---
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/builds/BuildVarResourceImpl.kt
@@ -0,0 +1,24 @@
+package com.tencent.devops.process.api.builds
+
+import com.tencent.devops.common.api.exception.ParamBlankException
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.process.engine.service.PipelineRuntimeService
+
+import org.apache.commons.lang.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+
+@RestResource
+class BuildVarResourceImpl @Autowired constructor(
+    private val pipelineRuntimeService: PipelineRuntimeService
+) : BuildVarResource {
+    override fun getBuildVar(buildId: String): Result<Map<String, String>> {
+        checkParam(buildId = buildId)
+        return Result(pipelineRuntimeService.getAllVariable(buildId))
+    }
+
+    fun checkParam(buildId: String) {
+        if (StringUtils.isBlank(buildId))
+            throw ParamBlankException("build Id is null or blank")
+    }
+}
