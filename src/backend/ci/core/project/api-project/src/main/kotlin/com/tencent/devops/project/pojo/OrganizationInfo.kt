04fcdad340c64d9e704becdf2304839aac73832e
warmli <957569558@qq.com>
2020-01-15 22:10:19 +0800

    feature:【研发商店工作台】可见范围审核调整

--- src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/OrganizationInfo.kt
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/OrganizationInfo.kt
@@ -29,4 +29,8 @@ package com.tencent.devops.project.pojo
 data class OrganizationInfo(
     val ID: String,
     val Name: String
-)
+) {
+    override fun toString(): String {
+        return "$ID $Name"
+    }
+}
--- src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreVisibleDeptServiceImpl.kt
+++ src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreVisibleDeptServiceImpl.kt
@@ -109,6 +109,7 @@ class StoreVisibleDeptServiceImpl @Autowired constructor(
         val resourceClient = client.get(ServiceProjectOrganizationResource::class)
         val result = resourceClient.getOrganizations(userId, OrganizationType.bg, 0)
         val deptInfoList = result.data
+        logger.info("deptInfo List: $deptInfoList")
         val approveList = mutableListOf<Int>()
         deptInfoList?.forEach {
             approveList.add(Integer.parseInt(it.ID))
