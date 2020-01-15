880ca3ae89cbb5257609468a4f705a05a29f822d
warmli <957569558@qq.com>
2020-01-15 22:17:04 +0800

    feature:【研发商店工作台】可见范围审核调整

--- src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/OrganizationInfo.kt
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/OrganizationInfo.kt
@@ -27,8 +27,8 @@
 package com.tencent.devops.project.pojo
 
 data class OrganizationInfo(
-    val ID: String,
-    val Name: String
+    val ID: String?,
+    val Name: String?
 ) {
     override fun toString(): String {
         return "$ID $Name"
