96bc8527cc0b84d11b41a43719f939fc91c52d00
terlinhe <terlinhe@tencent.com>
2020-02-12 15:06:04 +0800

    历史列表页移除终止构建、详情页增加终止构建

--- src/frontend/devops-pipeline/src/views/subpages/exec_detail.vue
+++ src/frontend/devops-pipeline/src/views/subpages/exec_detail.vue
@@ -18,6 +18,7 @@
                         </template>
                         <span v-else :class="{ [execDetail.status]: execDetail.status }">{{ getStatusLabel(execDetail.status) }}</span>
                         <i v-if="showRetryIcon" title="rebuild" class="bk-icon icon-retry" @click.stop="retry(execDetail.id, true)"></i>
+                        <i v-else :title="$t('history.stopBuild')" class="bk-icon icon-stop-shape" @click.stop="stopExecute(execDetail.id)"></i>
                     </div>
                     <div class="info-item">
                         <span class="item-label">{{ $t('details.executor') }}：</span>
@@ -536,6 +537,15 @@
                     font-size: 20px;
                     color: $primaryColor;
                     cursor: pointer;
+                }
+                .icon-stop-shape {
+                    font-size: 15px;
+                    color: $primaryColor;
+                    cursor: pointer;
+                    border: 1px solid $primaryColor;
+                    padding: 1px;
+                    border-radius: 50%;
+                    margin-left: 3px;
                 }
             }
         }
