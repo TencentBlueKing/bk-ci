b8f84fdaf9b62ab560e14744590fed79142ee382
尹超 <449036249@qq.com>
2020-02-14 21:08:31 +0800

    depend:调整研发商店容器镜像工程依赖 issue #690

--- src/backend/ci/core/store/biz-store-image/src/main/kotlin/com/tencent/devops/store/service/image/ImageNotifyService.kt
+++ src/backend/ci/core/store/biz-store-image/src/main/kotlin/com/tencent/devops/store/service/image/ImageNotifyService.kt
@@ -28,7 +28,6 @@ package com.tencent.devops.store.service.image
 import com.tencent.devops.store.pojo.common.enums.AuditTypeEnum
 import org.springframework.stereotype.Service
 
-@Service
 interface ImageNotifyService {
 
     /**
