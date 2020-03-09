e91581d52539f04df10909a19cc7f73e84be40c8
irwinsun <irwinsun@tencent.com>
2020-03-09 23:39:57 +0800

    refactor: 规范sql脚本 issue #782

--- support-files/sql/0004_ci_dispatch-update_v2.1.0_mysql.sql
+++ support-files/sql/0004_ci_dispatch-update_v2.1.0_mysql.sql
@@ -195,6 +195,41 @@ BEGIN
             ADD INDEX `HOST_TAG` (`HOST_TAG`);
     END IF;
 
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_DISPATCH_PIPELINE_DOCKER_DEBUG'
+                    AND COLUMN_NAME = 'IMAGE_PUBLIC_FLAG') THEN
+        ALTER TABLE T_DISPATCH_PIPELINE_DOCKER_DEBUG
+            ADD COLUMN `IMAGE_PUBLIC_FLAG` bit(1) DEFAULT NULL COMMENT '镜像是否为公共镜像：0否1是';
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_DISPATCH_PIPELINE_DOCKER_DEBUG'
+                    AND COLUMN_NAME = 'IMAGE_RD_TYPE') THEN
+        ALTER TABLE T_DISPATCH_PIPELINE_DOCKER_DEBUG
+            ADD COLUMN `IMAGE_RD_TYPE` tinyint(1) DEFAULT NULL COMMENT '镜像研发来源：0自研1第三方';
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_DISPATCH_PIPELINE_DOCKER_TASK'
+                    AND COLUMN_NAME = 'IMAGE_PUBLIC_FLAG') THEN
+        ALTER TABLE T_DISPATCH_PIPELINE_DOCKER_TASK
+            ADD COLUMN `IMAGE_PUBLIC_FLAG` bit(1) DEFAULT NULL COMMENT '镜像是否为公共镜像：0否1是';
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_DISPATCH_PIPELINE_DOCKER_TASK'
+                    AND COLUMN_NAME = 'IMAGE_RD_TYPE') THEN
+        ALTER TABLE T_DISPATCH_PIPELINE_DOCKER_TASK
+            ADD COLUMN `IMAGE_RD_TYPE` tinyint(1) DEFAULT NULL COMMENT '镜像研发来源：0自研1第三方';
+    END IF;
 
     COMMIT;
 END <CI_UBF>
