a2febc132bfa72677920b5fdcd74cd8920bde11b
lockiechen <lockiechen@tencent.com>
2020-03-11 21:30:43 +0800

    feature: 添加复制stage以及删除stage功能 modify style issue #115

--- src/frontend/devops-pipeline/src/components/StagePropertyPanel/StageControl.vue
+++ src/frontend/devops-pipeline/src/components/StagePropertyPanel/StageControl.vue
@@ -24,19 +24,18 @@
                     <i v-bk-tooltips="$t('stageReviewDesc')" class="bk-icon icon-info-circle" />
                 </bk-form-item>
                 <template v-if="manualTrigger">
-                    <bk-form-item class="stage-trigger-member-input" :label="$t('stageUserTriggers')" :required="true" :class="{ 'is-error': !hasTriggerMember }" :desc="$t('stageTriggerDesc')">
+                    
+                    <form-field :required="true" :label="$t('stageUserTriggers')" :is-error="!hasTriggerMember" :desc="$t('stageTriggerDesc')" :error-msg="$t('editPage.stageManualTriggerUserNoEmptyTips')">
                         <bk-input :clearable="true" :disabled="disabled" v-model="triggerUsers"></bk-input>
-                        <p v-if="!hasTriggerMember" class="mt5 mb0">{{ $t('editPage.stageManualTriggerUserNoEmptyTips') }}</p>
-                    </bk-form-item>
-                
-                    <bk-form-item class="stage-timeout-input" :label="$t('stageTimeoutLabel')" :required="true" :class="{ 'is-error': !validTimeout }" :desc="$t('stageTimeoutDesc')">
+                    </form-field>
+
+                    <form-field :required="true" :label="$t('stageTimeoutLabel')" :is-error="!validTimeout" :desc="$t('stageTimeoutDesc')" :error-msg="$t('stageTimeoutError')">
                         <bk-input type="number" :disabled="disabled" v-model="timeout" :min="1" :max="720">
                             <template slot="append">
                                 <div class="group-text">{{ $t('timeMap.hours') }}</div>
                             </template>
                         </bk-input>
-                        <p v-if="!validTimeout" class="mt5 mb0">{{ $t('stageTimeoutError') }}</p>
-                    </bk-form-item>
+                    </form-field>
                 </template>
                 <bk-form-item :label="$t('stageOptionLabel')">
                     <bk-select :disabled="disabled" v-model="stageCondition" searchable>
@@ -59,11 +58,13 @@
     import { mapActions } from 'vuex'
     import Accordion from '@/components/atomFormField/Accordion'
     import KeyValueNormal from '@/components/atomFormField/KeyValueNormal'
+    import FormField from '@/components/AtomPropertyPanel/FormField'
     export default {
         name: 'stage-control',
         components: {
             Accordion,
-            KeyValueNormal
+            KeyValueNormal,
+            FormField
         },
         props: {
             stageControl: {
