06ecc2deadcc7b3334481e5730fda6207334f9a9
fitzcao <fitzcao@tencent.com>
2020-03-13 23:14:02 +0800

    --story=856902347 上架无发布类型

--- src/backend/ci/ext/tencent/store/api-store-service/src/main/kotlin/com/tencent/devops/store/pojo/dto/SubmitDTO.kt
+++ src/backend/ci/ext/tencent/store/api-store-service/src/main/kotlin/com/tencent/devops/store/pojo/dto/SubmitDTO.kt
@@ -31,7 +31,7 @@ data class SubmitDTO(
     @ApiModelProperty("是否是最后版本")
     val latestFlag: Int? = 0,
     @ApiModelProperty("发布类型，NEW：新上架 INCOMPATIBILITY_UPGRADE：非兼容性升级 COMPATIBILITY_UPGRADE：兼容性功能更新 COMPATIBILITY_FIX：兼容性问题修正", required = true)
-    val releaseType: ReleaseTypeEnum,
+    val releaseType: ReleaseTypeEnum? = ReleaseTypeEnum.NEW,
     @ApiModelProperty("版本日志内容", required = true)
     val versionContent: String? = "",
     @ApiModelProperty("扩展点列表")
