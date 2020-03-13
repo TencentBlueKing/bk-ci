385861d6569085628aab2000c6a291db12bef736
freyzheng <freyzheng@tencent.com>
2020-03-14 01:11:48 +0800

    --story=857514099 【openapi】支持bk_ticket的验证方式请求蓝盾apigw接口

--- src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/aspect/ApiAsept.kt
+++ src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/aspect/ApiAsept.kt
@@ -13,51 +13,60 @@ class ApiAspect {
     companion object {
         private val logger = LoggerFactory.getLogger(ApiFilter::class.java)
     }
-    
+
     /**
      * 前置增强：目标方法执行之前执行
      *
      * @param jp
      */
-    @Before("execution(* com.tencent.devops.openapi.resources.*.*(..))") // 所有controller包下面的所有方法的所有参数
+    @Before(
+        "execution(* com.tencent.devops.openapi.resources.*.*(..))"
+            + "||execution(* com.tencent.devops.openapi.resources.v2.*.*(..))"
+            + "||execution(* com.tencent.devops.openapi.resources.v2.app.*.*(..))"
+            + "||execution(* com.tencent.devops.openapi.resources.v2.user*.*(..))"
+    ) // 所有controller包下面的所有方法的所有参数
     fun beforeMethod(jp: JoinPoint) {
         val methodName: String = jp.signature.name
-        logger.info("【前置增强】the method 【" + methodName + "】 begins with " + jp.args)
+        logger.info("【前置增强】the method 【$methodName】")
+        val args = jp.args
+        args.forEach {
+            logger.info("【前置增强】the arg: 【$it】")
+        }
     }
-
-    /**
-     * 后置增强：目标方法执行之后执行以下方法体的内容，不管是否发生异常。
-     *
-     * @param jp
-     */
-    @After("execution(* com.tencent.devops.openapi.resources.*.*(..)))")
-    fun afterMethod(jp: JoinPoint?) {
-        logger.info("【后置增强】this is a afterMethod advice...")
-    }
-
-    /**
-     * 返回增强：目标方法正常执行完毕时执行
-     *
-     * @param jp
-     * @param result
-     */
-    @AfterReturning(value = "execution(* com.tencent.devops.openapi.resources.*.*(..)))", returning = "result")
-    fun afterReturningMethod(jp: JoinPoint, result: Any) {
-        val methodName: String = jp.signature.name
-        logger.info("【返回增强】the method 【$methodName】 ends with 【$result】")
-    }
-
-    /**
-     * 异常增强：目标方法发生异常的时候执行，第二个参数表示补货异常的类型
-     *
-     * @param jp
-     * @param e
-     */
-    @AfterThrowing(value = "execution(* com.tencent.devops.openapi.resources.*.*(..))", throwing = "e")
-    fun afterThorwingMethod(jp: JoinPoint, e: Exception?) {
-        val methodName: String = jp.signature.name
-        logger.error("【异常增强】the method 【$methodName】 occurs exception: ", e)
-    }
+//
+//    /**
+//     * 后置增强：目标方法执行之后执行以下方法体的内容，不管是否发生异常。
+//     *
+//     * @param jp
+//     */
+//    @After("execution(* com.tencent.devops.openapi.resources.*.*(..))")
+//    fun afterMethod(jp: JoinPoint?) {
+//        logger.info("【后置增强】this is a afterMethod advice...")
+//    }
+//
+//    /**
+//     * 返回增强：目标方法正常执行完毕时执行
+//     *
+//     * @param jp
+//     * @param result
+//     */
+//    @AfterReturning(value = "execution(* com.tencent.devops.openapi.resources.*.*(..)))", returning = "result")
+//    fun afterReturningMethod(jp: JoinPoint, result: Any) {
+//        val methodName: String = jp.signature.name
+//        logger.info("【返回增强】the method 【$methodName】 ends with 【$result】")
+//    }
+//
+//    /**
+//     * 异常增强：目标方法发生异常的时候执行，第二个参数表示补货异常的类型
+//     *
+//     * @param jp
+//     * @param e
+//     */
+//    @AfterThrowing(value = "execution(* com.tencent.devops.openapi.resources.*.*(..))", throwing = "e")
+//    fun afterThorwingMethod(jp: JoinPoint, e: Exception?) {
+//        val methodName: String = jp.signature.name
+//        logger.error("【异常增强】the method 【$methodName】 occurs exception: ", e)
+//    }
     /**
      * 环绕增强：目标方法执行前后分别执行一些代码，发生异常的时候执行另外一些代码
      *
