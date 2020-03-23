6be19b7b1f77c3501c261b76116d0f5c7ee85868
fitzcao <fitzcao@tencent.com>
2020-03-23 21:34:42 +0800

    feature: 新增构件分发功能到服务 #902

--- src/backend/ci/core/artifactory/biz-artifactory-push/src/main/kotlin/com/tencent/devops/artifactory/resources/ServiceFilePushResourceImpl.kt
+++ src/backend/ci/core/artifactory/biz-artifactory-push/src/main/kotlin/com/tencent/devops/artifactory/resources/ServiceFilePushResourceImpl.kt
@@ -2,6 +2,7 @@ package com.tencent.devops.artifactory.resources
 
 import com.tencent.devops.artifactory.api.ServiceFilePushResource
 import com.tencent.devops.artifactory.pojo.dto.PushFileDTO
+import com.tencent.devops.artifactory.pojo.vo.PushResultVO
 import com.tencent.devops.artifactory.service.PushFileServiceExt
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
@@ -11,7 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired
 class ServiceFilePushResourceImpl @Autowired constructor(
     private val pushFileService: PushFileServiceExt
 ) : ServiceFilePushResource {
-    override fun pushFile(userId: String, pushInfo: PushFileDTO): Result<Boolean?> {
+    override fun pushFile(userId: String, pushInfo: PushFileDTO): Result<Long?> {
         return pushFileService.pushFileByJob(userId, pushInfo.remoteResourceInfo, pushInfo.fileInfo)
     }
+
+    override fun checkPushStatus(userId: String, projectId: String, jobInstanceId: Long): Result<PushResultVO?> {
+        return pushFileService.checkStatus(userId, projectId, jobInstanceId)
+    }
+
 }
