9880e7371e09723ea49834924a862e249f72bb22
irwinsun <irwinsun@tencent.com>
2020-03-26 01:08:11 +0800

    refactor: 迁移子流水线的接口 #926 严格校验，防止异常数据

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/engine/pojo/BuildInfo.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/engine/pojo/BuildInfo.kt
@@ -52,11 +52,14 @@ data class BuildInfo(
 ) {
 
     fun isFinish() = when {
-        status.name == BuildStatus.STAGE_SUCCESS.name && endTime != null -> true
+        status.name == BuildStatus.STAGE_SUCCESS.name && endTime != null && endTime > 0 && startTime != null && endTime > startTime -> true
         else -> BuildStatus.isFinish(status)
     }
 
-    fun isSuccess() = BuildStatus.isSuccess(status) && endTime != null
+    fun isSuccess() = when {
+        status.name == BuildStatus.STAGE_SUCCESS.name && endTime != null && endTime > 0 && startTime != null && endTime > startTime -> true
+        else -> BuildStatus.isSuccess(status)
+    }
 
     fun isFailure() = BuildStatus.isFailure(status)
 
