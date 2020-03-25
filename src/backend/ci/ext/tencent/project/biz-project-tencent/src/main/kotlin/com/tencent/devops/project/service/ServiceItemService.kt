ac8b884b47bca0a61b22b25f6ff86f6db57b8231
fitzcao <fitzcao@tencent.com>
2020-03-25 23:30:19 +0800

    --story=856893779  详情返回labelIdList

--- src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/ServiceItemService.kt
+++ src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/ServiceItemService.kt
@@ -20,9 +20,7 @@ import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.lang.RuntimeException
 import javax.annotation.PostConstruct
-import kotlin.RuntimeException
 import com.tencent.devops.project.api.pojo.ExtServiceEntity as ExtServiceEntity
 
 @Service
