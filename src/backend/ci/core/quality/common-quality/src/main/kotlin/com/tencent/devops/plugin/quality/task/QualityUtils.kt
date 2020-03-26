943ec0fa8be6a8176443f4561741954547966815
linjun-ddddd <654548718@qq.com>
2020-03-26 22:18:22 +0800

    feature: Codecc红线轮询修改 #988

--- src/backend/ci/core/quality/common-quality/src/main/kotlin/com/tencent/devops/plugin/quality/task/QualityUtils.kt
+++ src/backend/ci/core/quality/common-quality/src/main/kotlin/com/tencent/devops/plugin/quality/task/QualityUtils.kt
@@ -26,23 +26,36 @@
 
 package com.tencent.devops.plugin.quality.task
 
+import com.tencent.devops.common.api.exception.TaskExecuteException
+import com.tencent.devops.common.api.pojo.ErrorCode
+import com.tencent.devops.common.api.pojo.ErrorType
+import com.tencent.devops.common.api.util.timestamp
 import com.tencent.devops.common.client.Client
+import com.tencent.devops.process.engine.pojo.PipelineBuildTask
+import com.tencent.devops.process.utils.PIPELINE_BUILD_NUM
 import com.tencent.devops.quality.api.v2.ServiceQualityRuleResource
 import com.tencent.devops.quality.api.v2.pojo.request.BuildCheckParams
 import com.tencent.devops.quality.pojo.RuleCheckResult
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
 object QualityUtils {
 
     private val logger = LoggerFactory.getLogger(QualityUtils::class.java)
 
-    fun getAuditUserList(client: Client, projectId: String, pipelineId: String, buildId: String, taskId: String): Set<String> {
+    fun getAuditUserList(
+        client: Client,
+        projectId: String,
+        pipelineId: String,
+        buildId: String,
+        taskId: String
+    ): Set<String> {
         return try {
             client.get(ServiceQualityRuleResource::class).getAuditUserList(
-                    projectId,
-                    pipelineId,
-                    buildId,
-                    taskId
+                projectId,
+                pipelineId,
+                buildId,
+                taskId
             ).data ?: setOf()
         } catch (e: Exception) {
             logger.error("quality get audit user list fail: ${e.message}", e)
@@ -50,18 +63,76 @@ object QualityUtils {
         }
     }
 
-    fun check(client: Client, buildCheckParams: BuildCheckParams): RuleCheckResult {
+    private fun check(client: Client, buildCheckParams: BuildCheckParams): RuleCheckResult {
         return try {
             client.getWithoutRetry(ServiceQualityRuleResource::class).check(
-                    buildCheckParams
+                buildCheckParams
             ).data!!
         } catch (e: Exception) {
             logger.error("quality get audit user list fail: ${e.message}", e)
             return RuleCheckResult(
-                    true,
-                    true,
-                    15 * 6000,
-                    listOf()
+                true,
+                true,
+                15 * 6000,
+                listOf()
+            )
+        }
+    }
+
+    fun getCheckResult(
+        task: PipelineBuildTask,
+        interceptTaskName: String?,
+        interceptTask: String?,
+        runVariables: Map<String, String>,
+        client: Client,
+        position: String
+    ): RuleCheckResult {
+
+        val pipelineId = task.pipelineId
+        val projectId = task.projectId
+        val buildId = task.buildId
+        val templateId = task.templateId
+        val buildNo = runVariables[PIPELINE_BUILD_NUM].toString()
+
+        if (interceptTask == null) {
+            logger.error("Fail to find quality gate intercept element")
+            throw TaskExecuteException(
+                errorCode = ErrorCode.USER_RESOURCE_NOT_FOUND,
+                errorType = ErrorType.USER,
+                errorMsg = "Fail to find quality gate intercept element"
+            )
+        }
+
+        return try {
+            val buildCheckParams = BuildCheckParams(
+                projectId = projectId,
+                pipelineId = pipelineId,
+                buildId = buildId,
+                buildNo = buildNo,
+                interceptTaskName = interceptTaskName ?: "",
+                startTime = LocalDateTime.now().timestamp(),
+                taskId = interceptTask,
+                position = position,
+                templateId = templateId,
+                runtimeVariable = runVariables
+            )
+            if (QUALITY_CODECC_LAZY_ATOM.contains(interceptTask)) {
+                for (gap in QUALITY_LAZY_TIME_GAP) {
+                    val hisMetadata = client.get(ServiceQualityRuleResource::class).getHisMetadata(buildId).data ?: listOf()
+                    val hasMetadata = hisMetadata.any { it.elementType in QUALITY_CODECC_LAZY_ATOM }
+                    if (hasMetadata) break
+                    Thread.sleep(gap * 1000L)
+                }
+                check(client, buildCheckParams)
+            } else {
+                check(client, buildCheckParams)
+            }
+        } catch (t: Throwable) {
+            logger.error("Quality Gate check in fail", t)
+            throw TaskExecuteException(
+                errorCode = ErrorCode.USER_TASK_OPERATE_FAIL,
+                errorType = ErrorType.USER,
+                errorMsg = "质量红线(准入)检测失败"
             )
         }
     }
