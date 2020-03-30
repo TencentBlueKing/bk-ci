9d9fc1f7df738c287508e5ab3f3471f3ba0ba33d
royalhuang <royalhuang@tencent.com>
2020-03-30 10:03:08 +0800

    feature: 日志组件优化相关功能 issue #678

--- src/backend/ci/core/log/biz-log/src/main/kotlin/com/tencent/devops/log/service/v2/LogServiceV2.kt
+++ src/backend/ci/core/log/biz-log/src/main/kotlin/com/tencent/devops/log/service/v2/LogServiceV2.kt
@@ -1166,7 +1166,7 @@ class LogServiceV2 @Autowired constructor(
             logger.info("logs query time cost($type): ${System.currentTimeMillis() - startTime}")
             queryLogs.logs.addAll(logs)
             moreLogs.logs.addAll(logs)
-            moreLogs.hasMore = moreLogs.logs.size >= Constants.MAX_LINES
+            moreLogs.hasMore = moreLogs.logs.size >= Constants.MAX_LINES * times
         } catch (ex: IndexNotFoundException) {
             logger.error("Query after logs failed because of IndexNotFoundException. buildId: $buildId", ex)
             queryLogs.status = LogStatus.CLEAN
