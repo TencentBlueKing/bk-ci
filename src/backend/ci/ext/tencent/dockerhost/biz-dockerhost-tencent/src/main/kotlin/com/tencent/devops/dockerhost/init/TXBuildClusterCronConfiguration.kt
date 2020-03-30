b617b63f38bfab8793f82082b01310dfb88fc3a2
sawyersong2 <wudiduomjimo@gmail.com>
2020-03-30 15:40:32 +0800

    --story=857532129 改造devnet构建机分发方案，避免母机漂移

--- src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/init/TXBuildClusterCronConfiguration.kt
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/init/TXBuildClusterCronConfiguration.kt
@@ -37,7 +37,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.SchedulingConfigurer
-import org.springframework.scheduling.config.IntervalTask
 import org.springframework.scheduling.config.ScheduledTaskRegistrar
 import java.util.concurrent.Executors
 
