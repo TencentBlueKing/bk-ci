ef9a414340c9741b86533be036f348a4f34f0fbc
fitzcao <fitzcao@tencent.com>
2020-04-04 13:16:49 +0800

    --story=856902347 格式化

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/engine/atom/task/ExperienceTaskAtom.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/engine/atom/task/ExperienceTaskAtom.kt
@@ -26,7 +26,6 @@
 
 package com.tencent.devops.process.engine.atom.task
 
-
 import com.tencent.devops.artifactory.api.service.ServiceArtifactoryResource
 import com.tencent.devops.common.api.pojo.ErrorCode
 import com.tencent.devops.common.api.pojo.ErrorType
