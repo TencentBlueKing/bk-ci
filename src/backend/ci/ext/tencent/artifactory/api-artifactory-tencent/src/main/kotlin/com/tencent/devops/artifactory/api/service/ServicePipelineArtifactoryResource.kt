25ee8f23223ffc77aaaeb6410e8230c9060c1f6b
sawyersong2 <wudiduomjimo@gmail.com>
2020-04-15 11:57:15 +0800

    --task=7181321 解决类名重复导致 feign client 404

--- src/backend/ci/ext/tencent/artifactory/api-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/api/service/ServicePipelineArtifactoryResource.kt
+++ src/backend/ci/ext/tencent/artifactory/api-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/api/service/ServicePipelineArtifactoryResource.kt
@@ -24,38 +24,45 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.tencent.devops.artifactory.resources.service
+package com.tencent.devops.artifactory.api.service
 
-import com.tencent.devops.artifactory.api.service.ServicePipelineResource
 import com.tencent.devops.artifactory.pojo.enums.Permission
-import com.tencent.devops.artifactory.service.PipelineService
-import com.tencent.devops.common.api.exception.ParamBlankException
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.auth.api.AuthPermission
-import com.tencent.devops.common.web.RestResource
-import org.springframework.beans.factory.annotation.Autowired
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
 
-@RestResource
-class ServicePipelineResourceImpl @Autowired constructor(
-    private val pipelineService: PipelineService
-) : ServicePipelineResource {
-    override fun hasPermission(userId: String, projectId: String, pipelineId: String, permission: Permission): Result<Boolean> {
-        if (userId.isBlank()) {
-            throw ParamBlankException("Invalid userId")
-        }
-        if (projectId.isBlank()) {
-            throw ParamBlankException("Invalid projectId")
-        }
-        if (pipelineId.isBlank()) {
-            throw ParamBlankException("Invalid pipelineId")
-        }
-        val bkAuthPermission = when (permission) {
-            Permission.VIEW -> AuthPermission.VIEW
-            Permission.EDIT -> AuthPermission.EDIT
-            Permission.SHARE -> AuthPermission.SHARE
-            Permission.LIST -> AuthPermission.LIST
-            Permission.EXECUTE -> AuthPermission.EXECUTE
-        }
-        return Result(pipelineService.hasPermission(userId, projectId, pipelineId, bkAuthPermission))
-    }
+@Api(tags = ["SERVICE_PIPELINE"], description = "版本仓库-流水线目录")
+@Path("/service/pipelines/artifactory")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+interface ServicePipelineArtifactoryResource {
+    @ApiOperation("获取有目录权限")
+    // @Path("/projects/{projectId}/pipelines/{pipelineId}/hasPermission")
+    @Path("/{projectId}/{pipelineId}/hasPermission")
+    @GET
+    fun hasPermission(
+        @ApiParam("用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam("路径", required = false)
+        @PathParam("pipelineId")
+        pipelineId: String,
+        @ApiParam("权限", required = true)
+        @QueryParam("permission")
+        permission: Permission
+    ): Result<Boolean>
 }
--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceService.kt
@@ -29,13 +29,12 @@ package com.tencent.devops.experience.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.artifactory.api.service.ServiceArtifactoryResource
-import com.tencent.devops.artifactory.api.service.ServicePipelineResource
+import com.tencent.devops.artifactory.api.service.ServicePipelineArtifactoryResource
 import com.tencent.devops.artifactory.pojo.enums.Permission
 import com.tencent.devops.common.api.constant.CommonMessageCode
 import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.util.HashUtil
 import com.tencent.devops.common.api.util.ShaUtils
-import com.tencent.devops.common.api.util.timestamp
 import com.tencent.devops.common.archive.constant.ARCHIVE_PROPS_APP_BUNDLE_IDENTIFIER
 import com.tencent.devops.common.archive.constant.ARCHIVE_PROPS_APP_VERSION
 import com.tencent.devops.common.archive.constant.ARCHIVE_PROPS_BUILD_NO
@@ -125,7 +124,7 @@ class ExperienceService @Autowired constructor(
             )
         }
         val pipelineId = propertyMap[ARCHIVE_PROPS_PIPELINE_ID]!!
-        return client.get(ServicePipelineResource::class).hasPermission(
+        return client.get(ServicePipelineArtifactoryResource::class).hasPermission(
             userId = userId,
             projectId = projectId,
             pipelineId = pipelineId,
