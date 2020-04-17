cf40b65fa68f4e3af3e04a3adebc9dd979350ee8
sawyersong2 <wudiduomjimo@gmail.com>
2020-04-17 15:04:27 +0800

    feature：改造devnet构建机分发方案，避免母机漂移 #845

--- src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/service/dispatcher/docker/DockerDispatcher.kt
+++ src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/service/dispatcher/docker/DockerDispatcher.kt
@@ -95,12 +95,15 @@ class DockerDispatcher @Autowired constructor(
                 dockerIp = if (specialIpSet.isNotEmpty()) {
                     // 在专机列表中
                     if (specialIpSet.contains(taskHistory.dockerIp)) {
+                        logger.info("condition 1 =====> $specialIpSet")
                         checkAndSetIP(pipelineAgentStartupEvent, specialIpSet, taskHistory.dockerIp)
                     } else {
                         // 不在专机列表中，重新依据专机列表去选择
+                        logger.info("condition 2 =====> $specialIpSet")
                         resetDockerIp(pipelineAgentStartupEvent, specialIpSet, taskHistory.dockerIp, "专机漂移")
                     }
                 } else {
+                    logger.info("condition 3 =====> $specialIpSet")
                     checkAndSetIP(pipelineAgentStartupEvent, specialIpSet, taskHistory.dockerIp)
                 }
             } else {
