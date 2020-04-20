0d07c2f6124c7598f55a93c174665766e03d23ea
xuzhan <xuzhan@tencent.com>
2020-04-20 15:00:25 +0800

    feature: 修改yaml字体的颜色 issue #1068

--- src/frontend/devops-atomstore/src/components/common/detail-info/atom.vue
+++ src/frontend/devops-atomstore/src/components/common/detail-info/atom.vue
@@ -10,7 +10,7 @@
                         <span class="approve-msg">{{approveMsg}}</span>
                     </h5>
                 </template>
-                <h5 :title="$t('store.支持通过YAML的方式使用')" class="not-public" v-if="detail.yamlFlag">
+                <h5 :title="$t('store.支持通过YAML的方式使用')" class="nomal-title" v-if="detail.yamlFlag">
                     <icon class="detail-img" name="yaml" size="16" />
                     <span class="approve-msg">{{$t('store.YAML可用')}}</span>
                 </h5>
@@ -371,6 +371,11 @@
                 .detail-img {
                     fill: #9e9e9e;
                 }
+            }
+            h5.nomal-title {
+                cursor: auto;
+                background: none;
+                color: #333C48;
             }
         }
         .detail-info.detail-label {
