788ac12b68fc4c21528aa7c8f98292aa7c1405bc
TENCENT\carlyin <carlyin@tencent.com>
2020-04-20 10:48:13 +0800

    feature: 蓝盾接口层统一参数校验框架开发

---
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/validation/BkFieldValidator.kt
@@ -0,0 +1,20 @@
+package com.tencent.devops.common.api.validation
+
+import com.tencent.devops.common.api.annotation.BkField
+import com.tencent.devops.common.api.util.JsonUtil
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+
+class NotEmptyValidator : ConstraintValidator<BkField?, Any?> {
+
+    override fun initialize(parameters: BkField?) {}
+
+    override fun isValid(
+        paramObject: Any?,
+        constraintValidatorContext: ConstraintValidatorContext
+    ): Boolean {
+        println(paramObject?.let { JsonUtil.toJson(it) })
+        //return paramObject?.toString()?.isNotEmpty() ?: false
+        return false
+    }
+}
