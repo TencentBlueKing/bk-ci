b0b1432c430a70690a23452b343669144e05f2af
TENCENT\carlyin <carlyin@tencent.com>
2020-04-20 16:57:21 +0800

    feature:支持服务扩展 - 工作台上架/升级扩展 issue #547

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreMessageCode.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreMessageCode.kt
@@ -102,6 +102,7 @@ object StoreMessageCode {
     const val USER_SERVICE_PROJECT_UNENABLE = "2120407" // 研发商店：选中调试项目已被禁用
     const val USER_SERVICE_PROJECT_NOT_PERMISSION = "2120408" // 研发商店：选中调试项目无创建流水线权限
     const val USER_SERVICE_NOT_EXIST = "2120409" // 研发商店：扩展服务不存在{0}
+    const val USER_ITEM_SERVICE_USED_IS_NOT_ALLOW_DELETE = "2120410" // 研发商店：扩展点下还有可用的扩展服务，不能删除
 
 
     // store公共业务相关的错误提示
