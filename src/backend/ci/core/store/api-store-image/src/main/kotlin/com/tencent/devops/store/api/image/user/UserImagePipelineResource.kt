c5eb73fdb9845f568343b021b1bd25c6609184d4
jsonwan <1334793794@qq.com>
2020-04-23 11:33:40 +0800

    feature:【研发商店】支持CI镜像市场 - 流水线添加构建环境的job时，默认填充一个镜像，该镜像可配置 #1108

--- src/backend/ci/core/store/api-store-image/src/main/kotlin/com/tencent/devops/store/api/image/user/UserImagePipelineResource.kt
+++ src/backend/ci/core/store/api-store-image/src/main/kotlin/com/tencent/devops/store/api/image/user/UserImagePipelineResource.kt
@@ -26,26 +26,16 @@
 package com.tencent.devops.store.api.image.user
 
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
-import com.tencent.devops.common.api.pojo.Page
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.pipeline.type.docker.ImageType
-import com.tencent.devops.store.pojo.common.VersionInfo
 import com.tencent.devops.store.pojo.image.enums.ImageAgentTypeEnum
-import com.tencent.devops.store.pojo.image.enums.ImageRDTypeEnum
-import com.tencent.devops.store.pojo.image.enums.MarketImageSortTypeEnum
-import com.tencent.devops.store.pojo.image.request.ImageBaseInfoUpdateRequest
 import com.tencent.devops.store.pojo.image.response.*
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
-import javax.ws.rs.PUT
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
@@ -66,6 +56,6 @@ interface UserImagePipelineResource {
         @ApiParam("使用机器类型", required = true)
         @QueryParam("agentType")
         agentType: ImageAgentTypeEnum
-    ): Result<SimpleImageInfo>
+    ): Result<BaseImageInfo>
 
 }
