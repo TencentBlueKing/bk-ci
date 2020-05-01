cf098eb4c7d4dcd7c78d88120b2ce3e3df736042
irwinsun <irwinsun@tencent.com>
2020-05-01 15:01:40 +0800

    Chore: 梳理部署规范 #1199 init.sh 脚本的下载的sh和log存放位置规范

--- support-files/agent-package/script/init.sh
+++ support-files/agent-package/script/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "start to download the docker_init.sh..." > /data/devops/logs/docker.log
 mkdir /data/devops
 cd /data/devops
+
+echo "start to download the docker_init.sh..." > logs/docker.log
 #sleep 1000
 
 curl -k -s -H "X-DEVOPS-BUILD-TYPE: DOCKER" -H "X-DEVOPS-PROJECT-ID: ${devops_project_id}" -H "X-DEVOPS-AGENT-ID: ${devops_agent_id}" -H "X-DEVOPS-AGENT-SECRET-KEY: ${devops_agent_secret_key}" -o  docker_init.sh "${devops_gateway}/ms/dispatch/api/build/scripts?scriptName=docker_init.sh"
