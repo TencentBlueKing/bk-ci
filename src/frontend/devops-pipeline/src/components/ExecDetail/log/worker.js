ce015263a4e3b448740eb0c4e6b1ef310639663d
xuzhan <xuzhan@tencent.com>
2020-05-02 11:28:49 +0800

    feature: 兼容safari issue #1292 - 预留20px宽度展示

--- src/frontend/devops-pipeline/src/components/ExecDetail/log/worker.js
+++ src/frontend/devops-pipeline/src/components/ExecDetail/log/worker.js
@@ -323,11 +323,17 @@ if (self.OffscreenCanvas) {
     // 兼容safari
     getTextWidth = (text) => {
         let res = 0
+        let disNum = 0
         for (let i = 0, len = text.length; i < len; i++) {
-            if (/[\u4e00-\u9fa5]/.test(text[i])) res += 12
-            else res += 8
+            if (/[\u4e00-\u9fa5]/.test(text[i])) {
+                res += 12
+            } else {
+                res += 8
+                disNum++
+            }
         }
-        return res
+        res -= (disNum - 1)
+        return res + 20
     }
 }
 
