36afcfaa48dd72920161a04a19ce52957121d80a
sawyersong2 <wudiduomjimo@gmail.com>
2020-05-05 00:28:23 +0800

    feature：改造devnet构建机分发方案，避免母机漂移 #845 dockerhost心跳接口，优化dockerIP OP相关接口

--- src/backend/ci/core/dispatch/api-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/api/builds/BuildDockerHostResource.kt
+++ src/backend/ci/core/dispatch/api-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/api/builds/BuildDockerHostResource.kt
@@ -26,24 +26,19 @@
 
 package com.tencent.devops.dispatch.api.builds
 
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.mq.alert.AlertLevel
 import com.tencent.devops.dispatch.pojo.ContainerInfo
 import com.tencent.devops.dispatch.pojo.DockerHostBuildInfo
 import com.tencent.devops.dispatch.pojo.DockerHostInfo
-import com.tencent.devops.dispatch.pojo.DockerIpInfoVO
 import com.tencent.devops.store.pojo.image.response.ImageRepoInfo
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
 import javax.ws.rs.GET
-import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
