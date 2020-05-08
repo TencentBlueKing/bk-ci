4481e3bdab5f2df3c51e37805f8db553c3b97176
cao150260 <fitzcao@tencent.com>
2020-05-08 15:41:52 +0800

    refactor: websocket消费者支持配置化

--- src/backend/ci/core/websocket/biz-websocket/src/main/kotlin/com/tencent/devops/websocket/cron/ClearTimeoutCron.kt
+++ src/backend/ci/core/websocket/biz-websocket/src/main/kotlin/com/tencent/devops/websocket/cron/ClearTimeoutCron.kt
@@ -49,36 +49,6 @@ class ClearTimeoutCron(
     /**
      * 每分钟一次，计算session是否已经超时，若超时，剔除该session关联的所有websocket redis信息。
      */
-//    @Scheduled(cron = "0 */1 * * * ?")
-//    fun clearTimeoutAllCache() {
-//        val nowTime = System.currentTimeMillis()
-//        val sessionTimeoutStr = RedisUtlis.getSessionTimeOutFromRedis(redisOperation)
-//        if (sessionTimeoutStr != null) {
-//            val sessionTimeoutMap: MutableMap<String, String> = objectMapper.readValue(sessionTimeoutStr)
-//            val newSessionMap = mutableMapOf<String, String>()
-//            sessionTimeoutMap.forEach { (sessionId, key) ->
-//                val timeout: Long = key.substringBefore("#").toLong()
-//                val userId = key.substringAfter("#")
-//                if (nowTime < timeout) {
-//                    newSessionMap[sessionId] = key
-//                } else {
-//                    val sessionPage = RedisUtlis.getPageFromSessionPageBySession(redisOperation, sessionId)
-//                    RedisUtlis.cleanSessionPageBySessionId(redisOperation, sessionId)
-//                    if (sessionPage != null) {
-//                        RedisUtlis.cleanPageSessionBySessionId(redisOperation, sessionId, sessionPage)
-//                    }
-//                    RedisUtlis.cleanUserSessionBySessionId(redisOperation, userId, sessionId)
-//                    websocketService.removeCacheSession(sessionId)
-//                    logger.info("[clearTimeOutSession] sessionId:$sessionId,loadPage:$sessionPage,userId:$userId")
-//                }
-//            }
-//            RedisUtlis.saveSessionTimeOutAll(redisOperation, objectMapper.writeValueAsString(newSessionMap))
-//        }
-//    }
-
-    /**
-     * 每分钟一次，计算session是否已经超时，若超时，剔除该session关联的所有websocket redis信息。
-     */
     @Scheduled(cron = "0 */1 * * * ?")
     fun newClearTimeoutCache() {
         longSessionLog()
@@ -118,8 +88,6 @@ class ClearTimeoutCron(
                                 RedisUtlis.cleanSessionPageBySessionId(redisOperation, sessionId)
                                 if (sessionPage != null) {
                                     RedisUtlis.cleanPageSessionBySessionId(redisOperation, sessionPage, sessionId)
-                                }
-                                if(sessionPage != null) {
                                     RedisUtlis.cleanUserSessionBySessionId(redisOperation, userId, sessionId)
                                     logger.info("[clearTimeOutSession] sessionId:$sessionId,loadPage:$sessionPage,userId:$userId")
                                 }
