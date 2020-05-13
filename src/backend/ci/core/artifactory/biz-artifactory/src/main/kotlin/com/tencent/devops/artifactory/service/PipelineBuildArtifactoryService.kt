b6cb3674fe414552db25f533d8ec5b9af80e0f71
mingshewhe <mingshewhe@tencent.com>
2020-05-13 10:11:59 +0800

    format: 流水线执行历史的构件列表不能显示相关内容 #1427

--- src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/service/PipelineBuildArtifactoryService.kt
+++ src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/service/PipelineBuildArtifactoryService.kt
@@ -2,7 +2,7 @@ package com.tencent.devops.artifactory.service
 
 import com.tencent.devops.artifactory.pojo.FileInfo
 
-interface PipelineBuildExtService {
+interface PipelineBuildArtifactoryService {
 
     fun getArtifactList(projectId: String, pipelineId: String, buildId: String): List<FileInfo>
 
