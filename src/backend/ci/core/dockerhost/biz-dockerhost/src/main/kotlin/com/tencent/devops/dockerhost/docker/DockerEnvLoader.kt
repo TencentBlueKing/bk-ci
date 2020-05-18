223e02d65292fef8076e28633b3ef14fa7babeb7
sawyersong2 <wudiduomjimo@gmail.com>
2020-05-18 21:16:24 +0800

    feat: 限制dockerRun容器运行最长时间（默认8小时） #1491

--- src/backend/ci/core/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/docker/DockerEnvLoader.kt
+++ src/backend/ci/core/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/docker/DockerEnvLoader.kt
@@ -41,6 +41,7 @@ object DockerEnvLoader {
     fun loadEnv(dockerHostBuildInfo: DockerHostBuildInfo): List<String> {
 
         val envList = mutableListOf<String>()
+        envList.add("bk_devops_start_source=dockerRun")
         try {
             val generators: List<DockerEnvGenerator> =
                 SpringContextUtil.getBeansWithAnnotation(EnvGenerator::class.java) as List<DockerEnvGenerator>
