3ff03093cacc552224cc75556de4401415c31bf6
fitzcao <fitzcao@tencent.com>
2020-05-27 13:19:46 +0800

    --story=858473813 【OP】增加动态时，支持输入 ENGLISH_NAME

--- src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/RepositoryConfService.kt
+++ src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/RepositoryConfService.kt
@@ -29,8 +29,8 @@ package com.tencent.devops.gitci.service
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.gitci.dao.GitCISettingDao
 import com.tencent.devops.gitci.pojo.GitRepositoryConf
-import com.tencent.devops.project.api.service.service.ServiceTxProjectResource
 import com.tencent.devops.project.api.op.OPProjectResource
+import com.tencent.devops.project.api.service.service.ServiceTxProjectResource
 import com.tencent.devops.project.pojo.OpProjectGraySetRequest
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
