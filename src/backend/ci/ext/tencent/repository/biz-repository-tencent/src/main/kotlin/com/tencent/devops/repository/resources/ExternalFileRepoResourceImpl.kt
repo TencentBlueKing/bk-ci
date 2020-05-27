9c818f918660e9ddf9bf645796844febee35516f
linjun-ddddd <654548718@qq.com>
2020-05-27 17:10:45 +0800

    --story=858480529 增加无token获取文件内容接口

---
+++ src/backend/ci/ext/tencent/repository/biz-repository-tencent/src/main/kotlin/com/tencent/devops/repository/resources/ExternalFileRepoResourceImpl.kt
@@ -0,0 +1,18 @@
+package com.tencent.devops.repository.resources
+
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.repository.api.ExternalFileRepoResource
+import com.tencent.devops.repository.service.scm.GitService
+import com.tencent.devops.repository.service.scm.TencentGitCiService
+import org.springframework.beans.factory.annotation.Autowired
+
+@RestResource
+class ExternalFileRepoResourceImpl @Autowired constructor(
+    private val gitciService: TencentGitCiService,
+    private val gitService: GitService
+) : ExternalFileRepoResource{
+    override fun getFileContent(repoUrl: String, filePath: String, ref: String?, subModule: String?): Result<String> {
+        return Result(gitciService.getFileContent(repoUrl, filePath, ref, subModule))
+    }
+}
