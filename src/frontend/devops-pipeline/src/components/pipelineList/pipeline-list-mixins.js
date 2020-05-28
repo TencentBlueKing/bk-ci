4788c20d99d83026b58f04c4783ecf49196b62cd
lockiechen <lockiechen@tencent.com>
2020-05-28 19:19:23 +0800

    feature：流水线列表支持“在新标签页中打开链接”功能。 issue #872

---
+++ src/frontend/devops-pipeline/src/components/pipelineList/pipeline-list-mixins.js
@@ -0,0 +1,38 @@
+import { bus } from '@/utils/bus'
+import progressBar from '@/components/devops/progressBar'
+import triggers from '@/components/pipeline/triggers'
+import extMenu from '@/components/pipelineList/extMenu'
+export default {
+    components: {
+        progressBar,
+        triggers,
+        extMenu
+    },
+    methods: {
+        getHistoryURL (pipelineId) {
+            return `${WEB_URL_PIRFIX}/pipeline/${this.$route.params.projectId}/${pipelineId}/history`
+        },
+        emitEventHandler (eventName, pipelineId) {
+            bus.$emit(eventName, pipelineId)
+        },
+        goHistory (e, pipelineId) {
+            const withAltKey = e.metaKey || e.altKey
+            if (withAltKey) {
+                window.open(this.getHistoryURL(pipelineId), '_blank')
+            } else {
+                this.emitEventHandler('title-click', pipelineId)
+            }
+        },
+        triggersExec ({ pipelineId, ...params }) {
+            bus.$emit('triggers-exec', params, pipelineId)
+        },
+        applyPermission ({ pipelineName, pipelineId }) {
+            bus.$emit(
+                'set-permission',
+                `${this.$t('pipeline')}：${pipelineName}`,
+                this.$t('newlist.view'),
+                pipelineId
+            )
+        }
+    }
+}
