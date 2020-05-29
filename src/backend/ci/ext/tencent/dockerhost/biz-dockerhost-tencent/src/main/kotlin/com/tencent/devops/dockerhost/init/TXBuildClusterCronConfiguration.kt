31687f12479da26dc677cdaf7b8f2353c6fe808c
sawyersong2 <wudiduomjimo@gmail.com>
2020-05-29 15:00:47 +0800

    --story=858506997 dockerhost创建容器时控制containerName，防止并发时容器名相同导致的异常

--- src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/init/TXBuildClusterCronConfiguration.kt
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/init/TXBuildClusterCronConfiguration.kt
@@ -27,9 +27,7 @@
 package com.tencent.devops.dockerhost.init
 
 import com.tencent.devops.dockerhost.config.DockerHostConfig
-import com.tencent.devops.dockerhost.cron.DebugRunner
 import com.tencent.devops.dockerhost.cron.UpdateAgentRunner
-import com.tencent.devops.dockerhost.service.DockerHostDebugService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -56,32 +54,14 @@ class TXBuildClusterCronConfiguration : SchedulingConfigurer {
     override fun configureTasks(scheduledTaskRegistrar: ScheduledTaskRegistrar) {
         scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(100))
 
-        /*scheduledTaskRegistrar.addFixedRateTask(
-            IntervalTask(
-                Runnable { debugRunner.startBuild() }, 5 * 1000, 30 * 1000
-            )
-        )*/
-/*        scheduledTaskRegistrar.addFixedRateTask(
-            IntervalTask(
-                Runnable { debugRunner.endBuild() }, 10 * 1000, 60 * 1000
-            )
-        )*/
         scheduledTaskRegistrar.addCronTask(
 //                { updateAgentRunner.update() }, "0 0 3 * * ?"
             { updateAgentRunner.update() }, downloadAgentCron!!
         )
     }
 
-/*    @Autowired
-    private lateinit var debugRunner: DebugRunner*/
-
     @Autowired
     private lateinit var updateAgentRunner: UpdateAgentRunner
-
-    @Bean
-    fun debugRunner(dockerHostDebugService: DockerHostDebugService): DebugRunner {
-        return DebugRunner(dockerHostDebugService)
-    }
 
     @Bean
     fun updateAgentRunner(dockerHostConfig: DockerHostConfig): UpdateAgentRunner {
