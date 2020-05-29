1f7c1a6311b6c91f016bf169f47c69aefd98b30f
TENCENT\carlyin <carlyin@tencent.com>
2020-05-29 17:11:09 +0800

    feature: 支持动态刷新store组件构建流水线model #1646

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/service/ServicePipelineSettingResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/service/ServicePipelineSettingResource.kt
@@ -38,7 +38,6 @@ import javax.ws.rs.HeaderParam
 import javax.ws.rs.PUT
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["SERVICE_PIPELINE_SETTING"], description = "服务-流水线设置")
@@ -54,9 +53,6 @@ interface ServicePipelineSettingResource {
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
-        @ApiParam("项目ID", required = true)
-        @QueryParam("projectId")
-        projectId: String,
         @ApiParam("更新流水线模型请求报文")
         updatePipelineModelRequest: UpdatePipelineModelRequest
     ): Result<Boolean>
