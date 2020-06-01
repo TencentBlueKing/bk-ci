4328c9cc937f009c3db9a294b4dbdd0ba9ae1a4c
freyzheng <freyzheng@tencent.com>
2020-06-01 21:55:43 +0800

    feature:新增bkci-sign服务模块 issue #1481

--- src/backend/ci/core/sign/biz-sign/src/main/kotlin/com/tencent/devops/sign/service/IpaSignService.kt
+++ src/backend/ci/core/sign/biz-sign/src/main/kotlin/com/tencent/devops/sign/service/IpaSignService.kt
@@ -2,12 +2,20 @@ package com.tencent.devops.sign.service
 
 import com.tencent.devops.common.api.pojo.Result
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition
+import java.io.File
 import java.io.InputStream
 
 interface IpaSignService {
     fun resignIpaPackage(
-        userId: String,
-        ipaSignInfo: String?,
-        inputStream: InputStream
+            userId: String,
+            ipaSignInfo: String?,
+            inputStream: InputStream
     ): Result<String?>
+
+    fun resignApp(
+            appPath: File,
+            bundleId: String?,
+            mobileprovision: String?,
+            entitlement: String?
+    ): Result<Boolean>
 }
