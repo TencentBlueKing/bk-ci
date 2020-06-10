3a41659b67efcb96ad273c247068a24012cc55dc
irwinsun <irwinsun@tencent.com>
2020-06-10 17:57:28 +0800

    perf: repository调优 #1777 增加索引

--- support-files/sql/0005_ci_repository-update_v2.3_mysql.sql
+++ support-files/sql/0005_ci_repository-update_v2.3_mysql.sql
@@ -19,6 +19,29 @@ BEGIN
         ALTER TABLE T_REPOSITORY ADD INDEX inx_alias_name (ALIAS_NAME);
     END IF;
 
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.statistics
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_REPOSITORY_COMMIT'
+                    AND INDEX_NAME = 'IDX_BUILD_ID_TIME') THEN
+        ALTER TABLE T_REPOSITORY_COMMIT ADD INDEX `BUILD_ID_TIME_INDEX` (`BUILD_ID`, `COMMIT_TIME`);
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.statistics
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_REPOSITORY_COMMIT'
+                    AND INDEX_NAME = 'IDX_PIPE_ELEMENT_REPO_TIME') THEN
+        ALTER TABLE T_REPOSITORY_COMMIT ADD INDEX `IDX_PIPE_ELEMENT_REPO_TIME` (`PIPELINE_ID`, `ELEMENT_ID`, `REPO_ID`, `COMMIT_TIME`);
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.statistics
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_REPOSITORY_COMMIT'
+                    AND INDEX_NAME = 'IDX_PIPE_ELEMENT_NAME_REPO_TIME') THEN
+        ALTER TABLE T_REPOSITORY_COMMIT ADD INDEX `IDX_PIPE_ELEMENT_NAME_REPO_TIME` (`PIPELINE_ID`, `ELEMENT_ID`, `REPO_NAME`, `COMMIT_TIME`);
+    END IF;
 
     COMMIT;
 END <CI_UBF>
