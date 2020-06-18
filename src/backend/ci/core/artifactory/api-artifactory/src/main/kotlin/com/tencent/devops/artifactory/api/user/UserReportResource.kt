b91a17446f58d1fcabfdb33d6b8b3ec5ee4b7037
necrohuang <necrohuang@tencent.com>
2020-06-18 14:45:16 +0800

    feature: 增加获取报告内容功能 #1799

--- src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/api/user/UserReportResource.kt
+++ src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/api/user/UserReportResource.kt
@@ -38,7 +38,6 @@ import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
 @Api(tags = ["USER_REPORT"], description = "版本仓库-报告目录")
 @Path("/user/reports")
