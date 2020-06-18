e55b19fd5c6fe493e9374f532dcb0785d1af9ec9
xuzhan <xuzhan@tencent.com>
2020-06-18 17:35:06 +0800

    feature: 研发商店体验优化 issue #1863 - 修改镜像相关表单校验错误提示

--- src/frontend/devops-atomstore/src/components/common/workList/image.vue
+++ src/frontend/devops-atomstore/src/components/common/workList/image.vue
@@ -94,10 +94,22 @@
             :width="relateImageData.width">
             <template slot="content">
                 <bk-form ref="relateForm" class="relate-form" label-width="100" :model="relateImageData.form" v-bkloading="{ isLoading: relateImageData.isLoading }">
-                    <bk-form-item :label="$t('store.镜像名称')" :required="true" property="imageName" :desc="$t('store.镜像在研发商店中的别名')" :rules="[requireRule]">
+                    <bk-form-item :label="$t('store.镜像名称')"
+                        :required="true"
+                        property="imageName"
+                        :desc="$t('store.镜像在研发商店中的别名')"
+                        :rules="[requireRule]"
+                        error-display-type="normal"
+                    >
                         <bk-input v-model="relateImageData.form.imageName" :placeholder="$t('store.请输入镜像名称')"></bk-input>
                     </bk-form-item>
-                    <bk-form-item :label="$t('store.镜像标识')" :required="true" property="imageCode" :desc="$t('store.镜像英文名，为当前镜像在研发商店中的唯一标识')" :rules="[requireRule, alpRule]">
+                    <bk-form-item :label="$t('store.镜像标识')"
+                        :required="true"
+                        property="imageCode"
+                        :desc="$t('store.镜像英文名，为当前镜像在研发商店中的唯一标识')"
+                        :rules="[requireRule, alpRule]"
+                        error-display-type="normal"
+                    >
                         <bk-input v-model="relateImageData.form.imageCode" :placeholder="$t('store.请输入镜像标识')"></bk-input>
                     </bk-form-item>
                     <bk-form-item :label="$t('store.镜像源')" :required="true" property="imageSourceType" class="h32" :rules="[requireRule]">
@@ -105,7 +117,13 @@
                             <bk-radio value="THIRD"> {{ $t('store.第三方源') }} </bk-radio>
                         </bk-radio-group>
                     </bk-form-item>
-                    <bk-form-item :label="$t('store.调试项目')" :required="true" property="projectCode" :desc="$t('store.在发布过程中，可以在该项目下调试镜像')" :rules="[requireRule]">
+                    <bk-form-item :label="$t('store.调试项目')"
+                        :required="true"
+                        property="projectCode"
+                        :desc="$t('store.在发布过程中，可以在该项目下调试镜像')"
+                        :rules="[requireRule]"
+                        error-display-type="normal"
+                    >
                         <bk-select v-model="relateImageData.form.projectCode" searchable :placeholder="$t('store.请选择项目')" @change="toggleProjectList">
                             <bk-option v-for="option in projectList"
                                 :key="option.project_code"
