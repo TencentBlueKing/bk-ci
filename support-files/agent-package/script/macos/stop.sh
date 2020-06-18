778954731d97c73611a8a52a770171b2c765f84a
irwinsun <irwinsun@tencent.com>
2020-06-18 11:58:21 +0800

    Revert "Rc5"

--- support-files/agent-package/script/macos/stop.sh
+++ support-files/agent-package/script/macos/stop.sh
@@ -2,38 +2,22 @@
 workspace=`pwd`
 user=${USER}
 
-function isPidExists()
-{
-  for i in `ps aux | grep -v grep | awk '{print $2}'`;do
-    if [[ $1 == $i ]];then
-      return 0
-    fi
-  done
-  return 1
-}
-
 function stop()
 {
-  pid=0
-  if [[ -f "${workspace}/runtime/daemon.pid" ]]; then
-    pid=`cat "${workspace}/runtime/daemon.pid"`
-  fi
-  if isPidExists ${pid}; then
+  pid=$(ps aux | grep devopsDaemon | grep -v grep | awk '{print $2}')
+  if [[ -n "$pid" ]]; then
     echo "stop daemon process"
     kill -9 "$pid"
-    echo "daemon is stopped"
+    echo "daemon is stoped"
   else
     echo "daemon is not running, skip"
   fi
 
-  pid=0
-  if [[ -f "${workspace}/runtime/agent.pid" ]]; then
-    pid=`cat "${workspace}/runtime/agent.pid"`
-  fi
-  if isPidExists ${pid}; then
+  pid=$(ps aux | grep devopsAgent | grep -v grep | awk '{print $2}')
+  if [[ -n "$pid" ]]; then
     echo "stop agent process"
     kill -9 "$pid"
-    echo "agent is stopped"
+    echo "agent is stoped"
   else
     echo "agent is not running, skip"
   fi
