cf93ac242d1ba3d6b76ac2c758979a257db743e7
freyzheng <freyzheng@tencent.com>
2020-06-19 20:03:01 +0800

    featuer: 微服务接口增加jwt验证 issue #1795

--- src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/filter/ServiceSecurityFilter.kt
+++ src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/filter/ServiceSecurityFilter.kt
@@ -6,9 +6,11 @@ import com.tencent.devops.common.security.jwt.JwtManager
 import com.tencent.devops.common.web.RequestFilter
 import com.tencent.devops.common.web.constant.SecurityErrorCode
 import org.slf4j.LoggerFactory
+import javax.servlet.http.HttpServletRequest
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerRequestFilter
 import javax.ws.rs.container.PreMatching
+import javax.ws.rs.core.Context
 import javax.ws.rs.ext.Provider
 
 @Provider
@@ -19,6 +21,7 @@ class ServiceSecurityFilter(
 ) : ContainerRequestFilter {
 
     companion object {
+        private val excludeVeritfyPath = listOf("/api/swagger.json", "/api/external/service/versionInfo")
         private val logger = LoggerFactory.getLogger((ServiceSecurityFilter::class.java))
     }
 
@@ -30,7 +33,7 @@ class ServiceSecurityFilter(
                 throw ErrorCodeException(
                     statusCode = 401,
                     errorCode = SecurityErrorCode.ERROR_SERVICE_NO_AUTH,
-                    defaultMessage = "禁止访问微服务"
+                    defaultMessage = "禁止访问微服务:devops api jwt it empty."
                 )
             }
             val checkResult: Boolean = jwtManager.verifyJwt(jwt)
@@ -39,7 +42,7 @@ class ServiceSecurityFilter(
                 throw ErrorCodeException(
                     statusCode = 401,
                     errorCode = SecurityErrorCode.ERROR_SERVICE_NO_AUTH,
-                    defaultMessage = "禁止访问微服务"
+                    defaultMessage = "禁止访问微服务:devops api jwt it invalid or expired."
                 )
             }
         }
@@ -48,14 +51,24 @@ class ServiceSecurityFilter(
     private fun shouldFilter(requestContext: ContainerRequestContext): Boolean {
         val uri = requestContext.uriInfo.requestUri.path
         logger.info("HttpServletRequest.requestURI=$uri")
-        if (!jwtManager.isAuth()) {
+        if (!jwtManager.isEnable()) {
             return false
         }
 //        // dev环境需要支持swagger，请求无需认证
 //        if (!EnvironmentUtil.isProdProfileActive()) {
 //            return false
 //        }
-        // 只拦截web/service/esb的API请求
-        return uri.startsWith("/api/")
+        // 不拦截的接口
+        excludeVeritfyPath.forEach{
+            if(uri.startsWith(it)) {
+                return false
+            }
+        }
+        // 拦截api接口
+        if(uri.startsWith("/api/")) {
+            return true
+        }
+        // 默认不拦截
+        return false
     }
 }
