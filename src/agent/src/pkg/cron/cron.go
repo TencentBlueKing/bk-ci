3d2523d8e6fd0a49cc9370535343a40907f1a212
necrohuang <necrohuang@tencent.com>
2020-07-17 10:57:26 +0800

    bug: Agent构建日志logback在对日志文件进行按日期或按大小分割时可能会导致Java死锁 #2075

--- src/agent/src/pkg/cron/cron.go
+++ src/agent/src/pkg/cron/cron.go
@@ -38,44 +38,79 @@ import (
 	"github.com/astaxie/beego/logs"
 )
 
-func CleanDumpFileJob(intervalInHours int, cleanBeforeInHours int) {
-	TryCleanDumpFile(cleanBeforeInHours)
+func CleanJob() {
+	intervalInHours := 2
+	TryCleanFile()
 	for {
 		now := time.Now()
 		nextTime := now.Add(time.Hour * time.Duration(intervalInHours))
-		// nextTime := now.Add(time.Second * 30)
 		logs.Info("next clean time: ", util.FormatTime(nextTime))
 		t := time.NewTimer(nextTime.Sub(now))
 		<-t.C
-		TryCleanDumpFile(cleanBeforeInHours)
+		TryCleanFile()
 	}
 }
 
-func TryCleanDumpFile(hoursBefore int) {
-	logs.Info("clean jvm dump file starts")
+func TryCleanFile() {
+	logs.Info("clean task starts")
 	defer func() {
 		if err := recover(); err != nil {
-			logs.Error("remove jvm dump files error: ", err)
+			logs.Error("run clean task error: ", err)
 		}
 	}()
 
+	cleanDumpFile(36)
+	cleanLogFile(96)
+}
+
+func cleanDumpFile(timeBeforeInHours int) {
+	dumpFileBeforeStr := util.FormatTime(time.Now().Add(time.Hour * time.Duration(timeBeforeInHours*-1)))
 	workDir := systemutil.GetWorkDir()
-	logs.Info("clean dump file in " + workDir + " before " + util.FormatTime(time.Now().Add(time.Hour*time.Duration(hoursBefore*-1))))
+	logs.Info(fmt.Sprintf("clean dump file before %s(%d hours) in %s", dumpFileBeforeStr, timeBeforeInHours, workDir))
 	files, err := ioutil.ReadDir(workDir)
 	if err != nil {
-		logs.Error("read dir error: ", err.Error())
+		logs.Warn("read work dir error: ", err.Error())
+		return
 	}
 	for _, file := range files {
-		if !file.IsDir() && strings.HasPrefix(file.Name(), "hs_err_pid") &&
-			int(time.Since(file.ModTime()).Hours()) > hoursBefore {
+		if file.IsDir() {
+			continue
+		}
+		if strings.HasPrefix(file.Name(), "hs_err_pid") && int(time.Since(file.ModTime()).Hours()) > timeBeforeInHours {
 			fileFullName := workDir + "/" + file.Name()
 			err = os.Remove(fileFullName)
 			if err != nil {
-				logs.Warn(fmt.Sprintf("remove file %s failed: ", fileFullName), err.Error())
+				logs.Warn(fmt.Sprintf("remove file %s failed: ", fileFullName))
 			} else {
 				logs.Info(fmt.Sprintf("file %s removed", fileFullName))
 			}
 		}
 	}
-	logs.Info("clean jvm dump file done")
+	logs.Info("clean dump file done")
+}
+
+func cleanLogFile(timeBeforeInHours int) {
+	logFileBeforeStr := util.FormatTime(time.Now().Add(time.Hour * time.Duration(timeBeforeInHours*-1)))
+	logDir := systemutil.GetLogDir()
+	logs.Info(fmt.Sprintf("clean log file before %s(%d hours) in %s", logFileBeforeStr, timeBeforeInHours, logDir))
+	files, err := ioutil.ReadDir(logDir)
+	if err != nil {
+		logs.Warn("read log dir error: ", err.Error())
+		return
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if strings.HasSuffix(file.Name(), ".log") && int(time.Since(file.ModTime()).Hours()) > timeBeforeInHours {
+			fileFullName := logDir + "/" + file.Name()
+			err = os.Remove(fileFullName)
+			if err != nil {
+				logs.Warn(fmt.Sprintf("remove file %s failed: ", fileFullName))
+			} else {
+				logs.Info(fmt.Sprintf("file %s removed", fileFullName))
+			}
+		}
+	}
+	logs.Info("clean log file done")
 }
