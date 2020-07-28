53d8e3964d754136d474d3ccba0b215577df9471
mingshewhe <mingshewhe@tencent.com>
2020-07-28 14:57:23 +0800

    feature: 执行历史增加触发信息字段 #1644
    # Conflicts:
    #	support-files/sql/2004_ci_process-update_v1.2_mysql.sql

--- support-files/sql/2004_ci_process-update_v1.2_mysql.sql
+++ support-files/sql/2004_ci_process-update_v1.2_mysql.sql
@@ -11,13 +11,13 @@ BEGIN
     DECLARE db VARCHAR(100);
     SET AUTOCOMMIT = 0;
     SELECT DATABASE() INTO db;
-	
+
 	IF NOT EXISTS(SELECT 1
                   FROM information_schema.COLUMNS
                   WHERE TABLE_SCHEMA = db
                     AND TABLE_NAME = 'T_PIPELINE_WEBHOOK'
                     AND COLUMN_NAME = 'PROJECT_NAME') THEN
-        ALTER TABLE T_PIPELINE_WEBHOOK 
+        ALTER TABLE T_PIPELINE_WEBHOOK
 			ADD COLUMN `PROJECT_NAME` VARCHAR(128) DEFAULT NULL;
     END IF;
 	
@@ -26,19 +26,36 @@ BEGIN
                   WHERE TABLE_SCHEMA = db
                     AND TABLE_NAME = 'T_PIPELINE_WEBHOOK'
                     AND COLUMN_NAME = 'TASK_ID') THEN
-        ALTER TABLE T_PIPELINE_WEBHOOK 
+        ALTER TABLE T_PIPELINE_WEBHOOK
 			ADD COLUMN `TASK_ID` VARCHAR(34) DEFAULT NULL;
     END IF;
-	
+
 	IF NOT EXISTS(SELECT 1
               FROM information_schema.statistics
               WHERE TABLE_SCHEMA = db
                 AND TABLE_NAME = 'T_PIPELINE_WEBHOOK'
                 AND INDEX_NAME = 'IDX_PROJECT_NAME_REPOSITORY_TYPE') THEN
-        ALTER TABLE T_PIPELINE_WEBHOOK 
+        ALTER TABLE T_PIPELINE_WEBHOOK
 			ADD INDEX `IDX_PROJECT_NAME_REPOSITORY_TYPE` (`PROJECT_NAME`, `REPOSITORY_TYPE`);
     END IF;
 
+    IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_PIPELINE_BUILD_HISTORY'
+                        AND COLUMN_NAME = 'WEBHOOK_INFO') THEN
+            ALTER TABLE T_PIPELINE_BUILD_HISTORY
+                ADD COLUMN `WEBHOOK_INFO` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+        END IF;
+
+        IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_PIPELINE_BUILD_HISTORY'
+                        AND COLUMN_NAME = 'IS_RETRY') THEN
+            ALTER TABLE T_PIPELINE_BUILD_HISTORY
+                ADD COLUMN `IS_RETRY` BIT(1);
+        END IF;
 
     COMMIT;
 END <CI_UBF>
