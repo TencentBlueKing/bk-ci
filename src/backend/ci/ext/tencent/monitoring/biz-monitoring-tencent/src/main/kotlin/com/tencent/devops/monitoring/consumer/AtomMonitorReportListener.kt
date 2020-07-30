433a59f3ac2c34eaaab2545167f871587d15dc19
johuang <huangjiao511@163.com>
2020-07-30 15:19:12 +0800

    --story=859165921 【monitor】提供api接口，供process上报核心插件错误码以及错误信息，入库备查

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/consumer/AtomMonitorReportListener.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/consumer/AtomMonitorReportListener.kt
@@ -23,44 +23,33 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package com.tencent.devops.monitoring.consumer
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tencent.devops.common.api.exception.ErrorCodeException
-import com.tencent.devops.common.web.mq.EXCHANGE_NOTIFY_MESSAGE
-import com.tencent.devops.common.web.mq.QUEUE_NOTIFY_MESSAGE
-import com.tencent.devops.common.web.mq.ROUTE_NOTIFY_MESSAGE
+import com.tencent.devops.common.api.pojo.AtomMonitorData
+import com.tencent.devops.common.event.listener.Listener
+import com.tencent.devops.common.event.pojo.measure.AtomMonitorReportBroadCastEvent
 import com.tencent.devops.monitoring.client.InfluxdbClient
 import com.tencent.devops.monitoring.constant.MonitoringMessageCode.ERROR_MONITORING_INSERT_DATA_FAIL
-import com.tencent.devops.monitoring.pojo.AtomMonitorData
-import com.tencent.devops.monitoring.pojo.DispatchStatus
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.rabbit.annotation.Exchange
-import org.springframework.amqp.rabbit.annotation.Queue
-import org.springframework.amqp.rabbit.annotation.QueueBinding
-import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 import kotlin.reflect.full.declaredMemberProperties
 
-@Service
-class AtomMonitorDataConsumer @Autowired constructor(
+@Component
+class AtomMonitorReportListener @Autowired constructor(
     private val influxdbClient: InfluxdbClient
-) {
-    @RabbitListener(
-        bindings = [(QueueBinding(
-            key = ROUTE_NOTIFY_MESSAGE,
-            value = Queue(value = QUEUE_NOTIFY_MESSAGE, durable = "true"),
-            exchange = Exchange(value = EXCHANGE_NOTIFY_MESSAGE, durable = "true", delayed = "true", type = "topic")))
-        ]
-    )
-    fun onReceiveAtomMonitorData(data: AtomMonitorData) {
+) : Listener<AtomMonitorReportBroadCastEvent> {
+
+    override fun execute(event: AtomMonitorReportBroadCastEvent) {
         try {
-            logger.info("Receive atom monitor data - $data")
-            insertAtomMonitorData(data)
-            if (data.atomCode == "CodeccCheckAtomDebug" && data.extData != null && data.extData!!.isNotEmpty()) {
-                logger.info("CodeCC atom, insert extDAta - ${jacksonObjectMapper().writeValueAsString(data.extData)}")
-                insertExtMap(data.extData!!)
+            logger.info("Receive atom monitor event - $event")
+            insertAtomMonitorData(event.monitorData)
+            if (event.monitorData.atomCode == "CodeccCheckAtomDebug" && event.monitorData.extData != null && event.monitorData.extData!!.isNotEmpty()) {
+                logger.info("CodeCC atom, insert extDAta - ${jacksonObjectMapper().writeValueAsString(event.monitorData.extData)}")
+                insertExtMap(event.monitorData.extData!!)
             }
         } catch (t: Throwable) {
             logger.warn("Fail to insert the atom monitor data", t)
@@ -77,7 +66,7 @@ class AtomMonitorDataConsumer @Autowired constructor(
         properties.forEach {
             field[it.name] = jacksonObjectMapper().writeValueAsString(it.get(data)?.toString() ?: "")
         }
-        influxdbClient.insert(DispatchStatus::class.java.simpleName, emptyMap(), field)
+        influxdbClient.insert(AtomMonitorData::class.java.simpleName, emptyMap(), field)
     }
 
     fun insertExtMap(extData: Map<String, Any>) {
@@ -85,10 +74,10 @@ class AtomMonitorDataConsumer @Autowired constructor(
         extData.forEach { (t, u) ->
             field[t] = jacksonObjectMapper().writeValueAsString(u)
         }
-        influxdbClient.insert(DispatchStatus::class.java.simpleName + "_extData", emptyMap(), field)
+        influxdbClient.insert(AtomMonitorData::class.java.simpleName + "_extData", emptyMap(), field)
     }
 
     companion object {
-        private val logger = LoggerFactory.getLogger(AtomMonitorDataConsumer::class.java)
+        private val logger = LoggerFactory.getLogger(AtomMonitorReportListener::class.java)
     }
 }
