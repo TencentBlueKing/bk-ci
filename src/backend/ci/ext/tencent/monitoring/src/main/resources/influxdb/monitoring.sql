d088363221e14d5883758c4cd8e5e6dd0b52fb60
stubenhuang <stubenhuang@tencent.com>
2020-08-03 16:22:12 +0800

    --story=859165921 【monitor】提供api接口，供process上报核心插件错误码以及错误信息，入库备查

--- src/backend/ci/ext/tencent/monitoring/src/main/resources/influxdb/monitoring.sql
+++ src/backend/ci/ext/tencent/monitoring/src/main/resources/influxdb/monitoring.sql
@@ -1,3 +1,5 @@
+-- 连续查询
+-- 核心插件相关
 CREATE CONTINUOUS QUERY cq_atom_total_count ON monitoring  BEGIN SELECT count(errorCode) as total_count INTO AtomMonitorData_success_rat_count FROM AtomMonitorData group by time(5m) END
 CREATE CONTINUOUS QUERY cq_atom_failed_count ON monitoring  BEGIN SELECT count(errorCode) as failed_count INTO AtomMonitorData_success_rat_count FROM AtomMonitorData where errorCode != '0' group by time(5m) END
 CREATE CONTINUOUS QUERY cq_atom_success_count ON monitoring  BEGIN SELECT count(errorCode) as success_count INTO AtomMonitorData_success_rat_count FROM AtomMonitorData where errorCode = '0' group by time(5m) END
@@ -17,3 +19,15 @@ CREATE CONTINUOUS QUERY cq_atom_CODE_GIT_total_count ON monitoring  BEGIN SELECT
 CREATE CONTINUOUS QUERY cq_atom_CODE_GIT_failed_count ON monitoring  BEGIN SELECT count(errorCode) as CODE_GIT_failed_count INTO AtomMonitorData_success_rat_count FROM AtomMonitorData where atomCode = 'CODE_GIT' and errorCode != '0' group by time(5m) END
 CREATE CONTINUOUS QUERY cq_atom_CODE_GIT_success_count ON monitoring  BEGIN SELECT count(errorCode) as CODE_GIT_success_count INTO AtomMonitorData_success_rat_count FROM AtomMonitorData where atomCode = 'CODE_GIT' and errorCode = '0' group by time(5m) END
 CREATE CONTINUOUS QUERY cq_atom_CODE_GIT_success_rat ON monitoring  BEGIN SELECT sum(CODE_GIT_success_count) * 100 / sum(CODE_GIT_total_count) as CODE_GIT_success_rat INTO AtomMonitorData_success_rat_count FROM AtomMonitorData_success_rat_count group by time(5m) END
+
+-- 用户登录相关
+CREATE CONTINUOUS QUERY cq_user_total_count ON monitoring BEGIN SELECT count(errorCode) AS user_total_count INTO monitoring.monitoring_retention.UsersStatus_success_rat_count FROM monitoring.monitoring_retention.UsersStatus GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_user_failed_count ON monitoring BEGIN SELECT count(errorCode) AS user_failed_count INTO monitoring.monitoring_retention.UsersStatus_success_rat_count FROM monitoring.monitoring_retention.UsersStatus WHERE errorCode != '0' GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_user_success_count ON monitoring BEGIN SELECT count(errorCode) AS user_success_count INTO monitoring.monitoring_retention.UsersStatus_success_rat_count FROM monitoring.monitoring_retention.UsersStatus WHERE errorCode = '0' GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_user_success_rat ON monitoring BEGIN SELECT sum(user_success_count) * 100 / sum(user_total_count) AS UsersStatus_success_rat INTO monitoring.monitoring_retention.UsersStatus_success_rat_count FROM monitoring.monitoring_retention.UsersStatus_success_rat_count GROUP BY time(5m) END
+
+-- 构建机准备相关
+CREATE CONTINUOUS QUERY cq_dispatch_devcloud_total_count ON monitoring BEGIN SELECT count(errorCode) AS devcloud_total_count INTO monitoring.monitoring_retention.DispatchStatus_success_rat_count FROM monitoring.monitoring_retention.DispatchStatus WHERE buildType = '.devcloud.public' GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_dispatch_devcloud_failed_count ON monitoring BEGIN SELECT count(errorCode) AS devcloud_failed_count INTO monitoring.monitoring_retention.DispatchStatus_success_rat_count FROM monitoring.monitoring_retention.DispatchStatus WHERE buildType = '.devcloud.public' AND errorCode != '0' GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_dispatch_devcloud_success_count ON monitoring BEGIN SELECT count(errorCode) AS devcloud_success_count INTO monitoring.monitoring_retention.DispatchStatus_success_rat_count FROM monitoring.monitoring_retention.DispatchStatus WHERE buildType = '.devcloud.public' AND errorCode = '0' GROUP BY time(5m) END
+CREATE CONTINUOUS QUERY cq_dispatch_devcloud_success_rat ON monitoring BEGIN SELECT sum(devcloud_success_count) * 100 / sum(devcloud_total_count) AS devcloud_success_rat INTO monitoring.monitoring_retention.DispatchStatus_success_rat_count FROM monitoring.monitoring_retention.DispatchStatus_success_rat_count GROUP BY time(5m) END
