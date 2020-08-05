4352ffe79c260a6cb7d4849e385a39df54880dee
irwinsun <irwinsun@tencent.com>
2020-08-05 14:55:40 +0800

    Merge remote-tracking branch 'origin/story_sla' into integration

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineVMBuildService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineVMBuildService.kt
@@ -561,12 +561,6 @@ class PipelineVMBuildService @Autowired(required = false) constructor(
             errorMsg = result.message
         )
 
-        // 发送度量数据
-        sendElementData(
-            buildId = buildId,
-            vmSeqId = vmSeqId,
-            result = result
-        )
         logger.info("Complete the task(${result.taskId}) of build($buildId) and seqId($vmSeqId)")
         pipelineRuntimeService.completeClaimBuildTask(
             buildId = buildId,
@@ -592,12 +586,8 @@ class PipelineVMBuildService @Autowired(required = false) constructor(
         // 发送度量数据
         sendElementData(
             buildId = buildId,
-            taskId = result.taskId,
-            success = result.success,
-            type = result.type,
-            errorType = result.errorType,
-            errorCode = result.errorCode,
-            errorMsg = result.message
+            vmSeqId = vmSeqId,
+            result = result
         )
 
         LogUtils.stopLog(
