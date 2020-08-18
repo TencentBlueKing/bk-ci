46840163d67102b2020ec8b0e765a5050f3d86f6
stubenhuang <stubenhuang@tencent.com>
2020-08-18 11:05:54 +0800

    --story=859166189 【scm/prpject】监控工蜂接口以及users接口的情况，上报monitor

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/job/MonitorNotifyJob.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/job/MonitorNotifyJob.kt
@@ -32,33 +32,58 @@ class MonitorNotifyJob @Autowired constructor(
     fun notifyDaily() {
         /*OpenAPI访问成功率
         页面500错误率
-        CodeCC插件非编译型工具扫描成功率
+        CodeCC插件非编译型工具扫描成功率(DONE)
         工蜂回写成功率
         日志API成功率
-        核心插件故障率
-        核心插件含：
-        拉取GIT（命令行）
-        CodeCC代码检查
-        归档构件
-        归档报告
+        核心插件故障率(DONE)
         公共构建机准备成功率
         登录成功率*/
 
         val startTime = 0L
         val endTime = 2597664799999L
-        val moduleMap = HashMap<String/*模块*/, List<Triple<String/*名称*/, String/*成功率*/, String/*详情链接*/>>>()
 
-        codecc(startTime, endTime).run { moduleMap[first] = second }
-        atomMonitor(startTime, endTime).run { moduleMap[first] = second }
-
-        val emailBody = EmailUtil.getEmailBody(startTime, endTime, moduleMap)
+        val moduleMap = hashMapOf(
+            codecc(startTime, endTime),
+            atomMonitor(startTime, endTime),
+            dispatchStatus(startTime, endTime)
+        )
 
         val message = EmailNotifyMessage()
-        message.addAllReceivers(receivers.split(",").asSequence().toHashSet())
+        message.addAllReceivers(receivers.split(",").toHashSet())
         message.title = title
-        message.body = emailBody
+        message.body = EmailUtil.getEmailBody(startTime, endTime, moduleMap)
         message.format = EnumEmailFormat.HTML
         client.get(ServiceNotifyResource::class).sendEmailNotify(message)
+    }
+
+    fun dispatchStatus(startTime: Long, endTime: Long): Pair<String, MutableList<Triple<String, String, String>>> {
+        val sql =
+            "SELECT sum(devcloud_total_count),sum(devcloud_success_count) FROM DispatchStatus_success_rat_count WHERE time>${startTime}000000 AND time<${endTime}000000 GROUP BY buildType"
+        val queryResult = influxdbClient.select(sql)
+
+        val rowList = mutableListOf<Triple<String, String, String>>()
+
+        if (null != queryResult && !queryResult.hasError()) {
+            queryResult.results.forEach { result ->
+                result.series.forEach { serie ->
+                    serie.run {
+                        val count = serie.values[0][1].let { if (it is Number) it.toInt() else 1 }
+                        val success = serie.values[0][2].let { if (it is Number) it.toInt() else 0 }
+                        rowList.add(
+                            Triple(
+                                tags["buildType"] ?: "Unknown",
+                                "${success * 100 / count}%",
+                                "www.tencent.com"
+                            )
+                        )
+                    }
+                }
+            }
+        } else {
+            logger.error("atomMonitor , get map error , errorMsg:${queryResult?.error}")
+        }
+
+        return "公共构建机统计" to rowList
     }
 
     fun atomMonitor(startTime: Long, endTime: Long): Pair<String, List<Triple<String, String, String>>> {
