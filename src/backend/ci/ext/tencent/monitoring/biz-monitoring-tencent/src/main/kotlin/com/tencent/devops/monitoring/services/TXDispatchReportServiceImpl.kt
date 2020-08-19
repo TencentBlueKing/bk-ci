f77283dcec758b7a7597f38c868e0196d032dc48
sawyersong2 <wudiduomjimo@gmail.com>
2020-08-19 15:16:32 +0800

    --story=859623181 新增monitor模块，监控收集构建信息

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/services/TXDispatchReportServiceImpl.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/services/TXDispatchReportServiceImpl.kt
@@ -5,7 +5,11 @@ import com.tencent.devops.monitoring.pojo.DispatchStatus
 import com.tencent.devops.monitoring.service.DispatchReportService
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.stereotype.Service
 
+@Service
+@RefreshScope
 class TXDispatchReportServiceImpl @Autowired constructor(
     private val influxdbClient: InfluxdbClient
 ) : DispatchReportService {
