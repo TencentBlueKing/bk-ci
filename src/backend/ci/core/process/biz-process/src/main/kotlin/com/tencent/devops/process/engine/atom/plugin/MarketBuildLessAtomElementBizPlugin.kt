e1c060d343ebc5d16e191e88e003e57d87feef11
linjun-ddddd <654548718@qq.com>
2020-08-19 19:54:41 +0800

    feature: 支持插件市场插件的回调机制 #2395

---
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/atom/plugin/MarketBuildLessAtomElementBizPlugin.kt
@@ -0,0 +1,26 @@
+package com.tencent.devops.process.engine.atom.plugin
+
+import com.tencent.devops.common.pipeline.enums.ChannelCode
+import com.tencent.devops.common.pipeline.pojo.element.atom.BeforeDeleteParam
+import com.tencent.devops.common.pipeline.pojo.element.market.MarketBuildLessAtomElement
+import com.tencent.devops.process.plugin.ElementBizPlugin
+import com.tencent.devops.process.plugin.annotation.ElementBiz
+
+@ElementBiz
+class MarketBuildLessAtomElementBizPlugin : ElementBizPlugin<MarketBuildLessAtomElement> {
+
+    override fun elementClass(): Class<MarketBuildLessAtomElement> {
+        return MarketBuildLessAtomElement::class.java
+    }
+
+    override fun afterCreate(element: MarketBuildLessAtomElement, projectId: String, pipelineId: String, pipelineName: String, userId: String, channelCode: ChannelCode, create: Boolean) {
+    }
+
+    override fun beforeDelete(element: MarketBuildLessAtomElement, param: BeforeDeleteParam) {
+        val inputMap = element.data["input"] as Map<*, *>
+        MarketBuildUtils.beforeDelete(inputMap, element.getAtomCode(), param)
+    }
+
+    override fun check(element: MarketBuildLessAtomElement, appearedCnt: Int) {
+    }
+}
---
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/atom/plugin/MarketBuildUtils.kt
@@ -0,0 +1,63 @@
+package com.tencent.devops.process.engine.atom.plugin
+
+import com.tencent.devops.common.api.util.OkhttpUtils
+import com.tencent.devops.common.pipeline.pojo.element.atom.BeforeDeleteParam
+import okhttp3.MediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.slf4j.LoggerFactory
+import java.util.concurrent.Executors
+import javax.ws.rs.HttpMethod
+
+
+object MarketBuildUtils {
+    private const val BK_ATOM_HOOK_URL = "bk_atom_hook_url"
+    private const val BK_ATOM_HOOK_URL_METHOD = "bk_atom_hook_url_method"
+    private const val BK_ATOM_HOOK_URL_BODY = "bk_atom_hook_url_body"
+
+    private val PROJECT_ID = "project_id"
+    private val PIPELINE_ID = "pipeline_id"
+    private val USER_ID = "user_id"
+
+    private val logger = LoggerFactory.getLogger(MarketBuildUtils::class.java)
+
+    val marketBuildExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())!!
+
+    fun beforeDelete(inputMap: Map<*, *>, atomCode: String, param: BeforeDeleteParam) {
+        marketBuildExecutorService.execute {
+            val bkAtomHookUrl = inputMap.getOrDefault(BK_ATOM_HOOK_URL, "") as String
+
+            if (bkAtomHookUrl.isBlank()) return@execute
+
+            val bkAtomHookUrlMethod = inputMap.getOrDefault(BK_ATOM_HOOK_URL_METHOD, "") as String
+            logger.info("start to execute atom hook url: $atomCode, $bkAtomHookUrlMethod, $bkAtomHookUrl")
+
+            var request = Request.Builder()
+                .url(resolveParam(bkAtomHookUrl, param))
+
+            when (bkAtomHookUrlMethod) {
+                HttpMethod.GET -> {
+                    request = request.get()
+                }
+                HttpMethod.POST -> {
+                    val requestBody = resolveParam(inputMap.getOrDefault(BK_ATOM_HOOK_URL_BODY, "") as String, param)
+                    request = request.post(RequestBody.create(MediaType.parse("text/plain; charset=utf-8"), requestBody))
+                }
+                HttpMethod.PUT -> {
+                    val requestBody = resolveParam(inputMap.getOrDefault(BK_ATOM_HOOK_URL_BODY, "") as String, param)
+                    request = request.put(RequestBody.create(MediaType.parse("text/plain; charset=utf-8"), requestBody))
+                }
+                HttpMethod.DELETE -> {
+                    request = request.delete()
+                }
+            }
+            OkhttpUtils.doHttp(request.build())
+        }
+    }
+
+    private fun resolveParam(str: String, param: BeforeDeleteParam): String {
+        return str.replace("{$PROJECT_ID}", param.projectId)
+            .replace("{$PIPELINE_ID}", param.pipelineId)
+            .replace("{$USER_ID}", param.userId)
+    }
+}
