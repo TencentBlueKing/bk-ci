34bd7c556a86d93c8d876314ffb5279bc642880c
royalhuang <royalhuang@tencent.com>
2020-08-25 11:51:57 +0800

    pref: 构建历史错误信息调整 #2437

--- src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/pojo/ErrorType.kt
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/pojo/ErrorType.kt
@@ -26,11 +26,11 @@
 
 package com.tencent.devops.common.api.pojo
 
-enum class ErrorType(val typeName: String, val visiable: Boolean) {
-    SYSTEM("系统运行错误", true), // 0 系统运行报错
-    USER("用户配置错误", true), // 1 用户配置报错
-    THIRD_PARTY("第三方系统错误", true), // 2 第三方系统接入错误
-    TASK_ATOM("插件执行错误", true); // 3 插件执行错误
+enum class ErrorType(val typeName: String, val num: Int) {
+    SYSTEM("系统运行错误", 0), // 0 系统运行报错
+    USER("用户配置错误", 1), // 1 用户配置报错
+    THIRD_PARTY("第三方系统错误", 2), // 2 第三方系统接入错误
+    PLUGIN("插件执行错误", 3); // 3 插件执行错误
 
     companion object {
 
@@ -48,7 +48,7 @@ enum class ErrorType(val typeName: String, val visiable: Boolean) {
                 0 -> SYSTEM
                 1 -> USER
                 2 -> THIRD_PARTY
-                else -> TASK_ATOM
+                else -> PLUGIN
             }
         }
     }
