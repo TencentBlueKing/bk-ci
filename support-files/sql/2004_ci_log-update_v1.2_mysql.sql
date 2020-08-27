8f41853c43aa853eee303d8004a17a30684c2a32
royalhuang <royalhuang@tencent.com>
2020-08-27 11:14:10 +0800

    feature: 日志支持归类展示 #2342

---
+++ support-files/sql/2004_ci_log-update_v1.2_mysql.sql
@@ -0,0 +1,43 @@
+USE devops_ci_log;
+SET NAMES utf8mb4;
+
+DROP PROCEDURE IF EXISTS ci_log_schema_update;
+
+DELIMITER <CI_UBF>
+
+CREATE PROCEDURE ci_log_schema_update()
+BEGIN
+
+    DECLARE db VARCHAR(100);
+    SET AUTOCOMMIT = 0;
+    SELECT DATABASE() INTO db;
+
+	IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_LOG_STATUS_V2'
+                    AND COLUMN_NAME = 'SUB_TAG') THEN
+        ALTER TABLE T_LOG_STATUS_V2
+			ADD COLUMN `SUB_TAG` VARCHAR(256) DEFAULT NULL;
+    END IF;
+
+  IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_LOG_SUBTAGS') THEN
+        CREATE TABLE `T_LOG_SUBTAGS` (
+        `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+        `BUILD_ID` varchar(64) NOT NULL,
+        `TAG` varchar(64) NOT NULL DEFAULT '' COMMENT '插件标签',
+        `SUB_TAGS` text NOT NULL COMMENT '插件子标签',
+        `CREATED_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+        PRIMARY KEY (`ID`),
+        UNIQUE KEY `BUILD_ID_2` (`BUILD_ID`,`TAG`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    END IF;
+
+    COMMIT;
+END <CI_UBF>
+DELIMITER ;
+COMMIT;
+CALL ci_log_schema_update();
