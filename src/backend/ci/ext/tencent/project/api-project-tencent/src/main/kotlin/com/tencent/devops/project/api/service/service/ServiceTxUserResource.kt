74390f5ed5ff6cc7c935a2a1023e0fcb8d547d82
cao150260 <fitzcao@tencent.com>
2020-08-27 10:52:50 +0800

    --story=859757921 project增加查询项目下管理员列表接口

--- src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/service/ServiceTxUserResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/service/ServiceTxUserResource.kt
@@ -8,8 +8,8 @@ import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.PathParam
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
