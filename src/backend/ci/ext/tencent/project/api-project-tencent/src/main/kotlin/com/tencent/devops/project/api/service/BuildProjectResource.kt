d671d454272fa1461581d60d6684cf84dea2926b
cao150260 <fitzcao@tencent.com>
2020-08-28 15:31:00 +0800

    --story=857735217 【sdk api】支持根据项目英文名获取项目组织架构信息接口

--- src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/BuildProjectResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/BuildProjectResource.kt
@@ -33,7 +33,12 @@ import com.tencent.devops.project.pojo.Result
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.PathParam
+import javax.ws.rs.HeaderParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["BUILD_PROJECT"], description = "蓝盾项目列表接口")
