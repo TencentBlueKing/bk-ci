2f9695766f057d8f47a4a63583e6c03e26267834
cao150260 <fitzcao@tencent.com>
2020-08-28 15:17:30 +0800

    --story=857735217 【sdk api】支持根据项目英文名获取项目组织架构信息接口

--- src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/BuildProjectResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/BuildProjectResource.kt
@@ -26,16 +26,14 @@
 
 package com.tencent.devops.project.api.service
 
+import com.tencent.devops.common.api.auth.AUTH_HEADER_PROJECT_ID
+import com.tencent.devops.project.api.pojo.ProjectOrganization
 import com.tencent.devops.project.pojo.ProjectVO
 import com.tencent.devops.project.pojo.Result
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.Consumes
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["BUILD_PROJECT"], description = "蓝盾项目列表接口")
@@ -52,4 +50,13 @@ interface BuildProjectResource {
         @PathParam(value = "projectCode")
         projectCode: String
     ): Result<List<ProjectVO>>
+
+    @GET
+    @Path("/organizations")
+    @ApiOperation("查询指定项目组织信息")
+    fun getProjectOrganizations(
+        @ApiParam(value = "项目Id", required = true)
+        @HeaderParam(value = AUTH_HEADER_PROJECT_ID)
+        projectCode: String
+    ): Result<ProjectOrganization?>
 }
