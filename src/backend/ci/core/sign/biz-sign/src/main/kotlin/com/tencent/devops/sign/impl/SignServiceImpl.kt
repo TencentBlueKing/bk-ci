62830a11e94d9af7ee74e942c534fe1dfd6cd345
freyzheng <freyzheng@tencent.com>
2020-09-01 11:20:52 +0800

    feature:新增bkci-sign服务模块 #1481

--- src/backend/ci/core/sign/biz-sign/src/main/kotlin/com/tencent/devops/sign/impl/SignServiceImpl.kt
+++ src/backend/ci/core/sign/biz-sign/src/main/kotlin/com/tencent/devops/sign/impl/SignServiceImpl.kt
@@ -361,6 +361,7 @@ class SignServiceImpl @Autowired constructor(
         properties["pipelineId"] = ipaSignInfo.pipelineId ?: ""
         properties["buildId"] = ipaSignInfo.buildId ?: ""
         properties["userId"] = ipaSignInfo.userId
+        properties["buildNo"] = if (ipaSignInfo.buildNum == null) "" else ipaSignInfo.buildNum.toString()
         properties["source"] = "pipeline"
         properties["ipa.sign.status"] = "true"
         return properties
