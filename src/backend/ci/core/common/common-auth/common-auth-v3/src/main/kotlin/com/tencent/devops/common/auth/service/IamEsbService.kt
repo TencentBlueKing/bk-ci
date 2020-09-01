30f12d45eba1ac7cc022f9408a03d72da1f2127b
cao150260 <fitzcao@tencent.com>
2020-09-01 16:42:17 +0800

    feature:对接蓝鲸V3权限中心 优化实例化 权限模型#1916

--- src/backend/ci/core/common/common-auth/common-auth-v3/src/main/kotlin/com/tencent/devops/common/auth/service/IamEsbService.kt
+++ src/backend/ci/core/common/common-auth/common-auth-v3/src/main/kotlin/com/tencent/devops/common/auth/service/IamEsbService.kt
@@ -11,6 +11,7 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 
 class IamEsbService() {
 
