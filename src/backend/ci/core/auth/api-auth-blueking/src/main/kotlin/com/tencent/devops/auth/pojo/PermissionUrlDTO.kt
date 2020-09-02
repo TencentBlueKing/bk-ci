281cda977b7c700892e8d923dcf9b0362bee7c05
cao150260 <fitzcao@tencent.com>
2020-09-02 18:17:36 +0800

    feature:对接蓝鲸V3权限中心 入参调整 权限模型#1916

--- src/backend/ci/core/auth/api-auth-blueking/src/main/kotlin/com/tencent/devops/auth/pojo/PermissionUrlDTO.kt
+++ src/backend/ci/core/auth/api-auth-blueking/src/main/kotlin/com/tencent/devops/auth/pojo/PermissionUrlDTO.kt
@@ -2,9 +2,10 @@ package com.tencent.devops.auth.pojo
 
 import com.tencent.devops.common.auth.api.AuthPermission
 import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.pojo.Instance
 
 data class PermissionUrlDTO(
     val actionId: AuthPermission,
     val resourceId: AuthResourceType,
-    val instanceId: List<String>
+    val instanceId: List<Instance>
 )
