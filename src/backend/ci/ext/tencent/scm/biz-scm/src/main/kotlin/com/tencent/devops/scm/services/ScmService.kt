9c6ca8304d81124b6964eacaa788a8a8accb31e2
mingshewhe <mingshewhe@tencent.com>
2020-09-04 21:10:09 +0800

    --story=859729553 新增腾讯工蜂git.code.tencent.com的事件监听
    增加调试日志

--- src/backend/ci/ext/tencent/scm/biz-scm/src/main/kotlin/com/tencent/devops/scm/services/ScmService.kt
+++ src/backend/ci/ext/tencent/scm/biz-scm/src/main/kotlin/com/tencent/devops/scm/services/ScmService.kt
@@ -258,7 +258,7 @@ class ScmService @Autowired constructor(
             ScmFactory.getScm(projectName, url, type, null, privateKey, passPhrase, token, region, userName, event)
                 .addWebHook(hookUrl)
         } catch (e: Exception) {
-            logger.error("add webhook error", e)
+            logger.error("[$projectName|$url|$type|$token|$region|$userName|$event] add webhook error", e)
             throw e
         } finally {
             logger.info("It took ${System.currentTimeMillis() - startEpoch}ms to add web hook")
