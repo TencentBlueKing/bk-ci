cd53ce6d10d20a1933912c8e2f6f0b5b04b27167
cao150260 <fitzcao@tencent.com>
2020-09-18 14:38:43 +0800

    feat:节点列表通过websocket刷新变更 #2594

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/websocket/page/NodePath.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/websocket/page/NodePath.kt
@@ -5,6 +5,6 @@ import com.tencent.devops.common.websocket.pojo.BuildPageInfo
 
 class NodePath: IPath {
 	override fun buildPage(buildPageInfo: BuildPageInfo): String {
-		return "console/environment/${buildPageInfo.projectId}/nodeList"
+		return "/console/environment/${buildPageInfo.projectId}/nodeList"
 	}
 }
