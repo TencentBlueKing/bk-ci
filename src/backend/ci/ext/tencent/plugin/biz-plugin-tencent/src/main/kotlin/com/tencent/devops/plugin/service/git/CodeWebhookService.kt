d242479fe4dda3d73016b74f982f81274ce7f24d
mingshewhe <mingshewhe@tencent.com>
2020-09-24 21:14:51 +0800

    revert --story=859603791 如果是push触发，就增加回写commit checker的逻辑

--- src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/service/git/CodeWebhookService.kt
+++ src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/service/git/CodeWebhookService.kt
@@ -134,9 +134,7 @@ class CodeWebhookService @Autowired constructor(
             val webhookEventType = CodeEventType.valueOf(webhookEventTypeStr!!)
 
             when {
-                webhookType == CodeType.GIT &&
-                    (webhookEventType == CodeEventType.MERGE_REQUEST ||
-                        webhookEventType == CodeEventType.PUSH) -> {
+                webhookType == CodeType.GIT && webhookEventType == CodeEventType.MERGE_REQUEST  -> {
                     val block = variables[PIPELINE_WEBHOOK_BLOCK]?.toBoolean() ?: false
                     val mrId = variables[PIPELINE_WEBHOOK_MR_ID]?.toLong()
                     val state =
