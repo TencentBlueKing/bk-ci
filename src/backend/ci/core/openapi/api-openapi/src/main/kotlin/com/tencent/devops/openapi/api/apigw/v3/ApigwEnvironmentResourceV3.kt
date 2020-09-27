d0818e4851ec5678b391890bc55f58c2d4f5e5eb
cao150260 <fitzcao@tencent.com>
2020-09-27 18:10:05 +0800

    feat: 提供第三方构建机列表及状态的OpenAPI接口 #2675

---
+++ src/backend/ci/core/openapi/api-openapi/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v3/ApigwEnvironmentResourceV3.kt
@@ -0,0 +1,58 @@
+package com.tencent.devops.openapi.api.apigw.v3
+
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.environment.pojo.NodeWithPermission
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+
+@Api(tags = ["OPENAPI_ENVIRONMENT_V3"], description = "OPENAPI-环境管理")
+@Path("/{apigwType:apigw-user|apigw-app|apigw}/v3/projects/{projectId}/environment")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+interface ApigwEnvironmentResourceV3 {
+
+    @ApiOperation("获取项目下第三方构建机列表")
+    @GET
+    @Path("/thirdPartAgent/nodeList")
+    fun thirdPartAgentList(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        userId: String,
+        @ApiParam(value = "projectId", required = true)
+        @PathParam("projectId")
+        projectId: String
+    ): Result<List<NodeWithPermission>>
+
+    @ApiOperation("获取指定构建机状态")
+    @GET
+    @Path("/thirdPartAgent/nodes/{nodeId}/status")
+    fun getNodeStatus(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        userId: String,
+        @ApiParam(value = "projectId", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam("节点 hashIds", required = true)
+        nodeHashIds: List<String>
+    ): Result<List<NodeWithPermission>>
+}
---
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v3/ApigwEnvironmentResourceV3Impl.kt
@@ -0,0 +1,30 @@
+package com.tencent.devops.openapi.resources.apigw.v3
+
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.environment.api.ServiceNodeResource
+import com.tencent.devops.environment.pojo.NodeWithPermission
+import com.tencent.devops.openapi.api.apigw.v3.ApigwEnvironmentResourceV3
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+
+@RestResource
+class ApigwEnvironmentResourceV3Impl @Autowired constructor(
+		val client: Client
+) : ApigwEnvironmentResourceV3 {
+
+	override fun thirdPartAgentList(appCode: String?, apigwType: String?, userId: String, projectId: String): Result<List<NodeWithPermission>> {
+		logger.info("thirdPartAgentList userId $userId, project $projectId")
+		return client.get(ServiceNodeResource::class).listUsableServerNodes(userId, projectId)
+	}
+
+	override fun getNodeStatus(appCode: String?, apigwType: String?, userId: String, projectId: String, nodeHashIds: List<String>): Result<List<NodeWithPermission>> {
+		logger.info("getNodeStatus userId:$userId, projectId: $projectId, nodeHashIds: $nodeHashIds")
+		return client.get(ServiceNodeResource::class).listByHashIds(userId, projectId, nodeHashIds)
+	}
+
+	companion object {
+		val logger = LoggerFactory.getLogger(this:: class.java)
+	}
+}
