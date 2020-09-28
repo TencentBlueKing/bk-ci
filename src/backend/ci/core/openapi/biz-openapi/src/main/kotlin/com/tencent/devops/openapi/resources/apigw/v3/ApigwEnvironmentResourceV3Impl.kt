6541539d8514cfe61aabb8bff4fa48dfec6dfa38
cao150260 <fitzcao@tencent.com>
2020-09-28 11:22:01 +0800

    feat: 提供第三方构建机列表及状态的OpenAPI接口 #2675

--- src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v3/ApigwEnvironmentResourceV3Impl.kt
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v3/ApigwEnvironmentResourceV3Impl.kt
@@ -1,9 +1,11 @@
 package com.tencent.devops.openapi.resources.apigw.v3
 
+import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.environment.api.ServiceNodeResource
+import com.tencent.devops.environment.constant.EnvironmentMessageCode
 import com.tencent.devops.environment.pojo.NodeBaseInfo
 import com.tencent.devops.environment.pojo.NodeWithPermission
 import com.tencent.devops.environment.pojo.enums.NodeType
@@ -21,13 +23,16 @@ class ApigwEnvironmentResourceV3Impl @Autowired constructor(
 		return client.get(ServiceNodeResource::class).listNodeByNodeType(projectId, NodeType.THIRDPARTY)
 	}
 
-	override fun getNodeStatus(appCode: String?, apigwType: String?, userId: String, projectId: String, nodeId: String): Result<NodeWithPermission?> {
-		logger.info("getNodeStatus userId:$userId, projectId: $projectId, nodeId: $nodeId")
-		val nodeList = client.get(ServiceNodeResource::class).listByHashIds(userId, projectId, arrayListOf(nodeId)).data
+	override fun getNodeStatus(appCode: String?, apigwType: String?, userId: String, projectId: String, nodeHashId: String): Result<NodeWithPermission?> {
+		logger.info("getNodeStatus userId:$userId, projectId: $projectId, nodeHashId: $nodeHashId")
+		val nodeList = client.get(ServiceNodeResource::class).listByHashIds(userId, projectId, arrayListOf(nodeHashId)).data
 		if(nodeList != null && nodeList.isNotEmpty()) {
 			return Result(nodeList[0])
 		}
-		return Result(null)
+		throw ErrorCodeException(
+				errorCode = EnvironmentMessageCode.ERROR_NODE_NOT_EXISTS,
+				params = arrayOf(nodeHashId)
+		)
 	}
 
 	companion object {
