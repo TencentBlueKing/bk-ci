7bbc4f78fef4fa79c13b179a9188826ed016df6a
cao150260 <fitzcao@tencent.com>
2020-10-12 16:57:44 +0800

    --bug=82661245 turbo 解耦出主流程 格式化

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/turbo/PipelineBuildTurboExtService.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/turbo/PipelineBuildTurboExtService.kt
@@ -18,65 +18,64 @@ import java.util.*
 
 @Service
 class PipelineBuildTurboExtService @Autowired constructor(
-		private val consulClient: ConsulDiscoveryClient?
+    private val consulClient: ConsulDiscoveryClient?
 ) : PipelineBuildExtService {
 
-	override fun buildExt(task: PipelineBuildTask): Map<String, String> {
-		val taskType = task.taskType
-		if(taskType.contains("linuxPaasCodeCCScript") || taskType.contains("linuxScript")) {
-			logger.info("task need turbo, ${task.buildId}, ${task.taskName}, ${task.taskType}")
-			val turboTask = getTurboTask(task.pipelineId, task.taskId)
-			return mutableMapOf(
-					PIPELINE_TURBO_TASK_ID to turboTask
-			)
-		}
-		return emptyMap()
-	}
+    override fun buildExt(task: PipelineBuildTask): Map<String, String> {
+        val taskType = task.taskType
+        if (taskType.contains("linuxPaasCodeCCScript") || taskType.contains("linuxScript")) {
+            logger.info("task need turbo, ${task.buildId}, ${task.taskName}, ${task.taskType}")
+            val turboTask = getTurboTask(task.pipelineId, task.taskId)
+            return mutableMapOf(
+                    PIPELINE_TURBO_TASK_ID to turboTask
+            )
+        }
+        return emptyMap()
+    }
 
-	fun getTurboTask(pipelineId: String, elementId: String): String {
-		try {
-			val instances = consulClient!!.getInstances("turbo")
-					?: return ""
-			if (instances.isEmpty()) {
-				return ""
-			}
-			val instance = loadBalance(instances)
-			val url = "${if (instance.isSecure) "https" else
-				"http"}://${instance.host}:${instance.port}/api/service/turbo/task/pipeline/$pipelineId/$elementId"
+    fun getTurboTask(pipelineId: String, elementId: String): String {
+        try {
+            val instances = consulClient!!.getInstances("turbo")
+                    ?: return ""
+            if (instances.isEmpty()) {
+                return ""
+            }
+            val instance = loadBalance(instances)
+            val url = "${if (instance.isSecure) "https" else
+                "http"}://${instance.host}:${instance.port}/api/service/turbo/task/pipeline/$pipelineId/$elementId"
 
-			logger.info("Get turbo task info, request url: $url")
-			val startTime = System.currentTimeMillis()
-			val request = Request.Builder().url(url).get().build()
-			LogUtils.costTime("call turbo cost", startTime)
-			OkhttpUtils.doHttp(request).use { response ->
-				val data = response.body()?.string() ?: return ""
-				logger.info("Get turbo task info, response: $data")
-				if (!response.isSuccessful) {
-					throw RemoteServiceException(data)
-				}
-				val responseData: Map<String, Any> = jacksonObjectMapper().readValue(data)
-				val code = responseData["status"] as Int
-				if (0 == code) {
-					val dataMap = responseData["data"] as Map<String, Any>
-					return dataMap["taskId"] as String? ?: ""
-				} else {
-					throw RemoteServiceException(data)
-				}
-			}
-		} catch (e: Throwable) {
-			logger.warn("Get turbo task info failed, $e")
-			return ""
-		}
-	}
+            logger.info("Get turbo task info, request url: $url")
+            val startTime = System.currentTimeMillis()
+            val request = Request.Builder().url(url).get().build()
+            OkhttpUtils.doHttp(request).use { response ->
+                val data = response.body()?.string() ?: return ""
+                logger.info("Get turbo task info, response: $data")
+                if (!response.isSuccessful) {
+                    throw RemoteServiceException(data)
+                }
+                val responseData: Map<String, Any> = jacksonObjectMapper().readValue(data)
+                val code = responseData["status"] as Int
+                if (0 == code) {
+                    val dataMap = responseData["data"] as Map<String, Any>
+                    return dataMap["taskId"] as String? ?: ""
+                } else {
+                    throw RemoteServiceException(data)
+                }
+            }
+            LogUtils.costTime("call turbo cost", startTime)
+        } catch (e: Throwable) {
+            logger.warn("Get turbo task info failed, $e")
+            return ""
+        }
+    }
 
-	fun loadBalance(instances: List<ServiceInstance>) :  ServiceInstance{
-		val random = Random()
-		val index = random.nextInt(instances.size)
-		return instances[index]
-	}
+    fun loadBalance(instances: List<ServiceInstance>): ServiceInstance {
+        val random = Random()
+        val index = random.nextInt(instances.size)
+        return instances[index]
+    }
 
-
-	companion object {
-		val logger = LoggerFactory.getLogger(this :: class.java)
-	}
+    companion object {
+        val logger = LoggerFactory.getLogger(this :: class.java)
+    }
 }
