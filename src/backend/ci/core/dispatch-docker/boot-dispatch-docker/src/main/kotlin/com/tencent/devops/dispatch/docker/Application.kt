84e482342dbb97a477a416756bf5534f07ed25fa
sawyersong2 <wudiduomjimo@gmail.com>
2020-10-12 20:22:44 +0800

    feat: 拆封dispatcher-docker为独立服务 #2711

--- src/backend/ci/core/dispatch-docker/boot-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/docker/Application.kt
+++ src/backend/ci/core/dispatch-docker/boot-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/docker/Application.kt
@@ -23,7 +23,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.tencent.devops.dispatch.docker
 
 import com.tencent.devops.common.service.MicroService
@@ -31,7 +30,7 @@ import com.tencent.devops.common.service.MicroServiceApplication
 import org.springframework.context.annotation.ComponentScan
 
 @MicroService
-@ComponentScan("com.tencent.devops.dispatch")
+@ComponentScan("com.tencent.devops.dispatch", "com.tencent.devops.common.dispatch.sdk")
 class Application
 
 fun main(args: Array<String>) {
