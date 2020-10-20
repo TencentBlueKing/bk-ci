7cbf72ecc1a07ece4b2ee129f9734626521343ab
cao150260 <fitzcao@tencent.com>
2020-10-20 15:32:31 +0800

    feat:质量红线查询优化 去鎖 #2799

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineBuildService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineBuildService.kt
@@ -1588,9 +1588,6 @@ class PipelineBuildService(
                 model = model
             )
 
-            // 加锁为了保证同一流水线的同时构建数量拦截的准确性
-            runLock.lock()
-
             val interceptResult = pipelineInterceptorChain.filter(InterceptData(readyToBuildPipelineInfo, fullModel, startType))
             if (interceptResult.isNotOk()) {
                 // 发送排队失败的事件
@@ -1641,7 +1638,7 @@ class PipelineBuildService(
             pipelineRuntimeService.initBuildParameters(buildId)
             return buildId
         } finally {
-            runLock.unlock()
+            if (readyToBuildPipelineInfo.channelCode !in NO_LIMIT_CHANNEL) runLock.unlock()
         }
     }
 
