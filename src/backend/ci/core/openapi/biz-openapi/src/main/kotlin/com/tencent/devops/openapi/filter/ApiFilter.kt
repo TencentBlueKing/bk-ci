041b2ab26ed738f703ab99f4a6a71b99657c3411
irwinsun <irwinsun@tencent.com>
2020-10-20 21:05:00 +0800

    format: 代码缺陷规范处理 #2558 减少复杂度

--- src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/filter/ApiFilter.kt
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/filter/ApiFilter.kt
@@ -39,7 +39,6 @@ import net.sf.json.JSONObject
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.jce.provider.JCERSAPublicKey
 import org.bouncycastle.openssl.PEMReader
-import org.bouncycastle.openssl.PasswordFinder
 import org.slf4j.LoggerFactory
 import java.io.ByteArrayInputStream
 import java.io.InputStreamReader
@@ -57,89 +56,92 @@ class ApiFilter(
     private val apiGatewayUtil: ApiGatewayUtil
 ) : ContainerRequestFilter {
 
-    private val excludeVeritfyPath =
-        listOf("/api/apigw/", "/api/apigw-user/", "/api/apigw-app/")
-
     companion object {
         private val logger = LoggerFactory.getLogger(ApiFilter::class.java)
+        private const val appCodeHeader = "app_code"
+        private const val appSecHeader = "app_secret"
+        private const val jwtHeader = "X-Bkapi-JWT"
+        private const val apigwSourceHeader = "X-DEVOPS-APIGW-TYPE"
+    }
+
+    enum class ApiType(val startContextPath: String) {
+        DEFAULT("/api/apigw/"),
+        USER("/api/apigw-user/"),
+        APP("/api/apigw-app/");
+
+        companion object {
+            fun parseType(path: String): ApiType? {
+                values().forEach { type ->
+                    if (path.contains(other = type.startContextPath, ignoreCase = true)) {
+                        return type
+                    }
+                }
+                return null
+            }
+        }
     }
 
     override fun filter(requestContext: ContainerRequestContext) {
 
         // path为为空的时候，直接退出
         val path = requestContext.uriInfo.requestUri.path
-        logger.info("uriInfo uriInfo[$path]")
         // 判断是否需要处理apigw
-        var pass = true
-        excludeVeritfyPath.forEach {
-            if (path.contains(it)) {
-                pass = false
-            }
-        }
-        if (pass) {
-            return
-        }
+        val apiType = ApiType.parseType(path) ?: return
+
+        logger.info("FILTER| url=$path")
         if (!apiGatewayUtil.isAuth()) {
             // 将query中的app_code和app_secret设置成头部
-            val pathparam = requestContext.getUriInfo().pathParameters
-            pathparam.forEach {
-                if (it.key == "app_code" && it.value.isNotEmpty()) {
-                    requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE]?.set(0, null)
-                    if (requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE] != null) {
-                        requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE]?.set(0, it.value[0])
-                    } else {
-                        requestContext.headers.add(AUTH_HEADER_DEVOPS_APP_CODE, it.value[0])
-                    }
-                }
-                if (it.key == "app_secret" && it.value.isNotEmpty()) {
-                    requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET]?.set(0, null)
-                    if (requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET] != null) {
-                        requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET]?.set(0, it.value[0])
-                    } else {
-                        requestContext.headers.add(AUTH_HEADER_DEVOPS_APP_CODE, it.value[0])
-                    }
-                }
-            }
+            setupHeader(requestContext)
         } else {
-            val valid = verifyJWT(requestContext)
             // 验证通过
-            if (!valid) {
-                requestContext.abortWith(
-                    Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Devops OpenAPI Auth fail：user or app auth fail.")
-                        .build()
-                )
+            if (!verifyJWT(requestContext, apiType)) {
+                requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST).entity("Devops OpenAPI Auth fail：user or app auth fail.").build())
                 return
             }
         }
     }
 
-    fun verifyJWT(requestContext: ContainerRequestContext): Boolean {
-        val bkApiJwt = requestContext.getHeaderString("X-Bkapi-JWT")
-        val apigwtType = requestContext.getHeaderString("X-DEVOPS-APIGW-TYPE")
+    private fun setupHeader(requestContext: ContainerRequestContext) {
+        requestContext.uriInfo?.pathParameters?.forEach { pathParam ->
+            if (pathParam.key == appCodeHeader && pathParam.value.isNotEmpty()) {
+                requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE]?.set(0, null)
+                if (requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE] != null) {
+                    requestContext.headers[AUTH_HEADER_DEVOPS_APP_CODE]?.set(0, pathParam.value[0])
+                } else {
+                    requestContext.headers.add(AUTH_HEADER_DEVOPS_APP_CODE, pathParam.value[0])
+                }
+            } else if (pathParam.key == appSecHeader && pathParam.value.isNotEmpty()) {
+                requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET]?.set(0, null)
+                if (requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET] != null) {
+                    requestContext.headers[AUTH_HEADER_DEVOPS_APP_SECRET]?.set(0, pathParam.value[0])
+                } else {
+                    requestContext.headers.add(AUTH_HEADER_DEVOPS_APP_CODE, pathParam.value[0])
+                }
+            }
+        }
+    }
+
+    fun verifyJWT(requestContext: ContainerRequestContext, apiType: ApiType): Boolean {
+        val bkApiJwt = requestContext.getHeaderString(jwtHeader)
         if (bkApiJwt.isNullOrBlank()) {
             logger.error("Request bk api jwt is empty for ${requestContext.request}")
-            requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST)
-                .entity("Request bkapi jwt is empty.")
-                .build())
+            requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST).entity("Request bkapi jwt is empty.").build())
             return false
         }
 
-        val uriPath = requestContext.uriInfo.requestUri.path
-        val apiType = if (uriPath.startsWith("/api/apigw-app")) "apigw-app" else "apigw-user"
-
-        logger.info("Get the bkApiJwt header, X-Bkapi-JWT：$bkApiJwt")
-        val jwt = parseJwt(bkApiJwt, apigwtType)
-        logger.info("Get the parse bkApiJwt($jwt)")
+        val apigwSource = requestContext.getHeaderString(apigwSourceHeader)
+        logger.debug("Get the bkApiJwt header, X-Bkapi-JWT：{}", bkApiJwt)
+        val jwt = parseJwt(bkApiJwt, apigwSource)
+        logger.debug("Get the parse bkApiJwt({})", jwt)
 
         // 验证应用身份信息
         if (jwt.has("app")) {
             val app = jwt.getJSONObject("app")
             // 应用身份登录
-            if (app.has("app_code")) {
-                val appCode = app.getString("app_code")
+            if (app.has(appCodeHeader)) {
+                val appCode = app.getString(appCodeHeader)
                 val verified = app.get("verified") as Boolean
-                if (apigwtType == "apigw-app" && (appCode.isNullOrEmpty() || !verified)) {
+                if (apiType == ApiType.APP && (appCode.isNullOrEmpty() || !verified)) {
                     return false
                 } else {
                     if (!appCode.isNullOrBlank()) {
@@ -171,10 +173,8 @@ class ApiFilter(
                     } else {
                         requestContext.headers.add(AUTH_HEADER_DEVOPS_USER_ID, username)
                     }
-                } else if (apiType == "apigw-user") {
-                    requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Request don't has user's access_token.")
-                        .build())
+                } else if (apiType == ApiType.USER) {
+                    requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST).entity("Request don't has user's access_token.").build())
                     return false
                 }
             }
@@ -192,7 +192,7 @@ class ApiFilter(
             }
             Security.addProvider(BouncyCastleProvider())
             val bais = ByteArrayInputStream(key)
-            reader = PEMReader(InputStreamReader(bais), PasswordFinder { "".toCharArray() })
+            reader = PEMReader(InputStreamReader(bais)) { "".toCharArray() }
             val keyPair = reader.readObject() as JCERSAPublicKey
             val jwtParser = Jwts.parser().setSigningKey(keyPair)
             val parse = jwtParser.parse(bkApiJwt)
@@ -200,11 +200,7 @@ class ApiFilter(
             return JSONObject.fromObject(parse.body)
         } catch (e: Exception) {
             logger.error("Parse jwt failed.", e)
-            throw ErrorCodeException(
-                errorCode = ERROR_OPENAPI_JWT_PARSE_FAIL,
-                defaultMessage = "Parse jwt failed",
-                params = arrayOf(bkApiJwt)
-            )
+            throw ErrorCodeException(errorCode = ERROR_OPENAPI_JWT_PARSE_FAIL, defaultMessage = "Parse jwt failed", params = arrayOf(bkApiJwt))
         } finally {
             reader?.close()
         }
