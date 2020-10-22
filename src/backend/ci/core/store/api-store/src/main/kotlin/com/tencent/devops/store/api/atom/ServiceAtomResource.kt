10db22a27b9f8cd326d5828ae0f7b57d3c4e64f8
cao150260 <fitzcao@tencent.com>
2020-10-22 11:24:50 +0800

    feature: 流水线支持导入/导出为JSON 默认插件过滤 #1030

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/api/atom/ServiceAtomResource.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/api/atom/ServiceAtomResource.kt
@@ -31,7 +31,12 @@ import com.tencent.devops.store.pojo.atom.InstalledAtom
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.PathParam
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["SERVICE_PIPELINE_ATOM"], description = "流水线-插件")
