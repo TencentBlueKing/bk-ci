fbf2db36e7da0a751190546c1697e5ecea0310bf
stubenhuang <stubenhuang@tencent.com>
2020-10-26 21:02:24 +0800

    feat: 每个项目最多创建N条流水线 #2362

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/impl/AbsOpProjectServiceImpl.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/impl/AbsOpProjectServiceImpl.kt
@@ -453,7 +453,8 @@ abstract class AbsOpProjectServiceImpl @Autowired constructor(
             grayFlag = grayProjectSet.contains(projectData.englishName),
             hybridCCAppId = projectData.hybridCcAppId,
             enableExternal = projectData.enableExternal,
-            enableIdc = projectData.enableIdc
+            enableIdc = projectData.enableIdc,
+            pipelineLimit = projectData.pipelineLimit
         )
     }
 
@@ -532,7 +533,8 @@ abstract class AbsOpProjectServiceImpl @Autowired constructor(
             macosGrayFlag = macosProjectSet.contains(projectData.englishName),
             hybridCCAppId = projectData.hybridCcAppId,
             enableExternal = projectData.enableExternal,
-            enableIdc = projectData.enableIdc
+            enableIdc = projectData.enableIdc,
+            pipelineLimit = projectData.pipelineLimit
         )
     }
 
