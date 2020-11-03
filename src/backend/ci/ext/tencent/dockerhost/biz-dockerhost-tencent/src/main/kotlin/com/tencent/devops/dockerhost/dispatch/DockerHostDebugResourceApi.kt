38b8c754fb31f65815177293ec615f92a01828ba
sawyersong2 <wudiduomjimo@gmail.com>
2020-11-03 21:07:23 +0800

    --bug=83087059 DockerHost服务灰度逻辑调整

--- src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/dispatch/DockerHostDebugResourceApi.kt
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/dispatch/DockerHostDebugResourceApi.kt
@@ -32,6 +32,7 @@ import com.tencent.devops.common.api.pojo.ErrorCode
 import com.tencent.devops.common.api.pojo.ErrorType
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.api.util.OkhttpUtils
+import com.tencent.devops.common.service.gray.Gray
 import com.tencent.devops.dispatch.pojo.ContainerInfo
 import com.tencent.devops.dockerhost.config.DockerHostConfig
 import okhttp3.MediaType
@@ -41,8 +42,9 @@ import org.springframework.stereotype.Service
 
 @Service
 class DockerHostDebugResourceApi constructor(
-    dockerHostConfig: DockerHostConfig
-) : AbstractBuildResourceApi(dockerHostConfig) {
+    dockerHostConfig: DockerHostConfig,
+    gray: Gray
+) : AbstractBuildResourceApi(dockerHostConfig, gray) {
     private val logger = LoggerFactory.getLogger(DockerHostDebugResourceApi::class.java)
 
     fun startDebug(hostTag: String): Result<ContainerInfo>? {
