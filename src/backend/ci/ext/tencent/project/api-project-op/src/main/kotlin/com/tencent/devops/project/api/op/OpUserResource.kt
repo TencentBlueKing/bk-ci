871270c13b09df544dd66cd9d60bd23a3a5ab750
cao150260 <fitzcao@tencent.com>
2020-11-05 22:36:01 +0800

    --bug=82995263 [project] 组织信息解耦tof

--- src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OpUserResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OpUserResource.kt
@@ -5,7 +5,11 @@ import com.tencent.devops.project.pojo.user.UserDeptDetail
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.PathParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OP_PROJECT_USER"], description = "OP_用户")
