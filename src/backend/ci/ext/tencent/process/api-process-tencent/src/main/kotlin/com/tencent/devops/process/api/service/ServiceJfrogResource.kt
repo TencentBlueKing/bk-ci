669fb8f73a65561f2097733de37620d0bf68f7ba
irwinsun <irwinsun@tencent.com>
2020-11-05 21:51:43 +0800

    fix: 规范Process的后台接口响应异常处理 #2836 移除未用接口

--- src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceJfrogResource.kt
+++ src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceJfrogResource.kt
@@ -39,7 +39,7 @@ import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
-@Api(tags = ["SERVICE_JFROG"], description = "服务-jfrog资源")
+@Api(tags = ["SERVICE_JFROG"], description = "服务-Artifactory对接Process接口的资源")
 @Path("/service/jfrog")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -57,21 +57,6 @@ interface ServiceJfrogResource {
         pipelineIds: Set<String>
     ): Result<Map<String, String>>
 
-    @ApiOperation("根据构建id获取构建号")
-    @POST
-    // @Path("/projects/{projectId}/pipelines/{pipelineId}/getBuildNos")
-    @Path("/{projectId}/{pipelineId}/getBuildNos")
-    fun getBuildNoByBuildIds(
-        @ApiParam("项目id", required = true)
-        @PathParam("projectId")
-        projectId: String,
-        @ApiParam("流水线id", required = true)
-        @PathParam("pipelineId")
-        pipelineId: String,
-        @ApiParam("构建id列表", required = true)
-        buildIds: Set<String>
-    ): Result<Map<String, String>>
-
     @ApiOperation("根据流水线id和构建id对，获取build num")
     @POST
     // @Path("/projects/{projectId}/getBuildNoByBuildIds")
@@ -80,17 +65,4 @@ interface ServiceJfrogResource {
         @ApiParam("项目id", required = true)
         buildIds: Set<String>
     ): Result<Map<String, String>>
-
-    @ApiOperation("获取时间段内有构建产物的构建数量")
-    @GET
-    // @Path("/countArtifactoryByTime")
-    @Path("/getArtifactoryByTime/count")
-    fun getArtifactoryCountFromHistory(
-        @ApiParam("起始时间", required = true)
-        @QueryParam("startTime")
-        startTime: Long,
-        @ApiParam("终止时间", required = true)
-        @QueryParam("endTime")
-        endTime: Long
-    ): Result<Int>
 }
