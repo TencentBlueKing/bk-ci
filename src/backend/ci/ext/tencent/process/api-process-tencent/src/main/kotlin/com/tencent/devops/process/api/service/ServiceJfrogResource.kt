5b58296c22beceb5808c3c3f04d1abec91d8c1ff
irwinsun <irwinsun@tencent.com>
2020-11-06 12:26:54 +0800

    fix: 规范Process的后台接口响应异常处理 #2836 unused import

--- src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceJfrogResource.kt
+++ src/backend/ci/ext/tencent/process/api-process-tencent/src/main/kotlin/com/tencent/devops/process/api/service/ServiceJfrogResource.kt
@@ -31,12 +31,10 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
-import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["SERVICE_JFROG"], description = "服务-Artifactory对接Process接口的资源")
