a019646b8dd4035d459875b153c053a67b737794
cao150260 <fitzcao@tencent.com>
2020-11-11 18:13:40 +0800

    refactor: project 代码结构调整 兼容检验  #2886

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/pojo/ProjectCreateExtInfo.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/pojo/ProjectCreateExtInfo.kt
@@ -1,6 +1,6 @@
 package com.tencent.devops.project.pojo
 
-data class ProjectCreateExtInfo (
-		val needValidate: Boolean? = true,
-		val needAuth: Boolean? = true
+data class ProjectCreateExtInfo(
+    val needValidate: Boolean? = true,
+    val needAuth: Boolean? = true
 )
