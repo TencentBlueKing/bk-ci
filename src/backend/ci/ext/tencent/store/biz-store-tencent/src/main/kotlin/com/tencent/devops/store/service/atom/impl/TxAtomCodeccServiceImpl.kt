2c00bc7301eaa44e07b80260d2f6ae51bd46c95c
carlyin <449036249@qq.com>
2020-11-13 10:24:22 +0800

    --story=859928985 【研发商店】流水线插件等自动创建代码库的组件，增加代码质量检查机制

--- src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/atom/impl/TxAtomCodeccServiceImpl.kt
+++ src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/atom/impl/TxAtomCodeccServiceImpl.kt
@@ -50,10 +50,12 @@ import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service(value = "ATOM_CODECC_COMMON_SERVICE")
+@RefreshScope
 class TxAtomCodeccServiceImpl @Autowired constructor() : TxStoreCodeccCommonService {
 
     private val logger = LoggerFactory.getLogger(TxAtomCodeccServiceImpl::class.java)
