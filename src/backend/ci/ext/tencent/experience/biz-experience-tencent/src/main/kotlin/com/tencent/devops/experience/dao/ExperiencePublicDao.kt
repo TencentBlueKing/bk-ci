b137f088bbb0dea2e32429c50f806fa07a8d1fa8
stubenhuang <stubenhuang@tencent.com>
2020-11-21 15:43:37 +0800

    --story=860816285 新蓝盾APP后台story(仅供新分支挂靠)

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/dao/ExperiencePublicDao.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/dao/ExperiencePublicDao.kt
@@ -241,8 +241,9 @@ class ExperiencePublicDao {
     fun addDownloadTimeByRecordId(dslContext: DSLContext, recordId: Long) {
         with(TExperiencePublic.T_EXPERIENCE_PUBLIC) {
             dslContext.update(this)
-                .set(DOWNLOAD_TIME, DOWNLOAD_TIME.add(1))
+                .set(DOWNLOAD_TIME, DOWNLOAD_TIME.plus(1))
                 .where(RECORD_ID.eq(recordId))
+                .execute()
         }
     }
 }
