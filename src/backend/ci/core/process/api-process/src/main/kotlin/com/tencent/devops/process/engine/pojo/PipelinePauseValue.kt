78af29ca848a76d49cca2fb399cdcbd920eeaba9
cao150260 <fitzcao@tencent.com>
2020-11-23 22:48:52 +0800

    feat:流水线TASK执行前支持自动暂停 element替换优化 #1260

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/engine/pojo/PipelinePauseValue.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/engine/pojo/PipelinePauseValue.kt
@@ -25,7 +25,7 @@ package com.tencent.devops.process.engine.pojo
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-data class PipelinePauseValue (
+data class PipelinePauseValue(
     val buildId: String,
     val taskId: String,
     val defaultValue: String
