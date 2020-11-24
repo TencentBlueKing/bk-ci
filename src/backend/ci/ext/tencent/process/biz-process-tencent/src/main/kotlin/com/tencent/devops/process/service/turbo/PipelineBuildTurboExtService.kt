b7dea1da849f8fb9a938f78fcbdab7affff1ef04
cao150260 <fitzcao@tencent.com>
2020-11-24 15:10:57 +0800

    --story=861162917 插件暂停需发送rtx通知

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/turbo/PipelineBuildTurboExtService.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/turbo/PipelineBuildTurboExtService.kt
@@ -5,20 +5,13 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.exception.RemoteServiceException
 import com.tencent.devops.common.api.util.OkhttpUtils
 import com.tencent.devops.common.client.Client
-import com.tencent.devops.common.notify.enums.NotifyType
 import com.tencent.devops.common.service.utils.LogUtils
-import com.tencent.devops.common.websocket.pojo.BuildPageInfo
-import com.tencent.devops.notify.api.service.ServiceNotifyMessageTemplateResource
-import com.tencent.devops.notify.pojo.SendNotifyMessageTemplateRequest
 import com.tencent.devops.process.engine.dao.PipelineInfoDao
 import com.tencent.devops.process.engine.pojo.PipelineBuildTask
 import com.tencent.devops.process.engine.service.PipelineBuildExtService
 import com.tencent.devops.process.engine.service.PipelineRuntimeService
 import com.tencent.devops.process.service.ProjectNameService
-import com.tencent.devops.process.util.ServiceHomeUrlUtils
 import com.tencent.devops.process.utils.PIPELINE_TURBO_TASK_ID
-import com.tencent.devops.process.websocket.page.DetailPageBuild
-import com.tencent.devops.store.pojo.common.PIPELINE_TASK_PAUSE_NOTIFY
 import okhttp3.Request
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
@@ -30,12 +23,7 @@ import java.util.Random
 
 @Service
 class PipelineBuildTurboExtService @Autowired constructor(
-    private val consulClient: ConsulDiscoveryClient?,
-    private val dslContext: DSLContext,
-    private val pipelineInfoDao: PipelineInfoDao,
-    private val projectNameService: ProjectNameService,
-    private val client: Client,
-    private val pipelineRuntimeService: PipelineRuntimeService
+    private val consulClient: ConsulDiscoveryClient?
 ) : PipelineBuildExtService {
 
     override fun buildExt(task: PipelineBuildTask): Map<String, String> {
@@ -47,88 +35,6 @@ class PipelineBuildTurboExtService @Autowired constructor(
             extMap[PIPELINE_TURBO_TASK_ID] = turboTaskId
         }
         return extMap
-    }
-
-    override fun sendPauseNotify(buildId: String, buildTask: PipelineBuildTask) {
-        try {
-            // 发送消息给相关关注人
-            val sendUser = buildTask.additionalOptions!!.subscriptionPauseUser
-            val subscriptionPauseUser = mutableSetOf<String>()
-            if (!sendUser.isNullOrEmpty()) {
-                val sendUsers = sendUser!!.split(",").toSet()
-                subscriptionPauseUser.addAll(sendUsers)
-            }
-            sendPauseNotify(
-                buildId = buildId,
-                taskName = buildTask.taskName,
-                pipelineId = buildTask.pipelineId,
-                receivers = subscriptionPauseUser
-            )
-            logger.info("|$buildId| next task |$buildTask| need pause, send End status to Vm agent")
-        } catch (e: Exception) {
-            logger.warn("pause atom send notify fail", e)
-        }
-    }
-
-    private fun sendPauseNotify(
-        buildId: String,
-        taskName: String,
-        pipelineId: String,
-        receivers: Set<String>?
-    ) {
-        val pipelineRecord = pipelineInfoDao.getPipelineInfo(dslContext, pipelineId)
-        if (pipelineRecord == null) {
-            logger.warn("sendPauseNotify pipeline[$pipelineId] is empty record")
-            return
-        }
-
-        val buildRecord = pipelineRuntimeService.getBuildInfo(buildId)
-        val pipelineName = (pipelineRecord?.pipelineName ?: "")
-        val buildNum = buildRecord?.buildNum.toString()
-        val projectName = projectNameService.getProjectName(pipelineRecord.projectId) ?: ""
-        val host = ServiceHomeUrlUtils.server()
-        val url = host + DetailPageBuild().buildPage(
-            buildPageInfo = BuildPageInfo(
-                buildId = buildId,
-                pipelineId = pipelineId,
-                projectId = pipelineRecord.projectId,
-                atomId = null
-            )
-        )
-        // 指定通过rtx发送
-        val notifyType = mutableSetOf<String>()
-        notifyType.add(NotifyType.RTX.name)
-
-        // 若没有配置订阅人，则将暂停消息发送给发起人
-        val receiver = mutableSetOf<String>()
-        if (receivers == null || receivers.isEmpty()) {
-            receiver.add(buildRecord!!.startUser)
-            receiver.add(pipelineRecord.lastModifyUser)
-        } else {
-            receiver.addAll(receivers)
-        }
-        logger.info("sean pause notify: $buildId| $taskName| $receiver")
-
-        val msg = SendNotifyMessageTemplateRequest(
-            templateCode = PIPELINE_TASK_PAUSE_NOTIFY,
-            titleParams = mapOf(
-                "BK_CI_PIPELINE_NAME" to pipelineName,
-                "BK_CI_BUILD_NUM" to buildNum
-            ),
-            notifyType = notifyType,
-            bodyParams = mapOf(
-                "BK_CI_PROJECT_NAME_CN" to projectName,
-                "BK_CI_PIPELINE_NAME" to pipelineName,
-                "BK_CI_BUILD_NUM" to buildNum,
-                "taskName" to taskName,
-                "BK_CI_START_USER_ID" to (buildRecord?.startUser ?: ""),
-                "url" to url
-            ),
-            receivers = receiver
-        )
-        logger.info("sendPauseNotify|$buildId| $pipelineId| $msg")
-        client.get(ServiceNotifyMessageTemplateResource::class)
-            .sendNotifyMessageByTemplate(msg)
     }
 
     fun getTurboTask(pipelineId: String, elementId: String): String {
