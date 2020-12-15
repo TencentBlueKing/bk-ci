679ac7290bf28f1776d5c5b84718408e4ba574b0
sawyersong2 <wudiduomjimo@gmail.com>
2020-12-15 17:26:18 +0800

    --story=861247617 优化dockerhost单个容器负载异常收集机制

--- src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/api/ServiceIdcDockerHostResourceImpl.kt
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/api/ServiceIdcDockerHostResourceImpl.kt
@@ -29,7 +29,7 @@ package com.tencent.devops.dockerhost.api
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.common.web.mq.alert.AlertLevel
-import com.tencent.devops.dispatch.pojo.DockerHostBuildInfo
+import com.tencent.devops.dispatch.docker.pojo.DockerHostBuildInfo
 import com.tencent.devops.dockerhost.dispatch.AlertApi
 import com.tencent.devops.dockerhost.exception.ContainerException
 import com.tencent.devops.dockerhost.exception.NoSuchImageException
