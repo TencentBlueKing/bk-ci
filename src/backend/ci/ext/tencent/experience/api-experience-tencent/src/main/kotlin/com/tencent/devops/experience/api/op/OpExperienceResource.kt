fdab0385110f6b13e11f2b42545c25f73c017602
stubenhuang <stubenhuang@tencent.com>
2020-12-22 15:09:30 +0800

    --story=860816285 新蓝盾APP后台story(仅供新分支挂靠)

--- src/backend/ci/ext/tencent/experience/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/api/op/OpExperienceResource.kt
+++ src/backend/ci/ext/tencent/experience/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/api/op/OpExperienceResource.kt
@@ -9,7 +9,6 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
-import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
