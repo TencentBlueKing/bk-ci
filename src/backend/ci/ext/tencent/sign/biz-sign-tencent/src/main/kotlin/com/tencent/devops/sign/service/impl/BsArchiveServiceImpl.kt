ab0ab125aea28dcd6dc94b2c48976ae8567d081a
necrohuang <necrohuang@tencent.com>
2020-12-30 21:48:40 +0800

    --story=861870075 支持微服务以直连方式连接蓝盾制品库

--- src/backend/ci/ext/tencent/sign/biz-sign-tencent/src/main/kotlin/com/tencent/devops/sign/service/impl/BsArchiveServiceImpl.kt
+++ src/backend/ci/ext/tencent/sign/biz-sign-tencent/src/main/kotlin/com/tencent/devops/sign/service/impl/BsArchiveServiceImpl.kt
@@ -1,10 +1,6 @@
 package com.tencent.devops.sign.service.impl
 
-import com.tencent.devops.common.archive.client.BkRepoClient
-import com.tencent.devops.common.archive.client.JfrogClient
-import com.tencent.devops.common.redis.RedisOperation
-import com.tencent.devops.common.service.config.CommonConfig
-import com.tencent.devops.common.service.gray.RepoGray
+import com.tencent.devops.common.archive.client.DirectBkRepoClient
 import com.tencent.devops.sign.api.pojo.IpaSignInfo
 import com.tencent.devops.sign.service.ArchiveService
 import org.slf4j.LoggerFactory
@@ -14,60 +10,29 @@ import java.io.File
 
 @Service
 class BsArchiveServiceImpl @Autowired constructor(
-    private val bkRepoClient: BkRepoClient,
-    private val commonConfig: CommonConfig,
-    private val repoGray: RepoGray,
-    private val redisOperation: RedisOperation
+    private val directBkRepoClient: DirectBkRepoClient
 ) : ArchiveService {
 
-    companion object {
-        private val logger = LoggerFactory.getLogger(BsArchiveServiceImpl::class.java)
-        private const val METADATA_PREFIX = "X-BKREPO-META-"
-        private const val BK_REPO_CUSTOM = "X-BKREPO-OVERWRITE"
-    }
-
-    override fun archive(
-        signedIpaFile: File,
-        ipaSignInfo: IpaSignInfo,
-        properties: Map<String, String>?
-    ): Boolean {
-        val isRepoGray = repoGray.isGray(ipaSignInfo.projectId, redisOperation)
+    override fun archive(signedIpaFile: File, ipaSignInfo: IpaSignInfo, properties: Map<String, String>?): Boolean {
+        logger.info("archive, signedIpaFile: ${signedIpaFile.absolutePath}, ipaSignInfo: $ipaSignInfo, properties: $properties")
         val path = if (ipaSignInfo.archiveType.toLowerCase() == "pipeline") {
             "${ipaSignInfo.pipelineId}/${ipaSignInfo.buildId}/${signedIpaFile.name}"
         } else {
             "${ipaSignInfo.archivePath}/${signedIpaFile.name}"
         }
-        if (isRepoGray) {
-            bkRepoClient.uploadLocalFile(
-                    userId = ipaSignInfo.userId,
-                    projectId = ipaSignInfo.projectId,
-                    repoName = ipaSignInfo.archiveType.toLowerCase(),
-                    path = path,
-                    file = signedIpaFile,
-                    gatewayFlag = true,
-                    bkrepoApiUrl = null,
-                    userName = null,
-                    password = null,
-                    properties = properties,
-                    gatewayUrl = commonConfig.devopsDevnetProxyGateway!!
-
-            )
-        } else {
-            val jfrogClient = JfrogClient(
-                    gatewayUrl = commonConfig.devopsDevnetProxyGateway!!,
-                    projectId = ipaSignInfo.projectId,
-                    pipelineId = ipaSignInfo.pipelineId ?: "",
-                    buildId = ipaSignInfo.buildId ?: ""
-            )
-            jfrogClient.uploadFile(
-                    userId = ipaSignInfo.userId,
-                    repoName = ipaSignInfo.archiveType,
-                    path = path,
-                    file = signedIpaFile,
-                    properties = properties
-            )
-        }
-
+        directBkRepoClient.uploadLocalFile(
+            userId = ipaSignInfo.userId,
+            projectId = ipaSignInfo.projectId,
+            repoName = ipaSignInfo.archiveType.toLowerCase(),
+            path = path,
+            file = signedIpaFile,
+            metadata = properties ?: mapOf(),
+            override = true
+        )
         return true
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(BsArchiveServiceImpl::class.java)
     }
 }
