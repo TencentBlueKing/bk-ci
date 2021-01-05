98644e1263e4b856d06de7d0e6c19cccbd5038d8
stubenhuang <stubenhuang@tencent.com>
2021-01-05 11:26:16 +0800

    --story=861846689 下线oss网关，统一走里约

--- src/gateway/tencent/lua/idc_to_devnet.lua
+++ src/gateway/tencent/lua/idc_to_devnet.lua
@@ -1,15 +1,18 @@
-local hmac = require "resty.hmac"
-local str_util = require "resty.string"
-
 local timestamp = ngx.time()
 local token = "6fb7eb9f0e213e126bde00720d5553c5b785a97b1f0400b0ac4e"
 local sn = timestamp..token..timestamp
 local host = "devcloud.esb.woa.com" --TODO 区分环境
 local domain = "http://"..host.."/devops-idc2devnet/dev_devnet-backend_devops/"
-local signature = str_util.to_hex(hmac:new(sn, hmac.ALGOS.SHA256):final())
 
-ngx.log(ngx.ERR , "t:"..timestamp.." , signature:"..signature)
+local resty_sha256 = require "resty.sha256"
+local resty_str = require "util.string_util"
+local sha256 = resty_sha256:new()
+sha256:update(sn)
+local digest = sha256:final()
+local signature = resty_str.to_hex(digest)
 
-ngx.var.timestamp = timestamp
+ngx.log(ngx.ERR , "t:"..timestamp.." , sn:"..sn.." , signature:"..signature)
+
+ngx.var.timestamp = tostring(timestamp)
 ngx.var.signature = signature
 ngx.var.rio_domain = domain
