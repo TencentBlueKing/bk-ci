fb6a7cb53f370c413e589ab85b7fb2ad0717db3c
ruotiantang <ruotiantang@tencent.com>
2021-01-06 16:43:11 +0800

    --story=860736989 STKE-操作/STKE-更新ConfigMap插件权限过大问题优化 -新增更新configmap和workload接口

--- src/backend/ci/ext/tencent/plugin/api-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/pojo/stke/StkeType.kt
+++ src/backend/ci/ext/tencent/plugin/api-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/pojo/stke/StkeType.kt
@@ -1,7 +1,7 @@
 package com.tencent.devops.plugin.pojo.stke
 
-enum class StkeType {
-    DEPLOYMENT,
-    STATEFUL_SET,
-    STATEFUL_SET_PLUS
+enum class StkeType(val type: String) {
+    DEPLOYMENT("deployments"),
+    STATEFUL_SET("statefulsets"),
+    STATEFUL_SET_PLUS("statefulsetpluses")
 }
--- src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/resources/BuildStkeResourceImpl.kt
+++ src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/resources/BuildStkeResourceImpl.kt
@@ -3,6 +3,7 @@ package com.tencent.devops.plugin.resources
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.plugin.api.BuildStkeResource
+import com.tencent.devops.plugin.pojo.stke.ConfigMapData
 import com.tencent.devops.plugin.pojo.stke.StkeType
 import com.tencent.devops.plugin.pojo.stke.StkeUpdateParam
 import com.tencent.devops.plugin.service.StkeService
@@ -20,12 +21,12 @@ class BuildStkeResourceImpl @Autowired constructor(
         updateParam: StkeUpdateParam
     ): Result<String> {
         val value = stkeService.update(
-                stkeType = stkeType,
-                clusterName = clusterName,
-                namespace = namespace,
-                appsName = appsName,
-                updateParam = updateParam
-            )
+            stkeType = stkeType,
+            clusterName = clusterName,
+            namespace = namespace,
+            appsName = appsName,
+            updateParam = updateParam
+        )
         return if (value == null) {
             Result(
                 status = 0,
@@ -44,11 +45,11 @@ class BuildStkeResourceImpl @Autowired constructor(
         appsName: String
     ): Result<String> {
         val value = stkeService.getPodsStatus(
-                stkeType = stkeType,
-                clusterName = clusterName,
-                namespace = namespace,
-                appsName = appsName
-            )
+            stkeType = stkeType,
+            clusterName = clusterName,
+            namespace = namespace,
+            appsName = appsName
+        )
         return if (value == null) {
             Result(
                 status = 0,
@@ -67,11 +68,11 @@ class BuildStkeResourceImpl @Autowired constructor(
         appsName: String
     ): Result<String> {
         val value = stkeService.getWorkload(
-                stkeType = stkeType,
-                clusterName = clusterName,
-                namespace = namespace,
-                appsName = appsName
-            )
+            stkeType = stkeType,
+            clusterName = clusterName,
+            namespace = namespace,
+            appsName = appsName
+        )
         return if (value == null) {
             Result(
                 status = 0,
@@ -87,6 +88,56 @@ class BuildStkeResourceImpl @Autowired constructor(
         val value = stkeService.getManagers(
                 projectId = projectId
             )
+        return if (value == null) {
+            Result(
+                status = 0,
+                data = "",
+                message = "STKE_ERROR"
+            )
+        } else {
+            Result(value)
+        }
+    }
+
+    override fun updateConfigMap(
+        clusterName: String,
+        namespace: String,
+        configMapName: String,
+        configMapData: List<ConfigMapData>
+    ): Result<String> {
+        val value = stkeService.updateConfigMap(
+            clusterName = clusterName,
+            namespace = namespace,
+            configMapName = configMapName,
+            configMapData = configMapData
+        )
+        return if (value == null) {
+            Result(
+                status = 0,
+                data = "",
+                message = "STKE_ERROR"
+            )
+        } else {
+            Result(value)
+        }
+    }
+
+    override fun updateWorkLoad(
+        stkeType: StkeType,
+        clusterName: String,
+        namespace: String,
+        appsName: String,
+        operator: String,
+        buildId: String
+    ): Result<String> {
+        val value = stkeService.updateWorkLoad(
+            clusterName = clusterName,
+            namespace = namespace,
+            stkeType = stkeType,
+            workLoadName = appsName,
+            operator = operator,
+            buildId = buildId
+        )
         return if (value == null) {
             Result(
                 status = 0,
