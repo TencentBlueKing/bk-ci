fc723aff64e9f27e8a2c1fa050d356612e295769
lockiechen <lockiechen@tencent.com>
2021-01-12 14:37:25 +0800

    fix: 流水线详情页添加匹配校验, 不匹配时跳回流水线列表页 issue #3461

--- src/frontend/devops-pipeline/src/main.js
+++ src/frontend/devops-pipeline/src/main.js
@@ -60,7 +60,7 @@ ExtendsCustomRules(VeeValidate.Validator.extend)
 
 Vue.prototype.$setLocale = setLocale
 
-if (window.top === window) { // 只能以iframe形式嵌入
+if (window.top === window.self) { // 只能以iframe形式嵌入
     location.href = `${WEB_URL_PIRFIX}${location.pathname}`
 }
 
