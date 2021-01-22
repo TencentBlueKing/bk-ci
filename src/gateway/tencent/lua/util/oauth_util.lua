61b06e8f8f54d826992a0c828c02e1d08645a7fb
stubenhuang <stubenhuang@tencent.com>
2021-01-22 20:32:33 +0800

    --story=862330387 【蓝盾网关】prebuild的accessToken使用特定的key

--- src/gateway/tencent/lua/util/oauth_util.lua
+++ src/gateway/tencent/lua/util/oauth_util.lua
@@ -103,87 +103,100 @@ function _M:get_ticket(bk_ticket)
 end
 
 function _M:get_prebuild_ticket(bk_ticket)
-    -- TODO REDIS CACHE
-    local user_cache = ngx.shared.user_info_store
-    local user_cache_value = user_cache:get(bk_ticket)
-    if user_cache_value == nil then
-        ngx.log(ngx.STDERR, "no user info")
-        --- 初始化HTTP连接
-        local httpc = http.new()
-        --- 开始连接
-        httpc:set_timeout(3000)
-        httpc:connect(config.oauth.ip, config.oauth.port)
-
-        --- 组装请求body
-        local requestBody = {
-            env_name = config.oauth.env, 
-            app_code = config.oauth.app_code, 
-            app_secret = config.oauth.app_secret, 
-            grant_type =  "authorization_code",  
-            id_provider = "bk_login_ied", 
-            bk_ticket = bk_ticket
-        }
-
-        --- 转换请求内容
-        local requestBodyJson = json.encode(requestBody)
-        if requestBodyJson == nil then
-            ngx.log(ngx.ERR, "failed to encode auth/token request body: ", logUtil:dump(requestBody))
-            ngx.exit(500)
-            return
-        end
-
-        --- 发送请求
-        -- local url = config.oauth.scheme .. config.oauth.ip  .. config.oauth.loginUrl .. bk_token
-        local url = config.oauth.url
-        local res, err = httpc:request({
-            path = url,
-            method = "POST",
-            headers = {
-            ["Host"] = config.oauth.host,
-            ["Accept"] = "application/json",
-            ["Content-Type"] = "application/json"
-            }, 
-            body = requestBodyJson
-        })
-        --- 判断是否出错了
-        if not res then
-            ngx.log(ngx.ERR, "failed to request get_ticket: ", err)
-            ngx.exit(500)
-            return
-        end
-        --- 判断返回的状态码是否是200
-        if res.status ~= 200 then
-            ngx.log(ngx.STDERR, "failed to request get_ticket, status: ", res.status)
-            ngx.exit(500)
-            return
-        end
-        --- 获取所有回复
-        local responseBody = res:read_body()
-        --- 设置HTTP保持连接
-        httpc:set_keepalive(60000, 5)
-        --- 转换JSON的返回数据为TABLE
-        local result = json.decode(responseBody)
-        --- 判断JSON转换是否成功
-        if result == nil then 
-            ngx.log(ngx.ERR, "failed to parse get_ticket response：", responseBody)
-            ngx.exit(500)
-            return
-        end
-
-        --- 判断返回码:Q!
-        if result.code ~= 0 then
-            ngx.log(ngx.STDERR, "invalid get_ticket: ", result.message)
-            ngx.exit(401)
-            return
-        end
-        user_cache:set(bk_ticket, responseBody, 180)
-        return result.data
+    local red = redisUtil:new()
+    if not red then
+        ngx.log(ngx.ERR, "failed to new redis ", err)
+        ngx.exit(500)
+        return
     else
+        local red_key = "prebuild_token_" .. bk_ticket
+        --- 获取对应的buildId
+        local redRes, err = red:get(red_key)
+        --- 将redis连接放回pool中
+        local ok, err = red:set_keepalive(config.redis.max_idle_time, config.redis.pool_size)
+        if not ok then
+            ngx.log(ngx.STDERR, "failed to set keepalive: ", err)
+        end
 
-        ngx.log(ngx.STDERR, "has user info:", user_cache_value)
-        return json.decode(user_cache_value).data
+        if not redRes then
+            ngx.log(ngx.STDERR, "no user info")
+            --- 初始化HTTP连接
+            local httpc = http.new()
+            --- 开始连接
+            httpc:set_timeout(3000)
+            httpc:connect(config.oauth.ip, config.oauth.port)
+
+            --- 组装请求body
+            local requestBody = {
+                env_name = "prod",
+                app_code = "bk-prebuild",
+                app_secret = "oBv5hO54nR6USNPbc55nFRVrqkOzDZ4jX8MyTTWz6iGC8rIBP5", 
+                grant_type =  "authorization_code",  
+                id_provider = "bk_login_ied", 
+                bk_ticket = bk_ticket
+            }
+
+            --- 转换请求内容
+            local requestBodyJson = json.encode(requestBody)
+            if requestBodyJson == nil then
+                ngx.log(ngx.ERR, "failed to encode auth/token request body: ", logUtil:dump(requestBody))
+                ngx.exit(500)
+                return
+            end
+
+            --- 发送请求
+            -- local url = config.oauth.scheme .. config.oauth.ip  .. config.oauth.loginUrl .. bk_token
+            local url = config.oauth.url
+            local res, err = httpc:request({
+                path = url,
+                method = "POST",
+                headers = {
+                ["Host"] = config.oauth.host,
+                ["Accept"] = "application/json",
+                ["Content-Type"] = "application/json"
+                }, 
+                body = requestBodyJson
+            })
+            --- 判断是否出错了
+            if not res then
+                ngx.log(ngx.ERR, "failed to request get_ticket: ", err)
+                ngx.exit(500)
+                return
+            end
+            --- 判断返回的状态码是否是200
+            if res.status ~= 200 then
+                ngx.log(ngx.STDERR, "failed to request get_ticket, status: ", res.status)
+                ngx.exit(500)
+                return
+            end
+            --- 获取所有回复
+            local responseBody = res:read_body()
+            --- 设置HTTP保持连接
+            httpc:set_keepalive(60000, 5)
+            --- 转换JSON的返回数据为TABLE
+            local result = json.decode(responseBody)
+            --- 判断JSON转换是否成功
+            if result == nil then 
+                ngx.log(ngx.ERR, "failed to parse get_ticket response：", responseBody)
+                ngx.exit(500)
+                return
+            end
+
+            --- 判断返回码:Q!
+            if result.code ~= 0 then
+                ngx.log(ngx.STDERR, "invalid get_ticket: ", result.message)
+                ngx.exit(401)
+                return
+            end
+
+            red:set(red_key,result.data)
+            red:expire(red_key,29*24*60*60)
+
+            return result.data
+        else
+            return redRes
+        end
     end
-
 end
 
 function _M:verfiy_permis(project_code, service_code, policy_code, resource_code, resource_type, user_id, access_token)
