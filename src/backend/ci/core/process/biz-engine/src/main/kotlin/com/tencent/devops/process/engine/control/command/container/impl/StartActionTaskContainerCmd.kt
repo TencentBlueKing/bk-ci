49630580c59b9e5643b363ef9c13fd10fe2ba982
irwinsun <irwinsun@tencent.com>
2021-02-20 14:43:19 +0800

    feat: 引擎核心代码拆分出biz-process业务模块 #3400 非fast kill的强制终止时到最后无任务，最终状态必定是FAILED

--- src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/container/impl/StartActionTaskContainerCmd.kt
+++ src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/container/impl/StartActionTaskContainerCmd.kt
@@ -75,8 +75,9 @@ class StartActionTaskContainerCmd(
                     commandContext.buildStatus = BuildStatus.SUCCEED
                 }
                 val waitToDoTask = findTask(commandContext)
-                if (waitToDoTask == null) { // 强制终止时到最后无任务，最终状态必定是FAILED
-                    if (ActionType.isTerminate(actionType) && !commandContext.buildStatus.isFailure()) {
+                if (waitToDoTask == null) { // 非fast kill的强制终止时到最后无任务，最终状态必定是FAILED
+                    val fastKill = FastKillUtils.isFastKillCode(commandContext.event.errorCode)
+                    if (!fastKill && ActionType.isTerminate(actionType) && !commandContext.buildStatus.isFailure()) {
                         commandContext.buildStatus = BuildStatus.FAILED
                     }
                     commandContext.latestSummary = "status=${commandContext.buildStatus}"
@@ -244,7 +245,7 @@ class StartActionTaskContainerCmd(
         currentTask: PipelineBuildTask
     ): PipelineBuildTask? {
         // 终止将直接返回
-        if (ActionType.isTerminate(containerContext.event.actionType)) {
+        if (isTerminate(containerContext)) {
             return null
         }
 
