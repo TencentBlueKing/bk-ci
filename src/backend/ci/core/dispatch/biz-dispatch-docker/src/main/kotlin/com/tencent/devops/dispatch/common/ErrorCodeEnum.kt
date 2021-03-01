c43c83cc53b437e706a82acc7d74ff2def943f58
irwinsun <irwinsun@tencent.com>
2021-03-01 16:43:19 +0800

    feat: 引擎核心代码拆分出biz-process业务模块 #3400 业务复杂的旧代码逻辑先屏蔽代码告警

--- src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/common/ErrorCodeEnum.kt
+++ src/backend/ci/core/dispatch/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/common/ErrorCodeEnum.kt
@@ -31,15 +31,18 @@ enum class ErrorCodeEnum(
     val errorCode: Int,
     val formatErrorMessage: String
 ) {
-    SYSTEM_ERROR(2127001, "Dispatcher-docker系统错误"),
-    NO_IDLE_VM_ERROR(2127002, "构建机启动失败，没有空闲的构建机"),
-    POOL_VM_ERROR(2127003, "容器并发池分配异常"),
-    NO_SPECIAL_VM_ERROR(2127004, "Start build Docker VM failed, no available Docker VM in specialIpList"),
-    NO_AVAILABLE_VM_ERROR(2127005, "Start build Docker VM failed, no available Docker VM. Please wait a moment and try again."),
-    DOCKER_IP_NOT_AVAILABLE(2127006, "Docker ip is not available."),
-    END_VM_ERROR(2127007, "End build Docker VM failed"),
-    START_VM_FAIL(2127008, "Start build Docker VM failed"),
-    RETRY_START_VM_FAIL(2127009, "Start build Docker VM failed, retry times."),
-    GET_VM_STATUS_FAIL(2127010, "Get container status failed"),
-    GET_CREDENTIAL_FAIL(2127011, "Get credential failed")
+    SYSTEM_ERROR(errorCode = 2127001, formatErrorMessage = "Dispatcher-docker系统错误"),
+    NO_IDLE_VM_ERROR(errorCode = 2127002, formatErrorMessage = "构建机启动失败，没有空闲的构建机"),
+    POOL_VM_ERROR(errorCode = 2127003, formatErrorMessage = "容器并发池分配异常"),
+    NO_SPECIAL_VM_ERROR(errorCode = 2127004,
+        formatErrorMessage = "Start build Docker VM failed, no available Docker VM in specialIpList"),
+    NO_AVAILABLE_VM_ERROR(errorCode = 2127005,
+        formatErrorMessage =
+        "Start build Docker VM failed, no available Docker VM. Please wait a moment and try again."),
+    DOCKER_IP_NOT_AVAILABLE(errorCode = 2127006, formatErrorMessage = "Docker ip is not available."),
+    END_VM_ERROR(errorCode = 2127007, formatErrorMessage = "End build Docker VM failed"),
+    START_VM_FAIL(errorCode = 2127008, formatErrorMessage = "Start build Docker VM failed"),
+    RETRY_START_VM_FAIL(errorCode = 2127009, formatErrorMessage = "Start build Docker VM failed, retry times."),
+    GET_VM_STATUS_FAIL(errorCode = 2127010, formatErrorMessage = "Get container status failed"),
+    GET_CREDENTIAL_FAIL(errorCode = 2127011, formatErrorMessage = "Get credential failed")
 }
