b86aa63fd718ab3eeca06ed36b3007a59af68035
brooklin <brooklin@tencent.com>
2021-03-02 15:01:01 +0800

    del: 删除切换至V2版本提示

--- src/frontend/devops-codecc/src/views/task/detail.vue
+++ src/frontend/devops-codecc/src/views/task/detail.vue
@@ -108,12 +108,6 @@
             :title="$t('重新分析')">
             {{this.$t('任务正在分析中，是否中断并重新分析？')}}
         </bk-dialog>
-        <new-analyse
-            @changeItem="changeItem"
-            @newAnalyse="newAnalyse"
-            :never-show="neverShow"
-            :visible.sync="dialogAnalyseVisible">
-        </new-analyse>
     </div>
 </template>
 
@@ -123,14 +117,12 @@
     import toolStatusCard from '@/components/tool-status-card'
     import Record from '@/components/operate-record/index'
     import taskWebsocket from '@/common/taskWebSocket'
-    import newAnalyse from '@/components/new-analyse'
     import axios from 'axios'
 
     export default {
         components: {
             toolStatusCard,
-            Record,
-            newAnalyse
+            Record
         },
         data () {
             return {
