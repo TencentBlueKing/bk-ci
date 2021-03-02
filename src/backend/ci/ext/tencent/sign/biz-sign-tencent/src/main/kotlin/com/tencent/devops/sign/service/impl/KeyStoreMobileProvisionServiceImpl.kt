2d819c02eb1a56d0dbfb49d80c76141ee50ac4e8
irwinsun <irwinsun@tencent.com>
2021-03-02 17:44:03 +0800

    -story=861833869 拆分Process引擎部分代码结构 重构修正

--- src/backend/ci/ext/tencent/sign/biz-sign-tencent/src/main/kotlin/com/tencent/devops/sign/service/impl/KeyStoreMobileProvisionServiceImpl.kt
+++ src/backend/ci/ext/tencent/sign/biz-sign-tencent/src/main/kotlin/com/tencent/devops/sign/service/impl/KeyStoreMobileProvisionServiceImpl.kt
@@ -113,14 +113,17 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
         OkhttpUtils.doGet(url, headers).use { resp ->
             if (resp.code() != 200 || resp.body() == null) {
                 throw ErrorCodeException(
-                        errorCode = SignMessageCode.ERROR_MP_NOT_EXIST,
-                        defaultMessage = "KeyStore描述文件($mobileProvisionId)不存在或者下载失败。"
+                    errorCode = SignMessageCode.ERROR_MP_NOT_EXIST,
+                    defaultMessage = "KeyStore描述文件($mobileProvisionId)不存在或者下载失败。"
                 )
             }
             val decryptedMobileProvisionEncrypt = resp.body()!!.bytes()
             logger.info("Keystore decrypt decryptedMobileProvisionEncrypt:$decryptedMobileProvisionEncrypt")
             logger.info("Keystore decrypt keyStoreAuthSecret:$keyStoreAuthSecret")
-            val decryptedMobileProvisionDecrypt = EncryptUtil.decrypt(decryptedMobileProvisionEncrypt, keyStoreAuthSecret)
+            val decryptedMobileProvisionDecrypt = EncryptUtil.decrypt(
+                encrypted = decryptedMobileProvisionEncrypt,
+                key = keyStoreAuthSecret
+            )
             mobileProvisionFile.writeBytes(decryptedMobileProvisionDecrypt)
         }
         return mobileProvisionFile
@@ -134,7 +137,8 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
             val keychainArray = (rootDict.objectForKey(KEYCHAIN_ACCESS_GROUPS_KEY) as NSArray).array.withIndex()
             for ((index, e) in keychainArray) {
                 if (e.toString() == "com.apple.token") {
-                    val removeKeyChainGroupCMD = "plutil -remove keychain-access-groups.$index ${entitlementFile.canonicalPath}"
+                    val removeKeyChainGroupCMD =
+                        "plutil -remove keychain-access-groups.$index ${entitlementFile.canonicalPath}"
                     CommandLineUtils.execute(removeKeyChainGroupCMD, entitlementFile.parentFile, true)
                     break
                 }
@@ -156,8 +160,13 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
                 if (!teamId.isNullOrBlank() && keyChainGroupsList.isNotEmpty()) {
                     keyChainGroupsList.forEach {
                         if (it.isNotBlank()) {
-                            val insertKeyChainGroupCMD = "plutil -insert keychain-access-groups.0 -string '$teamId.$it' ${entitlementFile.canonicalPath}"
-                            CommandLineUtils.execute(insertKeyChainGroupCMD, entitlementFile.parentFile, true)
+                            val insertKeyChainGroupCMD =
+                                "plutil -insert keychain-access-groups.0 -string" +
+                                    " '$teamId.$it' ${entitlementFile.canonicalPath}"
+                            CommandLineUtils.execute(
+                                command = insertKeyChainGroupCMD,
+                                workspace = entitlementFile.parentFile,
+                                print2Logger = true)
                         }
                     }
                 }
@@ -187,9 +196,9 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
             return null
         }
         return downloadMobileProvision(
-                mobileProvisionDir = mobileProvisionDir,
-                projectId = ipaSignInfo.projectId,
-                mobileProvisionId = wildcardMobileProvisionId!!
+            mobileProvisionDir = mobileProvisionDir,
+            projectId = ipaSignInfo.projectId,
+            mobileProvisionId = wildcardMobileProvisionId!!
         )
     }
 
@@ -199,7 +208,7 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
         claims["timeMillis"] = Instant.now().epochSecond
         val key = Keys.hmacShaKeyFor(keyStoreAuthSecret.toByteArray(StandardCharsets.UTF_8))
         token = Jwts.builder().setClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, key).compact()
+            .signWith(SignatureAlgorithm.HS256, key).compact()
     }
 
     /*
@@ -211,43 +220,3 @@ class KeyStoreMobileProvisionServiceImpl @Autowired constructor() : MobileProvis
         generateToken()
     }
 }
-//
-// fun main(args: Array<String>) {
-//    val keyStoreAuthId = "devops"
-//    val keyStoreAuthSecret = "a21c218df41f6d7fd032535fe20394e2"
-//    val mobileProvisionDir  = File("/data/enterprise_sign_tmp/freyzheng/11111111/222222222/test.ipa.mobileProvisionDir")
-//    val mobileProvisionId = "16722ba2-6fd1-45ef-a009-f94b20bc0d4f"
-//    val keyStoreUrl = "https://proxy.test.keystore.oa.com"
-//    val keyStoreCertUrl = "/api/auth/getCert?id=%s&category=%s"
-//
-//
-//    val claims = mutableMapOf<String, Any>()
-//    claims["authId"] = keyStoreAuthId
-//    claims["timeMillis"] = Instant.now().epochSecond
-//    val key = Keys.hmacShaKeyFor(keyStoreAuthSecret.toByteArray(StandardCharsets.UTF_8))
-//    val token = Jwts.builder().setClaims(claims)
-//            .signWith(key, SignatureAlgorithm.HS256).compact()
-//    // 从keystore下载文件
-//    val mobileProvisionFile = File("${mobileProvisionDir.canonicalPath}/$mobileProvisionId.mobileprovision")
-//    val url = String.format(keyStoreUrl + keyStoreCertUrl, mobileProvisionId, KEYSTORE_CATEGORY_PROVISION)
-//    val request = Request.Builder()
-//            .url(url)
-//            .addHeader(KEYSTORE_HTTP_HEADER_AUTH, token)
-//            .addHeader(KEYSTORE_HTTP_HEADER_IP, InetAddress.getLocalHost().hostAddress)
-//            .build()
-//    val headers = mutableMapOf<String, String>()
-//    headers[KEYSTORE_HTTP_HEADER_AUTH] = token ?:""
-//    headers[KEYSTORE_HTTP_HEADER_IP] = InetAddress.getLocalHost().hostAddress
-//    OkhttpUtils.doGet(url, headers).use { resp ->
-//        if(resp.code() != 200 || resp.body() == null) {
-//            throw ErrorCodeException(
-//                    errorCode = SignMessageCode.ERROR_MP_NOT_EXIST,
-//                    defaultMessage = "KeyStore描述文件不存在或者下载失败。"
-//            )
-//        }
-//        val decryptedMobileProvisionEncrypt = resp.body()!!.bytes()
-//        val decryptedMobileProvisionDecrypt = EncryptUtil.decrypt(decryptedMobileProvisionEncrypt ,keyStoreAuthSecret)
-//        mobileProvisionFile.writeBytes(decryptedMobileProvisionDecrypt)
-//    }
-//    val a = 1
-// }
