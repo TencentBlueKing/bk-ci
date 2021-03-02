bcecbc26714902309425453261eb7fef90b528a7
necrohuang <necrohuang@tencent.com>
2021-03-02 20:44:18 +0800

    --bug=85686337 研发商店插件自定义资源上传失败

--- src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/JFrogArchiveFileServiceImpl.kt
+++ src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/JFrogArchiveFileServiceImpl.kt
@@ -10,12 +10,13 @@
  *
  * Terms of the MIT License:
  * ---------------------------------------------------
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
  * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
@@ -79,7 +80,8 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         props: Map<String, String?>?,
         fileChannelType: FileChannelTypeEnum
     ): String {
-        logger.info("uploadFile, userId: $userId, file: $file, projectId: $projectId, filePath: $filePath, fileName: $fileName, fileType: $fileType, props: $props, fileChannelType: $fileChannelType")
+        logger.info("uploadFile, userId: $userId, file: $file, projectId: $projectId, filePath: $filePath, " +
+            "fileName: $fileName, fileType: $fileType, props: $props, fileChannelType: $fileChannelType")
         val uploadFileName = fileName ?: file.name
         val fileTypeStr = fileType?.fileType ?: "file"
         val destPath = if (null == filePath) {
@@ -125,11 +127,24 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         TODO("not implemented")
     }
 
-    override fun downloadReport(userId: String, projectId: String, pipelineId: String, buildId: String, elementId: String, path: String) {
+    override fun downloadReport(
+        userId: String,
+        projectId: String,
+        pipelineId: String,
+        buildId: String,
+        elementId: String,
+        path: String) {
         TODO("not implemented")
     }
 
-    override fun searchFileList(userId: String, projectId: String, page: Int?, pageSize: Int?, searchProps: SearchProps): Page<FileInfo> {
+    override fun searchFileList(
+        userId: String,
+        projectId:
+        String,
+        page: Int?,
+        pageSize: Int?,
+        searchProps: SearchProps
+    ): Page<FileInfo> {
         TODO("not implemented")
     }
 
@@ -160,8 +175,17 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         FileCopyUtils.copy(httpResponse.body()!!.byteStream(), response.outputStream)
     }
 
-    override fun downloadArchiveFile(userId: String, projectId: String, pipelineId: String, buildId: String, fileType: FileTypeEnum, customFilePath: String, response: HttpServletResponse) {
-        logger.info("downloadArchiveFile, userId: $userId, projectId: $projectId, pipelineId: $pipelineId, buildId: $buildId, fileType: $fileType, customFilePath: $customFilePath")
+    override fun downloadArchiveFile(
+        userId: String,
+        projectId: String,
+        pipelineId: String,
+        buildId: String,
+        fileType: FileTypeEnum,
+        customFilePath: String,
+        response: HttpServletResponse
+    ) {
+        logger.info("downloadArchiveFile, userId: $userId, projectId: $projectId, pipelineId: $pipelineId, " +
+            "buildId: $buildId, fileType: $fileType, customFilePath: $customFilePath")
         val result = generateDestPath(
             fileType = fileType,
             projectId = projectId,
@@ -184,13 +208,18 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         }
         val folderName = generateFolderName(fileType)
         val handleDestPath = filePath.removePrefix(fileSeparator)
-        val finalFilePath = URLDecoder.decode(handleDestPath.substring(handleDestPath.indexOf(fileSeparator) + 1), "UTF-8")
-        val fileUrl = "${HomeHostUtil.getHost(commonConfig.devopsApiGateway!!)}/artifactory/$folderName/download/service/${URLDecoder.decode(finalFilePath, "UTF-8")}"
+        val finalFilePath = URLDecoder.decode(
+            handleDestPath.substring(handleDestPath.indexOf(fileSeparator) + 1), "UTF-8"
+        )
+        val host = HomeHostUtil.getHost(commonConfig.devopsApiGateway!!)
+        val path = URLDecoder.decode(finalFilePath, "UTF-8")
+        val fileUrl = "$host/artifactory/$folderName/download/service/$path"
         logger.info("getFileHttpResponse fileUrl is:$fileUrl")
         val request = Request.Builder().url(fileUrl).get().build()
         val httpResponse = OkhttpUtils.doHttp(request)
         if (!httpResponse.isSuccessful) {
-            logger.error("FAIL|Download file from $fileUrl| message=${httpResponse.message()}| code=${httpResponse.code()}")
+            logger.error("FAIL|Download file from $fileUrl| " +
+                "message=${httpResponse.message()}| code=${httpResponse.code()}")
             throw RemoteServiceException(httpResponse.message())
         }
         return httpResponse
@@ -205,9 +234,9 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         customFilePath: String?,
         fileChannelType: FileChannelTypeEnum
     ): GetFileDownloadUrlsResponse {
-        logger.info("getFileDownloadUrls fileChannelType is:$fileChannelType")
-        logger.info("getFileDownloadUrls userId is:$userId,projectId is:$projectId,pipelineId is:$pipelineId")
-        logger.info("getFileDownloadUrls buildId is:$buildId,artifactoryType is:$artifactoryType,customFilePath is:$customFilePath")
+        logger.info("getFileDownloadUrls userId: $userId, projectId: $projectId, pipelineId:$pipelineId, " +
+            "buildId: $buildId, artifactoryType: $artifactoryType, customFilePath : $customFilePath, " +
+            "fileChannelType :$fileChannelType")
         val param = ArtifactorySearchParam(
             projectId = projectId,
             pipelineId = pipelineId,
@@ -226,7 +255,8 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         artifactoryType: ArtifactoryType,
         fileChannelType: FileChannelTypeEnum
     ): GetFileDownloadUrlsResponse {
-        logger.info("getFileDownloadUrls, filePath: $filePath, artifactoryType, $artifactoryType, fileChannelType, $fileChannelType")
+        logger.info("getFileDownloadUrls, filePath: $filePath, artifactoryType, $artifactoryType, " +
+            "fileChannelType, $fileChannelType")
         val handleFilePath = filePath.replace(getBasePath(), "")
         val filePathParts = handleFilePath.split(fileSeparator)
         val dataList = mutableListOf<String>()
@@ -256,7 +286,13 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         )
     }
 
-    override fun generateDestPath(fileType: FileTypeEnum, projectId: String, customFilePath: String?, pipelineId: String?, buildId: String?): String {
+    override fun generateDestPath(
+        fileType: FileTypeEnum,
+        projectId: String,
+        customFilePath: String?,
+        pipelineId: String?,
+        buildId: String?
+    ): String {
         val destPathBuilder = StringBuilder(getBasePath()).append(fileType.fileType).append(fileSeparator)
         if (FileTypeEnum.BK_PLUGIN_FE != fileType) {
             if (!projectId.isNullOrBlank()) {
@@ -266,10 +302,12 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
 
         if (FileTypeEnum.BK_CUSTOM == fileType || FileTypeEnum.BK_PLUGIN_FE == fileType) {
             if (customFilePath == null) {
-                throw ErrorCodeException(errorCode = CommonMessageCode.PARAMETER_IS_NULL, params = arrayOf("customFilePath"))
+                throw ErrorCodeException(errorCode = CommonMessageCode.PARAMETER_IS_NULL,
+                    params = arrayOf("customFilePath"))
             }
             if (customFilePath.contains("..")) {
-                throw ErrorCodeException(errorCode = CommonMessageCode.PARAMETER_IS_INVALID, params = arrayOf("customFilePath"))
+                throw ErrorCodeException(errorCode = CommonMessageCode.PARAMETER_IS_INVALID,
+                    params = arrayOf("customFilePath"))
             }
             destPathBuilder.append(customFilePath.removePrefix(fileSeparator)) // 自定义方式归档文件
         } else {
@@ -283,7 +321,13 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
         return destPath
     }
 
-    override fun acrossProjectCopy(projectId: String, artifactoryType: ArtifactoryType, path: String, targetProjectId: String, targetPath: String): Count {
+    override fun acrossProjectCopy(
+        projectId: String,
+        artifactoryType: ArtifactoryType,
+        path: String,
+        targetProjectId: String,
+        targetPath: String
+    ): Count {
         return Count(0)
     }
 
@@ -298,13 +342,18 @@ class JFrogArchiveFileServiceImpl : ArchiveFileServiceImpl() {
     ): String {
         logger.info("generateFileDownloadUrl, fileChannelType: $fileChannelType, destPath: $destPath")
         val handleDestPath = destPath.removePrefix(fileSeparator)
-        val filePath = URLDecoder.decode(handleDestPath.substring(handleDestPath.indexOf(fileSeparator) + 1), "UTF-8")
+        val filePath = URLDecoder.decode(
+            handleDestPath.substring(handleDestPath.indexOf(fileSeparator) + 1), "UTF-8")
         val folderName = generateFolderName(fileType)
         val fileDownloadPath = when (fileChannelType) {
-            FileChannelTypeEnum.WEB_SHOW -> "${HomeHostUtil.getHost(commonConfig.devopsHostGateway!!)}/artifactory/$folderName/view/user/$filePath"
-            FileChannelTypeEnum.WEB_DOWNLOAD -> "${HomeHostUtil.getHost(commonConfig.devopsHostGateway!!)}/artifactory/$folderName/download/user/$filePath"
-            FileChannelTypeEnum.SERVICE -> "${HomeHostUtil.getHost(commonConfig.devopsApiGateway!!)}/artifactory/$folderName/download/service/$filePath"
-            FileChannelTypeEnum.BUILD -> "${HomeHostUtil.getHost(commonConfig.devopsBuildGateway!!)}/artifactory/$folderName/download/build/$filePath"
+            FileChannelTypeEnum.WEB_SHOW -> "${HomeHostUtil.getHost(commonConfig.devopsHostGateway!!)}/" +
+                "artifactory/$folderName/view/user/$filePath"
+            FileChannelTypeEnum.WEB_DOWNLOAD -> "${HomeHostUtil.getHost(commonConfig.devopsHostGateway!!)}/" +
+                "artifactory/$folderName/download/user/$filePath"
+            FileChannelTypeEnum.SERVICE -> "${HomeHostUtil.getHost(commonConfig.devopsApiGateway!!)}/" +
+                "artifactory/$folderName/download/service/$filePath"
+            FileChannelTypeEnum.BUILD -> "${HomeHostUtil.getHost(commonConfig.devopsBuildGateway!!)}/" +
+                "artifactory/$folderName/download/build/$filePath"
         }
         logger.info("generateFileDownloadPath fileDownloadPath is: $fileDownloadPath")
         return fileDownloadPath
