7562f92167e85cad8cb8925211391a3808104adf
mingshewhe <mingshewhe@tencent.com>
2021-03-10 10:49:26 +0800

    feature: 希望 bk-ci gitlab插件可以支持监听指定的事件 #3664

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/webhook/listener/WebhookEventListener.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/webhook/listener/WebhookEventListener.kt
@@ -28,6 +28,7 @@
 package com.tencent.devops.process.webhook.listener
 
 import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeGitWebHookTriggerElement
+import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeGitlabWebHookTriggerElement
 import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeTGitWebHookTriggerElement
 import com.tencent.devops.common.pipeline.pojo.element.trigger.enums.CodeType
 import com.tencent.devops.process.engine.service.PipelineWebhookBuildLogContext
@@ -67,7 +68,7 @@ class WebhookEventListener constructor(
                     e = event.requestContent
                 )
                 CommitEventType.GITLAB -> pipelineBuildService.externalCodeGitBuild(
-                    codeRepositoryType = event.requestContent,
+                    codeRepositoryType = CodeGitlabWebHookTriggerElement.classType,
                     e = event.requestContent
                 )
                 CommitEventType.TGIT -> pipelineBuildService.externalCodeGitBuild(
