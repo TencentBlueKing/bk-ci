095db781c51e41b8a872abb8c5da041e37e49f2a
royalhuang <royalhuang@tencent.com>
2021-03-23 18:08:07 +0800

    perf: 内部的构建日志输出通过服务api调用 #3503 调整Ansi调用包名

--- src/backend/ci/ext/tencent/worker/worker-agent/src/main/kotlin/com/tencent/devops/agent/runner/WorkRunner.kt
+++ src/backend/ci/ext/tencent/worker/worker-agent/src/main/kotlin/com/tencent/devops/agent/runner/WorkRunner.kt
@@ -31,7 +31,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.agent.utils.KillBuildProcessTree
 import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.common.api.util.ReplacementUtils
-import com.tencent.devops.common.log.Ansi
+import com.tencent.devops.log.meta.Ansi
 import com.tencent.devops.dispatch.pojo.thirdPartyAgent.ThirdPartyBuildInfo
 import com.tencent.devops.worker.common.Runner
 import com.tencent.devops.worker.common.SLAVE_AGENT_START_FILE
