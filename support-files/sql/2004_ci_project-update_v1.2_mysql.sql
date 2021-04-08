f4a2f544a1391af9d0e8c734c0b3f8007c6fe730
cao150260 <fitzcao@tencent.com>
2021-04-08 11:48:08 +0800

    feat: 请求服务支持consul tags路由 #3815 添加sql语句

--- support-files/sql/2004_ci_project-update_v1.2_mysql.sql
+++ support-files/sql/2004_ci_project-update_v1.2_mysql.sql
@@ -21,6 +21,15 @@ BEGIN
             ADD INDEX `idx_username` (`username`);
     END IF;
 
+	IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_PROJECT'
+                        AND COLUMN_NAME = 'router_tag') THEN
+        ALTER TABLE T_PIPELINE_BUILD_HISTORY
+            ADD COLUMN `router_tag` VARCHAR(32);
+    END IF;
+
     COMMIT;
 END <CI_UBF>
 DELIMITER ;
