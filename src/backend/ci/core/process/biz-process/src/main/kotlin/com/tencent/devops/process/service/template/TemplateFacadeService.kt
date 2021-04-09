314294b4224055ac57ceda6097a2379e6b1b09d7
carlyin <449036249@qq.com>
2021-04-09 16:39:54 +0800

    feat:安装模版到项目/模版实例化时，自动安装模版中需安装的容器镜像 #3756

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/TemplateFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/TemplateFacadeService.kt
@@ -1363,13 +1363,16 @@ class TemplateFacadeService @Autowired constructor(
         templateContent: String,
         templateInstanceUpdate: TemplateInstanceUpdate
     ) {
-        // 校验模板下组件可见范围
-        val validateRet = client.get(ServiceTemplateResource::class)
-            .validateUserTemplateComponentVisibleDept(
-                userId = userId,
-                templateCode = templateId,
-                projectCode = projectId
-            )
+        val srcTemplateId = templateDao.getSrcTemplateId(dslContext, templateId)
+        if (srcTemplateId != null) {
+            // 安装的研发商店模板需校验模板下组件可见范围
+            val validateRet = client.get(ServiceTemplateResource::class)
+                .validateUserTemplateComponentVisibleDept(
+                    userId = userId,
+                    templateCode = templateId,
+                    projectCode = projectId
+                )
+        }
         if (validateRet.isNotOk()) {
             throw ErrorCodeException(
                 errorCode = validateRet.status.toString(),
