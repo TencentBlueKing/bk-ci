630dec95a595c15add5235e209305cd2eef691a0
carlyin <449036249@qq.com>
2021-04-12 20:55:27 +0800

    feat:【流水线插件】工作台概览的流水线个数支持点击下钻查看流水线列表 #3840

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/dao/ProjectDao.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/dao/ProjectDao.kt
@@ -39,6 +39,7 @@ import com.tencent.devops.project.pojo.user.UserDeptDetail
 import org.jooq.Condition
 import org.jooq.DSLContext
 import org.jooq.Record
+import org.jooq.Record1
 import org.jooq.Result
 import org.jooq.UpdateConditionStep
 import org.jooq.impl.DSL
@@ -738,6 +739,15 @@ class ProjectDao {
             return dslContext.selectCount().from(this)
                 .where(PROJECT_NAME.like("%$projectName%"))
                 .fetchOne(0, Int::class.java)
+        }
+    }
+
+    fun listSecrecyProject(dslContext: DSLContext): Result<Record1<String>>? {
+        with(TProject.T_PROJECT) {
+            return dslContext.select(ENGLISH_NAME)
+                .from(this)
+                .where(IS_SECRECY.eq(true))
+                .fetch()
         }
     }
 }
