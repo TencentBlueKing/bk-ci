042e1c2afc2f0ba0800b1a86201b147329b6dd8f
cao150260 <fitzcao@tencent.com>
2021-04-12 20:44:30 +0800

    feat: 请求服务支持consul tags路由 #3815 mysql语句

--- support-files/sql/2004_ci_project-update_v1.4_mysql.sql
+++ support-files/sql/2004_ci_project-update_v1.4_mysql.sql
@@ -7,18 +7,16 @@ DELIMITER <CI_UBF>
 
 CREATE PROCEDURE ci_project_schema_update()
 BEGIN
-
     DECLARE db VARCHAR(100);
     SET AUTOCOMMIT = 0;
     SELECT DATABASE() INTO db;
-
-    IF NOT EXISTS(SELECT 1
-                  FROM information_schema.statistics
-                  WHERE TABLE_SCHEMA = db
-                    AND TABLE_NAME = 'T_FAVORITE'
-                    AND INDEX_NAME = 'idx_username') THEN
-        ALTER TABLE T_FAVORITE
-            ADD INDEX `idx_username` (`username`);
+	IF NOT EXISTS(SELECT 1
+                    FROM information_schema.COLUMNS
+                    WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_PROJECT'
+                        AND COLUMN_NAME = 'router_tag') THEN
+        ALTER TABLE T_PROJECT
+            ADD COLUMN `router_tag` VARCHAR(32);
     END IF;
 
     COMMIT;
