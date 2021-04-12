f71f924c6b5c3608ee5d7df37c62aff4651ce765
carlyin <449036249@qq.com>
2021-04-12 22:29:30 +0800

    feat:【流水线插件】工作台概览的流水线个数支持点击下钻查看流水线列表 #3840

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/impl/DefaultOpProjectServiceImpl.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/impl/DefaultOpProjectServiceImpl.kt
@@ -33,6 +33,7 @@ import com.tencent.devops.common.redis.RedisOperation
 import com.tencent.devops.common.service.gray.Gray
 import com.tencent.devops.common.service.gray.MacOSGray
 import com.tencent.devops.common.service.gray.RepoGray
+import com.tencent.devops.project.SECRECY_PROJECT_REDIS_KEY
 import com.tencent.devops.project.dao.ProjectDao
 import com.tencent.devops.project.dao.ProjectLabelRelDao
 import com.tencent.devops.project.dispatch.ProjectDispatcher
@@ -105,6 +106,11 @@ class DefaultOpProjectServiceImpl @Autowired constructor(
             val labelIdList = projectInfoRequest.labelIdList
             if (!CollectionUtils.isEmpty(labelIdList)) {
                 projectLabelRelDao.batchAdd(transactionContext, projectId = projectId, labelIdList = labelIdList!!)
+            }
+            if (!projectInfoRequest.secrecyFlag) {
+                redisOperation.removeSetMember(SECRECY_PROJECT_REDIS_KEY, dbProjectRecord.englishName)
+            } else {
+                redisOperation.addSetValue(SECRECY_PROJECT_REDIS_KEY, dbProjectRecord.englishName)
             }
         }
 
