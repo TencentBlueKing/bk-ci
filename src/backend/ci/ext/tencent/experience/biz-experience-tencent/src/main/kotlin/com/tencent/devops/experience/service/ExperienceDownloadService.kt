9d5fbbf1453805abce00d8fc72bae7ae2debd3ff
stubenhuang <stubenhuang@tencent.com>
2021-04-16 22:18:59 +0800

    --story=864038587 【Tony需求】下载记录页面优化
    # Conflicts:
    #	src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceDownloadService.kt

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceDownloadService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceDownloadService.kt
@@ -304,15 +304,16 @@ class ExperienceDownloadService @Autowired constructor(
             ).map { it.value1() }.toSet()
 
             val offset = (page - 1) * pageSize
+
             val experiences = experienceDao.listByIds(
                 dslContext = dslContext,
                 ids = experienceIdsByBundleId,
                 platform = PlatformEnum.of(platform)?.name,
                 expireTime = LocalDateTime.now(),
                 online = true,
-                offset = offset,
-                limit = pageSize
-            ).map {
+                offset = 0,
+                limit = experienceIdsByBundleId.size
+            ).asSequence().map {
                 DownloadRecordVO(
                     experienceHashId = HashUtil.encodeLongId(it.id),
                     size = it.size,
@@ -323,7 +324,7 @@ class ExperienceDownloadService @Autowired constructor(
                     downloadTime = experienceIdDownloadTimeMap[it.id]?.timestampmilli() ?: 0,
                     bundleIdentifier = it.bundleIdentifier
                 )
-            }.sortByDescending { it.downloadTime }.toList()
+            }.sortedByDescending { it.downloadTime }.toList().subList(offset, pageSize)
             val hasNext = page * pageSize < experienceDao.countByIds(
                 dslContext = dslContext,
                 ids = experienceIdsByBundleId,
