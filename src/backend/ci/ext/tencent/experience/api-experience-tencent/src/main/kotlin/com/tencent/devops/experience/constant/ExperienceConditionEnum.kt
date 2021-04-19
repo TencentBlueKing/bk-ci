d4d693582ae639352256144cb6c89d5740c10300
stubenhuang <stubenhuang@tencent.com>
2021-04-19 14:53:14 +0800

    --story=863941799 内部体验/公开体验标签展示逻辑优化

--- src/backend/ci/ext/tencent/experience/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/constant/ExperienceConditionEnum.kt
+++ src/backend/ci/ext/tencent/experience/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/constant/ExperienceConditionEnum.kt
@@ -5,12 +5,16 @@ enum class ExperienceConditionEnum(
 ) {
     UNKNOWN(0),
 
+    // 体验范围仅是公开体验
     JUST_PUBLIC(1),
 
+    // 体验范围仅是内部体验
     JUST_PRIVATE(2),
 
+    // 体验范围同时包括公开体验和内部体验组 , 用户属于内部体验组
     BOTH_WITH_PRIVATE(3),
 
+    // 体验范围同时包括公开体验和内部体验组 , 用户不在内部体验组
     BOTH_WITHOUT_PRIVATE(4),
 
     ;
