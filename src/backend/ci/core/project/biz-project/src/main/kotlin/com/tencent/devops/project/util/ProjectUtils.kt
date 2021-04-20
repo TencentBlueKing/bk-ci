e2a4ddfb2334013b730785ed56abb97497166c92
cao150260 <fitzcao@tencent.com>
2021-04-20 17:21:34 +0800

    feat: 全量对接权限中心V3 #3854 project列表返回routerTag

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/util/ProjectUtils.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/util/ProjectUtils.kt
@@ -87,7 +87,8 @@ object ProjectUtils {
             gray = grayProjectSet.contains(tProjectRecord.englishName),
             hybridCcAppId = tProjectRecord.hybridCcAppId,
             enableExternal = tProjectRecord.enableExternal,
-            pipelineLimit = tProjectRecord.pipelineLimit
+            pipelineLimit = tProjectRecord.pipelineLimit,
+            routerTag = tProjectRecord.routerTag
         )
     }
 }
