07de0a4b78bf48994fc060ffb0b2697b6305df00
v_hwweng <v_hwweng@tencent.com>
2021-04-22 15:02:07 +0800

    feat: bk-ci gitlab插件支持监听指定的事件 issue #3664

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/CodeGitlabWebHookTrigger.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/CodeGitlabWebHookTrigger.vue
@@ -22,14 +22,13 @@
         name: 'code-git-lab-hook-trigger',
         mixins: [atomMixin, validMixins],
         created () {
-            console.log(this.atomPropsModel, 'element')
-            if (this.element.eventType === 'MERGE_REQUEST') {
-                this.atomPropsModel.block.hidden = false
-                this.atomPropsModel.webhookQueue.hidden = false
-            } else {
-                this.atomPropsModel.block.hidden = true
-                this.atomPropsModel.webhookQueue.hidden = true
-            }
+            // if (this.element.eventType === 'MERGE_REQUEST') {
+            //     this.atomPropsModel.block.hidden = false
+            //     this.atomPropsModel.webhookQueue.hidden = false
+            // } else {
+            //     this.atomPropsModel.block.hidden = true
+            //     this.atomPropsModel.webhookQueue.hidden = true
+            // }
             if (!this.element.repositoryType) {
                 this.handleUpdateElement('repositoryType', 'ID')
             }
@@ -37,13 +36,13 @@
         },
         methods: {
             handleBlockEnable (name, value) {
-                if (value === 'MERGE_REQUEST') {
-                    this.atomPropsModel.block.hidden = false
-                    this.atomPropsModel.webhookQueue.hidden = false
-                } else {
-                    this.atomPropsModel.block.hidden = true
-                    this.atomPropsModel.webhookQueue.hidden = true
-                }
+                // if (value === 'MERGE_REQUEST') {
+                //     this.atomPropsModel.block.hidden = false
+                //     this.atomPropsModel.webhookQueue.hidden = false
+                // } else {
+                //     this.atomPropsModel.block.hidden = true
+                //     this.atomPropsModel.webhookQueue.hidden = true
+                // }
                 this.handleUpdateElement(name, value)
             },
             handleMethods (name, value) {
