7cc7066e1423e2e79057a2dace61a9ee7ea7da73
cao150260 <fitzcao@tencent.com>
2021-04-23 11:57:51 +0800

    --story=863222655 对接蓝鲸权限中心V3 data定义@Component清理

--- src/backend/ci/ext/tencent/common/common-gcloud/src/main/kotlin/com/tencent/devops/common/gcloud/api/pojo/CommonParam.kt
+++ src/backend/ci/ext/tencent/common/common-gcloud/src/main/kotlin/com/tencent/devops/common/gcloud/api/pojo/CommonParam.kt
@@ -27,9 +27,6 @@
 
 package com.tencent.devops.common.gcloud.api.pojo
 
-import org.springframework.stereotype.Component
-
-@Component
 data class CommonParam(
     val gameId: String,
     val accessId: String,
