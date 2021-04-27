35cc8975dc5665c34975575fcc08c9501d8a87bd
cao150260 <fitzcao@tencent.com>
2021-04-27 22:01:37 +0800

    feat: 全量对接权限中心V3 #3854 iam用户组命名规则

--- src/backend/ci/core/common/common-auth/common-auth-v3/src/test/kotlin/com/tencent/devops/common/auth/utils/IamUtilsTest.kt
+++ src/backend/ci/core/common/common-auth/common-auth-v3/src/test/kotlin/com/tencent/devops/common/auth/utils/IamUtilsTest.kt
@@ -23,25 +23,37 @@
  * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 package com.tencent.devops.common.auth.utils
 
-import com.tencent.devops.common.api.util.DateTimeUtil
-import java.time.LocalDateTime
+import org.junit.Assert
+import org.junit.Test
 
-object IamUtils {
+internal class IamUtilsTest {
 
-    fun buildIamGroup(projectName: String, groupName: String): String {
-        return "$projectName-$groupName"
+    @Test
+    fun buildIamGroup() {
+        val projectName = "测试项目"
+        val groupName = "管理员"
+        val group = IamUtils.buildIamGroup(projectName, groupName)
+        Assert.assertEquals("测试项目-管理员", group)
     }
 
-    fun buildDefaultDescription(projectName: String, groupName: String, userId: String): String {
-        return "$projectName 用户组:$groupName, 由$userId 创建于 ${DateTimeUtil.toDateTime(LocalDateTime.now(), "yyyy-MM-dd'T'HH:mm:ssZ")}"
+    @Test
+    fun buildDefaultDescription() {
+        val projectName = "测试项目"
+        val groupName = "管理员"
+        val userId = "admin"
+        val group = IamUtils.buildDefaultDescription(projectName, groupName, userId)
+        println(group)
     }
 
-    fun buildManagerDescription(projectName: String, userId: String) : String {
-        return "$projectName 分级管理员, 由$userId 创建于 ${DateTimeUtil.toDateTime(LocalDateTime.now(), "yyyy-MM-dd'T'HH:mm:ssZ")}"
+    @Test
+    fun buildManagerDescription() {
+        val projectName = "测试项目"
+        val userId = "admin"
+        val group = IamUtils.buildManagerDescription(projectName, userId)
+        println(group)
     }
 }
