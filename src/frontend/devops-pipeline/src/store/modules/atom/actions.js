e66eae9ec70c2a3683c830147c3e8592c20acb67
xuzhan <xuzhan@tencent.com>
2021-04-28 20:10:42 +0800

    feature: 项目下已安装的插件列表，支持分页 issue #3869

--- src/frontend/devops-pipeline/src/store/modules/atom/actions.js
+++ src/frontend/devops-pipeline/src/store/modules/atom/actions.js
@@ -426,7 +426,7 @@ export default {
 
     // 获取项目下已安装的插件列表
     getInstallAtomList ({ commit }, projectCode) {
-        return request.get(`${STORE_API_URL_PREFIX}/user/pipeline/atom/projectCodes/${projectCode}/list?page=1&pageSize=1000`)
+        return request.get(`${STORE_API_URL_PREFIX}/user/pipeline/atom/projectCodes/${projectCode}/list?page=1&pageSize=2000`)
     },
 
     // 获取已安装的插件详情
