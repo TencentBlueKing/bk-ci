119d9d2fabc9b7f8ed9a0fe7d96bfdab52cd55e6
sawyersong2 <wudiduomjimo@gmail.com>
2021-04-28 19:51:46 +0800

    --story=864167229 开发代码相关事项 stage job if表达式 job dependOn

--- src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/test/kotlin/com/tencent/devops/common/ci/yaml/v2/utils/ScriptYmlUtilsTest.kt
+++ src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/test/kotlin/com/tencent/devops/common/ci/yaml/v2/utils/ScriptYmlUtilsTest.kt
@@ -30,10 +30,9 @@ package com.tencent.devops.common.ci.yaml.v2.utils
 import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.common.api.util.YamlUtil
-import com.tencent.devops.common.ci.yaml.v2.PreScriptBuildYaml
-import com.tencent.devops.common.ci.yaml.v2.PushRule
-import com.tencent.devops.common.ci.yaml.v2.ScriptBuildYaml
-import com.tencent.devops.common.ci.yaml.v2.YmlVersion
+import com.tencent.devops.common.ci.v2.PreScriptBuildYaml
+import com.tencent.devops.common.ci.v2.PushRule
+import com.tencent.devops.common.ci.v2.utils.ScriptYmlUtils
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -42,6 +41,7 @@ import org.springframework.core.io.ClassPathResource
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.util.regex.Pattern
 
 class ScriptYmlUtilsTest {
 
@@ -110,7 +110,44 @@ class ScriptYmlUtilsTest {
 
             println(JsonUtil.toJson(pushRule!!))
         }
+    }
 
+    @Test
+    fun variableTest() {
+        val classPathResource = ClassPathResource("Sample1.yml")
+        val inputStream: InputStream = classPathResource.inputStream
+        val isReader = InputStreamReader(inputStream)
 
+        val reader = BufferedReader(isReader)
+        val sb = StringBuffer()
+        var str: String?
+        while (reader.readLine().also { str = it } != null) {
+            sb.append(str).append("\n")
+        }
+
+        val obj = YamlUtil.getObjectMapper().readValue(
+            ScriptYmlUtils.formatYaml(sb.toString()),
+            PreScriptBuildYaml::class.java
+        )
+
+        obj.variables!!.forEach { t, u ->
+            println("1111" + u.value)
+            val settingMap = mapOf("sss" to "123", "approve22" to "ssdsdsd")
+            println(formatVariablesValue(u.value!!, settingMap))
+        }
+        println(obj.variables)
+    }
+
+    private fun formatVariablesValue(value: String, settingMap: Map<String, String>): String {
+        var newValue = value
+        val pattern = Pattern.compile("\\$\\{\\{([^{}]+?)}}")
+        val matcher = pattern.matcher(value)
+        while (matcher.find()) {
+            println("2222" + matcher.group(0))
+            println("2222" + matcher.group(1))
+            val realValue = settingMap[matcher.group(1).trim()]
+            newValue = newValue.replace(matcher.group(), realValue!!)
+        }
+        return newValue
     }
 }
