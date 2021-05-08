3f8cf2f8dc106382f8d78703033af9b597c8ba56
cao150260 <fitzcao@tencent.com>
2021-05-08 19:39:05 +0800

    feat: 全量对接权限中心V3 #3854 构造函数

--- src/backend/ci/core/auth/biz-auth-blueking/src/main/kotlin/com/tencent/devops/auth/service/BkPermissionProjectService.kt
+++ src/backend/ci/core/auth/biz-auth-blueking/src/main/kotlin/com/tencent/devops/auth/service/BkPermissionProjectService.kt
@@ -1,11 +1,36 @@
 package com.tencent.devops.auth.service
 
+import com.tencent.bk.sdk.iam.config.IamConfiguration
+import com.tencent.bk.sdk.iam.helper.AuthHelper
+import com.tencent.bk.sdk.iam.service.PolicyService
+import com.tencent.devops.auth.service.iam.PermissionRoleMemberService
+import com.tencent.devops.auth.service.iam.PermissionRoleService
 import com.tencent.devops.auth.service.iam.impl.AbsPermissionProjectService
 import com.tencent.devops.common.auth.api.pojo.BKAuthProjectRolesResources
 import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import com.tencent.devops.common.auth.api.pojo.BkAuthGroupAndUserList
+import com.tencent.devops.common.client.Client
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class BkPermissionProjectService : AbsPermissionProjectService() {
+@Service
+class BkPermissionProjectService @Autowired constructor(
+    override val permissionRoleService: PermissionRoleService,
+    override val permissionRoleMemberService: PermissionRoleMemberService,
+    override val authHelper: AuthHelper,
+    override val policyService: PolicyService,
+    override val client: Client,
+    override val iamConfiguration: IamConfiguration,
+    override val deptService: DeptService
+) : AbsPermissionProjectService(
+    permissionRoleService = permissionRoleService,
+    permissionRoleMemberService = permissionRoleMemberService,
+    authHelper = authHelper,
+    policyService = policyService,
+    client = client,
+    iamConfiguration = iamConfiguration,
+    deptService = deptService
+) {
     override fun getProjectUsers(serviceCode: String, projectCode: String, group: BkAuthGroup?): List<String> {
         return super.getProjectUsers(serviceCode, projectCode, group)
     }
