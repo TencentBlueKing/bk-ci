856ef25b3a4a70e5f579da071ab69c02669aa275
ruotiantang <ruotiantang@tencent.com>
2021-05-18 18:01:07 +0800

    --story=864167229 开发代码相关事项
    添加根据流水线获取yaml接口

--- src/backend/ci/ext/tencent/gitci/api-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/api/user/UserGitCITriggerResource.kt
+++ src/backend/ci/ext/tencent/gitci/api-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/api/user/UserGitCITriggerResource.kt
@@ -103,7 +103,7 @@ interface UserGitCITriggerResource {
 
     @ApiOperation("根据PipelinId和分支查询Yaml内容")
     @GET
-    @Path("/getYaml/{projectId}/{pipelineId}")
+    @Path("/{projectId}/{pipelineId}/yaml")
     fun getYamlByPipeline(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
