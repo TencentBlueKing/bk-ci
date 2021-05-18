657db8a7eff876ca92dbeafb77344783cad2a6e8
johuang <huangjiao511@163.com>
2021-05-18 17:54:10 +0800

    --story=864429801 GITCI2.0-支持上下文功能

--- src/backend/ci/ext/tencent/worker/worker-api-tencent/src/main/kotlin/com/tencent/devops/worker/common/api/engine/impl/TencentEngineBuildResourceApi.kt
+++ src/backend/ci/ext/tencent/worker/worker-api-tencent/src/main/kotlin/com/tencent/devops/worker/common/api/engine/impl/TencentEngineBuildResourceApi.kt
@@ -49,14 +49,18 @@ class TencentEngineBuildResourceApi : EngineBuildResourceApi(), EngineBuildSDKAp
     }
 
     override fun getCiToken(): String {
-        val projectId = AgentEnv.getProjectId()
-        if (projectId.startsWith("git_")) {
-            val gitProjectId = projectId.removePrefix("git_")
-            val url = "/ms/repository/api/build/gitci/getToken?gitProjectId=$gitProjectId"
-            val request = buildGet(url)
-            val responseContent = request(request, "获取工蜂CI项目Token失败！")
-            val gitToken = objectMapper.readValue<Result<GitToken>>(responseContent)
-            return gitToken.data?.accessToken ?: ""
+        try {
+            val projectId = AgentEnv.getProjectId()
+            if (projectId.startsWith("git_")) {
+                val gitProjectId = projectId.removePrefix("git_")
+                val url = "/ms/repository/api/build/gitci/getToken?gitProjectId=$gitProjectId"
+                val request = buildGet(url)
+                val responseContent = request(request, "获取工蜂CI项目Token失败！")
+                val gitToken = objectMapper.readValue<Result<GitToken>>(responseContent)
+                return gitToken.data?.accessToken ?: ""
+            }
+        } catch (e: Exception) {
+            logger.error("get ci token failed.", e)
         }
 
         return super.getCiToken()
