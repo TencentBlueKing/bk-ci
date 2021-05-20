1117e525a77ffb6b9039c28dccb5061b720c60b3
stubenhuang <stubenhuang@tencent.com>
2021-05-20 16:12:17 +0800

    --story=865205375 【外部登录】后台 - 限制多端登录

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceOuterService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceOuterService.kt
@@ -11,10 +11,12 @@ import com.tencent.devops.common.api.util.UUIDUtil
 import com.tencent.devops.common.redis.RedisOperation
 import com.tencent.devops.common.service.utils.HomeHostUtil
 import com.tencent.devops.experience.constant.ExperienceMessageCode
+import com.tencent.devops.experience.dao.ExperienceOuterLoginRecordDao
 import com.tencent.devops.experience.pojo.outer.OuterLoginParam
 import com.tencent.devops.experience.pojo.outer.OuterProfileVO
 import org.apache.commons.codec.digest.DigestUtils
 import org.apache.commons.lang3.StringUtils
+import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.connection.RedisStringCommands
@@ -29,9 +31,16 @@ import javax.ws.rs.core.Response
 class ExperienceOuterService @Autowired constructor(
     private val redisOperation: RedisOperation,
     private val loginApi: V1Api,
-    private val profileApi: ProfilesApi
+    private val profileApi: ProfilesApi,
+    private val experienceOuterLoginRecordDao: ExperienceOuterLoginRecordDao,
+    private val dslContext: DSLContext
 ) {
-    fun outerLogin(realIp: String, params: OuterLoginParam): String {
+    fun outerLogin(
+        platform: Int,
+        appVersion: String?,
+        realIp: String,
+        params: OuterLoginParam
+    ): String {
         // IP黑名单
         if (isBlackIp(realIp)) {
             logger.warn("it is black ip : {}", realIp)
@@ -86,9 +95,23 @@ class ExperienceOuterService @Autowired constructor(
                 logo = logo(),
                 email = profile.email
             )
+
+            // 设置token
             val token = DigestUtils.md5Hex(profile.username + profile.id + UUIDUtil.generate())
-            redisOperation.set(tokenRedisKey(token), JsonUtil.toJson(outerProfileVO), expireSecs) // 设置token
+            redisOperation.set(tokenRedisKey(token), JsonUtil.toJson(outerProfileVO), expireSecs)
+
+            // 单token有效
             singleTokenLogin(profile.username, token)
+
+            // 记录登录历史
+            experienceOuterLoginRecordDao.add(
+                dslContext = dslContext,
+                username = profile.username,
+                realIp = realIp,
+                loginTime = LocalDateTime.now(),
+                appVersion = appVersion ?: "",
+                platform = platform
+            )
             return token
         } catch (e: ApiException) {
             logger.warn("login error", e)
