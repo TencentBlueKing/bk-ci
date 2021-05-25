ec0a48b36c024c1f6c8c6c3e036209dbb3250451
royalhuang <royalhuang@tencent.com>
2021-05-25 22:21:48 +0800

    bug: Stage审核的流水线运行构建计数的漏洞 #4255 解决单行过长问题

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineStageService.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineStageService.kt
@@ -225,7 +225,10 @@ class PipelineStageService @Autowired constructor(
 
             // #4255 stage审核超时恢复运行状态需要将运行状态+1，即使直接结束也会在finish阶段减回来
             pipelineBuildSummaryDao.updateRunningCount(
-                dslContext = context, pipelineId = buildStage.pipelineId, buildId = buildStage.buildId, runningIncrement = 1
+                dslContext = context,
+                pipelineId = buildStage.pipelineId,
+                buildId = buildStage.buildId,
+                runningIncrement = 1
             )
         }
         // #3138 Stage Cancel 需要走finally Stage流程
