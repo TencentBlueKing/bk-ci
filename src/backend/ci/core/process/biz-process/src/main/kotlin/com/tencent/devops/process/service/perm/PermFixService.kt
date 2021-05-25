fd2fb62d103dc94b66977d3a7a1bf5fd97fa7b52
irwinsun <irwinsun@tencent.com>
2021-05-25 20:29:35 +0800

    --bug=87994857 webook与子流水线的权限修正 先只打log并修改最后修改人，收集出有问题的人员和流水线 不改版本号

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/perm/PermFixService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/perm/PermFixService.kt
@@ -62,7 +62,7 @@ class PermFixService @Autowired constructor(
 
             val admin = authProjectApi.getProjectUsers(serviceCode, projectId, BkAuthGroup.MANAGER).firstOrNull()
             if (!admin.isNullOrBlank()) { // 遇到已经丢失权限的更新最后修改人为管理员
-                pipelineInfoDao.update(dslContext = dslContext, pipelineId = pipelineId, userId = admin)
+                pipelineInfoDao.update(dslContext, pipelineId = pipelineId, userId = admin, updateVersion = false)
                 LOG.warn("BKSystemErrorMonitor|BAD_USER|$userId|$projectId|$pipelineId|$admin")
             } else {
                 LOG.error("BKSystemErrorMonitor|EMPTY_ADMIN|$projectId|$pipelineId")
