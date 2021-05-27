9c01e7b8f9a897e5b5814257a6f774900ff1e4e6
fromschen <fromschen@tencent.com>
2021-05-27 16:47:58 +0800

    chore: 部署体验优化 #4193
    paas app注册后移, 允许失败. mysql命令在中控机渲染.

--- scripts/bk-ci-connect-bkce.sh
+++ scripts/bk-ci-connect-bkce.sh
@@ -16,16 +16,14 @@ cd "$CTRL_DIR"
 echo "注册 蓝鲸 ESB"
 ./bin/add_or_update_appcode.sh "$BK_CI_APP_CODE" "$BK_CI_APP_TOKEN" "蓝盾" "mysql-paas"  # 注册app。第4个参数即是login-path。
 
-echo "注册 PaaS 桌面图标"
-./bin/bk-ci-reg-paas-app.sh
-
 echo "配置数据库访问权限."
 ./bin/setup_mysql_loginpath.sh -n mysql-ci -h "${BK_CI_MYSQL_ADDR%:*}" -u "$BK_CI_MYSQL_USER" -p "$BK_CI_MYSQL_PASSWORD"
 sleep 1
 # 如果使用了蓝鲸默认的数据库, 则自动刷新权限.
 if [ "${BK_CI_MYSQL_ADDR%:*}" = "$BK_MYSQL_IP" ]; then
-  ./pcmd.sh -H "$BK_MYSQL_IP" '$CTRL_DIR/bin/grant_mysql_priv.sh -n default-root -u "$BK_CI_MYSQL_USER" -p "$BK_CI_MYSQL_PASSWORD" -H "$(<$CTRL_DIR/.controller_ip)"'
-  ./pcmd.sh -H "$BK_MYSQL_IP" '$CTRL_DIR/bin/grant_mysql_priv.sh -n default-root -u "$BK_CI_MYSQL_USER" -p "$BK_CI_MYSQL_PASSWORD" -H "$BK_CI_IP_COMMA"'
+  # 提前渲染变量, 避免mysql服务端没有ci的变量.
+  ./pcmd.sh -H "$BK_MYSQL_IP" "$CTRL_DIR/bin/grant_mysql_priv.sh -n default-root -u \"$BK_CI_MYSQL_USER\" -p \"$BK_CI_MYSQL_PASSWORD\" -H \"$(<$CTRL_DIR/.controller_ip)\""
+  ./pcmd.sh -H "$BK_MYSQL_IP" "$CTRL_DIR/bin/grant_mysql_priv.sh -n default-root -u \"$BK_CI_MYSQL_USER\" -p \"$BK_CI_MYSQL_PASSWORD\" -H \"$BK_CI_IP_COMMA\""
 else
   echo "你使用了第三方MySQL数据库. 请自行检查CI全部节点能否访问数据库."
 fi
@@ -40,10 +38,16 @@ echo " 服务端启用rabbitmq_delayed_message_exchange插件."
 echo " 检查所有服务端的 rabbitmq_delayed_message_exchange 插件是否启用"
 ./pcmd.sh -m rabbitmq 'rabbitmq-plugins list | grep rabbitmq_delayed_message_exchange | grep -F "E*"'
 echo " 创建vhost及账户."
-./pcmd.sh -H "$BK_RABBITMQ_IP" '$CTRL_DIR/bin/add_rabbitmq_user.sh -u "$BK_CI_RABBITMQ_USER" -p "$BK_CI_RABBITMQ_PASSWORD" -h "$BK_CI_RABBITMQ_VHOST" && rabbitmqctl change_password "$BK_CI_RABBITMQ_USER" "$BK_CI_RABBITMQ_PASSWORD"'
+./pcmd.sh -H "$BK_RABBITMQ_IP" "$CTRL_DIR/bin/add_rabbitmq_user.sh -u \"$BK_CI_RABBITMQ_USER\" -p \"$BK_CI_RABBITMQ_PASSWORD\" -h \"$BK_CI_RABBITMQ_VHOST\" && rabbitmqctl change_password \"$BK_CI_RABBITMQ_USER\" \"$BK_CI_RABBITMQ_PASSWORD\""
 
 
 echo "导入 IAM 权限模板"
 ./bin/bkiam_migrate.sh -t "$BK_IAM_PRIVATE_URL" -a "$BK_CI_APP_CODE" -s "$BK_CI_APP_TOKEN" $BK_PKG_SRC_PATH/ci/support-files/bkiam/*.json
 
+# 不影响部署结果, 如果报错可以忽略:
+echo "注册 PaaS 桌面图标"
+$BK_CI_SRC_DIR/scripts/bk-ci-reg-paas-app.sh || {
+  echo "未能注册PaaS桌面图标. 您可以忽略此步骤."
+  exit 1
+}
 
