9af56c36faaea4fd3400c955997c1fe5e9c836ae
ruotiantang <ruotiantang@tencent.com>
2021-06-04 19:55:18 +0800

    --bug=88482063 V1版本回写commitCheck失败

--- src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/listener/GitCIBuildFinishListener.kt
+++ src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/listener/GitCIBuildFinishListener.kt
@@ -158,7 +158,7 @@ class GitCIBuildFinishListener @Autowired constructor(
                 // 检查yml版本，根据yml版本选择不同的实现
                 // TODO: 目前V1的yaml转换会因为!-< 报错，后续订最终方案
                 val isV2 = try {
-                     ScriptYmlUtils.isV2Version(event.normalizedYaml)
+                    ScriptYmlUtils.isV2Version(event.normalizedYaml)
                 } catch (e: Exception) {
                     logger.error("$buildFinishEvent.buildId ，${e.message}")
                     false
