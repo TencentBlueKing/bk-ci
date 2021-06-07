f277eeebdcfc277d144cd1554235b362c4efa859
fitzcao <cao150260819@126.com>
2021-06-07 16:48:13 +0800

    feat: 全量对接权限中心V3 #3854 合并4335 auth接口相关变更

--- src/backend/ci/core/auth/biz-auth-blueking/src/main/kotlin/com/tencent/devops/auth/service/BkAuthPermissionProjectService.kt
+++ src/backend/ci/core/auth/biz-auth-blueking/src/main/kotlin/com/tencent/devops/auth/service/BkAuthPermissionProjectService.kt
@@ -4,16 +4,13 @@ import com.tencent.devops.auth.service.iam.PermissionProjectService
 import com.tencent.devops.common.auth.api.pojo.BKAuthProjectRolesResources
 import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import com.tencent.devops.common.auth.api.pojo.BkAuthGroupAndUserList
-import org.springframework.beans.factory.annotation.Autowired
 
-class BkAuthPermissionProjectService @Autowired constructor(
-
-): PermissionProjectService {
-    override fun getProjectUsers(serviceCode: String, projectCode: String, group: BkAuthGroup?): List<String> {
+class BkAuthPermissionProjectService : PermissionProjectService {
+    override fun getProjectUsers(projectCode: String, group: BkAuthGroup?): List<String> {
         return emptyList()
     }
 
-    override fun getProjectGroupAndUserList(serviceCode: String, projectCode: String): List<BkAuthGroupAndUserList> {
+    override fun getProjectGroupAndUserList(projectCode: String): List<BkAuthGroupAndUserList> {
         return emptyList()
     }
 
