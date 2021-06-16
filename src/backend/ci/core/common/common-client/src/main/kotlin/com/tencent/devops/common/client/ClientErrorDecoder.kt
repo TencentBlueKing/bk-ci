731e0defbd78491ee2f04e2db89b986105006cf7
mingshewhe <mingshewhe@tencent.com>
2021-06-16 10:33:27 +0800

    feature: git获取文件内容超过1M应返回指定的错误码 #4284

--- src/backend/ci/core/common/common-client/src/main/kotlin/com/tencent/devops/common/client/ClientErrorDecoder.kt
+++ src/backend/ci/core/common/common-client/src/main/kotlin/com/tencent/devops/common/client/ClientErrorDecoder.kt
@@ -31,8 +31,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.exception.ClientException
 import com.tencent.devops.common.api.exception.ErrorCodeException
-import com.tencent.devops.common.api.exception.OperationException
-import com.tencent.devops.common.api.exception.RemoteServiceException
 import com.tencent.devops.common.api.pojo.Result
 import feign.Response
 import feign.codec.ErrorDecoder
@@ -46,23 +44,20 @@ import java.io.IOException
  */
 @Service
 class ClientErrorDecoder @Autowired constructor(val objectMapper: ObjectMapper) : ErrorDecoder {
+
     override fun decode(methodKey: String, response: Response): Exception {
         // 首先判断返回结果是否能被序列化
         val responseStream = response.body().asInputStream()
         val result: Result<*>
         try {
             result = objectMapper.readValue(responseStream)
-        } catch (e: IOException) {
+        } catch (ignore: IOException) {
             return ClientException("内部服务返回结果无法解析")
         }
-        if (result.status == OperationException.statusCode) {
-            throw OperationException(result.message ?: "")
-        } else if (response.status() == OperationException.statusCode) {
-            throw ErrorCodeException(
-                errorCode = result.status.toString(),
-                defaultMessage = result.message
-            )
-        }
-        return RemoteServiceException(result.message ?: "", response.status())
+        return ErrorCodeException(
+            statusCode = response.status(),
+            errorCode = result.status.toString(),
+            defaultMessage = result.message
+        )
     }
 }
