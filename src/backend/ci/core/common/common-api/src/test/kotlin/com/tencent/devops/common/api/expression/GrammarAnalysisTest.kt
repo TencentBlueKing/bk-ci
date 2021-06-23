24dce634c54a8ccbfbb88946ffcf684525b8a851
irwinsun <irwinsun@tencent.com>
2021-06-23 22:31:03 +0800

    fix: 规范REST接口的业务与系统异常的HTTP状态码 #4382 修正

--- src/backend/ci/core/common/common-api/src/test/kotlin/com/tencent/devops/common/api/expression/GrammarAnalysisTest.kt
+++ src/backend/ci/core/common/common-api/src/test/kotlin/com/tencent/devops/common/api/expression/GrammarAnalysisTest.kt
@@ -30,7 +30,7 @@ package com.tencent.devops.common.api.expression
 import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
 import org.junit.Test
-import java.lang.RuntimeException
+import java.lang.IllegalArgumentException
 
 class GrammarAnalysisTest {
 
@@ -76,7 +76,7 @@ class GrammarAnalysisTest {
         try {
             GrammarAnalysis(items).analysis()
         } catch (e: Exception) {
-            assertEquals(e.javaClass, RuntimeException::class.java)
+            assertEquals(e.javaClass, IllegalArgumentException::class.java)
         }
     }
 }
