2607f437fbb11f64a48e5682f1c2ce9b0204714c
royalhuang <royalhuang@tencent.com>
2021-06-24 00:18:33 +0800

    feat: 研发商店支持新版yml文本导出接口 #4480

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomServiceImpl.kt
@@ -103,7 +103,6 @@ import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.util.StringUtils
-import java.lang.StringBuilder
 import java.time.LocalDateTime
 import java.util.Calendar
 import java.util.concurrent.Callable
