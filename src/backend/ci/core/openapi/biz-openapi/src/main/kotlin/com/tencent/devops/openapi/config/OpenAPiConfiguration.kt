6dbf8769ebb275a6145eb1a0f8ac5485a3eaeefc
fitzcao <cao150260819@126.com>
2021-06-23 11:37:58 +0800

    feat: 公共帐号与员工帐号绑定关系 #4390

--- src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/config/OpenAPiConfiguration.kt
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/config/OpenAPiConfiguration.kt
@@ -37,6 +37,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class OpenAPiConfiguration {
     @Bean
-    @ConditionalOnMissingBean(name = ["OpAppUserService"])
+    @ConditionalOnMissingBean(name = ["opAppUserService"])
     fun opAppUserService() = DefaultOpAppUserService()
 }
