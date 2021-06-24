3cc85f75553053c77fe112f4aaa1affcf3e7d623
fitzcao <cao150260819@126.com>
2021-06-24 17:00:51 +0800

    --story=863222655 对接蓝鲸权限中心V3  LOG+ART添加pipelineId转化

--- src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/permission/TxV3ArtPipelineServiceImpl.kt
+++ src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/permission/TxV3ArtPipelineServiceImpl.kt
@@ -2,12 +2,16 @@ package com.tencent.devops.artifactory.service.permission
 
 import com.tencent.devops.artifactory.service.PipelineService
 import com.tencent.devops.auth.api.service.ServicePermissionAuthResource
+import com.tencent.devops.auth.api.service.ServiceProjectAuthResource
 import com.tencent.devops.common.api.exception.PermissionForbiddenException
 import com.tencent.devops.common.auth.api.AuthPermission
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.utils.TActionUtils
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.client.ClientTokenService
 import com.tencent.devops.common.pipeline.enums.ChannelCode
 import com.tencent.devops.process.api.service.ServicePipelineResource
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
 class TxV3ArtPipelineServiceImpl @Autowired constructor(
@@ -32,13 +36,23 @@ class TxV3ArtPipelineServiceImpl @Autowired constructor(
         pipelineId: String?,
         permission: AuthPermission?
     ): Boolean {
-        return client.get(ServicePermissionAuthResource::class).validateUserResourcePermission(
-            userId = userId,
-            token = tokenCheckService.getSystemToken(null) ?: "",
-            action = "",
-            projectCode = projectId,
-            resourceCode = null
-        ).data ?: false
+        return if (pipelineId.isNullOrEmpty()) {
+            client.get(ServiceProjectAuthResource::class).isProjectUser(
+                userId = userId,
+                token = tokenCheckService.getSystemToken(null) ?: "",
+                projectCode = projectId
+            ).data ?: false
+        } else {
+            client.get(ServicePermissionAuthResource::class).validateUserResourcePermission(
+                userId = userId,
+                token = tokenCheckService.getSystemToken(null) ?: "",
+                action = TActionUtils.buildAction(
+                    authPermission = permission ?: AuthPermission.VIEW,
+                    authResourceType = AuthResourceType.PIPELINE_DEFAULT),
+                projectCode = projectId,
+                resourceCode = getPipelineId(pipelineId!!, projectId)
+            ).data ?: false
+        }
     }
 
     override fun filterPipeline(user: String, projectId: String): List<String> {
@@ -59,5 +73,20 @@ class TxV3ArtPipelineServiceImpl @Autowired constructor(
         } else {
             emptyList()
         }
+    }
+
+
+    private fun getPipelineId(pipelineId: String, projectCode: String): String {
+        val pipelineIdInfo = client.get(ServicePipelineResource::class)
+            .getPipelineId(projectCode, pipelineId).data
+        if (pipelineIdInfo == null) {
+            logger.warn("$pipelineId find autoId is empty")
+            return pipelineId
+        }
+        return pipelineIdInfo.id.toString()
+    }
+
+    companion object {
+        val logger = LoggerFactory.getLogger(TxV3ArtPipelineServiceImpl::class.java)
     }
 }
--- src/backend/ci/ext/tencent/log/biz-log-tencent/src/main/kotlin/com/tencent/devops/log/service/TxV3LogPermissionServiceImpl.kt
+++ src/backend/ci/ext/tencent/log/biz-log-tencent/src/main/kotlin/com/tencent/devops/log/service/TxV3LogPermissionServiceImpl.kt
@@ -4,8 +4,10 @@ import com.tencent.devops.auth.api.service.ServicePermissionAuthResource
 import com.tencent.devops.common.auth.api.AuthPermission
 import com.tencent.devops.common.auth.api.AuthResourceType
 import com.tencent.devops.common.auth.utils.GitCIUtils
+import com.tencent.devops.common.auth.utils.TActionUtils
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.client.ClientTokenService
+import com.tencent.devops.process.api.service.ServicePipelineResource
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -19,14 +21,24 @@ class TxV3LogPermissionServiceImpl @Autowired constructor(
         userId: String,
         permission: AuthPermission?
     ): Boolean {
-        val action = permission?.value ?: AuthPermission.VIEW.value
+        val action = TActionUtils.buildAction(permission ?: AuthPermission.VIEW, AuthResourceType.PIPELINE_DEFAULT)
         return client.get(ServicePermissionAuthResource::class).validateUserResourcePermission(
             userId = userId,
             token = tokenCheckService.getSystemToken(null) ?: "",
             action = action,
             projectCode = projectCode,
-            resourceCode = AuthResourceType.PIPELINE_DEFAULT.value
+            resourceCode = getPipelineId(pipelineId, projectCode)
         ).data ?: false
+    }
+
+    private fun getPipelineId(pipelineId: String, projectCode: String): String {
+        val pipelineIdInfo = client.get(ServicePipelineResource::class)
+            .getPipelineId(projectCode, pipelineId).data
+        if (pipelineIdInfo == null) {
+            logger.warn("$pipelineId find autoId is empty")
+            return pipelineId
+        }
+        return pipelineIdInfo.id.toString()
     }
 
     companion object {
