4427ae5d5d3e691f091479a0f750168f64838ce6
stubenhuang <stubenhuang@tencent.com>
2021-06-25 15:41:24 +0800

    --story=866054723 【蓝盾SLA】上报SLA到Oteam运营指标

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/util/EmailUtil.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/util/EmailUtil.kt
@@ -28,6 +28,7 @@
 package com.tencent.devops.monitoring.util
 
 import org.apache.commons.lang3.time.FastDateFormat
+import java.util.Locale
 
 data class EmailModuleData(
     val module: String,
@@ -77,7 +78,7 @@ object EmailUtil {
         return """
         <tr>
            <td>$name</td>
-           <td>${String.format("%.5f", percent).toDouble()}${unit}</td>
+           <td>${String.format(Locale.getDefault(), "%.5f", percent).toDouble()}$unit</td>
            <td align="center">
                <a href="$url">查看</a>
            </td>
@@ -91,159 +92,168 @@ object EmailUtil {
     private const val TABLE_COLUMN3_TITLE = "#{column3Title}"
 
     private const val SHARE_EMAIL_TABLE_PREFIX = """
-    <table border="0" cellpadding="0" cellspacing="0" width="100%">
-        <tbody>
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tbody>
+    <tr>
+      <td>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="963">
+          <tbody>
             <tr>
-                <td>
-                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="963">
-                        <tbody>
+              <td class="table-lr-blue"></td>
+              <td>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="873" style="background:#fff;">
+                  <tbody>
+                    <tr>
+                      <td style="width:20px;"></td>
+                      <td>
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="833">
+                          <tbody>
                             <tr>
-                                <td class="table-lr-blue"></td>
-                                <td>
-                                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="873" style="background:#fff;">
-                                        <tbody>
-                                            <tr>
-                                                <td style="width:20px;"></td>
-                                                <td>
-                                                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="833">
-                                                        <tbody>
-                                                            <tr>
-                                                                <td>
-                                                                    <div style="margin-top:10px;">#{bodyTitle}</div>
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>
-                                                                    <div style="margin-top:10px;"></div>
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>
-                                                                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="833" class="table-email">
-                                                                        <thead>
-                                                                            <tr>
-                                                                                <th align="left" style="width:40%;background-color:#F6F8F8;">#{column1Title}</th>
-                                                                                <th align="left" style="width:40%;background-color:#F6F8F8;">#{column2Title}</th>
-                                                                                <th style="width:20%;background-color:#F6F8F8;">#{column3Title}</th>
-                                                                            </tr>
-                                                                        </thead>
-                                                                        <tbody>
+                              <td>
+                                <div style="margin-top:10px;">#{bodyTitle}</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <div style="margin-top:10px;"></div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" width="833"
+                                  class="table-email">
+                                  <thead>
+                                    <tr>
+                                      <th align="left" style="width:40%;background-color:#F6F8F8;">
+                                        #{column1Title}</th>
+                                      <th align="left" style="width:40%;background-color:#F6F8F8;">
+                                        #{column2Title}</th>
+                                      <th style="width:20%;background-color:#F6F8F8;">
+                                        #{column3Title}</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+
     """
 
     private const val SHARE_EMAIL_TABLE_SUFFIX = """
-                                                                                   </tbody>
-                                                                    </table>
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>
-                                                                    <div style="margin-bottom:10px;"></div>
-                                                                </td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                                </td>
-                                                <td style="width:20px;"></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                                <td class="table-lr-blue"></td>
+                                  </tbody>
+                                </table>
+                              </td>
                             </tr>
                             <tr>
-                                <td colspan="3" style="height:40px;background:#0b1731;"></td>
+                              <td>
+                                <div style="margin-bottom:10px;"></div>
+                              </td>
                             </tr>
-                        </tbody>
-                    </table>
-                </td>
+                          </tbody>
+                        </table>
+                      </td>
+                      <td style="width:20px;"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+              <td class="table-lr-blue"></td>
             </tr>
-        </tbody>
-    </table>        
+            <tr>
+              <td colspan="3" style="height:40px;background:#0b1731;"></td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
     """
 
-    private const val SHARE_EMAIL_HTML_PREFIX =
-        """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    private const val SHARE_EMAIL_HTML_PREFIX = """
+<!DOCTYPE html
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0064)http://open.oa.com/static_api/v3/templates/template10/index.html -->
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-    <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<style type="text/css">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <style type="text/css">
     * {
-        font-family: '微软雅黑';
-        font-size: 14px;
+      font-family: '微软雅黑';
+      font-size: 14px;
     }
 
     img {
-        outline: none;
-        border: none;
-        text-decoration: none;
+      outline: none;
+      border: none;
+      text-decoration: none;
     }
 
     a {
-        border: none;
-        text-decoration: none;
-        color: #5c90d2;
+      border: none;
+      text-decoration: none;
+      color: #5c90d2;
     }
 
     a:hover {
-        color: #3169b1;
-        text-decoration: none;
+      color: #3169b1;
+      text-decoration: none;
     }
 
     .table-email thead tr th,
     .table-email tbody tr td {
-        padding: 10px;
-        border: 1px solid #ddd;
-        border-right: none;
-        border-bottom: none;
+      padding: 10px;
+      border: 1px solid #ddd;
+      border-right: none;
+      border-bottom: none;
     }
 
     .table-email thead tr th:last-child,
     .table-email tbody tr td:last-child {
-        border: 1px solid #ddd;
-        border-bottom: none;
-        text-align: center;
+      border: 1px solid #ddd;
+      border-bottom: none;
+      text-align: center;
     }
 
     .table-email tbody tr:last-child td {
-        border: 1px solid #ddd;
-        border-right: none;
+      border: 1px solid #ddd;
+      border-right: none;
     }
 
     .table-email tbody tr:last-child td:last-child {
-        border: 1px solid #ddd;
+      border: 1px solid #ddd;
     }
 
     .email-info {
-        display: inline-block;
-        width: 80px;
-        height: 28px;
-        line-height: 28px;
-        background-color: #4A9BFF;
-        border: #2180F5 1px solid;
+      display: inline-block;
+      width: 80px;
+      height: 28px;
+      line-height: 28px;
+      background-color: #4A9BFF;
+      border: #2180F5 1px solid;
     }
 
     .email-info>a {
-        font-size: 12px;
-        color: #fff;
+      font-size: 12px;
+      color: #fff;
     }
 
     .email-info>a:hover {
-        color: #fff;
+      color: #fff;
     }
 
     .course-titles {
-        font-size: 18px;
+      font-size: 18px;
     }
 
     .table-lr-blue {
-        width: 45px;
-        background: #0b1731;
+      width: 45px;
+      background: #0b1731;
     }
-</style></head>
+  </style>
+</head>
 <body>
         """
 
