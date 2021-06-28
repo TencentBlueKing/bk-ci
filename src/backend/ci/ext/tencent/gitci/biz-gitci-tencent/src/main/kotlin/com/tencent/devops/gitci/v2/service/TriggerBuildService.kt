06d4b21a6348856cc39e25c3c792015ceaea9876
johuang <huangjiao511@163.com>
2021-06-28 11:49:25 +0800

    --story=865769937 第三方构建资源池使用时，支持可以不指定 agent-selector

--- src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/v2/service/TriggerBuildService.kt
+++ src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/v2/service/TriggerBuildService.kt
@@ -524,7 +524,7 @@ class TriggerBuildService @Autowired constructor(
         }
 
         if (job.runsOn.agentSelector.isNullOrEmpty()) {
-            return VMBaseOS.LINUX
+            return VMBaseOS.ALL
         }
         return when (job.runsOn.agentSelector!![0]) {
             "linux" -> VMBaseOS.LINUX
