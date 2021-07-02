a978b5427b9e4e4bbae51158b005a2c6487a9b6f
royalhuang <royalhuang@tencent.com>
2021-07-02 16:04:48 +0800

    --bug=89336647 version和name关键字后的空格注释会导致YML校验失败

--- src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/utils/ScriptYmlUtils.kt
+++ src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/utils/ScriptYmlUtils.kt
@@ -256,8 +256,12 @@ object ScriptYmlUtils {
             return
         }
         yamlMap.forEach { (t, _) ->
-            if (t != formatTrigger && t != "extends" && t != "version" && t != "resources" && t != "name") {
-                throw CustomException(Response.Status.BAD_REQUEST, "使用 extends 时顶级关键字只能有触发器 on 与 resources")
+            if (t != formatTrigger && t != "extends" && t != "version" &&
+                t != "resources" && t != "name" && t != "on") {
+                throw CustomException(
+                    status = Response.Status.BAD_REQUEST,
+                    message = "使用 extends 时顶级关键字只能有触发器 on 与 resources"
+                )
             }
         }
     }
