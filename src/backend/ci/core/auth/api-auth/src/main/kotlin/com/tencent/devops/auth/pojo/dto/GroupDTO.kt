4ee910189752bb3bdbab29bd1f1cee7d3ba95444
fitzcao <cao150260819@126.com>
2021-07-02 20:21:56 +0800

    --story=866382161 权限优化：下载构件和日志需为项目成员但无需OAUTH 字段类型调整

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/dto/GroupDTO.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/dto/GroupDTO.kt
@@ -35,7 +35,7 @@ data class GroupDTO(
     @ApiModelProperty("用户组编号, 内置用户组编号固定, 自定义组动态生成")
     val groupCode: String,
     @ApiModelProperty("用户组分类")
-    val groupType: String,
+    val groupType: Boolean,
     @ApiModelProperty("用户组名称")
     val groupName: String,
     @ApiModelProperty("用户组别名")
