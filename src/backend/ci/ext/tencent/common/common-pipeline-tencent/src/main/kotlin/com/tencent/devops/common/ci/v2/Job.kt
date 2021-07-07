a50d88cf7bde452aa8d7747526911fa8dbab02d6
sawyersong2 <wudiduomjimo@gmail.com>
2021-07-07 19:47:54 +0800

    --story=866212221 runs-on: docker 时，默认镜像升级为 mirrors.tencent.com/ci/tlinux3_ci:1.2.1

--- src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/Job.kt
+++ src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/Job.kt
@@ -86,7 +86,7 @@ data class RunsOn(
     @JsonProperty("pool-name")
     val poolName: String = JobRunsOnType.DOCKER.type,
     val container: Container? = Container(
-        image = "http://mirrors.tencent.com/ci/tlinux3_ci:1.2.3",
+        image = "http://mirrors.tencent.com/ci/tlinux3_ci:1.2.2",
         credentials = null
     ),
     @JsonProperty("agent-selector")
