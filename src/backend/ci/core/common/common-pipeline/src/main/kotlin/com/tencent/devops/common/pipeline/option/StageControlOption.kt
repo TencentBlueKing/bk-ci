71cdf55fe7bed364f6829395ffd8589bbf67ad53
royalhuang <royalhuang@tencent.com>
2021-07-07 17:10:21 +0800

    feat: Stage准入支持审核流 #4531 修改旧字段默认值

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/option/StageControlOption.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/option/StageControlOption.kt
@@ -42,7 +42,7 @@ data class StageControlOption(
     val runCondition: StageRunCondition = StageRunCondition.AFTER_LAST_FINISHED, // 运行条件
     val manualTrigger: Boolean? = false,
     var triggerUsers: List<String>? = null, // 可触发用户，支持引用变量
-    var triggered: Boolean? = false, // 已通过审核
+    var triggered: Boolean? = null, // 已通过审核
     var reviewGroups: MutableList<StageReviewGroup>? = null, // 审核流配置
     val timeout: Int? = null, // 等待审核的超时时间
     val customVariables: List<NameAndValue>? = emptyList(), // 自定义变量
