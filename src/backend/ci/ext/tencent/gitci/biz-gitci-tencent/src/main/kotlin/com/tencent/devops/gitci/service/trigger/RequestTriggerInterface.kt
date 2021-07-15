e444bd61931b12462809e752e6494f3126b259c5
ruotiantang <ruotiantang@tencent.com>
2021-07-15 11:44:50 +0800

    --bug=89861063 fork库提mr到源库时，无法触发ci问题fix

--- src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/trigger/RequestTriggerInterface.kt
+++ src/backend/ci/ext/tencent/gitci/biz-gitci-tencent/src/main/kotlin/com/tencent/devops/gitci/service/trigger/RequestTriggerInterface.kt
@@ -45,7 +45,7 @@ interface RequestTriggerInterface<T> {
         filePath: String
     ): Boolean
 
-    fun isMatch(event: GitEvent, ymlObject: T): Pair</*isMatch*/Boolean, /*onlySchedule*/Boolean>
+    fun isMatch(event: GitEvent, gitRequestEvent: GitRequestEvent, ymlObject: T): Pair</*isMatch*/Boolean, /*onlySchedule*/Boolean>
 
     fun prepareCIBuildYaml(
         gitToken: GitToken,
