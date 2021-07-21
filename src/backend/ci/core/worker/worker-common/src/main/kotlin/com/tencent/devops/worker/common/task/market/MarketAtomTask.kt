a118bae00b9a0727cd1a427398559476295560e0
hingbong <hingbonglo@gmail.com>
2021-07-21 22:42:03 +0800

    feat: 【流水线插件】系统日志结构优化 #4191

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
@@ -144,8 +144,6 @@ open class MarketAtomTask : ITask() {
             variablesMap
         }
 
-        val command = StringBuilder()
-
         // 解析输出字段模板
         val props = JsonUtil.toMutableMapSkipEmpty(atomData.props!!)
 
@@ -268,6 +266,7 @@ open class MarketAtomTask : ITask() {
         var error: Throwable? = null
         try {
             // 下载atom执行文件
+            LoggerService.addFoldStartLine("[Install plugin]")
             atomExecuteFile = downloadAtomExecuteFile(atomData.pkgPath!!, atomTmpSpace)
 
             checkSha1(atomExecuteFile, atomData.shaContent!!)
@@ -285,16 +284,29 @@ open class MarketAtomTask : ITask() {
             atomDevLanguageEnvVars?.forEach {
                 systemEnvVariables[it.envKey] = it.envValue
             }
-            val preCmds = mutableListOf<String>()
-            if (!atomData.preCmd.isNullOrBlank()) {
-                if (atomData.preCmd!!.contains(Regex("^\\s*\\[[\\w\\s\\S\\W]*\\]\\s*$"))) {
-                    preCmds.addAll(JsonUtil.to(atomData.preCmd!!))
-                } else {
-                    preCmds.add(atomData.preCmd!!)
-                }
-            }
-            val atomTargetHandleService = AtomTargetFactory.createAtomTargetHandleService(atomLanguage)
+            val preCmd = atomData.preCmd
             val buildEnvs = buildVariables.buildEnvs
+            if (!preCmd.isNullOrBlank()) {
+                val preCmds = mutableListOf<String>()
+                if (preCmd.contains(Regex("^\\s*\\[[\\w\\s\\S\\W]*\\]\\s*$"))) {
+                    preCmds.addAll(JsonUtil.to(preCmd))
+                } else {
+                    preCmds.add(preCmd)
+                }
+                runPreCmds(
+                    preCmds = preCmds,
+                    buildVariables = buildVariables,
+                    atomTmpSpace = atomTmpSpace,
+                    workspace = workspace,
+                    environment = environment,
+                    buildEnvs = buildEnvs,
+                    systemEnvVariables = systemVariables,
+                    buildTask = buildTask
+                )
+            }
+            LoggerService.addFoldEndLine("-----")
+            LoggerService.addNormalLine("")
+            val atomTargetHandleService = AtomTargetFactory.createAtomTargetHandleService(atomLanguage)
             val additionalOptions = taskParams["additionalOptions"]
             // 获取插件post操作入口参数
             var postEntryParam: String? = null
@@ -312,17 +324,11 @@ open class MarketAtomTask : ITask() {
                 postEntryParam = postEntryParam
             )
             val errorMessage = "Fail to run the plugin"
-            when {
-                AgentEnv.getOS() == OSType.WINDOWS -> {
-                    if (preCmds.isNotEmpty()) {
-                        preCmds.forEach { cmd ->
-                            command.append("\r\n$cmd\r\n")
-                        }
-                    }
-                    command.append("\r\n$atomTarget\r\n")
+            when (AgentEnv.getOS()) {
+                OSType.WINDOWS -> {
                     BatScriptUtil.execute(
                         buildId = buildVariables.buildId,
-                        script = command.toString(),
+                        script = "\r\n$atomTarget\r\n",
                         runtimeVariables = environment,
                         dir = atomTmpSpace,
                         workspace = workspace,
@@ -330,16 +336,10 @@ open class MarketAtomTask : ITask() {
                         elementId = buildTask.elementId
                     )
                 }
-                AgentEnv.getOS() == OSType.LINUX || AgentEnv.getOS() == OSType.MAC_OS -> {
-                    if (preCmds.isNotEmpty()) {
-                        preCmds.forEach { cmd ->
-                            command.append("\n$cmd\n")
-                        }
-                    }
-                    command.append("\n$atomTarget\n")
+                OSType.LINUX, OSType.MAC_OS -> {
                     ShellUtil.execute(
                         buildId = buildVariables.buildId,
-                        script = command.toString(),
+                        script = "\n$atomTarget\n",
                         dir = atomTmpSpace,
                         workspace = workspace,
                         buildEnvs = buildEnvs,
@@ -348,6 +348,8 @@ open class MarketAtomTask : ITask() {
                         errorMessage = errorMessage,
                         elementId = buildTask.elementId
                     )
+                }
+                else -> {
                 }
             }
         } catch (e: Throwable) {
@@ -371,12 +373,59 @@ open class MarketAtomTask : ITask() {
         }
     }
 
+    private fun runPreCmds(
+        preCmds: List<String>,
+        buildVariables: BuildVariables,
+        atomTmpSpace: File,
+        workspace: File,
+        environment: Map<String, String>,
+        buildEnvs: List<com.tencent.devops.store.pojo.app.BuildEnv>,
+        systemEnvVariables: Map<String, String>,
+        buildTask: BuildTask
+    ) {
+        val preCmdErrorMessage = "Fail to run the plugin demand command"
+        when (AgentEnv.getOS()) {
+            OSType.WINDOWS -> {
+                if (preCmds.isNotEmpty()) {
+                    val preCommand = preCmds.joinToString { "\r\n${it}\r\n" }
+                    BatScriptUtil.execute(
+                        buildId = buildVariables.buildId,
+                        script = preCommand,
+                        runtimeVariables = environment,
+                        dir = atomTmpSpace,
+                        workspace = workspace,
+                        errorMessage = preCmdErrorMessage,
+                        elementId = buildTask.elementId
+                    )
+                }
+            }
+            OSType.LINUX, OSType.MAC_OS -> {
+                if (preCmds.isNotEmpty()) {
+                    val preCommand = preCmds.joinToString { "\n${it}\n" }
+                    ShellUtil.execute(
+                        buildId = buildVariables.buildId,
+                        script = preCommand,
+                        dir = atomTmpSpace,
+                        workspace = workspace,
+                        buildEnvs = buildEnvs,
+                        runtimeVariables = environment,
+                        systemEnvVariables = systemEnvVariables,
+                        errorMessage = preCmdErrorMessage,
+                        elementId = buildTask.elementId
+                    )
+                }
+            }
+            else -> {
+            }
+        }
+    }
+
     private fun printInput(
         atomData: AtomEnv,
         atomParams: MutableMap<String, String>,
         inputTemplate: Map<String, Map<String, Any>>
     ) {
-        LoggerService.addFoldStartLine("[Install plugin]")
+        LoggerService.addFoldStartLine("[Plugin info]")
         LoggerService.addNormalLine("=====================================================================")
         LoggerService.addNormalLine("Task           : ${atomData.atomName}")
         if (!atomData.summary.isNullOrBlank()) {
