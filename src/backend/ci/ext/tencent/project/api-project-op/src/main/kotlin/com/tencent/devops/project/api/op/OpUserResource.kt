db5072e9dd4808db591d0b8ef6b6d581152394de
royalhuang <royalhuang@tencent.com>
2021-07-29 14:50:37 +0800

    --bug=90236325 管理员无法查看私有项目的流水线执行数据

--- src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OpUserResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-op/src/main/kotlin/com/tencent/devops/project/api/op/OpUserResource.kt
@@ -38,6 +38,7 @@ import javax.ws.rs.PUT
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.PathParam
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OP_PROJECT_USER"], description = "OP_用户")
