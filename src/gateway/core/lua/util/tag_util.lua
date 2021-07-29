13e4f75cc3d06cfc4ae4fdd3613fa0d5d35ab4d3
stubenhuang <stubenhuang@tencent.com>
2021-07-29 11:12:44 +0800

    feat: 网关日志新增路由集群字段 #4800

--- src/gateway/core/lua/util/tag_util.lua
+++ src/gateway/core/lua/util/tag_util.lua
@@ -89,14 +89,18 @@ function _M:get_tag(ns_config)
             --- 将redis连接放回pool中
             red:set_keepalive(config.redis.max_idle_time, config.redis.pool_size)
         end
-
-        -- 设置tag到http请求头
-        ngx.header["X-GATEWAY-TAG"] = tag
+        _M:set_header(tag)
     else
         tag = header_tag
     end
 
     return tag
+end
+
+-- 设置tag到http请求头
+function _M:set_header(tag)
+    ngx.header["X-GATEWAY-TAG"] = tag
+    ngx.var.route_tag = tag
 end
 
 -- 获取tag对应的路径
