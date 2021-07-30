d28b8e310dee167b593ba08364e251ebf0585eb4
fitzcao <cao150260819@126.com>
2021-07-30 18:15:02 +0800

    --story=863222655 对接蓝鲸权限中心V3 ticket 启动

--- src/backend/ci/ext/tencent/ticket/boot-ticket-tencent/src/main/kotlin/com/tencent/devops/ticket/Application.kt
+++ src/backend/ci/ext/tencent/ticket/boot-ticket-tencent/src/main/kotlin/com/tencent/devops/ticket/Application.kt
@@ -29,8 +29,10 @@ package com.tencent.devops.ticket
 
 import com.tencent.devops.common.service.MicroService
 import com.tencent.devops.common.service.MicroServiceApplication
+import org.springframework.context.annotation.ComponentScan
 
 @MicroService
+@ComponentScan("com.tencent.devops.common", "com.tencent.devops.ticket")
 class Application
 
 fun main(args: Array<String>) {
