2ace906010e190cb38aa1a39edd6f19ecba2efb0
fitzcao <cao150260819@126.com>
2021-08-04 15:39:12 +0800

    feature: 将网关生成的rid在调用链中传递 #2405

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/trace/TraceTag.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/trace/TraceTag.kt
@@ -30,7 +30,8 @@ package com.tencent.devops.common.service.trace
 import com.tencent.devops.common.api.util.UUIDUtil
 
 object TraceTag {
-    const val BIZID = "X-DEVOPS-RID"
+    const val BIZID = "bizId"
+    const val X_DEVOPS_RID = "X-DEVOPS-RID"
     const val TRACE_HEADER_DEVOPS_BIZID = "X-DEVOPS-TRACE-BIZ-ID"
     const val TRACE_HEADER_DEVOPS_TRACE = "X-DEVOPS-TRACE-ID"
     const val BIZIDTAG = "biz"
