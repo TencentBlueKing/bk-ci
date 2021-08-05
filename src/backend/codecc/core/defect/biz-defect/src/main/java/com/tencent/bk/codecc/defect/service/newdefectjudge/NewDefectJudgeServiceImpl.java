90810f8283527f7cc3045972502960ecf14e2e41
ddlin0719 <ddlin@tencent.com>
2021-08-05 18:08:40 +0800

    CodeCC 1.5 产品体验问题调整 #4843 remove new/history defect

--- src/backend/codecc/core/defect/biz-defect/src/main/java/com/tencent/bk/codecc/defect/service/newdefectjudge/NewDefectJudgeServiceImpl.java
+++ src/backend/codecc/core/defect/biz-defect/src/main/java/com/tencent/bk/codecc/defect/service/newdefectjudge/NewDefectJudgeServiceImpl.java
@@ -48,90 +48,72 @@ public class NewDefectJudgeServiceImpl implements NewDefectJudgeService
     @Override
     public long getNewDefectJudgeTime(long taskId, String toolName, TaskDetailVO taskDetailVO)
     {
-        long result = 0;
-        NewDefectJudgeVO newDefectJudge;
-        if(null == taskDetailVO)
-        {
-            // 获取告警转为历史的时间
-            Result<TaskDetailVO> taskInfoResult = client.get(ServiceTaskRestResource.class).getTaskInfoById(taskId);
-            if (taskInfoResult.isNotOk() || null == taskInfoResult.getData())
-            {
-                log.error("get task info fail! task id is: {}, msg: {}", taskId, taskInfoResult.getMessage());
-                throw new CodeCCException(CommonMessageCode.INTERNAL_SYSTEM_FAIL);
-            }
-            newDefectJudge = taskInfoResult.getData().getNewDefectJudge();
-        }
-        else
-        {
-            newDefectJudge = taskDetailVO.getNewDefectJudge();
-        }
-        if (newDefectJudge != null)
-        {
-            if (newDefectJudge.getFromDateTime() != null && newDefectJudge.getFromDateTime() != 0)
-            {
-                result = newDefectJudge.getFromDateTime();
-            }
-            else if (StringUtils.isNotEmpty(newDefectJudge.getFromDate()))
-            {
-                result = DateTimeUtils.convertStringDateToLongTime(newDefectJudge.getFromDate(), DateTimeUtils.yyyyMMddFormat);
-            }
-        }
+//        long result = 0;
+//        NewDefectJudgeVO newDefectJudge;
+//        if(null == taskDetailVO)
+//        {
+//            // 获取告警转为历史的时间
+//            Result<TaskDetailVO> taskInfoResult = client.get(ServiceTaskRestResource.class).getTaskInfoById(taskId);
+//            if (taskInfoResult.isNotOk() || null == taskInfoResult.getData())
+//            {
+//                log.error("get task info fail! task id is: {}, msg: {}", taskId, taskInfoResult.getMessage());
+//                throw new CodeCCException(CommonMessageCode.INTERNAL_SYSTEM_FAIL);
+//            }
+//            newDefectJudge = taskInfoResult.getData().getNewDefectJudge();
+//        }
+//        else
+//        {
+//            newDefectJudge = taskDetailVO.getNewDefectJudge();
+//        }
+//        if (newDefectJudge != null)
+//        {
+//            if (newDefectJudge.getFromDateTime() != null && newDefectJudge.getFromDateTime() != 0)
+//            {
+//                result = newDefectJudge.getFromDateTime();
+//            }
+//            else if (StringUtils.isNotEmpty(newDefectJudge.getFromDate()))
+//            {
+//                result = DateTimeUtils.convertStringDateToLongTime(newDefectJudge.getFromDate(), DateTimeUtils.yyyyMMddFormat);
+//            }
+//        }
+//
+//        // 获取首次成功分析时间
+//        if (result == 0)
+//        {
+//            FirstAnalysisSuccessEntity firstSuccessTimeEntity = firstSuccessTimeRepository.findFirstByTaskIdAndToolName(taskId, toolName);
+//            if (firstSuccessTimeEntity != null)
+//            {
+//                result = firstSuccessTimeEntity.getFirstAnalysisSuccessTime();
+//            }
+//        }
+//        return result;
 
-        // 获取首次成功分析时间
-        if (result == 0)
-        {
-            FirstAnalysisSuccessEntity firstSuccessTimeEntity = firstSuccessTimeRepository.findFirstByTaskIdAndToolName(taskId, toolName);
-            if (firstSuccessTimeEntity != null)
-            {
-                result = firstSuccessTimeEntity.getFirstAnalysisSuccessTime();
-            }
-        }
-        return result;
+        return 0;
     }
 
     @Override
     public long getNewDefectJudgeTime(long taskId, TaskDetailVO taskDetailVO) {
-        long result = 0;
-        NewDefectJudgeVO newDefectJudge = taskDetailVO.getNewDefectJudge();
-
-        if (newDefectJudge != null)
-        {
-            if (newDefectJudge.getFromDateTime() != null && newDefectJudge.getFromDateTime() != 0)
-            {
-                result = newDefectJudge.getFromDateTime();
-            }
-            else if (StringUtils.isNotEmpty(newDefectJudge.getFromDate()))
-            {
-                result = DateTimeUtils.convertStringDateToLongTime(newDefectJudge.getFromDate(), DateTimeUtils.yyyyMMddFormat);
-            }
-        }
-
-        // 获取首次成功分析时间
-        if (result == 0)
-        {
-            result = taskDetailVO.getCreatedDate();
-        }
-        return result;
+        return 0;
     }
 
     @Override
     public Map<Long, Long> batchGetNewDefectJudgeTime(Collection<Long> taskIdList, String toolName)
     {
         Map<Long, Long> taskTimeMap = Maps.newHashMap();
-        if (CollectionUtils.isEmpty(taskIdList) || StringUtils.isEmpty(toolName))
-        {
-            return taskTimeMap;
-        }
-
-        List<FirstAnalysisSuccessEntity> successEntityList =
-                firstSuccessTimeRepository.findByTaskIdInAndToolNameIs(taskIdList, toolName);
-        if (CollectionUtils.isNotEmpty(successEntityList))
-        {
-            taskTimeMap = successEntityList.stream().collect(Collectors
-                    .toMap(FirstAnalysisSuccessEntity::getTaskId,
-                            FirstAnalysisSuccessEntity::getFirstAnalysisSuccessTime));
-
-        }
+//        if (CollectionUtils.isEmpty(taskIdList) || StringUtils.isEmpty(toolName))
+//        {
+//            return taskTimeMap;
+//        }
+//
+//        List<FirstAnalysisSuccessEntity> successEntityList =
+//                firstSuccessTimeRepository.findByTaskIdInAndToolNameIs(taskIdList, toolName);
+//        if (CollectionUtils.isNotEmpty(successEntityList))
+//        {
+//            taskTimeMap = successEntityList.stream().collect(Collectors
+//                    .toMap(FirstAnalysisSuccessEntity::getTaskId,
+//                            FirstAnalysisSuccessEntity::getFirstAnalysisSuccessTime));
+//
+//        }
         return taskTimeMap;
     }
 }
