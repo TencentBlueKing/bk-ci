848b5eb08629529c4a79b994dc229a853ee5bf4d
royalhuang <royalhuang@tencent.com>
2021-08-05 21:59:34 +0800

    --story=866469453 【质量红线】stage 准入/准出支持gates

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/bean/TencentPipelineUrlBeanImpl.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/bean/TencentPipelineUrlBeanImpl.kt
@@ -48,15 +48,15 @@ class TencentPipelineUrlBeanImpl constructor(
     override fun genBuildDetailUrl(projectCode: String, pipelineId: String, buildId: String): String {
         val devopsHostGateway = HomeHostUtil.getHost(commonConfig.devopsHostGateway!!)
         logger.info("[$buildId]|genBuildDetailUrl| host=$devopsHostGateway")
-        val url = "$devopsHostGateway/console/pipeline/$projectCode/$pipelineId/detail/$buildId"
-        return client.get(ServiceShortUrlResource::class).createShortUrl(CreateShortUrlRequest(url, TTL)).data!!
+        return "$devopsHostGateway/console/pipeline/$projectCode/$pipelineId/detail/$buildId"
+//        return client.get(ServiceShortUrlResource::class).createShortUrl(CreateShortUrlRequest(url, TTL)).data!!
     }
 
     override fun genAppBuildDetailUrl(projectCode: String, pipelineId: String, buildId: String): String {
         val devopsOuterHostGateWay = HomeHostUtil.getHost(commonConfig.devopsOuterHostGateWay!!)
         logger.info("[$buildId]|genBuildDetailUrl| outHost=$devopsOuterHostGateWay")
-        val url = "$devopsOuterHostGateWay/app/download/devops_app_forward.html" +
+        return "$devopsOuterHostGateWay/app/download/devops_app_forward.html" +
             "?flag=buildReport&projectId=$projectCode&pipelineId=$pipelineId&buildId=$buildId"
-        return client.get(ServiceShortUrlResource::class).createShortUrl(CreateShortUrlRequest(url, TTL)).data!!
+//        return client.get(ServiceShortUrlResource::class).createShortUrl(CreateShortUrlRequest(url, TTL)).data!!
     }
 }
