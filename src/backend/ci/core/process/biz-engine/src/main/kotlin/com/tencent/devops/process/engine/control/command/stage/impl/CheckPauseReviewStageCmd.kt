8444b9ef05fe61b60bde1b83cfe40a1ce3dadfeb
royalhuang <royalhuang@tencent.com>
2021-08-13 11:19:23 +0800

    feat: Stage准入支持审核流 #4531 在暂停时做变量替换，每步审核都做通知

--- src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/stage/impl/CheckPauseReviewStageCmd.kt
+++ src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/stage/impl/CheckPauseReviewStageCmd.kt
@@ -27,24 +27,19 @@
 
 package com.tencent.devops.process.engine.control.command.stage.impl
 
-import com.tencent.devops.common.api.util.DateTimeUtil
-import com.tencent.devops.common.api.util.EnvUtils
-import com.tencent.devops.common.event.dispatcher.pipeline.PipelineEventDispatcher
 import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.process.engine.common.BS_MANUAL_START_STAGE
 import com.tencent.devops.process.engine.control.command.CmdFlowState
 import com.tencent.devops.process.engine.control.command.stage.StageCmd
 import com.tencent.devops.process.engine.control.command.stage.StageContext
 import com.tencent.devops.process.engine.pojo.PipelineBuildStage
-import com.tencent.devops.process.engine.pojo.event.PipelineBuildNotifyEvent
 import com.tencent.devops.process.engine.pojo.event.PipelineBuildStageEvent
-import com.tencent.devops.process.pojo.PipelineNotifyTemplateEnum
+import com.tencent.devops.process.engine.service.PipelineStageService
 import com.tencent.devops.process.service.BuildVariableService
 import com.tencent.devops.process.utils.PIPELINE_BUILD_NUM
 import com.tencent.devops.process.utils.PIPELINE_NAME
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.util.Date
 
 /**
  * Stage暂停及审核事件的命令处理
@@ -52,7 +47,7 @@ import java.util.Date
 @Service
 class CheckPauseReviewStageCmd(
     private val buildVariableService: BuildVariableService,
-    private val pipelineEventDispatcher: PipelineEventDispatcher
+    private val pipelineStageService: PipelineStageService
 ) : StageCmd {
 
     override fun canExecute(commandContext: StageContext): Boolean {
@@ -76,7 +71,13 @@ class CheckPauseReviewStageCmd(
             if (needPause(event, stage)) {
                 // #3742 进入暂停状态则刷新完状态后直接返回，等待手动触发
                 LOG.info("ENGINE|${event.buildId}|${event.source}|STAGE_PAUSE|${event.stageId}")
-                pauseStageNotify(commandContext)
+                stage.checkIn?.parseReviewVariables(commandContext.variables)
+                pipelineStageService.pauseStageNotify(
+                    userId = event.userId,
+                    stage = stage,
+                    pipelineName = commandContext.variables[PIPELINE_NAME] ?: stage.pipelineId,
+                    buildNum = commandContext.variables[PIPELINE_BUILD_NUM] ?: "1"
+                )
                 commandContext.buildStatus = BuildStatus.STAGE_SUCCESS
                 commandContext.latestSummary = "s(${stage.stageId}) waiting for REVIEW"
                 commandContext.cmdFlowState = CmdFlowState.FINALLY
@@ -117,50 +118,6 @@ class CheckPauseReviewStageCmd(
         // TODO 下次发布去掉对triggered的判断
         return stage.checkIn?.groupToReview() != null ||
             stage.controlOption?.stageControlOption?.triggered != true
-    }
-
-    /**
-     * 发送通知
-     */
-    private fun pauseStageNotify(commandContext: StageContext) {
-        val stage = commandContext.stage
-        val checkIn = stage.checkIn ?: return
-        val group = stage.checkIn?.groupToReview() ?: return
-        val notifyUsers = mutableListOf<String>()
-
-        if (group.status == null) {
-            val reviewers = group.reviewers.joinToString(",")
-            val realReviewers = EnvUtils.parseEnv(reviewers, commandContext.variables)
-                .split(",").toList()
-            group.reviewers = realReviewers
-            notifyUsers.addAll(realReviewers)
-        }
-
-        val pipelineName = commandContext.variables[PIPELINE_NAME] ?: stage.pipelineId
-        val buildNum = commandContext.variables[PIPELINE_BUILD_NUM] ?: "1"
-        var reviewDesc = checkIn.reviewDesc
-        reviewDesc = EnvUtils.parseEnv(reviewDesc, commandContext.variables)
-        checkIn.reviewDesc = reviewDesc // 替换变量 #3395
-
-        pipelineEventDispatcher.dispatch(
-            PipelineBuildNotifyEvent(
-                notifyTemplateEnum = PipelineNotifyTemplateEnum.PIPELINE_MANUAL_REVIEW_STAGE_NOTIFY_TEMPLATE.name,
-                source = commandContext.latestSummary, projectId = stage.projectId, pipelineId = stage.pipelineId,
-                userId = commandContext.event.userId, buildId = stage.buildId,
-                receivers = notifyUsers,
-                titleParams = mutableMapOf(
-                    "projectName" to "need to add in notifyListener",
-                    "pipelineName" to pipelineName,
-                    "buildNum" to buildNum
-                ),
-                bodyParams = mutableMapOf(
-                    "projectName" to "need to add in notifyListener",
-                    "pipelineName" to pipelineName,
-                    "dataTime" to DateTimeUtil.formatDate(Date(), "yyyy-MM-dd HH:mm:ss"),
-                    "reviewDesc" to reviewDesc
-                )
-            )
-        )
     }
 
     companion object {
