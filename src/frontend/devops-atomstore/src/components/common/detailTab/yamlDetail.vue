a9feb05601ad38d8d83e725417b47d16b7a9505c
xuzhan <xuzhan@tencent.com>
2021-08-13 20:32:25 +0800

    feature: 研发商店插件支持展示插件输出的质量红线指标 issue #4918

--- src/frontend/devops-atomstore/src/components/common/detailTab/yamlDetail.vue
+++ src/frontend/devops-atomstore/src/components/common/detailTab/yamlDetail.vue
@@ -11,9 +11,9 @@
 
         <h3 class="yaml-title">{{ $t('store.质量红线指标：') }}</h3>
         <bk-table :data="qualityData" v-bkloading="{ isLoading }">
-            <bk-table-column :label="$t('store.指标名')" prop="cnName" show-overflow-tooltip></bk-table-column>
+            <bk-table-column :label="$t('store.指标名')" prop="enName" show-overflow-tooltip></bk-table-column>
             <bk-table-column :label="$t('store.参数说明')" prop="desc" show-overflow-tooltip></bk-table-column>
-            <bk-table-column :label="$t('store.值类型')" prop="type" show-overflow-tooltip></bk-table-column>
+            <bk-table-column :label="$t('store.值类型')" prop="thresholdType" show-overflow-tooltip></bk-table-column>
             <bk-table-column :label="$t('store.支持的操作')" prop="operationList" show-overflow-tooltip :formatter="operationFormatter"></bk-table-column>
         </bk-table>
     </section>
@@ -72,7 +72,17 @@
             },
 
             operationFormatter (row, column, cellValue, index) {
-                return (cellValue || []).join(', ')
+                const opeMap = {
+                    GT: '>',
+                    GE: '>=',
+                    LT: '<',
+                    LE: '<=',
+                    EQ: '='
+                }
+                return (cellValue || []).reduce((acc, cur) => {
+                    acc += (opeMap[cur] + ' ')
+                    return acc
+                }, '')
             }
         }
     }
