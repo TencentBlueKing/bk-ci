bd9227b05d364d2828863a91a8756a2f543f4462
yongyiduan <yongyiduan@tencent.com>
2021-08-16 14:41:19 +0800

    perf: Windows构建机的batch脚本执行编码问题 #4601

--- src/agent/src/pkg/config/config.go
+++ src/agent/src/pkg/config/config.go
@@ -135,7 +135,7 @@ func DetectAgentVersion() string {
 
 func DetectWorkerVersion() string {
 	output, err := command.RunCommand(GetJava(),
-		[]string{"-cp", BuildAgentJarPath(), "com.tencent.devops.agent.AgentVersionKt"}, systemutil.GetWorkDir(), nil)
+		[]string{"-Dfile.encoding=GBK", "-cp", BuildAgentJarPath(), "com.tencent.devops.agent.AgentVersionKt"}, systemutil.GetWorkDir(), nil)
 
 	if err != nil {
 		logs.Warn("detect worker version failed: ", err.Error())
