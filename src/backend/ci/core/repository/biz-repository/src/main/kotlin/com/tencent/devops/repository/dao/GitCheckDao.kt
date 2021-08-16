ca0875d310583a92952f56223cab3234d76ce848
ruotiantang <ruotiantang@tencent.com>
2021-08-16 21:23:07 +0800

    feat: Repository服务新增GitCheck #4950

---
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/dao/GitCheckDao.kt
@@ -0,0 +1,77 @@
+package com.tencent.devops.repository.dao
+
+import com.tencent.devops.common.api.enums.RepositoryConfig
+import com.tencent.devops.common.api.enums.RepositoryType
+import com.tencent.devops.model.repository.tables.TRepositoryGitCheck
+import com.tencent.devops.model.repository.tables.records.TRepositoryGitCheckRecord
+import com.tencent.devops.repository.pojo.RepositoryGitCheck
+import org.jooq.DSLContext
+import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
+
+@Repository
+class GitCheckDao {
+
+    fun getOrNull(
+        dslContext: DSLContext,
+        pipelineId: String,
+        repositoryConfig: RepositoryConfig,
+        commitId: String,
+        context: String
+    ): TRepositoryGitCheckRecord? {
+        with(TRepositoryGitCheck.T_REPOSITORY_GIT_CHECK) {
+            val step = dslContext.selectFrom(this)
+                .where(PIPELINE_ID.eq(pipelineId))
+                .and(COMMIT_ID.eq(commitId))
+                .and(CONTEXT.eq(context))
+            when (repositoryConfig.repositoryType) {
+                RepositoryType.ID -> step.and(REPO_ID.eq(repositoryConfig.getRepositoryId()))
+                RepositoryType.NAME -> step.and(REPO_NAME.eq(repositoryConfig.getRepositoryId()))
+            }
+            return step.fetchAny()
+        }
+    }
+
+    fun create(
+        dslContext: DSLContext,
+        repositoryGitCheck: RepositoryGitCheck
+    ) {
+        val now = LocalDateTime.now()
+        with(repositoryGitCheck) {
+            with(TRepositoryGitCheck.T_REPOSITORY_GIT_CHECK) {
+                dslContext.insertInto(
+                    this,
+                    PIPELINE_ID,
+                    BUILD_NUMBER,
+                    REPO_ID,
+                    REPO_NAME,
+                    COMMIT_ID,
+                    CREATE_TIME,
+                    UPDATE_TIME,
+                    CONTEXT,
+                    SOURCE
+                ).values(
+                    pipelineId,
+                    buildNumber,
+                    repositoryId,
+                    repositoryName,
+                    commitId,
+                    now,
+                    now,
+                    context,
+                    source.name
+                ).execute()
+            }
+        }
+    }
+
+    fun update(dslContext: DSLContext, id: Long, buildNumber: Int) {
+        with(TRepositoryGitCheck.T_REPOSITORY_GIT_CHECK) {
+            dslContext.update(this)
+                .set(UPDATE_TIME, LocalDateTime.now())
+                .set(BUILD_NUMBER, buildNumber)
+                .where(ID.eq(id))
+                .execute()
+        }
+    }
+}
