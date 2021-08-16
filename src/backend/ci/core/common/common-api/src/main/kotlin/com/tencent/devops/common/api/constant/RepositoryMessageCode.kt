2e6b3475827f2fdbb5e594fdbe21fdd5994a189b
carlyin <449036249@qq.com>
2021-08-16 15:55:25 +0800

    feat:【流水线插件】支持历史大版本下的小版本更新 #1841

--- src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/constant/RepositoryMessageCode.kt
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/constant/RepositoryMessageCode.kt
@@ -84,4 +84,5 @@ object RepositoryMessageCode {
     const val USER_DELETE_PEM_ERROR = "2115039" // 用户{0}在工程{1}下没有代码库{2}删除权限
     const val REPO_LOCK_UN_SUPPORT = "2115040" // 代码库{0}不支持锁定
     const val REPO_TYPE_NO_NEED_CERTIFICATION = "2115041" // 代码库类型{0}无需认证
+    const val CREATE_TAG_FAIL = "2115042" // 创建tag失败
 }
