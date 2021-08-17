f029235f3064471a4ac9fb53417b1688fa2741df
yongyiduan <yongyiduan@tencent.com>
2021-08-17 20:49:03 +0800

    --story=867594953 【OPENAPI】EPC迁移工具需求优化

--- src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v3/ApigwStreamResourceV3.kt
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v3/ApigwStreamResourceV3.kt
@@ -11,7 +11,14 @@ import com.tencent.devops.gitci.pojo.StreamTriggerBuildReq
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import javax.ws.rs.*
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OPEN_API_STREAM"], description = "OPEN-API-构建资源")
