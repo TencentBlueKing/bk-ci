392c3ab28989419ad1ff90dfadb3830d109a8cce
fitzcao <cao150260819@126.com>
2021-08-20 22:20:12 +0800

    --story=863222655 对接蓝鲸权限中心V3 添加权限校验openAPI接口

--- src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v3/ApigwAuthResourceV3.kt
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v3/ApigwAuthResourceV3.kt
@@ -3,6 +3,7 @@ package com.tencent.devops.openapi.api.apigw.v3
 import com.tencent.devops.auth.pojo.dto.GroupDTO
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BK_TOKEN
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
@@ -10,11 +11,13 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
+import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OPEN_API_BUILD"], description = "OPEN-API-构建资源")
@@ -41,4 +44,32 @@ interface ApigwAuthResourceV3 {
         @ApiParam("用户组信息", required = true)
         groupInfos: List<GroupDTO>
     ): Result<Boolean>
+
+    @GET
+    @Path("/projects/{projectCode}/resource/validate")
+    @ApiOperation("校验用户是否有action的权限")
+    fun validateUserResourcePermission(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        @ApiParam("待校验用户ID", required = true)
+        userId: String,
+        @QueryParam("action")
+        @ApiParam("资源类型", required = true)
+        action: String,
+        @QueryParam("projectCode")
+        @ApiParam("项目编码", required = true)
+        projectCode: String,
+        @QueryParam("resourceCode")
+        @ApiParam("资源编码", required = false)
+        resourceCode: String,
+        @QueryParam("resourceType")
+        @ApiParam("资源编码", required = false)
+        resourceType: String
+    ): Result<Boolean>
+
 }
