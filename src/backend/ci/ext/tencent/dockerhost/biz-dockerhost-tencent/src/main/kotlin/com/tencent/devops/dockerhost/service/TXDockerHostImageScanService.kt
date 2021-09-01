43590eb6c571ef6206619263c94d495fecfef454
sawyersong <sawyersong@tencent.com>
2021-09-01 17:27:00 +0800

    feat: 构建并推送镜像支持插件接入 #5032
    Signed-off-by: sawyersong <sawyersong@tencent.com>

---
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/service/TXDockerHostImageScanService.kt
@@ -0,0 +1,52 @@
+package com.tencent.devops.dockerhost.service
+
+import com.github.dockerjava.api.DockerClient
+import com.tencent.devops.common.api.util.script.ShellUtil
+import com.tencent.devops.dockerhost.services.DockerHostImageScanService
+import org.apache.commons.io.FileUtils
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.io.File
+import java.util.concurrent.Executors
+
+@Component
+class TXDockerHostImageScanService : DockerHostImageScanService {
+
+    @Value("\${dockerCli.dockerSavedPath:/data/dockersavedimages}")
+    var dockerSavedPath: String? = "/data/dockersavedimages"
+
+    override fun scanningDocker(
+        projectId: String,
+        pipelineId: String,
+        buildId: String,
+        vmSeqId: String,
+        imageTagSet: MutableSet<String>,
+        dockerClient: DockerClient
+    ) {
+        Executors.newFixedThreadPool(10).execute {
+            try {
+                imageTagSet.stream().forEach {
+                    val imageAndTagArray = it.split(":")
+                    val inputStream = dockerClient.saveImageCmd(imageAndTagArray[0])
+                        .withTag(imageAndTagArray[1])
+                        .exec()
+                    val imageSavedPath = "$dockerSavedPath/${imageAndTagArray[0]}.tar"
+                    val targetSavedImagesFile = File(imageSavedPath)
+                    FileUtils.copyInputStreamToFile(inputStream, targetSavedImagesFile)
+
+                    val script = "dockerscan -t $imageSavedPath -p $pipelineId -u $it -i dev " +
+                            "-T $projectId -b $buildId -n sawyersong"
+                    val scanResult = ShellUtil.executeEnhance(script)
+                    logger.info("[$buildId]|[$vmSeqId] scan docker $it result: $scanResult")
+                }
+            } catch (e: Exception) {
+                logger.error("[$buildId]|[$vmSeqId] scan docker error.", e)
+            }
+        }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(TXDockerHostImageScanService::class.java)
+    }
+}
