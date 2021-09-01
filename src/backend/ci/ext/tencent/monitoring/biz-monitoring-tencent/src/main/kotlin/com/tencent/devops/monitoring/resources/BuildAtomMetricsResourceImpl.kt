01d37d7bd49e8d0add93a589f4ed395b283de164
mingshewhe <mingshewhe@tencent.com>
2021-09-01 15:05:05 +0800

    --story=868091515 【流水线插件】sdk支持开发者上报度量数据

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/resources/BuildAtomMetricsResourceImpl.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/resources/BuildAtomMetricsResourceImpl.kt
@@ -4,15 +4,15 @@ import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.kafka.KafkaClient
 import com.tencent.devops.common.kafka.KafkaTopic.BUILD_ATOM_REPORT_TOPIC_PREFIX
 import com.tencent.devops.common.web.RestResource
-import com.tencent.devops.monitoring.api.service.BuildReportResource
+import com.tencent.devops.monitoring.api.service.BuildAtomMetricsResource
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
-class BuildReportResourceImpl @Autowired constructor(
+class BuildAtomMetricsResourceImpl @Autowired constructor(
     private val kafkaClient: KafkaClient
-) : BuildReportResource {
+) : BuildAtomMetricsResource {
 
-    override fun atomReport(atomCode: String, data: String): Result<Boolean> {
+    override fun reportAtomMetrics(atomCode: String, data: String): Result<Boolean> {
         kafkaClient.send(topic = "$BUILD_ATOM_REPORT_TOPIC_PREFIX-$atomCode", msg = data)
         return Result(true)
     }
