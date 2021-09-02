9eb776c2125689eddd5e288ad405dca1a0e206d4
sawyersong <sawyersong@tencent.com>
2021-09-02 17:55:39 +0800

    feat: 构建并推送镜像支持插件接入 #5032
    Signed-off-by: sawyersong <sawyersong@tencent.com>

--- src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/service/TXDockerHostImageScanService.kt
+++ src/backend/ci/ext/tencent/dockerhost/biz-dockerhost-tencent/src/main/kotlin/com/tencent/devops/dockerhost/service/TXDockerHostImageScanService.kt
@@ -1,18 +1,27 @@
 package com.tencent.devops.dockerhost.service
 
 import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.core.DefaultDockerClientConfig
+import com.github.dockerjava.core.DockerClientBuilder
+import com.github.dockerjava.okhttp.OkDockerHttpClient
+import com.github.dockerjava.transport.DockerHttpClient
 import com.tencent.devops.common.api.util.script.ShellUtil
+import com.tencent.devops.dockerhost.config.DockerHostConfig
 import com.tencent.devops.dockerhost.services.DockerHostImageScanService
+import com.tencent.devops.dockerhost.services.DockerHostImageService
 import com.tencent.devops.dockerhost.services.image.ImageDeleteHandler
 import org.apache.commons.io.FileUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.io.File
+import java.io.IOException
 import java.util.concurrent.Executors
 
 @Component
-class TXDockerHostImageScanService : DockerHostImageScanService {
+class TXDockerHostImageScanService(
+    private val dockerHostConfig: DockerHostConfig
+) : DockerHostImageScanService {
 
     @Value("\${dockerCli.dockerSavedPath:/data/dockersavedimages}")
     var dockerSavedPath: String? = "/data/dockersavedimages"
@@ -26,9 +35,24 @@ class TXDockerHostImageScanService : DockerHostImageScanService {
         dockerClient: DockerClient
     ) {
         Executors.newFixedThreadPool(10).execute {
+            lateinit var longDockerClient: DockerClient
             try {
+                val config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                    .withDockerConfig(dockerHostConfig.dockerConfig)
+                    .withApiVersion(dockerHostConfig.apiVersion)
+                    .build()
+
+                val longHttpClient: DockerHttpClient = OkDockerHttpClient.Builder()
+                    .dockerHost(config.dockerHost)
+                    .sslConfig(config.sslConfig)
+                    .connectTimeout(5000)
+                    .readTimeout(600000)
+                    .build()
+
+                longDockerClient = DockerClientBuilder.getInstance(config).withDockerHttpClient(longHttpClient).build()
+
                 imageTagSet.stream().forEach {
-                    val inputStream = dockerClient.saveImageCmd(it.substringBeforeLast(":"))
+                    val inputStream = longDockerClient.saveImageCmd(it.substringBeforeLast(":"))
                         .withTag(it.substringAfterLast(":"))
                         .exec()
                     val imageSavedPath = "$dockerSavedPath/${it.substringBeforeLast(":")}.tar"
@@ -42,7 +66,7 @@ class TXDockerHostImageScanService : DockerHostImageScanService {
 
                     logger.info("[$buildId]|[$vmSeqId] Push image success, now remove local image, image name and tag: $it")
                     try {
-                        dockerClient.removeImageCmd(it).exec()
+                        longDockerClient.removeImageCmd(it).exec()
                         logger.info("[$buildId]|[$vmSeqId] Remove local image success")
                     } catch (e: Throwable) {
                         logger.error("[$buildId]|[$vmSeqId] Docker rmi failed, msg: ${e.message}")
@@ -50,6 +74,12 @@ class TXDockerHostImageScanService : DockerHostImageScanService {
                 }
             } catch (e: Exception) {
                 logger.error("[$buildId]|[$vmSeqId] scan docker error.", e)
+            } finally {
+                try {
+                    longDockerClient.close()
+                } catch (e: IOException) {
+                    logger.error("[$buildId]|[$vmSeqId] Long docker client close exception: ${e.message}")
+                }
             }
         }
     }
