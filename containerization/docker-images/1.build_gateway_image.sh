f4312254479894401a5f82be8e63b6949d2677f3
stubenhuang <stubenhuang@tencent.com>
2021-09-11 11:14:33 +0800

    feat: 提供kubernetes部署方案 #4935

--- containerization/docker-images/1.build_gateway_image.sh
+++ containerization/docker-images/1.build_gateway_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "######################## BUILD GATEWAY IMAGE START... ########################"
-docker build -t mirrors.tencent.com/bkci/gateway:0.0.1 . -f ./dockerfile/gateway.Dockerfile
+docker build -t mirrors.tencent.com/stuben/bkci/gateway:0.0.1 . -f ./dockerfile/gateway.Dockerfile
 echo "######################## BUILD GATEWAY IMAGE FINISH ! ########################"
 echo ''
