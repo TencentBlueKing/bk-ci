4bd51d44b531f22779b62a8b70c73f1d830aa6eb
fitzcao <cao150260819@126.com>
2021-10-06 15:37:45 +0800

    --story=868483777 蓝盾消息通知支持企业微信机器人

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/notify/TxNotifyReceiverCmdImpl.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/main/kotlin/com/tencent/devops/process/service/notify/TxNotifyReceiverCmdImpl.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service
 
 @Service
 @Suppress("ComplexMethod", "NestedBlockDepth")
-class TxNotifyReceiverCmdImpl @@Autowired constructor(
+class TxNotifyReceiverCmdImpl @Autowired constructor(
     val bsAuthProjectApi: AuthProjectApi,
     val bsPipelineAuthServiceCode: BSPipelineAuthServiceCode
 ) : NotifyReceiversCmd() {
