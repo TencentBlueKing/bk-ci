b1350ce688fb9c1ee2958b6729f9582419a0deb0
fitzcao <cao150260819@126.com>
2021-10-07 11:51:17 +0800

    --story=868483777 蓝盾消息通知支持企业微信机器人

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/notify/command/impl/NotifyPipelineCmd.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/notify/command/impl/NotifyPipelineCmd.kt
@@ -36,7 +36,7 @@ abstract class NotifyPipelineCmd @Autowired constructor(
         val buildInfo = pipelineRuntimeService.getBuildInfo(commandContextBuild.buildId) ?: return
         val startTime = buildInfo.startTime
         val endTime = System.currentTimeMillis()
-        val duration = ((endTime - startTime!!) / 1000).toString()
+        val duration = endTime - startTime!!
 
         val trigger = executionVar.trigger
         val buildNum = buildInfo.buildNum
@@ -59,7 +59,7 @@ abstract class NotifyPipelineCmd @Autowired constructor(
             "trigger" to trigger,
             "username" to user,
             "failTask" to failTask,
-            "duration" to DateTimeUtil.formatMillSecond(duration.toLong() * 1000).removeSuffix("秒")
+            "duration" to DateTimeUtil.formatMillSecond(duration).removeSuffix("秒")
         )
         commandContextBuild.notifyValue.putAll(pipelineMap)
     }
