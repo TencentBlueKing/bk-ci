1938e461e426310a380bd55a0709c32344f4b9c8
fitzcao <cao150260819@126.com>
2021-10-08 10:04:00 +0800

    --story=868730567 [openAPI] 兼容V0,V3相关接口

--- src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/service/TxPermissionGrantServiceImpl.kt
+++ src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/service/TxPermissionGrantServiceImpl.kt
@@ -3,6 +3,9 @@ package com.tencent.devops.auth.service
 import com.tencent.bk.sdk.iam.config.IamConfiguration
 import com.tencent.bk.sdk.iam.service.impl.GrantServiceImpl
 import com.tencent.devops.auth.service.iam.impl.AbsPermissionGrantServiceImpl
+import com.tencent.devops.common.auth.api.AuthPermission
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.utils.TActionUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -21,6 +24,7 @@ class TxPermissionGrantServiceImpl @Autowired constructor(
     ): Boolean {
         // 如果校验的资源为pipeline,需要兼容传pipelineId的情况
         val useResourceCode = authPipelineIdService.findPipelineAutoId(resourceType, resourceCode)
-        return super.grantInstancePermission(userId, action, projectId, useResourceCode, resourceType)
+        val newAction = TActionUtils.buildAction(AuthPermission.get(action), AuthResourceType.get(resourceType))
+        return super.grantInstancePermission(userId, newAction, projectId, useResourceCode, resourceType)
     }
 }
