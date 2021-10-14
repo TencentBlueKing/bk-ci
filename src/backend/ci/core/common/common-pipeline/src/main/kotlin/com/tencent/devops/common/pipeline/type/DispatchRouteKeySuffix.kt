3778f50779ac8767644a3f971110cafee0d68a6a
sawyersong2 <wudiduomjimo@gmail.com>
2021-10-14 21:37:46 +0800

    feat: 流水线Job配额管理 #5154

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/type/DispatchRouteKeySuffix.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/type/DispatchRouteKeySuffix.kt
@@ -34,6 +34,8 @@ enum class DispatchRouteKeySuffix(val routeKeySuffix: String) {
     DEVCLOUD(".devcloud.public"),
     IDC(".idc.public"),
     GITCI(".gitci.public"),
+    STREAM(".stream.docker"),
+    STREAM_DEMOTE(".stream.docker.demote"),
     CODECC(".codecc.scan"),
     MACOS(".macos")
 }
