456bd030147239cf75fd37abc35c537bfdc10733
sawyersong2 <wudiduomjimo@gmail.com>
2021-10-15 14:46:36 +0800

    feat: 流水线Job配额管理 #5154

--- src/backend/ci/core/dispatch/api-dispatch/src/main/kotlin/com/tencent/devops/dispatch/api/BuildJobQuotaBusinessResource.kt
+++ src/backend/ci/core/dispatch/api-dispatch/src/main/kotlin/com/tencent/devops/dispatch/api/BuildJobQuotaBusinessResource.kt
@@ -40,7 +40,6 @@ import javax.ws.rs.DELETE
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
