bf9d7ea1cf38424fa4ef540e70451d1dbb411476
irwinsun <irwinsun@tencent.com>
2021-10-17 22:09:51 +0800

    feat: 在windows服务启动的devopsAgent支持构建拉起GUI程序 #5340 抽离根据平台不同生成脚本函数

--- src/agent/src/pkg/job/build_script_win.go
+++ src/agent/src/pkg/job/build_script_win.go
@@ -40,7 +40,7 @@ import (
 )
 
 
-func writeStartBuildAgentScriptBat(buildInfo *api.ThirdPartyBuildInfo) (string, error) {
+func writeStartBuildAgentScript(buildInfo *api.ThirdPartyBuildInfo) (string, error) {
 	scriptFile := fmt.Sprintf(
 		"%s/devops_agent_start_%s_%s_%s.bat",
 		systemutil.GetWorkDir(),
