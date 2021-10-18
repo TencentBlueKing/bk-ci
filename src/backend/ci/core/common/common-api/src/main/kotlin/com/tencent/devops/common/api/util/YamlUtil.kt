0eadf35bc18f5f7cd50a06822435d04b8c07b478
ruotiantang <ruotiantang@tencent.com>
2021-10-18 20:45:45 +0800

    feat: 修改YAML配置避免超过长度后自动换行 #5347

--- src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/util/YamlUtil.kt
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/util/YamlUtil.kt
@@ -29,7 +29,6 @@ package com.tencent.devops.common.api.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 /**
  *
