3d8c274e2fdfa0d31843ddbeae48b7f199e91c41
yongyiduan <yongyiduan@tencent.com>
2021-10-20 12:00:29 +0800

    feat: --story=868968611 【导出YAML工具】导出为YAML时，去掉未启用的节点，且上下文就近取值以已启用的有效节点为准

--- src/backend/ci/ext/tencent/process/biz-process-tencent/src/test/kotlin/com/tencent/devops/process/service/TXPipelineExportServiceTest.kt
+++ src/backend/ci/ext/tencent/process/biz-process-tencent/src/test/kotlin/com/tencent/devops/process/service/TXPipelineExportServiceTest.kt
@@ -96,7 +96,12 @@ class TXPipelineExportServiceTest {
     fun testReplaceMapWithDoubleCurlybraces3() {
         val inputMap = mutableMapOf(
             "key1" to "value" as Any,
-            "key2" to listOf("\${{haha}}", "abcedf\${{haha}}hijklmn", "\${{123456}}aaaaaa\${{haha}}hijklmn\${{aaaa}}", 123) as Any
+            "key2" to listOf(
+                "\${{haha}}",
+                "abcedf\${{haha}}hijklmn",
+                "\${{123456}}aaaaaa\${{haha}}hijklmn\${{aaaa}}",
+                123
+            ) as Any
         )
         val variables = mapOf("haha" to "value")
         val output2Elements = mutableMapOf(
@@ -181,8 +186,10 @@ class TXPipelineExportServiceTest {
                 "name=FILENAME::package.zip\"\n# 然后在后续的插件的表单中使用\${{ FILENAME }}引用这个变量\n\n#" +
                 " 您可以在质量红线中创建自定义指标，然后通过setGateValue函数设置指标值\n# setGateValue \"CodeCoverage\" " +
                 "\$myValue\n# 然后在质量红线选择相应指标和阈值。若不满足，流水线在执行时将会被卡住\n\n# cd \${{ WORKSPACE }} " +
-                "可进入当前工作空间目录\n\nset -x\n\n# 编译镜像\necho \"::set-output name=compile_img_str::trpc-golang-compile" +
-                ":0.1.2:tlinux:common\"\n# 运行镜像\necho \"::set-output name=img_str::trpc-golang-runtime:0.1.0\"\necho " +
+                "可进入当前工作空间目录\n\nset -x\n\n# 编译镜像\necho " +
+                "\"::set-output name=compile_img_str::trpc-golang-compile" +
+                ":0.1.2:tlinux:common\"\n# 运行镜像\necho \"::set-output" +
+                " name=img_str::trpc-golang-runtime:0.1.0\"\necho " +
                 "\"::set-output name=img_str2::trpc-golang-runtime:0.1.1\"\n# something\necho \"::set-output " +
                 "name=TestDir::src/go-test\"\n# something\nrm \${{ TestDir }} -rf\n\necho \"::set-output " +
                 "name=user::\${{ default_user }}\"\n"
