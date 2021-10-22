50424a51b5122c5b6ba752e29ff91af2074b1a80
stubenhuang <stubenhuang@tencent.com>
2021-10-22 17:55:57 +0800

    --story=867229939 支持通过跳转至目标公开体验应用的最新版本

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/resources/open/OpenExperienceResourceImpl.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/resources/open/OpenExperienceResourceImpl.kt
@@ -1,29 +1,19 @@
 package com.tencent.devops.experience.resources.open
 
-import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.experience.api.open.OpenExperienceResource
-import com.tencent.devops.experience.constant.ExperienceMessageCode
-import com.tencent.devops.experience.dao.ExperiencePublicDao
-import com.tencent.devops.experience.pojo.ExperienceJumpInfo
 import com.tencent.devops.experience.pojo.outer.OuterLoginParam
 import com.tencent.devops.experience.pojo.outer.OuterProfileVO
 import com.tencent.devops.experience.service.ExperienceAppService
-import com.tencent.devops.experience.service.ExperienceDownloadService
 import com.tencent.devops.experience.service.ExperienceOuterService
-import com.tencent.devops.experience.service.ExperienceService
-import org.jooq.DSLContext
 import org.springframework.beans.factory.annotation.Autowired
 import javax.ws.rs.core.Response
 
 @RestResource
 class OpenExperienceResourceImpl @Autowired constructor(
     private val experienceOuterService: ExperienceOuterService,
-    private val experienceAppService: ExperienceAppService,
-    private val experienceDownloadService: ExperienceDownloadService,
-    private val experiencePublicDao: ExperiencePublicDao,
-    private val dslContext: DSLContext
+    private val experienceAppService: ExperienceAppService
 ) : OpenExperienceResource {
     override fun outerLogin(
         platform: Int,
