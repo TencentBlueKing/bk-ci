2e4f17390a7aa767f6e4395fa8af73b72e82cb6d
mingshewhe <mingshewhe@tencent.com>
2021-10-26 19:53:23 +0800

    bug: 定时任务增加耗时watcher #5381
    (cherry picked from commit 2e398a0ca8b4182a25687e9a1b880f08713ff811)

--- src/backend/ci/core/process/plugin-trigger/src/main/kotlin/com/tencent/devops/process/plugin/trigger/configuration/TriggerConfiguration.kt
+++ src/backend/ci/core/process/plugin-trigger/src/main/kotlin/com/tencent/devops/process/plugin/trigger/configuration/TriggerConfiguration.kt
@@ -64,10 +64,12 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(QuartzProperties::class)
-open class TriggerConfiguration {
+@SuppressWarnings("TooManyFunctions")
+class TriggerConfiguration {
 
     @Bean
-    open fun pipelineJobBean(
+    @SuppressWarnings("LongParameterList")
+    fun pipelineJobBean(
         pipelineEventDispatcher: PipelineEventDispatcher,
         schedulerManager: SchedulerManager,
         pipelineTimerService: PipelineTimerService,
@@ -86,16 +88,16 @@ open class TriggerConfiguration {
     }
 
     @Bean
-    open fun schedulerManager(quartzProperties: QuartzProperties) = QuartzSchedulerManager(quartzProperties)
+    fun schedulerManager(quartzProperties: QuartzProperties) = QuartzSchedulerManager(quartzProperties)
 
     @Bean
-    open fun pipelineEventDispatcher(rabbitTemplate: RabbitTemplate) = MQEventDispatcher(rabbitTemplate)
+    fun pipelineEventDispatcher(rabbitTemplate: RabbitTemplate) = MQEventDispatcher(rabbitTemplate)
 
     @Value("\${queueConcurrency.timerTrigger:5}")
     private val timerConcurrency: Int? = null
 
     @Bean
-    open fun pipelineCoreExchange(): DirectExchange {
+    fun pipelineCoreExchange(): DirectExchange {
         val directExchange = DirectExchange(MQ.ENGINE_PROCESS_LISTENER_EXCHANGE, true, false)
         directExchange.isDelayed = true
         return directExchange
@@ -105,10 +107,10 @@ open class TriggerConfiguration {
      * 定时构建队列--- 并发一般
      */
     @Bean
-    open fun pipelineTimerBuildQueue() = Queue(MQ.QUEUE_PIPELINE_TIMER)
+    fun pipelineTimerBuildQueue() = Queue(MQ.QUEUE_PIPELINE_TIMER)
 
     @Bean
-    open fun pipelineTimerBuildQueueBind(
+    fun pipelineTimerBuildQueueBind(
         @Autowired pipelineTimerBuildQueue: Queue,
         @Autowired pipelineCoreExchange: DirectExchange
     ): Binding {
@@ -116,7 +118,7 @@ open class TriggerConfiguration {
     }
 
     @Bean
-    open fun pipelineTimerBuildListenerContainer(
+    fun pipelineTimerBuildListenerContainer(
         @Autowired connectionFactory: ConnectionFactory,
         @Autowired pipelineTimerBuildQueue: Queue,
         @Autowired rabbitAdmin: RabbitAdmin,
@@ -141,7 +143,7 @@ open class TriggerConfiguration {
      * 构建定时构建定时变化的广播交换机
      */
     @Bean
-    open fun pipelineTimerChangeFanoutExchange(): FanoutExchange {
+    fun pipelineTimerChangeFanoutExchange(): FanoutExchange {
         val fanoutExchange = FanoutExchange(MQ.EXCHANGE_PIPELINE_TIMER_CHANGE_FANOUT, true, false)
         fanoutExchange.isDelayed = true
         return fanoutExchange
@@ -154,12 +156,12 @@ open class TriggerConfiguration {
      * 用于接收定时任务状态变化广播的临时队列，队列将自动销毁
      */
     @Bean
-    open fun timerChangeQueueTemp(): Queue {
+    fun timerChangeQueueTemp(): Queue {
         return QueueBuilder.nonDurable().autoDelete().build()
     }
 
     @Bean
-    open fun timerChangeQueueTempBind(
+    fun timerChangeQueueTempBind(
         @Autowired timerChangeQueueTemp: Queue,
         @Autowired pipelineTimerChangeFanoutExchange: FanoutExchange
     ): Binding {
@@ -167,7 +169,7 @@ open class TriggerConfiguration {
     }
 
     @Bean
-    open fun timerChangeQueueListenerContainer(
+    fun timerChangeQueueListenerContainer(
         @Autowired connectionFactory: ConnectionFactory,
         @Autowired timerChangeQueueTemp: Queue,
         @Autowired rabbitAdmin: RabbitAdmin,
