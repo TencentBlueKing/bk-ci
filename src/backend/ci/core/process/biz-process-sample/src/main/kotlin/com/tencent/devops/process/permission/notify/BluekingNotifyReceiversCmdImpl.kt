2e7908c518e2ef4e1648ed6284d72298b178b621
fitzcao <cao150260819@126.com>
2021-10-28 15:58:14 +0800

    feat: 手动取消流水线，希望能够不发送企业微信消息通知 #5329

--- src/backend/ci/core/process/biz-process-sample/src/main/kotlin/com/tencent/devops/process/permission/notify/BluekingNotifyReceiversCmdImpl.kt
+++ src/backend/ci/core/process/biz-process-sample/src/main/kotlin/com/tencent/devops/process/permission/notify/BluekingNotifyReceiversCmdImpl.kt
@@ -1,12 +1,9 @@
 package com.tencent.devops.process.permission.notify
 
+import com.tencent.devops.common.api.util.EnvUtils
 import com.tencent.devops.process.notify.command.BuildNotifyContext
 import com.tencent.devops.process.notify.command.impl.NotifyReceiversCmd
-import org.springframework.beans.factory.annotation.Configurable
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 
-@Configurable
-@ConditionalOnMissingBean(NotifyReceiversCmd::class)
 class BluekingNotifyReceiversCmdImpl : NotifyReceiversCmd() {
     override fun canExecute(commandContext: BuildNotifyContext): Boolean {
         return true
@@ -14,10 +11,20 @@ class BluekingNotifyReceiversCmdImpl : NotifyReceiversCmd() {
 
     override fun execute(commandContext: BuildNotifyContext) {
         val setting = commandContext.pipelineSetting
-        if (commandContext.buildStatus.isFailure() || commandContext.buildStatus.isCancel()) {
-            commandContext.receivers = setting.successSubscription.users.split(",").toMutableSet()
+        val receivers = mutableSetOf<String>()
+        if (commandContext.buildStatus.isFailure()) {
+            val failReceivers = setting.failSubscription.users.split(",").toMutableSet()
+            failReceivers.forEach {
+                // 替换占位符
+                receivers.add(EnvUtils.parseEnv(it, commandContext.variables, true))
+            }
         } else if (commandContext.buildStatus.isSuccess()) {
-            commandContext.receivers = setting.failSubscription.users.split(",").toMutableSet()
+            val successReceivers = setting.successSubscription.users.split(",").toMutableSet()
+            successReceivers.forEach {
+                // 替换占位符
+                receivers.add(EnvUtils.parseEnv(it, commandContext.variables, true))
+            }
         }
+        commandContext.receivers = receivers
     }
 }
