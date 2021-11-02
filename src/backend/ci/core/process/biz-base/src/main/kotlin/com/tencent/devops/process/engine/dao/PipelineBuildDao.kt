13a613070106ea5714c315442e1da1037d906f2a
royalhuang <royalhuang@tencent.com>
2021-11-02 22:36:38 +0800

    bug: 启动参数内容过大时触发报错 #5264 统一使用history表进行存储

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildDao.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildDao.kt
@@ -28,6 +28,8 @@
 package com.tencent.devops.process.engine.dao
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation
+import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.ErrorInfo
 import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.common.api.util.timestampmilli
@@ -38,6 +40,7 @@ import com.tencent.devops.common.pipeline.enums.StartType
 import com.tencent.devops.common.pipeline.pojo.BuildParameters
 import com.tencent.devops.model.process.Tables.T_PIPELINE_BUILD_HISTORY
 import com.tencent.devops.model.process.tables.records.TPipelineBuildHistoryRecord
+import com.tencent.devops.process.constant.ProcessMessageCode
 import com.tencent.devops.process.engine.pojo.BuildInfo
 import com.tencent.devops.process.pojo.BuildStageStatus
 import org.jooq.Condition
@@ -47,6 +50,7 @@ import org.jooq.Result
 import org.springframework.stereotype.Repository
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import javax.ws.rs.core.Response
 
 @Suppress("ALL")
 @Repository
@@ -78,54 +82,61 @@ class PipelineBuildDao {
         buildMsg: String?,
         buildNumAlias: String? = null
     ) {
-
-        with(T_PIPELINE_BUILD_HISTORY) {
-            dslContext.insertInto(
-                this,
-                BUILD_ID,
-                BUILD_NUM,
-                PROJECT_ID,
-                PIPELINE_ID,
-                PARENT_BUILD_ID,
-                PARENT_TASK_ID,
-                START_TIME,
-                START_USER,
-                TRIGGER_USER,
-                STATUS,
-                TRIGGER,
-                TASK_COUNT,
-                FIRST_TASK_ID,
-                CHANNEL,
-                VERSION,
-                QUEUE_TIME,
-                BUILD_PARAMETERS,
-                WEBHOOK_TYPE,
-                WEBHOOK_INFO,
-                BUILD_MSG,
-                BUILD_NUM_ALIAS
-            ).values(
-                buildId,
-                buildNum,
-                projectId,
-                pipelineId,
-                parentBuildId,
-                parentTaskId,
-                LocalDateTime.now(),
-                startUser,
-                triggerUser,
-                status.ordinal,
-                trigger,
-                taskCount,
-                firstTaskId,
-                channelCode.name,
-                version,
-                LocalDateTime.now(),
-                JsonUtil.toJson(buildParameters, formatted = false),
-                webhookType,
-                webhookInfo,
-                buildMsg,
-                buildNumAlias
-            ).execute()
+        try {
+            with(T_PIPELINE_BUILD_HISTORY) {
+                dslContext.insertInto(
+                    this,
+                    BUILD_ID,
+                    BUILD_NUM,
+                    PROJECT_ID,
+                    PIPELINE_ID,
+                    PARENT_BUILD_ID,
+                    PARENT_TASK_ID,
+                    START_TIME,
+                    START_USER,
+                    TRIGGER_USER,
+                    STATUS,
+                    TRIGGER,
+                    TASK_COUNT,
+                    FIRST_TASK_ID,
+                    CHANNEL,
+                    VERSION,
+                    QUEUE_TIME,
+                    BUILD_PARAMETERS,
+                    WEBHOOK_TYPE,
+                    WEBHOOK_INFO,
+                    BUILD_MSG,
+                    BUILD_NUM_ALIAS
+                ).values(
+                    buildId,
+                    buildNum,
+                    projectId,
+                    pipelineId,
+                    parentBuildId,
+                    parentTaskId,
+                    LocalDateTime.now(),
+                    startUser,
+                    triggerUser,
+                    status.ordinal,
+                    trigger,
+                    taskCount,
+                    firstTaskId,
+                    channelCode.name,
+                    version,
+                    LocalDateTime.now(),
+                    JsonUtil.toJson(buildParameters, formatted = false),
+                    webhookType,
+                    webhookInfo,
+                    buildMsg,
+                    buildNumAlias,
+                ).execute()
+            }
+        } catch (e: MysqlDataTruncation) {
+            throw ErrorCodeException(
+                statusCode = Response.Status.BAD_REQUEST.statusCode,
+                errorCode = ProcessMessageCode.ERROR_PIPELINE_PARAM_VALUE_TOO_LARGE,
+                defaultMessage = "流水线启动参数内容过大: ${e.message}"
+            )
         }
     }
 
