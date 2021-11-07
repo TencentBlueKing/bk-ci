2effc338d6d666d91fb5d622422b607eff24b96d
irwinsun <irwinsun@tencent.com>
2021-11-07 11:54:27 +0800

    refactor: 移除获取代码库BUILD无用接口 #1981 format

--- src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/BuildRepositoryResource.kt
+++ src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/BuildRepositoryResource.kt
@@ -41,7 +41,6 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
-import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
