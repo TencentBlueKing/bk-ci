c1d7afb33d4b35658426b08ec0c430f88113896a
yongyiduan <yongyiduan@tencent.com>
2021-11-19 17:02:13 +0800

    --story=869529171 run插件使用研发商店版本 携带测试代码，须在后期删除

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/modelCreate/ModelElement.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/modelCreate/ModelElement.kt
@@ -52,7 +52,6 @@ import javax.ws.rs.core.Response
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
