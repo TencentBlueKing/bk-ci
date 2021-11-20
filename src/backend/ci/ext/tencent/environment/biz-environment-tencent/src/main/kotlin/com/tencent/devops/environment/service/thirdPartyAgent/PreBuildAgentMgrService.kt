4d832f2d5dfe6b1aa6b3e2bc2038f1d302b60d07
irwinsun <irwinsun@tencent.com>
2021-11-20 23:43:31 +0800

    feat: 构建机导入流程优化 #4686 format

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/thirdPartyAgent/PreBuildAgentMgrService.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/thirdPartyAgent/PreBuildAgentMgrService.kt
@@ -46,6 +46,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+@Suppress("LongMethod")
 @Service
 class PreBuildAgentMgrService @Autowired constructor(
     private val dslContext: DSLContext,
