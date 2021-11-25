4c8dee91193b0ee83ff23e01235a154befa59d19
yaoxuwan <yaoxuwan@tencent.com>
2021-11-25 16:47:11 +0800

    feature: 新增制品库服务 #2093 修正代码规范问题

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/ArtifactApiFactory.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/ArtifactApiFactory.kt
@@ -1,27 +1,10 @@
 package com.tencent.devops.worker.common.api
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_AGENT_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_AGENT_SECRET_KEY
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BUILD_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BUILD_TYPE
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_PROJECT_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_VM_SEQ_ID
-import com.tencent.devops.common.api.exception.TaskExecuteException
-import com.tencent.devops.common.api.pojo.ErrorCode
-import com.tencent.devops.common.api.pojo.ErrorType
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.worker.common.api.archive.ArchiveSDKApi
 import com.tencent.devops.worker.common.api.report.ReportSDKApi
-import com.tencent.devops.worker.common.api.utils.ThirdPartyAgentBuildInfoUtils
-import com.tencent.devops.worker.common.env.AgentEnv
-import com.tencent.devops.worker.common.env.BuildEnv
-import com.tencent.devops.worker.common.env.BuildType
 import com.tencent.devops.worker.common.exception.ApiNotExistException
-import okhttp3.Headers
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import org.reflections.Reflections
 import org.slf4j.LoggerFactory
 import java.lang.reflect.Modifier
