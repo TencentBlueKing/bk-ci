41f3c6b04b764c95bb908a91a396326ad7353428
royalhuang <royalhuang@tencent.com>
2021-11-30 16:31:03 +0800

    feat: 引擎内所有审核流程触发时产生广播事件 #5533

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/listener/BuildReviewListener.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/listener/BuildReviewListener.kt
@@ -8,7 +8,8 @@ import com.tencent.devops.common.event.dispatcher.pipeline.mq.MQ
 import com.tencent.devops.common.event.pojo.pipeline.PipelineBuildReviewBroadCastEvent
 import com.tencent.devops.stream.dao.GitRequestEventBuildDao
 import com.tencent.devops.stream.dao.GitRequestEventDao
-import com.tencent.devops.stream.listener.buildFinish.SendCommitCheck
+import com.tencent.devops.stream.listener.components.SendCommitCheck
+import com.tencent.devops.stream.listener.components.SendQualityMrComment
 import com.tencent.devops.stream.v2.dao.StreamBasicSettingDao
 import com.tencent.devops.stream.v2.service.StreamPipelineService
 import org.jooq.DSLContext
@@ -28,7 +29,8 @@ class BuildReviewListener @Autowired constructor(
     private val gitRequestEventDao: GitRequestEventDao,
     private val streamPipelineService: StreamPipelineService,
     private val streamBasicSettingDao: StreamBasicSettingDao,
-    private val sendCommitCheck: SendCommitCheck
+    private val sendCommitCheck: SendCommitCheck,
+    private val sendQualityMrComment: SendQualityMrComment
 ) {
 
     companion object {
@@ -73,26 +75,30 @@ class BuildReviewListener @Autowired constructor(
         val pipeline = streamPipelineService.getPipelineById(gitProjectId, pipelineId)
             ?: throw OperationException("git ci pipeline not exist")
 
+        val context = StreamBuildStageListenerContextV2(
+            buildEvent = BuildEvent(
+                projectId = buildReviewEvent.projectId,
+                pipelineId = buildReviewEvent.pipelineId,
+                userId = buildReviewEvent.userId,
+                buildId = buildReviewEvent.buildId,
+                status = buildReviewEvent.status,
+                startTime = streamBuild.createTime.timestampmilli()
+            ),
+            requestEvent = requestEvent,
+            streamBuildEvent = buildEvent,
+            pipeline = pipeline,
+            streamSetting = v2GitSetting,
+            reviewType = buildReviewEvent.reviewType
+        )
+
         when (buildReviewEvent.reviewType) {
             BuildReviewType.STAGE_REVIEW, BuildReviewType.QUALITY_CHECK_IN, BuildReviewType.QUALITY_CHECK_OUT -> {
                 // 推送构建消息
-                sendCommitCheck.sendCommitCheck(
-                    StreamBuildStageListenerContextV2(
-                        buildEvent = BuildEvent(
-                            projectId = buildReviewEvent.projectId,
-                            pipelineId = buildReviewEvent.pipelineId,
-                            userId = buildReviewEvent.userId,
-                            buildId = buildReviewEvent.buildId,
-                            status = buildReviewEvent.status,
-                            startTime = streamBuild.createTime.timestampmilli()
-                        ),
-                        requestEvent = requestEvent,
-                        streamBuildEvent = buildEvent,
-                        pipeline = pipeline,
-                        streamSetting = v2GitSetting,
-                        reviewType = buildReviewEvent.reviewType
-                    )
-                )
+                sendCommitCheck.sendCommitCheck(context)
+            }
+            // TODO: 红线检查完了的事件
+            TODO() -> {
+                sendQualityMrComment.sendMrComment(context)
             }
             // 这里先这么写，未来如果这么枚举扩展代码编译时可以第一时间感知，防止漏过事件
             BuildReviewType.TASK_REVIEW -> {
@@ -101,4 +107,3 @@ class BuildReviewListener @Autowired constructor(
         }
     }
 }
-
