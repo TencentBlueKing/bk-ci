178f62f128219695a9186a123edfb0e407bd9f60
carlyin <449036249@qq.com>
2021-12-02 10:10:07 +0800

    feat:增加组件与项目间的权限校验open类接口 #5667

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/env/AgentEnv.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/env/AgentEnv.kt
@@ -29,6 +29,7 @@ package com.tencent.devops.worker.common.env
 
 import com.tencent.devops.common.api.enums.OSType
 import com.tencent.devops.common.api.exception.ParamBlankException
+import com.tencent.devops.common.api.util.PropertyUtil
 import com.tencent.devops.common.log.pojo.enums.LogStorageMode
 import com.tencent.devops.common.service.env.Env
 import com.tencent.devops.worker.common.exception.PropertyNotExistException
@@ -55,6 +56,7 @@ object AgentEnv {
     private const val AGENT_ENV = "landun.env"
     private const val AGENT_LOG_SAVE_MODE = "devops_log_save_mode"
     private const val MACOS_WORKSPACE = "DEVOPS_MACOS_DIR"
+    private const val AGENT_PROPERTIES_FILE_NAME = ".agent.properties"
 
     private var projectId: String? = null
     private var agentId: String? = null
@@ -67,7 +69,7 @@ object AgentEnv {
 
     private var property: Properties? = null
 
-    private val propertyFile = File(getLandun(), ".agent.properties")
+    private val propertyFile = File(getLandun(), AGENT_PROPERTIES_FILE_NAME)
 
     fun getProjectId(): String {
 
@@ -127,7 +129,7 @@ object AgentEnv {
                     } else {
                         // Get it from .agent.property
                         try {
-                            Env.parse(getProperty(AGENT_ENV) ?: "")
+                            Env.parse(PropertyUtil.getPropertyValue(AGENT_ENV, "/$AGENT_PROPERTIES_FILE_NAME"))
                         } catch (t: Throwable) {
                             logger.warn("Fail to get the agent env, use prod as default", t)
                             Env.PROD
