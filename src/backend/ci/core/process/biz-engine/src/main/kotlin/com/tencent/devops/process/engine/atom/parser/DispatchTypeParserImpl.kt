7bbd683da6abd3f9bbd7e53da5a9c25aa793f407
royalhuang <royalhuang@tencent.com>
2021-12-06 16:37:31 +0800

    feat: 流水线新增构建矩阵 #4518

--- src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/atom/parser/DispatchTypeParserImpl.kt
+++ src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/atom/parser/DispatchTypeParserImpl.kt
@@ -87,4 +87,8 @@ class DispatchTypeParserImpl @Autowired constructor(
             dispatchType.credentialProject = projectId
         }
     }
+
+    override fun parseRunsOn(runsOnStr: String): DispatchType? {
+        return null
+    }
 }
