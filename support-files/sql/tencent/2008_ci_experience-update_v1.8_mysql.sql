bdbdb2b1f1a6c6a06746feca3fd42c19db14832d
stubenhuang <stubenhuang@tencent.com>
2021-12-06 15:20:22 +0800

    --story=869218343 提供获取体验信息接口

---
+++ support-files/sql/tencent/2008_ci_experience-update_v1.8_mysql.sql
@@ -0,0 +1,51 @@
+USE devops_ci_experience;
+SET NAMES utf8mb4;
+
+DROP PROCEDURE IF EXISTS ci_experience_schema_update;
+
+DELIMITER <CI_UBF>
+
+CREATE PROCEDURE ci_experience_schema_update()
+BEGIN
+
+    DECLARE db VARCHAR(100);
+    SET AUTOCOMMIT = 0;
+    SELECT DATABASE() INTO db;
+
+	IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_EXPERIENCE'
+                        AND COLUMN_NAME = 'PIPELINE_ID') THEN
+        ALTER TABLE T_EXPERIENCE ADD COLUMN PIPELINE_ID varchar(34) NOT NULL COMMENT '流水线ID';
+    END IF;
+
+  IF NOT EXISTS(SELECT 1
+                FROM information_schema.statistics
+                WHERE TABLE_SCHEMA = db
+                  AND TABLE_NAME = 'T_EXPERIENCE'
+                  AND INDEX_NAME = 'IDX_PIPELINE_ID') THEN
+      ALTER TABLE T_EXPERIENCE ADD INDEX IDX_PIPELINE_ID (`PIPELINE_ID`);
+  END IF;
+
+	IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_EXPERIENCE'
+                        AND COLUMN_NAME = 'BUILD_ID') THEN
+        ALTER TABLE T_EXPERIENCE ADD COLUMN BUILD_ID varchar(34) NOT NULL COMMENT '构建ID';
+    END IF;
+
+  IF NOT EXISTS(SELECT 1
+                FROM information_schema.statistics
+                WHERE TABLE_SCHEMA = db
+                  AND TABLE_NAME = 'T_EXPERIENCE'
+                  AND INDEX_NAME = 'IDX_BUILD_ID') THEN
+      ALTER TABLE T_EXPERIENCE ADD INDEX IDX_BUILD_ID (`BUILD_ID`);
+  END IF;
+
+    COMMIT;
+END <CI_UBF>
+DELIMITER ;
+COMMIT;
+CALL ci_experience_schema_update();
