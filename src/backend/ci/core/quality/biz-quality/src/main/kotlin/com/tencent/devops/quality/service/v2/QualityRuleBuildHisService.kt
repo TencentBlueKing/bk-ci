1936b790db8512cca662c447595bed21e072a440
jamikxu <jamikxu@tencent.com>
2021-12-06 11:03:58 +0800

    bug: 批量处理后台超过24h未审核的红线失败记录 #5695. add process trigger

--- src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleBuildHisService.kt
+++ src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleBuildHisService.kt
@@ -411,6 +411,20 @@ class QualityRuleBuildHisService constructor(
             logger.info("QUALITY|update_rule_status_count: $count")
             timeOutRules.map { rule ->
                 qualityRuleBuildHisOperationDao.create(dslContext, rule.createUser, rule.id, rule.stageId)
+                val trigger = client.get(ServiceBuildResource::class).qualityTriggerStage(
+                    userId = rule.createUser,
+                    projectId = rule.projectId,
+                    pipelineId = rule.pipelineId,
+                    buildId = rule.buildId,
+                    stageId = rule.stageId,
+                    qualityRequest = StageQualityRequest(
+                        position = rule.rulePos,
+                        pass = false,
+                        checkTimes = 1
+                    )
+                ).data ?: false
+                logger.info("QUALITY|project: ${rule.projectId}, pipelineId: ${rule.pipelineId}, " +
+                        "buildId: ${rule.buildId}, trigger: $trigger")
             }
         }
         return count
