fbdcbdb02ce65c1fcaa9214cc432945f3ccf48fc
stubenhuang <stubenhuang@tencent.com>
2021-12-06 14:29:26 +0800

    --story=869218343 提供获取体验信息接口

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceDownloadService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceDownloadService.kt
@@ -419,9 +419,11 @@ class ExperienceDownloadService @Autowired constructor(
         }
 
         val scheme = if (platform == "ANDROID") {
-            "bkdevopsapp://bkdevopsapp/app/experience/expDetail/${HashUtil.encodeLongId(experiencePublicRecord.recordId)}"
+            "bkdevopsapp://bkdevopsapp/app/experience/expDetail/" +
+                    HashUtil.encodeLongId(experiencePublicRecord.recordId)
         } else {
-            "bkdevopsapp://app/experience/expDetail/${HashUtil.encodeLongId(experiencePublicRecord.id)}"
+            "bkdevopsapp://app/experience/expDetail/" +
+                    HashUtil.encodeLongId(experiencePublicRecord.id)
         }
 
         val shortUrlRequest = CreateShortUrlRequest(
