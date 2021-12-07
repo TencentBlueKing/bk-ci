934d60725aa8b405affa53e6f47fdf7157221aef
jamikxu <jamikxu@tencent.com>
2021-12-07 10:38:55 +0800

    --bug=94808523 【Bug】批量处理后台超过24h未审核的红线失败记录

--- src/backend/ci/settings.gradle.kts
+++ src/backend/ci/settings.gradle.kts
@@ -451,5 +451,3 @@ include(":ext:tencent:statistics:api-statistics-tencent")
 include(":ext:tencent:statistics:biz-statistics-tencent")
 include(":ext:tencent:statistics:boot-statistics-tencent")
 include(":ext:tencent:statistics:model-statistics-tencent")
-include("ext:tencent:quality:api-quality-tencent")
-findProject(":ext:tencent:quality:api-quality-tencent")?.name = "api-quality-tencent"
