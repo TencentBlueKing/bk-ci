6d28e688e5a170a8abbbbb9744c989dadb0b61b6
carlyin <449036249@qq.com>
2021-12-10 15:57:35 +0800

    perf:蓝盾引擎模块对应的数据库支持分库 #5267

--- src/backend/ci/ext/tencent/statistics/biz-statistics-tencent/src/main/kotlin/com/tencent/devops/statistics/config/JooqConfiguration.kt
+++ src/backend/ci/ext/tencent/statistics/biz-statistics-tencent/src/main/kotlin/com/tencent/devops/statistics/config/JooqConfiguration.kt
@@ -86,7 +86,7 @@ class JooqConfiguration {
 
     @Bean
     fun processJooqConfiguration(
-        @Qualifier("processDataSource")
+        @Qualifier("shardingDataSource")
         processDataSource: DataSource
     ): DefaultConfiguration {
         val configuration = DefaultConfiguration()
