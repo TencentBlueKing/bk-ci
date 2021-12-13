c5d1e2c71aa10a468321f2e41c2ba8fa503191cd
yongyiduan <yongyiduan@tencent.com>
2021-12-13 18:28:31 +0800

    feat: 流水线新增构建矩阵 #4518 add test

--- src/backend/ci/core/common/common-pipeline/src/test/kotlin/com/tencent/devops/common/pipeline/option/MatrixControlOptionTest.kt
+++ src/backend/ci/core/common/common-pipeline/src/test/kotlin/com/tencent/devops/common/pipeline/option/MatrixControlOptionTest.kt
@@ -331,89 +331,227 @@ internal class MatrixControlOptionTest {
     }
 
     @Test
-    fun convertStrategyYaml() {
+    fun calculateValueMatrixMaxSizeTest() {
+        val matrixControlOption = MatrixControlOption(
+            // 2*3*3 = 18
+            strategyStr = """
+                    os: [docker,macos,os1,os2]
+                    var1: [a,b,c,d]
+                    var2: [1,2,3,4]
+                    var3: [Q,W,E,R]
+                """,
+            // +2
+            includeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // +1 额外的情况
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    ),
+                    // +0 重复值不加入
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1",
+                        "var3" to "Q"
+                    )
+                )
+            ),
+            excludeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // -0 并没有完全匹配项
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    )
+                )
+            ),
+            totalCount = 10, // 3*3 + 2 - 1
+            finishCount = 1,
+            fastKill = true,
+            maxConcurrency = 50
+        )
+        val contextCase = matrixControlOption.convertMatrixConfig(emptyMap()).getAllContextCase()
+        println(contextCase.size)
+        contextCase.forEachIndexed { index, map ->
+            println("$index: $map")
+        }
+        Assert.assertEquals(contextCase.size, 257)
+    }
+
+    @Test
+    fun calculateValueMatrixMinSizeTest1() {
         val matrixControlOption = MatrixControlOption(
             strategyStr = """
-                    os: [docker,macos]
-                    var1: [a,b,c]
-                    var2: [1,2,3]
+                    os: [docker]
                 """,
-            includeCaseStr = YamlUtil.toYaml(listOf(mapOf("var1" to "a"), mapOf("var2" to "2"))),
-            excludeCaseStr = YamlUtil.toYaml(listOf(mapOf("var2" to "1"))),
+            includeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // +1 额外的情况
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    )
+                )
+            ),
+            excludeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // -0 先exclude再include
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    ),
+                    // -1
+                    mapOf(
+                        "os" to "docker"
+                    )
+                )
+            ),
             totalCount = 10, // 3*3 + 2 - 1
             finishCount = 1,
             fastKill = true,
             maxConcurrency = 50
         )
-        val result = mapOf(
-            "os" to listOf("docker", "macos"),
-            "var1" to listOf("a", "b", "c"),
-            "var2" to listOf("1", "2", "3")
-        )
-//        Assert.assertEquals(JsonUtil.toJson(matrixControlOption.getAllContextCase()), JsonUtil.toJson(result))
+        val contextCase = matrixControlOption.convertMatrixConfig(emptyMap()).getAllContextCase()
+        println(contextCase.size)
+        contextCase.forEachIndexed { index, map ->
+            println("$index: $map")
+        }
+        Assert.assertEquals(contextCase.size, 1)
     }
 
     @Test
-    fun convertStrategyJson() {
+    fun calculateValueMatrixMinSizeTest2() {
         val matrixControlOption = MatrixControlOption(
-            strategyStr = """{
-                    "os": [docker,macos],
-                    "var1": [a,b,c],
-                    "var2": [1,2,3],
-                }""",
-            includeCaseStr = YamlUtil.toYaml(listOf(mapOf("var1" to "a"), mapOf("var2" to "2"))),
-            excludeCaseStr = YamlUtil.toYaml(listOf(mapOf("var2" to "1"))),
+            strategyStr = """
+                    os: [docker]
+                """,
+            includeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // +1 额外的情况
+                    mapOf(
+                        "os" to "docker"
+                    )
+                )
+            ),
+            excludeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // -0 先exclude再include
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    ),
+                    // -1
+                    mapOf(
+                        "os" to "docker"
+                    )
+                )
+            ),
             totalCount = 10, // 3*3 + 2 - 1
             finishCount = 1,
             fastKill = true,
             maxConcurrency = 50
         )
-        val result =
-            mapOf("os" to listOf("docker", "macos"), "var1" to listOf("a", "b", "c"), "var2" to listOf(1, 2, 3))
-
-//        print(matrixControlOption.getAllContextCase())
-//        Assert.assertEquals(JsonUtil.toJson(matrixControlOption.getAllContextCase()), JsonUtil.toJson(result))
+        val contextCase = matrixControlOption.convertMatrixConfig(emptyMap()).getAllContextCase()
+        println(contextCase.size)
+        contextCase.forEachIndexed { index, map ->
+            println("$index: $map")
+        }
+        Assert.assertEquals(contextCase.size, 1)
     }
 
     @Test
-    fun convertIncludeCase() {
+    fun calculateValueMatrixMinSizeTest3() {
         val matrixControlOption = MatrixControlOption(
-            strategyStr = """{
-                    "os": [docker,macos],
-                    "var1": [a,b,c],
-                    "var2": [1,2,3],
-                }""",
-            includeCaseStr = YamlUtil.toYaml(listOf(mapOf("var1" to "a"), mapOf("var2" to "2"))),
-            excludeCaseStr = YamlUtil.toYaml(listOf(mapOf("var2" to "1"))),
+            strategyStr = """
+                    os: [docker]
+                """,
+            includeCaseStr = "",
+            excludeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    // -0 先exclude再include
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    ),
+                    // -1
+                    mapOf(
+                        "os" to "docker"
+                    )
+                )
+            ),
             totalCount = 10, // 3*3 + 2 - 1
             finishCount = 1,
             fastKill = true,
             maxConcurrency = 50
         )
-        val result = listOf(mapOf("var1" to "a"), mapOf("var2" to "2"))
-
-//        print(matrixControlOption.convertIncludeCase())
-//        Assert.assertEquals(JsonUtil.toJson(matrixControlOption.convertIncludeCase()), JsonUtil.toJson(result))
+        val contextCase = matrixControlOption.convertMatrixConfig(emptyMap()).getAllContextCase()
+        println(contextCase.size)
+        contextCase.forEachIndexed { index, map ->
+            println("$index: $map")
+        }
+        Assert.assertEquals(contextCase.size, 0)
     }
 
     @Test
-    fun convertExcludeCase() {
+    fun calculateValueMatrixJson() {
         val matrixControlOption = MatrixControlOption(
-            strategyStr = """{
-                    "os": [docker,macos],
-                    "var1": [a,b,c],
-                    "var2": [1,2,3],
-                }""",
-            includeCaseStr = YamlUtil.toYaml(listOf(mapOf("var1" to "a"), mapOf("var2" to "2"))),
-            excludeCaseStr = YamlUtil.toYaml(listOf(mapOf("var2" to "1"))),
+            // 2*3*3 = 18
+            strategyStr = """
+                    {
+                        "include": [
+                            {
+                                "os": "docker",
+                                "var1": "form_json",
+                                "var2": 2
+                            },
+                            {
+                                "os": "macos",
+                                "var1": "form_json",
+                                "var2": 1
+                            }
+                        ],
+                        "exclude": [
+                            {
+                                "os": "docker",
+                                "var1": "b",
+                                "var2": 3
+                            },
+                            {
+                                "os": "macos",
+                                "var1": "c",
+                                "var2": 2
+                            }
+                        ]
+                    }
+                """,
+            includeCaseStr = "",
+            // -1
+            excludeCaseStr = YamlUtil.toYaml(
+                listOf(
+                    mapOf(
+                        "os" to "docker",
+                        "var1" to "a",
+                        "var2" to "1"
+                    )
+                )
+            ),
             totalCount = 10, // 3*3 + 2 - 1
             finishCount = 1,
             fastKill = true,
             maxConcurrency = 50
         )
-        val result = listOf(mapOf("var2" to "1"))
-
-//        print(matrixControlOption.convertExcludeCase())
-//        Assert.assertEquals(JsonUtil.toJson(matrixControlOption.convertExcludeCase()), JsonUtil.toJson(result))
+        val contextCase = matrixControlOption.convertMatrixConfig(emptyMap()).getAllContextCase()
+        println(contextCase.size)
+        contextCase.forEachIndexed { index, map ->
+            println("$index: $map")
+        }
+        Assert.assertEquals(contextCase.size, 2)
     }
 }
