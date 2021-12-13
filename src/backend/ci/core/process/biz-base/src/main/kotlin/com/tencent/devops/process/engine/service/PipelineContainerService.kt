ced13ac313190765bb849e1f81ff3b55a6054f24
royalhuang <royalhuang@tencent.com>
2021-12-13 14:54:26 +0800

    feat: 流水线新增构建矩阵 #4518

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineContainerService.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineContainerService.kt
@@ -412,7 +412,7 @@ class PipelineContainerService @Autowired constructor(
                         }
                     }
                     needUpdateContainer = true
-                } else if (container.matrixGroupFlag == true && BuildStatus.parse(container.status).isFailure()) {
+                } else if (container.matrixGroupFlag == true && BuildStatus.parse(container.status).isFinish()) {
                     // 构建矩阵没有对应的重试插件，单独进行重试判断
                     setRetryBuildContainer(container, context.executeCount)
                     needUpdateContainer = true
