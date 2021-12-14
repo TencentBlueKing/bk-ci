92fe21fb4d0da27922737b213d18e0723640a7de
mingshewhe <mingshewhe@tencent.com>
2021-12-14 14:29:21 +0800

    bug: api创建回调事件异常也会返回true #5762

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/service/ServiceCallBackResourceImpl.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/service/ServiceCallBackResourceImpl.kt
@@ -27,11 +27,13 @@
 
 package com.tencent.devops.process.api.service
 
+import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.Page
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.api.util.PageUtil
 import com.tencent.devops.common.pipeline.event.CallBackEvent
 import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.process.constant.ProcessMessageCode.ERROR_CALLBACK_SAVE_FAIL
 import com.tencent.devops.process.engine.service.ProjectPipelineCallBackService
 import com.tencent.devops.process.pojo.CreateCallBackResult
 import com.tencent.devops.process.pojo.ProjectPipelineCallBack
@@ -62,7 +64,11 @@ class ServiceCallBackResourceImpl @Autowired constructor(
             secretToken = secretToken
         )
         return if (result.failureEvents.isNotEmpty()) {
-            Result(false)
+            throw ErrorCodeException(
+                errorCode = ERROR_CALLBACK_SAVE_FAIL,
+                defaultMessage = "创建callback失败",
+                params = arrayOf(result.failureEvents[event.name] ?: "")
+            )
         } else {
             Result(true)
         }
