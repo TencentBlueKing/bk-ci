6d7013a27e11607730af2e248e576aefd23ca4af
stubenhuang <stubenhuang@tencent.com>
2021-12-16 21:12:51 +0800

    feat: 统一gradle插件拉取的仓库 #5815

--- src/backend/ci/settings.gradle.kts
+++ src/backend/ci/settings.gradle.kts
@@ -27,15 +27,17 @@
 
 rootProject.name = "bk-ci-backend"
 
+// 适用于project的plugins
 pluginManagement {
-    val devopsBootVersion: String by settings
-    plugins {
-        id("com.tencent.devops.boot") version devopsBootVersion
-    }
     repositories {
         mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
+        if (System.getenv("GITHUB_WORKFLOW") == null) { // 普通环境
+            maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public")
+            maven(url = "https://mirrors.tencent.com/nexus/repository/gradle-plugins/")
+        } else { // GitHub Action 环境
+            mavenCentral()
+            gradlePluginPortal()
+        }
     }
 }
 
