9079f4fc42f83c4c0ef96f8fef77ff556cdb701d
yongyiduan <yongyiduan@tencent.com>
2021-12-24 16:47:43 +0800

    bug: 矩阵上下文覆盖问题 #5881

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/modelCreate/ModelCreate.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/modelCreate/ModelCreate.kt
@@ -121,9 +121,6 @@ class ModelCreate @Autowired constructor(
         var stageIndex = 1
         val stageId = VMUtils.genStageId(stageIndex++)
         val stage1 = Stage(listOf(triggerContainer), id = stageId, name = stageId)
-        stageList.add(stage1)
-
-        val jobIdCheckList = mutableListOf<String?>()
 
         // 其他的stage
         yaml.stages.forEach { stage ->
@@ -135,7 +132,6 @@ class ModelCreate @Autowired constructor(
                     // stream的stage标号从1开始，后续都加1
                     stageIndex = stageIndex++,
                     resources = yaml.resource,
-                    jobIdCheckList = jobIdCheckList,
                     pipeline = pipeline
                 )
             )
@@ -159,7 +155,6 @@ class ModelCreate @Autowired constructor(
                     stageIndex = stageIndex,
                     finalStage = true,
                     resources = yaml.resource,
-                    jobIdCheckList = jobIdCheckList,
                     pipeline = pipeline
                 )
             )
