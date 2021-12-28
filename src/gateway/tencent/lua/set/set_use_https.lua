c8c55e48f90194e22ce7f417fcc7b8cdee7bef08
stubenhuang <stubenhuang@tencent.com>
2021-12-28 17:24:45 +0800

    --story=870808879 【蓝盾网关】让浏览器过来的请求自动跳转到https

--- src/gateway/tencent/lua/set/set_use_https.lua
+++ src/gateway/tencent/lua/set/set_use_https.lua
@@ -1,4 +1,6 @@
-if config.openHttps == 'true' and ngx.var.agent_type == 'brower' and ngx.var.http_x_forwarded_proto == 'http' then
+local in_http = ngx.var.http_x_forwarded_proto == 'http' or ngx.var.http_protocol ~= 'https:'
+
+if config.openHttps == 'true' and ngx.var.agent_type == 'brower' and in_http then
     return '1'
 else
     return '0'
