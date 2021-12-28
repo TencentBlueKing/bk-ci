fbbacd361e79ce8ed3863d596fcc17e08b170c61
fitzcao <cao150260819@126.com>
2021-12-28 11:51:11 +0800

    --story=871150567 Auto集群log鉴权方式采用Simple

--- src/backend/ci/ext/tencent/log/boot-log-tencent/build.gradle.kts
+++ src/backend/ci/ext/tencent/log/boot-log-tencent/build.gradle.kts
@@ -28,6 +28,7 @@
 dependencies {
     api(project(":ext:tencent:log:biz-log-tencent"))
     api(project(":core:log:biz-log-sample"))
+    api(project(":core:common:common-auth:common-auth-mock"))
 }
 
 
