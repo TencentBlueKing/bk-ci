3d5129be5c6547160e71d9097de6eb797fba5049
yongyiduan <yongyiduan@tencent.com>
2022-01-05 19:30:59 +0800

    --story=869382189 【触发器】支持监听分支/tag删除事件

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/utils/StreamTriggerMessageUtils.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/utils/StreamTriggerMessageUtils.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.webhook.enums.code.tgit.TGitObjectKind
+import com.tencent.devops.common.webhook.enums.code.tgit.TGitPushOperationKind
+import com.tencent.devops.common.webhook.enums.code.tgit.TGitTagPushOperationKind
 import com.tencent.devops.common.webhook.pojo.code.git.GitPushEvent
 import com.tencent.devops.common.webhook.pojo.code.git.GitTagPushEvent
 import com.tencent.devops.stream.pojo.GitRequestEvent
@@ -48,15 +50,25 @@ class StreamTriggerMessageUtils @Autowired constructor(
                 "[${event.branch}] Commit [${event.commitId.subSequence(0, 7)}] schedule"
             }
             TGitObjectKind.PUSH.value -> {
-
-                val eventMap = try {
-                    objectMapper.readValue<GitPushEvent>(event.event)
-                } catch (e: Exception) {
-                    logger.error("event as GitTagPushEvent error ${e.message}")
-                    null
+                if (event.operationKind == TGitPushOperationKind.DELETE.value) {
+                    "[${event.branch}] branch [${event.commitId}] delete by ${event.userId}"
+                } else {
+                    val eventMap = try {
+                        objectMapper.readValue<GitPushEvent>(event.event)
+                    } catch (e: Exception) {
+                        logger.error("event as GitTagPushEvent error ${e.message}")
+                        null
+                    }
+                    if (eventMap?.isCreateBranch() == true) {
+                        "Branch [${event.branch}] added by ${event.userId}"
+                    } else {
+                        "[${event.branch}] Commit [${event.commitId.subSequence(0, 7)}] pushed by ${event.userId}"
+                    }
                 }
-                if (eventMap?.isCreateBranch() == true) {
-                    "Branch [${event.branch}] added by ${event.userId}"
+            }
+            TGitObjectKind.TAG_PUSH.value -> {
+                if (event.operationKind == TGitTagPushOperationKind.DELETE.value) {
+                    "[${event.branch}] tag [${event.commitId}] delete by ${event.userId}"
                 } else {
                     "[${event.branch}] Commit [${event.commitId.subSequence(0, 7)}] pushed by ${event.userId}"
                 }
