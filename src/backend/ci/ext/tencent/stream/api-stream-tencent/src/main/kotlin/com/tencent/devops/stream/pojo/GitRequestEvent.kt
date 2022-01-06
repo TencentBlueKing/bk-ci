8e77aa760b1d08ea3d4d8134d960f117997c5a3d
yongyiduan <yongyiduan@tencent.com>
2022-01-06 16:30:16 +0800

    --story=869382189 【触发器】支持监听分支/tag删除事件

--- src/backend/ci/ext/tencent/stream/api-stream-tencent/src/main/kotlin/com/tencent/devops/stream/pojo/GitRequestEvent.kt
+++ src/backend/ci/ext/tencent/stream/api-stream-tencent/src/main/kotlin/com/tencent/devops/stream/pojo/GitRequestEvent.kt
@@ -24,7 +24,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.tencent.devops.stream.pojo
 
 import com.tencent.devops.common.webhook.enums.code.tgit.TGitObjectKind
