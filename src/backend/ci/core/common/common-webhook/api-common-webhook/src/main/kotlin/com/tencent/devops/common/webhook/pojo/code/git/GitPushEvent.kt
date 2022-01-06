89913f3b70924647c56f2d1e1c940e2a882a2298
yongyiduan <yongyiduan@tencent.com>
2022-01-06 11:09:47 +0800

    【触发器】支持监听分支/tag删除事件的core部分改动 #5907

--- src/backend/ci/core/common/common-webhook/api-common-webhook/src/main/kotlin/com/tencent/devops/common/webhook/pojo/code/git/GitPushEvent.kt
+++ src/backend/ci/core/common/common-webhook/api-common-webhook/src/main/kotlin/com/tencent/devops/common/webhook/pojo/code/git/GitPushEvent.kt
@@ -67,6 +67,10 @@ data class GitPushEvent(
     }
 
     fun isCreateBranch(): Boolean {
+        // 工蜂web端创建分支
+        if (action_kind == TGitPushActionKind.CREATE_BRANCH.value) {
+            return true
+        }
         // 发送到工蜂的客户端创建
         if (action_kind == TGitPushActionKind.CLIENT_PUSH.value &&
             operation_kind == TGitPushOperationKind.CREAT.value &&
