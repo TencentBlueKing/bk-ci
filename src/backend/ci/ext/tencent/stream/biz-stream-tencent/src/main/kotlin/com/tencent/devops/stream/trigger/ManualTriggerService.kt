ad0985b2c9a6cc3c873da9a15becc0097a6259c3
mingshewhe <mingshewhe@tencent.com>
2022-01-09 19:13:57 +0800

    --story=871093371 【OPENAPI】触发流水线执行API 优化

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/ManualTriggerService.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/ManualTriggerService.kt
@@ -37,6 +37,7 @@ import com.tencent.devops.common.ci.v2.utils.ScriptYmlUtils
 import com.tencent.devops.common.ci.v2.utils.YamlCommonUtils
 import com.tencent.devops.common.ci.yaml.CIBuildYaml
 import com.tencent.devops.common.pipeline.enums.BuildStatus
+import com.tencent.devops.common.webhook.enums.code.tgit.TGitObjectKind.Companion.OBJECT_KIND_OPENAPI
 import com.tencent.devops.common.webhook.pojo.code.git.GitEvent
 import com.tencent.devops.process.pojo.BuildId
 import com.tencent.devops.stream.dao.GitPipelineResourceDao
@@ -270,7 +271,7 @@ class ManualTriggerService @Autowired constructor(
                 message = "Only events in the current repository [${triggerBuildReq.gitProjectId}] are supported"
             )
         }
-        return gitRequestEvent
+        return gitRequestEvent.copy(objectKind = OBJECT_KIND_OPENAPI)
     }
 
     fun handleTrigger(
