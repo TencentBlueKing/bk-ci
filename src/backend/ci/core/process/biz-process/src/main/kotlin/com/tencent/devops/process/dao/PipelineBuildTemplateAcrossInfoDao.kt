10713b522e7f9e441e961b6845305f579e7d4b59
ruotiantang <ruotiantang@tencent.com>
2022-01-13 20:53:50 +0800

    feat: 支持凭证跨项目使用 #5988

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineBuildTemplateAcrossInfoDao.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineBuildTemplateAcrossInfoDao.kt
@@ -55,36 +55,37 @@ class PipelineBuildTemplateAcrossInfoDao {
         userId: String,
         templateAcrossInfos: List<BuildTemplateAcrossInfo>
     ) {
-        with(TPipelineBuildTemplateAcrossInfo.T_PIPELINE_BUILD_TEMPLATE_ACROSS_INFO) {
-            dslContext.batch(
-                templateAcrossInfos.map { info ->
-                    with(TPipelineBuildTemplateAcrossInfo.T_PIPELINE_BUILD_TEMPLATE_ACROSS_INFO) {
-                        dslContext.insertInto(
-                            this,
-                            TEMPLATE_ID,
-                            PROJECT_ID,
-                            PIPELINE_ID,
-                            BUILD_ID,
-                            TEMPLATE_TYPE,
-                            TEMPLATE_INSTANCE_IDS,
-                            TARGET_PROJECT_ID,
-                            CREATE_TIME,
-                            CREATOR
-                        ).values(
-                            info.templateId,
-                            projectId,
-                            pipelineId,
-                            buildId,
-                            info.templateType.name,
-                            JsonUtil.toJson(info.templateInstancesIds),
-                            info.targetProjectId,
-                            LocalDateTime.now(),
-                            userId
-                        )
-                    }
-                }
-            ).execute()
+        if (templateAcrossInfos.isEmpty()) {
+            return
         }
+        dslContext.batch(
+            templateAcrossInfos.map { info ->
+                with(TPipelineBuildTemplateAcrossInfo.T_PIPELINE_BUILD_TEMPLATE_ACROSS_INFO) {
+                    dslContext.insertInto(
+                        this,
+                        TEMPLATE_ID,
+                        PROJECT_ID,
+                        PIPELINE_ID,
+                        BUILD_ID,
+                        TEMPLATE_TYPE,
+                        TEMPLATE_INSTANCE_IDS,
+                        TARGET_PROJECT_ID,
+                        CREATE_TIME,
+                        CREATOR
+                    ).values(
+                        info.templateId,
+                        projectId,
+                        pipelineId,
+                        buildId,
+                        info.templateType.name,
+                        JsonUtil.toJson(info.templateInstancesIds),
+                        info.targetProjectId,
+                        LocalDateTime.now(),
+                        userId
+                    )
+                }
+            }
+        ).execute()
     }
 
     fun get(
