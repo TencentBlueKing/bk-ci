e11ab7b0b69128d251a375515a7ba60671dbe11a
royalhuang <royalhuang@tencent.com>
2022-01-18 18:24:19 +0800

    feat: 矩阵输出上下文支持数组访问 #5964 增加矩阵内stepId声明

--- src/backend/ci/core/common/common-pipeline/src/test/kotlin/com/tencent/devops/common/pipeline/pojo/StagePauseCheckTest.kt
+++ src/backend/ci/core/common/common-pipeline/src/test/kotlin/com/tencent/devops/common/pipeline/pojo/StagePauseCheckTest.kt
@@ -1,22 +1,6 @@
 package com.tencent.devops.common.pipeline.pojo
 
-import com.tencent.devops.common.api.util.JsonUtil
-import com.tencent.devops.common.api.util.YamlUtil
-import com.tencent.devops.common.pipeline.Model
-import com.tencent.devops.common.pipeline.container.NormalContainer
-import com.tencent.devops.common.pipeline.container.Stage
-import com.tencent.devops.common.pipeline.container.TriggerContainer
-import com.tencent.devops.common.pipeline.container.VMBuildContainer
-import com.tencent.devops.common.pipeline.enums.BuildScriptType
-import com.tencent.devops.common.pipeline.enums.BuildStatus
-import com.tencent.devops.common.pipeline.enums.VMBaseOS
-import com.tencent.devops.common.pipeline.option.MatrixControlOption
-import com.tencent.devops.common.pipeline.pojo.element.agent.LinuxScriptElement
 import com.tencent.devops.common.pipeline.pojo.element.atom.ManualReviewParam
-import com.tencent.devops.common.pipeline.pojo.element.market.MarketBuildAtomElement
-import com.tencent.devops.common.pipeline.pojo.element.market.MarketBuildLessAtomElement
-import com.tencent.devops.common.pipeline.pojo.element.matrix.MatrixStatusElement
-import com.tencent.devops.common.pipeline.pojo.element.trigger.ManualTriggerElement
 import org.junit.Assert
 import org.junit.Test
 
