0cbbc30497f386a3277b526c46943c692b9fdc2d
mingshewhe <mingshewhe@tencent.com>
2022-01-18 11:34:41 +0800

    --story=869411721 【触发器】issue 事件触发

--- src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/triggerMatch/TriggerMatcher.kt
+++ src/backend/ci/ext/tencent/stream/biz-stream-tencent/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/triggerMatch/TriggerMatcher.kt
@@ -211,7 +211,7 @@ class TriggerMatcher @Autowired constructor(
         needMatch: Boolean = true
     ): Pair<Boolean, Map<String, String>> {
         with(context) {
-            logger.info("match and start params|triggerOn:$triggerOn|gitEvent:$gitEvent")
+            logger.info("match and start params|triggerOn:$triggerOn")
             val element = TriggerBuilder.buildCodeGitWebHookTriggerElement(
                 gitEvent = gitEvent,
                 triggerOn = triggerOn
