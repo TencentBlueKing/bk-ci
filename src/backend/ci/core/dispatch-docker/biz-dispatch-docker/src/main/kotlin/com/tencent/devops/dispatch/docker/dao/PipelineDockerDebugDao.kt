62578298484575f396d6e2eb84491fd3241b0557
sawyersong <sawyersong@tencent.com>
2022-01-19 15:47:36 +0800

    feat: 登录调试流程优化 #5997
    Signed-off-by: sawyersong <sawyersong@tencent.com>

--- src/backend/ci/core/dispatch-docker/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/docker/dao/PipelineDockerDebugDao.kt
+++ src/backend/ci/core/dispatch-docker/biz-dispatch-docker/src/main/kotlin/com/tencent/devops/dispatch/docker/dao/PipelineDockerDebugDao.kt
@@ -55,7 +55,6 @@ class PipelineDockerDebugDao {
         imageName: String,
         hostTag: String,
         containerId: String,
-        wsUrl: String = "",
         buildEnv: String,
         registryUser: String?,
         registryPwd: String?,
@@ -75,7 +74,6 @@ class PipelineDockerDebugDao {
                 IMAGE_NAME,
                 HOST_TAG,
                 CONTAINER_ID,
-                WS_URL,
                 CREATED_TIME,
                 UPDATED_TIME,
                 BUILD_ENV,
@@ -95,7 +93,6 @@ class PipelineDockerDebugDao {
                     imageName,
                     hostTag,
                     containerId,
-                    wsUrl,
                     now,
                     now,
                     buildEnv,
@@ -112,7 +109,6 @@ class PipelineDockerDebugDao {
                 .set(IMAGE_NAME, imageName)
                 .set(HOST_TAG, hostTag)
                 .set(CONTAINER_ID, containerId)
-                .set(WS_URL, wsUrl)
                 .set(UPDATED_TIME, now)
                 .set(BUILD_ENV, buildEnv)
                 .set(REGISTRY_USER, registryUser)
