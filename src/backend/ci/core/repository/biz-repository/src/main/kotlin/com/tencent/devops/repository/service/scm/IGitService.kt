d824b793f5df29b2fe475462ae32f868a3005b73
mingshewhe <mingshewhe@tencent.com>
2022-01-20 11:04:17 +0800

    feat: 新增工蜂issue事件 #5940

--- src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/scm/IGitService.kt
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/scm/IGitService.kt
@@ -201,4 +201,10 @@ interface IGitService {
         page: Int,
         pageSize: Int
     ): List<ChangeFileInfo>
+
+    fun getGitProjectInfo(
+        id: String,
+        token: String,
+        tokenType: TokenTypeEnum
+    ): Result<GitProjectInfo?>
 }
