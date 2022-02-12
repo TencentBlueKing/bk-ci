15a36a6c40d2b9f386c15e0897ec634860390eab
jamikxu <jamikxu@tencent.com>
2022-02-12 14:22:12 +0800

    需要人工审批的质量红线，告警通知时除“审批人”之外，希望也能通知“流水线触发者” #3741

--- src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleCheckService.kt
+++ src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleCheckService.kt
@@ -708,6 +708,7 @@ class QualityRuleCheckService @Autowired constructor(
         val notifyUserSet = auditNotifyUserList.toMutableSet()
         val triggerUserId = runtimeVariable?.get(PIPELINE_START_WEBHOOK_USER_ID)
             ?: runtimeVariable?.get(PIPELINE_START_USER_ID) ?: ""
+        notifyUserSet.add(triggerUserId)
 
         val messageResult = StringBuilder()
         val emailResult = StringBuilder()
@@ -734,7 +735,7 @@ class QualityRuleCheckService @Autowired constructor(
                 "buildNo" to buildNo
             ),
             bodyParams = mapOf(
-                "title" to "$pipelineName(#$buildNo)被拦截，待审核(审核人$notifyUserSet)",
+                "title" to "$pipelineName(#$buildNo)被拦截，待审核(审核人$auditNotifyUserList)",
                 "projectName" to projectName,
                 "cc" to triggerUserId,
                 "time" to time,
