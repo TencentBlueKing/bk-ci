95df2a55cb30c98b44def307e9d79b501b19700e
stubenhuang <stubenhuang@tencent.com>
2022-02-22 20:32:00 +0800

    --story=870736007 蓝盾容器化

--- helm-charts/ext/ci/build_chart.py
+++ helm-charts/ext/ci/build_chart.py
@@ -1,11 +1,13 @@
 import os
 import yaml
+import base64
 
 output_value_yaml = './values.yaml'
 default_value_yaml = './build/values.yaml'
 config_path = os.environ.get("config_path")
 spring_profile = os.environ.get("spring_profile")
-template_parent = './templates/configmap/tpl/'
+configmap_template_parent = './templates/configmap/tpl/'
+secret_template_parent = './templates/secret/tpl/'
 
 # 生成value.yaml
 image_gateway_tag = '0.0'
@@ -18,12 +20,11 @@ for line in open(default_value_yaml, 'r'):
 value_file.flush()
 value_file.close()
 
-# 生成 tpl
 config_server = config_path+"/config-server/"
-# common config
+# 生成 tpl
 common_yaml = yaml.safe_load(open(config_server+'application.yml', 'r'))
 common_yaml.update(yaml.safe_load(open(config_server+'application-'+spring_profile+'.yml', 'r')))
-common_tpl = open(template_parent+'_common.tpl', 'a+')
+common_tpl = open(configmap_template_parent+'_common.tpl', 'a+')
 common_tpl.write('{{- define "bkci.common.yaml" -}}')
 yaml.dump(common_yaml, common_tpl)
 common_tpl.write('{{- end -}}')
@@ -34,7 +35,7 @@ for c_path in os.listdir(config_server):
     service_path = config_server+c_path
     if os.path.isdir(service_path):
         service_name = c_path
-        service_tpl = open(template_parent+'_'+service_name+'.tpl', 'w')
+        service_tpl = open(configmap_template_parent+'_'+service_name+'.tpl', 'w')
         service_yaml = {}
         if os.path.isfile(service_path+'/application.yml'):
             service_yaml.update(yaml.safe_load(open(service_path+'/application.yml')))
@@ -45,8 +46,28 @@ for c_path in os.listdir(config_server):
         service_tpl.write('{{- end -}}')
         service_tpl.flush()
         service_tpl.close()
+
+# 生成lambda的es的certificate
+lambda_certificate_path = config_server+'lambda/'+spring_profile
+if os.path.isdir(lambda_certificate_path):
+    keystore64 = ''
+    truststore64 = ''
+    with open(lambda_certificate_path+'/keystore.jks', 'rb') as ksObj:
+        keystore64 = base64.b64encode(ksObj.read())
+        print(keystore64)
+    with open(lambda_certificate_path+'/truststore.jks', 'rb') as tsObj:
+        truststore64 = base64.b64encode(tsObj.read())
+        print(truststore64)
+    lambda_tpl = open(secret_template_parent+'_lambda.tpl', 'w')
+    lambda_tpl.write('{{- define "secret.lambda.yaml" -}}\n')
+    lambda_tpl.write('keystore.jks: "'+keystore64+'"\n')
+    lambda_tpl.write('truststore.jks: "'+truststore64+'"\n')
+    lambda_tpl.write('{{- end -}}')
+    lambda_tpl.flush()
+
+
 # gateway config(伪装)
-gateway_tpl = open(template_parent+'_gateway.tpl', 'w')
+gateway_tpl = open(configmap_template_parent+'_gateway.tpl', 'w')
 gateway_tpl.write('{{- define "bkci.gateway.yaml" -}}\n')
 gateway_tpl.write('{{- end -}}')
 gateway_tpl.flush()
