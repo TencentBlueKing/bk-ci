fd9ab7c98bcf1643e2a3deb122ac2817366c07b7
fitzcao <fitzcao@tencent.com>
2022-03-01 21:38:24 +0800

    feat: 流水线长度限制支持255以内动态配置 #6229

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/config/PipelineCommonSettingConfig.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/config/PipelineCommonSettingConfig.kt
@@ -39,9 +39,9 @@ class PipelineCommonSettingConfig {
     @Value("\${pipeline.setting.common.stage.maxStageNum:20}")
     val maxStageNum: Int = 20
 
-    @Value("\${pipeline.setting.common.maxPipelineNameSize:20}")
+    @Value("\${pipeline.setting.common.maxPipelineNameSize:255}")
     val maxPipelineNameSize: Int = 255
 
-    @Value("\${pipeline.setting.common.maxPipelineDescSize:20}")
+    @Value("\${pipeline.setting.common.maxPipelineDescSize:255}")
     val maxPipelineDescSize: Int = 255
 }
