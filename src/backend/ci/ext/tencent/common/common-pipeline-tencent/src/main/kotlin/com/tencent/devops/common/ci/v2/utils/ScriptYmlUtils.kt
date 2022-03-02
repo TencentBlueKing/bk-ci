f783a1d1aad79f0bb6582043ca2fabb4cf61fea9
yongyiduan <yongyiduan@tencent.com>
2022-03-02 14:17:57 +0800

    --bug=97159485 【Bug】模板替换self-hosted:true后变为了false

--- src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/utils/ScriptYmlUtils.kt
+++ src/backend/ci/ext/tencent/common/common-pipeline-tencent/src/main/kotlin/com/tencent/devops/common/ci/v2/utils/ScriptYmlUtils.kt
@@ -388,7 +388,7 @@ object ScriptYmlUtils {
             }
             throw YamlFormatException(
                 "runs-on 中 ${e?.path[0]?.fieldName} 格式有误," +
-                    "应为 ${e?.targetType?.name} error message:${e.message}"
+                    "应为 ${e?.targetType?.name}, error message:${e.message}"
             )
         }
     }
