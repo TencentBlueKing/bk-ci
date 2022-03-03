fe4c3469ef840fcf7ac97cb0d47ec02b0accad7d
v_yjieliang <v_yjieliang@tencent.com>
2022-03-03 14:23:01 +0800

    【微扩展】环境变量bug fix #5023

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreEnvVarServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/impl/StoreEnvVarServiceImpl.kt
@@ -84,6 +84,16 @@ class StoreEnvVarServiceImpl @Autowired constructor(
         if (!storeMemberDao.isStoreMember(dslContext, userId, storeCode, storeType)) {
             return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PERMISSION_DENIED)
         }
+        if (storeEnvVarDao.querySameNameVariable(
+                dslContext = dslContext,
+                userId = userId,
+                storeType = storeType,
+                storeCode = storeCode,
+                scope = storeEnvVarRequest.scope,
+                varName = storeEnvVarRequest.varName
+        )) {
+            return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PARAMETER_IS_EXIST)
+        }
         val lockKey = "$storeCode:$storeType:${storeEnvVarRequest.varName}"
         val lock = RedisLock(redisOperation, lockKey, 60)
         try {
@@ -93,6 +103,7 @@ class StoreEnvVarServiceImpl @Autowired constructor(
                     dslContext = dslContext,
                     storeType = storeType,
                     storeCode = storeCode,
+                    scope = storeEnvVarRequest.scope,
                     varName = storeEnvVarRequest.varName
                 ) ?: 0
                 storeEnvVarDao.create(
@@ -111,10 +122,103 @@ class StoreEnvVarServiceImpl @Autowired constructor(
         return Result(true)
     }
 
+    override fun update(userId: String, variableId: String, storeEnvVarRequest: StoreEnvVarRequest): Result<Boolean> {
+        logger.info("storeEnvVar update userId:$userId,storeEnvVarRequest:$storeEnvVarRequest")
+        val storeCode = storeEnvVarRequest.storeCode
+        val storeType = StoreTypeEnum.valueOf(storeEnvVarRequest.storeType).type.toByte()
+        if (!storeMemberDao.isStoreMember(dslContext, userId, storeCode, storeType)) {
+            return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PERMISSION_DENIED)
+        }
+        // 查询该环境变量在数据库中最大的版本的一行记录
+        val envVarOne = storeEnvVarDao.getEnvVarOne(
+            dslContext = dslContext,
+            storeType = storeType,
+            storeCode = storeCode,
+            variableId = variableId
+        )
+        val maxVersionData = StoreEnvVarInfo(
+            id = envVarOne?.get(KEY_ID) as String,
+            storeCode = envVarOne[KEY_STORE_CODE] as String,
+            storeType = StoreTypeEnum.getStoreType((envVarOne[KEY_STORE_TYPE] as Byte).toInt()),
+            varName = envVarOne[KEY_VAR_NAME] as String,
+            varValue = envVarOne[KEY_VAR_VALUE] as String,
+            varDesc = envVarOne[KEY_VAR_DESC] as? String,
+            encryptFlag = envVarOne[KEY_ENCRYPT_FLAG] as Boolean,
+            scope = envVarOne[KEY_SCOPE] as String,
+            version = envVarOne[KEY_VERSION] as Int,
+            creator = envVarOne[KEY_CREATOR] as String,
+            modifier = envVarOne[KEY_MODIFIER] as String,
+            createTime = DateTimeUtil.toDateTime(envVarOne[KEY_CREATE_TIME] as LocalDateTime),
+            updateTime = DateTimeUtil.toDateTime(envVarOne[KEY_UPDATE_TIME] as LocalDateTime)
+        )
+        if (storeEnvVarRequest.scope != maxVersionData.scope && storeEnvVarDao.querySameNameVariable(
+                dslContext = dslContext,
+                userId = userId,
+                storeType = storeType,
+                storeCode = storeCode,
+                scope = storeEnvVarRequest.scope,
+                varName = storeEnvVarRequest.varName
+            )) {
+            return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PARAMETER_IS_EXIST)
+        }
+        val lockKey = "$storeCode:$storeType:${storeEnvVarRequest.varName}"
+        val lock = RedisLock(redisOperation, lockKey, 60)
+        try {
+            if (lock.tryLock()) {
+                // 判断是否修改变量环境或变量名
+                if (storeEnvVarRequest.scope != maxVersionData.scope || storeEnvVarRequest.varName != maxVersionData.varName) {
+                    if (storeEnvVarDao.updateVariableEnvironment(
+                            dslContext = dslContext,
+                            userId = userId,
+                            storeType = storeType,
+                            storeCode = storeCode,
+                            oldScope = maxVersionData.scope,
+                            scope = storeEnvVarRequest.scope,
+                            oldName = maxVersionData.varName,
+                            varName = storeEnvVarRequest.varName
+                        )
+                    ) {
+                        return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.PARAMETER_IS_EXIST)
+                    }
+                }
+                // 如变量值变更，则添加新记录
+                if (storeEnvVarRequest.varValue != maxVersionData.varValue && storeEnvVarRequest.varValue != "******") {
+                    storeEnvVarDao.create(
+                        dslContext = dslContext,
+                        userId = userId,
+                        version = maxVersionData.version + 1,
+                        storeEnvVarRequest = storeEnvVarRequest
+                    )
+                } else {
+                    // 判断变量值是否需要进行加密或解密
+                    val value = if (storeEnvVarRequest.encryptFlag != maxVersionData.encryptFlag) {
+                        if (storeEnvVarRequest.encryptFlag) AESUtil.encrypt(aesKey, maxVersionData.varValue) else AESUtil.decrypt(aesKey, maxVersionData.varValue)
+                    } else maxVersionData.varValue
+                    storeEnvVarDao.updateVariable(
+                        dslContext = dslContext,
+                        storeType = storeType,
+                        storeCode = storeCode,
+                        variableId = maxVersionData.id,
+                        varValue = value,
+                        varDesc = storeEnvVarRequest.varDesc ?: "",
+                        encryptFlag = storeEnvVarRequest.encryptFlag
+                    )
+                }
+            }
+        } catch (t: Throwable) {
+            logger.error("storeEnvVar update failed", t)
+            return MessageCodeUtil.generateResponseDataObject(CommonMessageCode.SYSTEM_ERROR)
+        } finally {
+            lock.unlock()
+        }
+        return Result(true)
+    }
+
     override fun delete(
         userId: String,
         storeType: String,
         storeCode: String,
+        scope: String,
         varNames: String
     ): Result<Boolean> {
         logger.info("storeEnvVar delete userId:$userId,storeType:$storeType,storeCode:$storeCode,varNames:$varNames")
@@ -132,6 +236,7 @@ class StoreEnvVarServiceImpl @Autowired constructor(
             dslContext = dslContext,
             storeType = storeTypeObj.type.toByte(),
             storeCode = storeCode,
+            scope = scope,
             varNameList = varNames.split(",")
         )
         return Result(true)
@@ -201,6 +306,7 @@ class StoreEnvVarServiceImpl @Autowired constructor(
         userId: String,
         storeType: String,
         storeCode: String,
+        scopes: String,
         varName: String
     ): Result<List<StoreEnvChangeLogInfo>?> {
         logger.info("storeEnvVar getEnvVarChangeLogList userId:$userId,storeType:$storeType,storeCode:$storeCode")
@@ -213,6 +319,7 @@ class StoreEnvVarServiceImpl @Autowired constructor(
             dslContext = dslContext,
             storeType = storeTypeObj.type.toByte(),
             storeCode = storeCode,
+            scopes = scopes,
             varName = varName
         )
         return if (storeEnvVarRecords != null && storeEnvVarRecords.size > 1) {
