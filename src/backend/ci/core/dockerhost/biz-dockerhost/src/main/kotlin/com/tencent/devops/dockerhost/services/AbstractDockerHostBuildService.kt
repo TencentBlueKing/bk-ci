f1e6dda771bb3734664d40dc1be6cae1c7f6df6c
sawyersong <sawyersong@tencent.com>
2022-03-05 11:09:39 +0800

    feat: 拉取代码加速缓存方案优化 #6241
    Signed-off-by: sawyersong <sawyersong@tencent.com>

--- src/backend/ci/core/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/services/AbstractDockerHostBuildService.kt
+++ src/backend/ci/core/dockerhost/biz-dockerhost/src/main/kotlin/com/tencent/devops/dockerhost/services/AbstractDockerHostBuildService.kt
@@ -248,13 +248,13 @@ abstract class AbstractDockerHostBuildService constructor(
 
             // 针对overlayfs白名单项目清理工作空间
             if (qpcGitProjectList != null && qpcGitProjectList.isNotEmpty()) {
-                val upperDir = "${getWorkspace(pipelineId, vmSeqId, poolNo, dockerHostConfig.hostPathWorkspace!!)}upper"
-                val workDir = "${getWorkspace(pipelineId, vmSeqId, poolNo, dockerHostConfig.hostPathWorkspace!!)}work"
+                val upperDir = "${getWorkspace(buildId, vmSeqId, poolNo, dockerHostConfig.hostPathWorkspace!!)}upper"
+                val workDir = "${getWorkspace(buildId, vmSeqId, poolNo, dockerHostConfig.hostPathWorkspace!!)}work"
                 FileUtils.deleteQuietly(File(upperDir))
                 FileUtils.deleteQuietly(File(workDir))
             }
         } catch (e: Throwable) {
-            logger.info("afterOverlayFs $pipelineId $vmSeqId $poolNo error: ${e.message}")
+            logger.info("afterOverlayFs $buildId $vmSeqId $poolNo error: ${e.message}")
         }
     }
 
