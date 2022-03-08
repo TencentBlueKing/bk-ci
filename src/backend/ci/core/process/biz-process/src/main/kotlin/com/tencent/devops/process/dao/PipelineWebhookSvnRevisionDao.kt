caeba693a9249aff34761ff1d90414117128fca0
v_jinleiliu <v_jinleiliu@tencent.com>
2022-03-08 17:50:55 +0800

    feat:社区版本支持svn触发事件 #5914
    数据表名修改

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineWebhookSvnRevisionDao.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineWebhookSvnRevisionDao.kt
@@ -1,6 +1,6 @@
 package com.tencent.devops.process.dao
 
-import com.tencent.devops.model.process.Tables.T_PIPELINE_WEBHOOK_SVN_REVISION
+import com.tencent.devops.model.process.Tables.T_PIPELINE_WEBHOOK_REVISION
 import org.jooq.DSLContext
 import org.jooq.Record2
 import org.jooq.Result
@@ -15,7 +15,7 @@ class PipelineWebhookSvnRevisionDao {
         projectName: String,
         revision: String
     ) {
-        with(T_PIPELINE_WEBHOOK_SVN_REVISION) {
+        with(T_PIPELINE_WEBHOOK_REVISION) {
             dslContext.insertInto(
                 this,
                 PROJECT_NAME,
@@ -40,7 +40,7 @@ class PipelineWebhookSvnRevisionDao {
         dslContext: DSLContext,
         projectName: List<String>
     ): Result<Record2<String, String>> {
-        return with(T_PIPELINE_WEBHOOK_SVN_REVISION) {
+        return with(T_PIPELINE_WEBHOOK_REVISION) {
             dslContext.select(PROJECT_NAME, REVISION).from(this).where(PROJECT_NAME.`in`(projectName)).fetch()
         }
     }
