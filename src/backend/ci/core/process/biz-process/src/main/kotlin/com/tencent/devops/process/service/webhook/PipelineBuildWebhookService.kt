7519768ffbbc9697554aa1c8ef3b6d383cc7c6b8
stubenhuang <stubenhuang@tencent.com>
2022-03-10 20:15:43 +0800

    --story=872605665 蓝盾SLA

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
@@ -47,7 +47,6 @@ import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeP4WebHookTrig
 import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeSVNWebHookTriggerElement
 import com.tencent.devops.common.pipeline.pojo.element.trigger.CodeTGitWebHookTriggerElement
 import com.tencent.devops.common.pipeline.pojo.element.trigger.WebHookTriggerElement
-import com.tencent.devops.common.service.prometheus.BkTimed
 import com.tencent.devops.common.webhook.pojo.code.git.GitEvent
 import com.tencent.devops.common.webhook.pojo.code.git.GitReviewEvent
 import com.tencent.devops.common.webhook.pojo.code.github.GithubCheckRunEvent
@@ -341,8 +340,7 @@ abstract class PipelineBuildWebhookService @Autowired constructor() {
         return canWebhookStartup
     }
 
-    @BkTimed
-    fun webhookTriggerPipelineBuild(
+    open fun webhookTriggerPipelineBuild(
         projectId: String,
         pipelineId: String,
         codeRepositoryType: String,
