aca8df7dbd6b40181688bd9ab55dceafbc307522
v_jinleiliu <v_jinleiliu@tencent.com>
2022-03-16 17:09:20 +0800

    feat:社区版本支持svn触发事件 #5914

--- src/backend/ci/core/process/biz-process-sample/src/main/kotlin/com/tencent/devops/process/config/PollSvnConfig.kt
+++ src/backend/ci/core/process/biz-process-sample/src/main/kotlin/com/tencent/devops/process/config/PollSvnConfig.kt
@@ -35,7 +35,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.SchedulingConfigurer
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.scheduling.config.ScheduledTaskRegistrar
-import java.util.*
+import java.util.Calendar
+import java.util.Date
 
 @Configuration
 class PollSvnConfig : SchedulingConfigurer {
