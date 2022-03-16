4e34d8c5fcb41b23b7d04b54286227d898d07b0c
v_jinleiliu <v_jinleiliu@tencent.com>
2022-03-16 16:17:45 +0800

    feat:社区版本支持svn触发事件 #5914
    修改rediskey

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/TriggerSvnService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/TriggerSvnService.kt
@@ -463,15 +463,10 @@ class TriggerSvnService(
 
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(TriggerSvnService::class.java.name)
-        private const val getRedisKey: String = "web_repository_svn_polling"
+        private const val getRedisKey: String = "repository_svn_polling"
         private const val getRedisLockKey: String = "repository_svn_polling_redis_lock"
         private const val THREE = 3L
     }
-
-    data class CommitMessage(
-        val commitTime: Long,
-        val paths: List<String>
-    )
 
     data class SvnRepoInfo(
         val url: String,
