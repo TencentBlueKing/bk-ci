e56aa87335707ff6704c5a3f19bde5eb1e3bdaf2
fitzcao <fitzcao@tencent.com>
2022-03-17 18:48:25 +0800

    hofitx: 20220317暂停空指针

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelinePauseBuildFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelinePauseBuildFacadeService.kt
@@ -156,6 +156,10 @@ class PipelinePauseBuildFacadeService(
         }
         val newInputData = ParameterUtils.getElementInput(newElement)
 
+        if (newInputData.isNullOrEmpty()) {
+            return isDiff
+        }
+
         // issues_6210 若原input为空,新input不为空。则直接返回有变化
         val oldInputData = ParameterUtils.getParamInputs(oldTask.taskParams) ?: emptyMap()
 
