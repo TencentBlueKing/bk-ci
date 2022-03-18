18796ba3ce2ca0433159a5547ede47832b5dcaa2
lockiechen <lockiechen@tencent.com>
2022-03-18 14:29:01 +0800

    refactor: pipeline 编排封装 issue #5592

--- src/frontend/bk-pipeline/src/StageCheckIcon.vue
+++ src/frontend/bk-pipeline/src/StageCheckIcon.vue
@@ -96,7 +96,7 @@
                             return 'quality-check'
                         case this.stageStatus === STATUS_MAP.SKIP:
                         case !this.stageStatus && this.isExecDetail:
-                        case stageCheck.status === undefined && this.isExecDetail:
+                        case stageCheck.status === undefined && this.isExecDetail && !this.stageStatus:
                             return stageCheck.manualTrigger || this.hasRuleId ? 'review-pause' : 'review-auto-gray'
                         case !!this.stageStatus:
                             return 'review-auto-pass'
