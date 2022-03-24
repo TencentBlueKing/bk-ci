9014e69195a1384c6179b4d3fbdf3d1fb6ec7714
sawyersong2 <wudiduomjimo@gmail.com>
2022-03-24 16:01:29 +0800

    镜像商店支持通用构建类型

--- src/backend/ci/ext/tencent/store/biz-store-image-tencent/src/main/kotlin/com/tencent/devops/store/service/image/ImageHistoryDataService.kt
+++ src/backend/ci/ext/tencent/store/biz-store-image-tencent/src/main/kotlin/com/tencent/devops/store/service/image/ImageHistoryDataService.kt
@@ -71,6 +71,9 @@ class ImageHistoryDataService @Autowired constructor(
             ImageAgentTypeEnum.PUBLIC_DEVCLOUD -> {
                 "devcloud/$value"
             }
+            ImageAgentTypeEnum.PUBLIC_BCS -> {
+                throw Exception("not support !")
+            }
         }
         realImageNameTag = realImageNameTag.removePrefix("/")
         // 拆分repoName与tag
