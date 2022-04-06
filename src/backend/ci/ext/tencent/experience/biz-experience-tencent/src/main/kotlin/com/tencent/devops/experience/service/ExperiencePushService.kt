7c18b480ec1ecc22f4eaccd14e700f190f8c3121
stubenhuang <stubenhuang@tencent.com>
2022-04-06 18:09:31 +0800

    --story=871145433 体验页改版

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperiencePushService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperiencePushService.kt
@@ -262,7 +262,7 @@ class ExperiencePushService @Autowired constructor(
         ) ?: return Result("该用户未绑定设备", false)
 
         val platform = userTokenRecord.platform
-        if (platform != userTokenRecord.platform) {
+        if (platform != appNotifyMessage.platform) {
             logger.warn("pushMessage , platform is not same")
             return Result("绑定平台与包平台不一致", false)
         }
