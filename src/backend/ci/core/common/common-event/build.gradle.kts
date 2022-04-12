d18aab8106031d05c55973437d78352df13facfe
royalhuang <royalhuang@tencent.com>
2022-04-12 17:36:29 +0800

    perf: 日志模块缓冲MQ队列机制优化 #3089

--- src/backend/ci/core/common/common-event/build.gradle.kts
+++ src/backend/ci/core/common/common-event/build.gradle.kts
@@ -29,5 +29,10 @@ dependencies {
     api(project(":core:common:common-service"))
     api("org.apache.pulsar:pulsar-client")
     api("org.springframework.cloud:spring-cloud-stream")
+    api("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+    api("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+    {
+        exclude("org.bouncycastle", "bcprov-ext-jdk15on")
+    }
     compileOnly("org.springframework.boot:spring-boot-starter-amqp")
 }
