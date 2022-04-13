f3dc936c114434c7e043b76e5990844856710743
carlyin <449036249@qq.com>
2022-04-13 10:13:27 +0800

    feat:【研发商店】支持注册流水线使用到的工具/平台服务 - 后台 #6462

---
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/common/StoreDockingPlatformDao.kt
@@ -0,0 +1,185 @@
+/*
+ * Tencent is pleased to support the open source community by making BK-CI 蓝鲸持续集成平台 available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * BK-CI 蓝鲸持续集成平台 is licensed under the MIT license.
+ *
+ * A copy of the MIT License is included in this file.
+ *
+ *
+ * Terms of the MIT License:
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.tencent.devops.store.dao.common
+
+import com.tencent.devops.common.api.util.DateTimeUtil
+import com.tencent.devops.common.api.util.UUIDUtil
+import com.tencent.devops.model.store.tables.TStoreDockingPlatform
+import com.tencent.devops.model.store.tables.records.TStoreDockingPlatformRecord
+import com.tencent.devops.store.pojo.common.StoreDockingPlatformInfo
+import com.tencent.devops.store.pojo.common.StoreDockingPlatformRequest
+import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
+import org.jooq.Condition
+import org.jooq.DSLContext
+import org.jooq.Result
+import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
+
+@Repository
+class StoreDockingPlatformDao {
+
+    fun add(
+        dslContext: DSLContext,
+        userId: String,
+        storeDockingPlatformRequest: StoreDockingPlatformRequest
+    ) {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            dslContext.insertInto(
+                this,
+                ID,
+                STORE_CODE,
+                STORE_TYPE,
+                PLATFORM_CODE,
+                PLATFORM_NAME,
+                CREATOR,
+                MODIFIER
+            )
+                .values(
+                    UUIDUtil.generate(),
+                    storeDockingPlatformRequest.storeCode,
+                    StoreTypeEnum.valueOf(storeDockingPlatformRequest.storeType).type.toByte(),
+                    storeDockingPlatformRequest.platformCode,
+                    storeDockingPlatformRequest.platformName,
+                    userId,
+                    userId
+                ).execute()
+        }
+    }
+
+    fun update(
+        dslContext: DSLContext,
+        id: String,
+        userId: String,
+        storeDockingPlatformRequest: StoreDockingPlatformRequest
+    ) {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            dslContext.update(this)
+                .set(PLATFORM_CODE, storeDockingPlatformRequest.platformCode)
+                .set(PLATFORM_NAME, storeDockingPlatformRequest.platformName)
+                .set(UPDATE_TIME, LocalDateTime.now())
+                .set(MODIFIER, userId)
+                .where(ID.eq(id))
+                .execute()
+        }
+    }
+
+    fun delete(dslContext: DSLContext, id: String) {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            dslContext.deleteFrom(this)
+                .where(ID.eq(id))
+                .execute()
+        }
+    }
+
+    fun countByName(dslContext: DSLContext, platformName: String, storeType: Byte): Int {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            return dslContext.selectCount().from(this)
+                .where(PLATFORM_NAME.eq(platformName).and(STORE_TYPE.eq(storeType)))
+                .fetchOne(0, Int::class.java)!!
+        }
+    }
+
+    fun countByCode(dslContext: DSLContext, platformCode: String, storeType: Byte): Int {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            return dslContext.selectCount().from(this)
+                .where(PLATFORM_CODE.eq(platformCode).and(STORE_TYPE.eq(storeType)))
+                .fetchOne(0, Int::class.java)!!
+        }
+    }
+
+    fun getStoreDockingPlatform(dslContext: DSLContext, id: String): TStoreDockingPlatformRecord? {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            return dslContext.selectFrom(this)
+                .where(ID.eq(id))
+                .fetchOne()
+        }
+    }
+
+    fun getStoreDockingPlatforms(
+        dslContext: DSLContext,
+        storeCode: String,
+        storeType: Byte,
+        platformName: String?,
+        page: Int,
+        pageSize: Int
+    ): Result<TStoreDockingPlatformRecord>? {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            val conditions = getStoreDockingPlatformsCondition(storeCode, storeType, platformName)
+            return dslContext
+                .selectFrom(this)
+                .where(conditions).orderBy(CREATE_TIME.desc())
+                .offset((page - 1) * pageSize).limit(pageSize)
+                .fetch()
+        }
+    }
+
+    fun getStoreDockingPlatformCount(
+        dslContext: DSLContext,
+        storeCode: String,
+        storeType: Byte,
+        platformName: String?
+    ): Long {
+        with(TStoreDockingPlatform.T_STORE_DOCKING_PLATFORM) {
+            val conditions = getStoreDockingPlatformsCondition(storeCode, storeType, platformName)
+            return dslContext
+                .selectFrom(this)
+                .where(conditions)
+                .fetchOne(0, Long::class.java)!!
+        }
+    }
+
+    private fun TStoreDockingPlatform.getStoreDockingPlatformsCondition(
+        storeCode: String,
+        storeType: Byte,
+        platformName: String?
+    ): MutableList<Condition> {
+        val conditions = mutableListOf<Condition>()
+        conditions.add(STORE_CODE.eq(storeCode))
+        conditions.add(STORE_TYPE.eq(storeType))
+        if (!platformName.isNullOrBlank()) {
+            conditions.add(PLATFORM_NAME.contains(platformName))
+        }
+        return conditions
+    }
+
+    fun convert(record: TStoreDockingPlatformRecord): StoreDockingPlatformInfo {
+        with(record) {
+            return StoreDockingPlatformInfo(
+                id = id,
+                storeCode = storeCode,
+                storeType = StoreTypeEnum.getStoreType(storeType.toInt()),
+                platformCode = platformCode,
+                platformName = platformName,
+                creator = creator,
+                modifier = modifier,
+                createTime = DateTimeUtil.toDateTime(createTime),
+                updateTime = DateTimeUtil.toDateTime(updateTime)
+            )
+        }
+    }
+}
