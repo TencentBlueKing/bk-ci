124dec144a600dbe4343ae553e5f4a8a76c150b6
stubenhuang <stubenhuang@tencent.com>
2022-04-18 18:33:48 +0800

    feat: 提供kubernetes部署方案 issue #4935

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/BkTag.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/BkTag.kt
@@ -1,22 +1,14 @@
 package com.tencent.devops.common.service
 
 import com.tencent.devops.common.service.utils.KubernetesUtils
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.ApplicationContext
-import org.springframework.stereotype.Component
 
-@Component
-class BkTag @Autowired constructor(
-    private val applicationContext: ApplicationContext
+class BkTag constructor(
+    private val consulTag: String
 ) {
-    @Value("\${spring.cloud.consul.discovery.tags:prod}")
-    private val tag: String = "prod"
-
     fun getTag(): String {
         if (KubernetesUtils.inContainer()) {
             return "kubernetes-" + KubernetesUtils.getNamespace()
         }
-        return tag
+        return consulTag
     }
 }
