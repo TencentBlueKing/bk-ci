9738774a3a30480616ec9650c39db96327da48c5
irwinsun <irwinsun@tencent.com>
2022-04-18 09:53:17 +0800

    fix: agent build_tmp 目录权限问题 #6545 完善用户不对时的提示

--- src/agent/src/pkg/util/command/command.go
+++ src/agent/src/pkg/util/command/command.go
@@ -87,7 +87,8 @@ func StartProcess(command string, args []string, workDir string, envMap map[stri
 	err := setUser(cmd, runUser)
 	if err != nil {
 		logs.Error("set user failed: ", err.Error())
-		return -1, errors.New("set user failed")
+		return -1, errors.New(
+			fmt.Sprintf("%s, Please check [devops.slave.user] in the {agent_dir}/agent.properties", err.Error()))
 	}
 
 	logs.Info("cmd.Path: ", cmd.Path)
