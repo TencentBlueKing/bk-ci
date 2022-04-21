d6e97848a4c78c95aebeb1a4ef7319a83ce0bf96
RJ <eazence@163.com>
2022-04-21 21:49:42 +0800

    format: 【编译加速】代码格式化 #6592

--- src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IServiceTurboController.kt
+++ src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IServiceTurboController.kt
@@ -15,7 +15,12 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import java.time.LocalDate
 
 
--- src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IUserTurboRecordController.kt
+++ src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IUserTurboRecordController.kt
@@ -13,7 +13,13 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Api(tags = ["TURBO_RECORD"], description = "编译加速记录接口")
 @RequestMapping("/user/turboRecord")
