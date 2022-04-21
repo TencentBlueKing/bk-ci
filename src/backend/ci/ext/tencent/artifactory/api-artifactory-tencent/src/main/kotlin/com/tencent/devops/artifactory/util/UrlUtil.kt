07a46fbb5a894823d75fd01f761caddf19894724
stubenhuang <stubenhuang@tencent.com>
2022-04-21 18:14:15 +0800

    --story=873934677 APP图标支持bkrepo的静态域名

--- src/backend/ci/ext/tencent/artifactory/api-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/util/UrlUtil.kt
+++ src/backend/ci/ext/tencent/artifactory/api-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/util/UrlUtil.kt
@@ -54,7 +54,7 @@ object UrlUtil {
             innerPhotoAddr.replace(
                 Regex("(dev|test|)staticfile.woa.com"),
                 "${HomeHostUtil.outerServerHost()}/bkrepo/api/external/generic/bkcdn/static"
-            )
+            ).let { if (it.contains("?v=")) it.split("?v=")[0] else it }
         } else {
             innerPhotoAddr
         }
