53843461137a6b02182a3fb7c7ce89516b60a674
RJ <eazence@163.com>
2022-04-22 15:21:04 +0800

    feat: 【编译加速】清理publish脚本 #6592

--- src/backend/turbo/buildSrc/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-// 适用于buildSrc的plugins
-pluginManagement {
-    repositories {
-        mavenLocal()
-        maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public")
-        maven(url = "https://mirrors.tencent.com/nexus/repository/gradle-plugins/")
-    }
-}
