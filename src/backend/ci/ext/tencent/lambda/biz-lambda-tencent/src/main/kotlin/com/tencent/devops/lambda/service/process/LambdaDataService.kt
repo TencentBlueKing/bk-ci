58552f05f2a0b31dec9b16237c3680c2c3cc749d
v_baiyunbai <v_baiyunbai@tencent.com>
2022-04-27 15:16:22 +0800

    --story=874058879 推送流水线构建历史数据 产物列表获取优化

--- src/backend/ci/ext/tencent/lambda/biz-lambda-tencent/src/main/kotlin/com/tencent/devops/lambda/service/process/LambdaDataService.kt
+++ src/backend/ci/ext/tencent/lambda/biz-lambda-tencent/src/main/kotlin/com/tencent/devops/lambda/service/process/LambdaDataService.kt
@@ -102,6 +102,7 @@ class LambdaDataService @Autowired constructor(
 ) {
 
     fun onBuildFinish(event: PipelineBuildFinishBroadCastEvent) {
+        Thread.sleep(2000)
         val history = lambdaPipelineBuildDao.getBuildHistory(
             dslContext = dslContext,
             projectId = event.projectId,
