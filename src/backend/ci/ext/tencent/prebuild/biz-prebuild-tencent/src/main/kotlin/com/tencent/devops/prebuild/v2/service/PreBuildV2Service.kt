fe3a45b18a8fe36e5194aa2c95fb5fa3bc0afd59
weijianguan <weijianguan@tencent.com>
2022-05-20 16:43:17 +0800

    --story=870682313 PreCI YAML与Stream一致，也能支持模板

--- src/backend/ci/ext/tencent/prebuild/biz-prebuild-tencent/src/main/kotlin/com/tencent/devops/prebuild/v2/service/PreBuildV2Service.kt
+++ src/backend/ci/ext/tencent/prebuild/biz-prebuild-tencent/src/main/kotlin/com/tencent/devops/prebuild/v2/service/PreBuildV2Service.kt
@@ -43,7 +43,7 @@ import com.tencent.devops.prebuild.service.CommonPreBuildService
 import com.tencent.devops.prebuild.v2.component.PreCIYAMLValidator
 import com.tencent.devops.process.api.service.ServiceBuildResource
 import com.tencent.devops.process.pojo.BuildId
-import com.tencent.devops.process.yaml.modelCreate.ModelCreate
+import com.tencent.devops.process.yaml.modelCreate.TXModelCreate
 import com.tencent.devops.process.yaml.modelCreate.inner.ModelCreateEvent
 import com.tencent.devops.process.yaml.modelCreate.inner.PipelineInfo
 import com.tencent.devops.process.yaml.v2.models.PreTemplateScriptBuildYaml
@@ -64,7 +64,7 @@ class PreBuildV2Service @Autowired constructor(
     private val client: Client,
     private val dslContext: DSLContext,
     private val prebuildProjectDao: PrebuildProjectDao,
-    private val modelCreate: ModelCreate
+    private val modelCreate: TXModelCreate
 ) : CommonPreBuildService(client, dslContext, prebuildProjectDao) {
     companion object {
         private val logger = LoggerFactory.getLogger(PreBuildV2Service::class.java)
