04d7a81bcc295eeaa62cca4f134ea11d08858197
sawyersong <sawyersong@tencent.com>
2022-05-26 20:05:04 +0800

    feat: 对接bcs构建资源 #6658 docker构建资源统一路由

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/type/BuildType.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/type/BuildType.kt
@@ -40,7 +40,7 @@ enum class BuildType(
     MACOS("蓝盾公共构建资源(NEW)", listOf(OS.MACOS), false, false, false),
     DOCKER("Docker公共构建机", listOf(OS.LINUX), true, true, true),
     IDC("公共：Docker on IDC CVM", listOf(OS.LINUX), true, false, false),
-    PUBLIC_DEVCLOUD("公共：Docker on DevCloud", listOf(OS.LINUX), true, false, false),
+    PUBLIC_DEVCLOUD("公共：Docker on DevCloud", listOf(OS.LINUX), false, false, false),
     PUBLIC_BCS("公共：Docker on Bcs", listOf(OS.LINUX), true, false, false),
     TSTACK("Windows构建", listOf(OS.WINDOWS), false, false, false), // tstack is deleted
     THIRD_PARTY_AGENT_ID("私有：单构建机", listOf(OS.MACOS, OS.LINUX, OS.WINDOWS), false, true, true),
