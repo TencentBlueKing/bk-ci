3ac862daf44b7e6a9fccd2d844793a0217de1768
Yuanruitao <1075097840@qq.com>
2022-05-30 14:37:34 +0800

    bug: fastjson 高危版本修复 #6866
    (cherry picked from commit a996f7c5659e9c12b5ba551782bd008239465bfb)

--- src/backend/codecc/core/defect/model-defect/build.gradle.kts
+++ src/backend/codecc/core/defect/model-defect/build.gradle.kts
@@ -2,6 +2,6 @@ dependencies {
     api(project(":core:common:common-db"))
     api("org.projectlombok:lombok")
     api("com.fasterxml.jackson.core:jackson-annotations")
-    api("com.alibaba:fastjson:1.2.76")
+    api("com.alibaba:fastjson")
     api(group = "org.json", name = "json", version = "20180130")
 }
