63d15cabae918dfdac6ff5e6d27ab35f23f07511
sawyersong <sawyersong@tencent.com>
2022-06-07 19:49:21 +0800

    feat: 对接bcs构建资源 #6658 cr修改

--- src/backend/ci/core/dispatch-bcs/biz-dispatch-bcs/src/main/kotlin/com/tencent/devops/dispatch/bcs/client/BcsJobClient.kt
+++ src/backend/ci/core/dispatch-bcs/biz-dispatch-bcs/src/main/kotlin/com/tencent/devops/dispatch/bcs/client/BcsJobClient.kt
@@ -42,7 +42,6 @@ import okhttp3.MediaType
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
--- src/backend/ci/core/dispatch-bcs/biz-dispatch-bcs/src/main/kotlin/com/tencent/devops/dispatch/bcs/client/BcsTaskClient.kt
+++ src/backend/ci/core/dispatch-bcs/biz-dispatch-bcs/src/main/kotlin/com/tencent/devops/dispatch/bcs/client/BcsTaskClient.kt
@@ -41,7 +41,6 @@ import com.tencent.devops.dispatch.bcs.pojo.bcs.isSuccess
 import com.tencent.devops.dispatch.bcs.pojo.bcs.resp.BcsTaskStatusResp
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.net.SocketTimeoutException
 
