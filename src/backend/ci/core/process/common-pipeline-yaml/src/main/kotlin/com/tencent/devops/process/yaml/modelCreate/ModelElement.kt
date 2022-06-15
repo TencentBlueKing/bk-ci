bf31a916214ab8833914a39ba7f1901dac96cd74
yongyiduan <yongyiduan@tencent.com>
2022-06-15 21:05:23 +0800

    【stream】插件执行失败后显示出“重试”按钮，又不可触发，点击后会一直转菊花 #6974

--- src/backend/ci/core/process/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/modelCreate/ModelElement.kt
+++ src/backend/ci/core/process/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/modelCreate/ModelElement.kt
@@ -84,7 +84,8 @@ class ModelElement @Autowired(required = false) constructor(
                         RunCondition.PRE_TASK_FAILED_ONLY
                     else -> RunCondition.CUSTOM_CONDITION_MATCH
                 },
-                customCondition = step.ifFiled
+                customCondition = step.ifFiled,
+                manualRetry = false
             )
 
             additionalOptions.enable = jobEnable && PathMatchUtils.isIncludePathMatch(step.ifModify, changeSet)
