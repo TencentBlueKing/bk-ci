b557eb07799d375a2ba898971b3a7fc762a16a65
yjieliang <yundantengteng@163.com>
2022-06-23 15:18:45 +0800

    feat:【数据统计】用户态数据统计- 总览 #6587

--- src/backend/ci/core/metrics/biz-metrics/src/main/kotlin/com/tencent/devops/metrics/dao/AtomDisplayConfigDao.kt
+++ src/backend/ci/core/metrics/biz-metrics/src/main/kotlin/com/tencent/devops/metrics/dao/AtomDisplayConfigDao.kt
@@ -113,7 +113,7 @@ class AtomDisplayConfigDao {
             val step = dslContext.select(ATOM_CODE, ATOM_NAME).from(this)
                 .where(conditions)
                 return step.groupBy(ATOM_CODE)
-                    .orderBy(TOTAL_EXECUTE_COUNT.desc())
+                    .orderBy(SUCCESS_RATE)
                     .limit((page - 1) * pageSize, pageSize)
                     .fetchInto(AtomBaseInfoDO::class.java)
         }
