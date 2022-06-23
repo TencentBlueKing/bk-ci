1936eb5f5c4f21ee7ecb49590e9e82c836be53bc
mingshewhe <mingshewhe@tencent.com>
2022-06-23 21:38:52 +0800

    feat: stream支持github #6799

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/config/StreamLoginConfig.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/config/StreamLoginConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-data class StreamLoginConfig(
+class StreamLoginConfig {
     @Value("\${login.github.redirectUrl:}")
     val githubRedirectUrl: String = ""
-)
+}
