ccc0d3387162fd23b3bd70226a229822993443b5
yjieliang <yundantengteng@163.com>
2022-06-28 17:18:36 +0800

    feat:【数据统计】用户态数据统计- 总览 #6587

--- src/backend/ci/core/metrics/api-metrics/src/main/kotlin/com/tencent/devops/metrics/pojo/message/TurboReportEvent.kt
+++ src/backend/ci/core/metrics/api-metrics/src/main/kotlin/com/tencent/devops/metrics/pojo/message/TurboReportEvent.kt
@@ -32,10 +32,21 @@ import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("Turbo上报消息")
 data class TurboReportEvent(
+    /**
+     * 统计日期，格式yyyy-MM-dd
+     */
     @ApiModelProperty("统计日期")
-    val statisticsDate: String,
+    val statisticsTime: String,
+
+    /**
+     * 项目ID
+     */
     @ApiModelProperty("项目ID")
     val projectId: String,
+
+    /**
+     * 编译加速节省时间，单位：秒 保留2位小数
+     */
     @ApiModelProperty("编译加速节省时间，单位：秒")
-    val turboSaveTime: Double
+    val turboSaveTime: Double,
 )
