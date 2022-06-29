18570f7113ee8fc87e1f611eaacfdeebb0d48181
RJ <eazence@163.com>
2022-06-29 14:38:18 +0800

    chore: 蓝盾度量需求-编译加速turbo数据上报-清理无效的版本定义 #6973

--- src/backend/turbo/buildSrc/src/main/kotlin/Versions.kt
+++ src/backend/turbo/buildSrc/src/main/kotlin/Versions.kt
@@ -5,10 +5,8 @@ object Versions {
     const val swaggerVersion = "1.6.2"
     const val guavaVersion = "29.0-jre"
     const val ciVersion = "1.5.0-RC.2-RELEASE"
-    const val springFeign = "3.1.2"
     const val commonIo = "2.7"
     const val jjwtVersion = "0.9.0"
     const val ciAuthVersion = "1.8.0-RC.3-RELEASE"
-    const val micrometerSpringVersion = "1.3.9"
     const val micrometerVersion = "1.6.6"
 }
