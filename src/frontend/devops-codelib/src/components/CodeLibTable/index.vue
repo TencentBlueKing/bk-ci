cf683dc7c1323b34767697d19d3084e3ff4be2d7
vhwweng <1010382269@qq.com>
2022-07-01 10:43:42 +0800

    perf: 列表页属性支持点击排序 issue #2946

--- src/frontend/devops-codelib/src/components/CodeLibTable/index.vue
+++ src/frontend/devops-codelib/src/components/CodeLibTable/index.vue
@@ -7,9 +7,9 @@
         v-bkloading="{ isLoading }"
     >
         <bk-table-column type="index" :label="$t('codelib.index')" align="center" width="60"></bk-table-column>
-        <bk-table-column :label="$t('codelib.aliasName')" prop="aliasName"></bk-table-column>
-        <bk-table-column :label="$t('codelib.address')" prop="url"></bk-table-column>
-        <bk-table-column :label="$t('codelib.type')" prop="type" :formatter="typeFormatter"></bk-table-column>
+        <bk-table-column :label="$t('codelib.aliasName')" sortable prop="aliasName"></bk-table-column>
+        <bk-table-column :label="$t('codelib.address')" sortable prop="url"></bk-table-column>
+        <bk-table-column :label="$t('codelib.type')" sortable prop="type" :formatter="typeFormatter"></bk-table-column>
         <bk-table-column :label="$t('codelib.authIdentity')">
             <template slot-scope="props">
                 <span>{{ props.row.authType }}@</span><!--
