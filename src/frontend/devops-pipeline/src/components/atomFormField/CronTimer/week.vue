3fa44c4f60a72e1ccb1b22bf46a485f3b2015cbd
lockiechen <lockiechen@tencent.com>
2022-07-04 21:01:17 +0800

    feat: 定时触发基础规则为空时，报错文案需优化 issue #3532

--- src/frontend/devops-pipeline/src/components/atomFormField/CronTimer/week.vue
+++ src/frontend/devops-pipeline/src/components/atomFormField/CronTimer/week.vue
@@ -12,7 +12,7 @@
                 </li>
             </ul>
         </div>
-        <p v-if="isEmtpyVal" class="bk-form-help is-danger">{{$t('cronWeekHint')}}</p>
+        <p v-if="isEmtpyVal" class="bk-form-help is-danger">{{$t('editPage.cronWeekHint')}}</p>
         <div class="cron-build-time">
             <span>触发于</span>
             <bk-input :disabled="disabled" :value="normalTime" @input="changeTimes" placeholder="多个触发时间可以用英文逗号分隔，比如09:00,15:30，为空的时候默认为00:00" />
