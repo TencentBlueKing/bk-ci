5ea2ad89c18375c94d927b6e504ba972e35e86c7
v_jinleiliu <v_jinleiliu@tencent.com>
2022-07-04 16:41:20 +0800

    feat:远程触发无法识别调起哪条流水线，且被调的流水线无法知道调用源在哪里 #6583

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineContainerService.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineContainerService.kt
@@ -941,7 +941,11 @@ class PipelineContainerService @Autowired constructor(
                 atomElement.executeCount = context.executeCount
                 buildLogPrinter.addLine(
                     buildId = context.buildId,
-                    message = "触发人(trigger user): ${context.triggerUser}, 执行人(start user): ${context.userId} ${printSourceIp(context.sourceIp)}",
+                    message = "触发人(trigger user): ${context.triggerUser}, 执行人(start user): ${context.userId} ${
+                        printSourceIp(
+                            context.sourceIp
+                        )
+                    }",
                     tag = context.firstTaskId,
                     jobId = container.id,
                     executeCount = context.executeCount
