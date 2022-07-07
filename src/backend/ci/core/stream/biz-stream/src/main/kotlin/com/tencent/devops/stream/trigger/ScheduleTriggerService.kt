7d7e6b952a5b666efc3afc8a152b006afc4a0b12
mingshewhe <mingshewhe@tencent.com>
2022-07-07 10:02:49 +0800

    bug: 【stream】定时触发获取YAML时，使用开启人身份获取 #7176

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/ScheduleTriggerService.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/ScheduleTriggerService.kt
@@ -101,7 +101,7 @@ class ScheduleTriggerService @Autowired constructor(
                 homepage = it.homePage
             )
         }
-        if (streamTriggerSetting == null || streamTriggerSetting?.enableCi == false) {
+        if (streamTriggerSetting == null || streamTriggerSetting.enableCi == false) {
             logger.warn("project ${streamTimerEvent.projectId} not enable ci no trigger schedule")
             return null
         }
@@ -109,7 +109,7 @@ class ScheduleTriggerService @Autowired constructor(
         val action = eventActionFactory.loadScheduleAction(
             setting = streamTriggerSetting,
             event = StreamScheduleEvent(
-                userId = streamTimerEvent.userId,
+                userId = streamTriggerSetting.enableUser,
                 gitProjectId = streamTimerEvent.gitProjectId.toString(),
                 projectCode = streamTimerEvent.projectId,
                 branch = buildBranch,
