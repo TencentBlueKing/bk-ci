e60c7f7a49279f46d5292bdefa78dc7884ab1053
yjieliang <yundantengteng@163.com>
2022-07-08 15:26:26 +0800

    --story=875980165 【op】工具平台注册时，支持上传logo

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/StoreLogoService.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/common/StoreLogoService.kt
@@ -47,6 +47,7 @@ interface StoreLogoService {
     fun uploadStoreLogo(
         userId: String,
         contentLength: Long,
+        rangeFlag: Boolean? = true,
         compressFlag: Boolean? = false,
         inputStream: InputStream,
         disposition: FormDataContentDisposition
