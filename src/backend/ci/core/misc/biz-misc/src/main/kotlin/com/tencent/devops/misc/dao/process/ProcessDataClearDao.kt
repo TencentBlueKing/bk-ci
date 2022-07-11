a9077f845f49e133ae5669d56895cad509f90d34
mingshewhe <mingshewhe@tencent.com>
2022-07-11 19:57:40 +0800

    feat: 自动清理T_PIPELINE_WEBHOOK_BUILD_PARAMETER表数据 #7223

--- src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/dao/process/ProcessDataClearDao.kt
+++ src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/dao/process/ProcessDataClearDao.kt
@@ -45,6 +45,7 @@ import com.tencent.devops.model.process.tables.TPipelineSetting
 import com.tencent.devops.model.process.tables.TPipelineSettingVersion
 import com.tencent.devops.model.process.tables.TPipelineTimer
 import com.tencent.devops.model.process.tables.TPipelineWebhook
+import com.tencent.devops.model.process.tables.TPipelineWebhookBuildParameter
 import com.tencent.devops.model.process.tables.TReport
 import com.tencent.devops.model.process.tables.TTemplatePipeline
 import org.jooq.DSLContext
@@ -116,6 +117,15 @@ class ProcessDataClearDao {
 
     fun deleteBuildHistoryByBuildId(dslContext: DSLContext, projectId: String, buildId: String) {
         with(TPipelineBuildHistory.T_PIPELINE_BUILD_HISTORY) {
+            dslContext.deleteFrom(this)
+                .where(PROJECT_ID.eq(projectId))
+                .and(BUILD_ID.eq(buildId))
+                .execute()
+        }
+    }
+
+    fun deleteBuildWebhookParameter(dslContext: DSLContext, projectId: String, buildId: String) {
+        with(TPipelineWebhookBuildParameter.T_PIPELINE_WEBHOOK_BUILD_PARAMETER) {
             dslContext.deleteFrom(this)
                 .where(PROJECT_ID.eq(projectId))
                 .and(BUILD_ID.eq(buildId))
--- src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/service/process/ProcessDataClearService.kt
+++ src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/service/process/ProcessDataClearService.kt
@@ -110,6 +110,7 @@ class ProcessDataClearService @Autowired constructor(
                 buildId = buildId
             )
             processDataClearDao.deletePipelineBuildTemplateAcrossInfo(context, projectId, buildId)
+            processDataClearDao.deleteBuildWebhookParameter(context, projectId, buildId)
             // 添加删除记录，插入要实现幂等
             processDao.addBuildHisDataClear(
                 dslContext = context,
