e054f7b5cbcaabc5c0b5a8710e6c0bc9a669e189
carlyin <449036249@qq.com>
2022-07-12 20:38:54 +0800

    feat:【数据统计】用户态数据统计- 总览 #6587

--- src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/event/dispatcher/pipeline/mq/MQ.kt
+++ src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/event/dispatcher/pipeline/mq/MQ.kt
@@ -272,9 +272,6 @@ object MQ {
     // 流水线webhook commit记录
     const val EXCHANGE_PIPELINE_BUILD_COMMIT_FINISH_FANOUT = "e.engine.pipeline.build.commits.finish.fanout"
 
-    // 质量红线每日数据上报队列
-    const val EXCHANGE_QUALITY_DAILY_FANOUT = "e.metrics.quality.daily.exchange.fanout"
-
     // 流水线质量红线人工审核时间广播
     const val EXCHANGE_PIPELINE_BUILD_QUALITY_REVIEW_FANOUT = "e.quality.pipeline.build.review.fanout"
 
