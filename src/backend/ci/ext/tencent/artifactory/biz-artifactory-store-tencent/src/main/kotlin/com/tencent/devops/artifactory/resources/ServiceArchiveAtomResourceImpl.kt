ed8516201b415d3e158fe4a20d33fbf95b3eb1b5
yaoxuwan <yaoxuwan@tencent.com>
2022-07-20 14:26:55 +0800

    --bug=101734947 删除插件失败

---
+++ src/backend/ci/ext/tencent/artifactory/biz-artifactory-store-tencent/src/main/kotlin/com/tencent/devops/artifactory/resources/ServiceArchiveAtomResourceImpl.kt
@@ -0,0 +1,27 @@
+package com.tencent.devops.artifactory.resources
+
+import com.tencent.devops.artifactory.api.ServiceArchiveAtomResource
+import com.tencent.devops.artifactory.config.BkRepoStoreConfig
+import com.tencent.devops.artifactory.pojo.enums.BkRepoEnum
+import com.tencent.devops.artifactory.service.ArchiveAtomService
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.archive.client.BkRepoClient
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ServiceArchiveAtomResourceImpl @Autowired constructor(
+    private val archiveAtomService: ArchiveAtomService,
+    private val bkRepoClient: BkRepoClient,
+    private val bkRepoStoreConfig: BkRepoStoreConfig
+) : ServiceArchiveAtomResource {
+
+    override fun getAtomFileContent(filePath: String): Result<String> {
+        return Result(archiveAtomService.getAtomFileContent(filePath))
+    }
+
+    override fun deleteAtomFile(userId: String, projectCode: String, atomCode: String): Result<Boolean> {
+        bkRepoClient.delete(userId, bkRepoStoreConfig.bkrepoStoreProjectName, BkRepoEnum.PLUGIN.repoName, atomCode)
+        return Result(true)
+    }
+}
