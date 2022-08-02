3334f8b26a35f8af0c8749f5df055a18f897f0e7
greysonfang <851485863@qq.com>
2022-08-02 11:30:46 +0800

    feat: 版本体验更新人员名单时，额外通知新增人员当前体验版本的信息 #7234

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/GroupService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/GroupService.kt
@@ -24,6 +24,8 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+
 package com.tencent.devops.experience.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
