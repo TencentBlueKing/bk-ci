63a07c3f9f8c9a74ec24d896f60fa0744e548553
yongyiduan <yongyiduan@tencent.com>
2022-08-05 12:09:56 +0800

    [stream]mr检查冲突有误 #7366 修复env变量

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
@@ -158,12 +158,12 @@ open class MarketAtomTask : ITask() {
         // 将Job传入的流水线变量先进行凭据替换
         // 插件接收的流水线参数 = Job级别参数 + Task调度时参数 + 本插件上下文 + 编译机环境参数
         val acrossInfo by lazy { TemplateAcrossInfoUtil.getAcrossInfo(buildVariables.variables, buildTask.taskId) }
-        var variables = buildVariables.variables.map {
+        var variables = buildVariables.variables.plus(buildTask.buildVariable ?: emptyMap()).map {
             it.key to it.value.parseCredentialValue(
                 context = buildTask.buildVariable,
                 acrossProjectId = acrossInfo?.targetProjectId
             )
-        }.toMap().plus(buildTask.buildVariable ?: emptyMap())
+        }.toMap()
 
         // 解析输入输出字段模板
         val props = JsonUtil.toMutableMap(atomData.props!!)
