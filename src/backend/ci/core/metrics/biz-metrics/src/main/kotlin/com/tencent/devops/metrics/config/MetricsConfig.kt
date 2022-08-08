43b1150ba006f1b7b7bdbfb1c568200d2291dde9
yjieliang <yundantengteng@163.com>
2022-08-08 10:14:36 +0800

    feat：数据分析服务下，选择时间段逻辑优化 #7326

--- src/backend/ci/core/metrics/biz-metrics/src/main/kotlin/com/tencent/devops/metrics/config/MetricsConfig.kt
+++ src/backend/ci/core/metrics/biz-metrics/src/main/kotlin/com/tencent/devops/metrics/config/MetricsConfig.kt
@@ -47,6 +47,6 @@ class MetricsConfig {
     @Value("\${metrics.defaultLimitNum:10}")
     val defaultLimitNum = 10
 
-    @Value("\${metrics.queryDaysMax:6}")
+    @Value("\${metrics.queryDaysMax:180}")
     val queryDaysMax: Long = 180
 }
