51248f5475b52dc25663208e65f10a464514cf85
yjieliang <yundantengteng@163.com>
2022-08-08 19:35:05 +0800

    feat:获取流水线接口的返回报文增加模板版本字段 #7353

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/template/TemplatePipelineInfo.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/template/TemplatePipelineInfo.kt
@@ -6,11 +6,11 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel("流水线模板信息")
 class TemplatePipelineInfo (
     @ApiModelProperty("模板id", required = false)
-    val templateId: String,
+    val templateId: String? = null,
     @ApiModelProperty("版本名称", required = false)
-    val versionName: String,
+    val versionName: String? = null,
     @ApiModelProperty("版本", required = false)
-    val version: Long,
+    val version: Long? = null,
     @ApiModelProperty("流水线id", required = false)
     val pipelineId: String
 )
