72640aa9012d51a41569eb848f7cf8dc1325e397
greysonfang <851485863@qq.com>
2022-08-09 15:49:53 +0800

    优化程序后台打印的运行日志内容及级别 #7243

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/ApiAccessTokenService.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/ApiAccessTokenService.kt
@@ -77,15 +77,19 @@ class ApiAccessTokenService @Autowired constructor(
     fun generateUserToken(userDetails: String): TokenInfo {
         logger.info("AUTH | generateUserToken | userId = $userDetails")
         if (secret.isNullOrBlank()) {
-            logger.error("AUTH | generateUserToken failed, " +
-                "because config[auth.accessToken.secret] is not found")
+            logger.error(
+                "AUTH | generateUserToken failed, " +
+                    "because config[auth.accessToken.secret] is not found"
+            )
             throw ErrorCodeException(
                 errorCode = PARAMETER_SECRET_ERROR,
                 statusCode = Response.Status.BAD_REQUEST.statusCode,
                 defaultMessage = "AUTH| generateUserToken failed, " +
                     "because config[auth.accessToken.secret] is not found",
-                params = arrayOf("config", "AUTH| generateUserToken failed, " +
-                    "because config[auth.accessToken.secret] is not found")
+                params = arrayOf(
+                    "config", "AUTH| generateUserToken failed, " +
+                    "because config[auth.accessToken.secret] is not found"
+                )
             )
         }
         val tokenInfo = TokenInfo(
@@ -94,10 +98,12 @@ class ApiAccessTokenService @Autowired constructor(
             accessToken = null
         )
         tokenInfo.accessToken = try {
-            URLEncoder.encode(AESUtil.encrypt(
-                secret,
-                JsonUtil.toJson(tokenInfo, formatted = false)
-            ), "UTF-8")
+            URLEncoder.encode(
+                AESUtil.encrypt(
+                    secret,
+                    JsonUtil.toJson(tokenInfo, formatted = false)
+                ), "UTF-8"
+            )
         } catch (ignore: Throwable) {
             logger.error("AUTH | generateUserToken failed because $ignore ")
             throw ErrorCodeException(
@@ -117,8 +123,7 @@ class ApiAccessTokenService @Autowired constructor(
         val result = try {
             AESUtil.decrypt(secret!!, token)
         } catch (ignore: Throwable) {
-            logger.error("AUTH|getTokenInfo Access token illegal : secret = $secret |" +
-                             " token = $token | error=$ignore")
+            logger.error("AUTH|getTokenInfo Access token illegal : token = $token | error=$ignore")
             throw ErrorCodeException(
                 errorCode = PARAMETER_ILLEGAL_ERROR,
                 statusCode = Response.Status.BAD_REQUEST.statusCode,
