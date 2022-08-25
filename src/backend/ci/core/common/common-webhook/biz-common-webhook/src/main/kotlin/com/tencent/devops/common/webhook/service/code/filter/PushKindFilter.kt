05564c997bee8de7a60aa00ec9fe28e00a8037f1
v_jinleiliu <v_jinleiliu@tencent.com>
2022-08-25 15:43:10 +0800

    feat: "git事件触发"&"gitlab事件触发"当事件为"Commit Push Hook"时，能够区分「新增分支」和「变更文件」

--- src/backend/ci/core/common/common-webhook/biz-common-webhook/src/main/kotlin/com/tencent/devops/common/webhook/service/code/filter/PushKindFilter.kt
+++ src/backend/ci/core/common/common-webhook/biz-common-webhook/src/main/kotlin/com/tencent/devops/common/webhook/service/code/filter/PushKindFilter.kt
@@ -1,15 +1,12 @@
 package com.tencent.devops.common.webhook.service.code.filter
 
 import com.tencent.devops.common.pipeline.pojo.element.trigger.enums.PushActionType
-import com.tencent.devops.common.pipeline.pojo.element.trigger.enums.PushIncludeActionType
-import com.tencent.devops.common.pipeline.pojo.element.trigger.enums.PushOperationType
 import org.slf4j.LoggerFactory
 
 class PushKindFilter(
     private val pipelineId: String,
-    private val operationKind: String,
-    private val actionKind: String,
-    private val included: List<String>
+    private val checkCreateAndUpdate: Boolean?,
+    private val actionList: List<String>
 ) : WebhookFilter {
 
     companion object {
@@ -19,37 +16,21 @@ class PushKindFilter(
     override fun doFilter(response: WebhookFilterResponse): Boolean {
         // isMonitorCreate 为true时 监听创建分支的push事件
         logger.info(
-            "$pipelineId|includAction:$included|" +
-                    "operationKind:$operationKind|actionKind:$actionKind|push kind filter"
+            "$pipelineId|actionList:$actionList|" +
+                    "checkCreateAndUpdate:$checkCreateAndUpdate|push kind filter"
         )
-        var isUpdatePush = false
-        if (included.isEmpty() || included.contains(PushIncludeActionType.updateFile.name)) {
-            isUpdatePush = isUpdatePush(operationKind, actionKind)
-        }
-        var isCreatePush = false
-        if (included.isEmpty() || included.contains(PushIncludeActionType.createBranch.name)) {
-            // 目前来看 不是pushFile事件 即为createBranch事件，但是还是用不同方法做区分，增加代码可读性和之后扩展
-            isCreatePush = isCreatePush(operationKind, actionKind)
-        }
-
-        return isUpdatePush || isCreatePush
-    }
-
-    private fun isCreatePush(operationKind: String, actionKind: String): Boolean {
-        if (operationKind == PushOperationType.CREATE.value && actionKind == PushActionType.CREATE_BRANCH.value) {
-            // 如果operationKind 为create 并且是在工蜂仓库上创建则非更新
+        if (actionList.isEmpty()) {
             return true
         }
-        // 否则即为updatFile 并且 用户本地向远程仓库创建新分支会被认为是更新
-        return false
-    }
-
-    private fun isUpdatePush(operationKind: String, actionKind: String): Boolean {
-        if (operationKind == PushOperationType.CREATE.value && actionKind == PushActionType.CREATE_BRANCH.value) {
-            // 如果operationKind 为create 并且是在工蜂仓库上创建则非更新
-            return false
+        actionList.forEach {
+            if (it == PushActionType.NEW_BRANCH.value && checkCreateAndUpdate != null) {
+                return true
+            }
+            if (it == PushActionType.PUSH_FILE.value && checkCreateAndUpdate != false) {
+                return true
+            }
         }
-        // 否则即为updatFile 并且 用户本地向远程仓库创建新分支会被认为是更新
-        return true
+
+        return false
     }
 }
