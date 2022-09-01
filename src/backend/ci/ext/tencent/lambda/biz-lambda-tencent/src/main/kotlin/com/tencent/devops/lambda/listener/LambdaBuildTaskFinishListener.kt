67abbc33238aa3c98f85f0f0df751aa9ffbd4166
baiyun3510 <985017819@qq.com>
2022-09-01 10:15:25 +0800

    --task=860913935 build_history新增buildMsg字段

--- src/backend/ci/ext/tencent/lambda/biz-lambda-tencent/src/main/kotlin/com/tencent/devops/lambda/listener/LambdaBuildTaskFinishListener.kt
+++ src/backend/ci/ext/tencent/lambda/biz-lambda-tencent/src/main/kotlin/com/tencent/devops/lambda/listener/LambdaBuildTaskFinishListener.kt
@@ -41,6 +41,7 @@ class LambdaBuildTaskFinishListener @Autowired constructor(
 
     override fun run(event: PipelineBuildTaskFinishBroadCastEvent) {
 //        logger.info("[${event.projectId}|${event.pipelineId}|${event.buildId}] Receive build element finish event - ($event)")
+        logger.info("[${event.projectId}|${event.pipelineId}|${event.buildId}|${event.taskId}] Receive build element finish event - ($event)")
         lambdaDataService.onBuildTaskFinish(event)
     }
 }
