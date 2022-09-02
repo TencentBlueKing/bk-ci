851cd845d09d5c59fbfb7944786c7f5ca12c5fd8
yjieliang <yundantengteng@163.com>
2022-09-02 11:55:56 +0800

    --story=877180397 插件协作申请提供给MOA的回调接口由service类型改成open类型

--- src/backend/ci/ext/tencent/store/api-store-tencent/src/main/kotlin/com/tencent/devops/store/api/common/TxServiceStoreApproveResource.kt
+++ src/backend/ci/ext/tencent/store/api-store-tencent/src/main/kotlin/com/tencent/devops/store/api/common/TxServiceStoreApproveResource.kt
@@ -37,7 +37,6 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OPEN_MARKET_APPROVAL"], description = "open-store组件审批")
