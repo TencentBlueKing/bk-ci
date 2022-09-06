b6390c09c37ebfa5a7ec8c4991c424b28ba2ccc4
royalhuang <royalhuang@tencent.com>
2022-09-06 12:01:12 +0800

    feat: 引擎支持高级表达式解析 #7115 修复settings问题

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/script/ScriptTask.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/script/ScriptTask.kt
@@ -85,10 +85,13 @@ open class ScriptTask : ITask() {
         logger.info("Start to execute the script task($scriptType) ($script)")
         val command = CommandFactory.create(scriptType)
         val buildId = buildVariables.buildId
-        val runtimeVariables = buildVariables.variables.map {
-            it.key to it.value.parseCredentialValue(buildTask.buildVariable)
-        }.toMap()
-            .plus(buildTask.buildVariable ?: emptyMap())
+        val runtimeVariables = buildVariables.variables.plus(buildTask.buildVariable ?: emptyMap()).let { vars ->
+            if (buildVariables.pipelineAsCodeSettings?.enable == true) {
+                vars.map {
+                    it.key to it.value.parseCredentialValue(buildTask.buildVariable)
+                }.toMap()
+            } else vars
+        }
         val projectId = buildVariables.projectId
 
         ScriptEnvUtils.cleanEnv(buildId, workspace)
