b527e25d813dfe65f91409b99c6ab9ed5849cd7e
ruotiantang <ruotiantang@tencent.com>
2022-09-07 14:25:06 +0800

    [BUG] 删除环境后使用被删除的第三方构建机环境卡住构建 #7567

--- src/backend/ci/core/dispatch/biz-dispatch/src/main/kotlin/com/tencent/devops/dispatch/service/dispatcher/agent/ThirdPartyAgentDispatcher.kt
+++ src/backend/ci/core/dispatch/biz-dispatch/src/main/kotlin/com/tencent/devops/dispatch/service/dispatcher/agent/ThirdPartyAgentDispatcher.kt
@@ -30,7 +30,6 @@ package com.tencent.devops.dispatch.service.dispatcher.agent
 import com.tencent.devops.common.api.enums.AgentStatus
 import com.tencent.devops.common.api.exception.InvalidParamException
 import com.tencent.devops.common.api.exception.RemoteServiceException
-import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.event.dispatcher.pipeline.PipelineEventDispatcher
 import com.tencent.devops.common.log.utils.BuildLogPrinter
