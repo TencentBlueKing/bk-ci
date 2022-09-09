9a1b651f8b36f787d4af39070c9de359e44b482c
yaoxuwan <yaoxuwan@tencent.com>
2022-09-09 10:05:25 +0800

    feat: CI制品增加更多类型 #6302 合并bkrepoClient

--- src/backend/ci/ext/tencent/common/common-archive-tencent/src/main/kotlin/com/tencent/devops/common/archive/TxBkRepoAutoConfiguration.kt
+++ src/backend/ci/ext/tencent/common/common-archive-tencent/src/main/kotlin/com/tencent/devops/common/archive/TxBkRepoAutoConfiguration.kt
@@ -25,19 +25,20 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.tencent.devops.common.archive.util
+package com.tencent.devops.common.archive
 
-object PathUtil {
-    fun getParentFolder(path: String): String {
-        val tmpPath = path.removeSuffix("/")
-        return tmpPath.removeSuffix(getFileName(tmpPath))
-    }
+import com.tencent.devops.common.archive.config.BkRepoConfig
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 
-    fun isFolder(path: String): Boolean {
-        return path.endsWith("/")
-    }
+@Configuration
+@ConditionalOnWebApplication
+@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
+class TxBkRepoAutoConfiguration {
 
-    fun getFileName(path: String): String {
-        return path.removeSuffix("/").split("/").last()
-    }
+    @Bean
+    fun bkRepoConfig() = BkRepoConfig()
 }
