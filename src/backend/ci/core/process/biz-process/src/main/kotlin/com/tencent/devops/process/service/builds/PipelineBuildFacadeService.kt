a04453eb873f7e5a5456271078dfc9995cf8390f
yongyiduan <yongyiduan@tencent.com>
2022-09-09 14:29:42 +0800

    feat: stage 人工审核进入审核流程时，审核通知同时发送给触发人 #7470

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelineBuildFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelineBuildFacadeService.kt
@@ -935,6 +935,9 @@ class PipelineBuildFacadeService(
             val success = if (isCancel) {
                 pipelineStageService.cancelStage(
                     userId = userId,
+                    triggerUserId = buildInfo.triggerUser,
+                    pipelineName = pipelineInfo.pipelineName,
+                    buildNum = buildInfo.buildNum,
                     buildStage = buildStage,
                     reviewRequest = reviewRequest
                 )
