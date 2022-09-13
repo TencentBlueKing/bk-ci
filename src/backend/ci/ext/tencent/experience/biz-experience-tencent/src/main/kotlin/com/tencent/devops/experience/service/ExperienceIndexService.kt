4a8027a0dcadf92dab08e3eca51344f87f22ab77
greysonfang <greysonfang@tencent.com>
2022-09-13 11:32:05 +0800

    --story=877204207 【蓝盾APP】【产品-已评审】minigame活动支持

--- src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceIndexService.kt
+++ src/backend/ci/ext/tencent/experience/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/service/ExperienceIndexService.kt
@@ -333,6 +333,7 @@ class ExperienceIndexService @Autowired constructor(
         if (minigamePicture == null) {
             throw NotFoundException("MiniGame Picture not found")
         }
+        logger.info("minigame picture :${UrlUtil.toOuterPhotoAddr(minigamePicture!!)}")
         return Result(UrlUtil.toOuterPhotoAddr(minigamePicture!!))
     }
 
