2340b703723fffdfd76f43c6d33f9e40f46b1937
irwinsun <irwinsun@tencent.com>
2022-09-22 11:56:03 +0800

    --task=860995351 无用历史代码清理 移除金刚权限（无用）

--- src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/resources/BuildJinGangAppResourceImpl.kt
+++ src/backend/ci/ext/tencent/plugin/biz-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/resources/BuildJinGangAppResourceImpl.kt
@@ -28,21 +28,14 @@
 package com.tencent.devops.plugin.resources
 
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.api.util.HashUtil
-import com.tencent.devops.common.auth.api.BSAuthResourceApi
-import com.tencent.devops.common.auth.api.AuthResourceType
-import com.tencent.devops.common.auth.code.VSAuthServiceCode
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.plugin.api.BuildJinGangAppResource
 import com.tencent.devops.plugin.service.JinGangService
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
 class BuildJinGangAppResourceImpl @Autowired constructor(
-    private val jinGangService: JinGangService,
-    private val bkAuthResourceApi: BSAuthResourceApi,
-    private val serviceCode: VSAuthServiceCode
+    private val jinGangService: JinGangService
 ) : BuildJinGangAppResource {
     override fun updateTask(buildId: String, md5: String, status: Int, taskId: Long, scanUrl: String, result: String) {
         jinGangService.updateTask(buildId, md5, status, taskId, scanUrl, result)
@@ -63,18 +56,6 @@ class BuildJinGangAppResourceImpl @Autowired constructor(
         return Result(jinGangService.createTask(projectId, pipelineId, buildId, buildNo, userId, path, md5, size, version, type))
     }
 
-    override fun createResource(
-        userId: String,
-        projectId: String,
-        jinGangTaskId: String,
-        resourceName: String
-    ): Result<Boolean> {
-        // 在权限中心注册资源
-        logger.info("register resources started|userId: $userId, projectId: $projectId, jinGangTaskId: $jinGangTaskId,resourceName: $resourceName")
-        bkAuthResourceApi.createResource(userId, serviceCode, AuthResourceType.SCAN_TASK, projectId, HashUtil.encodeLongId(jinGangTaskId.toLong()), resourceName)
-        return Result(true)
-    }
-
     override fun scanApp(
         userId: String,
         projectId: String,
@@ -87,9 +68,5 @@ class BuildJinGangAppResourceImpl @Autowired constructor(
         runType: String
     ): Result<String> {
         return Result(jinGangService.scanApp(userId, projectId, pipelineId, buildId, buildNo, elementId, file, isCustom, runType))
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(BuildJinGangAppResourceImpl::class.java)
     }
 }
