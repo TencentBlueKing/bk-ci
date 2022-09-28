228a97f5c3e00c766274a392a73751fc594c8ef8
mingshewhe <mingshewhe@tencent.com>
2022-09-28 09:43:05 +0800

    feat: Stream腾讯云部署 #7028 auth启动调整

--- src/backend/ci/ext/tencent/common/common-auth/common-auth-tencent/src/main/kotlin/com/tencent/devops/common/auth/CommonConfiguration.kt
+++ src/backend/ci/ext/tencent/common/common-auth/common-auth-tencent/src/main/kotlin/com/tencent/devops/common/auth/CommonConfiguration.kt
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.jmx.export.MBeanExporter
 
 @Configuration
