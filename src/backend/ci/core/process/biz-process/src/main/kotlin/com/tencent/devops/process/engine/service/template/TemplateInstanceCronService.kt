6b224d91b138936d9c554291fecf484434a267ee
yjieliang <yundantengteng@163.com>
2022-10-12 14:14:05 +0800

    pref: 调整流水线模板更新实例化失败时原因提示信息长度 #7773

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/template/TemplateInstanceCronService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/service/template/TemplateInstanceCronService.kt
@@ -74,8 +74,8 @@ class TemplateInstanceCronService @Autowired constructor(
     @Value("\${template.instanceListUrl}")
     private val instanceListUrl: String = ""
 
-    @Value("\${template.maxErrorReasonLength:200}")
-    private val maxErrorReasonLength: Int = 200
+    @Value("\${template.maxErrorReasonLength:400}")
+    private val maxErrorReasonLength: Int = 400
 
     @Scheduled(cron = "0 0/1 * * * ?")
     fun templateInstance() {
