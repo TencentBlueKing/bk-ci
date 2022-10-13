860961c569f6b51c34e7cdfaef6ed8fa67189100
RJ <eazence@163.com>
2022-10-13 15:02:38 +0800

    feat: 【编译加速】支持指定turbo工具版本号 #7764

--- src/backend/turbo/biz-turbo/src/main/kotlin/com/tencent/devops/turbo/sdk/BKDistccApi.kt
+++ src/backend/turbo/biz-turbo/src/main/kotlin/com/tencent/devops/turbo/sdk/BKDistccApi.kt
@@ -1,37 +1,23 @@
 package com.tencent.devops.turbo.sdk
 
 import com.tencent.devops.common.api.util.OkhttpUtil
+import com.tencent.devops.turbo.config.TBSProperties
+import com.tencent.devops.web.util.SpringContextHolder
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
 
-@Component
-@ConfigurationProperties(prefix = "bkdistcc")
 object BKDistccApi {
 
     private val logger = LoggerFactory.getLogger(BKDistccApi::class.java)
 
-    private var bkdistccHost: String? = null
-
-    private var port: String? = null
-
-    @Value("\${bkdistcc.host:#{null}}")
-    fun setBkdistccHost(bkdistccHost: String) {
-        BKDistccApi.bkdistccHost = bkdistccHost
-    }
-
-    @Value("\${bkdistcc.port:#{null}}")
-    fun setPort(port: String) {
-        BKDistccApi.port = port
-    }
 
     /**
      * 查询编译加速工具版本清单
      */
     fun queryBkdistccVersion(): String {
-        logger.info("queryBkdistccVersion url: [$bkdistccHost:$port]")
-        return OkhttpUtil.doGet(url = "http://$bkdistccHost:$port/api/v1/disttask/resource/version")
+        val properties = SpringContextHolder.getBean<TBSProperties>()
+        val url = "${properties.rootPath}/api/v1/disttask/resource/version"
+        logger.info("queryBkdistccVersion url: [$url]")
+        return OkhttpUtil.doGet(url = url)
     }
 }
