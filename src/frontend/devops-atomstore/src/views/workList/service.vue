ceb9d82fdaa054a7281326ac64536735129b4892
vhwweng <1010382269@qq.com>
2022-10-17 10:24:04 +0800

    --story=878434431 工蜂域名切换为 git.woa.com

--- src/frontend/devops-atomstore/src/views/workList/service.vue
+++ src/frontend/devops-atomstore/src/views/workList/service.vue
@@ -307,8 +307,8 @@
         computed: {
             createTips () {
                 const host = location.host
-                const devHosts = ['dev.devops.oa.com', 'v2.dev.devops.oa.com']
-                const testHosts = ['test.devops.oa.com', 'v2.test.devops.oa.com']
+                const devHosts = ['dev.devops.woa.com', 'v2.dev.devops.woa.com']
+                const testHosts = ['test.devops.woa.com', 'v2.test.devops.woa.com']
                 const devIndex = devHosts.findIndex(innerHost => innerHost === host)
                 const testIndex = testHosts.findIndex(innerHost => innerHost === host)
                 const group = devIndex > -1 ? 'dev-bkdevops-extension-service' : (testIndex > -1 ? 'test-bkdevops-extension-service' : 'bkdevops-extension-service')
