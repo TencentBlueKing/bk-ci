cd0256639b5c80ad731ad246315b1b0eb1fb7c67
sawyersong <sawyersong@tencent.com>
2022-10-18 15:37:44 +0800

    feat: 提供统一的云原生调度接入层 #6725

--- src/backend/ci/buildSrc/src/main/kotlin/plugins/task-gen-jooq.gradle.kts
+++ src/backend/ci/buildSrc/src/main/kotlin/plugins/task-gen-jooq.gradle.kts
@@ -55,10 +55,6 @@ var moduleNames = when (moduleName) {
     else -> listOf(moduleName)
 }
 
-if (name == "model-dispatch-bcs") {
-    moduleNames = listOf("dispatch_bcs")
-}
-
 if (name == "model-dispatch-kubernetes") {
     moduleNames = listOf("dispatch_kubernetes")
 }
