2eeead5ed03acc535c4f9ef65508d66f4ade4789
yongyiduan <yongyiduan@tencent.com>
2022-10-18 15:37:48 +0800

    [stream]github 审核权限优化 #7812

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/actions/github/GithubPRActionGit.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/actions/github/GithubPRActionGit.kt
@@ -118,7 +118,8 @@ class GithubPRActionGit(
                 userId = this.data.eventCommon.userId
             ).accessLevel
 
-            accessLevel >= 30
+            // >= TRIAGE
+            accessLevel >= 20
         } catch (error: ErrorCodeException) {
             false
         }
