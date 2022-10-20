62377ac09c4a071497aedab7cb6bd7f4bcdb8e79
ruotiantang <ruotiantang@tencent.com>
2022-10-20 09:56:38 +0800

    Agnet支持启动docker镜像作为构建机 #7748

--- src/backend/ci/ext/tencent/dispatch/boot-dispatch-tencent/src/main/kotlin/com/tencent/devops/dispatch/Application.kt
+++ src/backend/ci/ext/tencent/dispatch/boot-dispatch-tencent/src/main/kotlin/com/tencent/devops/dispatch/Application.kt
@@ -32,7 +32,7 @@ import com.tencent.devops.common.service.MicroServiceApplication
 import org.springframework.context.annotation.ComponentScan
 
 @MicroService
-@ComponentScan("com.tencent.devops.plugin", "com.tencent.devops.dispatch")
+@ComponentScan("com.tencent.devops.plugin", "com.tencent.devops.dispatch", "com.tencent.devops.common.dispatch.sdk")
 class Application
 
 fun main(args: Array<String>) {
