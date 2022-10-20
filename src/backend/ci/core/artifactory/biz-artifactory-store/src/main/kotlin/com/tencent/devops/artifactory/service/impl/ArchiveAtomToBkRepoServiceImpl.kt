44dfb19980e300847adb2d01ae179116a3e0d262
yjieliang <yundantengteng@163.com>
2022-10-20 10:22:50 +0800

    pref:插件自定义错误码管理 #7539

--- src/backend/ci/core/artifactory/biz-artifactory-store/src/main/kotlin/com/tencent/devops/artifactory/service/impl/ArchiveAtomToBkRepoServiceImpl.kt
+++ src/backend/ci/core/artifactory/biz-artifactory-store/src/main/kotlin/com/tencent/devops/artifactory/service/impl/ArchiveAtomToBkRepoServiceImpl.kt
@@ -20,7 +20,7 @@ import javax.ws.rs.NotFoundException
 
 @Service
 @ConditionalOnProperty(prefix = "artifactory", name = ["realm"], havingValue = REALM_BK_REPO)
-class ArchiveAtomToBkRepoServiceImpl() : ArchiveAtomServiceImpl() {
+class ArchiveAtomToBkRepoServiceImpl : ArchiveAtomServiceImpl() {
 
     override fun getAtomArchiveBasePath(): String {
         return System.getProperty("java.io.tmpdir")
