0d06bec7db1e258912f61343605cfbe1cd173915
yjieliang <yundantengteng@163.com>
2022-10-25 19:25:27 +0800

    issue: 发布者和工具认证信息入库 #7378

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/resources/common/UserStorePublishersResourceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/resources/common/UserStorePublishersResourceImpl.kt
@@ -28,11 +28,13 @@
 package com.tencent.devops.store.resources.common
 
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.store.api.common.UserStorePublishersResource
 import com.tencent.devops.store.pojo.common.PublisherInfo
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
 import com.tencent.devops.store.service.common.PublishersDataService
 
+@RestResource
 class UserStorePublishersResourceImpl constructor(
     private val publishersDataService: PublishersDataService
 ) : UserStorePublishersResource {
