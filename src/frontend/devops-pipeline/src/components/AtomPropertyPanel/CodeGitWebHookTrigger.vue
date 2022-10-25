788d1fffb6d4fddb7168102f147c115a5a2da698
vhwweng <1010382269@qq.com>
2022-10-25 12:07:30 +0800

    feat: git事件触发插件支持第三方服务过滤 #7743

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/CodeGitWebHookTrigger.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/CodeGitWebHookTrigger.vue
@@ -13,16 +13,16 @@
             </form-field>
         </template>
         <form-field v-if="Object.keys(customTriggerControlModel).length">
-            <accordion show-checkbox :show-content="enableCustomTrigger" key="customTriggerControl" :is-version="true">
+            <accordion show-checkbox :show-content="enableThirdFilter" key="customTriggerControl" :is-version="true">
                 <header class="var-header" style="height: 16px;" slot="header">
                     <span>
                         {{ $t('editPage.customTriggerControl') }}
                         <i class="bk-icon icon-info-circle ml5" v-bk-tooltips="$t('editPage.customTriggerControlTips')"></i>
                         <a class="title-link" target="blink" :href="customTriggerDocsLink">{{ $t('editPage.customTriggerLinkDesc') }}</a>
                     </span>
-                    <input class="accordion-checkbox" :disabled="disabled" :checked="enableCustomTrigger" type="checkbox" @click.stop @change="toggleCustomTrigger" />
+                    <input class="accordion-checkbox" :disabled="disabled" :checked="enableThirdFilter" type="checkbox" @click.stop @change="toggleEnableThirdFilter" />
                 </header>
-                <div slot="content" class="bk-form bk-form-vertical" v-if="enableCustomTrigger">
+                <div slot="content" class="bk-form bk-form-vertical" v-if="enableThirdFilter">
                     <template v-for="(obj, key) in customTriggerControlModel">
                         <form-field :key="key" :desc="obj.desc" :desc-link="obj.descLink" :desc-link-text="obj.descLinkText" :required="obj.required" :label="obj.label" :is-error="errors.has(key)" :error-msg="errors.first(key)">
                             <component
@@ -50,7 +50,7 @@
         data () {
             return {
                 customTriggerControlModel: {},
-                enableCustomTrigger: false,
+                enableThirdFilter: false,
                 customTriggerDocsLink: 'https://github.com/Tencent/bk-ci/issues/7743#issue-1391717634'
             }
         },
@@ -70,12 +70,12 @@
             }
         },
         created () {
+            this.enableThirdFilter = this.element.enableThirdFilter || false
             this.customTriggerControlModel = {}
             const { thirdUrl, thirdSecretToken } = this.atomPropsModel
             if (thirdUrl && thirdSecretToken) {
                 this.customTriggerControlModel.thirdUrl = thirdUrl
                 this.customTriggerControlModel.thirdSecretToken = thirdSecretToken
-                this.enableCustomTrigger = Boolean(this.element.thirdSecretToken || this.element.thirdUrl)
                 this.atomPropsModel.thirdUrl.hidden = true
                 this.atomPropsModel.thirdSecretToken.hidden = true
             }
@@ -118,8 +118,9 @@
                 }
                 this.handleUpdateElement(name, value)
             },
-            toggleCustomTrigger () {
-                this.enableCustomTrigger = !this.enableCustomTrigger
+            toggleEnableThirdFilter () {
+                this.enableThirdFilter = !this.enableThirdFilter
+                this.handleUpdateElement('enableThirdFilter', this.enableThirdFilter)
             }
         }
     }
