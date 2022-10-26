d0be5c79e535041f0de459b41a20c8e586082eee
yjieliang <yundantengteng@163.com>
2022-10-26 15:29:53 +0800

    --story=877962331 迁移oa域名相关研发商店组件描述的图片等文件链接

--- src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/common/impl/TxOpMigrateStoreDescriptionServiceImpl.kt
+++ src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/service/common/impl/TxOpMigrateStoreDescriptionServiceImpl.kt
@@ -51,16 +51,21 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 @Service
-class TxOpMigrateStoreDescriptionServiceImpl @Autowired constructor(
-    private val dslContext: DSLContext,
-    private val txOpMigrateStoreDescriptionDao: TxOpMigrateStoreDescriptionDao,
-    private val client: Client
-) : TxOpMigrateStoreDescriptionService {
+class TxOpMigrateStoreDescriptionServiceImpl : TxOpMigrateStoreDescriptionService {
+
+    @Autowired
+    lateinit var dslContext: DSLContext
+
+    @Autowired
+    lateinit var txOpMigrateStoreDescriptionDao: TxOpMigrateStoreDescriptionDao
+
+    @Autowired
+    lateinit var client: Client
 
     companion object {
         private val logger = LoggerFactory.getLogger(TxOpMigrateStoreDescriptionServiceImpl::class.java)
         private const val DEFAULT_PAGE_SIZE = 100
-        private const val BK_CI_PATH_REGEX = "!\\[(.*)]\\((http://radosgw.open.oa.com(.*))\\)"
+        private const val BK_CI_PATH_REGEX = "(!\\[(.*?)]\\()(http[s]?://radosgw.open.oa.com(.*?))(\\))"
     }
 
     override fun migrateStoreDescription(): Boolean {
@@ -282,7 +287,7 @@ class TxOpMigrateStoreDescriptionServiceImpl @Autowired constructor(
         }
     }
 
-    private fun checkLogoUrlCondition(description: String?): List<String>? {
+    fun checkLogoUrlCondition(description: String?): List<String>? {
         if (description.isNullOrBlank()) {
             return null
         }
@@ -290,7 +295,7 @@ class TxOpMigrateStoreDescriptionServiceImpl @Autowired constructor(
         val matcher: Matcher = pattern.matcher(description)
         val pathList = mutableListOf<String>()
         while (matcher.find()) {
-            pathList.add(matcher.group(2))
+            pathList.add(matcher.group(3))
         }
         return pathList
     }
@@ -302,11 +307,11 @@ class TxOpMigrateStoreDescriptionServiceImpl @Autowired constructor(
         return url.substring(index + 1).toLowerCase()
     }
 
-    private fun replaceDescription(description: String, pathMap: Map<String, String>): String {
-        var newDescription = ""
+    fun replaceDescription(description: String, pathMap: Map<String, String>): String {
+        var newDescription = description
         pathMap.forEach {
             val pattern: Pattern = Pattern.compile("(!\\[(.*)]\\()(${it.key})(\\))")
-            val matcher: Matcher = pattern.matcher(description)
+            val matcher: Matcher = pattern.matcher(newDescription)
             newDescription = matcher.replaceAll("$1${it.value}$4")
         }
         return newDescription
