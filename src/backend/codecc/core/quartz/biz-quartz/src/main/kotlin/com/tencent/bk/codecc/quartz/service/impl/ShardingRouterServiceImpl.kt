ce12d7ae27096fe6ed11e3d74b8f62c3b09a4aa5
Yuanruitao <1075097840@qq.com>
2022-10-27 14:47:46 +0800

    feat: codecc 支持使用github账号登录态和鉴权 #7363

--- src/backend/codecc/core/quartz/biz-quartz/src/main/kotlin/com/tencent/bk/codecc/quartz/service/impl/ShardingRouterServiceImpl.kt
+++ src/backend/codecc/core/quartz/biz-quartz/src/main/kotlin/com/tencent/bk/codecc/quartz/service/impl/ShardingRouterServiceImpl.kt
@@ -14,7 +14,6 @@ import org.quartz.Scheduler
 import org.quartz.impl.matchers.GroupMatcher
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.client.ServiceInstance
 import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.client.serviceregistry.Registration
 import org.springframework.stereotype.Service
