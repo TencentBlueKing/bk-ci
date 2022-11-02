e83e9f427bf51ca14447370e3ff4cf38e4d9dc9c
yjieliang <yundantengteng@163.com>
2022-11-02 16:34:39 +0800

    perf：【插件治理】增加对 platformCode 的有效性校验 #7919

--- src/backend/ci/core/worker/worker-api-sdk/src/main/kotlin/com/tencent/devops/worker/common/api/atom/StoreSDKResourceApi.kt
+++ src/backend/ci/core/worker/worker-api-sdk/src/main/kotlin/com/tencent/devops/worker/common/api/atom/StoreSDKResourceApi.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.worker.common.api.AbstractBuildResourceApi
 
-class StoreSDKResourceApi : AbstractBuildResourceApi(),StoreSDKApi {
+class StoreSdkResourceApi : AbstractBuildResourceApi(),StoreSdkApi {
 
     override fun isPlatformCodeRegistered(platformCode: String): Result<Boolean> {
         val path = "/ms/store/api/build/store/docking/platforms/codes/$platformCode/user/validate"
--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/atom/StoreSdkApi.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/atom/StoreSdkApi.kt
@@ -3,7 +3,7 @@ package com.tencent.devops.worker.common.api.atom
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.worker.common.api.WorkerRestApiSDK
 
-interface StoreSDKApi : WorkerRestApiSDK {
+interface StoreSdkApi : WorkerRestApiSDK {
 
     fun isPlatformCodeRegistered(platformCode: String): Result<Boolean>
 }
