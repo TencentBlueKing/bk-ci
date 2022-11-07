cc23423d149d8dc5abb5c593bc2ab611473b078d
xuzhan <xuzhan@tencent.com>
2022-11-07 21:14:45 +0800

    feature: 添加项目管理前端服务 issue #7923

--- src/frontend/devops-manage/bk.config.js
+++ src/frontend/devops-manage/bk.config.js
@@ -3,6 +3,7 @@ module.exports = {
   host: process.env.BK_APP_HOST,
   port: process.env.BK_APP_PORT,
   publicPath: process.env.BK_STATIC_URL,
+  outputDir: process.env.BK_OUTPUT_DIR,
   cache: true,
   open: false,
   replaceStatic: false,
