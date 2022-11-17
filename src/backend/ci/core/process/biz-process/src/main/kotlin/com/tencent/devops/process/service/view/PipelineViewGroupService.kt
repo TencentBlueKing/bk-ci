2793bdd9201f7755ac23adaf7a1220b749b88aac
stubenhuang <stubenhuang@tencent.com>
2022-11-17 10:20:23 +0800

    feat: 流水线组及流水线列表页优化 #7101

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/view/PipelineViewGroupService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/view/PipelineViewGroupService.kt
@@ -617,11 +617,17 @@ class PipelineViewGroupService @Autowired constructor(
         val isProjectManager = checkPermission(userId, projectId)
         if (isProjectManager && !view.isProject && view.createUser != userId) {
             logger.warn("bulkRemove , $userId is ProjectManager , but can`t remove other view")
-            return false
+            throw ErrorCodeException(
+                errorCode = ProcessMessageCode.ERROR_VIEW_GROUP_NO_PERMISSION,
+                defaultMessage = "user:$userId has no permission to edit view group, project:$projectId"
+            )
         }
         if (!isProjectManager && (view.isProject || view.createUser != userId)) {
             logger.warn("bulkRemove , $userId isn`t ProjectManager , just can remove self view")
-            return false
+            throw ErrorCodeException(
+                errorCode = ProcessMessageCode.ERROR_VIEW_GROUP_NO_PERMISSION,
+                defaultMessage = "user:$userId has no permission to edit view group, project:$projectId"
+            )
         }
         pipelineViewGroupDao.batchRemove(dslContext, projectId, viewId, bulkRemove.pipelineIds)
         return true
