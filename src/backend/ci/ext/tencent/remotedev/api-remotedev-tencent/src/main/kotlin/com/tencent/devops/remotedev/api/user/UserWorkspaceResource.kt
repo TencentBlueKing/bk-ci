8aae3475556bd746f2a45ce12584b145ff4f0eee
sawyersong <sawyersong@tencent.com>
2022-11-18 10:20:23 +0800

    --story=879900551 【remoteDev】远程开发微服务初始化

---
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserWorkspaceResource.kt
@@ -0,0 +1,134 @@
+/*
+ * Tencent is pleased to support the open source community by making BK-CI 蓝鲸持续集成平台 available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * BK-CI 蓝鲸持续集成平台 is licensed under the MIT license.
+ *
+ * A copy of the MIT License is included in this file.
+ *
+ *
+ * Terms of the MIT License:
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.tencent.devops.remotedev.api.user
+
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.process.pojo.github.GithubAppUrl
+import com.tencent.devops.remotedev.pojo.Workspace
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Api(tags = ["USER_WORKSPACE"], description = "用户-工作空间")
+@Path("/user/workspace")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+interface UserWorkspaceResource {
+
+    @ApiOperation("获取用户已授权代码库列表")
+    @GET
+    @Path("/repository/get")
+    fun getAuthorizedGitRepository(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String
+    ): Result<GithubAppUrl>
+
+    @ApiOperation("创建新的工作空间")
+    @POST
+    @Path("/create")
+    fun createWorkspace(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("工作空间描述", required = false)
+        workspace: Workspace
+    ): Result<Boolean>
+
+    @ApiOperation("开启工作空间")
+    @POST
+    @Path("/start/workspaces/{workspaceId}")
+    fun startWorkspace(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("工作空间ID", required = false)
+        @PathParam("workspaceId")
+        workspaceId: Long
+    ): Result<Boolean>
+
+    @ApiOperation("分享工作空间")
+    @POST
+    @Path("/share/workspaces/{workspaceId}")
+    fun shareWorkspace(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("工作空间ID", required = false)
+        @PathParam("workspaceId")
+        workspaceId: Long,
+        @ApiParam("分享用户", required = false)
+        sharedUser: String
+    ): Result<Boolean>
+
+    @ApiOperation("删除工作空间")
+    @DELETE
+    @Path("/delete/workspaces/{workspaceId}")
+    fun deleteWorkspace(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("工作空间ID", required = false)
+        @PathParam("workspaceId")
+        workspaceId: Long
+    ): Result<Boolean>
+
+    @ApiOperation("获取用户工作空间列表")
+    @GET
+    @Path("/list")
+    fun getWorkspaceList(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String
+    ): Result<Workspace>
+
+    //todo 获取运行日志的接口
+
+    @ApiOperation("获取用户工作空间列表")
+    @GET
+    @Path("/detail/workspaces/{workspaceId}")
+    fun getWorkspaceDetail(
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam("工作空间ID", required = false)
+        @PathParam("workspaceId")
+        workspaceId: Long
+    ): Result<Workspace>
+}
---
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/resources/mysql/remotedev.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE IF NOT EXISTS `devops_remotedev` DEFAULT CHARACTER SET utf8mb4;
+
+-- ----------------------------
+-- Table structure for T_WORKSPACE
+-- 工作空间相关属性
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `T_WORKSPACE` (
+                                             `ID` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '工作空间ID',
+                                             `USER_ID` varchar(64) NOT NULL DEFAULT '' COMMENT '用户',
+                                             `PROJECT_ID` varchar(64) NOT NULL DEFAULT '' COMMENT '项目ID',
+                                             `NAME` varchar(128) NOT NULL DEFAULT '' COMMENT '工作空间名称，唯一性',
+                                             `TEMPLATE_ID` int(11) NOT NULL DEFAULT 16 COMMENT '模板ID',
+                                             `URL` varchar(1024) NOT NULL DEFAULT '' COMMENT '工蜂项目URL',
+                                             `BRANCH` varchar(1024) NOT NULL DEFAULT '' COMMENT '工蜂项目分支',
+                                             `YAML` longtext NOT NULL DEFAULT ''  COMMENT '配置yaml内容',
+                                             `YAML_PATH` varchar(1024) NOT NULL DEFAULT ''  COMMENT '配置yaml路径',
+                                             `DOCKERFILE` longtext NOT NULL DEFAULT ''  COMMENT '依赖镜像的DockerFile内容',
+                                             `IMAGE_PATH` varchar(256) NOT NULL DEFAULT '' COMMENT '镜像地址',
+                                             `CPU` int(11) NOT NULL DEFAULT 16 COMMENT 'CPU',
+                                             `MEMORY` varchar(64) NOT NULL DEFAULT '32768M' COMMENT '内存',
+                                             `DISK` varchar(64) NOT NULL DEFAULT '100G' COMMENT '磁盘',
+                                             `CREATOR` varchar(1024) NOT NULL DEFAULT '' COMMENT '创建人',
+                                             `CREATOR_BG_NAME` varchar(128) NOT NULL DEFAULT ''  COMMENT '预留字段，CI开启人所在事业群，用作度量统计',
+                                             `CREATOR_DEPT_NAME` varchar(128) NOT NULL DEFAULT '' COMMENT '预留字段，CI开启人所在部门，用作度量统计',
+                                             `CREATOR_CENTER_NAME` varchar(128) NOT NULL DEFAULT '' COMMENT '预留字段，CI开启人所在中心，用作度量统计',
+                                             `STATUS` int(11) NOT NULL DEFAULT 0 COMMENT '工作空间状态',
+                                             `CREATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                             `UPDATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+                                             PRIMARY KEY (`ID`),
+                                             UNIQUE INDEX `NAME`(`NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for T__WORKSPACE_TEMPLATE
+-- 工作空间模板信息表
+-- ----------------------------
+
+CREATE TABLE `T_WORKSPACE_TEMPLATE`  (
+                                         `ID` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                                         `NAME` varchar(64) NOT NULL COMMENT '模板名称',
+                                         `IMAGE` varchar(256) NOT NULL DEFAULT ''  COMMENT '模板镜像',
+                                         `SOURCE` varchar(64) NOT NULL DEFAULT ''  COMMENT '模板来源',
+                                         `DESCRIPTION` varchar(256) NOT NULL DEFAULT '' COMMENT '模板描述',
+                                         `CREATOR` varchar(64) NOT NULL DEFAULT '' COMMENT '最初创建人',
+                                         `CREATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                         `UPDATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+                                         PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='工作空间模板信息表';
+
+
+-- ----------------------------
+-- Table structure for T_WORKSPACE_OP_HIS
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `T_WORKSPACE_OP_HIS` (
+                                                    `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+                                                    `WORKSPACE_ID` bigint(20) NOT NULL DEFAULT 0 COMMENT '工作空间ID',
+                                                    `OPERATOR` varchar(64) NOT NULL DEFAULT '' COMMENT '操作人',
+                                                    `ACTION` int(10) NOT NULL DEFAULT 0 COMMENT '操作行为: CREATE, START, STOP, DELETE, SHARE',
+                                                    `ACTION_MSG` varchar(256) NOT NULL DEFAULT '' COMMENT '操作行为描述',
+                                                    `CREATED_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+
+                                                    PRIMARY KEY (`ID`) USING BTREE,
+                                                    KEY `uni_1` (`WORKSPACE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '工作空间操作记录表';
+
+-- ----------------------------
+-- Table structure for T_WORKSPACE_OP_HIS
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `T_WORKSPACE_HISTORY` (
+                                                     `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+                                                     `WORKSPACE_ID` bigint(20) NOT NULL DEFAULT 0 COMMENT '工作空间ID',
+                                                     `STARTER` varchar(64) NOT NULL DEFAULT '' COMMENT '启动人',
+                                                     `STOPPER` varchar(64) NOT NULL DEFAULT '' COMMENT '停止人',
+                                                     `START_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',
+                                                     `END_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '结束时间',
+                                                     `UPDATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+                                                     `CREATED_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+
+                                                     PRIMARY KEY (`ID`) USING BTREE,
+                                                     KEY `uni_1` (`WORKSPACE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '工作空间运行记录表';
+
+-- ----------------------------
+-- Table structure for T_WORKSPACE_OP_HIS
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `T_REMOTE_DEV_SETTINGS` (
+                                                       `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+                                                       `USER_ID` varchar(64) NOT NULL DEFAULT '' COMMENT '用户',
+                                                       `DEFAULT_SHELL` varchar(10) NOT NULL DEFAULT '' COMMENT '默认shell: zsh | bash',
+                                                       `APPEARANCE` varchar(64) NOT NULL DEFAULT '' COMMENT '外观',
+                                                       `LANGUAGE` varchar(64) NOT NULL DEFAULT '' COMMENT '语言',
+                                                       `VOICE_NOTICE` tinyint NOT NULL DEFAULT 0 COMMENT '是否开启声音提醒',
+                                                       `DOCK_NOTICE` tinyint NOT NULL DEFAULT 0 COMMENT '是否开启DOCK显示',
+                                                       `GIT_ATTACHED` tinyint NOT NULL DEFAULT 0 COMMENT '是否连接git',
+                                                       `TAPD_ATTACHED` tinyint NOT NULL DEFAULT 0 COMMENT '是否连接TAPD',
+                                                       `GITHUB_ATTACHED` tinyint NOT NULL DEFAULT 0 COMMENT '是否连接github',
+                                                       `ENV` varchar(1024) NOT NULL DEFAULT '' COMMENT '环境变量配置',
+                                                       `FILE_CONTENT` longtext NOT NULL DEFAULT '' COMMENT '文件内容',
+                                                       `UPDATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+                                                       `CREATED_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+
+                                                       PRIMARY KEY (`ID`) USING BTREE,
+                                                       KEY `uni_1` (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '用户远程开发配置表';
