bd109ebaed6b6c48c3a6a82cf5728248774f1616
stubenhuang <stubenhuang@tencent.com>
2022-11-21 10:35:21 +0800

    feat: kotlin升级 #7990

--- src/backend/ci/buildSrc/src/main/kotlin/plugins/task-gen-jooq.gradle.kts
+++ src/backend/ci/buildSrc/src/main/kotlin/plugins/task-gen-jooq.gradle.kts
@@ -24,7 +24,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-import gradle.kotlin.dsl.accessors._aea957cc1a32034839ddc7a6074ba75d.main
 import nu.studer.gradle.jooq.JooqGenerate
 
 plugins {
