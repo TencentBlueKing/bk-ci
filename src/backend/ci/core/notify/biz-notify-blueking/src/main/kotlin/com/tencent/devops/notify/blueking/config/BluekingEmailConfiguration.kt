de39339585de5293b4f346dbc841bba4cc4b1308
yongyiduan <yongyiduan@tencent.com>
2022-11-21 15:35:09 +0800

    feat: notify新增tencentCloud 邮件发送能力 #7760 core

--- src/backend/ci/core/notify/biz-notify-blueking/src/main/kotlin/com/tencent/devops/notify/blueking/config/BluekingEmailConfiguration.kt
+++ src/backend/ci/core/notify/biz-notify-blueking/src/main/kotlin/com/tencent/devops/notify/blueking/config/BluekingEmailConfiguration.kt
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 import com.tencent.devops.common.notify.utils.Configuration as NotifyConfig
 
 @Configuration
-class EmailConfiguration {
+class BluekingEmailConfiguration {
 
     @Bean
     @ConditionalOnProperty(prefix = "notify", name = ["emailChannel"], havingValue = "blueking")
--- src/backend/ci/core/notify/biz-notify-tencentcloud/src/main/kotlin/com/tencent/devops/notify/tencentcloud/config/TencentClouldEmailConfiguration.kt
+++ src/backend/ci/core/notify/biz-notify-tencentcloud/src/main/kotlin/com/tencent/devops/notify/tencentcloud/config/TencentClouldEmailConfiguration.kt
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class EmailConfiguration {
+class TencentClouldEmailConfiguration {
 
     @Bean
     @ConditionalOnProperty(prefix = "notify", name = ["emailChannel"], havingValue = "tencentCloud")
