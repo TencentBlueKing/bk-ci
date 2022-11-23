e35767de3eba1827f26802f7daf4b0a84caddf6a
stubenhuang <stubenhuang@tencent.com>
2022-11-23 14:59:02 +0800

    feat: 流水线组及流水线列表页优化 #7101

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineInfoFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineInfoFacadeService.kt
@@ -523,7 +523,13 @@ class PipelineInfoFacadeService @Autowired constructor(
                 defaultMessage = "指定要复制的流水线-模型不存在"
             )
         try {
-            val copyMode = Model(pipelineCopy.name, pipelineCopy.desc ?: model.desc, model.stages)
+            val copyMode = Model(
+                name = pipelineCopy.name,
+                desc = pipelineCopy.desc ?: model.desc,
+                stages = model.stages,
+                staticViews = pipelineCopy.staticViews,
+                labels = pipelineCopy.labels
+            )
             modelCheckPlugin.clearUpModel(copyMode)
             val newPipelineId = createPipeline(userId, projectId, copyMode, channelCode)
             val settingInfo = pipelineSettingFacadeService.getSettingInfo(projectId, pipelineId)
@@ -542,18 +548,6 @@ class PipelineInfoFacadeService @Autowired constructor(
                     dispatchPipelineUpdateEvent = false
                 )
             }
-            // 添加标签
-            pipelineGroupService.addPipelineLabel(
-                userId = userId,
-                projectId = projectId,
-                pipelineId = newPipelineId,
-                labelIds = pipelineCopy.labels
-            )
-
-            // 添加到静态分组
-            val bulkAdd = PipelineViewBulkAdd(pipelineIds = listOf(newPipelineId), viewIds = pipelineCopy.staticViews)
-            pipelineViewGroupService.bulkAdd(userId, projectId, bulkAdd)
-
             return newPipelineId
         } catch (e: JsonParseException) {
             logger.error("Parse process($pipelineId) fail", e)
