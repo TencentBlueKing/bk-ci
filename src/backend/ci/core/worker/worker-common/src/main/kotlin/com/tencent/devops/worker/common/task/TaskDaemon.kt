5690621479ebbceff96152ef2dab3cf9fa80c98f
royalhuang <royalhuang@tencent.com>
2022-11-25 16:10:59 +0800

    feat: 日志脱敏 #4273 调整成内部不屏蔽

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/TaskDaemon.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/TaskDaemon.kt
@@ -109,10 +109,10 @@ class TaskDaemon(
                     )
                     return@forEach
                 }
-                if (SensitiveValueService.matchSensitiveValue(value)) {
-                    LoggerService.addWarnLine("Warning, credentials cannot be assigned to variable[$key]")
-                    return@forEach
-                }
+//                if (SensitiveValueService.matchSensitiveValue(value)) {
+//                    LoggerService.addWarnLine("Warning, credentials cannot be assigned to variable[$key]")
+//                    return@forEach
+//                }
                 buildResult[key] = value
             }
         }
