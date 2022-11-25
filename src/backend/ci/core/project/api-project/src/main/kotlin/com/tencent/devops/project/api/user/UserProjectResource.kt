2332b8ac7e2970b92c8f91a91caacf236f45f469
greysonfang <greysonfang@tencent.com>
2022-11-25 19:24:01 +0800

    --story=878402073 feat: 创建项目流程优化

--- src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/api/user/UserProjectResource.kt
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/api/user/UserProjectResource.kt
@@ -78,7 +78,7 @@ interface UserProjectResource {
         enabled: Boolean?,
         @ApiParam("是否拉取未审批通过的项目，若为false，会拉取审批[未通过+通过]的项目", required = false)
         @QueryParam("approved")
-        approved: Boolean? = true
+        approved: Boolean?
     ): Result<List<ProjectVO>>
 
     @GET
