f65edd29a63c2fe7ee1fb47e06abf23655887bf0
sawyersong <sawyersong@tencent.com>
2022-11-28 11:07:52 +0800

    --story=879960659 创建和启动工作空间（增删查）

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/client/KubernetesRemoteDevClient.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/client/KubernetesRemoteDevClient.kt
@@ -38,6 +38,7 @@ import com.tencent.devops.dispatch.kubernetes.common.ErrorCodeEnum
 import com.tencent.devops.dispatch.kubernetes.pojo.JobStatus
 import com.tencent.devops.dispatch.kubernetes.pojo.KubernetesResult
 import com.tencent.devops.dispatch.kubernetes.pojo.KubernetesWorkspace
+import com.tencent.devops.dispatch.kubernetes.pojo.KubernetesWorkspaceUrlRsp
 import com.tencent.devops.dispatch.kubernetes.pojo.TaskResp
 import com.tencent.devops.dispatch.kubernetes.pojo.getCodeMessage
 import okhttp3.MediaType
@@ -179,12 +180,12 @@ class KubernetesRemoteDevClient @Autowired constructor(
                         "获取工作空间url接口异常（Fail to get workspaceUrl, http response code: ${response.code()}"
                     )
                 }
-                val result: KubernetesResult<String> = objectMapper.readValue(responseContent)
+                val result: KubernetesResult<KubernetesWorkspaceUrlRsp> = objectMapper.readValue(responseContent)
                 if (result.isNotOk()) {
                     throw RuntimeException(result.message)
                 }
 
-                return result.data
+                return result.data!!.webVscodeUrl
             }
         } catch (e: Exception) {
             logger.error("[$userId $workspaceName get workspaceUrl failed.", e)
---
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/KubernetesWorkspaceUrlRsp.kt
@@ -0,0 +1,25 @@
+package com.tencent.devops.dispatch.kubernetes.pojo
+
+/*
+{
+    "workspaceId": "remoting-ruotian12138",
+    "userId": "ruotiantang",
+    "gitRepo": {
+        "gitRepoName": "go-test",
+        "gitRepoRef": "master"
+    },
+    "gitUsername": "ruotiantang",
+    "gitEmail": "ruotiantang@tencent.com",
+    "userFiles": [
+        {
+            "from": "/data/apps/a.yaml",
+            "to": "/data/landun/workspace/go-test"
+        }
+    ]
+}
+ */
+data class KubernetesWorkspaceUrlRsp(
+    val webVscodeUrl: String,
+    val sshUrl: String,
+    val apiUrl: GitRepo
+)
