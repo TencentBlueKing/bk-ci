054228364ee6953e838af61227fc1e46f95d98fe
sawyersong <sawyersong@tencent.com>
2022-11-28 11:15:35 +0800

    --story=879960659 创建和启动工作空间（增删查）

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/KubernetesWorkspaceUrlRsp.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/KubernetesWorkspaceUrlRsp.kt
@@ -21,5 +21,5 @@ package com.tencent.devops.dispatch.kubernetes.pojo
 data class KubernetesWorkspaceUrlRsp(
     val webVscodeUrl: String,
     val sshUrl: String,
-    val apiUrl: GitRepo
+    val apiUrl: String
 )
