223ec0a60c8a9646ab09685affa58bc14e6049d6
yongyiduan <yongyiduan@tencent.com>
2022-11-28 17:08:50 +0800

    repository 接口通用性改动 #8051

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/git/service/TGitApiService.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/git/service/TGitApiService.kt
@@ -277,7 +277,7 @@ class TGitApiService @Autowired constructor(
             apiErrorCode = ErrorCodeEnum.GET_GIT_FILE_TREE_ERROR
         ) {
             client.get(ServiceGitResource::class).getGitFileTree(
-                gitProjectId = gitProjectId.toLong(),
+                gitProjectId = gitProjectId,
                 path = path ?: "",
                 token = cred.toToken(),
                 ref = ref,
