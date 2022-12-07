fe9b3b451e6edc72304b80ba33871944dd589002
v_ayzyzhong <v_ayzyzhong@tencent.com>
2022-12-07 17:44:25 +0800

    bug: 质量红线生效流水线信息在红线列表页面和详情页面数据不一致 #8073  :打印日志

--- src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleService.kt
+++ src/backend/ci/core/quality/biz-quality/src/main/kotlin/com/tencent/devops/quality/service/v2/QualityRuleService.kt
@@ -279,11 +279,11 @@ class QualityRuleService @Autowired constructor(
         }
         val templateRange = templateMap?.map { UserQualityRule.RangeItem(it.key, it.value.name) } ?: listOf()
 
-        logger.info("QualityRuleService userGetRule range: [$range.size]", )
+        logger.info("QualityRuleService userGetRule range: [$range.size]")
         // 获取模板对应的流水线总数
         val templatePipelineCount = client.get(ServiceTemplateInstanceResource::class)
             .countTemplateInstance(projectId, templateIds).data ?: 0
-        logger.info("QualityRuleService userGetRule template pipelineCount: [$templatePipelineCount]", )
+        logger.info("QualityRuleService userGetRule template pipelineCount: [$templatePipelineCount]")
 
         return UserQualityRule(
             hashId = rule.hashId,
