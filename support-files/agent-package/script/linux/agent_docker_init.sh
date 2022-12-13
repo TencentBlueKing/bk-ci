89e8e078e56e6ebe4371817546ed761b25010049
ruotiantang <ruotiantang@tencent.com>
2022-12-13 12:18:52 +0800

    Agent支持启动docker镜像作为构建机 #7748

--- support-files/agent-package/script/linux/agent_docker_init.sh
+++ support-files/agent-package/script/linux/agent_docker_init.sh
@@ -2,7 +2,7 @@
 set -x
 mkdir  -p /data/devops
 cd /data/devops
-mkdir -p logs
+mkdir -p /data/logs
 echo "start to download the docker_init.sh..." > /data/logs/docker.log
 curl -k -s -H "X-DEVOPS-BUILD-TYPE: DOCKER" -H "X-DEVOPS-PROJECT-ID: ${devops_project_id}" -H "X-DEVOPS-AGENT-ID: ${devops_agent_id}" -H "X-DEVOPS-AGENT-SECRET-KEY: ${devops_agent_secret_key}" -o  docker_init.sh "${devops_gateway}/static/local/files/docker_init.sh" -L
 echo docker_init
