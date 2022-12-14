2e5e65c39094af2e86a114edd1ad300e3c51f2a3
v_yjieliang <v_yjieliang@tencent.com>
2022-12-14 11:49:58 +0800

    bug：metrics插件趋势错误流水线构建页面的错误码要按错误类型查询 #8103

--- support-files/sql/2010_ci_metrics-update_v1.10_mysql.sql
+++ support-files/sql/2010_ci_metrics-update_v1.10_mysql.sql
@@ -36,19 +36,19 @@ BEGIN
 
     DECLARE db VARCHAR(100);
     SET AUTOCOMMIT = 0;
-SELECT DATABASE() INTO db;
+    SELECT DATABASE() INTO db;
 
-IF NOT EXISTS(SELECT 1
-                  FROM information_schema.COLUMNS
-                  WHERE TABLE_SCHEMA = db
-                    AND TABLE_NAME = 'T_ERROR_CODE_INFO'
-                    AND COLUMN_NAME = 'ATOM_CODE') THEN
-ALTER TABLE `T_ERROR_CODE_INFO`
-    ADD COLUMN `ATOM_CODE` varchar(64) DEFAULT NULL COMMENT '关联插件代码'
-	
-END IF;
+    IF NOT EXISTS(SELECT 1
+                      FROM information_schema.COLUMNS
+                      WHERE TABLE_SCHEMA = db
+                        AND TABLE_NAME = 'T_ERROR_CODE_INFO'
+                        AND COLUMN_NAME = 'ATOM_CODE') THEN
+    ALTER TABLE `T_ERROR_CODE_INFO`
+        ADD COLUMN `ATOM_CODE` varchar(64) DEFAULT NULL COMMENT '关联插件代码'
 
-COMMIT;
+    END IF;
+
+    COMMIT;
 END <CI_UBF>
 DELIMITER ;
 COMMIT;
