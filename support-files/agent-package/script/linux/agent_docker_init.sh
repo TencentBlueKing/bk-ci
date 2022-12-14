716384b69dce63fc2531d5a80bd08e92723c98dd
ruotiantang <ruotiantang@tencent.com>
2022-12-14 11:16:16 +0800

    Agent支持启动docker镜像作为构建机 #7748

--- support-files/agent-package/script/linux/agent_docker_init.sh
+++ support-files/agent-package/script/linux/agent_docker_init.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-set -x
-mkdir  -p /data/devops
-cd /data/devops
-mkdir -p /data/logs
+# 本脚本应该兼容 POSIX shell.
+
+CI_DIR="/data"
+CI_LOG_DIR="$CI_DIR/logs"
+CI_LOG_FILE="$CI_LOG_DIR/docker.log"
+
+ci_log() {
+   date=$(date +%Y%m%d-%H%M%S)
+   logl=${ci_log_LEVEL:-INFO}
+   msg="$date $logl $*"
+   if [ -n "$CI_LOG_FILE" ]; then echo "$msg" >>"$CI_LOG_FILE"; fi
+   echo "$msg" >&2
+}
 
 unset_proxy() {
     unset http_proxy
@@ -23,21 +32,27 @@ cert_update() {
     update-ca-trust
 }
 
-echo "unset proxy" > /data/logs/docker.log
+mkdir  -p /data/devops
+cd /data/devops
+mkdir -p /data/logs
+
+ci_log "docker_init.sh was launched."
+
+ci_log "unset proxy"
 unset_proxy
-echo "git.code.oa.com certs update" >> /data/logs/docker.log
+ci_log "git.code.oa.com certs update"
 cert_update
 
 yum clean all || echo no yum
 rm -rf /var/lib/rpm/__db*
 rpm --rebuilddb || echo no rpm
 
-echo "start to download the docker_init.sh..." > /data/logs/docker.log
+ci_log "start to download the docker_init.sh..."
+
 curl -k -s -H "X-DEVOPS-BUILD-TYPE: DOCKER" -H "X-DEVOPS-PROJECT-ID: ${devops_project_id}" -H "X-DEVOPS-AGENT-ID: ${devops_agent_id}" -H "X-DEVOPS-AGENT-SECRET-KEY: ${devops_agent_secret_key}" -o  docker_init.sh "${devops_gateway}/static/bkrepo/files/docker_init.sh" -L
-echo docker_init
-cat docker_init.sh
-echo "download docker_init.sh success, start it..." >> /data/logs/docker.log
-cat docker_init.sh >> /data/logs/docker.log
+
+ci_log "download docker_init.sh success, start it..."
+ci_log $(cat docker_init.sh)
 
 chmod +x ./docker_init.sh
 exec ./docker_init.sh "$@"
