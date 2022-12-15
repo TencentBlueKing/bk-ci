bcfe874c802f6e8d1806ef6e7e2cc5b89d74d6bd
stubenhuang <stubenhuang@tencent.com>
2022-12-15 23:27:42 +0800

    feat: 流水线组及流水线列表页优化 #7101

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResource.kt
@@ -21,7 +21,7 @@ interface OpPipelineViewResource {
     @ApiOperation("初始化所有动态视图")
     @GET
     @Path("/initAllView")
-    fun getViewSettings(
+    fun initAllView(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String
--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResourceImpl.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResourceImpl.kt
@@ -2,16 +2,15 @@ package com.tencent.devops.process.api.op
 
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
-import com.tencent.devops.process.dao.label.PipelineViewDao
-import com.tencent.devops.process.dao.label.PipelineViewGroupDao
+import com.tencent.devops.process.service.view.PipelineViewGroupService
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
 class OpPipelineViewResourceImpl @Autowired constructor(
-    private val pipelineViewDao: PipelineViewDao,
-    private val pipelineViewGroupDao: PipelineViewGroupDao
-) : OpPipelineViewResource{
-    override fun getViewSettings(userId: String): Result<Boolean> {
-        TODO("Not yet implemented")
+    private val pipelineViewGroupService: PipelineViewGroupService
+) : OpPipelineViewResource {
+    override fun initAllView(userId: String): Result<Boolean> {
+        pipelineViewGroupService.initAllView()
+        return Result(true)
     }
 }
