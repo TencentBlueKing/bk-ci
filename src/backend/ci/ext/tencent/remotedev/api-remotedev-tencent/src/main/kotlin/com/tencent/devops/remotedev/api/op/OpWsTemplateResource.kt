5337a24bd7f5279307388bcca3a1789ae33d56cc
randychen <randychen@tencent.com>
2022-12-19 10:26:33 +0800

    --story=880368107 云研发模板配置接入op平台

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpWsTemplateResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpWsTemplateResource.kt
@@ -40,6 +40,7 @@ import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.PUT
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
@@ -52,7 +53,7 @@ import javax.ws.rs.core.MediaType
 interface OpWsTemplateResource {
 
     @ApiOperation("新增工作空间模板")
-    @GET
+    @POST
     @Path("/add")
     fun addWorkspaceTemplate(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
