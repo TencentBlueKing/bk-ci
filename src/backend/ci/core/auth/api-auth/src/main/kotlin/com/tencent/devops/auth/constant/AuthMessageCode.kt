c9412168616d7b832b60080c7751cc60f1f7ab00
mingshewhe <mingshewhe@tencent.com>
2022-12-20 11:48:09 +0800

    feat: 蓝盾对接权限中心RBAC #7794

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/constant/AuthMessageCode.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/constant/AuthMessageCode.kt
@@ -57,6 +57,7 @@ object AuthMessageCode {
     const val IAM_SYSTEM_ERROR = "2121012" // 权限系统：Iam权限中心异常。异常信息{0}
     const val USER_NOT_EXIST = "2121013" // 权限系统: 用户中心非法用户/组织
     const val RESOURCE_NOT_FOUND = "2121014" // 权限系统：资源{0}不存在
+    const val DEFAULT_GROUP_NOT_FOUND = "2121015" // 权限系统：资源类型{0}关联的默认组{1}不存在
 
     const val TOKEN_TICKET_FAIL = "2121106" // 权限系统：token校验失败
     const val PARENT_TYPE_FAIL = "2121107" // 权限系统：父类资源必须为"项目"
