291b692b300d063a31c8a0eb24c17c104a1408ab
stubenhuang <stubenhuang@tencent.com>
2022-12-27 16:46:44 +0800

    feat: 支持ipv6单栈 #8110

--- src/backend/ci/ext/tencent/common/common-notify-tencent/src/main/kotlin/com/tencent/devops/common/notify/utils/TOFService.kt
+++ src/backend/ci/ext/tencent/common/common-notify-tencent/src/main/kotlin/com/tencent/devops/common/notify/utils/TOFService.kt
@@ -42,7 +42,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import sun.misc.BASE64Decoder
-import java.util.Locale
 import java.util.Random
 
 @Service
