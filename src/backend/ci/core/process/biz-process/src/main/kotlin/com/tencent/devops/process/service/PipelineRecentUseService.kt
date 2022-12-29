ed66f46543996ab03697ff28893cf70e1dd673f7
stubenhuang <stubenhuang@tencent.com>
2022-12-29 16:08:20 +0800

    【产品-无需评审会】增加“最近使用的流水线”功能及相关优化 #8183

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineRecentUseService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineRecentUseService.kt
@@ -2,7 +2,6 @@ package com.tencent.devops.process.service
 
 import com.tencent.devops.process.dao.PipelineRecentUseDao
 import org.jooq.DSLContext
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
