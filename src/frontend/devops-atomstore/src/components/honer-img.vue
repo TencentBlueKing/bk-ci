7ca7bbf38790b1509b30c76d3eb2299ec6721910
xuzhan <xuzhan@tencent.com>
2023-01-03 15:08:48 +0800

    feature: 流水线插件支持展示获得的荣誉和SLA等信息 issue #8114

---
+++ src/frontend/devops-atomstore/src/components/honer-img.vue
@@ -0,0 +1,73 @@
+<template>
+    <section class="honer-img">
+        <section class="img-wrapper">
+            <img class="img-self" :src="detail.logoUrl">
+            <template v-if="recentHoner">
+                <img class="honer-self" src="../images/honer.png">
+                <span
+                    :class="{
+                        'honer-txt': true,
+                        'is-big': isBig
+                    }"
+                >{{ recentHoner.honorTitle }}</span>
+            </template>
+        </section>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            detail: Object,
+            isBig: Boolean
+        },
+
+        computed: {
+            recentHoner () {
+                return this.detail?.honorInfos?.[0]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .honer-img {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        .img-wrapper {
+            position: absolute;
+            top: 2%;
+            left: 7%;
+            right: 7%;
+            bottom: 11%;
+            background: #F5F7FA;
+            border-radius: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .img-self {
+            width: 69%;
+            height: 69%;
+        }
+        .honer-self {
+            position: absolute;
+            width: 130%;
+            height: 130%;
+            left: -16%;
+            bottom: -13px;
+        }
+        .honer-txt {
+            position: absolute;
+            bottom: -2px;
+            color: #FFFFFF;
+            font-size: 12px;
+            scale: 83%;
+            &.is-big {
+                scale: 100%;
+                bottom: 3%;
+            }
+        }
+    }
+</style>
---
+++ src/frontend/devops-pipeline/src/components/AtomSelector/honer-tag.vue
@@ -0,0 +1,86 @@
+<template>
+    <section class="honer-tags">
+        <span
+            v-for="renderHoner in renderHoners"
+            :key="renderHoner.honorId"
+            class="honer-tag"
+        >
+            <img class="tag-image" src="../../images/honer-left.png">
+            <span class="tag-txt">
+                <span class="tag-txt-main">{{ renderHoner.honorTitle }}</span>
+                <img class="tag-txt-image" src="../../images/honer-center.png">
+            </span>
+            <img class="tag-image" src="../../images/honer-right.png">
+        </span>
+        <span v-if="honorInfos.length > maxNum" class="honer-num">
+            +{{ honorInfos.length - maxNum }}
+        </span>
+        <span class="honer-gap" v-if="renderHoners.length"></span>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            detail: Object,
+            maxNum: Number
+        },
+
+        computed: {
+            honorInfos () {
+                return this.detail?.honorInfos || []
+            },
+
+            renderHoners () {
+                return this.honorInfos.slice(0, this.maxNum)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .honer-tags {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .honer-tag {
+            display: flex;
+            align-items: center;
+            margin-right: 4px;
+        }
+        .tag-txt {
+            line-height: 16px;
+            position: relative;
+        }
+        .tag-txt-main {
+            position: relative;
+            z-index: 11;
+            font-size: 12px;
+            color: #fff;
+        }
+        .tag-txt-image {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            width: 100%;
+            height: 16px;
+            z-index: 10;
+        }
+        .tag-image {
+            height: 19px;
+        }
+        .honer-num {
+            color: #FF9C01;
+            margin-right: 8px;
+        }
+        .honer-gap {
+            display: inline-block;
+            width: 1px;
+            height: 16px;
+            margin-right: 8px;
+            background: #DCDEE5;
+        }
+    }
+</style>
