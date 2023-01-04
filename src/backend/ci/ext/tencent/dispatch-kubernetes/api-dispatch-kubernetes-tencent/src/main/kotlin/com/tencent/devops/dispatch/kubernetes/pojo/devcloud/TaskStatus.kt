d7714d0faa0f3fb8c742ac928e1937b1b59a6e79
sawyersong <sawyersong@tencent.com>
2023-01-04 17:38:50 +0800

    --story=879960659 创建和启动工作空间（增删查）

--- src/backend/ci/ext/tencent/dispatch-kubernetes/api-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/devcloud/TaskStatus.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/api-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/devcloud/TaskStatus.kt
@@ -2,8 +2,8 @@ package com.tencent.devops.dispatch.kubernetes.pojo.devcloud
 
 data class TaskStatus(
     val uid: String,
-    val createdAt: Proto3Timestamp? = null,
-    val updatedAt: Proto3Timestamp? = null,
+    val createdAt: String? = null,
+    val updatedAt: String? = null,
     val status: TaskStatusEnum? = null,
     val statuscode: Int? = null,
     val logs: List<String> = emptyList()
