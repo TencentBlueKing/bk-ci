8dbab8284962f2a3d640b7667a8e5d15b3bccf02
royalhuang <royalhuang@tencent.com>
2023-01-09 15:00:48 +0800

    feat：流水线构建详情页重构需求 #7983 处理detekt检查问题

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/utils/ModelUtils.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/utils/ModelUtils.kt
@@ -42,7 +42,7 @@ import com.tencent.devops.common.pipeline.pojo.element.RunCondition
 import com.tencent.devops.common.pipeline.pojo.element.trigger.ManualTriggerElement
 import com.tencent.devops.common.pipeline.pojo.element.trigger.RemoteTriggerElement
 
-@Suppress(    "ComplexMethod")
+@Suppress("ComplexMethod")
 object ModelUtils {
 
     /**
