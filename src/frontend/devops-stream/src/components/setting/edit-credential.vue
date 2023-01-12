b142fd9c3826d983d2c470c724b8fab194f58bb0
xuzhan <xuzhan@tencent.com>
2023-01-12 20:11:47 +0800

    feature: repo-hook 监听凭据支持 OAUTH token #8216

--- src/frontend/devops-stream/src/components/setting/edit-credential.vue
+++ src/frontend/devops-stream/src/components/setting/edit-credential.vue
@@ -152,10 +152,10 @@
                     },
                     {
                         id: 'OAUTHTOKEN',
-                        name: this.$t('setting.ticket.oauthToken'),
+                        name: 'OauthToken',
                         desc: this.$t('setting.ticket.oauthTokenDesc'),
                         content: [
-                            { id: 'v1', label: this.$t('setting.ticket.oauthToken'), type: 'password', required: true, rules: [this.requireRule('oauth_token')], placeholder: this.$t('setting.ticket.oauthTokenDesc') }
+                            { id: 'v1', label: 'OauthToken', type: 'password', required: true, rules: [this.requireRule('oauth_token')], placeholder: this.$t('setting.ticket.oauthTokenPlaceholder') }
                         ]
                     }
                 ],
