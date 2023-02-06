55154f2ee42e15022a5130a48c08d5e1ab59acd9
stubenhuang <stubenhuang@tencent.com>
2023-02-06 20:59:12 +0800

    feat: 通过网关进行的跨服务调用, 必须携带token进行校验 #8317

--- src/backend/ci/core/buildless/biz-buildless/src/main/kotlin/com/tencent/devops/buildless/client/DispatchClient.kt
+++ src/backend/ci/core/buildless/biz-buildless/src/main/kotlin/com/tencent/devops/buildless/client/DispatchClient.kt
@@ -48,7 +48,6 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
