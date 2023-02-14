88bd755249b6cb425ef088eb5ae35982ccbeefbe
randychen <randychen@tencent.com>
2023-02-14 19:13:04 +0800

    --story=882205269 提供更新bkticket接口

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/WorkspaceService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/WorkspaceService.kt
@@ -83,6 +83,7 @@ import com.tencent.devops.remotedev.websocket.pojo.WebSocketActionType
 import com.tencent.devops.remotedev.websocket.push.WorkspaceWebsocketPush
 import com.tencent.devops.scm.enums.GitAccessLevelEnum
 import com.tencent.devops.scm.utils.code.git.GitUtils
+import okhttp3.MediaType
 import okhttp3.Request
 import okhttp3.RequestBody
 import org.jooq.DSLContext
@@ -1436,11 +1437,11 @@ class WorkspaceService @Autowired constructor(
         val request = Request.Builder()
             .url(commonService.getProxyUrl(url))
             .header("Cookie", "X-DEVOPS-BK-TICKET=$bkTicket")
-            .post(RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), JsonUtil.toJson(params)))
+            .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), JsonUtil.toJson(params)))
             .build()
 
         OkhttpUtils.doHttp(request).use { response ->
-            val data = response.body!!.string()
+            val data = response.body()!!.string()
             val dataMap = JsonUtil.toMap(data)
             val status = dataMap["status"]
             return (status == 0)
