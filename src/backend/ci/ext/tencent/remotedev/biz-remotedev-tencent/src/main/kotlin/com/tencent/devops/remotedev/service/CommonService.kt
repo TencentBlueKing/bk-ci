b220b977729d80d78c0967606f2a2d7d510c42c0
randychen <randychen@tencent.com>
2023-02-14 18:16:12 +0800

    --story=882205269 提供更新bkticket接口

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/CommonService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/CommonService.kt
@@ -2,8 +2,10 @@ package com.tencent.devops.remotedev.service
 
 import com.tencent.devops.common.service.config.CommonConfig
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 import java.net.URLEncoder
 
+@Service
 class CommonService @Autowired constructor(
     private val commonConfig: CommonConfig
 ) {
