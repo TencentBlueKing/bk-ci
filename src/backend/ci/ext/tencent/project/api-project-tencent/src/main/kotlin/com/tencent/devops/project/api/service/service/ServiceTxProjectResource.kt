4bd58025f5056f75c2d47a708b3102b9398cb2fb
irwinsun <irwinsun@tencent.com>
2023-02-15 12:19:24 +0800

    流水线异常监控机制完善 #8366 清理无用Error日志：Project的无用Error日志

--- src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/service/ServiceTxProjectResource.kt
+++ src/backend/ci/ext/tencent/project/api-project-tencent/src/main/kotlin/com/tencent/devops/project/api/service/service/ServiceTxProjectResource.kt
@@ -61,6 +61,7 @@ import javax.ws.rs.core.MediaType
 @Path("/service/projects/tx")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Suppress("TooManyFunctions", "LongParameterList")
 interface ServiceTxProjectResource {
 
     @GET
