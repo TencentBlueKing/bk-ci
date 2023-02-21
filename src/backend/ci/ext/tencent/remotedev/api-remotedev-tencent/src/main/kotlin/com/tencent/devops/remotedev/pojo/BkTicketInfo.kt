5c9905fcd0546b0258af424a1f0eedb6b75977f8
randychen <randychen@tencent.com>
2023-02-21 18:01:52 +0800

    --story=882205269 提供更新bkticket接口

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/BkTicketInfo.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/BkTicketInfo.kt
@@ -2,8 +2,6 @@ package com.tencent.devops.remotedev.pojo
 
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
-import io.swagger.annotations.ApiParam
-import javax.ws.rs.QueryParam
 
 @ApiModel("更新bkticket信息")
 data class BkTicketInfo(
