88acd2231635000b0118884a534e09682e9182ce
yongyiduan <yongyiduan@tencent.com>
2023-02-28 13:08:49 +0800

    --story=882548483 客户端白名单接口设置

--- src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/ApigwRemoteDevResourceImpl.kt
+++ src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/ApigwRemoteDevResourceImpl.kt
@@ -2,12 +2,14 @@ package com.tencent.devops.openapi.resources.apigw
 
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.openapi.api.apigw.ApigwRemoteDevResource
 import com.tencent.devops.project.api.service.service.ServiceTxProjectResource
 import com.tencent.devops.remotedev.api.service.ServiceRemoteDevResource
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 
+@RestResource
 class ApigwRemoteDevResourceImpl @Autowired constructor(private val client: Client)
     : ApigwRemoteDevResource {
     override fun updateClientVersion(
