7612e7e6c23311f8b5bbe0c88000ec8687ec2114
yjieliang <yundantengteng@163.com>
2023-03-03 15:28:25 +0800

    pref:完善部分build类接口权限校验 #8256

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/builds/BuildBuildResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/builds/BuildBuildResource.kt
@@ -33,7 +33,6 @@ import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_PROJECT_ID
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.pipeline.enums.ChannelCode
 import com.tencent.devops.common.web.annotation.BuildApiPermission
-import com.tencent.devops.common.web.constant.BuildApiHandleType
 import com.tencent.devops.common.web.constant.BuildApiHandleType.UNAUTHORIZED
 import com.tencent.devops.process.pojo.BuildHistory
 import com.tencent.devops.process.pojo.pipeline.ModelDetail
