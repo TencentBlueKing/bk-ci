afb9c36661cecb47857fe13f6dafb2770f2dea0b
royalhuang <royalhuang@tencent.com>
2023-03-03 20:19:45 +0800

    feat：流水线构建详情页重构需求 #7983 固化矩阵生成的taskId

--- src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/container/impl/InitializeMatrixGroupStageCmd.kt
+++ src/backend/ci/core/process/biz-engine/src/main/kotlin/com/tencent/devops/process/engine/control/command/container/impl/InitializeMatrixGroupStageCmd.kt
@@ -48,7 +48,6 @@ import com.tencent.devops.common.pipeline.pojo.element.quality.QualityGateOutEle
 import com.tencent.devops.common.redis.RedisOperation
 import com.tencent.devops.process.engine.atom.parser.DispatchTypeParser
 import com.tencent.devops.process.engine.cfg.ModelContainerIdGenerator
-import com.tencent.devops.process.engine.cfg.ModelTaskIdGenerator
 import com.tencent.devops.process.engine.common.VMUtils
 import com.tencent.devops.process.engine.context.MatrixBuildContext
 import com.tencent.devops.process.engine.control.command.CmdFlowState
@@ -553,9 +552,14 @@ class InitializeMatrixGroupStageCmd(
         contextCaseHash: Int
     ): List<MatrixStatusElement> {
         val originToNewId = mutableMapOf<String, String>()
-        return elements.map { e ->
+        return elements.mapIndexed { index, e ->
             // 每次写入TASK表都要是新获取的taskId，统一调整为不可重试
-            val newTaskId = "${e.id!!}-$contextCaseHash"
+            // 保障同变量组合生成的id相同，如果elementId出现不替换部分重复，则对hash值增加相同的偏移量
+            val newTaskId = VMUtils.getMatrixElementId(e.id!!, contextCaseHash.toString()).let {
+                if (originToNewId.containsKey(it)) {
+                    VMUtils.getMatrixElementId(e.id!!, (contextCaseHash + index).toString())
+                } else it
+            }
             // 记录所有新ID对应的原ID，并将post-action信息更新父插件的ID
             originToNewId[e.id!!] = newTaskId
             e.additionalOptions?.elementPostInfo?.parentElementId?.let { originId ->
