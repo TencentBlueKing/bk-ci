aa2bc71e67459277a75bd262ddb6ed790e1da067
greysonfang <greysonfang@tencent.com>
2023-03-06 16:18:05 +0800

    feat: 蓝盾对接权限中心RBAC #7794

---
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/user/UserPermissionAuthResource.kt
@@ -0,0 +1,39 @@
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
+import com.tencent.devops.common.api.pojo.Result
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+
+@Api(tags = ["USER_PERMISSION"], description = "用户-权限校验")
+@Path("/user/auth/permission")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+interface UserPermissionAuthResource {
+    @GET
+    @Path("/{projectCode}/validate/batch")
+    @ApiOperation("批量校验用户是否拥有某个资源实例的操作")
+    fun batchValidateUserResourcePermission(
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        @ApiParam("待校验用户ID", required = true)
+        userId: String,
+        @PathParam("projectCode")
+        @ApiParam("项目ID", required = true)
+        projectCode: String,
+        @QueryParam("resourceType")
+        @ApiParam("资源类型", required = true)
+        resourceType: String,
+        @QueryParam("resourceCode")
+        @ApiParam("资源code", required = true)
+        resourceCode: String,
+        @ApiParam("action类型列表", required = true)
+        action: List<String>
+    ): Result<Map<String, Boolean>>
+}
