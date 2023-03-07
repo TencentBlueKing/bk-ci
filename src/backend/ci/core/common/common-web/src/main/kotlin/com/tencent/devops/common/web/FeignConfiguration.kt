5e202ef4c8f94ddce714deb8a4564a72d8553a82
carlyin <449036249@qq.com>
2023-03-07 12:26:59 +0800

    pref:蓝盾国际化方案优化 #8211

--- src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/FeignConfiguration.kt
+++ src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/FeignConfiguration.kt
@@ -29,6 +29,7 @@ package com.tencent.devops.common.web
 
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_JWT_TOKEN
 import com.tencent.devops.common.api.auth.AUTH_HEADER_GATEWAY_TAG
+import com.tencent.devops.common.api.constant.REQUEST_CHANNEL
 import com.tencent.devops.common.security.jwt.JwtManager
 import com.tencent.devops.common.security.util.EnvironmentUtil
 import com.tencent.devops.common.service.BkTag
@@ -91,7 +92,12 @@ class FeignConfiguration @Autowired constructor(
             if (!languageHeaderValue.isNullOrBlank()) {
                 requestTemplate.header(languageHeaderName, languageHeaderValue) // 设置Accept-Language请求头
             }
-
+            // 设置请求渠道信息
+            val requestChannel =
+                (request.getAttribute(REQUEST_CHANNEL) ?: request.getHeader(REQUEST_CHANNEL))?.toString()
+            if (!requestChannel.isNullOrBlank()) {
+                requestTemplate.header(REQUEST_CHANNEL, requestChannel)
+            }
             val cookies = request.cookies
             if (cookies != null && cookies.isNotEmpty()) {
                 val cookieBuilder = StringBuilder()
