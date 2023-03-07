b823dd0808294d534ad3ebd8b6b00a089eea7518
yjieliang <yundantengteng@163.com>
2023-03-07 14:55:05 +0800

    bug：metrics插件趋势错误流水线构建页面的错误码要按错误类型查询 #8103

--- support-files/sql/2010_ci_metrics-update_v1.10_mysql.sql
+++ support-files/sql/2010_ci_metrics-update_v1.10_mysql.sql
@@ -48,13 +48,21 @@ BEGIN
     END IF;
 
     IF NOT EXISTS(SELECT 1
-                          FROM information_schema.COLUMNS
+                          FROM information_schema.constraint
                           WHERE TABLE_SCHEMA = db
                             AND TABLE_NAME = 'T_ERROR_CODE_INFO'
                             AND KEY = 'UNI_TECI_TYPE_CODE') THEN
     ALTER TABLE `T_ERROR_CODE_INFO`
         DROP KEY UNI_TECI_TYPE_CODE;
-        ADD CONSTRAINT T_ERROR_CODE_INFO_UN UNIQUE KEY (ERROR_TYPE,ERROR_CODE,ATOM_CODE);
+
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                              FROM information_schema.constraint
+                              WHERE TABLE_SCHEMA = db
+                                AND TABLE_NAME = 'T_ERROR_CODE_INFO') THEN
+    ALTER TABLE `T_ERROR_CODE_INFO`
+    ADD CONSTRAINT T_ERROR_CODE_INFO_UN UNIQUE KEY (ERROR_TYPE,ERROR_CODE,ATOM_CODE);
     END IF;
 
     COMMIT;
