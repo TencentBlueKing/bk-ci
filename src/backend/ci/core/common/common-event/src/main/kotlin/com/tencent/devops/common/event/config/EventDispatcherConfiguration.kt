0e40a46eda1433b7feaeaabcb2f615f18bd3db47
royalhuang <royalhuang@tencent.com>
2023-03-08 11:29:39 +0800

    feat: 引擎等MQ场景接入SCS框架 #7443 common中自带配置加载

--- src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/event/config/EventDispatcherConfiguration.kt
+++ src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/event/config/EventDispatcherConfiguration.kt
@@ -25,7 +25,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package com.tencent.devops.process.engine
+package com.tencent.devops.common.event.config
 
 import com.tencent.devops.common.event.dispatcher.mq.MQEventDispatcher
 import org.springframework.boot.autoconfigure.AutoConfigureOrder
