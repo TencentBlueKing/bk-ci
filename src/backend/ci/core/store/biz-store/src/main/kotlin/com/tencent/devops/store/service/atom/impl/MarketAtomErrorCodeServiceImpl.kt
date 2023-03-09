3f5f716c0fc5e39d985ffccac9cef9a807cea871
yjieliang <yundantengteng@163.com>
2023-03-09 14:49:12 +0800

    feat: 流水线插件支持展示获得的荣誉和SLA等信息 #8114

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomErrorCodeServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomErrorCodeServiceImpl.kt
@@ -36,7 +36,6 @@ import com.tencent.devops.store.pojo.common.enums.ErrorCodeTypeEnum
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
 import com.tencent.devops.store.service.atom.MarketAtomErrorCodeService
 import org.jooq.DSLContext
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
