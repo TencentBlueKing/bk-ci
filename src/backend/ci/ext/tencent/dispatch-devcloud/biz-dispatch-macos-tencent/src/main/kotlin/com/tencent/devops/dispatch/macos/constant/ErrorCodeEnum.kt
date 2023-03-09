84737d29f8e3d5db16120748d46ac5fdc8430bcd
sawyersong <sawyersong@tencent.com>
2023-03-09 19:41:40 +0800

    --story=882590785 合并dispatch-xxx服务到bk-ci

--- src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/constant/ErrorCodeEnum.kt
+++ src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/constant/ErrorCodeEnum.kt
@@ -8,5 +8,6 @@ enum class ErrorCodeEnum(
     val formatErrorMessage: String
 ) {
     SYSTEM_ERROR(ErrorType.SYSTEM, 2123001, "Dispatcher-macos系统错误"),
-    NO_IDLE_MACOS_ERROR(ErrorType.THIRD_PARTY, 2123002, "DEVCLOUD MACOS构建机启动失败，没有空闲的构建机"),
+    NO_IDLE_MACOS_ERROR(ErrorType.THIRD_PARTY, 2123002,
+                        "DEVCLOUD MACOS构建机启动失败，没有空闲的构建机"),
 }
