f45d0d97fcd4f9ca44a11ba5d55c7d9fc99edef7
xuzhan <xuzhan@tencent.com>
2023-03-10 12:23:46 +0800

    feature: 添加项目管理前端服务 issue #7923

--- src/frontend/devops-nav/src/views/ProjectManage.vue
+++ src/frontend/devops-nav/src/views/ProjectManage.vue
@@ -233,7 +233,7 @@
                 const projectTag = this.getProjectTag(routerTag)
                 switch (projectTag) {
                     case 'v0':
-                        window.open(`/console/perm/my-project?project_code=${projectCode}?x-devops-project-id=${projectCode}`)
+                        window.open(`/console/perm/my-project?project_code=${projectCode}`)
                         break
                     case 'v3':
                         window.open(`/console/ps/${projectCode}/${relationId}/member?x-devops-project-id=${projectCode}`)
@@ -272,7 +272,7 @@
                 const projectTag = this.getProjectTag(routerTag)
                 switch (projectTag) {
                     case 'v0':
-                        window.open(`/console/perm/my-project?project_code=${projectCode}&x-devops-project-id=${projectCode}`)
+                        window.open(`/console/perm/my-project?project_code=${projectCode}`)
                         break
                     case 'v3':
                         window.open(`/console/ps/${projectCode}/${relationId}/member?x-devops-project-id=${projectCode}`)
