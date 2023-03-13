3cc4eaa3b614b789c689ebc6c21ba844243481d0
sawyersong <sawyersong@tencent.com>
2023-03-13 21:27:11 +0800

    --story=882590785 合并dispatch-xxx服务到bk-ci

--- src/backend/ci/ext/tencent/dispatch-devcloud/boot-dispatch-devcloud-tencent/build.gradle.kts
+++ src/backend/ci/ext/tencent/dispatch-devcloud/boot-dispatch-devcloud-tencent/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     api(project(":ext:tencent:dispatch-devcloud:biz-dispatch-devcloud-tencent"))
     api(project(":ext:tencent:dispatch-devcloud:biz-dispatch-macos-tencent"))
     api(project(":ext:tencent:dispatch-devcloud:biz-dispatch-windows-tencent"))
+    api(project(":ext:tencent:dispatch-devcloud:biz-dispatch-pcg-tencent"))
     api(project(":ext:tencent:common:common-digest-tencent"))
     api(project(":ext:tencent:common:common-auth:common-auth-tencent"))
 }
