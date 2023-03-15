eb35353c7ea99a82e51395e606a61abeda08b572
lockiechen <33082528+lockiechen@users.noreply.github.com>
2023-03-15 15:15:07 +0800

    feat: set publicPath at runtime issue #8476

--- src/frontend/devops-nav/webpack.config.js
+++ src/frontend/devops-nav/webpack.config.js
@@ -34,96 +34,94 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const AssetPlugin = require("../webpackPlugin/assets-webpack-plugin");
-const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
-const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require("path");
-const webpackBaseConfig = require("../webpack.base");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const AssetPlugin = require('../webpackPlugin/assets-webpack-plugin')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+const webpackBaseConfig = require('../webpack.base')
+const webpack = require('webpack')
 module.exports = (env = {}, argv) => {
-  const isDev = argv.mode === "development";
-  const envDist = env && env.dist ? env.dist : "frontend";
-  const lsVersion = env && env.lsVersion ? env.lsVersion : "v2"; // 最后一个命令行参数为localStorage版本
-  const dist = path.join(__dirname, `../${envDist}/console`);
-  const config = webpackBaseConfig({
-    env,
-    argv,
-    entry: "./src/entry",
-    publicPath: "/console/",
-    dist: "/console",
-    port: 8080,
-  });
-  config.module.rules = [
-    ...config.module.rules,
-    {
-      test: /\.tsx?$/,
-      include: path.resolve("src"),
-      use: [
+    const isDev = argv.mode === 'development'
+    const envDist = env && env.dist ? env.dist : 'frontend'
+    const lsVersion = env && env.lsVersion ? env.lsVersion : 'v2' // 最后一个命令行参数为localStorage版本
+    const dist = path.join(__dirname, `../${envDist}/console`)
+    const config = webpackBaseConfig({
+        env,
+        argv,
+        entry: './src/entry',
+        publicPath: '/console/',
+        dist: '/console',
+        port: 8080
+    })
+    config.module.rules = [
+        ...config.module.rules,
         {
-          loader: "babel-loader",
-        },
-        {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-            appendTsSuffixTo: [/\.vue$/],
-          },
-        },
-      ],
-    },
-  ];
-  config.plugins.pop();
-  config.plugins = [
-    ...config.plugins,
-    new ForkTsCheckerWebpackPlugin({
-      eslint: {
-        files: "./src/**/*.{ts,tsx,vue,js,jsx}",
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: isDev ? "index.html" : `${dist}/frontend#console#index.html`,
-      inject: false,
-      publicPath: "__BK_CI_PUBLIC_PATH__/console/",
-      minify: {
-        removeComments: false,
-      },
-      DEVOPS_LS_VERSION: lsVersion,
-    }),
-    new AssetPlugin(),
-    new SpriteLoaderPlugin({
-      plainSprite: true,
-    }),
-    new AddAssetHtmlPlugin([
-      {
-        filepath: require.resolve("./src/assets/static/main.dll.js"),
-        publicPath: path.posix.join(
-          "__BK_CI_PUBLIC_PATH__",
-          "/console/",
-          "static/"
-        ),
-        hash: true,
-        includeSourcemap: false,
-      },
-    ]),
-    new webpack.DllReferencePlugin({
-      context: __dirname,
-      manifest: require("./src/assets/static/manifest.json"),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.join(__dirname, "./src/assets/static"),
-          to: `${dist}/static`,
-        },
-      ],
-    }),
-  ];
-  config.devServer.historyApiFallback = {
-    rewrites: [{ from: /^\/console/, to: "/console/index.html" }],
-  };
-  return config;
-};
+            test: /\.tsx?$/,
+            include: path.resolve('src'),
+            use: [
+                {
+                    loader: 'babel-loader'
+                },
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                        appendTsSuffixTo: [/\.vue$/]
+                    }
+                }
+            ]
+        }
+    ]
+    config.plugins.pop()
+    config.plugins = [
+        ...config.plugins,
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: './src/**/*.{ts,tsx,vue,js,jsx}'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: isDev
+                ? 'index.html'
+                : `${dist}/frontend#console#index.html`,
+            inject: false,
+            publicPath: '__BK_CI_PUBLIC_PATH__/console/',
+            minify: {
+                removeComments: false
+            },
+            DEVOPS_LS_VERSION: lsVersion
+        }),
+        new AssetPlugin(),
+        new SpriteLoaderPlugin({
+            plainSprite: true
+        }),
+        new AddAssetHtmlPlugin([
+            {
+                filepath: require.resolve('./src/assets/static/main.dll.js'),
+                publicPath: path.posix.join('__BK_CI_PUBLIC_PATH__', '/console/', 'static/'),
+                hash: true,
+                includeSourcemap: false
+            }
+        ]),
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require('./src/assets/static/manifest.json')
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, './src/assets/static'),
+                    to: `${dist}/static`
+                }
+            ]
+        })
+    ]
+    config.devServer.historyApiFallback = {
+        rewrites: [{ from: /^\/console/, to: '/console/index.html' }]
+    }
+    return config
+}
