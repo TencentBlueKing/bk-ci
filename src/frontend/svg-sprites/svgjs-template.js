d9b9dea0511d815347b829b218b40921a0ad1bb5
lockiechen <33082528+lockiechen@users.noreply.github.com>
2023-03-15 17:25:19 +0800

    feat: set publicPath at runtime issue #8476

--- src/frontend/svg-sprites/svgjs-template.js
+++ src/frontend/svg-sprites/svgjs-template.js
@@ -1,6 +1,6 @@
 !(function () {
   var svgCode =
-    '<div style="style="position:absolute;width:0;height:0;visibility:hidden"">__SVG_SPRITES_SYMBOLS__</div>';
+    '<div style="position:absolute;width:0;height:0;visibility:hidden">__SVG_SPRITES_SYMBOLS__</div>';
   if (document.body) {
     document.body.insertAdjacentHTML("afterbegin", svgCode);
   } else {
