4f03a67eafb5eb0831db0ece95bc73f75652ec8d
hejieehe <904696180@qq.com>
2023-03-16 12:32:04 +0800

    bug: 关联SVN代码库时，若源代码地址包含具体文件夹，会导致projectName字段解析异常 #8503

--- src/backend/ci/core/common/common-scm/src/main/kotlin/com/tencent/devops/scm/utils/code/svn/SvnUtils.kt
+++ src/backend/ci/core/common/common-scm/src/main/kotlin/com/tencent/devops/scm/utils/code/svn/SvnUtils.kt
@@ -114,21 +114,22 @@ object SvnUtils {
     }
 
     fun getSvnProjectName(svnUrl: String): String {
-        val urlSplitArray = svnUrl.split("tencent.com/")
+        val urlSplitArray = svnUrl.split("//")
         if (urlSplitArray.size < 2) {
             throw ScmException("Invalid svn url($svnUrl)", ScmType.CODE_SVN.name)
         }
-
+        // urlSplitArray[0] -> 协议  urlSplitArray[1] -> repo路径
         val path = urlSplitArray[1]
         val pathArray = path.split("/")
         if (pathArray.size < 2) {
             throw ScmException("Invalid svn url($svnUrl)", ScmType.CODE_SVN.name)
         }
-
-        return if (pathArray.size >= 3 && pathArray[2].endsWith("_proj")) {
-            "${pathArray[0]}/${pathArray[1]}/${pathArray[2]}"
+        // pathArray[0] -> 域名
+        return if (pathArray.size >= 4 && pathArray[3].endsWith("_proj")) {
+            // 兼容旧工蜂svn
+            "${pathArray[1]}/${pathArray[2]}/${pathArray[3]}"
         } else {
-            "${pathArray[0]}/${pathArray[1]}"
+            "${pathArray[1]}/${pathArray[2]}"
         }
     }
 
@@ -175,26 +176,6 @@ object SvnUtils {
             filePath.removePrefix("/").removePrefix(StringUtils.substring(url, start))
         } else {
             filePath.removePrefix("/")
-        }
-    }
-
-    fun getProjectName(svnUrl: String): String {
-        val urlArray = svnUrl.split("//")
-        // urlArray[0] -> 协议  urlArray[1] -> repo路径
-        if (urlArray.size < 2) {
-            throw ScmException("Invalid svn url($svnUrl)", ScmType.CODE_SVN.name)
-        }
-        val path = urlArray[1]
-        val pathArray = path.split("/")
-        // pathArray[0] -> 域名
-        if (pathArray.size < 3) {
-            throw ScmException("Invalid svn url($svnUrl)", ScmType.CODE_SVN.name)
-        }
-        return if (pathArray.size >= 4 && pathArray[3].endsWith("_proj")) {
-            // 兼容旧工蜂svn
-            "${pathArray[1]}/${pathArray[2]}/${pathArray[3]}"
-        } else {
-            "${pathArray[1]}/${pathArray[2]}"
         }
     }
 }
