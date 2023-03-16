cbab97a3927c26e0905394c9778cfaf7165a01d7
greysonfang <greysonfang@tencent.com>
2023-03-16 16:25:41 +0800

    feat：记录vo、v3鉴权结果 #8500

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/dao/AuthVerifyRecordDao.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/dao/AuthVerifyRecordDao.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
+@Suppress("ALL")
 class AuthVerifyRecordDao {
     fun createOrUpdate(
         dslContext: DSLContext,
