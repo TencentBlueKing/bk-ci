b1de95370a1ebc8d066798d4e23c30fa5cddf6d8
yongyiduan <yongyiduan@tencent.com>
2023-03-17 11:17:21 +0800

    --story=882925397 devfile中image关键字改成runs-on

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/PreDevfile.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/PreDevfile.kt
@@ -1,8 +1,12 @@
 package com.tencent.devops.remotedev.pojo
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.tencent.devops.dispatch.kubernetes.pojo.remotedev.Container
 import com.tencent.devops.dispatch.kubernetes.pojo.remotedev.DevfileCommands
 import com.tencent.devops.dispatch.kubernetes.pojo.remotedev.DevfilePorts
 import com.tencent.devops.dispatch.kubernetes.pojo.remotedev.DevfileVscode
+import com.tencent.devops.process.yaml.v2.models.job.Credentials
+import com.tencent.devops.process.yaml.v2.models.job.JobRunsOnType
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
@@ -22,11 +26,37 @@ data class PreDevfile(
      *        file: ./.Dockerfile
      * @see DevfileImage
      */
-    val image: Any?,
+    @JsonProperty("runs-on")
+    val runsOn: Any?,
     @ApiModelProperty("配置vscode")
     val vscode: DevfileVscode?,
     @ApiModelProperty("配置需要监听的端口信息")
     val ports: List<DevfilePorts>?,
     @ApiModelProperty("用来指定工作空间声明周期命令")
     val commands: DevfileCommands?
+)
+
+data class PreRunsOn(
+    @ApiModelProperty(name = "self-hosted")
+    @JsonProperty("self-hosted")
+    val selfHosted: Boolean? = null,
+    @ApiModelProperty(name = "pool-name")
+    @JsonProperty("pool-name")
+    var poolName: String = JobRunsOnType.DOCKER.type,
+    val container: PreContainer? = null,
+    @ApiModelProperty(name = "agent-selector")
+    @JsonProperty("agent-selector")
+    val agentSelector: List<String>? = null,
+    val workspace: String? = null,
+    val xcode: String? = null,
+    @ApiModelProperty(name = "queue-timeout-minutes")
+    @JsonProperty("queue-timeout-minutes")
+    val queueTimeoutMinutes: Int? = null,
+    val needs: Map<String, String>? = null
+)
+
+data class PreContainer(
+    val image: String,
+    val host: String?,
+    val credentials: Any? = null
 )
