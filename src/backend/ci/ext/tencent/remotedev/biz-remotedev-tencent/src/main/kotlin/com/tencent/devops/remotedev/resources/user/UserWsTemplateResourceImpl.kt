d91ebc79610b45a64ae54a2405f41b7b015f47cd
mingshewhe <mingshewhe@tencent.com>
2023-03-20 19:27:59 +0800

    feat: 蓝盾对接权限中心RBAC #7794

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserWsTemplateResourceImpl.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserWsTemplateResourceImpl.kt
@@ -23,17 +23,8 @@
  * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
-<<<<<<<< HEAD:src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/iam/impl/PermissionResourceCallbackServiceImpl.kt
-package com.tencent.devops.auth.service.iam.impl
-
-import com.tencent.bk.sdk.iam.dto.callback.request.CallbackRequestDTO
-import com.tencent.bk.sdk.iam.dto.callback.response.CallbackBaseResponseDTO
-import com.tencent.devops.auth.service.ResourceService
-import com.tencent.devops.auth.service.iam.PermissionResourceCallbackService
-========
 package com.tencent.devops.remotedev.resources.user
 
 import com.tencent.devops.common.api.pojo.Result
@@ -42,25 +33,8 @@ import com.tencent.devops.remotedev.api.user.UserWsTemplateResource
 import com.tencent.devops.remotedev.pojo.WorkspaceTemplate
 import com.tencent.devops.remotedev.service.WorkspaceTemplateService
 import org.slf4j.LoggerFactory
->>>>>>>> origin/integration:src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserWsTemplateResourceImpl.kt
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
 
-<<<<<<<< HEAD:src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/iam/impl/PermissionResourceCallbackServiceImpl.kt
-@Service
-class PermissionResourceCallbackServiceImpl @Autowired constructor(
-    private val resourceService: ResourceService,
-) : PermissionResourceCallbackService {
-    override fun getProject(callBackInfo: CallbackRequestDTO, token: String): CallbackBaseResponseDTO {
-        return resourceService.getProject(callBackInfo, token)
-    }
-
-    override fun getInstanceByResource(callBackInfo: CallbackRequestDTO, token: String): CallbackBaseResponseDTO? {
-        return resourceService.getInstanceByResource(
-            callBackInfo = callBackInfo,
-            token = token
-        )
-========
 @RestResource
 @Suppress("ALL")
 class UserWsTemplateResourceImpl @Autowired constructor(
@@ -74,6 +48,5 @@ class UserWsTemplateResourceImpl @Autowired constructor(
     override fun getWorkspaceTemplateList(userId: String): Result<List<WorkspaceTemplate>> {
         logger.info("WorkspaceTemplateService|getWorkspaceTemplateList|userId|$userId")
         return Result(workspaceTemplateService.getWorkspaceTemplateList())
->>>>>>>> origin/integration:src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserWsTemplateResourceImpl.kt
     }
 }
