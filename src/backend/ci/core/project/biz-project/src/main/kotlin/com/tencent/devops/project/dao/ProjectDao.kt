586b38ea8b8957d304b363a210586f1ac93cfc84
greysonfang <greysonfang@tencent.com>
2023-03-21 16:22:20 +0800

    feat: 蓝盾对接权限中心RBAC #7794

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/dao/ProjectDao.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/dao/ProjectDao.kt
@@ -523,18 +523,15 @@ class ProjectDao {
         limit: Int
     ): Result<Record2<String, String>> {
         return with(TProject.T_PROJECT) {
-            dslContext.select(PROJECT_NAME, ENGLISH_NAME).from(this).where()
-                .and(
+            dslContext.select(PROJECT_NAME, ENGLISH_NAME).from(this)
+                .where(generateQueryProjectForApplyCondition())
+                .and(AUTH_SECRECY.eq(ProjectAuthSecrecyStatus.PUBLIC.value))
+                .or(
                     ID.`in`(
                         dslContext.select(ID).from(this)
                             .where(generateQueryProjectForApplyCondition())
-                            .and(AUTH_SECRECY.eq(ProjectAuthSecrecyStatus.PUBLIC.value))
-                            .unionAll(
-                                dslContext.select(ID).from(this)
-                                    .where(generateQueryProjectForApplyCondition())
-                                    .and(ENGLISH_NAME.`in`(englishNameList))
-                                    .and(AUTH_SECRECY.eq(ProjectAuthSecrecyStatus.PRIVATE.value))
-                            )
+                            .and(ENGLISH_NAME.`in`(englishNameList))
+                            .and(AUTH_SECRECY.eq(ProjectAuthSecrecyStatus.PRIVATE.value))
                     )
                 )
                 .let { if (projectName == null) it else it.and(PROJECT_NAME.like("%${projectName.trim()}%")) }
