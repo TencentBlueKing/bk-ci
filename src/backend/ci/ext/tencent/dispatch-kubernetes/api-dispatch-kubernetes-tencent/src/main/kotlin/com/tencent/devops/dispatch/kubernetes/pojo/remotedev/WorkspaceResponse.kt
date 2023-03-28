7e9a2326edb9132e0303a309bf7e42834c320852
yongyiduan <yongyiduan@tencent.com>
2023-03-28 17:13:14 +0800

    优化v4_user_template_get接口 #8550

--- src/backend/ci/ext/tencent/dispatch-kubernetes/api-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/remotedev/WorkspaceResponse.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/api-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/remotedev/WorkspaceResponse.kt
@@ -3,5 +3,5 @@ package com.tencent.devops.dispatch.kubernetes.pojo.remotedev
 data class WorkspaceResponse(
     val environmentHost: String,
     val environmentUid: String,
-    val environmentIp: String,
+    val environmentIp: String
 )
