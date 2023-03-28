89a80e78f80231c809eebfd6d98256149dfa3dbd
yongyiduan <yongyiduan@tencent.com>
2023-03-28 18:10:51 +0800

    优化v4_user_template_get接口 #8550

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/dao/NodeDao.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/dao/NodeDao.kt
@@ -71,14 +71,14 @@ class NodeDao {
         }
     }
 
-    fun anyThirdPartyNodeByIp(dslContext: DSLContext, projectId: String, nodeIp: String): TNodeRecord? {
+    fun getThirdPartyNodeByIp(dslContext: DSLContext, projectId: String, nodeIp: String): List<TNodeRecord> {
         with(TNode.T_NODE) {
             return dslContext.selectFrom(this)
                 .where(PROJECT_ID.eq(projectId))
                 .and(NODE_TYPE.eq(NodeType.THIRDPARTY.name))
                 .and(NODE_IP.eq(nodeIp))
                 .orderBy(NODE_ID.desc())
-                .fetchAny()
+                .fetch()
         }
     }
 
