27b12e4b9b585ed3ce3d9010faacaa45466820df
jamikxu <jamikxu@tencent.com>
2023-04-11 12:16:53 +0800

    bug: 质量红线获取项目下可使用插件的指标缺失 #8565

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/common/StoreProjectRelDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/common/StoreProjectRelDao.kt
@@ -441,12 +441,12 @@ class StoreProjectRelDao {
         dslContext: DSLContext,
         projectCode: String,
         storeType: StoreTypeEnum
-    ): Result<Record1<String>>? {
+    ): Result<Record2<String, Byte>>? {
         with(TStoreProjectRel.T_STORE_PROJECT_REL) {
             val conditions = mutableListOf<Condition>()
             conditions.add(PROJECT_CODE.eq(projectCode))
             conditions.add(STORE_TYPE.eq(storeType.type.toByte()))
-            return dslContext.select(STORE_CODE).from(this).where(conditions).groupBy(STORE_CODE).fetch()
+            return dslContext.select(STORE_CODE, TYPE).from(this).where(conditions).groupBy(STORE_CODE).fetch()
         }
     }
 
