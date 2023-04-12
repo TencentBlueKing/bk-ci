2b89c0f9004d44b4428154c753252df3be646a61
mingshewhe <mingshewhe@tencent.com>
2023-04-12 17:09:21 +0800

    feat: 蓝盾对接权限中心RBAC #7794

--- src/backend/ci/core/common/common-auth/common-auth-rbac/src/main/kotlin/com/tencent/devops/common/auth/api/RbacAuthTokenApi.kt
+++ src/backend/ci/core/common/common-auth/common-auth-rbac/src/main/kotlin/com/tencent/devops/common/auth/api/RbacAuthTokenApi.kt
@@ -27,33 +27,81 @@
 
 package com.tencent.devops.common.auth.api
 
+import com.tencent.bk.sdk.iam.service.impl.TokenServiceImpl
 import com.tencent.devops.common.auth.code.AuthServiceCode
+import com.tencent.devops.common.redis.RedisOperation
+import java.nio.charset.Charset
+import java.util.Base64
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 
-class RbacAuthTokenApi : AuthTokenApi {
+class RbacAuthTokenApi constructor(
+    private val redisOperation: RedisOperation,
+    private val tokenServiceImpl: TokenServiceImpl
+) : AuthTokenApi {
+
+    @Value("\${auth.iamCallBackUser}")
+    private val iamClientName = ""
 
     override fun refreshAccessToken(serviceCode: AuthServiceCode): String {
-        return refreshAccessToken()
-    }
-
-    private fun refreshAccessToken(): String {
-        return "auth_token_mock"
+        return getRemoteToken()
     }
 
     override fun getAccessToken(serviceCode: AuthServiceCode): String {
-        return getAccessToken()
+        return getRemoteToken()
     }
 
-    private fun getAccessToken(): String {
-        return refreshAccessToken()
-    }
-
+    @Suppress("ReturnCount")
     override fun checkToken(token: String): Boolean {
-        return true
+        val pair = decodeAuth(token)
+        if (pair.first != iamClientName) {
+            logger.warn("iam tokenCheck: userName error ${pair.first}")
+            return false
+        }
+        val redisToken = redisOperation.get(TOKEN_REDIS_KEY)
+        if (redisToken.isNullOrEmpty()) {
+            logger.info("iam tokenCheck: redis is empty")
+            val remoteToken = getRemoteToken()
+            return if (remoteToken == pair.second) {
+                redisOperation.set(TOKEN_REDIS_KEY, remoteToken)
+                true
+            } else {
+                false
+            }
+        }
+        logger.info("iam tokenCheck: redis data $redisToken")
+        if (pair.second == redisToken) {
+            return true
+        }
+
+        logger.info("iam tokenCheck: redis notEqual input, redis[$redisToken], input[${pair.second}]")
+        // 最终验证权在auth服务端
+        val remoteToken = getRemoteToken()
+        if (pair.second == remoteToken) {
+            return true
+        }
+        logger.info("iam tokenCheck fail]")
+        return false
+    }
+
+    private fun getRemoteToken(): String {
+        val token = tokenServiceImpl.token
+        logger.info("get iam token: $token")
+        return token
+    }
+
+    private fun decodeAuth(token: String): Pair<String, String> {
+        val str = if (token.contains("Basic ")) {
+            token.substringAfter("Basic ")
+        } else {
+            token
+        }
+        val decodeStr = String(Base64.getDecoder().decode(str), Charset.forName("UTF-8"))
+        return Pair(decodeStr.substringBefore(":"), decodeStr.substringAfter(":"))
     }
 
     companion object {
-        const val TOKEN_REDIS_KEY = "_BK:AUTH:V3:TOKEN_"
-        val logger = LoggerFactory.getLogger(RbacAuthTokenApi::class.java)
+        const val TOKEN_REDIS_KEY = "_BK:AUTH:RBAC:TOKEN_"
+        private val logger = LoggerFactory.getLogger(RbacAuthTokenApi::class.java)
     }
 }
