54c5ff30600d55c4838f8e261ec418618c3f723e
jamikxu <jamikxu@tencent.com>
2023-04-12 11:47:16 +0800

    feat: 质量红线查询流水线加分页 #8007

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineListFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/PipelineListFacadeService.kt
@@ -110,7 +110,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.util.StopWatch
 import javax.ws.rs.core.Response
 
 @Suppress("ALL")
