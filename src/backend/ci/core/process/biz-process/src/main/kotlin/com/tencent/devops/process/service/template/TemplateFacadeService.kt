86420577e85409a2f472c4bc9d9b35cec3bc1a7c
Mr-SakuraManJi <1516908003@qq.com>
2023-04-13 16:01:38 +0800

    feat：希望流水线模板实例化流水线时能够继承分组名称的配置 #8616

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/TemplateFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/TemplateFacadeService.kt
@@ -1617,6 +1617,7 @@ class TemplateFacadeService @Autowired constructor(
                 labels = labels,
                 waitQueueTimeMinute = waitQueueTimeMinute,
                 maxQueueSize = maxQueueSize,
+                concurrencyGroup = concurrencyGroup,
                 hasPermission = hasPermission,
                 maxPipelineResNum = maxPipelineResNum,
                 maxConRunningQueueSize = maxConRunningQueueSize,
