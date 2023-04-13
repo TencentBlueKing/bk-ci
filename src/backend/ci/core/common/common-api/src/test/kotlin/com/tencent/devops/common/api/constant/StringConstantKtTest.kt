46aef9564ee9f1ff46442a80f385000bebfba68e
irwinsun <irwinsun@tencent.com>
2023-04-13 15:44:08 +0800

    bug: 流水线启动接口性能优化 #8615
    修正未启动，直接取消的情况下，通知模板显示的耗时不准确

---
+++ src/backend/ci/core/common/common-api/src/test/kotlin/com/tencent/devops/common/api/constant/StringConstantKtTest.kt
@@ -0,0 +1,17 @@
+package com.tencent.devops.common.api.constant
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StringConstantKtTest {
+
+    @Test
+    fun coerceAtMaxLength() {
+        val expect = "12345678"
+        val s10 = "1234567890"
+        assertEquals(expect, s10.coerceAtMaxLength(expect.length))
+        assertEquals(s10, s10.coerceAtMaxLength(s10.length))
+        assertEquals(s10, s10.coerceAtMaxLength(s10.length + 100))
+    }
+}
