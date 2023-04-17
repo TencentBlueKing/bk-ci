ae0e1bc7f28bd79e4cbeb5ed3453d0ba7c63a6db
yjieliang <yundantengteng@163.com>
2023-04-17 17:47:44 +0800

    feat: 流水线插件支持展示获得的荣誉和SLA等信息 #8114

--- support-files/sql/2002_v1.x/2010_ci_metrics-update_v1.10_mysql.sql
+++ support-files/sql/2002_v1.x/2010_ci_metrics-update_v1.10_mysql.sql
@@ -73,6 +73,21 @@ BEGIN
     ALTER TABLE `T_ERROR_CODE_INFO`ADD INDEX `INX_TECI_ATOM_CODE` (`ATOM_CODE`);
     END IF;
 
+    IF NOT EXISTS(SELECT 1
+                              FROM information_schema.statistics
+                              WHERE TABLE_SCHEMA = db
+                                AND TABLE_NAME = 'T_ERROR_CODE_INFO'
+                                AND INDEX_NAME = 'T_ERROR_CODE_INFO_UN') THEN
+    ALTER TABLE T_ERROR_CODE_INFO DROP INDEX T_ERROR_CODE_INFO_UN;
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                                  FROM information_schema.statistics
+                                  WHERE TABLE_SCHEMA = db
+                                    AND TABLE_NAME = 'T_ERROR_CODE_INFO') THEN
+    ALTER TABLE T_ERROR_CODE_INFO ADD CONSTRAINT T_ERROR_CODE_INFO_UN UNIQUE KEY (ERROR_TYPE,ERROR_CODE,ATOM_CODE);
+    END IF;
+
     COMMIT;
 END <CI_UBF>
 DELIMITER ;
