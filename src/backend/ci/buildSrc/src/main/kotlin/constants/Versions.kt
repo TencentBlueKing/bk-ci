9802c563f03905a0ea47db7b292bb85b90423ede
mingshewhe <mingshewhe@tencent.com>
2023-04-18 19:37:25 +0800

    feat: 蓝盾对接权限中心RBAC #7794

--- src/backend/ci/buildSrc/src/main/kotlin/constants/Versions.kt
+++ src/backend/ci/buildSrc/src/main/kotlin/constants/Versions.kt
@@ -45,5 +45,5 @@ object Versions {
     const val jjwt = "0.11.5"
     const val Okhttp = "4.9.0"
     const val jgit = "5.13.1.202206130422-r"
-    const val iam = "1.0.17-SNAPSHOT"
+    const val iam = "1.0.18-SNAPSHOT"
 }
