4ad19170f68eb6202bb12f102cf77b7eb9d69032
greysonfang <greysonfang@tencent.com>
2023-04-21 17:58:47 +0800

    feat：记录vo、v3鉴权结果 #8500

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/service/ServiceVerifyRecordResource.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/service/ServiceVerifyRecordResource.kt
@@ -12,6 +12,8 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.AuthPermission
+import javax.ws.rs.QueryParam
 
 @Api(tags = ["AUTH_VERIFY_RECORD"], description = "鉴权记录")
 @Path("/service/verify/record")
@@ -28,5 +30,22 @@ interface ServiceVerifyRecordResource {
         userId: String,
         @ApiParam("鉴权记录实体")
         verifyRecordDTO: VerifyRecordDTO
+    ): Result<Boolean>
+
+    @POST
+    @Path("/batch")
+    @ApiOperation("批量记录鉴权结果")
+    fun bathCreateOrUpdate(
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        @ApiParam("用户ID")
+        userId: String,
+        @ApiParam("项目ID")
+        @QueryParam("projectCode")
+        projectCode: String,
+        @ApiParam("资源类型")
+        @QueryParam("resourceType")
+        resourceType: String,
+        @ApiParam("资源权限结果")
+        permissionsResourcesMap: Map<AuthPermission, List<String>>
     ): Result<Boolean>
 }
