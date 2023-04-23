a681faab1ec59d4a446480f58cf2d86117e56275
mingshewhe <mingshewhe@tencent.com>
2023-04-23 21:14:22 +0800

    Merge pull request #8561 from Mr-SakuraManJi/feat_8553

--- src/backend/ci/ext/tencent/plugin/api-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/pojo/GcloudConf.kt
+++ src/backend/ci/ext/tencent/plugin/api-plugin-tencent/src/main/kotlin/com/tencent/devops/plugin/pojo/GcloudConf.kt
@@ -40,8 +40,11 @@ data class GcloudConf(
     val address: String,
     @ApiModelProperty("文件地址")
     val fileAddress: String,
+    @ApiModelProperty("更新时间")
+    val updateTime: Long,
     @ApiModelProperty("操作人")
     val userId: String,
     @ApiModelProperty("备注")
     val remark: String?
+
 )
