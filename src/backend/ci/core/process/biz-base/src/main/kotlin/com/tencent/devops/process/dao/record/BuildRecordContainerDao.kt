5b253ebbe1a0aa5f56d463863fb17e840b4ecf21
royalhuang <royalhuang@tencent.com>
2023-04-27 18:58:56 +0800

    feat：流水线构建详情页重构需求 issue #7983 修复显示数据

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/dao/record/BuildRecordContainerDao.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/dao/record/BuildRecordContainerDao.kt
@@ -164,13 +164,13 @@ class BuildRecordContainerDao {
         with(TPipelineBuildRecordContainer.T_PIPELINE_BUILD_RECORD_CONTAINER) {
             val update = dslContext.update(this)
                 .set(STATUS, buildStatus.name)
-            update.where(
-                BUILD_ID.eq(buildId)
-                    .and(PROJECT_ID.eq(projectId))
-                    .and(PIPELINE_ID.eq(pipelineId))
-                    .and(EXECUTE_COUNT.eq(executeCount))
-            )
-            stageId?.let { update.set(STAGE_ID, stageId) }
+                .where(
+                    BUILD_ID.eq(buildId)
+                        .and(PROJECT_ID.eq(projectId))
+                        .and(PIPELINE_ID.eq(pipelineId))
+                        .and(EXECUTE_COUNT.eq(executeCount))
+                )
+            stageId?.let { update.and(STAGE_ID.eq(stageId)) }
             update.execute()
         }
     }
