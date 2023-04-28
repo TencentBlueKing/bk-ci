3023da3f186cb261bb7a4dcb9109a544e9c3643a
yjieliang <yundantengteng@163.com>
2023-04-28 10:30:14 +0800

    pref:蓝盾国际化方案优化 #8211

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/utils/CommonUtils.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/utils/CommonUtils.kt
@@ -40,17 +40,17 @@ import com.tencent.devops.common.service.PROFILE_PRODUCTION
 import com.tencent.devops.common.service.PROFILE_STREAM
 import com.tencent.devops.common.service.PROFILE_TEST
 import com.tencent.devops.common.service.Profile
-import org.apache.commons.lang3.StringUtils
-import org.slf4j.LoggerFactory
-import org.springframework.context.i18n.LocaleContextHolder
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
 import java.io.File
 import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.NetworkInterface
 import java.net.SocketException
-import java.util.Enumeration
+import java.util.*
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
+import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 
 object CommonUtils {
 
@@ -111,7 +111,7 @@ object CommonUtils {
     }
 
     private fun parseIp(
-        allNetInterfaces: io.prometheus.client.Enumeration<NetworkInterface>,
+        allNetInterfaces: Enumeration<NetworkInterface>,
         allIp: HashMap<String, String>
     ) {
         val netInterface = allNetInterfaces.nextElement()
