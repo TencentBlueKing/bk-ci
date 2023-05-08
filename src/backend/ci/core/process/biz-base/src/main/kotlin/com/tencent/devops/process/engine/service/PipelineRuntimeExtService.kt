4e9715ece9db1ebdf2bcf7fd34856b57f8ffdc2a
royalhuang <royalhuang@tencent.com>
2023-05-08 11:11:58 +0800

    feat：流水线构建详情页重构需求 issue #7983 修复显示数据

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeExtService.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeExtService.kt
@@ -121,7 +121,6 @@ class PipelineRuntimeExtService @Autowired constructor(
                 dslContext = dslContext,
                 projectId = projectId,
                 buildId = buildInfo.buildId,
-                startTime = LocalDateTime.now(), // 设置startTime解决在按天查询时查不出来的问题
                 oldBuildStatus = buildInfo.status,
                 newBuildStatus = buildStatus
             )
