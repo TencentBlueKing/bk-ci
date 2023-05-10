980e0312d147b58749f70b13e010f6706463513e
v_yjieliang <v_yjieliang@tencent.com>
2023-05-10 20:20:07 +0800

    --story=882151013 国际化资源文件整理

--- src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/constant/SlaPluginError.kt
+++ src/backend/ci/ext/tencent/monitoring/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/constant/SlaPluginError.kt
@@ -33,13 +33,13 @@ enum class SlaPluginError(
     private val code: String,
     private val mean: String
 ) {
-    DEFAULT_ERROR("2199001", "插件默认异常"),
-    CONFIG_ERROR("2199002", "用户配置有误"),
-    DEPEND_ERROR("2199003", "插件依赖异常"),
-    EXEC_FAILED("2199004", "用户任务执行失败"),
-    TIMEOUT("2199005", "用户任务执行超时失败（自行限制）"),
-    GITCI_ERROR("2199006", "工蜂服务异常"),
-    LOW_QUALITY("2199007", "触碰质量红线");
+    DEFAULT_ERROR("2110001", "插件默认异常"),
+    CONFIG_ERROR("2110002", "用户配置有误"),
+    DEPEND_ERROR("2110003", "插件依赖异常"),
+    EXEC_FAILED("2110004", "用户任务执行失败"),
+    TIMEOUT("2110005", "用户任务执行超时失败（自行限制）"),
+    GITCI_ERROR("2110006", "工蜂服务异常"),
+    LOW_QUALITY("2110007", "触碰质量红线");
 
     companion object {
         fun getMean(code: String?): String {
