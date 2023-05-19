a4d3d74968db9d2cd8ff14c0a3e29472935bd96b
randychen <randychen@tencent.com>
2023-05-19 17:36:38 +0800

    --story=884473993 接口：用于客户端重新登陆后，更新运行中容器的session文件所存的bkticket

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserWorkspaceResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserWorkspaceResource.kt
@@ -326,10 +326,11 @@ interface UserWorkspaceResource {
     @POST
     @Path("/updateAllBkTicket")
     fun updateAllBkTicket(
-        @ApiParam("userId", required = true)
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
-        @ApiParam("bkTicket信息", required = true)
-        bkTicketInfo: BkTicketInfo
+        @ApiParam(value = "bkTicket", required = true)
+        @HeaderParam(AUTH_HEADER_DEVOPS_BK_TICKET)
+        bkTicket: String
     ): Result<Boolean>
 }
