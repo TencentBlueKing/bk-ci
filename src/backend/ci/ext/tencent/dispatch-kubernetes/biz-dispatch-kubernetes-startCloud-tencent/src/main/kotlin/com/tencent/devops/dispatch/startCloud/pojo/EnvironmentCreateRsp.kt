c7d7a142be654fbae000fb65fa914bccc9c022a7
yongyiduan <yongyiduan@tencent.com>
2023-05-22 10:24:20 +0800

    --story=884356431 新建环境时根据devfile定义的runs-on，区分对接START云桌面的接口。

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentCreateRsp.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentCreateRsp.kt
@@ -1,5 +1,8 @@
 package com.tencent.devops.dispatch.startCloud.pojo
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class EnvironmentCreateRsp(
     val code: Int,
     val data: EnvironmentCreateRspData,
--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentDefaltRsp.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentDefaltRsp.kt
@@ -1,5 +1,8 @@
 package com.tencent.devops.dispatch.startCloud.pojo
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class EnvironmentDefaltRsp(
     val code: Int,
     val message: String
