11b693d573b16d17b58cb0d09c1c094310daed2e
sawyersong <sawyersong@tencent.com>
2023-05-25 15:02:40 +0800

    feat: 优化agent环境变量获取逻辑 #8857

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/AbstractBuildResourceApi.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/api/AbstractBuildResourceApi.kt
@@ -255,15 +255,7 @@ abstract class AbstractBuildResourceApi : WorkerRestApiSDK {
 
             map[AUTH_HEADER_DEVOPS_BUILD_TYPE] = buildType.name
             when (buildType) {
-                BuildType.AGENT -> {
-                    map[AUTH_HEADER_DEVOPS_PROJECT_ID] = AgentEnv.getProjectId()
-                    map[AUTH_HEADER_DEVOPS_AGENT_ID] = AgentEnv.getAgentId()
-                    map[AUTH_HEADER_DEVOPS_AGENT_SECRET_KEY] = AgentEnv.getAgentSecretKey()
-                    map[AUTH_HEADER_DEVOPS_PROJECT_ID] = AgentEnv.getProjectId()
-                    map[AUTH_HEADER_DEVOPS_AGENT_ID] = AgentEnv.getAgentId()
-//                    map[AUTH_HEADER_AGENT_SECRET_KEY] = AgentEnv.getAgentSecretKey()
-                }
-                BuildType.DOCKER -> {
+                BuildType.DOCKER, BuildType.AGENT, BuildType.MACOS -> {
                     map[AUTH_HEADER_DEVOPS_PROJECT_ID] = AgentEnv.getProjectId()
                     map[AUTH_HEADER_DEVOPS_AGENT_ID] = AgentEnv.getAgentId()
                     map[AUTH_HEADER_DEVOPS_AGENT_SECRET_KEY] = AgentEnv.getAgentSecretKey()
