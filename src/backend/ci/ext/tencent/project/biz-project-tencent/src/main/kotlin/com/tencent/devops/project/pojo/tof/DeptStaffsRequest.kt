e39333e700c7b4296fd34b0fffd369585fc77d01
stubenhuang <stubenhuang@tencent.com>
2023-05-31 17:16:15 +0800

    --story=884478349 【蓝盾】【产品-无需评审会】版本体验的体验组新建和编辑页面优化

--- src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/pojo/tof/DeptStaffsRequest.kt
+++ src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/pojo/tof/DeptStaffsRequest.kt
@@ -3,6 +3,6 @@ package com.tencent.devops.project.pojo.tof
 data class DeptStaffsRequest(
     val dept_id: String,
     val level: Int,
-    val bk_app_code: String,
-    val bk_app_secret: String
+    val app_code: String,
+    val app_secret: String
 )
