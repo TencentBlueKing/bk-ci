92cb6a0af710a0c4a462c6b31846e7409cbc919b
carlyin <449036249@qq.com>
2023-06-05 14:57:41 +0800

    pref:蓝盾国际化方案优化 #8211

--- src/backend/ci/buildSrc/src/main/kotlin/plugins/task-i18n-load.gradle.kts
+++ src/backend/ci/buildSrc/src/main/kotlin/plugins/task-i18n-load.gradle.kts
@@ -24,6 +24,7 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+import org.springframework.util.FileSystemUtils
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.util.Properties
@@ -62,37 +63,34 @@ if (File(i18nPath).isDirectory) {
             val moduleFileNames = getFileNames(joinPath(i18nPath, moduleName))
 
             println("copy i18n into $moduleName classpath... , moduleFileNames is : $moduleFileNames")
-            for (fileName in moduleFileNames) {
-                // set variables for input files
-                val file1 = File(joinPath(i18nPath, fileName))
-                val file2 = File(joinPath(i18nPath, moduleName, fileName))
-                val targetFile = File(
+            val srcFile = File(joinPath(i18nPath, moduleName))
+            if (srcFile.exists()) {
+                val destFile = File(
                     joinPath(
                         projectDir.absolutePath,
                         "src",
                         "main",
                         "resources",
-                        "i18n",
-                        fileName
+                        "i18n"
                     )
                 )
-                targetFile.parentFile.mkdirs()
-                val targetProperties = Properties()
-                if (targetFile.createNewFile()) {
-                    println("create target file : ${targetFile.absolutePath}")
-                    // create output file with first input
-                    if (file1.exists()) {
-                        println("copy file1: ${file1.absolutePath} now...")
-                        targetProperties.load(FileInputStream(file1))
+                println("copy srcFile: ${srcFile.absolutePath} now...")
+                FileSystemUtils.copyRecursively(srcFile, destFile)
+                // 处理模块的properties文件，要合并公共的properties文件
+                destFile.listFiles()?.filter { it.name.endsWith("properties") }?.forEach { propertyFile ->
+                    val commonPropertyFile = File(joinPath(i18nPath, propertyFile.name))
+                    val targetProperties = Properties()
+                    if (commonPropertyFile.exists()) {
+                        println("copy commonPropertyFile: ${commonPropertyFile.absolutePath} now...")
+                        targetProperties.load(FileInputStream(commonPropertyFile))
                     }
                     // append second input to output file if it exists
-                    if (file2.exists()) {
-                        println("copy file2: ${file2.absolutePath} now...")
-                        targetProperties.load(FileInputStream(file2))
+                    if (propertyFile.exists()) {
+                        println("copy modulePropertyFile: ${propertyFile.absolutePath} now...")
+                        targetProperties.load(FileInputStream(propertyFile))
                     }
+                    targetProperties.store(FileOutputStream(propertyFile), "i18n")
                 }
-                targetProperties.store(FileOutputStream(targetFile), "i18n")
-                println("Target file generated: ${targetFile.absolutePath}")
             }
         }
     }
