2d9d7e43ee90774c484267fe312ccd86773b8545
sawyersong <sawyersong@tencent.com>
2023-06-07 15:38:32 +0800

    --story=883658367 macos公共构建机调度优化

--- src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/listener/MacBuildListener.kt
+++ src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/listener/MacBuildListener.kt
@@ -1,26 +1,40 @@
 package com.tencent.devops.dispatch.macos.listener
 
+import com.tencent.devops.common.api.constant.CommonMessageCode
+import com.tencent.devops.common.api.pojo.ErrorType
+import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.dispatch.sdk.BuildFailureException
+import com.tencent.devops.common.dispatch.sdk.DispatchSdkErrorCode
 import com.tencent.devops.common.dispatch.sdk.listener.BuildListener
 import com.tencent.devops.common.dispatch.sdk.pojo.DispatchMessage
+import com.tencent.devops.common.dispatch.sdk.service.DispatchService
 import com.tencent.devops.common.log.utils.BuildLogPrinter
+import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.common.redis.RedisLock
 import com.tencent.devops.common.redis.RedisOperation
+import com.tencent.devops.common.service.utils.SpringContextUtil
+import com.tencent.devops.common.web.utils.I18nUtil
 import com.tencent.devops.dispatch.macos.enums.MacJobStatus
 import com.tencent.devops.dispatch.macos.pojo.devcloud.DevCloudMacosVmDelete
 import com.tencent.devops.dispatch.macos.service.BuildHistoryService
 import com.tencent.devops.dispatch.macos.service.BuildTaskService
 import com.tencent.devops.dispatch.macos.service.DevCloudMacosService
 import com.tencent.devops.dispatch.macos.service.MacosVMRedisService
+import com.tencent.devops.dispatch.macos.util.MacOSThreadPoolUtils
+import com.tencent.devops.dispatch.macos.util.ThreadPoolName
 import com.tencent.devops.dispatch.pojo.enums.JobQuotaVmType
 import com.tencent.devops.model.dispatch.macos.tables.records.TBuildTaskRecord
+import com.tencent.devops.process.api.service.ServiceBuildResource
 import com.tencent.devops.process.pojo.mq.PipelineAgentShutdownEvent
 import org.jooq.Result
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.util.concurrent.RejectedExecutionException
 
 @Component
 class MacBuildListener @Autowired constructor(
+    private val client: Client,
     private val buildHistoryService: BuildHistoryService,
     private val buildTaskService: BuildTaskService,
     private val redisOperation: RedisOperation,
@@ -46,38 +60,15 @@ class MacBuildListener @Autowired constructor(
     }
 
     override fun onStartup(dispatchMessage: DispatchMessage) {
-        logger.info("MacOS Dispatch on start up - ($dispatchMessage)")
-        val devCloudMacosVmInfo = devCloudMacosService.creatVM(dispatchMessage)
-
-        devCloudMacosVmInfo?.let {
-            devCloudMacosService.saveVM(it)
-            buildHistoryService.saveBuildHistory(dispatchMessage, it.ip, it.id, "DEVCLOUD")
-            macosVMRedisService.saveRedisBuild(dispatchMessage, it.ip)
-
-            logger.info("[${dispatchMessage.projectId}|${dispatchMessage.pipelineId}|${dispatchMessage.buildId}] " +
-                            "Success to start vm(${it.ip}|${it.id})")
-
-            log(
-                buildLogPrinter = buildLogPrinter,
-                buildId = dispatchMessage.buildId,
-                containerHashId = dispatchMessage.containerHashId,
-                vmSeqId = dispatchMessage.vmSeqId,
-                message = "DevCloud MacOS IP：${it.ip}",
-                executeCount = dispatchMessage.executeCount
-            )
-        } ?: run {
-            // 如果没有找到合适的vm机器，则等待10秒后再执行, 总共执行30次（5min）
-            logRed(
-                buildLogPrinter,
-                dispatchMessage.buildId,
-                dispatchMessage.containerHashId,
-                dispatchMessage.vmSeqId,
-                "No idle macOS resources found, wait 10 seconds and try again",
-                dispatchMessage.executeCount
-            )
-
-            logger.error("Can not found any idle vm for this build($dispatchMessage),wait for 10s")
-            retry(sleepTimeInMS = 10000, retryTimes = 30)
+        try {
+            MacOSThreadPoolUtils.instance.getThreadPool(ThreadPoolName.STARTUP).execute {
+                doStartup(dispatchMessage)
+            }
+        } catch (e: RejectedExecutionException) {
+            // 构建任务被线程池拒绝，重新回队列
+            logger.info("${dispatchMessage.buildId}|${dispatchMessage.vmSeqId}|${dispatchMessage.executeCount} " +
+                            "build task rejected. Retry")
+            retry(sleepTimeInMS = 5000, retryTimes = 120)
         }
     }
 
@@ -120,6 +111,91 @@ class MacBuildListener @Autowired constructor(
         } finally {
             redisLock.unlock()
         }
+    }
+
+    private fun doStartup(dispatchMessage: DispatchMessage) {
+        logger.info("MacOS Dispatch on start up - ($dispatchMessage)")
+
+        try {
+            val devCloudMacosVmInfo = devCloudMacosService.creatVM(dispatchMessage)
+
+            devCloudMacosVmInfo?.let {
+                devCloudMacosService.saveVM(it)
+                buildHistoryService.saveBuildHistory(dispatchMessage, it.ip, it.id, "DEVCLOUD")
+                macosVMRedisService.saveRedisBuild(dispatchMessage, it.ip)
+
+                logger.info("[${dispatchMessage.projectId}|${dispatchMessage.pipelineId}|${dispatchMessage.buildId}] " +
+                                "Success to start vm(${it.ip}|${it.id})")
+
+                log(
+                    buildLogPrinter = buildLogPrinter,
+                    buildId = dispatchMessage.buildId,
+                    containerHashId = dispatchMessage.containerHashId,
+                    vmSeqId = dispatchMessage.vmSeqId,
+                    message = "DevCloud MacOS IP：${it.ip}",
+                    executeCount = dispatchMessage.executeCount
+                )
+            } ?: run {
+                // 如果没有找到合适的vm机器，则等待5秒后再执行, 总共执行120次（10min）
+                logRed(
+                    buildLogPrinter,
+                    dispatchMessage.buildId,
+                    dispatchMessage.containerHashId,
+                    dispatchMessage.vmSeqId,
+                    "No idle macOS resources found, wait 10 seconds and try again",
+                    dispatchMessage.executeCount
+                )
+
+                logger.error("Can not found any idle vm for this build($dispatchMessage),wait for 5s")
+                retry(sleepTimeInMS = 5000, retryTimes = 120)
+            }
+        } catch (e: BuildFailureException) {
+            handleStartupException(
+                t = e,
+                errorCode = e.errorCode,
+                errorMessage = e.formatErrorMessage,
+                errorType = e.errorType,
+                dispatchMessage = dispatchMessage
+            )
+        } catch (t: Throwable) {
+            handleStartupException(
+                t = t,
+                errorCode = DispatchSdkErrorCode.SDK_SYSTEM_ERROR,
+                errorMessage = "Fail to handle the start up message",
+                errorType = ErrorType.SYSTEM,
+                dispatchMessage = dispatchMessage
+            )
+        }
+    }
+
+    private fun handleStartupException(
+        t: Throwable,
+        errorCode: Int,
+        errorType: ErrorType,
+        errorMessage: String,
+        dispatchMessage: DispatchMessage
+    ) {
+        logger.error("Fail to handle the start up message: $dispatchMessage)", t)
+        val dispatchService = SpringContextUtil.getBean(DispatchService::class.java)
+        dispatchService.logRed(
+            buildId = dispatchMessage.buildId,
+            containerHashId = dispatchMessage.containerHashId,
+            vmSeqId = dispatchMessage.vmSeqId,
+            message = "${I18nUtil.getCodeLanMessage("${CommonMessageCode.BK_FAILED_START_BUILD_MACHINE}")} " +
+                "- ${t.message}",
+            executeCount = dispatchMessage.executeCount
+        )
+
+        client.get(ServiceBuildResource::class).setVMStatus(
+            projectId = dispatchMessage.projectId,
+            pipelineId = dispatchMessage.pipelineId,
+            buildId = dispatchMessage.buildId,
+            vmSeqId = dispatchMessage.vmSeqId,
+            status = BuildStatus.FAILED,
+            errorType = errorType,
+            errorCode = errorCode,
+            errorMsg = errorMessage
+        )
     }
 
     private fun doShutdown(
