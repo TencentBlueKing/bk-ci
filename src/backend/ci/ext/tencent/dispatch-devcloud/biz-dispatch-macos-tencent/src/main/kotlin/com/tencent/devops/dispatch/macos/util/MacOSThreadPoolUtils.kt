1d3379001c25f13de938731916e47ff8fa41ab59
sawyersong <sawyersong@tencent.com>
2023-06-08 10:20:13 +0800

    --story=883658367 macos公共构建机调度优化

--- src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/util/MacOSThreadPoolUtils.kt
+++ src/backend/ci/ext/tencent/dispatch-devcloud/biz-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/util/MacOSThreadPoolUtils.kt
@@ -1,19 +1,12 @@
 package com.tencent.devops.dispatch.macos.util
 
-import com.tencent.devops.common.redis.RedisLock
-import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Component
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
-import javax.annotation.PreDestroy
 
 /**
  * 线程池封装类
- *
  */
-@Component
 class MacOSThreadPoolUtils private constructor() {
 
     @Volatile
@@ -55,39 +48,8 @@ class MacOSThreadPoolUtils private constructor() {
         }
     }
 
-    @PreDestroy
-    fun destroy() {
-        // 当线程池内还有线程执行时，阻塞服务的退出
-        ThreadPoolName.values().forEach { poolName ->
-            threadPoolMap[poolName.name]?.let {
-                it.shutdown()
-                while (!it.awaitTermination(EXECUTOR_DESTROY_AWAIT_SECOND, TimeUnit.SECONDS)) {
-                    logger.warn("SignTaskBean still has sign tasks.")
-                }
-            }
-        }
-    }
-
-    @Scheduled(cron = "0 0/3 * * * ?")
-    fun monitorTask() {
-        ThreadPoolName.values().forEach { poolName ->
-            threadPoolMap[poolName.name]?.let {
-                logger.info("=================$poolName==================");
-                logger.info("线程池大小（Pool Size）: ${it.poolSize}}")
-                logger.info("活动线程数（Active Threads）: ${it.activeCount}")
-                logger.info("已完成任务数（Completed Task Count）: ${it.completedTaskCount}")
-                logger.info("待执行任务队列长度（Queue Length）: ${it.queue.size}")
-                logger.info("最大允许的线程数（Max Pool Size）: ${it.maximumPoolSize}")
-                logger.info("=================$poolName==================")
-            }
-        }
-    }
-
     companion object {
         val instance = MacOSThreadPoolUtils()
-
-        private const val EXECUTOR_DESTROY_AWAIT_SECOND = 5L
-        private val logger = LoggerFactory.getLogger(MacOSThreadPoolUtils::class.java)
     }
 }
 
