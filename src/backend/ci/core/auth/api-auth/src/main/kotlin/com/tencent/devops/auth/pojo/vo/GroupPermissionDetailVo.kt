eda4e2679bb5213317582c46cefd9b2a3565317e
yjieliang <yundantengteng@163.com>
2023-06-09 17:43:05 +0800

    feat:auth微服务的初始化数据需支持国际化 #8898

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/vo/GroupPermissionDetailVo.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/vo/GroupPermissionDetailVo.kt
@@ -13,6 +13,5 @@ data class GroupPermissionDetailVo(
     @BkFieldI18n(convertName = "actionName")
     val name: String,
     @ApiModelProperty("关联资源")
-    @BkFieldI18n(reusePrefixFlag = false)
     val relatedResourceInfo: RelatedResourceInfo
 )
