871bed39f7a5cc1fe9850612e57be6bde7b5e5d4
yongyiduan <yongyiduan@tencent.com>
2023-06-13 19:52:02 +0800

    --story=885092439 云桌面创建白名单+创建个数限制

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/redis/RedisCacheService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/redis/RedisCacheService.kt
@@ -9,12 +9,15 @@ import com.tencent.devops.remotedev.pojo.WorkSpaceCacheInfo
 import com.tencent.devops.remotedev.service.redis.RedisKeys.WORKSPACE_CACHE_KEY_PREFIX
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
 
 @Service
 class RedisCacheService @Autowired constructor(
     private val redisOperation: RedisOperation,
+    @Qualifier("redisStringHashOperation")
+    private val redisHashOperation: RedisOperation,
     private val objectMapper: ObjectMapper
 ) {
 
@@ -35,7 +38,7 @@ class RedisCacheService @Autowired constructor(
     private val redisCacheHash = Caffeine.newBuilder()
         .maximumSize(10)
         .expireAfterWrite(1, TimeUnit.MINUTES)
-        .build<String, Map<String, String>?> { key -> redisOperation.hentries(key) }
+        .build<String, Map<String, String>?> { key -> redisHashOperation.hentries(key) }
 
     fun get(key: String) = redisCache.get(key)
 
