c7ffcc53e476b6bc93d781a6b94ab521524fbb3c
randychen <randychen@tencent.com>
2023-06-21 18:34:17 +0800

    --story=883782297 云开发对接BCS

--- src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/ContainerStatus.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/ContainerStatus.kt
@@ -2,12 +2,13 @@ package com.tencent.devops.dispatch.kubernetes.pojo
 
 data class ContainerStatus(
     val name: String,
-    val state: ContainerState,
-    val lastState: ContainerState,
-    val ready: Boolean,
-    val restartCount: Int,
+    val state: ContainerState?,
+    val status: ContainerState?,
+    val lastState: ContainerState?,
+    val ready: Boolean?,
+    val restartCount: Int?,
     val image: String,
     val imageID: String,
     val containerID: String,
-    val started: Boolean
+    val started: Boolean?
 )
