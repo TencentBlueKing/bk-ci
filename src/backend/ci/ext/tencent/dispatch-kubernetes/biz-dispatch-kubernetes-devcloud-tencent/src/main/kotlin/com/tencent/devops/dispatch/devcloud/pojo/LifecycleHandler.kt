1271f34811750cf5a88642f70e89d371ed462903
randychen <randychen@tencent.com>
2023-06-21 15:13:27 +0800

    --story=883782297 云开发对接BCS

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-devcloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/devcloud/pojo/LifecycleHandler.kt
+++
@@ -1,6 +0,0 @@
-package com.tencent.devops.dispatch.devcloud.pojo
-
-data class LifecycleHandler(
-    val exec: ExecAction,
-    val httpGet: HTTPGetAction
-)
--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-devcloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/devcloud/pojo/ResourceRequirements.kt
+++
@@ -1,6 +0,0 @@
-package com.tencent.devops.dispatch.devcloud.pojo
-
-data class ResourceRequirements(
-    val cpu: Int,
-    val memory: Int
-)
