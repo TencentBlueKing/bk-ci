5f1dbc6714e92fd22ec631d3c4d5ff812a02fb1f
randychen <randychen@tencent.com>
2023-06-21 23:51:07 +0800

    --story=883782297 云开发对接BCS

--- src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/EnvironmentStatus.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/EnvironmentStatus.kt
@@ -4,13 +4,15 @@ import com.tencent.devops.dispatch.kubernetes.pojo.kubernetes.EnvStatusEnum
 
 data class EnvironmentStatus(
     val status: EnvStatusEnum,
-    val phase: String,
-    val message: String,
-    val reason: String,
+    val phase: String? = null,
+    val message: String ? = null,
+    val reason: String ? = null,
     val hostIP: String,
-    val environmentIP: String,
+    val podIP: String ? = null,
+    val environmentIP: String ? = null,
     val clusterId: String,
     val namespace: String,
+    val name: String ? = null,
     val startTime: String ? = null,
     val containerStatuses: List<ContainerStatus>,
     val initContainerStatuses: List<ContainerStatus> ? = null
