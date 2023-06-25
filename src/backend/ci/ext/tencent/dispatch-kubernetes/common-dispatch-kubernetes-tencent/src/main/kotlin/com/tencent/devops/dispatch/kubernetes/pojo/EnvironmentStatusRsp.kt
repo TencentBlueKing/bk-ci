525c583d5555fb88fcbf94f6cde7f98b84ef4315
randychen <randychen@tencent.com>
2023-06-25 14:27:48 +0800

    --story=883782297 云开发对接BCS

--- src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/EnvironmentStatusRsp.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/common-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/pojo/EnvironmentStatusRsp.kt
@@ -1,6 +1,7 @@
 package com.tencent.devops.dispatch.kubernetes.pojo
 
 data class EnvironmentStatusRsp(
+    val result: Boolean? = false,
     val code: Int,
     val message: String,
     val data: EnvironmentStatus
