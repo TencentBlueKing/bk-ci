e0986925d1e4002f891eff377cb923603c695ea2
ruotiantang <ruotiantang@tencent.com>
2023-06-29 10:38:58 +0800

    第三方构建机容器化环境支持登录调试 #8915

--- src/agent/src/pkg/logs/logs.go
+++ src/agent/src/pkg/logs/logs.go
@@ -1,8 +1,10 @@
 package logs
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
@@ -23,11 +25,7 @@ func Init(filepath string, isDebug bool) error {
 
 	logInfo.Logger.Out = lumLog
 
-	logInfo.Logger.SetFormatter(&jsonFormatter{
-		log.JSONFormatter{
-			TimestampFormat: "2006-01-02 15:04:05.000",
-		},
-	})
+	logInfo.Logger.SetFormatter(&MyFormatter{})
 
 	go DoDailySplitLog(filepath, lumLog)
 
@@ -43,14 +41,70 @@ func Init(filepath string, isDebug bool) error {
 // DebugInit 初始化为debug模式下的log，将日志输出到标准输出流，只是为了单元测试使用
 func UNTestDebugInit() {
 	logInfo := log.WithFields(log.Fields{})
-	logInfo.Logger.SetFormatter(&jsonFormatter{
-		log.JSONFormatter{
-			TimestampFormat: "2006-01-02 15:04:05.000",
-		},
-	})
+	logInfo.Logger.SetFormatter(&MyFormatter{})
 	logInfo.Logger.SetOutput(os.Stdout)
 	logInfo.Logger.SetLevel(logrus.DebugLevel)
 	Logs = logInfo
+}
+
+type MyFormatter struct{}
+
+func (m *MyFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+
+	timestamp := entry.Time.Format("2006-01-02 15:04:05.000")
+	var newLog string
+
+	level, err := parseLevel(entry.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	newLog = fmt.Sprintf("%s [%s]  %s", timestamp, level, entry.Message)
+	b.WriteString(newLog)
+
+	for k, v := range entry.Data {
+		switch v := v.(type) {
+		case error:
+			// Otherwise errors are ignored by `encoding/json`
+			// https://github.com/sirupsen/logrus/issues/137
+			//
+			// Print errors verbosely to get stack traces where available
+			b.WriteString(fmt.Sprintf("%s:%+v ", k, v))
+		default:
+			b.WriteString(fmt.Sprintf("%s:%v ", k, v))
+		}
+	}
+
+	b.WriteString("/n")
+
+	return b.Bytes(), nil
+}
+
+func parseLevel(l logrus.Level) (string, error) {
+	switch strings.ToLower(l.String()) {
+	case "panic":
+		return "P", nil
+	case "fatal":
+		return "F", nil
+	case "error":
+		return "E", nil
+	case "warn", "warning":
+		return "W", nil
+	case "info":
+		return "I", nil
+	case "debug":
+		return "D", nil
+	case "trace":
+		return "T", nil
+	}
+
+	return "U", fmt.Errorf("not a valid logrus Level: %q", l)
 }
 
 type jsonFormatter struct {
