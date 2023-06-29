a7473b79119aff58a039e8c808ead4f372b610e4
randychen <randychen@tencent.com>
2023-06-29 15:11:52 +0800

    --story=883782297 云开发对接BCS

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/BkTicketService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/BkTicketService.kt
@@ -95,7 +95,7 @@ class BkTicketService @Autowired constructor(
         params["ticket"] = bkTicket
         params["user"] = userId
         val headers = mutableMapOf(
-            "Cookie" to "X-DEVOPS-BK-TICKET=$bkTicket;X-Remotedev-Gateway-Tag=${mountType.toString().lowercase()}"
+            "Cookie" to "X-DEVOPS-BK-TICKET=$bkTicket;X-REMOTEDEV-GATEWAY-TAG=${mountType.toString().lowercase()}"
         )
         val request = Request.Builder()
             .url(commonService.getProxyUrl(url))
