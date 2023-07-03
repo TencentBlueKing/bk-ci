f2fe1c830f624d71afcca23f4687093d3cd48cda
ruotiantang <ruotiantang@tencent.com>
2023-07-03 15:06:01 +0800

    feat: 修改第三方构建机监控指标 #8418

--- src/agent/src/pkg/collector/telegrafConf/telegrafConf.go
+++ src/agent/src/pkg/collector/telegrafConf/telegrafConf.go
@@ -51,13 +51,13 @@ const TelegrafConf = `
   logfile = ""
   hostname = ""
   omit_hostname = false
-{{- if eq . "stream" -}}
+{{ if eq . "stream" }}
 [[outputs.influxdb]]
   urls = ["###{gateway}###/ms/environment/api/buildAgent/agent/thirdPartyAgent/agents/metrix"]
   database = "agentMetric"
   skip_database_creation = true
   ###{tls_ca}###
-{{- else -}}
+{{ else }}
 [[outputs.http]]
   url = "###{gateway}###/ms/environment/api/buildAgent/agent/thirdPartyAgent/agents/metrics"
   # timeout = "5s"
@@ -69,7 +69,7 @@ const TelegrafConf = `
     X-DEVOPS-PROJECT-ID = "###{projectId}###"
     X-DEVOPS-AGENT-ID = "###{agentId}###"
     X-DEVOPS-AGENT-SECRET-KEY = "###{agentSecret}###"
-{{- end -}}
+{{ end }}
 [[inputs.cpu]]
   percpu = true
   totalcpu = true
