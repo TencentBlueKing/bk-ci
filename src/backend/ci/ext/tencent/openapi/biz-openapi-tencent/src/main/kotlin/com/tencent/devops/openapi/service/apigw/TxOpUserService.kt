6118bb512dec00d7199dc308e8c63ceec372667f
mingshewhe <mingshewhe@tencent.com>
2023-07-05 15:26:59 +0800

    --story=885541937 openapi用户过滤器接口使用用户管理

--- src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/service/apigw/TxOpUserService.kt
+++ src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/service/apigw/TxOpUserService.kt
@@ -4,7 +4,6 @@ import com.google.common.cache.CacheBuilder
 import com.tencent.devops.auth.api.service.ServiceDeptResource
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.openapi.service.op.OpAppUserService
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
@@ -14,26 +13,17 @@ class TxOpUserService @Autowired constructor(
     val client: Client
 ) : OpAppUserService {
 
-    private val errorUserId = CacheBuilder.newBuilder()
-        .maximumSize(1000)
+    private val userCache = CacheBuilder.newBuilder()
+        .maximumSize(5000)
         .expireAfterWrite(2, TimeUnit.MINUTES)
         .build<String, Boolean>()
 
     override fun checkUser(userId: String): Boolean {
-        if (errorUserId.getIfPresent(userId) != null &&
-            errorUserId.getIfPresent(userId) == true) {
-            return false
+        return userCache.getIfPresent(userId) ?: let {
+            val result =
+                client.get(ServiceDeptResource::class).getUserInfo(userId = "admin", name = userId).data != null
+            userCache.put(userId, result)
+            return result
         }
-        return try {
-            client.get(ServiceDeptResource::class).getUserInfo(userId = "admin", name = userId).data != null
-        } catch (e: Exception) {
-            logger.warn("checkUser $userId is not rtx user")
-            errorUserId.put(userId, true)
-            false
-        }
-    }
-
-    companion object {
-        val logger = LoggerFactory.getLogger(TxOpUserService::class.java)
     }
 }
