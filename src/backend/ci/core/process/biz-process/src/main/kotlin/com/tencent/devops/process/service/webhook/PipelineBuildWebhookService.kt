f380039f668c0231ddda646b6dcefdb2c54d1ac5
hejieehe <904696180@qq.com>
2023-07-11 08:57:39 +0800

    feat：build_msg需要根据事件触发场景细化 #8831

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
@@ -434,7 +434,6 @@ abstract class PipelineBuildWebhookService : ApplicationContextAware {
                         eventType = matcher.getEventType(),
                         codeType = matcher.getCodeType()
                     )
-                    logger.info("webhook trigger pipeline build by webhookCommit|$webhookCommit")
                     val buildId =
                         client.getGateway(ServiceScmWebhookResource::class).webhookCommit(projectId, webhookCommit).data
                     PipelineWebhookBuildLogContext.addLogBuildInfo(
@@ -508,7 +507,6 @@ abstract class PipelineBuildWebhookService : ApplicationContextAware {
                 pipelineParamMap[newVarName] = BuildParameters(key = newVarName, value = it.value)
             }
         }
-        logger.info("start build by webhook trigger commit|$pipelineParamMap")
         val startEpoch = System.currentTimeMillis()
         try {
             val buildId = pipelineBuildService.startPipeline(
