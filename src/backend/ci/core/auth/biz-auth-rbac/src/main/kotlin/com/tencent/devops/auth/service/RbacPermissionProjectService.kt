83d67d64eddacc2f71ff102977da25676729fd6d
greysonfang <greysonfang@tencent.com>
2023-07-11 17:47:30 +0800

    实现openapi RBAC权限版本 项目下用户组添加成员 #9093

--- src/backend/ci/core/auth/biz-auth-rbac/src/main/kotlin/com/tencent/devops/auth/service/RbacPermissionProjectService.kt
+++ src/backend/ci/core/auth/biz-auth-rbac/src/main/kotlin/com/tencent/devops/auth/service/RbacPermissionProjectService.kt
@@ -223,8 +223,9 @@ class RbacPermissionProjectService(
         members: List<String>
     ): Boolean {
         // 由于v0迁移过来的ci管理员没有存储在用户组表中，需要去iam搜索
+        logger.info("batchCreateProjectUser:$userId|$projectCode|$roleCode|$members")
         val iamGroupId = if (roleCode == BkAuthGroup.CI_MANAGER.value) {
-            projectCode2CiManagerGroupId.getIfPresent(projectCode)?.run {
+            projectCode2CiManagerGroupId.getIfPresent(projectCode) ?: run {
                 val gradeManagerId = authResourceService.get(
                     projectCode = projectCode,
                     resourceType = AuthResourceType.PROJECT.value,
@@ -236,7 +237,7 @@ class RbacPermissionProjectService(
                     gradeManagerId = gradeManagerId,
                     searchGroupDTO = searchGroupDTO,
                     page = 1,
-                    pageSize = 1000
+                    pageSize = 10
                 ).firstOrNull { it.name == BkAuthGroup.CI_MANAGER.groupName }?.id?.toString()
                     ?: throw ErrorCodeException(
                         errorCode = AuthMessageCode.ERROR_AUTH_GROUP_NOT_EXIST,
