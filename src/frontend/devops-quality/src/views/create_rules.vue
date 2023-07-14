2c479b307af59091c98cc966a3ce470472173b99
vhwweng <1010382269@qq.com>
2023-07-14 10:44:59 +0800

    feat: 蓝盾国际化优化 #8975

--- src/frontend/devops-quality/src/views/create_rules.vue
+++ src/frontend/devops-quality/src/views/create_rules.vue
@@ -172,8 +172,8 @@
                             <bk-select v-model="createRuleForm.controlPointPosition" style="width:467px;">
                                 <bk-option v-for="(option, index) in createRuleForm.availablePosition"
                                     :key="index"
-                                    :id="option.name"
-                                    :name="option.i18nName">
+                                    :id="option.code"
+                                    :name="option.name">
                                 </bk-option>
                             </bk-select>
                         </bk-form-item>
