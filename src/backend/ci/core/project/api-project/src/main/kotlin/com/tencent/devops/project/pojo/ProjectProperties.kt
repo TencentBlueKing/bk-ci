45d994de48fbf1b77e6132b8084fce5c51d0a495
randychen <randychen@tencent.com>
2023-07-20 20:48:24 +0800

    feature:【OP需求】项目设置支持“启用云研发” #9178

--- src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/ProjectProperties.kt
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/ProjectProperties.kt
@@ -34,5 +34,7 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel("项目其他配置")
 data class ProjectProperties(
     @ApiModelProperty("YAML流水线功能设置")
-    val pipelineAsCodeSettings: PipelineAsCodeSettings
+    val pipelineAsCodeSettings: PipelineAsCodeSettings,
+    @ApiModelProperty("是否启用云研发", required = true)
+    val remotedev: Boolean? = false
 )
