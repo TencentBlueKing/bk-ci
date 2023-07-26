8b8644685dd81858fa6cfc5917e10e155006d2e1
yongyiduan <yongyiduan@tencent.com>
2023-07-26 15:00:53 +0800

    增加job.retry_task_id标识当前被重试的任务ID #9173

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelineRetryFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/builds/PipelineRetryFacadeService.kt
@@ -34,8 +34,10 @@ import com.tencent.devops.common.log.utils.BuildLogPrinter
 import com.tencent.devops.common.pipeline.container.Container
 import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.common.pipeline.pojo.time.BuildRecordTimeLine
+import com.tencent.devops.common.redis.RedisOperation
 import com.tencent.devops.process.constant.ProcessMessageCode
 import com.tencent.devops.process.engine.common.VMUtils
+import com.tencent.devops.process.engine.control.lock.ContainerIdLock
 import com.tencent.devops.process.engine.pojo.PipelineBuildContainer
 import com.tencent.devops.process.engine.pojo.PipelineBuildTask
 import com.tencent.devops.process.engine.pojo.event.PipelineBuildContainerEvent
@@ -56,6 +58,7 @@ import org.springframework.stereotype.Service
 @Suppress("ReturnCount", "LongParameterList", "LongMethod")
 class PipelineRetryFacadeService @Autowired constructor(
     val dslContext: DSLContext,
+    val redisOperation: RedisOperation,
     val pipelineEventDispatcher: PipelineEventDispatcher,
     val pipelineTaskService: PipelineTaskService,
     val pipelineContainerService: PipelineContainerService,
@@ -97,16 +100,19 @@ class PipelineRetryFacadeService @Autowired constructor(
         // 校验当前job的关机事件是否有完成
         checkStopTask(projectId, buildId, containerInfo!!)
         // 刷新当前job的开关机以及job状态， container状态， detail数据, record数据
-        refreshTaskAndJob(
-            userId = userId,
-            projectId = projectId,
-            buildId = buildId,
-            executeCount = executeCount,
-            resourceVersion = resourceVersion,
-            taskId = taskId,
-            containerInfo = containerInfo,
-            skipFailedTask = skipFailedTask
-        )
+        ContainerIdLock(redisOperation, buildId, containerInfo.containerId).use {
+            it.lock()
+            refreshTaskAndJob(
+                userId = userId,
+                projectId = projectId,
+                buildId = buildId,
+                executeCount = executeCount,
+                resourceVersion = resourceVersion,
+                taskId = taskId,
+                containerInfo = containerInfo,
+                skipFailedTask = skipFailedTask
+            )
+        }
         // 发送container Refresh事件，重新开始task对应的调度
         sendContainerEvent(taskInfo, userId)
         buildLogPrinter.addYellowLine(
