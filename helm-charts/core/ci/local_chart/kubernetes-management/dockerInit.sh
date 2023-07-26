6c020d231c234fade63949a4070fc97d0cf4ce61
sawyersong <sawyersong@tencent.com>
2023-07-26 12:31:22 +0800

    feat: 容器化无编译构建环境改造 #8534

--- helm-charts/core/ci/local_chart/kubernetes-management/dockerInit.sh
+++ helm-charts/core/ci/local_chart/kubernetes-management/dockerInit.sh
@@ -21,5 +21,20 @@ else
   echo "local /data/bkdevops/apps/jdk/KonaJDK8_landun is not exsited." > logs/docker.log
 fi
 
-cat docker_init.sh >> logs/docker.log
-sh docker_init.sh $@
+## Download the docker.jar
+echo "start to download the docker.jar..." >> logs/docker.log
+
+curl -k -s -H "X-DEVOPS-BUILD-TYPE: DOCKER" -H "X-DEVOPS-PROJECT-ID: ${devops_project_id}" -H "X-DEVOPS-AGENT-ID: ${devops_agent_id}" -H "X-DEVOPS-AGENT-SECRET-KEY: ${devops_agent_secret_key}"  -o docker.jar "${devops_gateway}/static/local/files/jar/worker-agent.jar"
+
+echo "download the docker.jar finished, ready to start it..." >> logs/docker.log
+md5sum docker.jar >> logs/docker.log 2>&1
+fileSize=`wc -c docker.jar | awk '{print $1}'`
+echo $fileSize >> logs/docker.log
+if [ "$fileSize" -lt 102400 ]
+then
+    cat docker.jar >> logs/docker.log
+fi
+
+random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+
+/usr/local/jre/bin/java -Dfile.encoding=UTF-8 -DLC_CTYPE=UTF-8 -Dsun.jnu.encoding=UTF-8 -DLOG_PATH=/data/devops/logs/${random_string} -Dlandun.env=prod -Dbuild.type=DOCKER -Ddevops.gateway=${devops_gateway}  -jar docker.jar $@ >> logs/docker.log 2>&1
