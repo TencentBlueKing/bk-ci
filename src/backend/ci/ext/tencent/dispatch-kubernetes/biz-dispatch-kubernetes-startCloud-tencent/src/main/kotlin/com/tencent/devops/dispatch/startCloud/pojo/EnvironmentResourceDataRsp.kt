777bb04d23f42e59cc735f129b88f2378265259c
randychen <randychen@tencent.com>
2023-07-26 17:38:29 +0800

    --story=886112313 云桌面：定时查询数据；增加视图面板

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentResourceDataRsp.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentResourceDataRsp.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class EnvironmentResourceDataRsp(
     val code: Int,
     val data: List<EnvironmentResourceData>?,
