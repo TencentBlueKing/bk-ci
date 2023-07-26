2c0dc4c6475fd6ab886e966ba8748141f1528363
stubenhuang <stubenhuang@tencent.com>
2023-07-26 15:08:16 +0800

    feat: 支持ESB登录逻辑 #9141

--- src/gateway/core/lua/util/oauth_util.lua
+++ src/gateway/core/lua/util/oauth_util.lua
@@ -132,7 +132,8 @@ function _M:is_login(bk_token)
     end
     if result.code == 1302403 then
         ngx.log(ngx.ERR, "is_login code is 1302403 , need Authentication")
-        ngx.header["X-DEVOPS-ERROR-RETURN"] = '{"status": 417,"message": "'..result.message..'", "errorCode": 1302403}'
+        ngx.header["X-DEVOPS-ERROR-RETURN"] = '{"status": 417,"message": "'..result.message..'", "errorCode": 1302403 , "redirectUrl":"/esb.error.html?message='..result.message..'"}'
+        ngx.header["X-DEVOPS-ERROR-STATUS"] = 417
         ngx.exit(401)
     elseif result.code ~= 0 then
         ngx.log(ngx.ERR, "is_login code is " .. result.code .. " , return 401")
