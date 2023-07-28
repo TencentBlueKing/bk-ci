9f73a5a6479d6080cd2629581c7b45c21df911b7
irwinsun <irwinsun@tencent.com>
2023-07-28 17:00:12 +0800

    缩减微服务的子模块数量 #9166 artifactory迁移子模块sample

--- src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/resources/UserLogFileResourceImpl.kt
+++ src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/resources/UserLogFileResourceImpl.kt
@@ -30,9 +30,7 @@ package com.tencent.devops.artifactory.resources
 import com.tencent.devops.artifactory.api.user.UserLogFileResource
 import com.tencent.devops.artifactory.pojo.Url
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.web.RestResource
 
-@RestResource
 class UserLogFileResourceImpl : UserLogFileResource {
 
     override fun getPluginLogUrl(
