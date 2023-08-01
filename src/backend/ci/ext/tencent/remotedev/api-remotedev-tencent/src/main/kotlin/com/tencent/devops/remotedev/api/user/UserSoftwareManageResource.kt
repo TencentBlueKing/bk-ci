c29aa11436b608e44fbfaff57013e5649af47535
randychen <randychen@tencent.com>
2023-08-01 14:36:29 +0800

    --story=886017433 软件管理

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserSoftwareManageResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserSoftwareManageResource.kt
@@ -66,7 +66,7 @@ interface UserSoftwareManageResource {
 
     @ApiOperation("导入软件到项目列表")
     @POST
-    @Path("/list")
+    @Path("/import")
     fun importSoftwareToProject(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
