828e55cc39bcf44198afb9fc14d5ed6c4c9cebde
yaoxuwan <yaoxuwan@tencent.com>
2023-08-04 11:20:44 +0800

    fix: 归档报告时，pdf类型的入口文件无法显示 #9250

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/plugin/worker/task/archive/ReportArchiveTask.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/plugin/worker/task/archive/ReportArchiveTask.kt
@@ -125,7 +125,6 @@ class ReportArchiveTask : ITask() {
             addEnv(REPORT_DYNAMIC_ROOT_URL, reportRootUrl)
 
             indexFileContent = indexFile.readText()
-            logger.info("index file name: ${indexFile.name}, ${indexFile.name.endsWith(".pdf")}")
             // pdf文件即使变量不存在，重新写入文件，虽然文件md5不变，但是会无法正常显示
             if (!indexFile.name.endsWith(".pdf")) {
                 indexFileContent = indexFileContent.replace("\${$REPORT_DYNAMIC_ROOT_URL}", reportRootUrl)
