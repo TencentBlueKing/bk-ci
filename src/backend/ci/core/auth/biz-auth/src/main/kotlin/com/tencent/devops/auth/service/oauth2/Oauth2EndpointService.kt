ba99cd08ab618139ff8c12fae6736f6a659baeb0
greysonfang <greysonfang@tencent.com>
2023-08-04 16:26:17 +0800

    蓝盾oauth2鉴权实现 #9110

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/oauth2/Oauth2EndpointService.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/oauth2/Oauth2EndpointService.kt
@@ -23,7 +23,7 @@ class Oauth2EndpointService constructor(
         userId: String,
         clientId: String
     ): Oauth2AuthorizationInfoVo {
-        logger.info("get authorization information:$userId|$clientId")
+        logger.info("get authorization information :$userId|$clientId")
         // 1、校验clientId是否存在
         val clientDetails = clientService.getClientDetails(clientId = clientId)
         // 2、校验客户端信息是否正确
