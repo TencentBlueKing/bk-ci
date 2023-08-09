162733b8d096130f07f4855f1a5545072a33a56b
randychen <randychen@tencent.com>
2023-08-09 15:53:42 +0800

    --story=886017433 软件管理

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/service/StartCloudRemoteDevService.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/service/StartCloudRemoteDevService.kt
@@ -80,7 +80,7 @@ class StartCloudRemoteDevService @Autowired constructor(
             EnvironmentCreate(
                 userId = userId,
                 appName = appName,
-                pipeLineId = event.projectId,
+                pipeLineId = appName + "_" + event.projectId,
                 zoneId = event.devFile.zoneId,
                 machineType = event.devFile.machineType
             )
