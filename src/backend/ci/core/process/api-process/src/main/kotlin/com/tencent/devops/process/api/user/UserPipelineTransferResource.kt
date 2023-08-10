5a74846a8c9b1bc005c5a77d6543fefb68498186
yongyiduan <yongyiduan@tencent.com>
2023-08-10 11:05:57 +0800

    【蓝盾-评审会已评审】【PAC】feat：新建/编辑流水线支持以 Code 方式编排流水线 #8125

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineTransferResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineTransferResource.kt
@@ -92,7 +92,7 @@ interface UserPipelineTransferResource {
 
     @ApiOperation("task转json格式")
     @POST
-    @Path("/{projectId}/task2yaml")
+    @Path("/{projectId}/task2model")
     fun yamlTaskTransfer(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
