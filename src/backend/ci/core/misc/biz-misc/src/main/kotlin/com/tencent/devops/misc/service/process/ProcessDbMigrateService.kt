e4c38a43556861686f44fc1562b95fe11ae6df67
carlyin <449036249@qq.com>
2023-08-14 12:07:07 +0800

    pref:支持将分区库里的项目的数据迁移至指定数据库 #9188

--- src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/service/process/ProcessDbMigrateService.kt
+++ src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/service/process/ProcessDbMigrateService.kt
@@ -33,6 +33,8 @@ import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.common.pipeline.enums.ChannelCode
 import com.tencent.devops.common.service.utils.SpringContextUtil
 import com.tencent.devops.misc.dao.process.ProcessDao
+import com.tencent.devops.misc.pojo.process.MigratePipelineDataParam
+import com.tencent.devops.misc.task.MigratePipelineDataTask
 import com.tencent.devops.model.process.tables.TPipelineBuildHistory
 import com.tencent.devops.process.api.service.ServiceBuildResource
 import org.jooq.DSLContext
@@ -63,103 +65,55 @@ class ProcessDbMigrateService @Autowired constructor(
         cancelFlag: Boolean = false
     ): Boolean {
         logger.info("migrateProjectData params:[$userId|$projectId]")
-        // 查询项目下流水线数量
-        val pipelineNum = processDao.getPipelineNumByProjectId(dslContext, projectId)
-        // 根据流水线数量计算线程数量
-        val threadNum = if (pipelineNum < DEFAULT_THREAD_NUM) {
-            pipelineNum
-        } else {
-            DEFAULT_THREAD_NUM
-        }
-        // 根据线程数量创建线程池
-        val executor = Executors.newFixedThreadPool(threadNum)
-        // 根据线程数量创建信号量
-        val semaphore = Semaphore(threadNum)
-        // 根据流水线数量创建计数器
-        val doneSignal = CountDownLatch(pipelineNum)
-        var minPipelineInfoId = processDao.getMinPipelineInfoIdByProjectId(dslContext, projectId)
-        do {
-            val pipelineIdList = processDao.getPipelineIdListByProjectId(
-                dslContext = dslContext,
-                projectId = projectId,
-                minId = minPipelineInfoId,
-                limit = DEFAULT_PAGE_SIZE.toLong()
-            )?.map { it.getValue(0).toString() }
-            if (!pipelineIdList.isNullOrEmpty()) {
-                // 重置minId的值
-                minPipelineInfoId = (processDao.getPipelineInfoByPipelineId(
-                    dslContext = dslContext,
-                    projectId = projectId,
-                    pipelineId = pipelineIdList[pipelineIdList.size - 1]
-                )?.id ?: 0L) + 1
+        Executors.newFixedThreadPool(1).submit {
+            // 查询项目下流水线数量
+            val pipelineNum = processDao.getPipelineNumByProjectId(dslContext, projectId)
+            // 根据流水线数量计算线程数量
+            val threadNum = if (pipelineNum < DEFAULT_THREAD_NUM) {
+                pipelineNum
+            } else {
+                DEFAULT_THREAD_NUM
             }
-            pipelineIdList?.forEach { pipelineId ->
-                // executor.submit()
-
-            }
-        } while (pipelineIdList?.size == DEFAULT_PAGE_SIZE)
-        return true
-    }
-
-    class MigratePipelineDataTask constructor(
-        private val projectId: String,
-        private val pipelineId: String,
-        private val cancelFlag: Boolean,
-        private val semaphore: Semaphore,
-        private val doneSignal: CountDownLatch,
-        private val dslContext: DSLContext,
-        private val processDao: ProcessDao
-    ) : Runnable {
-        override fun run() {
-            // 获取是否允许执行的信号量
-            semaphore.acquire()
-            logger.info("migrateProjectData project[$projectId],pipeline[$pipelineId] start..............")
-            try {
-                if (cancelFlag) {
-                    handleUnFinishPipelines()
-                }
-            } finally {
-                // 业务逻辑执行完成后释放信号量
-                semaphore.release()
-                // 业务逻辑执行完后计数器减1
-                doneSignal.countDown()
-            }
-        }
-
-        private fun handleUnFinishPipelines() {
-            // 查看项目下是否还有未结束的构建
-            val unFinishStatusList = listOf(
-                BuildStatus.QUEUE,
-                BuildStatus.QUEUE_CACHE,
-                BuildStatus.RUNNING
-            )
-            var offset = 0
+            // 根据线程数量创建线程池
+            val executor = Executors.newFixedThreadPool(threadNum)
+            // 根据线程数量创建信号量
+            val semaphore = Semaphore(threadNum)
+            // 根据流水线数量创建计数器
+            val doneSignal = CountDownLatch(pipelineNum)
+            var minPipelineInfoId = processDao.getMinPipelineInfoIdByProjectId(dslContext, projectId)
             do {
-                // 查询未结束的构建记录
-                val historyInfoRecords = processDao.getHistoryInfoList(
+                val pipelineIdList = processDao.getPipelineIdListByProjectId(
                     dslContext = dslContext,
                     projectId = projectId,
-                    pipelineId = pipelineId,
-                    offset = offset,
-                    limit = DEFAULT_PAGE_SIZE,
-                    statusList = unFinishStatusList
-                )
-                val tPipelineBuildHistory = TPipelineBuildHistory.T_PIPELINE_BUILD_HISTORY
-                historyInfoRecords?.forEach { historyInfoRecord ->
-                    val buildId = historyInfoRecord[tPipelineBuildHistory.BUILD_ID]
-                    val channel = historyInfoRecord[tPipelineBuildHistory.CHANNEL]
-                    val startUser = historyInfoRecord[tPipelineBuildHistory.START_USER]
-                    val client = SpringContextUtil.getBean(Client::class.java)
-                    client.get(ServiceBuildResource::class).manualShutdown(
-                        userId = startUser,
+                    minId = minPipelineInfoId,
+                    limit = DEFAULT_PAGE_SIZE.toLong()
+                )?.map { it.getValue(0).toString() }
+                if (!pipelineIdList.isNullOrEmpty()) {
+                    // 重置minId的值
+                    minPipelineInfoId = (processDao.getPipelineInfoByPipelineId(
+                        dslContext = dslContext,
                         projectId = projectId,
-                        pipelineId = pipelineId,
-                        buildId = buildId,
-                        channelCode = ChannelCode.getChannel(channel) ?: ChannelCode.BS
+                        pipelineId = pipelineIdList[pipelineIdList.size - 1]
+                    )?.id ?: 0L) + 1
+                }
+                pipelineIdList?.forEach { pipelineId ->
+                    executor.submit(
+                        MigratePipelineDataTask(
+                            migratePipelineDataParam = MigratePipelineDataParam(
+                                projectId = projectId,
+                                pipelineId = pipelineId,
+                                cancelFlag = cancelFlag,
+                                semaphore = semaphore,
+                                doneSignal = doneSignal,
+                                dslContext = dslContext,
+                                processDao = processDao
+                            )
+                        )
                     )
                 }
-                offset += DEFAULT_PAGE_SIZE
-            } while (historyInfoRecords?.size == DEFAULT_PAGE_SIZE)
+            } while (pipelineIdList?.size == DEFAULT_PAGE_SIZE)
+            doneSignal.await()
         }
+        return true
     }
 }
---
+++ src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/task/MigratePipelineDataTask.kt
@@ -0,0 +1,99 @@
+package com.tencent.devops.misc.task
+
+import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.pipeline.enums.BuildStatus
+import com.tencent.devops.common.pipeline.enums.ChannelCode
+import com.tencent.devops.common.service.utils.SpringContextUtil
+import com.tencent.devops.misc.pojo.process.MigratePipelineDataParam
+import com.tencent.devops.model.process.tables.TPipelineBuildHistory
+import com.tencent.devops.process.api.service.ServiceBuildResource
+import org.slf4j.LoggerFactory
+
+class MigratePipelineDataTask constructor(
+    private val migratePipelineDataParam: MigratePipelineDataParam
+) : Runnable {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(MigratePipelineDataTask::class.java)
+        private const val DEFAULT_PAGE_SIZE = 20
+        private const val RETRY_NUM = 3
+    }
+        override fun run() {
+            val semaphore = migratePipelineDataParam.semaphore
+            val projectId = migratePipelineDataParam.projectId
+            val pipelineId = migratePipelineDataParam.pipelineId
+            val cancelFlag = migratePipelineDataParam.cancelFlag
+            val doneSignal = migratePipelineDataParam.doneSignal
+            // 获取是否允许执行的信号量
+            semaphore.acquire()
+            logger.info("migrateProjectData project[$projectId],pipeline[$pipelineId] start..............")
+            try {
+                if (cancelFlag) {
+                    handleUnFinishPipelines(RETRY_NUM)
+                }
+            } finally {
+                // 业务逻辑执行完成后释放信号量
+                semaphore.release()
+                // 业务逻辑执行完后计数器减1
+                doneSignal.countDown()
+            }
+        }
+
+        private fun handleUnFinishPipelines(retryNum: Int) {
+            // 查看项目下是否还有未结束的构建
+            val unFinishStatusList = listOf(
+                BuildStatus.QUEUE,
+                BuildStatus.QUEUE_CACHE,
+                BuildStatus.RUNNING
+            )
+            val projectId = migratePipelineDataParam.projectId
+            val pipelineId = migratePipelineDataParam.pipelineId
+            var offset = 0
+            var retryFlag = false
+            do {
+                // 查询未结束的构建记录
+                val historyInfoRecords = migratePipelineDataParam.processDao.getHistoryInfoList(
+                    dslContext = migratePipelineDataParam.dslContext,
+                    projectId = projectId,
+                    pipelineId = pipelineId,
+                    offset = offset,
+                    limit = DEFAULT_PAGE_SIZE,
+                    statusList = unFinishStatusList
+                )
+                val tPipelineBuildHistory = TPipelineBuildHistory.T_PIPELINE_BUILD_HISTORY
+                historyInfoRecords?.forEach { historyInfoRecord ->
+                    val buildId = historyInfoRecord[tPipelineBuildHistory.BUILD_ID]
+                    val channel = historyInfoRecord[tPipelineBuildHistory.CHANNEL]
+                    val startUser = historyInfoRecord[tPipelineBuildHistory.START_USER]
+                    val client = SpringContextUtil.getBean(Client::class.java)
+                    try {
+                        val shutdownResult = client.get(ServiceBuildResource::class).manualShutdown(
+                            userId = startUser,
+                            projectId = projectId,
+                            pipelineId = pipelineId,
+                            buildId = buildId,
+                            channelCode = ChannelCode.getChannel(channel) ?: ChannelCode.BS
+                        )
+                        if (shutdownResult.isNotOk()) {
+                            logger.warn("porject[$projectId]-pipelineId[$pipelineId]-buildId[$buildId] cancel fail")
+                            retryFlag = true
+                        }
+                    } catch (ignored: Throwable) {
+                        logger.warn(
+                            "porject[$projectId]-pipelineId[$pipelineId]-buildId[$buildId] cancel fail",
+                            ignored
+                        )
+                        retryFlag = true
+                    }
+                }
+                offset += DEFAULT_PAGE_SIZE
+            } while (historyInfoRecords?.size == DEFAULT_PAGE_SIZE)
+            if (retryFlag) {
+                if (retryNum > 0) {
+                    Thread.sleep(25000)
+                    // 重试取消动作
+                    handleUnFinishPipelines(retryNum - 1)
+                }
+            }
+        }
+    }
