48ef302efa1a1c1be515eb8dba600e155a420a13
royalhuang <royalhuang@tencent.com>
2023-08-15 10:48:39 +0800

    feat：流水线版本管理机制 #8161 增加获取含yaml内容接口

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineVersionResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineVersionResource.kt
@@ -62,7 +62,7 @@ interface UserPipelineVersionResource {
 
     @ApiOperation("通过指定模板创建流水线")
     @POST
-    @Path("/projects/{projectId}/create_pipeline")
+    @Path("/projects/{projectId}/create_pipeline_with_template")
     fun createPipelineFromPipeline(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
