c3cc857ea8adbb2936852bec93f82cb72d0b59e7
greysonfang <greysonfang@tencent.com>
2023-08-22 11:18:19 +0800

    feat：蓝盾APP Oauth2授权登录实现 #9353

--- src/backend/ci/core/openapi/api-openapi/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v4/ApigwUserManagementResourceV4.kt
+++ src/backend/ci/core/openapi/api-openapi/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v4/ApigwUserManagementResourceV4.kt
@@ -10,8 +10,8 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
+import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
-import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
@@ -22,7 +22,7 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 interface ApigwUserManagementResourceV4 {
-    @POST
+    @GET
     @Path("/getUserInfo")
     @ApiOperation(
         "获取用户信息",
