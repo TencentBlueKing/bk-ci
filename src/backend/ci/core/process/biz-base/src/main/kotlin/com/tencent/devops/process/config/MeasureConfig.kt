52fe2f29fdd088b17a7ceeb955ee61793e1af18a
irwinsun <irwinsun@tencent.com>
2023-08-22 09:47:14 +0800

    缩减process微服务的子模块数量 #9307 修改为明确指定的EXTEND_RABBIT_TEMPLATE_NAME

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/config/MeasureConfig.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/config/MeasureConfig.kt
@@ -1,15 +1,21 @@
 package com.tencent.devops.process.config
 
+import com.tencent.devops.common.web.mq.EXTEND_RABBIT_TEMPLATE_NAME
 import com.tencent.devops.process.service.measure.MeasureEventDispatcher
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.annotation.Resource
 
 @Configuration
-class SampleMeasureConfig {
+class MeasureConfig {
 
     @Bean
     @ConditionalOnMissingBean(MeasureEventDispatcher::class)
-    fun measureEventDispatcher(rabbitTemplate: RabbitTemplate) = MeasureEventDispatcher(rabbitTemplate)
+    fun measureEventDispatcher(
+        @Autowired extendRabbitTemplate: RabbitTemplate
+    ): MeasureEventDispatcher =
+        MeasureEventDispatcher(extendRabbitTemplate)
 }
