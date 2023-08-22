f4d5c51f0574ea278f5452190d7cf5c1c49a5b69
greysonfang <greysonfang@tencent.com>
2023-08-22 09:55:02 +0800

    feat：蓝盾APP Oauth2授权登录实现 #9353

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/oauth2/Oauth2ServiceEndpointResource.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/oauth2/Oauth2ServiceEndpointResource.kt
@@ -1,7 +1,10 @@
 package com.tencent.devops.auth.api.oauth2
 
 import com.tencent.devops.auth.pojo.Oauth2AccessTokenRequest
+import com.tencent.devops.auth.pojo.dto.Oauth2AuthorizationCodeDTO
 import com.tencent.devops.auth.pojo.vo.Oauth2AccessTokenVo
+import com.tencent.devops.auth.pojo.vo.Oauth2AuthorizationInfoVo
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_OAUTH2_AUTHORIZATION
 import com.tencent.devops.common.api.auth.AUTH_HEADER_OAUTH2_CLIENT_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_OAUTH2_CLIENT_SECRET
@@ -10,10 +13,12 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
+import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OAUTH2_ENDPOINT"], description = "oauth2相关")
@@ -21,6 +26,38 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 interface Oauth2ServiceEndpointResource {
+    @GET
+    @Path("/getAuthorizationInformation")
+    @ApiOperation("获取授权信息")
+    fun getAuthorizationInformation(
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        @ApiParam("待校验用户ID", required = true)
+        userId: String,
+        @QueryParam("clientId")
+        @ApiParam("客户端ID", required = true)
+        clientId: String,
+        @QueryParam("redirectUri")
+        @ApiParam("跳转链接", required = true)
+        redirectUri: String
+    ): Result<Oauth2AuthorizationInfoVo>
+
+    @POST
+    @Path("/getAuthorizationCode")
+    @ApiOperation("获取授权码")
+    fun getAuthorizationCode(
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        @ApiParam("待校验用户ID", required = true)
+        userId: String,
+        @QueryParam("clientId")
+        @ApiParam("客户端ID", required = true)
+        clientId: String,
+        @QueryParam("redirectUri")
+        @ApiParam("跳转链接", required = true)
+        redirectUri: String,
+        @ApiParam("oauth2获取授权码请求报文体", required = true)
+        authorizationCodeDTO: Oauth2AuthorizationCodeDTO
+    ): Result<String>
+
     @POST
     @Path("/getAccessToken")
     @ApiOperation("获取accessToken")
