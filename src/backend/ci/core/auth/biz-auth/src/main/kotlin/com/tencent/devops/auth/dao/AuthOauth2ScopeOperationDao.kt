12e5aa529d54995d326d49df5c88d2b4f30166be
greysonfang <greysonfang@tencent.com>
2023-08-28 20:07:16 +0800

    feat：蓝盾APP Oauth2授权登录实现 #9353

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/dao/AuthOauth2ScopeOperationDao.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/dao/AuthOauth2ScopeOperationDao.kt
@@ -1,7 +1,7 @@
 package com.tencent.devops.auth.dao
 
-import com.tencent.devops.model.auth.tables.TAuthOauth2ScopeOperationInformation
-import com.tencent.devops.model.auth.tables.records.TAuthOauth2ScopeOperationInformationRecord
+import com.tencent.devops.model.auth.tables.TAuthOauth2ScopeOperation
+import com.tencent.devops.model.auth.tables.records.TAuthOauth2ScopeOperationRecord
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 
@@ -10,8 +10,8 @@ class AuthOauth2ScopeOperationDao {
     fun get(
         dslContext: DSLContext,
         operationId: String
-    ): TAuthOauth2ScopeOperationInformationRecord? {
-        return with(TAuthOauth2ScopeOperationInformation.T_AUTH_OAUTH2_SCOPE_OPERATION_INFORMATION) {
+    ): TAuthOauth2ScopeOperationRecord? {
+        return with(TAuthOauth2ScopeOperation.T_AUTH_OAUTH2_SCOPE_OPERATION) {
             dslContext.selectFrom(this).where(OPERATION_ID.eq(operationId))
                 .fetchOne()
         }
