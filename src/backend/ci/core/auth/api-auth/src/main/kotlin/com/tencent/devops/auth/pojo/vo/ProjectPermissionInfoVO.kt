0b667e3a1213ffe2ea7542796ee264e82e32351a
greysonfang <greysonfang@tencent.com>
2023-08-29 18:14:35 +0800

    --story=886987567 增加获取项目信息及成员信息接口

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/vo/ProjectPermissionInfoVO.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/pojo/vo/ProjectPermissionInfoVO.kt
@@ -1,13 +1,16 @@
 package com.tencent.devops.auth.pojo.vo
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("项目权限信息")
 data class ProjectPermissionInfoVO(
     @ApiModelProperty("项目ID")
+    @JsonProperty("project_id")
     val projectCode: String,
     @ApiModelProperty("项目名称")
+    @JsonProperty("project_name")
     val projectName: String,
     @ApiModelProperty("创建人")
     val creator: String,
