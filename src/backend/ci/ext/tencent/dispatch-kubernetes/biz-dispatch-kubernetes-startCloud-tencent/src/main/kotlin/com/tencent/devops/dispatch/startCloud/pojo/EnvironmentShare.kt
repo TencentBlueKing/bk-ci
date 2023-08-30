5ca3b801e0615c8834e5804882f70c597044a828
yongyiduan(段永億) <yongyiduan@tencent.com>
2023-08-30 14:47:45 +0800

    --story=886879823 控制台分配拥有者和共享人时调用START接口注册共享信息

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentShare.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startCloud/pojo/EnvironmentShare.kt
@@ -1,9 +1,11 @@
 package com.tencent.devops.dispatch.startCloud.pojo
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class EnvironmentShare(
     @JsonProperty("cgsId")
     val cgsId: String,
