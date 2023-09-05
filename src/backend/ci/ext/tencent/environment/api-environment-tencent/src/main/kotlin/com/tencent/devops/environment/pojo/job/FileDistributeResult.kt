dc2fe4a25796021811a4d610faad7c561f32266e
jianingzhao <jianingzhao@tencent.com>
2023-09-05 18:14:52 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    优化environment模块代码格式

--- src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/FileDistributeResult.kt
+++ src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/FileDistributeResult.kt
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("文件分发的结果")
-data class FileDistributeResult (
+data class FileDistributeResult(
     @ApiModelProperty(value = "作业实例ID", required = true)
     val jobInstanceId: Long,
     @ApiModelProperty(value = "作业实例名称", required = true)
--- src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
+++ src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("执行脚本的结果")
-data class ScriptExecuteResult (
+data class ScriptExecuteResult(
     @ApiModelProperty(value = "作业实例ID", required = true)
     val jobInstanceId: Long,
     @ApiModelProperty(value = "作业实例名称", required = true)
