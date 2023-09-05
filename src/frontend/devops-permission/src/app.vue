6c4c5ad40cc39e7dab04ebc7510f91eae435de7a
vhwweng <1010382269@qq.com>
2023-09-05 16:15:35 +0800

    feat：完善权限申请单内容 #9394

--- src/frontend/devops-permission/src/app.vue
+++ src/frontend/devops-permission/src/app.vue
@@ -1,10 +1,3 @@
 <template>
   <router-view></router-view>
 </template>
-
-<script lang="ts">
-if (window.top.GLOBAL_PID && window.top.GLOBAL_PID === 'bkdevops') {
-  const devopsApp = window.top.document.getElementsByClassName('devops-app')[0];
-  devopsApp?.setAttribute('class', 'devops-app permission-model');
-}
-</script>
