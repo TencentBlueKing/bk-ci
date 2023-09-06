ef5f94df804b0b17c1ca5069e1ca5684643fad9f
greysonfang <greysonfang@tencent.com>
2023-09-06 15:53:49 +0800

    feat：接入审计中心 #9414

--- src/backend/ci/buildSrc/src/main/kotlin/constants/Versions.kt
+++ src/backend/ci/buildSrc/src/main/kotlin/constants/Versions.kt
@@ -47,5 +47,5 @@ object Versions {
     const val Okhttp = "4.9.0"
     const val jgit = "5.13.1.202206130422-r"
     const val iam = "1.0.30-SNAPSHOT"
-    const val audit = "1.0.5"
+    const val audit = "1.0.6"
 }
