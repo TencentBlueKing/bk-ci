af3832791f97571feaf4cffaf52bf95bf5b04900
jianingzhao <jianingzhao@tencent.com>
2023-09-07 15:10:19 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    添加openapi v4的job apigw接口

---
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v4/job/ApigwJobResourceV4.kt
@@ -0,0 +1,119 @@
+package com.tencent.devops.openapi.api.apigw.v4.job
+
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
+import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.environment.pojo.job.FileDistributeInfoReq
+import com.tencent.devops.environment.pojo.job.FileDistributeResult
+import com.tencent.devops.environment.pojo.job.QueryJobInstanceLogsReq
+import com.tencent.devops.environment.pojo.job.QueryJobInstanceLogsResult
+import com.tencent.devops.environment.pojo.job.QueryJobInstanceStatusResult
+import com.tencent.devops.environment.pojo.job.ScriptExecuteInfoReq
+import com.tencent.devops.stream.pojo.TriggerBuildResult
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.HeaderParam
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+
+@Api(tags = ["OPENAPI_JOB_V4"], description = "OPENAPI-JOB")
+@Path("/{apigwType:apigw-user|apigw-app|apigw}/v4/job")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+interface ApigwJobResourceV4 {
+
+    @ApiOperation("脚本执行的Job接口", tags = ["v4_app_job_script_execute"])
+    @POST
+    @Path("/{projectId}/script_execute")
+    fun triggerStartup(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_USER_ID)
+        userId: String,
+        @ApiParam("项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam(value = "执行脚本的信息", required = true)
+        @QueryParam("scriptExecuteInfo")
+        scriptExecuteInfo: ScriptExecuteInfoReq
+    ): Result<TriggerBuildResult>
+
+    @ApiOperation("文件分发的Job接口", tags = ["v4_app_job_file_distribute"])
+    @POST
+    @Path("/{projectId}/file_distribute")
+    fun distributeFile(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam(value = "项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam(value = "文件分发的信息", required = true)
+        @QueryParam("fileDistributeInfo")
+        fileDistributeInfo: FileDistributeInfoReq
+    ): Result<FileDistributeResult>
+
+    @ApiOperation("查询任务状态的Job接口", tags = ["v4_app_job_query_job_instance_status"])
+    @GET
+    @Path("/{projectId}/query_job_instance_status")
+    fun queryJobInstanceStatus(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam(value = "项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam(value = "作业实例ID", required = true)
+        @QueryParam("jobInstanceId")
+        jobInstanceId: Long
+    ): Result<QueryJobInstanceStatusResult>
+
+    @ApiOperation("批量查询日志的Job接口", tags = ["v4_app_job_query_job_instance_logs"])
+    @POST
+    @Path("/{projectId}/query_job_instance_logs")
+    fun queryJobInstanceLogs(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @ApiParam(value = "项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @ApiParam(value = "批量查询日志的请求信息", required = true)
+        @QueryParam("queryLogsReq")
+        queryLogsReq: QueryJobInstanceLogsReq
+    ): Result<QueryJobInstanceLogsResult>
+}
