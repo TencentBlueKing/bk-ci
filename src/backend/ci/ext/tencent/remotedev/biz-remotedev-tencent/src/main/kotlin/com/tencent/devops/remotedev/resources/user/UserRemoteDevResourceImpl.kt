a845d7d6d0ae0c72d3374132e11d6f9816966e04
randychen <randychen@tencent.com>
2023-09-11 15:23:18 +0800

    --story=887231001 OP接口拆分出workspace相关的操作

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserRemoteDevResourceImpl.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserRemoteDevResourceImpl.kt
@@ -34,6 +34,7 @@ import com.tencent.devops.remotedev.pojo.BKGPT
 import com.tencent.devops.remotedev.pojo.RemoteDevSettings
 import com.tencent.devops.remotedev.pojo.Watermark
 import com.tencent.devops.remotedev.pojo.windows.WindowsResourceConfig
+import com.tencent.devops.remotedev.pojo.windows.ZoneConfig
 import com.tencent.devops.remotedev.service.BKGPTService
 import com.tencent.devops.remotedev.service.RemoteDevSettingService
 import com.tencent.devops.remotedev.service.WatermarkService
@@ -112,5 +113,10 @@ class UserRemoteDevResourceImpl @Autowired constructor(
     override fun getAllWindowsResourceConfig(userId: String): Result<List<WindowsResourceConfig>> {
         logger.info("getAllWindowsResourceConfig|$userId")
         return Result(windowsResourceConfigService.getAllConfig())
+    }
+
+    override fun getWindowsZoneConfig(userId: String): Result<List<ZoneConfig>> {
+        logger.info("getWindowsZoneConfig|$userId")
+        return Result(windowsResourceConfigService.getAllZone())
     }
 }
