6e13fb1a7804864d079ed2f594dc15d267d8f8b1
hejieehe <904696180@qq.com>
2023-09-14 14:46:18 +0800

    【PAC】feat：支持从代码库维度查看对应的代码库事件 #8122

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineTriggerEventResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPipelineTriggerEventResource.kt
@@ -67,7 +67,8 @@ interface UserPipelineTriggerEventResource {
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
-        @QueryParam("代码库类型,为空则返回所有事件类型")
+        @ApiParam("代码库类型,为空则返回所有事件类型", required = false)
+        @QueryParam("scmType")
         scmType: ScmType?
     ): Result<List<IdValue>>
 
