786f59982bc787464f122006ad47c8a826daf4da
greysonfang <greysonfang@tencent.com>
2023-09-15 15:01:57 +0800

    feat：离岸用户登录devx.tencent.com，项目列表和服务列表过滤 #9433

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/Profile.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/Profile.kt
@@ -60,10 +60,6 @@ class Profile(private val environment: Environment) {
         return activeProfiles.contains(PROFILE_DEVELOPMENT)
     }
 
-    fun isDevTest(): Boolean {
-        return activeProfiles.contains(PROFILE_DEVELOPMENT) && !activeProfiles.contains("dev-rbac")
-    }
-
     fun isTest(): Boolean {
         return activeProfiles.contains(PROFILE_TEST)
     }
