b0a64b242ac8b271ae8db8cd22d93926778df380
stubenhuang <stubenhuang@tencent.com>
2023-09-25 20:56:23 +0800

    feat: spring的健康检查加上剩余内存检查 #9507

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/health/MemoryHealthIndicator.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/health/MemoryHealthIndicator.kt
@@ -1,18 +1,14 @@
 package com.tencent.devops.common.service.health
 
 import org.slf4j.LoggerFactory
-import org.springframework.boot.actuate.availability.LivenessStateHealthIndicator
+import org.springframework.boot.actuate.health.AbstractHealthIndicator
 import org.springframework.boot.actuate.health.Health
-import org.springframework.boot.availability.ApplicationAvailability
 
 /**
- * 自定义健康检查
+ * 内存容量检查, 小于100MB时健康检查失败
  */
-class CustomLivenessStateHealthIndicator(applicationAvailability: ApplicationAvailability) :
-    LivenessStateHealthIndicator(applicationAvailability) {
+class MemoryHealthIndicator : AbstractHealthIndicator() {
     override fun doHealthCheck(builder: Health.Builder) {
-        super.doHealthCheck(builder)
-        // 内存容量检查, 小于100MB时健康检查失败
         val freeMemory = Runtime.getRuntime().freeMemory()
         if (freeMemory >= THRESHOLD) {
             builder.up().withDetail("freeMemory", freeMemory)
@@ -24,7 +20,7 @@ class CustomLivenessStateHealthIndicator(applicationAvailability: ApplicationAva
     }
 
     companion object {
-        private val logger = LoggerFactory.getLogger(CustomLivenessStateHealthIndicator::class.java)
+        private val logger = LoggerFactory.getLogger(MemoryHealthIndicator::class.java)
         private const val THRESHOLD = 100 * 1024 * 1024 // 100MB
     }
 
