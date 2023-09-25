6c696f3b8688ccb8b02a1264aecccdf0c0ef6816
stubenhuang <stubenhuang@tencent.com>
2023-09-25 20:15:44 +0800

    feat: spring的健康检查加上剩余内存检查 #9507

--- src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/health/MemoryHealthIndicator.kt
+++ src/backend/ci/core/common/common-service/src/main/kotlin/com/tencent/devops/common/service/health/MemoryHealthIndicator.kt
@@ -1,21 +1,21 @@
 package com.tencent.devops.common.service.health
 
 import org.slf4j.LoggerFactory
+import org.springframework.boot.actuate.health.AbstractHealthIndicator
 import org.springframework.boot.actuate.health.Health
-
-import org.springframework.boot.actuate.health.HealthIndicator
 
 /**
  * 内存容量检查, 小于100MB时健康检查失败
  */
-class MemoryHealthIndicator : HealthIndicator {
-    override fun health(): Health {
+class MemoryHealthIndicator : AbstractHealthIndicator() {
+    override fun doHealthCheck(builder: Health.Builder) {
         val freeMemory = Runtime.getRuntime().freeMemory()
-        return if (freeMemory < THRESHOLD) {
-            logger.error("Not enough free memory , freeMemory: $freeMemory")
-            Health.down().withDetail("error", "Not enough free memory").build()
+        if (freeMemory >= THRESHOLD) {
+            builder.up().withDetail("freeMemory", freeMemory)
         } else {
-            Health.up().build()
+            val errorMsg = "Not enough free memory , freeMemory: $freeMemory"
+            logger.error(errorMsg)
+            throw RuntimeException(errorMsg)
         }
     }
 
@@ -23,4 +23,5 @@ class MemoryHealthIndicator : HealthIndicator {
         private val logger = LoggerFactory.getLogger(MemoryHealthIndicator::class.java)
         private const val THRESHOLD = 100 * 1024 * 1024 // 100MB
     }
+
 }
