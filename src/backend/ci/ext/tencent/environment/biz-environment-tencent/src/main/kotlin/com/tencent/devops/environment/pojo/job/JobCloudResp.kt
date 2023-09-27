bf70ca4c254a389033c99a4294515d6fef451069
jianingzhao <jianingzhao@tencent.com>
2023-09-27 11:17:55 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    fix：添加Service返回值JobCloudResp的JsonProperty

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/JobCloudResp.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/JobCloudResp.kt
@@ -1,12 +1,24 @@
 package com.tencent.devops.environment.pojo.job
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("请求上云版job接口的返回")
 data class JobCloudResp<T>(
+    @ApiModelProperty(value = "状态码", required = true)
+    @JsonProperty("code")
     var code: Int,
+    @ApiModelProperty(value = "执行成功失败", required = true)
+    @JsonProperty("result")
     var result: Boolean,
+    @ApiModelProperty(value = "请求ID", required = true)
+    @JsonProperty("job_request_id")
     var jobRequestId: String,
+    @ApiModelProperty(value = "结果消息", required = true)
+    @JsonProperty("message")
     var message: String?,
+    @ApiModelProperty(value = "返回数据", required = true)
+    @JsonProperty("data")
     var data: T? = null
 )
