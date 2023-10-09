f32b3684580c6c09e3600da902e5a067f681c716
jianingzhao <jianingzhao@tencent.com>
2023-10-09 15:02:16 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    debug:ScriptExecuteResult中字段允许为null

--- src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
+++ src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
@@ -6,13 +6,13 @@ import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("执行脚本的结果")
 data class ScriptExecuteResult(
-    @ApiModelProperty(value = "作业实例ID", required = true)
+    @ApiModelProperty(value = "作业实例ID")
     @JsonProperty("job_instance_id")
     val jobInstanceId: Long,
-    @ApiModelProperty(value = "作业实例名称", required = true)
+    @ApiModelProperty(value = "作业实例名称")
     @JsonProperty("job_instance_name")
     val jobInstanceName: String,
-    @ApiModelProperty(value = "步骤实例ID", required = true)
+    @ApiModelProperty(value = "步骤实例ID")
     @JsonProperty("step_instance_id")
     val stepInstanceId: Long
 )
