d3e9a82fac5b804c460ea18fa9d9ebf4d6d6af11
vhwweng <1010382269@qq.com>
2023-10-10 17:26:26 +0800

    feat：远程触发的 url http 协议，和当前环境支持的http协议对齐 #9540

--- src/frontend/devops-pipeline/src/components/atomFormField/RemoteCurlUrl/index.vue
+++ src/frontend/devops-pipeline/src/components/atomFormField/RemoteCurlUrl/index.vue
@@ -19,7 +19,7 @@
         },
         computed: {
             baseUrl () {
-                return `${location.host}${API_URL_PREFIX}/${PROCESS_API_URL_PREFIX}`
+                return `${location.origin}${API_URL_PREFIX}/${PROCESS_API_URL_PREFIX}`
             },
             stringifyParmas () {
                 const { params } = this.container
