92747409c477a6ea8b3502a498f67f7d053efb4a
jianingzhao <jianingzhao@tencent.com>
2023-10-10 20:56:48 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    fix: [查询日志接口] 接收响应中的job_instance_id、step_instance_id字段

--- src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/QueryJobInstanceLogsResult.kt
+++ src/backend/ci/ext/tencent/environment/api-environment-tencent/src/main/kotlin/com/tencent/devops/environment/pojo/job/QueryJobInstanceLogsResult.kt
@@ -6,6 +6,12 @@ import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("批量查询日志的结果")
 data class QueryJobInstanceLogsResult(
+    @ApiModelProperty(value = "作业实例ID", required = true)
+    @JsonProperty("job_instance_id")
+    val jobInstanceId: Long,
+    @ApiModelProperty(value = "步骤实例ID", required = true)
+    @JsonProperty("step_instance_id")
+    val stepInstanceId: Long,
     @ApiModelProperty(value = "日志类型", notes = "1-脚本执行任务日志，2-文件分发任务日志", required = true)
     @JsonProperty("log_type")
     val logType: Int,
@@ -16,5 +22,5 @@ data class QueryJobInstanceLogsResult(
     @JsonProperty("file_task_logs")
     val fileTaskLogs: List<FileDistributeLog>? = null
 ) {
-    constructor() : this(-1, null, null)
+    constructor() : this(-1, -1, -1, null, null)
 }
