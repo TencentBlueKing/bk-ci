a103baa6d7729fc9c88e39d3923972c1d6a1d497
jianingzhao <jianingzhao@tencent.com>
2023-10-12 20:01:52 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    feat: 添加TaskTerminateResult的JsonProperty

--- src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/TaskTerminateResult.kt
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/TaskTerminateResult.kt
@@ -1,10 +1,12 @@
 package com.tencent.devops.environment.pojo.job
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 @ApiModel("终止任务的结果")
 data class TaskTerminateResult(
     @ApiModelProperty(value = "作业实例ID", required = true)
+    @JsonProperty("job_instance_id")
     val jobInstanceId: Long
 )
