842de5db23b7b5a3248327f9b015e29ebf4f3961
yjieliang <yundantengteng@163.com>
2023-10-17 15:55:13 +0800

    feat:希望支持分支进行上架测试 #4780

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/atom/MarketAtomDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/dao/atom/MarketAtomDao.kt
@@ -620,7 +620,9 @@ class MarketAtomDao : AtomBaseDao() {
     ): Result<TAtomRecord> {
         return with(TAtom.T_ATOM) {
             dslContext.selectFrom(this)
-                .where(ATOM_CODE.eq(atomCode).and(VERSION.startsWith(versionPrefix)))
+                .where(ATOM_CODE.eq(atomCode))
+                .and(VERSION.startsWith(versionPrefix))
+                .and(ATOM_STATUS.eq(AtomStatusEnum.TESTING.status.toByte()))
                 .fetch()
         }
     }
