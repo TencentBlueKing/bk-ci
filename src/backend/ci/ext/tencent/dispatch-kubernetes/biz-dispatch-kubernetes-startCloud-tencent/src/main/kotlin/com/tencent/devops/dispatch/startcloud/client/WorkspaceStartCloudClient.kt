447ceff25ceff27956457c7a2830cc501c58fe88
yongyiduan(段永億) <yongyiduan@tencent.com>
2023-10-17 20:47:35 +0800

    --story=887435849 云研发控制台总览页面需求

--- src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startcloud/client/WorkspaceStartCloudClient.kt
+++ src/backend/ci/ext/tencent/dispatch-kubernetes/biz-dispatch-kubernetes-startCloud-tencent/src/main/kotlin/com/tencent/devops/dispatch/startcloud/client/WorkspaceStartCloudClient.kt
@@ -23,6 +23,7 @@ import com.tencent.devops.dispatch.startcloud.pojo.EnvironmentUnShare
 import com.tencent.devops.dispatch.startcloud.pojo.EnvironmentUserCreate
 import com.tencent.devops.dispatch.startcloud.pojo.Page
 import java.net.SocketTimeoutException
+import java.util.UUID
 import okhttp3.Headers.Companion.toHeaders
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.Request
@@ -69,8 +70,9 @@ class WorkspaceStartCloudClient @Autowired constructor(
 
     fun createWorkspace(userId: String, environment: EnvironmentCreate): EnvironmentCreateRsp.EnvironmentCreateRspData {
         val url = "$bcsCloudUrl/api/v1/remotedevenv/createvm"
+        val id = UUID.randomUUID()
         val body = JsonUtil.toJson(environment, false)
-        logger.info("User $userId request url: $url, body: $body")
+        logger.info("$id|User $userId request url: $url, body: $body")
         val request = Request.Builder()
             .url(url)
             .headers(makeBcsHeaders().toHeaders())
@@ -80,7 +82,7 @@ class WorkspaceStartCloudClient @Autowired constructor(
         try {
             OkhttpUtils.doHttp(request).use { response ->
                 val responseContent = response.body!!.string()
-                logger.info("User $userId create environment response: ${response.code} || $responseContent")
+                logger.info("$id|User $userId create environment response: ${response.code} || $responseContent")
                 if (!response.isSuccessful) {
                     throw BuildFailureException(
                         ErrorCodeEnum.CREATE_ENVIRONMENT_INTERFACE_ERROR.errorType,
@@ -91,7 +93,7 @@ class WorkspaceStartCloudClient @Autowired constructor(
                 }
 
                 val environmentRsp: EnvironmentCreateRsp = jacksonObjectMapper().readValue(responseContent)
-                logger.info("createWorkspace rsp: $environmentRsp")
+                logger.info("$id|createWorkspace rsp: $environmentRsp")
                 when {
                     OK == environmentRsp.code && environmentRsp.data != null && !environmentRsp.data.existed
                     -> return environmentRsp.data
@@ -180,7 +182,8 @@ class WorkspaceStartCloudClient @Autowired constructor(
     fun createUser(userId: String, environment: EnvironmentUserCreate): Boolean {
         val url = "$apiUrl/openapi/user/create"
         val body = JsonUtil.toJson(environment, false)
-        logger.info("User $userId request url: $url, body: $body")
+        val id = UUID.randomUUID()
+        logger.info("$id|User $userId request url: $url, body: $body")
         val request = Request.Builder()
             .url(url)
             .headers(
@@ -194,7 +197,7 @@ class WorkspaceStartCloudClient @Autowired constructor(
         try {
             OkhttpUtils.doHttp(request).use { response ->
                 val responseContent = response.body!!.string()
-                logger.info("User $userId create environment response: ${response.code} || $responseContent")
+                logger.info("$id|User $userId create environment response: ${response.code} || $responseContent")
                 if (!response.isSuccessful) {
                     throw BuildFailureException(
                         ErrorCodeEnum.CREATE_ENVIRONMENT_INTERFACE_ERROR.errorType,
@@ -225,7 +228,7 @@ class WorkspaceStartCloudClient @Autowired constructor(
                 }
             }
         } catch (e: SocketTimeoutException) {
-            logger.error("User $userId create environment get SocketTimeoutException", e)
+            logger.error("$id|User $userId create environment get SocketTimeoutException", e)
             throw BuildFailureException(
                 errorType = ErrorCodeEnum.CREATE_ENVIRONMENT_INTERFACE_FAIL.errorType,
                 errorCode = ErrorCodeEnum.CREATE_ENVIRONMENT_INTERFACE_FAIL.errorCode,
@@ -238,7 +241,8 @@ class WorkspaceStartCloudClient @Autowired constructor(
     fun shareWorkspace(userId: String, environment: EnvironmentShare): String {
         val url = "$apiUrl/openapi/computer/share"
         val body = JsonUtil.toJson(environment, false)
-        logger.info("User $userId request url: $url, body: $body")
+        val id = UUID.randomUUID()
+        logger.info("$id|User $userId request url: $url, body: $body")
         val request = Request.Builder()
             .url(url)
             .headers(
@@ -252,7 +256,7 @@ class WorkspaceStartCloudClient @Autowired constructor(
         try {
             OkhttpUtils.doHttp(request).use { response ->
                 val responseContent = response.body!!.string()
-                logger.info("User $userId share environment response: ${response.code} || $responseContent")
+                logger.info("$id|User $userId share environment response: ${response.code} || $responseContent")
                 if (!response.isSuccessful) {
                     throw BuildFailureException(
                         ErrorCodeEnum.CREATE_ENVIRONMENT_INTERFACE_ERROR.errorType,
