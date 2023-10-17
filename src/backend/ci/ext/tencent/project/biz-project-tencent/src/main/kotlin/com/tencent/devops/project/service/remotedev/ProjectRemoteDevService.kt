ea02f9e22d40ba68c38e1f6570f0cf703e23b947
ruotiantang <ruotiantang@tencent.com>
2023-10-17 12:11:24 +0800

    --story=887873353 项目管理中开启云研发后，调用bkm的接口创建云桌面仪表盘

---
+++ src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/remotedev/ProjectRemoteDevService.kt
@@ -0,0 +1,170 @@
+package com.tencent.devops.project.service.remotedev
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.tencent.devops.auth.api.service.ServiceMonitorSpaceResource
+import com.tencent.devops.common.api.util.OkhttpUtils
+import com.tencent.devops.common.client.Client
+import okhttp3.Headers.Companion.toHeaders
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+/**
+ * 将 project 中 remotedev 相关业务逻辑放到这里，避免污染
+ */
+@Service
+class ProjectRemoteDevService @Autowired constructor(
+    private val objectMapper: ObjectMapper,
+    private val client: Client
+) {
+
+    @Value("\${remoteDev.appCode:}")
+    val appCode = ""
+
+    @Value("\${remoteDev.appToken:}")
+    val appSecret = ""
+
+    @Value("\${remoteDev.bkMonitorUrl:}")
+    val bkMonitorUrl = ""
+
+    @Value("\${bkrepo.bkrepoDevxUrl:}")
+    val bkrepoDevxUrl = ""
+
+    @Value("\${bkrepo.bkrepoDevxHeaderUserAuth:}")
+    val bkrepoDevxHeaderUserAuth = ""
+
+    // 开启 remotedev 相关逻辑
+    fun enableRemoteDev(
+        userId: String,
+        projectCode: String
+    ) {
+        // 从 auth 获取 bizid
+        val bizId = try {
+            client.get(ServiceMonitorSpaceResource::class)
+                .getMonitorSpaceBizId(userId, projectCode).data?.toLong()
+        } catch (e: Exception) {
+            logger.error("enableRemoteDev getMonitorSpaceBizId error", e)
+            null
+        }
+
+        // 创建监控表盘
+        if (bizId != null) {
+            quickImportDashboard(bizId)
+        }
+
+        // 创建 lsync generic类型的仓库，做单向同步盘
+        createLsyncGeneric(projectCode)
+    }
+
+    private fun quickImportDashboard(
+        bizId: Long
+    ) {
+        val url = "$bkMonitorUrl/stage/quick_import_dashboard/"
+        val headerStr = objectMapper.writeValueAsString(
+            mapOf("bk_app_code" to appCode, "bk_app_secret" to appSecret)
+        ).replace("\\s".toRegex(), "")
+        val requestBody = objectMapper.writeValueAsString(QuickImportDashboardReq(bkBizId = bizId))
+            .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .addHeader("X-Bkapi-Authorization", headerStr)
+            .build()
+
+        try {
+            OkhttpUtils.doHttp(request).use {
+                val responseStr = it.body!!.string()
+                if (!it.isSuccessful) {
+                    logger.warn("quickImportDashboard request failed, uri:($url)|response: ($requestBody)")
+                    return
+                }
+                val resp = objectMapper.readValue<BkMonitorResp>(responseStr)
+                if (resp.code != 200L || !resp.result) {
+                    logger.warn("quickImportDashboard request failed, url:($url)|response:($responseStr)")
+                    return
+                }
+                return
+            }
+        } catch (e: Exception) {
+            logger.warn("quickImportDashboard request failed", e)
+        }
+    }
+
+    private fun createLsyncGeneric(
+        projectId: String
+    ) {
+        val requestData = CreateRepoData(
+            projectId = projectId,
+            name = "lsync",
+            type = "GENERIC",
+            category = "LOCAL",
+            public = false,
+            description = "repo",
+            configuration = null,
+            storageCredentialsKey = null
+        )
+        val url = "$bkrepoDevxUrl/repository/api/repo/create"
+        val requestBody = objectMapper.writeValueAsString(requestData)
+            .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url(url)
+            .headers(getBkrepoCommonHeaders("admin").toHeaders())
+            .post(requestBody)
+            .build()
+        try {
+            OkhttpUtils.doHttp(request).use {
+                val responseStr = it.body!!.string()
+                if (!it.isSuccessful) {
+                    logger.warn("quickImportDashboard request failed, uri:($url)|response: ($responseStr)")
+                    return
+                }
+            }
+        } catch (e: Exception) {
+            logger.error("request api[${request.url.toUrl()}] error: ${e.localizedMessage}")
+        }
+    }
+
+    private fun getBkrepoCommonHeaders(userId: String): MutableMap<String, String> {
+        val headers = mutableMapOf<String, String>()
+        headers["Authorization"] = bkrepoDevxHeaderUserAuth
+        headers["X-BKREPO-UID"] = userId
+        return headers
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ProjectRemoteDevService::class.java)
+    }
+}
+
+data class QuickImportDashboardReq(
+    @JsonProperty("bk_biz_id")
+    val bkBizId: Long,
+    @JsonProperty("dash_name")
+    val dashName: String = "bkci/BKCI-云桌面"
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class BkMonitorResp(
+    val result: Boolean,
+    val code: Long,
+    val message: String
+)
+
+data class CreateRepoData(
+    val projectId: String,
+    val name: String,
+    val type: String,
+    val category: String,
+    val public: Boolean,
+    val description: String?,
+    val configuration: Any?,
+    val storageCredentialsKey: Any?
+)
