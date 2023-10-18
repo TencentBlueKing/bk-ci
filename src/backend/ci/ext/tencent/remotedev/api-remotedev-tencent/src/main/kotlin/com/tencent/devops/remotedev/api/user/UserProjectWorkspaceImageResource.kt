018c53f1866e10958e550f98bc068a6abd11d4bd
sawyersong <sawyersong@tencent.com>
2023-10-18 14:46:05 +0800

    --story=887436237 START云桌面创建流程对接BCS开关机接口

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserProjectWorkspaceImageResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserProjectWorkspaceImageResource.kt
@@ -41,7 +41,6 @@ import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["USER_IMAGE_MANAGE"], description = "用户-镜像管理")
