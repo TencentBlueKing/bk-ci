3f8f32dec49d65759c1300f56de991d275b9d7e0
vhwweng <1010382269@qq.com>
2023-10-23 11:46:12 +0800

    feat：代码库优化一期功能点 #9347

--- src/frontend/devops-codelib/src/config/index.js
+++ src/frontend/devops-codelib/src/config/index.js
@@ -84,8 +84,8 @@ export const codelibConfig = {
         typeName: 'github'
     },
     git_http: {
-        credentialTypes: 'USERNAME_PASSWORD',
-        addType: 'USERNAME_PASSWORD',
+        credentialTypes: 'TOKEN_USERNAME_PASSWORD',
+        addType: 'TOKEN_USERNAME_PASSWORD',
         label: 'Git',
         typeName: 'codeGit'
     },
