00d6d814243a735ff003df0791c9fab4b7dff6f5
royalhuang <royalhuang@tencent.com>
2023-10-26 20:49:36 +0800

    bug: stage审核取消兼容数据处理 #9611

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/option/StageControlOption.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/option/StageControlOption.kt
@@ -43,22 +43,28 @@ data class StageControlOption(
     val enable: Boolean = true, // 是否启用该阶段
     @ApiModelProperty("运行条件", required = false)
     val runCondition: StageRunCondition = StageRunCondition.AFTER_LAST_FINISHED, // 运行条件
-    @ApiModelProperty("是否人工触发", required = false)
-    @Deprecated("被StagePauseCheck.manualTrigger代替")
-    val manualTrigger: Boolean? = false,
-    @ApiModelProperty("可触发用户，支持引用变量", required = false)
-    @Deprecated("被StagePauseCheck.reviewGroups")
-    var triggerUsers: List<String>? = null, // 可触发用户，支持引用变量
-    @ApiModelProperty("已通过审核", required = false)
-    var triggered: Boolean? = null, // 已通过审核
-    @ApiModelProperty("等待审核的超时时间", required = false)
-    val timeout: Int? = null, // 等待审核的超时时间
     @ApiModelProperty("自定义变量", required = false)
     val customVariables: List<NameAndValue>? = emptyList(), // 自定义变量
     @ApiModelProperty("自定义条件", required = false)
     val customCondition: String? = null, // 自定义条件
+
+    // 废弃旧数据字段
+    @ApiModelProperty("是否人工触发", required = false)
+    @Deprecated("被StagePauseCheck.manualTrigger代替")
+    val manualTrigger: Boolean? = false,
+    @ApiModelProperty("可触发用户，支持引用变量", required = false)
+    @Deprecated("被StagePauseCheck.reviewGroups代替")
+    var triggerUsers: List<String>? = null, // 可触发用户，支持引用变量
+    @ApiModelProperty("已通过审核", required = false)
+    @Deprecated("被StagePauseCheck.status代替")
+    var triggered: Boolean? = null, // 已通过审核
+    @ApiModelProperty("等待审核的超时时间", required = false)
+    @Deprecated("被StagePauseCheck.timeout代替")
+    val timeout: Int? = null, // 等待审核的超时时间
     @ApiModelProperty("审核变量", required = false)
+    @Deprecated("被StagePauseCheck.reviewParams代替")
     var reviewParams: List<ManualReviewParam>? = null, // 审核变量
     @ApiModelProperty("审核说明", required = false)
+    @Deprecated("被StagePauseCheck.reviewDesc代替")
     var reviewDesc: String? = null // 审核说明
 )
