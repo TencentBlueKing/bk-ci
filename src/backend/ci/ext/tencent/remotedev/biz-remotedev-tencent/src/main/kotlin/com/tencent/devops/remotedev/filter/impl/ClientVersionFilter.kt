1ba16163502dcd83955c4fede4dd23847d7e2045
yongyiduan(段永億) <yongyiduan@tencent.com>
2023-10-26 18:31:37 +0800

    --story=114803063 后台接口增加客户端版本号校验

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/filter/impl/ClientVersionFilter.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/filter/impl/ClientVersionFilter.kt
@@ -29,6 +29,7 @@ class ClientVersionFilter constructor(
     enum class ApiType(val startContextPath: String, val verify: Boolean) {
 
         USER_SETTINGS("api/user/remotedev/settings", true),
+        DESKTOP_SETTINGS("api/desktop/remotedev/settings", true),
         USER("api/user/", false),
         DESKTOP("/api/desktop/", false),
         EXTERNAL("/api/external/", false),
