63478eea2f563a3513db26ab0aa4b70666c35ec1
jianingzhao <jianingzhao@tencent.com>
2023-10-26 10:34:22 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    fix: 编译失败

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/req/JobCloudQueryAccountAliasReq.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/req/JobCloudQueryAccountAliasReq.kt
@@ -15,13 +15,7 @@ data class JobCloudQueryAccountAliasReq(
     @JsonProperty("account")
     val account: String,
     // TODO: 用于请求上云job的查询字段
-    @ApiModelProperty(value = "应用ID", required = true)
-    @JsonProperty("bk_app_code")
-    override var bkAppCode: String? = "",
-    @ApiModelProperty(value = "安全秘钥", required = true)
-    @JsonProperty("bk_app_secret")
-    override var bkAppSecret: String? = "",
     @ApiModelProperty(value = "当前用户用户名", required = true)
     @JsonProperty("bk_username")
     override var bkUsername: String
-) : JobCloudPermission(bkScopeType, bkScopeId, bkAppCode, bkAppSecret, bkUsername)
+) : JobCloudPermission(bkScopeType, bkScopeId, bkUsername)
