52299e32b2f59597cccd95cbffc54195581f4f1b
jianingzhao <jianingzhao@tencent.com>
2023-10-31 16:28:35 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    feat: 去掉get:JsonProperty的使用

--- src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/JobResult.kt
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/JobResult.kt
@@ -12,11 +12,9 @@ data class JobResult<out T>(
     @ApiModelProperty("结果bool值")
     val result: Boolean? = null,
     @ApiModelProperty("请求ID", required = true)
-    @get:JsonProperty("job_request_id")
+    @JsonProperty("job_request_id")
     val jobRequestId: String?,
     @ApiModelProperty("数据")
     @BkFieldI18n
     val data: T? = null
-) {
-    constructor(data: T) : this(0, true, null, data)
-}
+)
--- src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/job/ScriptExecuteResult.kt
@@ -7,12 +7,12 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel("执行脚本的结果")
 data class ScriptExecuteResult(
     @ApiModelProperty(value = "作业实例ID", required = true)
-    @get:JsonProperty("job_instance_id")
+    @JsonProperty("job_instance_id")
     val jobInstanceId: Long,
     @ApiModelProperty(value = "作业实例名称", required = true)
-    @get:JsonProperty("job_instance_name")
+    @JsonProperty("job_instance_name")
     val jobInstanceName: String,
     @ApiModelProperty(value = "步骤实例ID", required = true)
-    @get:JsonProperty("step_instance_id")
+    @JsonProperty("step_instance_id")
     val stepInstanceId: Long
 )
