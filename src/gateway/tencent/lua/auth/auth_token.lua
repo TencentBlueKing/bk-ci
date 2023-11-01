c98e6cc56c084a46340f5e31d24f8c1a4dd949d5
stubenhuang <stubenhuang@tencent.com>
2023-11-01 15:22:55 +0800

    --story=114936374 【蓝盾网关】obs网关转发

--- src/gateway/tencent/lua/auth/auth_token.lua
+++ src/gateway/tencent/lua/auth/auth_token.lua
@@ -1,5 +1,5 @@
 local auth_header = ngx.req.get_headers()["Authorization"]
-if auth_header == "Bearer "..config.auth then
+if auth_header == "Bearer "..config.auth_token then
     ngx.exit(ngx.HTTP_OK)
 else
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
