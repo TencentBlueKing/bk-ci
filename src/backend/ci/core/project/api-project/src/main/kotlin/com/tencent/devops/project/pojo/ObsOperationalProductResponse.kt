99b1550c40a94c8cf53b3cf18e338713464635ac
greysonfang <greysonfang@tencent.com>
2023-11-02 11:34:22 +0800

    项目支持关联运营产品和根据运营产品搜索 #9636

---
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/pojo/ObsOperationalProductResponse.kt
@@ -0,0 +1,14 @@
+package com.tencent.devops.project.pojo
+
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+@ApiModel("OBS运营产品")
+data class ObsOperationalProductResponse(
+    @ApiModelProperty("jsonrpc")
+    val jsonrpc: String,
+    @ApiModelProperty("id")
+    val id: String,
+    @ApiModelProperty("运营产品")
+    val operationalProducts: List<OperationalProductVO>
+)
