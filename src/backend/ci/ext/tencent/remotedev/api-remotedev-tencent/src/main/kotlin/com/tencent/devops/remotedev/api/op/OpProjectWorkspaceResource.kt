c1600981fbc2831df259df240e156d000c950d1d
ruotiantang <ruotiantang@tencent.com>
2023-11-08 09:03:38 +0800

    --story=114979542 OP需求：实例管理页面支持导出Excel

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpProjectWorkspaceResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/op/OpProjectWorkspaceResource.kt
@@ -40,7 +40,6 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
-import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.POST
 import javax.ws.rs.Path
