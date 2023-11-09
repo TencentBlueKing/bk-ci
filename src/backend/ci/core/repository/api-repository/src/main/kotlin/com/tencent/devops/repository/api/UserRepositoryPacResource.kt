e0a43412322a9c347c17a4e4b839ccc2a6cd8572
mingshewhe <mingshewhe@tencent.com>
2023-11-09 20:31:06 +0800

    【PAC】feat：开启PAC模式的代码库支持自动同步代码库YAML变更到蓝盾 #8130

--- src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/UserRepositoryPacResource.kt
+++ src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/UserRepositoryPacResource.kt
@@ -114,7 +114,7 @@ interface UserRepositoryPacResource {
 
     @ApiOperation("重试")
     @PUT
-    @Path("/{projectId}/{repositoryHashId}/refresh")
+    @Path("/{projectId}/{repositoryHashId}/retry")
     fun retry(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
@@ -159,7 +159,7 @@ interface UserRepositoryPacResource {
 
     @ApiOperation("获取同步详情")
     @GET
-    @Path("/{projectId}/{repositoryHashId}/getPacSyncDetail")
+    @Path("/{projectId}/{repositoryHashId}/listYamlSync")
     fun listYamlSync(
         @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
