43abdd97d96fb5c061976729efaa621bbc9afb2e
greysonfang <greysonfang@tencent.com>
2023-11-16 15:34:26 +0800

    feat：蓝盾APP Oauth2授权登录实现 #9353

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/service/ServiceSecurityResource.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/service/ServiceSecurityResource.kt
@@ -6,13 +6,18 @@ import com.tencent.devops.common.api.pojo.Result
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
+import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
 
 @Api(tags = ["SERVICE_SECURITY"], description = "安全相关")
 @Path("/service/security")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 interface ServiceSecurityResource {
     @GET
     @Path("/getUserSecurityInfo")
