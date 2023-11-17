35a68d8d14e15360541b9e20ce90b609e3367fd2
greysonfang <greysonfang@tencent.com>
2023-11-17 12:05:08 +0800

    feat：接入审计中心 #9414

--- src/backend/ci/core/common/common-audit/src/main/kotlin/com/tencent/devops/common/audit/BkAuditConfiguration.kt
+++ src/backend/ci/core/common/common-audit/src/main/kotlin/com/tencent/devops/common/audit/BkAuditConfiguration.kt
@@ -1,11 +1,13 @@
 package com.tencent.devops.common.audit
 
 import com.tencent.bk.audit.AuditRequestProvider
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 
 @Configuration
+@ConditionalOnProperty(name = ["audit.enabled"], havingValue = "true", matchIfMissing = true)
 class BkAuditConfiguration {
     @Bean
     @Primary
