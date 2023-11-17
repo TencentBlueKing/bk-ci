d02a470fb5c7cc93fa9ea9aa699d4f07b15da3eb
randychen <randychen@tencent.com>
2023-11-17 18:04:38 +0800

    --story=114971616 对接安全侧需求：云桌面设备访问管控

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/ProjectAccessDevicePermissionsResp.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/ProjectAccessDevicePermissionsResp.kt
@@ -1,6 +1,12 @@
 package com.tencent.devops.remotedev.pojo
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ProjectAccessDevicePermissionsResp(
+    @JsonProperty("access_control")
     val accessControl: Boolean,
+    @JsonProperty("has_permissions")
     val hasPermissions: Boolean
 )
