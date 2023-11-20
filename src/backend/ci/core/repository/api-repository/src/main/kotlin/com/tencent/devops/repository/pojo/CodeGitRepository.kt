c596eb639e97e9e9185ff3651cc84e294dae1e1c
hejieehe <904696180@qq.com>
2023-11-20 18:54:45 +0800

    【PAC】feat：新增代码库详情页 #8118

--- src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/pojo/CodeGitRepository.kt
+++ src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/pojo/CodeGitRepository.kt
@@ -27,7 +27,6 @@
 
 package com.tencent.devops.repository.pojo
 
-import com.tencent.devops.common.api.enums.ScmType
 import com.tencent.devops.repository.pojo.enums.RepoAuthType
 import com.tencent.devops.scm.utils.code.git.GitUtils
 import io.swagger.annotations.ApiModel
