8be8355f52ebd05e40799752c160e771a5d21106
yongyiduan(段永億) <yongyiduan@tencent.com>
2023-11-22 12:12:32 +0800

    [openapi]新增权限中心获取组用户接口 #9703

--- src/backend/ci/core/openapi/api-openapi/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v4/ApigwAuthProjectResourceV4.kt
+++ src/backend/ci/core/openapi/api-openapi/src/main/kotlin/com/tencent/devops/openapi/api/apigw/v4/ApigwAuthProjectResourceV4.kt
@@ -4,6 +4,8 @@ import com.tencent.devops.auth.pojo.vo.ProjectPermissionInfoVO
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
@@ -13,6 +15,7 @@ import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Api(tags = ["OPENAPI_AUTH_V4"], description = "OPENAPI-权限相关")
@@ -38,4 +41,31 @@ interface ApigwAuthProjectResourceV4 {
         @ApiParam("项目ID", required = true)
         projectId: String
     ): Result<ProjectPermissionInfoVO>
+
+    @GET
+    @Path("/getResourceGroupUsers")
+    @ApiOperation(
+        "获取项目权限分组成员",
+        tags = ["v4_app_get_project_permission_members"]
+    )
+    fun getResourceGroupUsers(
+        @ApiParam(value = "appCode", required = true, defaultValue = AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VALUE)
+        @HeaderParam(AUTH_HEADER_DEVOPS_APP_CODE)
+        appCode: String?,
+        @ApiParam(value = "apigw Type", required = true)
+        @PathParam("apigwType")
+        apigwType: String?,
+        @PathParam("projectId")
+        @ApiParam("项目Code", required = true)
+        projectId: String,
+        @QueryParam("resourceType")
+        @ApiParam("资源类型", required = false)
+        resourceType: AuthResourceType,
+        @QueryParam("resourceCode")
+        @ApiParam("资源code", required = false)
+        resourceCode: String,
+        @QueryParam("group")
+        @ApiParam("资源用户组类型", required = false)
+        group: BkAuthGroup? = null
+    ): Result<List<String>>
 }
