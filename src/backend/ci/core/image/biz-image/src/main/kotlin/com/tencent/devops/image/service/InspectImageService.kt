00cad01c23b6bcd878367aa92b3adcecea7f9e3c
yjieliang <yundantengteng@163.com>
2023-12-04 11:01:02 +0800

    pref:研发商店组件内置打包流水线废弃dockerhost构建机方案 #9689

--- src/backend/ci/core/image/biz-image/src/main/kotlin/com/tencent/devops/image/service/InspectImageService.kt
+++ src/backend/ci/core/image/biz-image/src/main/kotlin/com/tencent/devops/image/service/InspectImageService.kt
@@ -82,7 +82,7 @@ class InspectImageService @Autowired constructor(
         val imageInspectList = mutableListOf<CheckDockerImageResponse>()
         checkDockerImageRequestList.parallelStream().forEach {
             // 判断用户录入的镜像信息是否能正常拉取到镜像
-            val imageName = it.imageName.replace("${it.registryHost}/", "")
+            val imageName = it.imageName
             logger.info("checkDockerImage imageName:$imageName")
             try {
                 val authConfig = CommonUtils.getAuthConfig(
