bc9935529ce58b14ad420ace8d86350305597451
lockiechen <33082528+lockiechen@users.noreply.github.com>
2023-12-05 20:29:33 +0800

    doc: 文档链接优化 issue #9766

--- src/frontend/common-lib/docs.ts
+++ src/frontend/common-lib/docs.ts
@@ -16,6 +16,6 @@ export const BkciDocs = {
 }
 export const pipelineDocs = {
     ALIAS_BUILD_NO_DOC: `${commonPrefix}/Services/Pipeline/pipeline-edit-guide/alias-buildno.md`, // 构建号别名文档
-    PIPELINE_ERROR_GUIDE_DOC: `${commonPrefix}/FAQS/FAQ.md`, // 流水线编辑指引文档
-    NAMESPACE_DOC: `${DOCS_URL_PREFIX}/docs/document/6.1/183/22916` // 命名空间文档
+    PIPELINE_ERROR_GUIDE_DOC: `${commonPrefix}/Reference/faqs/pipelines/execute.md`, // 流水线编辑指引文档
+    NAMESPACE_DOC: `${commonPrefix}/Services/Pipeline/pipeline-edit-guide/pipeline-variables/variables-custom.md` // 命名空间文档
 }
