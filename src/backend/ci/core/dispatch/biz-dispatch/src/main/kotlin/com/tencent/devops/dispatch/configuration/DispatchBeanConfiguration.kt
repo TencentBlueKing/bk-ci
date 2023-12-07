2f93118db0ff1d0ab11ebf40c882b0eea07d5834
sawyersong <sawyersong@tencent.com>
2023-12-07 19:44:48 +0800

    feat：流水线运营数据统计 #9735

--- src/backend/ci/core/dispatch/biz-dispatch/src/main/kotlin/com/tencent/devops/dispatch/configuration/DispatchBeanConfiguration.kt
+++ src/backend/ci/core/dispatch/biz-dispatch/src/main/kotlin/com/tencent/devops/dispatch/configuration/DispatchBeanConfiguration.kt
@@ -27,8 +27,12 @@
 
 package com.tencent.devops.dispatch.configuration
 
+import com.tencent.devops.common.event.dispatcher.pipeline.mq.MeasureEventDispatcher
+import com.tencent.devops.common.web.mq.EXTEND_RABBIT_TEMPLATE_NAME
 import com.tencent.devops.dispatch.service.jobquota.JobQuotaInterface
 import com.tencent.devops.dispatch.service.jobquota.JobQuotaInterfaceImpl
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.AutoConfigureOrder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
@@ -45,4 +49,10 @@ class DispatchBeanConfiguration {
     @Bean
     @ConditionalOnMissingBean(JobQuotaInterface::class)
     fun jobQuotaInterface() = JobQuotaInterfaceImpl()
+
+    @Bean
+    fun measureEventDispatcher(
+        @Qualifier(EXTEND_RABBIT_TEMPLATE_NAME) extendRabbitTemplate: RabbitTemplate
+    ): MeasureEventDispatcher =
+        MeasureEventDispatcher(extendRabbitTemplate)
 }
