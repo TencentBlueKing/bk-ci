2b11ea1a7d691be54ad9997b7a53410238294518
vhwweng <1010382269@qq.com>
2023-12-07 11:07:40 +0800

    feat: 代码库优化一期功能点 #9347

--- src/frontend/devops-codelib/src/components/CodeLibDetail/index.vue
+++ src/frontend/devops-codelib/src/components/CodeLibDetail/index.vue
@@ -262,6 +262,7 @@
             curRepoId: {
                 handler (val) {
                     this.errorCode = 0
+                    this.pacProjectName = ''
                     this.fetchRepoDetail(val)
                 },
                 immediate: true
@@ -518,14 +519,11 @@
              */
             handleCheckPacProject () {
                 if (this.repoInfo.scmType === 'CODE_GIT') {
-                    this.isLoading = true
                     this.checkPacProject({
                         repoUrl: this.repoInfo.url,
                         repositoryType: this.repoInfo.scmType
                     }).then((res) => {
                         this.pacProjectName = res
-                    }).finally(() => {
-                        this.isLoading = false
                     })
                 }
             }
