15ef8386aaed4c816910c8661d85576e054d0c24
stubenhuang <stubenhuang@tencent.com>
2023-12-15 17:13:35 +0800

    feat: 将边缘微服务合并成misc服务 #9770

--- src/backend/ci/core/misc/boot-misc/src/main/kotlin/com/tencent/devops/misc/MiscApplication.kt
+++ src/backend/ci/core/misc/boot-misc/src/main/kotlin/com/tencent/devops/misc/MiscApplication.kt
@@ -29,8 +29,13 @@ package com.tencent.devops.misc
 
 import com.tencent.devops.common.service.MicroService
 import com.tencent.devops.common.service.MicroServiceApplication
+import org.springframework.context.annotation.ComponentScan
 
 @MicroService
+@ComponentScan(
+    "com.tencent.devops.misc",
+    "com.tencent.devops.image"
+)
 class MiscApplication
 
 fun main(args: Array<String>) {
