aaf0b82ba1455dbc62827a9fe4569e5f23d7530f
randychen <randychen@tencent.com>
2023-12-15 16:02:03 +0800

    --story=115540597 删除镜像时，改成软删除，不做自己删掉记录。

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/dao/ImageManageDao.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/dao/ImageManageDao.kt
@@ -21,6 +21,7 @@ class ImageManageDao {
         return with(TProjectImages.T_PROJECT_IMAGES) {
             dslContext.selectFrom(this)
                 .where(PROJECT_ID.eq(projectId))
+                .and(STATUS.notEqual(ImageStatus.DELETED.ordinal))
                 .orderBy(CREATE_TIME.desc())
                 .fetch()
         }
