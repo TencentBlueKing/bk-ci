55a4365a3c04b195379c1ccbf2ae9991e5cde640
jianingzhao <jianingzhao@tencent.com>
2024-01-02 18:14:22 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    feat: 添加定时轮询agent状态/版本的定时任务

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/job/NodeScheduledService.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/job/NodeScheduledService.kt
@@ -44,7 +44,7 @@ class NodeScheduledService @Autowired constructor(
      * 每小时执行一次。
      * 条件：NODE_TYPE为cmdb的，查询该节点的agent安装状态以及版本，并对比差异更新
      */
-    @Scheduled(cron = "0 57 17 * * 1-5")
+    @Scheduled(cron = "0 22 18 * * 1-5")
     fun scheduledUpdateAgent() {
         taskWithRedisLock(SCHEDULED_UPDATE_AGENT_TIMEOUT_LOCK_KEY, ::updateAgent)
     }
@@ -121,6 +121,12 @@ class NodeScheduledService @Autowired constructor(
                     NodeStatus.NORMAL.name
                 else null
             it.agentStatus = AGENT_NORMAL_NODE_STATUS == ipToAgentUpdateList[it.nodeIp]?.status
+            if (logger.isDebugEnabled)
+                logger.debug(
+                    "ip:${it.nodeIp}, agentStatus:${ipToAgentUpdateList[it.nodeIp]?.status}," +
+                        "${AGENT_NORMAL_NODE_STATUS == ipToAgentUpdateList[it.nodeIp]?.status}"
+                )
+            it.agentVersion = ipToAgentUpdateList[it.nodeIp]?.version
             it
         }
         if (logger.isDebugEnabled) logger.debug("[updateAgent]agentUpdateRecords:$agentUpdateRecords.")
