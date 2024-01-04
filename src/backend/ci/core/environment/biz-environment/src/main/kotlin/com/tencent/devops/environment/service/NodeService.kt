4878e96452093a584b84c2d86a1b98004209bb78
jianingzhao <jianingzhao@tencent.com>
2024-01-04 21:05:47 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    feat: list节点接口补充逻辑

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/NodeService.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/NodeService.kt
@@ -254,13 +254,9 @@ class NodeService @Autowired constructor(
                     it.nodeType == NodeType.DEVCLOUD.name
                 ) {
                     it.nodeStatus
-                } else if ((it.nodeType == NodeType.CMDB.name ||
-                        it.nodeType == NodeType.UNKNOWN.name ||
-                        it.nodeType == NodeType.OTHER.name) &&
-                    (it.nodeStatus == NodeStatus.NOT_IN_CC.name ||
-                        it.nodeStatus == NodeStatus.NOT_IN_CMDB.name ||
-                        it.nodeStatus == NodeStatus.NOT_INSTALLED.name ||
-                        it.nodeStatus == NodeStatus.AGENT_ABNORMAL.name)
+                } else if (it.nodeType == NodeType.CMDB.name ||
+                    it.nodeType == NodeType.UNKNOWN.name ||
+                    it.nodeType == NodeType.OTHER.name
                 ) {
                     it.nodeStatus
                 } else {
