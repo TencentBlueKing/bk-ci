9188ba88dcb00499f9128703576a1d8f78721c6e
yongyiduan <yongyiduan@tencent.com>
2024-01-09 17:09:50 +0800

    feat：流水线版本管理机制 #8161 优化代码格式

--- src/backend/ci/core/common/common-pipeline-yaml/src/test/kotlin/com/tencent/devops/process/yaml/parsers/template/YamlTemplateTest.kt
+++ src/backend/ci/core/common/common-pipeline-yaml/src/test/kotlin/com/tencent/devops/process/yaml/parsers/template/YamlTemplateTest.kt
@@ -28,7 +28,6 @@
 package com.tencent.devops.process.yaml.parsers.template
 
 import com.tencent.devops.common.api.util.JsonUtil
-import com.tencent.devops.common.api.util.YamlUtil
 import com.tencent.devops.process.yaml.transfer.TransferMapper
 import com.tencent.devops.process.yaml.v2.exception.YamlFormatException
 import com.tencent.devops.process.yaml.v2.models.PreScriptBuildYaml
