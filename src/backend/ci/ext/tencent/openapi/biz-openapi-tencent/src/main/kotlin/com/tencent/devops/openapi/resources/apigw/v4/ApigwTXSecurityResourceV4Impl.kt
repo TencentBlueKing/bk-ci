73e30aaa420139926a1f399c48e4a4f700c0aaf3
greysonfang <greysonfang@tencent.com>
2024-01-17 10:28:02 +0800

    --story=115464824 feat：根据moa票据校验用户权限并返回安全信息

--- src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v4/ApigwTXSecurityResourceV4Impl.kt
+++ src/backend/ci/ext/tencent/openapi/biz-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v4/ApigwTXSecurityResourceV4Impl.kt
@@ -3,8 +3,10 @@ package com.tencent.devops.openapi.resources.apigw.v4
 import com.tencent.devops.auth.api.ServiceSecurityResource
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.openapi.api.apigw.v4.ApigwTXSecurityResourceV4
 
+@RestResource
 class ApigwTXSecurityResourceV4Impl constructor(
     val client: Client
 ) : ApigwTXSecurityResourceV4 {
