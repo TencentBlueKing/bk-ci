3c08829683712dd6476fd0f85b8a5f0d19549413
jianingzhao <jianingzhao@tencent.com>
2024-01-18 16:59:04 +0800

    --story=887060303 环境管理服务对接上云版作业平台
    feat: T_PROJECT_JOB表改为使用PROJECT_ID+JOB_INSTANCE_ID作为联合主键

--- support-files/sql/2003_v2.x/2020_ci_environment-update_v2.0_mysql.sql
+++ support-files/sql/2003_v2.x/2020_ci_environment-update_v2.0_mysql.sql
@@ -52,7 +52,7 @@ BEGIN
         `PROJECT_ID`      varchar(64)     DEFAULT NULL COMMENT '蓝盾项目id',
         `CREATED_USER`    varchar(64)     DEFAULT NULL COMMENT 'job实例创建人',
         `CREATED_TIME`    timestamp  NULL DEFAULT NULL COMMENT 'job实例创建时间',
-        PRIMARY KEY (`JOB_INSTANCE_ID`)
+        PRIMARY KEY (`PROJECT_ID`, `JOB_INSTANCE_ID`)
     ) ENGINE = InnoDB
       DEFAULT CHARSET = utf8mb4 COMMENT ='';
 
