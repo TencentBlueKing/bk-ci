bf5b7f415c85cf7865fab7ae32c0a5f2d17888c3
royalhuang <royalhuang@tencent.com>
2024-01-23 20:10:04 +0800

    feat：流水线版本管理机制 #8161 增加正式版本号

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/service/PipelineRuntimeService.kt
@@ -1209,6 +1209,8 @@ class PipelineRuntimeService @Autowired constructor(
                 pipelineId = buildInfo.pipelineId,
                 projectId = buildInfo.projectId,
                 resourceVersion = buildInfo.version,
+                versionNum = buildInfo.versionNum,
+                versionName = buildInfo.versionName,
                 executeCount = executeCount,
                 firstTaskId = buildInfo.firstTaskId,
                 actionType = ActionType.START,
