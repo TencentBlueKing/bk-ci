71113691d3c6bbd2b63184972e2eda8268ba8f99
lockiechen <33082528+lockiechen@users.noreply.github.com>
2024-01-25 14:56:49 +0800

    feat: pipeline as code issue #8125

--- src/frontend/devops-pipeline/constConfig.js
+++ src/frontend/devops-pipeline/constConfig.js
@@ -1,7 +1,6 @@
 module.exports = (version = 'tencent') => {
     const isTx = version === 'tencent'
     return {
-        BADGE_URL_PREFIX: isTx ? JSON.stringify('https://api.bkdevops.qq.com') : JSON.stringify(''),
         USER_IMG_URL: isTx ? JSON.stringify('//dayu.woa.com') : JSON.stringify(''),
         OPEN_URL: isTx ? JSON.stringify('//open.woa.com') : JSON.stringify(''),
         OIED_URL: isTx ? JSON.stringify('//o.ied.com') : JSON.stringify(''),
