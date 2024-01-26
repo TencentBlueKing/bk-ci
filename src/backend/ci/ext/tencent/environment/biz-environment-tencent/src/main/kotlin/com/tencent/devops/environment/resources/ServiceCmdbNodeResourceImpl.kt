2b69f3d7753ea42028c330213767d7426faccd21
jianingzhao <jianingzhao@tencent.com>
2024-01-26 21:50:15 +0800

    --story=887060303 prefer：优化代码格式

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/resources/ServiceCmdbNodeResourceImpl.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/resources/ServiceCmdbNodeResourceImpl.kt
@@ -27,7 +27,6 @@
 
 package com.tencent.devops.environment.resources
 
-import com.tencent.devops.common.api.exception.ParamBlankException
 import com.tencent.devops.common.api.pojo.Page
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
