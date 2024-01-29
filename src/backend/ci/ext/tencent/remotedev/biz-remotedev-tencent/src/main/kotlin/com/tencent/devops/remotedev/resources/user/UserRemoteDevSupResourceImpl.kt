6d3eb81bb6355947ad04d6dfae1ebcb7796726cf
ruotiantang <ruotiantang@tencent.com>
2024-01-29 16:44:39 +0800

    --story=115858143 云研发配套资源 - CVM登记

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserRemoteDevSupResourceImpl.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/resources/user/UserRemoteDevSupResourceImpl.kt
@@ -1,11 +1,13 @@
 package com.tencent.devops.remotedev.resources.user
 
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.remotedev.api.user.UserRemoteDevSupResource
 import com.tencent.devops.remotedev.pojo.remotedevsup.DevcloudCVMData
 import com.tencent.devops.remotedev.service.devcloud.DevcloudService
 import org.springframework.beans.factory.annotation.Autowired
 
+@RestResource
 class UserRemoteDevSupResourceImpl @Autowired constructor(
     private val devcloudService: DevcloudService
 ) : UserRemoteDevSupResource {
