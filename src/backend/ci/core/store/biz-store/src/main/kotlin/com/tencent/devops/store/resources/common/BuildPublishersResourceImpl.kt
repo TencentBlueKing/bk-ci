f8e6c57d2f45dd71f84d81dd407e7d52adce965d
yjieliang <yundantengteng@163.com>
2024-01-31 11:11:45 +0800

    bug： 用户组织架构新增线一级别组织层级，用户信息获取逻辑未更新导致部分组织架构校验逻辑校验失败 #9967

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/resources/common/BuildPublishersResourceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/resources/common/BuildPublishersResourceImpl.kt
@@ -27,13 +27,13 @@
 
 package com.tencent.devops.store.resources.common
 
+import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.RestResource
+import com.tencent.devops.common.web.annotation.SensitiveApiPermission
 import com.tencent.devops.store.api.common.BuildPublishersResource
 import com.tencent.devops.store.pojo.common.PublishersRequest
 import com.tencent.devops.store.pojo.common.StoreDockingPlatformRequest
 import com.tencent.devops.store.service.common.PublishersDataService
-import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.common.web.annotation.SensitiveApiPermission
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
@@ -43,17 +43,17 @@ class BuildPublishersResourceImpl @Autowired constructor(
 
     @SensitiveApiPermission("syn_publisher_data")
     override fun synAddPublisherData(userId: String, publishers: List<PublishersRequest>): Result<Int> {
-        return Result(publishersDataService.createPublisherData(userId, publishers))
+        return Result(data = publishersDataService.createPublisherData(userId, publishers))
     }
 
     @SensitiveApiPermission("syn_publisher_data")
     override fun synDeletePublisherData(userId: String, publishers: List<PublishersRequest>): Result<Int> {
-        return Result(publishersDataService.deletePublisherData(userId, publishers))
+        return Result(data = publishersDataService.deletePublisherData(userId, publishers))
     }
 
     @SensitiveApiPermission("syn_publisher_data")
     override fun synUpdatePublisherData(userId: String, publishers: List<PublishersRequest>): Result<Int> {
-        return Result(publishersDataService.updatePublisherData(userId, publishers))
+        return Result(data = publishersDataService.updatePublisherData(userId, publishers))
     }
 
     @SensitiveApiPermission("syn_platforms_data")
@@ -61,7 +61,7 @@ class BuildPublishersResourceImpl @Autowired constructor(
         userId: String,
         storeDockingPlatformRequests: List<StoreDockingPlatformRequest>
     ): Result<Int> {
-        return Result(publishersDataService.savePlatformsData(userId, storeDockingPlatformRequests))
+        return Result(data = publishersDataService.savePlatformsData(userId, storeDockingPlatformRequests))
     }
 
     @SensitiveApiPermission("syn_platforms_data")
@@ -69,7 +69,7 @@ class BuildPublishersResourceImpl @Autowired constructor(
         userId: String,
         storeDockingPlatformRequests: List<StoreDockingPlatformRequest>
     ): Result<Int> {
-        return Result(publishersDataService.deletePlatformsData(userId, storeDockingPlatformRequests))
+        return Result(data = publishersDataService.deletePlatformsData(userId, storeDockingPlatformRequests))
     }
 
     @SensitiveApiPermission("syn_platforms_data")
@@ -77,11 +77,11 @@ class BuildPublishersResourceImpl @Autowired constructor(
         userId: String,
         storeDockingPlatformRequests: List<StoreDockingPlatformRequest>
     ): Result<Int> {
-        return Result(publishersDataService.savePlatformsData(userId, storeDockingPlatformRequests))
+        return Result(data = publishersDataService.savePlatformsData(userId, storeDockingPlatformRequests))
     }
 
     @SensitiveApiPermission("syn_platforms_data")
     override fun synUpdatePlatformsLogoInfo(userId: String, platformCode: String, logoUrl: String): Result<Boolean> {
-        return Result(publishersDataService.updatePlatformsLogoInfo(userId, platformCode, logoUrl))
+        return Result(data = publishersDataService.updatePlatformsLogoInfo(userId, platformCode, logoUrl))
     }
 }
