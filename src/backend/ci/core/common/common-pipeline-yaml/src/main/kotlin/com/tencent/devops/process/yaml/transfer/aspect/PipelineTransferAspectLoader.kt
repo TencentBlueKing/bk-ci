60f5e878d09189a01029c264429a851dcb990c4c
yongyiduan(段永億) <yongyiduan@tencent.com>
2024-02-20 15:32:42 +0800

    【PAC】feat：流水线常量 Code 语法和规范 #9971

--- src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/transfer/aspect/PipelineTransferAspectLoader.kt
+++ src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/transfer/aspect/PipelineTransferAspectLoader.kt
@@ -40,16 +40,17 @@ object PipelineTransferAspectLoader {
                 }
             }
         )
-        aspects.add(
-            object : IPipelineTransferAspectElement {
-                override fun before(jp: PipelineTransferJoinPoint): Any? {
-                    if (jp.yamlStep() != null && jp.yamlStep()!!.checkout == "self") {
-                        jp.yamlStep()!!.checkout = repoName.value
-                    }
-                    return null
-                }
-            }
-        )
+        /*checkout 新增 self类型，此处暂时去掉转换 */
+//        aspects.add(
+//            object : IPipelineTransferAspectElement {
+//                override fun before(jp: PipelineTransferJoinPoint): Any? {
+//                    if (jp.yamlStep() != null && jp.yamlStep()!!.checkout == "self") {
+//                        jp.yamlStep()!!.checkout = repoName.value
+//                    }
+//                    return null
+//                }
+//            }
+//        )
         aspects.add(
             // 一个触发器时，如果为默认仓库则忽略repoName和type
             object : IPipelineTransferAspectModel {
