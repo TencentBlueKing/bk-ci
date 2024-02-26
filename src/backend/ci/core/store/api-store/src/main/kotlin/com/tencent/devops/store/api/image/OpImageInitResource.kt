a7964c71f79678f76a02770056c6450c19748907
yjieliang <yundantengteng@163.com>
2024-02-26 18:05:22 +0800

    feat:缩减store微服务的子模块数量 #9225

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/api/image/OpImageInitResource.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/api/image/OpImageInitResource.kt
@@ -28,7 +28,7 @@
 package com.tencent.devops.store.api.image
 
 import com.tencent.devops.common.api.pojo.Result
-import com.tencent.devops.store.api.image.op.pojo.ImageInitRequest
+import com.tencent.devops.store.pojo.image.request.ImageInitRequest
 import io.swagger.v3.oas.annotations.tags.Tag
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
