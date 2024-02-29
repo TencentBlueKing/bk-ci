2cb4ebb092d9d2f7aa2136e33da07ebaa379bf92
yjieliang <yundantengteng@163.com>
2024-02-29 16:33:05 +0800

    feat：插件代码库自动开启 stream ci 时，项目归属信息优化 #9996

--- src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/ServiceRepositoryResource.kt
+++ src/backend/ci/core/repository/api-repository/src/main/kotlin/com/tencent/devops/repository/api/ServiceRepositoryResource.kt
@@ -39,9 +39,9 @@ import com.tencent.devops.repository.pojo.RepositoryId
 import com.tencent.devops.repository.pojo.RepositoryInfo
 import com.tencent.devops.repository.pojo.RepositoryInfoWithPermission
 import com.tencent.devops.repository.pojo.enums.Permission
-import io.swagger.v3.oas.annotations.tags.Tag
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
 import javax.ws.rs.GET
@@ -232,4 +232,15 @@ interface ServiceRepositoryResource {
         @Parameter(description = "流水线引用信息", required = true)
         request: RepoPipelineRefRequest
     ): Result<Boolean>
+
+    @Operation(summary = "根据代码库哈希ID查询GIT项目ID")
+    @POST
+    @Path("/git/branch/default/rules")
+    fun getGitProjectIdByRepositoryHashId(
+        @Parameter(description = "用户ID", required = true)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @Parameter(description = "代码库哈希ID列表", required = true)
+        repositoryHashIdList: List<String>
+    ): Result<List<String>>
 }
