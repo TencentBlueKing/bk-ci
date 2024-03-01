a5ee423be043e50b9e664120312edc3d409a9322
greysonfang <greysonfang@tencent.com>
2024-03-01 14:56:25 +0800

    feat：项目属性监控 #10032

--- src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v4/ApigwAuthProjectResourceV4Impl.kt
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/resources/apigw/v4/ApigwAuthProjectResourceV4Impl.kt
@@ -10,7 +10,6 @@ import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.client.ClientTokenService
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.openapi.api.apigw.v4.ApigwAuthProjectResourceV4
-import com.tencent.devops.openapi.service.OpenapiPermissionService
 import com.tencent.devops.project.pojo.ProjectCreateUserInfo
 import com.tencent.devops.project.pojo.ProjectDeleteUserInfo
 import org.slf4j.LoggerFactory
@@ -20,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired
 class ApigwAuthProjectResourceV4Impl @Autowired constructor(
     val tokenService: ClientTokenService,
     val client: Client,
-    val openapiPermissionService: OpenapiPermissionService
 ) : ApigwAuthProjectResourceV4 {
     companion object {
         val logger = LoggerFactory.getLogger(ApigwAuthProjectResourceV4Impl::class.java)
