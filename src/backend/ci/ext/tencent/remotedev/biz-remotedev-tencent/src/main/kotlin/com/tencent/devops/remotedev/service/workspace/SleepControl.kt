0ae3e494330e3f535474e012ec6e01c9d365e17a
yongyiduan(段永億) <yongyiduan@tencent.com>
2024-03-04 16:43:40 +0800

    --story=116254852 云桌面交付失败人工介入处理知会新增 envid信息

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/workspace/SleepControl.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/workspace/SleepControl.kt
@@ -334,15 +334,21 @@ class SleepControl @Autowired constructor(
                         .onFailure { i ->
                             logger.warn("auto sleep fail|${i.message}", i)
                         }.onSuccess {
-                            notifyControl.notify4UserAndCCRemoteDevManager(
-                                userIds = permissionService.getWorkspaceOwner(workspace.workspaceName).toMutableSet(),
-                                projectId = workspace.projectId,
-                                notifyTemplateCode = NotifyControl.NOT_LOGIN_AUTO_SLEEP_NOTIFY,
-                                notifyType = mutableSetOf(RemoteDevNotifyType.EMAIL),
-                                bodyParams = mapOf(
-                                    "cgsIp" to (workspace.hostName ?: "")
-                                )
+                            logger.info(
+                                "sleep $it when not login 7 day " +
+                                        "|${workspace.workspaceName}|${workspace.lastStatusUpdateTime}|${workspace.hostName}"
                             )
+                            if (it) {
+                                notifyControl.notify4UserAndCCRemoteDevManager(
+                                    userIds = permissionService.getWorkspaceOwner(workspace.workspaceName).toMutableSet(),
+                                    projectId = workspace.projectId,
+                                    notifyTemplateCode = NotifyControl.NOT_LOGIN_AUTO_SLEEP_NOTIFY,
+                                    notifyType = mutableSetOf(RemoteDevNotifyType.EMAIL),
+                                    bodyParams = mapOf(
+                                        "cgsIp" to (workspace.hostName ?: "")
+                                    )
+                                )
+                            }
                         }
                 }
             }
