b2dfa6ed68ea8459fe9534dfd90f99d3e73d8bfb
hejieehe <904696180@qq.com>
2024-03-05 11:36:01 +0800

    feat: svn webhook接口切换 #9302

--- src/backend/ci/core/common/common-scm/src/main/kotlin/com/tencent/devops/scm/code/CodeSvnScmImpl.kt
+++ src/backend/ci/core/common/common-scm/src/main/kotlin/com/tencent/devops/scm/code/CodeSvnScmImpl.kt
@@ -159,16 +159,15 @@ class CodeSvnScmImpl constructor(
         try {
             addWebhookByToken(hookUrl, projectName)
         } catch (ignored: ScmException) {
-            // 工蜂迁移svn项目后，svn项目名与原有git项目名相同，导致项目名冲突，为此在工蜂在svn项目名后添加[_svn]后缀，但在复制svn路径时可能
-            // 缺少[_svn]，如实际路径为[bk_ci/ci_svn],但复制路径为[bk_ci/ci]，所以当报项目不存在时，增加[_svn]后缀后重试
-            if (ignored.message == I18nUtil.getCodeLanMessage(CommonMessageCode.ENGINEERING_REPO_NOT_EXIST) &&
-                !projectName.endsWith(SVN_PROJECT_NAME_SUFFIX)
-            ) {
+            // 工蜂迁移svn项目后，svn项目名与原有git项目名相同，导致项目名冲突，为此在工蜂在svn组名后添加[_svn]后缀，但在复制svn路径时可能
+            // 缺少[_svn]，如实际路径为[bk_ci_svn/ci],但复制路径为[bk_ci/ci]，所以当报项目不存在时，组名处增加[_svn]后缀后重试
+            val (needTry, newProjectName) = tryAddSuffixToGroupName(projectName, ignored.message)
+            if (needTry) {
                 try {
-                    logger.info("retry addWebHookSVN|newProjectName=$projectName$SVN_PROJECT_NAME_SUFFIX")
+                    logger.info("retry addWebHookSVN|newProjectName=$newProjectName")
                     addWebhookByToken(
                         hookUrl = hookUrl,
-                        projectName = "$projectName$SVN_PROJECT_NAME_SUFFIX"
+                        projectName = newProjectName
                     )
                 } catch (ignored: ScmException) {
                     logger.error("Fail to retry add the webhook", ignored)
@@ -352,6 +351,29 @@ class CodeSvnScmImpl constructor(
         } catch (e: ScmException) {
             logger.warn("fail get the svn session", e)
             null
+        }
+    }
+
+    /**
+     * 尝试添加后缀到组名
+     */
+    private fun tryAddSuffixToGroupName(projectName: String, errorMsg: String?): Pair<Boolean, String> {
+        val projectNameArr = projectName.split("/").toMutableList()
+        return when {
+            projectNameArr.size <= 1 ||
+                    errorMsg != I18nUtil.getCodeLanMessage(CommonMessageCode.ENGINEERING_REPO_NOT_EXIST) -> {
+                Pair(false, projectName)
+            }
+
+            !projectNameArr[0].endsWith(SVN_PROJECT_NAME_SUFFIX) -> {
+                // 替换组名，[bk_ci/ci] -> [bk_ci_svn/ci]
+                projectNameArr[0] = projectNameArr[0].plus(SVN_PROJECT_NAME_SUFFIX)
+                Pair(true, projectNameArr.joinToString("/"))
+            }
+
+            else -> {
+                Pair(false, projectName)
+            }
         }
     }
 
