711a1be96f0b7f8483ef83b45f45fe5dce75a652
yjieliang <yundantengteng@163.com>
2024-03-06 15:05:56 +0800

    feat:缩减store微服务的子模块数量 #9225

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/OpAtomServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/OpAtomServiceImpl.kt
@@ -86,6 +86,7 @@ import com.tencent.devops.store.common.service.action.StoreDecorateFactory
 import com.tencent.devops.store.common.service.StoreWebsocketService
 import com.tencent.devops.store.common.utils.TextReferenceFileAnalysisUtil
 import com.tencent.devops.store.common.utils.StoreUtils
+import com.tencent.devops.store.utils.ThreadPoolUtil
 import java.io.File
 import java.io.InputStream
 import java.nio.charset.Charset
