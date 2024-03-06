a4da185c70435aa6052dd075614407e2f08ee5cd
yjieliang <yundantengteng@163.com>
2024-03-06 16:31:38 +0800

    feat：插件代码库自动开启 stream ci 时，项目归属信息优化 #9996

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/config/StreamGitConfig.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/config/StreamGitConfig.kt
@@ -49,8 +49,5 @@ class StreamGitConfig {
     @Value("\${github.signSecret:}")
     val signSecret: String = ""
 
-    @Value("\${bkci.defaultProductId:#{0}}")
-    val defaultBkProductId: Int = 0
-
     fun getScmType() = ScmType.valueOf(scmType!!)
 }
