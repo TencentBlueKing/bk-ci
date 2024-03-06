2cdcfb13abf765fec05e18ee67768ce9f8c7d32b
royalhuang <royalhuang@tencent.com>
2024-03-06 15:25:32 +0800

    feat：流水线版本管理机制 #8161 增加通知校验

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/extend/DefaultModelCheckPlugin.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/extend/DefaultModelCheckPlugin.kt
@@ -47,8 +47,8 @@ import com.tencent.devops.common.pipeline.pojo.element.market.MarketBuildLessAto
 import com.tencent.devops.common.pipeline.pojo.setting.PipelineSetting
 import com.tencent.devops.common.pipeline.pojo.setting.Subscription
 import com.tencent.devops.process.constant.ProcessMessageCode
-import com.tencent.devops.process.constant.ProcessMessageCode.BK_INCORRECT_NOTIFICATION_METHOD
-import com.tencent.devops.process.constant.ProcessMessageCode.BK_MESSAGE_CONTENT_EMPTY
+import com.tencent.devops.process.constant.ProcessMessageCode.ERROR_INCORRECT_NOTIFICATION_MESSAGE_CONTENT
+import com.tencent.devops.process.constant.ProcessMessageCode.ERROR_INCORRECT_NOTIFICATION_TYPE
 import com.tencent.devops.process.engine.atom.AtomUtils
 import com.tencent.devops.process.engine.common.Timeout
 import com.tencent.devops.process.engine.utils.PipelineUtils
@@ -174,12 +174,12 @@ open class DefaultModelCheckPlugin constructor(
         this.forEach { subscription ->
             if (subscription.types.isEmpty()) {
                 throw ErrorCodeException(
-                    errorCode = BK_INCORRECT_NOTIFICATION_METHOD
+                    errorCode = ERROR_INCORRECT_NOTIFICATION_TYPE
                 )
             }
             if (subscription.content.isBlank()) {
                 throw ErrorCodeException(
-                    errorCode = BK_MESSAGE_CONTENT_EMPTY
+                    errorCode = ERROR_INCORRECT_NOTIFICATION_MESSAGE_CONTENT
                 )
             }
         }
