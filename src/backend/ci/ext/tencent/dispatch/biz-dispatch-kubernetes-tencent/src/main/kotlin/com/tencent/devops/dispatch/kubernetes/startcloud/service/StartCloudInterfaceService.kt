06330e3a19c2507755378e4c506c003b8a91706c
randychen <randychen@tencent.com>
2024-03-18 15:48:49 +0800

    --story=116571838 查询cgslist接口，兼容注册时间字段为空的情况。

--- src/backend/ci/ext/tencent/dispatch/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/startcloud/service/StartCloudInterfaceService.kt
+++ src/backend/ci/ext/tencent/dispatch/biz-dispatch-kubernetes-tencent/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/startcloud/service/StartCloudInterfaceService.kt
@@ -49,8 +49,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 @Service("startcloudInterfaceService")
 class StartCloudInterfaceService @Autowired constructor(
