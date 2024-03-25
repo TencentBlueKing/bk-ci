5116417f7d2af101445f56df8e7cfbf685c03f34
yjieliang <yundantengteng@163.com>
2024-03-25 16:57:46 +0800

    feat：支持插件开发者设置默认的超时时间和默认的失败时的策略 #10019

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreConstants.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreConstants.kt
@@ -33,13 +33,18 @@ object StoreConstants {
     const val BK_PIPELINED_JOB = "bkPipelinedJob" // 流水线Job
     const val BK_IMAGE_STORE_ONLINE = "bkImageStoreOnline" // 容器镜像商店上线，历史镜像数据自动生成
     const val BK_OLD_VERSION_BUILD_IMAGE = "bkOldVersionBuildImage" // 旧版的构建镜像，通过拷贝为构建镜像入口生成
-    const val BK_AUTOMATICALLY_CONVERTED = "bkAutomaticallyConverted" // 已自动转换为容器镜像商店数据，请项目管理员在研发商店工作台进行管理。
-    const val BK_COPY_FOR_BUILD_IMAGE = "bkCopyForBuildImage" // 旧版的构建镜像，通过蓝盾版本仓库“拷贝为构建镜像”入口生成。
-    const val BK_AFTER_IMAGE_STORE_ONLINE = "bkAfterImageStoreOnline" // 容器镜像商店上线后，旧版入口已下线。因历史原因，此类镜像没有办法对应到实际的镜像推送人，暂时先挂到项目管理员名下。
-    const val BK_PROJECT_MANAGER_CAN_OPERATION = "bkProjectManagerCanOperation" // 项目管理员可在研发商店工作台进行上架/升级/下架等操作，或者交接给实际负责人进行管理。
+    // 已自动转换为容器镜像商店数据，请项目管理员在研发商店工作台进行管理。
+    const val BK_AUTOMATICALLY_CONVERTED = "bkAutomaticallyConverted"
+    // 旧版的构建镜像，通过蓝盾版本仓库“拷贝为构建镜像”入口生成。
+    const val BK_COPY_FOR_BUILD_IMAGE = "bkCopyForBuildImage"
+    // 容器镜像商店上线后，旧版入口已下线。因历史原因，此类镜像没有办法对应到实际的镜像推送人，暂时先挂到项目管理员名下。
+    const val BK_AFTER_IMAGE_STORE_ONLINE = "bkAfterImageStoreOnline"
+    // 项目管理员可在研发商店工作台进行上架/升级/下架等操作，或者交接给实际负责人进行管理。
+    const val BK_PROJECT_MANAGER_CAN_OPERATION = "bkProjectManagerCanOperation"
     const val BK_HISTORY_DATA_MIGRATE_PASS = "bkHistoryDataMigratePass" // historyData数据迁移自动通过
     const val BK_WORKER_BEE_PROJECT_NOT_EXIST = "bkWorkerBeeProjectNotExist" // 工蜂项目信息不存在，请检查链接
-    const val BK_WORKER_BEE_PROJECT_NOT_STREAM_ENABLED = "bkWorkerBeeProjectNotStreamEnabled" // 工蜂项目未开启Stream，请前往仓库的CI/CD进行配置
+    // 工蜂项目未开启Stream，请前往仓库的CI/CD进行配置
+    const val BK_WORKER_BEE_PROJECT_NOT_STREAM_ENABLED = "bkWorkerBeeProjectNotStreamEnabled"
     // 研发商店：插件配置文件[task.json]config配置格式不正确，defaultTimeout应为1-10080之间的正整数
     const val TASK_JSON_CONFIG_DEFAULT_TIMEOUT_FIELD_IS_INVALID = "bkTaskJsonConfigDefaultTimeoutFieldIsInvalid"
     // 研发商店：插件配置文件[task.json]config配置格式不正确，retryTimes应为1～5之间的正整数
