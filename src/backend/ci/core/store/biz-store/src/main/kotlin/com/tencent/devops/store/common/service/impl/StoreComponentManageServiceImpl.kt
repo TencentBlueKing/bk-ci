ba639f3fee3a04948d6363d80a27f8abbf77b799
yjieliang <yundantengteng@163.com>
2024-03-27 17:02:35 +0800

    pref：研发商店通用化接口封装 #10123

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/StoreComponentManageServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/StoreComponentManageServiceImpl.kt
@@ -31,26 +31,20 @@ import com.tencent.devops.common.api.constant.CommonMessageCode
 import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.pipeline.enums.ChannelCode
-import com.tencent.devops.common.service.utils.SpringContextUtil
 import com.tencent.devops.store.common.dao.StoreBaseFeatureManageDao
 import com.tencent.devops.store.common.dao.StoreBaseQueryDao
 import com.tencent.devops.store.common.service.StoreComponentManageService
-import com.tencent.devops.store.common.service.StoreLogicExtendService
 import com.tencent.devops.store.common.service.StoreProjectService
 import com.tencent.devops.store.constant.StoreMessageCode
 import com.tencent.devops.store.pojo.common.InstallStoreReq
+import com.tencent.devops.store.pojo.common.StoreBaseInfo
 import com.tencent.devops.store.pojo.common.StoreBaseInfoUpdateRequest
 import com.tencent.devops.store.pojo.common.UnInstallReq
-import com.tencent.devops.store.pojo.common.enums.StoreStatusEnum
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
-import com.tencent.devops.store.pojo.common.publication.StoreBaseDataPO
-import com.tencent.devops.store.pojo.common.publication.StoreBaseFeatureDataPO
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
 
-@Service
-class StoreComponentManageServiceImpl(
+abstract class StoreComponentManageServiceImpl(
     private val dslContext: DSLContext,
     private val storeBaseQueryDao: StoreBaseQueryDao,
     private val storeBaseFeatureManageDao: StoreBaseFeatureManageDao,
@@ -84,49 +78,36 @@ class StoreComponentManageServiceImpl(
             errorCode = CommonMessageCode.PARAMETER_IS_INVALID,
             params = arrayOf(installStoreReq.storeCode)
         )
-        val storeBaseDataPO = componentBaseInfoRecord.let {
-            StoreBaseDataPO(
-                id = it.id,
-                storeCode = it.storeCode,
-                storeType = StoreTypeEnum.getStoreTypeObj(it.storeType.toInt()),
-                name = it.name,
-                version = it.version,
-                status = StoreStatusEnum.valueOf(it.status),
-                logoUrl = it.logoUrl,
-                latestFlag = it.latestFlag,
-                publisher = it.publisher,
-                classifyId = it.classifyId,
-                creator = it.creator,
-                modifier = it.modifier
-            )
-        }
-        val componentFeatureInfo = storeBaseFeatureManageDao.getComponentFeatureDataByCode(
+        val publicFlag = storeBaseFeatureManageDao.isPublic(
             dslContext = dslContext,
             storeCode = installStoreReq.storeCode,
             storeType = installStoreReq.storeType
         )
-        val publicFlag = componentFeatureInfo?.publicFlag ?: true
+        val storeBaseInfo = componentBaseInfoRecord.let {
+            StoreBaseInfo(
+                storeId = it.id,
+                storeCode = it.storeCode,
+                storeName = it.name,
+                storeType = StoreTypeEnum.getStoreTypeObj(it.storeType.toInt()),
+                version = it.name,
+                publicFlag = publicFlag,
+                status = it.status,
+                logoUrl = it.logoUrl,
+                publisher = it.publisher,
+                classifyId = it.classifyId
+            )
+        }
+        // 校验组件安装权限
         validateInstall(
             userId = userId,
-            publicFlag = publicFlag,
-            installStoreReq = installStoreReq,
-            channelCode = channelCode
+            storeBaseInfo = storeBaseInfo,
+            projectCodeList = installStoreReq.projectCodes
         )
-        val installComponentExtResult = getStoreLogicExtendService(installStoreReq.storeType).installComponentExt(
+        // 安装操作逻辑扩展
+        val installComponentExtResult = installComponentExt(
             userId = userId,
             projectCodeList = installStoreReq.projectCodes,
-            storeBaseDataPO = storeBaseDataPO,
-            storeBaseFeatureDataPO = componentFeatureInfo?.let {
-                StoreBaseFeatureDataPO(
-                    id = componentFeatureInfo.id,
-                    storeCode = componentFeatureInfo.storeCode,
-                    storeType = StoreTypeEnum.getStoreTypeObj(it.storeType.toInt()),
-                    type = componentFeatureInfo.type,
-                    rdType = componentFeatureInfo.rdType,
-                    creator = componentFeatureInfo.creator,
-                    modifier = componentFeatureInfo.modifier
-                )
-            }
+            storeBaseInfo = storeBaseInfo
         )
         if (installComponentExtResult.isNotOk()) {
             throw ErrorCodeException(
@@ -137,7 +118,7 @@ class StoreComponentManageServiceImpl(
         return storeProjectService.installStoreComponent(
             userId = userId,
             projectCodeList = installStoreReq.projectCodes,
-            storeId = storeBaseDataPO.id,
+            storeId = storeBaseInfo.storeId,
             storeCode = installStoreReq.storeCode,
             storeType = installStoreReq.storeType,
             publicFlag = publicFlag,
@@ -146,46 +127,24 @@ class StoreComponentManageServiceImpl(
 
     }
 
-    private fun getStoreLogicExtendService(storeType: StoreTypeEnum): StoreLogicExtendService {
-        return SpringContextUtil.getBean(StoreLogicExtendService::class.java, getLogicExtendServiceBeanName(storeType))
-    }
 
-    private fun getLogicExtendServiceBeanName(storeType: StoreTypeEnum): String {
-        return "${storeType}_LOGIC_EXTEND_SERVICE"
-    }
-
-    private fun validateInstall(
+    /**
+     * 组件安装校验
+     */
+    abstract fun validateInstall(
         userId: String,
-        publicFlag: Boolean,
-        channelCode: ChannelCode,
-        installStoreReq: InstallStoreReq
-    ): Result<Boolean> {
-        val storeCode = installStoreReq.storeCode
-        val storeType = installStoreReq.storeType
-        val validateInstallExtResult = getStoreLogicExtendService(installStoreReq.storeType).validateInstallExt(
-            userId = userId,
-            storeCode = storeCode,
-            projectCodeList = installStoreReq.projectCodes
-        )
-        if (validateInstallExtResult.isNotOk()) {
-            throw ErrorCodeException(
-                errorCode = StoreMessageCode.STORE_INSTALL_VALIDATE_FAIL,
-                params = arrayOf(storeCode, "${validateInstallExtResult.message}")
-            )
-        }
-        val validateInstallResult = storeProjectService.validateInstallPermission(
-            userId = userId,
-            projectCodeList = installStoreReq.projectCodes,
-            storeCode = storeCode,
-            storeType = storeType,
-            publicFlag = publicFlag,
-            channelCode = channelCode
-        )
-        if (validateInstallResult.isNotOk()) {
-            return validateInstallResult
-        }
-        return Result(true)
-    }
+        storeBaseInfo: StoreBaseInfo,
+        projectCodeList: ArrayList<String>
+    ): Result<Boolean>
+
+    /**
+     * 组件安装校逻辑扩展
+     */
+    abstract fun installComponentExt(
+        userId: String,
+        projectCodeList: ArrayList<String>,
+        storeBaseInfo: StoreBaseInfo
+    ): Result<Boolean>
 
     override fun uninstallComponent(
         userId: String,
