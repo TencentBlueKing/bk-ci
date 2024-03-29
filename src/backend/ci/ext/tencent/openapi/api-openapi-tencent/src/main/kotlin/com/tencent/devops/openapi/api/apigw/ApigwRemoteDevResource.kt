a2758b619757caad1543338993b21d2610b4ee27
randychen <randychen@tencent.com>
2024-03-29 11:47:59 +0800

    --story=116788311 【接口优化】获取云桌面的ip信息接口header：X-BK-NGGW-CLIENT-ADDRESS和X-DEVOPS-DEVX-GW-TOKEN改从Header.kt获取

--- src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/ApigwRemoteDevResource.kt
+++ src/backend/ci/ext/tencent/openapi/api-openapi-tencent/src/main/kotlin/com/tencent/devops/openapi/api/apigw/ApigwRemoteDevResource.kt
@@ -5,6 +5,8 @@ import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_APP_CODE_DEFAULT_VA
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_ORIGINAL_HOST
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID_DEFAULT_VALUE
+import com.tencent.devops.common.api.auth.DEVX_HEADER_GW_TOKEN
+import com.tencent.devops.common.api.auth.DEVX_HEADER_NGGW_CLIENT_ADDRESS
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.remotedev.pojo.WindowsResourceTypeConfig
 import com.tencent.devops.remotedev.pojo.op.OpProjectWorkspaceAssignData
@@ -81,13 +83,13 @@ interface ApigwRemoteDevResource {
         @PathParam("apigwType")
         apigwType: String?,
         @Parameter(description = "IP", required = false)
-        @HeaderParam("X-BK-NGGW-CLIENT-ADDRESS")
+        @HeaderParam(DEVX_HEADER_NGGW_CLIENT_ADDRESS)
         ip: String?,
         @Parameter(description = "original host", required = false)
         @HeaderParam(AUTH_HEADER_DEVOPS_ORIGINAL_HOST)
         originalHost: String?,
         @Parameter(description = "devx token", required = false)
-        @HeaderParam("X-DEVOPS-DEVX-GW-TOKEN")
+        @HeaderParam(DEVX_HEADER_GW_TOKEN)
         devxToken: String?
     ): Result<WeSecProjectWorkspace?>
 
