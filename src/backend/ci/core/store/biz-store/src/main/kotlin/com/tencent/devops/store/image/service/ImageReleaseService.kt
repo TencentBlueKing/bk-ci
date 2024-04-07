d35ec76735810130b0478af7d6e51fa6e9a80f5b
carlyin <449036249@qq.com>
2024-04-07 17:35:09 +0800

    pref：研发商店通用化接口封装 #10123

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/image/service/ImageReleaseService.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/image/service/ImageReleaseService.kt
@@ -101,7 +101,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
-import java.util.*
+import java.util.Base64
 
 @Suppress("ALL")
 @Service
