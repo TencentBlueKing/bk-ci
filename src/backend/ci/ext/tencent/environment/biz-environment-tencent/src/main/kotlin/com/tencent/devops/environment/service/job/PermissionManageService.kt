5df36bb3cc63dab837e127d4db054404c1c0d5ea
jianingzhao <jianingzhao@tencent.com>
2024-04-07 20:46:06 +0800

    --story=887060303 环境管理部分代码优化
    feat：脚本执行/文件分发 任务执行时，去掉判断节点使用权限的逻辑，只判断 用户or节点导入人 是机器的主备负责人

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/job/PermissionManageService.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/job/PermissionManageService.kt
@@ -27,11 +27,6 @@
 
 package com.tencent.devops.environment.service.job
 
-import com.tencent.devops.common.api.exception.ErrorCodeException
-import com.tencent.devops.common.api.util.HashUtil
-import com.tencent.devops.common.auth.api.AuthPermission
-import com.tencent.devops.environment.constant.EnvironmentMessageCode
-import com.tencent.devops.environment.constant.T_NODE_NODE_ID
 import com.tencent.devops.environment.constant.T_NODE_NODE_IP
 import com.tencent.devops.environment.dao.job.CmdbNodeDao
 import com.tencent.devops.environment.dao.job.JobDao
@@ -67,34 +62,11 @@ class PermissionManageService @Autowired constructor(
     }
 
     fun isUserHasAllPermission(userId: String, projectId: String, allHostList: List<Host>) {
-        // 用户有使用该节点的权限
         val nodeRecords = getNodesFromHostList(dslContext, projectId, allHostList).toSet() // 所有host对应的T_NODE表中的记录
-        val recordByHostIdList = mutableListOf<Host>()
-        val getRecordByIpAndBkCloudId = mutableListOf<Host>()
-        allHostList.map {
-            if (null != it.bkHostId) recordByHostIdList.add(it)
-            else getRecordByIpAndBkCloudId.add(it)
-        }
-        val nodeIdList: List<Long> = nodeRecords.mapNotNull { it[T_NODE_NODE_ID] as? Long } // 所有host对应的T_NODE表中的host_id
         logger.info(
             "Wait to detect permission node ip(s): " +
                 nodeRecords.mapNotNull { it[T_NODE_NODE_IP] as? String }.joinToString()
         )
-        val canUseNodeIds = environmentPermissionService.listNodeByPermission(
-            userId, projectId, AuthPermission.USE
-        ) // 用户所有有权限使用的节点记录
-        val unauthorizedNodeIds = nodeIdList.filterNot {
-            canUseNodeIds.contains(it)
-        } // 传进来的host 不在用户有权限使用的记录列表中的（用node_id来筛选）
-        if (logger.isDebugEnabled)
-            logger.debug("All nodes that the user has permission to use: ${canUseNodeIds.joinToString()}")
-        if (unauthorizedNodeIds.isNotEmpty()) {
-            logger.warn("Unauthorized node id list: $unauthorizedNodeIds")
-            throw ErrorCodeException(
-                errorCode = EnvironmentMessageCode.ERROR_NODE_NO_USE_PERMISSSION,
-                params = arrayOf(unauthorizedNodeIds.joinToString(",") { HashUtil.encodeLongId(it) })
-            )
-        } // unauthorizedNodeIds - 为空：用户有所有传进来的host节点的权限；- 不为空：其中的节点用户没权限，抛出异常。
         // 判断：用户or节点导入人 是机器的主备负责人（用户：函数中形参userId；节点导入人：T_NODE表中的createdUser）
         tencentQueryFromCmdbService.isOperatorOrBakOperator(userId, nodeRecords)
     }
