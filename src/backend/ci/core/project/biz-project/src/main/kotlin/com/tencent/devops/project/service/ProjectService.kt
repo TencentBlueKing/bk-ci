de1cba9b20a9cc73ed4e4abbe4d93d25e6c7ddf3
greysonfang <greysonfang@tencent.com>
2024-04-15 11:51:40 +0800

    --story=116909042 feat：PreCI创建的项目需关联运营产品

--- src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/ProjectService.kt
+++ src/backend/ci/core/project/biz-project/src/main/kotlin/com/tencent/devops/project/service/ProjectService.kt
@@ -51,8 +51,8 @@ import com.tencent.devops.project.pojo.ProjectWithPermission
 import com.tencent.devops.project.pojo.Result
 import com.tencent.devops.project.pojo.enums.ProjectChannelCode
 import com.tencent.devops.project.pojo.enums.ProjectValidateType
-import java.io.InputStream
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition
+import java.io.InputStream
 
 @Suppress("ALL")
 interface ProjectService {
@@ -278,5 +278,4 @@ interface ProjectService {
         userId: String,
         englishName: String
     ): Boolean
-
 }
