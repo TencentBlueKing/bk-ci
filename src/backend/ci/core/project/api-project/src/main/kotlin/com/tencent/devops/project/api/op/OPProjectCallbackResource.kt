f1f891f98272e0dcd4edabaaa42616069b385843
hejieehe <904696180@qq.com>
2024-04-16 17:22:49 +0800

    feat: 新增项目级事件回调 #10146

--- src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/api/op/OPProjectCallbackResource.kt
+++ src/backend/ci/core/project/api-project/src/main/kotlin/com/tencent/devops/project/api/op/OPProjectCallbackResource.kt
@@ -64,7 +64,7 @@ interface OPProjectCallbackResource {
         @QueryParam("event")
         event: ProjectEventType,
         @Parameter(description = "事件类型", required = true)
-        @QueryParam("event")
+        @QueryParam("gatewayType")
         gatewayType: GatewayType,
         @Parameter(description = "回调凭证", required = true)
         secretParam: ISecretParam
