d88d8d0d40c0a8f8bab6a1541d391c7a19245d3f
yjieliang <yundantengteng@163.com>
2024-04-26 19:23:56 +0800

    pref：研发商店通用化接口封装 #10123

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/dao/MarketStoreQueryDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/dao/MarketStoreQueryDao.kt
@@ -112,16 +112,24 @@ class MarketStoreQueryDao {
         } else {
             baseStep.where(conditions).asTable()
         }
-//        // 查询每个组件中最新记录
-//        val maxCreateTimeSubquery = dslContext.select(
-//            filteredResultsSubquery.field(tStoreBase.STORE_CODE.name),
-//            filteredResultsSubquery.field(DSL.max(tStoreBase.CREATE_TIME).name)
-//        ).from(filteredResultsSubquery)
-//            .groupBy(DSL.field(tStoreBase.STORE_CODE.name))
+        // 查询每个组件中最新记录
+        val maxCreateTimeSubquery = dslContext.select(
+            filteredResultsSubquery.field(tStoreBase.STORE_CODE.name),
+            filteredResultsSubquery.field(DSL.max(tStoreBase.CREATE_TIME).name)
+        ).from(filteredResultsSubquery)
+            .groupBy(DSL.field(tStoreBase.STORE_CODE.name))
 
         return dslContext.select()
             .from(filteredResultsSubquery)
-            .limit(
+            .leftJoin(maxCreateTimeSubquery)
+            .on(
+                filteredResultsSubquery.field(tStoreBase.STORE_CODE)!!
+                    .eq(maxCreateTimeSubquery.field(tStoreBase.STORE_CODE))
+                    .and(
+                        filteredResultsSubquery.field(tStoreBase.CREATE_TIME)!!
+                            .eq(maxCreateTimeSubquery.field(tStoreBase.CREATE_TIME))
+                    )
+            ).limit(
                 (storeInfoQuery.page - 1) * storeInfoQuery.pageSize,
                 storeInfoQuery.pageSize
             ).fetch()
