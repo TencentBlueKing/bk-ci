bcc966c8e59e7a5c7b68a9156c27300d0c65cecf
ruotiantang <ruotiantang@tencent.com>
2024-05-09 18:10:56 +0800

    feat：构建资源类型为第三方构建集群时支持指定Job并发数 #9810

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/extend/DefaultModelCheckPlugin.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/extend/DefaultModelCheckPlugin.kt
@@ -68,7 +68,7 @@ import com.tencent.devops.store.pojo.common.StoreParam
 import com.tencent.devops.store.pojo.common.StoreVersion
 import org.slf4j.LoggerFactory
 
-@Suppress("ComplexMethod")
+@Suppress("ComplexMethod", "ComplexCondition")
 open class DefaultModelCheckPlugin constructor(
     open val client: Client,
     open val pipelineCommonSettingConfig: PipelineCommonSettingConfig,
