73d901b09aed3d3c1085c3bccb19dfffb9f99fde
v_hwweng <v_hwweng@tencent.com>
2024-05-09 17:31:35 +0800

    --story=116704259 蓝盾首页右侧-浮窗按钮集成易事厅“问一下”

--- src/frontend/devops-nav/src/components/ConsultTools/index.vue
+++ src/frontend/devops-nav/src/components/ConsultTools/index.vue
@@ -1,24 +1,35 @@
 <template>
-    <div class="consult-tools">
-        <a
-            v-for="(entry, index) in consultTypeList"
-            :key="index"
-            class="consult-item"
-            :href="entry.href"
-            :id="entry.id"
-            :target="entry.target"
+    <div>
+        <div class="consult-tools">
+            <a
+                v-for="(entry, index) in consultTypeList"
+                :key="index"
+                class="consult-item"
+                :href="entry.href"
+                :id="entry.id"
+                :target="entry.target"
+                @click="handleClickConsult(entry)"
+            >
+                <icon
+                    class="devops-icon"
+                    :name="entry.icon"
+                    size="13"
+                />
+                <p v-if="entry.id === 'contactUs'">
+                    O2000
+                    <br />
+                </p>
+                <p>{{ entry.label }}</p>
+            </a>
+        </div>
+        <div :class="{
+                 'fixation-wrap': true,
+                 'fixation-wrap-active': showTabAssistant
+             }"
+            v-bk-clickoutside="handleHideTabAssistant"
         >
-            <icon
-                class="devops-icon"
-                :name="entry.icon"
-                size="13"
-            />
-            <p v-if="entry.id === 'contactUs'">
-                O2000
-                <br />
-            </p>
-            <p>{{ entry.label }}</p>
-        </a>
+            <iframe class="help-center-iframe" src="" frameborder="0"></iframe>
+        </div>
     </div>
 </template>
 
@@ -30,12 +41,14 @@
     @Component
     export default class ConsultTools extends Vue {
         @State user
+        showTabAssistant: boolean = false
+        
         get consultTypeList () {
             return [
                 {
                     icon: 'customer',
                     label: this.$t('contactUs'),
-                    href: 'wxwork://message?uin=8444250473321980',
+                    // href: 'wxwork://message?uin=8444250473321980',
                     id: 'contactUs',
                     target: ''
                 },
@@ -54,6 +67,16 @@
                     target: '_blank'
                 }
             ]
+        }
+
+        handleClickConsult (entry) {
+            if (entry.id === 'contactUs') {
+                this.showTabAssistant = true
+            }
+        }
+
+        handleHideTabAssistant () {
+            if (this.showTabAssistant) this.showTabAssistant = false
         }
     }
 </script>
@@ -89,5 +112,32 @@
             font-size: 13px;
         }
     }
+}
+.fixation-wrap {
+    position: fixed;
+    right: 120px;
+    bottom: 104px;
+    width: 615px;
+    height: 640px;
+    pointer-events: none;
+    opacity: 0;
+    z-index: 9999;
+    background-color: #ecf2fe;
+    border: 1px solid rgba(59, 66, 75, .24);
+}
+.fixation-wrap-active {
+    opacity: 1;
+    pointer-events: auto;
+}
+.help-center-iframe {
+    position: fixed;
+    right: 120px;
+    bottom: 104px;
+    width: 615px;
+    height: 640px;
+    pointer-events: auto;
+    z-index: 9999;
+    background-color: #ecf2fe;
+    border: 1px solid rgba(59, 66, 75, .24);
 }
 </style>
