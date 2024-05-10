a3b54b1d7c36f6c233aa0db2783a781600b2915b
v_hwweng <v_hwweng@tencent.com>
2024-05-10 11:00:30 +0800

    --story=116704259 蓝盾首页右侧-浮窗按钮集成易事厅“问一下” # Reviewed, transaction id: 7460

--- src/frontend/devops-nav/src/components/ConsultTools/index.vue
+++ src/frontend/devops-nav/src/components/ConsultTools/index.vue
@@ -28,7 +28,7 @@
              }"
             v-bk-clickoutside="handleHideTabAssistant"
         >
-            <iframe class="help-center-iframe" src="" frameborder="0"></iframe>
+            <iframe class="help-center-iframe" src="https://yst.woa.com/chat/chatComp?web_key=1715309665_4428_joyuezhang" frameborder="0"></iframe>
         </div>
     </div>
 </template>
