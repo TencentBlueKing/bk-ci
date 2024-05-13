c294b36c870f89bc513dbc5cde0d61b75cf45d19
carlyin <449036249@qq.com>
2024-05-13 09:49:32 +0800

    pref：研发商店通用化接口封装 #10123

--- src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/api/UserArchiveComponentPkgResource.kt
+++ src/backend/ci/core/artifactory/api-artifactory/src/main/kotlin/com/tencent/devops/artifactory/api/UserArchiveComponentPkgResource.kt
@@ -27,7 +27,6 @@
 
 package com.tencent.devops.artifactory.api
 
-import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_PROJECT_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.web.annotation.BkField
