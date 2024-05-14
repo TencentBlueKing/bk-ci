39c31e5f7224e0330ecbb34ea2b74eb4846a0f2a
lockiechen <lockiechen@tencent.com>
2024-05-14 17:04:17 +0800

    feat: 【蓝盾-产品-已评审】流水线支持展示运行进度 issue #7932

--- src/frontend/bk-pipeline/src/Atom.vue
+++ src/frontend/bk-pipeline/src/Atom.vue
@@ -45,7 +45,10 @@
                     {{ atom.atomCode ? atom.name : t("pendingAtom") }}
                 </span>
             </p>
-            <span class="atom-execounter" v-if="isExecuting">{{ execTime }}</span>
+            <template v-if="isExecuting">
+                <span class="atom-execounter">{{ execTime }}</span>
+                <span class="atom-execounter" v-if="Number.isInteger(atom.progressRate)">{{ Math.min(1, atom.progressRate) * 100 }}%</span>
+            </template>
             <Logo v-if="isBusy" name="circle-2-1" size="14" class="spin-icon" />
             <bk-popover :delay="[300, 0]" v-else-if="isReviewing" placement="top">
                 <span
--- src/frontend/devops-pipeline/src/components/StageSteps/index.vue
+++ src/frontend/devops-pipeline/src/components/StageSteps/index.vue
@@ -4,7 +4,7 @@
             <span v-if="step.tooltip" v-bk-tooltips="step.tooltip" :class="`stage-step ${step.statusCls}`" :key="step.icon">
                 <logo :class="`step-icon ${step.statusCls} ${getRunningCls(step.statusCls)}`" :name="step.icon" size="16" />
             </span>
-            <span v-else :class="`stage-step ${step.statusCls}`" :key="step.icon">
+            <span v-else v-bk-tooltips="progressTooltips" @click.stop="getProgress(step)" :class="`stage-step ${step.statusCls}`" :key="step.icon">
                 <logo :class="`step-icon ${step.statusCls} ${getRunningCls(step.statusCls)}`" :name="step.icon" size="16" />
             </span>
         </template>
@@ -13,6 +13,7 @@
 
 <script>
     import Logo from '@/components/Logo'
+    import { PROCESS_API_URL_PREFIX } from '@/store/constants'
     export default {
         name: 'stage-steps',
         components: {
@@ -22,11 +23,50 @@
             steps: {
                 type: Array,
                 default: () => []
+            },
+            buildId: {
+                type: String,
+                required: true
+            }
+        },
+        data () {
+            return {
+                progressTooltips: {
+                    html: '<i class="devops-icon icon-circle-2-1 spin-icon"/>',
+                    placement: 'bottom',
+                    trigger: 'click'
+                }
             }
         },
         methods: {
             getRunningCls (statusCls) {
                 return statusCls === 'RUNNING' ? ' spin-icon' : ''
+            },
+            async getProgress (step) {
+                try {
+                    console.log(step)
+
+                    const { projectId, pipelineId } = this.$route.params
+                    const { data } = await this.$ajax.get(`${PROCESS_API_URL_PREFIX}/user/builds/${projectId}/${pipelineId}/getStageProgressRate?buildId=${this.buildId}&stageId=${step.stageId}`)
+                    Object.assign(this.progressTooltips, {
+                        html: `
+                            <p>${this.$t('completeness')}${Math.min(1, data.stageProgressRete) * 100}%</p>
+                            <p>${this.$t('runningSteps')}</p>
+                            ${data.taskProgressList.map(item =>
+                                `<p style="text-indent: 12px">${[
+                                    `[${item.taskOrder}]`,
+                                    item.taskName,
+                                    `${Math.min(1, item.taskProgressRete) * 100}%`
+                                ].join('')}</p>`
+                            ).join('')
+                        }`
+                    })
+                } catch (error) {
+                    this.$showTips({
+                        theme: 'error',
+                        message: error.message
+                    })
+                }
             }
         }
     }
