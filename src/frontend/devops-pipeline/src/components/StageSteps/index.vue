f7f31fc8c8f756a60e09606ada1810af661835ce
lockiechen <lockiechen@tencent.com>
2024-05-16 10:02:36 +0800

    feat: 【蓝盾-产品-已评审】流水线支持展示运行进度 issue #7932

--- src/frontend/devops-pipeline/src/components/StageSteps/index.vue
+++ src/frontend/devops-pipeline/src/components/StageSteps/index.vue
@@ -4,7 +4,10 @@
             <span v-if="step.tooltip" v-bk-tooltips="step.tooltip" :class="`stage-step ${step.statusCls}`" :key="step.icon">
                 <logo :class="`step-icon ${step.statusCls} ${getRunningCls(step.statusCls)}`" :name="step.icon" size="16" />
             </span>
-            <span v-else v-bk-tooltips="progressTooltips(step)" :class="`stage-step ${step.statusCls}`" :key="step.icon">
+            <span v-else-if="step.status === 'RUNNING'" v-bk-tooltips="progressTooltips" :data-stageId="step.stageId" :class="`stage-step ${step.statusCls}`" :key="step.icon">
+                <logo :class="`step-icon ${step.statusCls} ${getRunningCls(step.statusCls)}`" :name="step.icon" size="16" />
+            </span>
+            <span v-else :class="`stage-step ${step.statusCls}`" :key="step.icon">
                 <logo :class="`step-icon ${step.statusCls} ${getRunningCls(step.statusCls)}`" :name="step.icon" size="16" />
             </span>
         </template>
@@ -13,7 +16,8 @@
 
 <script>
     import Logo from '@/components/Logo'
-import { PROCESS_API_URL_PREFIX } from '@/store/constants'
+    import { PROCESS_API_URL_PREFIX } from '@/store/constants'
+    import { isCancel } from 'axios'
     export default {
         name: 'stage-steps',
         components: {
@@ -29,6 +33,17 @@ import { PROCESS_API_URL_PREFIX } from '@/store/constants'
                 required: true
             }
         },
+        data () {
+            return {
+                progressTooltips: {
+                    content: '<i class="devops-icon icon-circle-2-1 spin-icon"/>',
+                    placement: 'bottom',
+                    delay: [500, 0],
+                    onShow: this.getProgress,
+                    onClose: this.hideTooltips
+                }
+            }
+        },
         methods: {
             getRunningCls (statusCls) {
                 return statusCls === 'RUNNING' ? ' spin-icon' : ''
@@ -38,18 +53,14 @@ import { PROCESS_API_URL_PREFIX } from '@/store/constants'
                 if (typeof progress !== 'number') return ''
                 return `${parseInt(Math.min(1, progress) * precision * 100, 10) / precision}%`
             },
-            progressTooltips (step) {
-                return {
-                    content: '<i class="devops-icon icon-circle-2-1 spin-icon"/>',
-                    placement: 'bottom',
-                    delay: [500, 0],
-                    onShow: () => this.getProgress(step)
-                }
-            },
-            async getProgress (step) {
+            async getProgress (ref) {
                 try {
+                    this.controller = new AbortController()
+                    const stageId = ref.reference.getAttribute('data-stageId')
                     const { projectId, pipelineId } = this.$route.params
-                    const { data } = await this.$ajax.get(`${PROCESS_API_URL_PREFIX}/user/builds/${projectId}/${pipelineId}/getStageProgressRate?buildId=${this.buildId}&stageId=${step.stageId}`)
+                    const { data } = await this.$ajax.get(`${PROCESS_API_URL_PREFIX}/user/builds/${projectId}/${pipelineId}/getStageProgressRate?buildId=${this.buildId}&stageId=${stageId}`, {
+                        signal: this.controller.signal
+                    })
                     this.progressTooltips = {
                         ...this.progressTooltips,
                         content: `<p>${this.$t('completeness')}${this.calcProgress(data.stageProgressRete)}</p>
@@ -65,12 +76,22 @@ import { PROCESS_API_URL_PREFIX } from '@/store/constants'
                                 }`
                         : ''}`
                     }
+
+                    console.log(1111, this.progressTooltips.content)
                 } catch (error) {
-                    this.$showTips({
-                        theme: 'error',
-                        message: error.message
-                    })
+                    console.log(error, isCancel(error))
+                    if (!isCancel(error)) {
+                        this.$showTips({
+                            theme: 'error',
+                            message: error.message
+                        })
+                    }
                 }
+            },
+            hideTooltips () {
+                this.controller?.abort?.()
+
+                this.progressTooltips.content = '<i class="devops-icon icon-circle-2-1 spin-icon"/>'
             }
         }
     }
