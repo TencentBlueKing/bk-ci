abf8d2bd788b7e91fc7cf4e7affbd491f6adb1c2
hejieehe <904696180qq.com>
2024-05-21 12:02:53 +0800

    feat：代码库触发事件结果展示优化 #10307

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineTriggerEventDao.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/dao/PipelineTriggerEventDao.kt
@@ -660,52 +660,36 @@ class PipelineTriggerEventDao {
     fun triggerReasonStatistics(
         dslContext: DSLContext,
         projectId: String,
-        eventId: Long? = null,
-        eventType: String? = null,
-        triggerType: String? = null,
-        triggerUser: String? = null,
+        eventId: Long,
         pipelineId: String? = null,
-        pipelineName: String? = null,
-        startTime: Long? = null,
-        endTime: Long? = null
+        pipelineName: String? = null
     ): PipelineTriggerReasonStatistics {
-        val t1 = T_PIPELINE_TRIGGER_EVENT.`as`("t1")
-        val t2 = T_PIPELINE_TRIGGER_DETAIL.`as`("t2")
-        val conditions = buildConditions(
-            t1 = t1,
-            t2 = t2,
-            projectId = projectId,
-            eventId = eventId,
-            eventType = eventType,
-            triggerUser = triggerUser,
-            triggerType = triggerType,
-            pipelineId = pipelineId,
-            pipelineName = pipelineName,
-            startTime = startTime,
-            endTime = endTime
-        )
-        return dslContext.select(
-            count(
-                `when`(t2.REASON.eq(PipelineTriggerReason.TRIGGER_SUCCESS.name), 1)
-            ).`as`("triggerSuccess"),
-            count(
-                `when`(t2.REASON.eq(PipelineTriggerReason.TRIGGER_FAILED.name), 1)
-            ).`as`("triggerFailed"),
-            count(
-                `when`(t2.REASON.eq(PipelineTriggerReason.TRIGGER_NOT_MATCH.name), 1)
-            ).`as`("triggerNotMatch")
-        )
-            .from(t2)
-            .leftJoin(t1)
-            .on(t1.EVENT_ID.eq(t2.EVENT_ID)).and(t1.PROJECT_ID.eq(t2.PROJECT_ID))
-            .where(conditions)
-            .groupBy(t2.EVENT_ID)
-            .fetchOne().let {
-                PipelineTriggerReasonStatistics(
-                    triggerSuccess = it?.value1() ?: 0,
-                    triggerFailed = it?.value2() ?: 0,
-                    triggerNotMatch = it?.value3() ?: 0
-                )
-            }
+        return with(T_PIPELINE_TRIGGER_DETAIL) {
+            dslContext.select(
+                REASON,
+                count()
+            ).from(this)
+                .where(
+                    EVENT_ID.eq(EVENT_ID).and(PROJECT_ID.eq(projectId)).let {
+                        if (!pipelineId.isNullOrBlank()) {
+                            it.and(PIPELINE_ID.eq(pipelineId))
+                        }
+                        if (!pipelineName.isNullOrBlank()) {
+                            PIPELINE_NAME.eq(pipelineName)
+                        }
+                        it
+                    }
+                ).groupBy(EVENT_ID)
+                .fetch()
+                .associate {
+                    it.value1() to it.value2()
+                }.let {
+                    PipelineTriggerReasonStatistics(
+                        triggerSuccess = it[PipelineTriggerReason.TRIGGER_SUCCESS.name] ?: 0,
+                        triggerFailed = it[PipelineTriggerReason.TRIGGER_FAILED.name] ?: 0,
+                        triggerNotMatch = it[PipelineTriggerReason.TRIGGER_NOT_MATCH.name] ?: 0
+                    )
+                }
+        }
     }
 }
