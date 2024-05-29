1056a914a84cfdb1b0abda6de7b58af3079ed2db
sawyersong <sawyersong@tencent.com>
2024-05-29 14:25:02 +0800

    feat：整合微拓展资源调度能力 #10122

--- src/backend/dispatch-k8s-manager/pkg/apiserver/apis/open_deployment.go
+++ src/backend/dispatch-k8s-manager/pkg/apiserver/apis/open_deployment.go
@@ -65,7 +65,8 @@ func createDeployment(c *gin.Context) {
 
 	deploymentInfo, err := kubeclient.GetDeployment(deployment.Name)
 	if err == nil || deploymentInfo == nil {
-		createErr := kubeclient.CreateNativeDeployment(deploymentInfo)
+		logs.Info(fmt.Sprintf("Deployment: %s not exist, create.", deployment.Name))
+		createErr := kubeclient.CreateNativeDeployment(deployment)
 		if createErr != nil {
 			fail(c, http.StatusInternalServerError, createErr)
 			return
