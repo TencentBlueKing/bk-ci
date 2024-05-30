936a66865eda58a5d05b20b9ae7088c958007b3f
lockiechen <lockiechen@tencent.com>
2024-05-30 14:35:45 +0800

    Merge remote-tracking branch 'github/master' into integration

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/AtomOption.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/AtomOption.vue
@@ -36,6 +36,9 @@
             atomVersion () {
                 return this.element.version
             },
+            atomOptionConfig () {
+                return this.atomPropsModel.config || {}
+            },
             optionModel () {
                 const model = { ...this.ATOM_OPTION }
                 const failControlManualRetryOption = {
