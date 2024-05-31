50a6d7951a4c6fa512793f6705a1a472eef56e74
hejieehe <904696180@qq.com>
2024-05-31 17:57:44 +0800

    feat：支持在父流水线中查看异步执行的子流水线的状态 #10260 补充SQL

--- support-files/sql/2003_v2.x/2020_ci_process-update_v2.0_mysql.sql
+++ support-files/sql/2003_v2.x/2020_ci_process-update_v2.0_mysql.sql
@@ -544,15 +544,6 @@ BEGIN
             ADD COLUMN `SECRET_PARAM` text DEFAULT NULL COMMENT '鉴权参数';
     END IF;
 
-    IF NOT EXISTS(SELECT 1
-                  FROM information_schema.COLUMNS
-                  WHERE TABLE_SCHEMA = db
-                    AND TABLE_NAME = 'T_PIPELINE_BUILD_RECORD_TASK'
-                    AND COLUMN_NAME = 'ASYNC_STATUS') THEN
-    ALTER TABLE `T_PIPELINE_BUILD_RECORD_TASK`
-        ADD COLUMN `ASYNC_STATUS` varchar(32) DEFAULT NULL COMMENT '插件异步执行状态';
-    END IF;
-
     COMMIT;
 END <CI_UBF>
 DELIMITER ;
