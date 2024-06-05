a71439a2bcce14424cd23c5d784d1f22f95e8007
royalhuang <royalhuang@tencent.com>
2024-06-05 17:05:57 +0800

    feat: 支持蓝盾新表达式运行条件 #10467 调整sql

--- support-files/sql/2002_v1.x/2010_ci_process-update_v1.10_mysql.sql
+++ support-files/sql/2002_v1.x/2010_ci_process-update_v1.10_mysql.sql
@@ -10,26 +10,18 @@ BEGIN
 
     DECLARE db VARCHAR(100);
     SET AUTOCOMMIT = 0;
-    SELECT DATABASE() INTO db;
+SELECT DATABASE() INTO db;
 
 IF NOT EXISTS(SELECT 1
                   FROM information_schema.statistics
                   WHERE TABLE_SCHEMA = db
                     AND TABLE_NAME = 'T_AUDIT_RESOURCE'
                     AND INDEX_NAME = 'IDX_SEARCH_ID') THEN
-        alter table T_AUDIT_RESOURCE ADD INDEX
-            IDX_SEARCH_ID (`RESOURCE_TYPE`, `PROJECT_ID`, `RESOURCE_ID`);
-    END IF;
+alter table T_AUDIT_RESOURCE ADD INDEX
+    IDX_SEARCH_ID (`RESOURCE_TYPE`, `PROJECT_ID`, `RESOURCE_ID`);
+END IF;
 
-	IF NOT EXISTS(SELECT 1
-            FROM information_schema.COLUMNS
-            WHERE TABLE_SCHEMA = db
-                AND TABLE_NAME = 'T_PIPELINE_RESOURCE_VERSION'
-                AND COLUMN_NAME = 'UPDATER') THEN
-        ALTER TABLE T_PIPELINE_RESOURCE_VERSION ADD `UPDATER` varchar(64) DEFAULT NULL COMMENT '最近更新人';
-    END IF;
-
-    COMMIT;
+COMMIT;
 END <CI_UBF>
 DELIMITER ;
 COMMIT;
