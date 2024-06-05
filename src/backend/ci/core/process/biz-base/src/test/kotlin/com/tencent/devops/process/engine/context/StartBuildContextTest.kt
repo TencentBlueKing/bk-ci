1a8cbaa2ddfcccb49b208dd723939cfdac99e08c
royalhuang <royalhuang@tencent.com>
2024-06-05 10:16:33 +0800

    feat: 支持蓝盾新表达式运行条件 #10467 修复部分传参逻辑

--- src/backend/ci/core/process/biz-base/src/test/kotlin/com/tencent/devops/process/engine/context/StartBuildContextTest.kt
+++ src/backend/ci/core/process/biz-base/src/test/kotlin/com/tencent/devops/process/engine/context/StartBuildContextTest.kt
@@ -27,7 +27,6 @@
 
 package com.tencent.devops.process.engine.context
 
-import com.tencent.devops.common.pipeline.Model
 import com.tencent.devops.common.pipeline.enums.BuildStatus
 import com.tencent.devops.common.pipeline.enums.ChannelCode
 import com.tencent.devops.common.pipeline.enums.StartType
