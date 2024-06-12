3bc9fb285879f8cf7b90be6dfd7ff0dbb231b215
randychen <randychen@tencent.com>
2024-06-12 15:40:23 +0800

    --story=117952833 【云桌面】实例操作判断是否项目成员权限改成新接口，不依赖组权限

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/WorkspaceService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/WorkspaceService.kt
@@ -630,7 +630,7 @@ class WorkspaceService @Autowired constructor(
                 regionId = detail?.regionId.toString(),
                 innerIp = detail?.hostIP,
                 createTime = DateTimeUtil.toDateTime(res["CREATE_TIME"] as LocalDateTime),
-                owner = owners[name],
+                owner = res["SHARED_USER"] as? String ?: res["CREATOR"] as String,
                 realOwner = owner,
                 status = WorkspaceStatus.values()[res["STATUS"] as Int],
                 displayName = res["DISPLAY_NAME"] as String,
