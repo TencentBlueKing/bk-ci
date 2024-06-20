67e554a996daef4165397a2f2ad029ba2c657399
greysonfang <greysonfang@tencent.com>
2024-06-20 16:21:39 +0800

    feat：项目停用后，定期清理项目下占资源的数据 #10370

--- src/backend/ci/core/misc/biz-misc/src/main/kotlin/com/tencent/devops/misc/pojo/PlatformDataClearEnum.kt
+++
@@ -1,9 +0,0 @@
-package com.tencent.devops.misc.pojo
-
-enum class PlatformDataClearEnum {
-    // 清除流水线制品库构建数据
-    CLEAR_PIPELINE_REPOSITORY_BUILD_DATA,
-
-    // 清除项目制品库数据
-    CLEAR_PROJECT_REPOSITORY_DATA
-}
