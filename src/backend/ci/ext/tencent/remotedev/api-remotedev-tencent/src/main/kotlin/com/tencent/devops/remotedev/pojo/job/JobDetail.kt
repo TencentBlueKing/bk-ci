8bc415719ac1a1192852ea9ad1bac715693c14bf
ruotiantang <ruotiantang@tencent.com>
2024-06-26 15:10:16 +0800

    --story=117787334 【云桌面】优化一键喊话执行信息提示

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/job/JobDetail.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/job/JobDetail.kt
@@ -1,5 +1,6 @@
 package com.tencent.devops.remotedev.pojo.job
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class JobDetail(
@@ -9,18 +10,21 @@ data class JobDetail(
 
 interface JobActionDetail
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(title = "流水线任务详情")
 data class JobPipelineDetail(
     @get:Schema(title = "标准运维任务详情")
     var sopData: List<JobPipelineSopDetail>?
 ) : JobActionDetail
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(title = "流水线任务中标准运维任务详情")
 data class JobPipelineSopDetail(
     @get:Schema(title = "job任务详情")
-    val jobTask: JobPipelineSopJobTask?
+    val jobTasks: List<JobPipelineSopJobTask>?
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(title = "流水线任务中标准运维任务中Job任务详情")
 data class JobPipelineSopJobTask(
     @get:Schema(title = "执行成功的机器")
@@ -29,6 +33,7 @@ data class JobPipelineSopJobTask(
     val failedIps: List<JobPipelineSopJobIpData>?
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(title = "流水线任务中标准运维任务中Job任务Ip执行数据")
 data class JobPipelineSopJobIpData(
     @get:Schema(title = "机器IP")
