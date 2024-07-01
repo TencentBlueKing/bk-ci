5059d0be1d77203de45615cf143faf6df8d47911
hejieehe <904696180@qq.com>
2024-07-01 14:40:19 +0800

    feat：P4触发器支持 Code 编写 #10551

--- src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/v3/models/on/PreTriggerOnV3.kt
+++ src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/v3/models/on/PreTriggerOnV3.kt
@@ -56,19 +56,19 @@ data class PreTriggerOnV3(
     override val remote: Any? = null,
     @JsonProperty("change-commit")
     @get:Schema(title = "change-commit")
-    override var changeCommit: PushRule? = null,
+    override var changeCommit: Any? = null,
     @JsonProperty("change-content")
     @get:Schema(title = "change-content")
-    override var changeContent: PushRule? = null,
+    override var changeContent: Any? = null,
     @JsonProperty("change-submit")
     @get:Schema(title = "change-submit")
-    override var changeSubmit: PushRule? = null,
+    override var changeSubmit: Any? = null,
     @JsonProperty("shelve-commit")
     @get:Schema(title = "shelve-commit")
-    override var shelveCommit: PushRule? = null,
+    override var shelveCommit: Any? = null,
     @JsonProperty("shelve-submit")
     @get:Schema(title = "shelve-submit")
-    override var shelveSubmit: PushRule? = null
+    override var shelveSubmit: Any? = null
 ) : IPreTriggerOn {
     override fun yamlVersion() = YamlVersion.V3_0
 }
