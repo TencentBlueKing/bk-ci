5b0719c5183d2bf85082a5efc65b2abfd1a3706e
jsonwan <jsonwan@tencent.com>
2024-07-04 12:20:05 +0800

    --story=117513038 feat：部署节点支持手动安装 GSE Agent
    获取手动安装命令接口提供网络策略文档链接

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/config/NodeManProperties.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/config/NodeManProperties.kt
@@ -28,7 +28,9 @@
 package com.tencent.devops.environment.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "environment.nodeman")
 data class NodeManProperties(
     /**
