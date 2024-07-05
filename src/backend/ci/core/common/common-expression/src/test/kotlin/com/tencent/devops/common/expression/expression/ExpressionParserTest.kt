2aee8836d8913c70ddec7cb834daf354a9342252
ruotiantang <ruotiantang@tencent.com>
2024-07-05 19:04:08 +0800

    perf: 表达式解析器增加对流水线变量处理的兼容 #10609

--- src/backend/ci/core/common/common-expression/src/test/kotlin/com/tencent/devops/common/expression/expression/ExpressionParserTest.kt
+++ src/backend/ci/core/common/common-expression/src/test/kotlin/com/tencent/devops/common/expression/expression/ExpressionParserTest.kt
@@ -27,7 +27,6 @@
 
 package com.tencent.devops.common.expression.expression
 
-import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.common.expression.ExecutionContext
 import com.tencent.devops.common.expression.ExpressionException
 import com.tencent.devops.common.expression.ExpressionParseException
