6eff894ecd97f90b941dfbb91436ab58e4b34960
v_hwweng <v_hwweng@tencent.com>
2024-07-10 17:47:14 +0800

    --bug=126558611 修改组织和运营产品报错问题 # Reviewed, transaction id: 11821

--- src/frontend/devops-manage/src/views/manage/project/edit-project.vue
+++ src/frontend/devops-manage/src/views/manage/project/edit-project.vue
@@ -101,7 +101,8 @@ const infoBoxInstance = ref();
 const updateProject = async () => {
   infoBoxInstance.value?.hide();
   btnLoading.value = true;
-  productIdChange({
+  await fetchOperationalList(projectData.value.bgName);
+  await checkProductIdName({
     id: projectData.value?.productId,
     list: operationalList.value,
   });
@@ -149,8 +150,8 @@ const fetchOperationalList = async (bgName) => {
   }));
 };
 
-const productIdChange = ({ id, list }) => {
-  projectData.value.productName = list.find(i => i.ProductId === id)?.ProductName;
+const checkProductIdName = ({ id, list }) => {
+  projectData.value.productName = list.find(i => i.ProductId === id)?.ProductName || '';
 };
 
 const showNeedApprovedTips = () => {
@@ -196,7 +197,6 @@ const handleNoPermission = () => {
 
 onMounted(async () => {
   await fetchProjectData();
-  await fetchOperationalList(projectData.value.bgName);
 });
 </script>
 
