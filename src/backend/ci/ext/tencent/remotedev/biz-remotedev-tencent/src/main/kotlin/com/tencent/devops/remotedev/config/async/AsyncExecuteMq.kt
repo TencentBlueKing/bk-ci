e42950ab53ac898a83e3ccc0fd15ae97e04c8efe
stubenhuang <stubenhuang@tencent.com>
2024-07-17 17:18:58 +0800

    feat: 引擎等MQ场景接入SCS框架 #7443

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/config/async/AsyncExecuteMq.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/config/async/AsyncExecuteMq.kt
@@ -5,7 +5,6 @@ import com.tencent.devops.common.stream.ScsConsumerBuilder
 import com.tencent.devops.remotedev.listener.AsyncExecuteListener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
 
 /**
  * remotedev 服务内作为更安全的线程池的异步实现方式
@@ -13,7 +12,7 @@ import org.springframework.context.annotation.Lazy
 @Configuration
 class AsyncExecuteMq {
     @EventConsumer
-    fun asyncExecuteListener(
-        @Lazy @Autowired asyncExecuteListener: AsyncExecuteListener
+    fun asyncExecute(
+        @Autowired asyncExecuteListener: AsyncExecuteListener
     ) = ScsConsumerBuilder.build<AsyncExecuteEvent> { asyncExecuteListener.listenAsyncExecuteEvent(it) }
 }
