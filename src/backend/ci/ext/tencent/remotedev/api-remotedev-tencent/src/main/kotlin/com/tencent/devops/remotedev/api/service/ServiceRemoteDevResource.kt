a51e2d942024d9f06ae55a6f16537599037ab976
yongyiduan(段永億) <yongyiduan@tencent.com>
2024-07-19 12:11:52 +0800

    --story=117609432 openapi：后台接口生存dtoken

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/service/ServiceRemoteDevResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/service/ServiceRemoteDevResource.kt
@@ -1,5 +1,6 @@
 package com.tencent.devops.remotedev.api.service
 
+import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_BK_TOKEN
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Page
@@ -7,6 +8,7 @@ import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.remotedev.pojo.DesktopTokenSign
 import com.tencent.devops.remotedev.pojo.OperateCvmData
 import com.tencent.devops.remotedev.pojo.ProjectWorkspaceAssign
+import com.tencent.devops.remotedev.pojo.UserOnePassword
 import com.tencent.devops.remotedev.pojo.WindowsResourceTypeConfig
 import com.tencent.devops.remotedev.pojo.WindowsResourceZoneConfigType
 import com.tencent.devops.remotedev.pojo.WindowsWorkspaceCreate
@@ -56,6 +58,18 @@ interface ServiceRemoteDevResource {
         @QueryParam("ticket")
         ticket: String
     ): Result<Boolean>
+
+    @Operation(summary = "校验token")
+    @GET
+    @Path("/desktop_token_check")
+    fun desktopTokenCheck(
+        @HeaderParam(AUTH_HEADER_DEVOPS_BK_TOKEN)
+        @Parameter(description = "认证token", required = true)
+        token: String,
+        @QueryParam("dToken")
+        @Parameter(description = "dToken", required = false)
+        dToken: String
+    ): Result<UserOnePassword>
 
     @Operation(summary = "提供给wesec获取项目下批量云桌面信息")
     @GET
