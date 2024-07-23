845d9d25b3ca6425c9ebeb2d1534412d7df727e7
lockiechen <lockiechen@tencent.com>
2024-07-23 14:27:48 +0800

    feat: update lerna +yarn workspace to pnpm issue #8125

--- src/frontend/devops-pipeline/src/App.vue
+++ src/frontend/devops-pipeline/src/App.vue
@@ -14,6 +14,7 @@
     export default {
         name: 'App',
         data () {
+            console.log(222)
             return {
                 stayCurrentPage: ['atomDebug', '']
             }
