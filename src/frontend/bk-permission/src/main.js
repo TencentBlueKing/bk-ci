93c57d2b6c7a779a0cf0c0cc23d150ba20d8dfb3
xuzhan <xuzhan@tencent.com>
2024-07-30 14:27:55 +0800

    feat：支持管理员查看项目成员 #9620 # Reviewed, transaction id: 13454

--- src/frontend/bk-permission/src/main.js
+++ src/frontend/bk-permission/src/main.js
@@ -1,10 +1,11 @@
-
 import BkPermission from './components/permission-main.vue';
 import PermissionDirective from './directive/authority-directive';
 import { AuthorityDirectiveV3 } from './directive/authority-directive';
 import handleNoPermission from './function/permission';
 import { handleNoPermissionV3 } from './function/permission';
 import { loadI18nMessages } from './utils/locale';
+import './css/index.css';
+
 
 function install (Vue, opts = {}) {
   loadI18nMessages(opts.i18n)
