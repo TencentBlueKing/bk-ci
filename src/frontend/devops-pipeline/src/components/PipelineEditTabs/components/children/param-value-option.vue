0ebc5cd8c269a6a32895f5c003dc5fab8504d98c
lockiechen <lockiechen@tencent.com>
2024-07-30 15:49:06 +0800

    feat: 调试记录提示和入口优化 #10720

--- src/frontend/devops-pipeline/src/components/PipelineEditTabs/components/children/param-value-option.vue
+++ src/frontend/devops-pipeline/src/components/PipelineEditTabs/components/children/param-value-option.vue
@@ -7,11 +7,12 @@
             :handle-update-payload="handleUpdatePayload"
             :reset-default-val="handleResetDefaultVal"
         />
-        
+
         <form-field
             :hide-colon="true"
             :label="valueRequired ? $t('newui.pipelineParam.constValue') : $t(`editPage.${getParamsDefaultValueLabel(param.type)}`)"
-            :required="valueRequired" :is-error="errors.has(`pipelineParam.defaultValue`)"
+            :required="valueRequired"
+            :is-error="errors.has(`pipelineParam.defaultValue`)"
             :error-msg="errors.first(`pipelineParam.defaultValue`)"
             :desc="valueRequired ? undefined : $t(`editPage.${getParamsDefaultValueLabelTips(param.type)}`)"
         >
@@ -51,14 +52,14 @@
                 name="defaultValue"
                 :list="boolList"
                 :disabled="disabled"
-                    
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
-                :value="param.defaultValue">
+                :handle-change="handleChange"
+                :value="param.defaultValue"
+            >
             </enum-input>
             <vuex-input
                 v-if="isStringParam(param.type) || isSvnParam(param.type) || isGitParam(param.type) || isArtifactoryParam(param.type)"
                 :disabled="disabled"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 name="defaultValue"
                 v-validate="{ required: valueRequired }"
                 :data-vv-scope="'pipelineParam'"
@@ -73,12 +74,12 @@
                 :disabled="disabled"
                 :value="param.defaultValue"
                 :upload-file-name="uploadFileName"
-                :handle-change="(name, value) => handleChange(name, value)"
+                :handle-change="handleChange"
             />
             <vuex-textarea
                 v-if="isTextareaParam(param.type)"
                 :disabled="disabled"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 name="defaultValue"
                 v-validate="{ required: valueRequired }"
                 :data-vv-scope="'pipelineParam'"
@@ -95,7 +96,8 @@
                 v-validate="{ required: valueRequired }"
                 :data-vv-scope="'pipelineParam'"
                 :value="param.defaultValue"
-                :handle-change="(name, value) => handleUpdateParam(name, value)">
+                :handle-change="handleChange"
+            >
             </request-selector>
             <request-selector
                 v-if="isBuildResourceParam(param.type)"
@@ -107,9 +109,10 @@
                 v-validate="{ required: valueRequired }"
                 :data-vv-scope="'pipelineParam'"
                 :value="param.defaultValue"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 :replace-key="param.replaceKey"
-                :search-url="param.searchUrl">
+                :search-url="param.searchUrl"
+            >
             </request-selector>
             <request-selector
                 v-if="isSubPipelineParam(param.type)"
@@ -120,59 +123,129 @@
                 v-validate="{ required: valueRequired }"
                 :data-vv-scope="'pipelineParam'"
                 :value="param.defaultValue"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 :replace-key="param.replaceKey"
-                :search-url="param.searchUrl">
+                :search-url="param.searchUrl"
+            >
             </request-selector>
         </form-field>
 
-        <form-field :hide-colon="true" v-if="isSvnParam(param.type)" :label="$t('editPage.svnParams')" :is-error="errors.has(`repoHashId`)" :error-msg="errors.first(`pipelineParam.repoHashId`)">
+        <form-field
+            :hide-colon="true"
+            v-if="isSvnParam(param.type)"
+            :label="$t('editPage.svnParams')"
+            :is-error="errors.has(`repoHashId`)"
+            :error-msg="errors.first(`pipelineParam.repoHashId`)"
+        >
             <request-selector
                 v-bind="getRepoOption('CODE_SVN')"
                 :disabled="disabled"
                 name="repoHashId"
                 :value="param.repoHashId"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 v-validate="'required'"
                 :data-vv-scope="'pipelineParam'"
                 :replace-key="param.replaceKey"
-                :search-url="param.searchUrl">
+                :search-url="param.searchUrl"
+            >
             </request-selector>
         </form-field>
 
-        <form-field :hide-colon="true" v-if="isSvnParam(param.type)" :label="$t('editPage.relativePath')" :is-error="errors.has(`relativePath`)" :error-msg="errors.first(`pipelineParam.relativePath`)">
-            <vuex-input :disabled="disabled" :handle-change="(name, value) => handleUpdateParam(name, value)" name="relativePath" :placeholder="$t('editPage.relativePathTips')" :value="param.relativePath"></vuex-input>
+        <form-field
+            :hide-colon="true"
+            v-if="isSvnParam(param.type)"
+            :label="$t('editPage.relativePath')"
+            :is-error="errors.has(`relativePath`)"
+            :error-msg="errors.first(`pipelineParam.relativePath`)"
+        >
+            <vuex-input
+                :disabled="disabled"
+                :handle-change="handleChange"
+                name="relativePath"
+                :placeholder="$t('editPage.relativePathTips')"
+                :value="param.relativePath"
+            ></vuex-input>
         </form-field>
 
-        <form-field :hide-colon="true" v-if="isGitParam(param.type)" :label="$t('editPage.gitRepo')" :is-error="errors.has(`repoHashId`)" :error-msg="errors.first(`pipelineParam.repoHashId`)">
+        <form-field
+            :hide-colon="true"
+            v-if="isGitParam(param.type)"
+            :label="$t('editPage.gitRepo')"
+            :is-error="errors.has(`repoHashId`)"
+            :error-msg="errors.first(`pipelineParam.repoHashId`)"
+        >
             <request-selector
                 v-bind="getRepoOption('CODE_GIT,CODE_GITLAB,GITHUB,CODE_TGIT')"
                 :disabled="disabled"
                 name="repoHashId"
                 :value="param.repoHashId"
-                :handle-change="(name, value) => handleUpdateParam(name, value)"
+                :handle-change="handleChange"
                 v-validate="'required'"
                 :data-vv-scope="'pipelineParam'"
                 replace-key="{keyword}"
-                :search-url="getSearchUrl()">
+                :search-url="getSearchUrl()"
+            >
             </request-selector>
         </form-field>
 
-        <form-field :hide-colon="true" v-if="isCodelibParam(param.type)" :label="$t('editPage.codelibParams')" :is-error="errors.has(`scmType`)" :error-msg="errors.first(`pipelineParam.scmType`)">
-            <selector :disabled="disabled" :list="codeTypeList" :handle-change="(name, value) => handleCodeTypeChange(name, value)" name="scmType" placeholder="" :value="param.scmType"></selector>
+        <form-field
+            :hide-colon="true"
+            v-if="isCodelibParam(param.type)"
+            :label="$t('editPage.codelibParams')"
+            :is-error="errors.has(`scmType`)"
+            :error-msg="errors.first(`pipelineParam.scmType`)"
+        >
+            <selector
+                :disabled="disabled"
+                :list="codeTypeList"
+                :handle-change="(name, value) => handleCodeTypeChange(name, value)"
+                name="scmType"
+                placeholder=""
+                :value="param.scmType"
+            ></selector>
         </form-field>
 
         <template v-if="isBuildResourceParam(param.type)">
-            <form-field :hide-colon="true" :label="$t('editPage.buildEnv')" :is-error="errors.has(`os`)" :error-msg="errors.first(`pipelineParam.os`)">
-                <selector :popover-min-width="510" :disabled="disabled" :list="baseOSList" :handle-change="(name, value) => handleBuildResourceChange(name, value, param)" name="os" placeholder="" :value="param.containerType.os"></selector>
+            <form-field
+                :hide-colon="true"
+                :label="$t('editPage.buildEnv')"
+                :is-error="errors.has(`os`)"
+                :error-msg="errors.first(`pipelineParam.os`)"
+            >
+                <selector
+                    :popover-min-width="510"
+                    :disabled="disabled"
+                    :list="baseOSList"
+                    :handle-change="(name, value) => handleBuildResourceChange(name, value, param)"
+                    name="os"
+                    placeholder=""
+                    :value="param.containerType.os"
+                ></selector>
             </form-field>
 
-            <form-field :hide-colon="true" :label="$t('editPage.addMetaData')" :is-error="errors.has(`buildType`)" :error-msg="errors.first(`pipelineParam.buildType`)">
-                <selector :popover-min-width="510" :disabled="disabled" :list="getBuildTypeList(param.containerType.os)" setting-key="type" :handle-change="(name, value) => handleBuildResourceChange(name, value, param)" name="buildType" placeholder="" :value="param.containerType.buildType"></selector>
+            <form-field
+                :hide-colon="true"
+                :label="$t('editPage.addMetaData')"
+                :is-error="errors.has(`buildType`)"
+                :error-msg="errors.first(`pipelineParam.buildType`)"
+            >
+                <selector
+                    :popover-min-width="510"
+                    :disabled="disabled"
+                    :list="getBuildTypeList(param.containerType.os)"
+                    setting-key="type"
+                    :handle-change="(name, value) => handleBuildResourceChange(name, value, param)"
+                    name="buildType"
+                    placeholder=""
+                    :value="param.containerType.buildType"
+                ></selector>
             </form-field>
         </template>
 
-        <form-field :hide-colon="true" v-if="isFileParam(param.type)">
+        <form-field
+            :hide-colon="true"
+            v-if="isFileParam(param.type)"
+        >
             <file-upload
                 name="fileName"
                 :file-path="param.defaultValue"
@@ -319,6 +392,9 @@
             isFileParam,
             getParamsDefaultValueLabel,
             getParamsDefaultValueLabelTips,
+            handleUpdateParam (name, value) {
+                this.handleChange(name, value)
+            },
             isSelectorParam (type) {
                 return isMultipleParam(type) || isEnumParam(type)
             },
@@ -407,7 +483,7 @@
             handleUpdateOptions (key, val) {
                 this.handleChange(key, val)
                 this.transformOpt(val)
-                
+
                 // 选项变更后，重制默认值
                 const { param } = this
                 if (typeof param.defaultValue === 'string' && (isMultipleParam(param.type) || isEnumParam(param.type))) { // 选项清除时，修改对应的默认值
