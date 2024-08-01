57f8791510d8ccf213d5f4d5ad4cfe0a35f5e620
mingshewhe <mingshewhe@tencent.com>
2024-08-01 20:46:24 +0800

    feat：新建/编辑流水线时支持调试流水线 #8164 修复构建历史区分源材料分支和触发分支搜索
    1. 构建历史支持源材料和触发材料搜索
    2. 触发分支和源材料分支支持下来搜索

--- src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildDao.kt
+++ src/backend/ci/core/process/biz-base/src/main/kotlin/com/tencent/devops/process/engine/dao/PipelineBuildDao.kt
@@ -73,7 +73,7 @@ class PipelineBuildDao {
     companion object {
         private val mapper = PipelineBuildInfoJooqMapper()
         private val debugMapper = PipelineDebugBuildInfoJooqMapper()
-        private const val DEFAULT_PAGE_SIZE = 10
+        private const val DEFAULT_PAGE_SIZE = 100
     }
 
     fun create(dslContext: DSLContext, startBuildContext: StartBuildContext) {
