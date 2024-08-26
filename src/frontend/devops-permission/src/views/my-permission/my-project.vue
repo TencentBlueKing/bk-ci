a3157cbc2cdb934b7838345e47e3f33b3e08a66c
v_hwweng <v_hwweng@tencent.com>
2024-08-26 19:55:24 +0800

    feat: Permission服务-项目成员管理 #9620

--- src/frontend/devops-permission/src/views/my-permission/my-project.vue
+++ src/frontend/devops-permission/src/views/my-permission/my-project.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
+import tools from '@/utils/tools';
 const urlPerfix = location.origin
 const baseUrl = `${urlPerfix}/manage/userManage`
 const route = useRoute();
-const projectCode = route?.params.projectCode || route?.query.projectCode || route?.query.project_code
+const projectCode = route?.params.projectCode || route?.query.projectCode || route?.query.project_code || tools.getCookie('X-DEVOPS-PROJECT-ID') || '';
 const iframeUrl = `${baseUrl}?projectCode=${projectCode}&disableSyncUrl=true&showSelectProject=true`
 </script>
 
