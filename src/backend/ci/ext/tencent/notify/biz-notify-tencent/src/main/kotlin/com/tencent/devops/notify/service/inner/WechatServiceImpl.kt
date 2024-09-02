5bdceb712319300acd7c386bb280c9310fc109c7
irwinsun <irwinsun@tencent.com>
2024-09-02 21:56:49 +0800

    --task=75281257 停服超1年时间+代码逻辑清理：【发通知-微信】

--- src/backend/ci/ext/tencent/notify/biz-notify-tencent/src/main/kotlin/com/tencent/devops/notify/service/inner/WechatServiceImpl.kt
+++ src/backend/ci/ext/tencent/notify/biz-notify-tencent/src/main/kotlin/com/tencent/devops/notify/service/inner/WechatServiceImpl.kt
@@ -26,30 +26,17 @@
  */
 package com.tencent.devops.notify.service.inner
 
-import com.google.common.collect.Lists
-import com.google.common.collect.Sets
 import com.tencent.devops.common.api.util.DateTimeUtil
-import com.tencent.devops.common.api.util.UUIDUtil
 import com.tencent.devops.common.notify.enums.EnumNotifyPriority
 import com.tencent.devops.common.notify.enums.EnumNotifySource
-import com.tencent.devops.common.notify.pojo.WechatNotifyPost
-import com.tencent.devops.common.notify.utils.NotifyDigestUtils
-import com.tencent.devops.common.notify.utils.TOFConfiguration
-import com.tencent.devops.common.notify.utils.TOFService
 import com.tencent.devops.model.notify.tables.records.TNotifyWechatRecord
-import com.tencent.devops.notify.EXCHANGE_NOTIFY
-import com.tencent.devops.notify.ROUTE_WECHAT
 import com.tencent.devops.notify.dao.WechatNotifyDao
 import com.tencent.devops.notify.model.WechatNotifyMessageWithOperation
 import com.tencent.devops.notify.pojo.NotificationResponse
 import com.tencent.devops.notify.pojo.NotificationResponseWithPage
 import com.tencent.devops.notify.pojo.WechatNotifyMessage
 import com.tencent.devops.notify.service.WechatService
-import com.tencent.devops.common.notify.utils.TOFService.Companion.WECHAT_URL
-import org.slf4j.LoggerFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
@@ -58,168 +45,18 @@ import java.util.stream.Collectors
 @Primary
 @Service
 class WechatServiceImpl @Autowired constructor(
-    private val tofService: TOFService,
     private val wechatNotifyDao: WechatNotifyDao,
-    private val rabbitTemplate: RabbitTemplate,
-    private val tofConfiguration: TOFConfiguration
 ) : WechatService {
 
-    private val logger = LoggerFactory.getLogger(WechatServiceImpl::class.java)
-
-    @Value("\${tof.defaultSystem.default-wechat-sender}")
-    private lateinit var defaultWechatSender: String
-
     override fun sendMqMsg(message: WechatNotifyMessage) {
-        rabbitTemplate.convertAndSend(EXCHANGE_NOTIFY, ROUTE_WECHAT, message)
+        // 微信告警平台已于2021年6月30日下线了
     }
 
     /**
      * 发送短信消息
      * @param wechatNotifyMessageWithOperation 消息对象
      */
-    override fun sendMessage(wechatNotifyMessageWithOperation: WechatNotifyMessageWithOperation) {
-        val wechatNotifyPost = generateWechatNotifyPost(wechatNotifyMessageWithOperation)
-        if (wechatNotifyPost == null) {
-            logger.warn("WechatNotifyPost is empty after being processed, $wechatNotifyMessageWithOperation")
-            return
-        }
-
-        val retryCount = wechatNotifyMessageWithOperation.retryCount
-        val id = wechatNotifyMessageWithOperation.id ?: UUIDUtil.generate()
-        val tofConfs = tofConfiguration.getConfigurations(wechatNotifyMessageWithOperation.tofSysId)
-        val result = tofService.post(
-            url = WECHAT_URL,
-            postData = wechatNotifyPost,
-            tofConf = tofConfs!!
-        )
-        if (result.Ret == 0) {
-            // 成功
-            wechatNotifyDao.insertOrUpdateWechatNotifyRecord(
-                success = true,
-                source = wechatNotifyMessageWithOperation.source,
-                id = id,
-                retryCount = retryCount,
-                lastErrorMessage = null,
-                receivers = wechatNotifyPost.receiver,
-                sender = wechatNotifyPost.sender,
-                body = wechatNotifyPost.msgInfo,
-                priority = wechatNotifyPost.priority.toInt(),
-                contentMd5 = wechatNotifyPost.contentMd5,
-                frequencyLimit = wechatNotifyPost.frequencyLimit,
-                tofSysId = tofConfs["sys-id"],
-                fromSysId = wechatNotifyPost.fromSysId
-            )
-        } else {
-            // 写入失败记录
-            wechatNotifyDao.insertOrUpdateWechatNotifyRecord(
-                success = false,
-                source = wechatNotifyMessageWithOperation.source,
-                id = id,
-                retryCount = retryCount,
-                lastErrorMessage = result.ErrMsg,
-                receivers = wechatNotifyPost.receiver,
-                sender = wechatNotifyPost.sender,
-                body = wechatNotifyPost.msgInfo,
-                priority = wechatNotifyPost.priority.toInt(),
-                contentMd5 = wechatNotifyPost.contentMd5,
-                frequencyLimit = wechatNotifyPost.frequencyLimit,
-                tofSysId = tofConfs["sys-id"],
-                fromSysId = wechatNotifyPost.fromSysId
-            )
-            if (retryCount < 3) {
-                // 开始重试
-                reSendMessage(
-                    post = wechatNotifyPost,
-                    source = wechatNotifyMessageWithOperation.source,
-                    retryCount = retryCount + 1,
-                    id = id
-                )
-            }
-        }
-    }
-
-    private fun reSendMessage(
-        post: WechatNotifyPost,
-        source: EnumNotifySource,
-        retryCount: Int,
-        id: String
-    ) {
-        val wechatNotifyMessageWithOperation = WechatNotifyMessageWithOperation()
-        wechatNotifyMessageWithOperation.apply {
-            this.id = id
-            this.retryCount = retryCount
-            this.source = source
-            sender = post.sender
-            addAllReceivers(Sets.newHashSet(post.receiver.split(",")))
-            priority = EnumNotifyPriority.parse(post.priority)
-            body = post.msgInfo
-            frequencyLimit = post.frequencyLimit
-            tofSysId = post.tofSysId
-            fromSysId = post.fromSysId
-        }
-
-        rabbitTemplate.convertAndSend(EXCHANGE_NOTIFY, ROUTE_WECHAT, wechatNotifyMessageWithOperation) { message ->
-            var delayTime = 0
-            when (retryCount) {
-                1 -> delayTime = 30000
-                2 -> delayTime = 120000
-                3 -> delayTime = 300000
-            }
-            if (delayTime > 0) {
-                message.messageProperties.setHeader("x-delay", delayTime)
-            }
-            message
-        }
-    }
-
-    private fun generateWechatNotifyPost(wechatNotifyMessage: WechatNotifyMessage): WechatNotifyPost? {
-        val contentMd5 = NotifyDigestUtils.getMessageContentMD5("", wechatNotifyMessage.body)
-        val receivers = Lists.newArrayList(
-            filterReceivers(
-                receivers = wechatNotifyMessage.getReceivers(),
-                contentMd5 = contentMd5,
-                frequencyLimit = wechatNotifyMessage.frequencyLimit
-            )
-        )
-        if (receivers == null || receivers.isEmpty()) {
-            return null
-        }
-
-        val post = WechatNotifyPost()
-        post.apply {
-            receiver = wechatNotifyMessage.getReceivers().joinToString(",")
-            msgInfo = wechatNotifyMessage.body
-            priority = wechatNotifyMessage.priority.getValue()
-            sender = wechatNotifyMessage.sender.ifEmpty { defaultWechatSender }
-            this.contentMd5 = contentMd5
-            frequencyLimit = wechatNotifyMessage.frequencyLimit
-            tofSysId = wechatNotifyMessage.tofSysId
-            fromSysId = wechatNotifyMessage.fromSysId
-        }
-
-        return post
-    }
-
-    private fun filterReceivers(receivers: Set<String>, contentMd5: String, frequencyLimit: Int): Set<String> {
-        val filteredReceivers = HashSet(receivers)
-        val filteredOutReceivers = HashSet<String>()
-        if (frequencyLimit > 0) {
-            val recordedReceivers = wechatNotifyDao.getReceiversByContentMd5AndTime(
-                contentMd5, (frequencyLimit * 60).toLong()
-            )
-            receivers.forEach { rec ->
-                for (recordedRec in recordedReceivers) {
-                    if (",$recordedRec,".contains(rec)) {
-                        filteredReceivers.remove(rec)
-                        filteredOutReceivers.add(rec)
-                        break
-                    }
-                }
-            }
-            logger.warn("Filtered out receivers:$filteredOutReceivers")
-        }
-        return filteredReceivers
-    }
+    override fun sendMessage(wechatNotifyMessageWithOperation: WechatNotifyMessageWithOperation) = Unit
 
     override fun listByCreatedTime(
         page: Int,
