dc8865e1cbb4b65d12c0e24c99a581fecddc22af
yjieliang <yundantengteng@163.com>
2024-09-12 15:33:13 +0800

    pref：拉取插件task.json文件内容报错提示优化 #10446

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreMessageCode.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/constant/StoreMessageCode.kt
@@ -91,12 +91,10 @@ object StoreMessageCode {
     const val GET_ATOM_LANGUAGE_ENV_INFO_FAILED = "2120039" // 获取插件开发语言相关的环境变量信息失败
     // 研发商店：插件配置文件[task.json]config配置格式不正确,{0}
     const val TASK_JSON_CONFIG_IS_INVALID = "2120040"
-    // 研发商店：java插件配置文件[task.json]target配置不正确,java插件的target命令配置需要以java开头
-    const val JAVA_ATOM_TASK_JSON_TARGET_IS_INVALID = "2120041"
     // 研发商店: 拉取文件[{0}]失败，失败原因：{1}
-    const val USER_PULL_FILE_FAIL = "2120042"
+    const val USER_PULL_FILE_FAIL = "2120041"
     // 插件包文件[{0}]不存在，请检查文件所在路径是否正确
-    const val ATOM_PACKAGE_FILE_NOT_FOUND = "2120043"
+    const val ATOM_PACKAGE_FILE_NOT_FOUND = "2120042"
 
     const val USER_TEMPLATE_VERSION_IS_NOT_FINISH = "2120201" // 研发商店：模板{0}的{1}版本发布未结束，请稍后再试
     const val USER_TEMPLATE_RELEASE_STEPS_ERROR = "2120202" // 研发商店：模板发布流程状态变更顺序不正确
