d08feeb92501fe7b685ced9a7c4734a684108b97
randychen <randychen@tencent.com>
2024-09-12 17:12:35 +0800

    --task=75297355 【后台】客户端灰度升级优化

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/clientupgrade/ClientUpgradeService.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/service/clientupgrade/ClientUpgradeService.kt
@@ -59,7 +59,7 @@ class ClientUpgradeService @Autowired constructor(
         return ClientUpgradeResp(clientVersion, startVersion)
     }
 
-    @Scheduled(initialDelay = 5 * 60 * 1000L, fixedDelay = 6 * 60 * 1000L)
+    @Scheduled(initialDelay = 60 * 1000L, fixedDelay = 3 * 60 * 1000L)
     fun updateCanUpgradeClients() {
         val watcher = Watcher("updateCanUpgradeClients")
         logger.debug("updateCanUpgradeClients start")
