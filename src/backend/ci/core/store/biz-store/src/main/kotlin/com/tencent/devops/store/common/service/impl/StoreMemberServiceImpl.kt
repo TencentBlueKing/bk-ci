d9b78a0438e843f504d9e47f857328a33d558fa2
v_yjieliang <v_yjieliang@tencent.com>
2024-09-13 15:32:46 +0800

    pref:研发商店组件内置打包流水线都归属到统一的平台项目下 #10475

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/StoreMemberServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/StoreMemberServiceImpl.kt
@@ -36,6 +36,7 @@ import com.tencent.devops.common.client.Client
 import com.tencent.devops.common.web.utils.I18nUtil
 import com.tencent.devops.model.store.tables.records.TStoreMemberRecord
 import com.tencent.devops.project.api.service.ServiceProjectResource
+import com.tencent.devops.store.common.configuration.StoreInnerPipelineConfig
 import com.tencent.devops.store.constant.StoreMessageCode
 import com.tencent.devops.store.constant.StoreMessageCode.GET_INFO_NO_PERMISSION
 import com.tencent.devops.store.constant.StoreMessageCode.NO_COMPONENT_ADMIN_PERMISSION
@@ -69,6 +70,8 @@ abstract class StoreMemberServiceImpl : StoreMemberService {
     lateinit var storeProjectRelDao: StoreProjectRelDao
     @Autowired
     lateinit var storeNotifyService: StoreNotifyService
+    @Autowired
+    lateinit var storeInnerPipelineConfig: StoreInnerPipelineConfig
 
     private val executorService = Executors.newFixedThreadPool(5)
 
@@ -404,6 +407,9 @@ abstract class StoreMemberServiceImpl : StoreMemberService {
      * 判断是否为成员
      */
     override fun isStoreMember(userId: String, storeCode: String, storeType: Byte): Boolean {
+        if (userId == storeInnerPipelineConfig.innerPipelineUser) {
+            return true
+        }
         return storeMemberDao.isStoreMember(dslContext, userId, storeCode, storeType)
     }
 
