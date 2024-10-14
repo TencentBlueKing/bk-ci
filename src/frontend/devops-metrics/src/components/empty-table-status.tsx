beccd2ccc3ebfab8e7e7f7f3fc1f9ec693ae4885
lockiechen <lockiechen@tencent.com>
2024-10-14 10:41:37 +0800

    --story=120020974  修复metrics看版问题

--- src/frontend/devops-metrics/src/components/empty-table-status.tsx
+++ src/frontend/devops-metrics/src/components/empty-table-status.tsx
@@ -9,19 +9,19 @@ export default defineComponent({
         }
     },
 
-    setup(props) {
+    setup(props, { emit }) {
         const { t } = useI18n();
         const typeMap = {
             empty: t('noData'),
             'search-empty': t('searchEmpty')
         } 
         function handleClear () {
-
+            emit('clear')
         }
         return () => (
             <bk-exception type={props.type} scene="part">
                 <div style="font-size: 14px;">{ typeMap[props.type] }</div>
-                {props.type === 'search-empty' && <template>
+                {props.type === 'search-empty' && (
                     <i18n-t 
                         tag="div"
                         keypath="可以尝试 调整关键词 或 清空筛选条件"
@@ -35,7 +35,7 @@ export default defineComponent({
                             {t('清空筛选条件')}
                         </bk-button>
                     </i18n-t>
-                </template>}
+                )}
             </bk-exception>
         )
     }
