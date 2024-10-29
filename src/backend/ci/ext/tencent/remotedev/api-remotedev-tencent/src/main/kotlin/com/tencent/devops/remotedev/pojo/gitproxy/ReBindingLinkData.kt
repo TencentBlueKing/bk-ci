4ebc215e165f8380e0ff54f7ce6c144d1a6b607c
ruotiantang <ruotiantang@tencent.com>
2024-10-29 16:15:34 +0800

    --story=118108412 云控制台绑定及权限异常逻辑优化方案

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/gitproxy/ReBindingLinkData.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/gitproxy/ReBindingLinkData.kt
@@ -11,11 +11,3 @@ data class ReBindingLinkData(
     @get:Schema(title = "凭据ID,为空时使用,调用用户的OAUTH")
     val credId: String?
 )
-
-@Schema(title = "重新绑定工蜂项目信息返回")
-data class ReBindingLinkResp(
-    @get:Schema(title = "工蜂项目ID")
-    val errorIds: Set<Long>,
-    @get:Schema(title = "未成功时错误信息")
-    val errMsg: String?
-)
