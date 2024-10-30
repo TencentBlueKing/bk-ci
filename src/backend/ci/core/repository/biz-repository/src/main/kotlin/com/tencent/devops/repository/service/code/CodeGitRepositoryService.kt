1932e07b8119fc0fe17ac0a3193371f720465b24
mingshewhe <mingshewhe@tencent.com>
2024-10-30 17:33:28 +0800

    bug: 修复代码库开启PAC时,git_project_id字段为空 #11167

--- src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/code/CodeGitRepositoryService.kt
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/code/CodeGitRepositoryService.kt
@@ -418,6 +418,15 @@ class CodeGitRepositoryService @Autowired constructor(
             userName = userId,
             event = CodeGitWebhookEvent.MERGE_REQUESTS_EVENTS.value
         )
+        // 修复历史数据
+        if (repository.gitProjectId == null || repository.gitProjectId == 0L) {
+            val repositoryId = HashUtil.decodeOtherIdToLong(repository.repoHashId!!)
+            repositoryCodeGitDao.updateGitProjectId(
+                dslContext = dslContext,
+                id = repositoryId,
+                gitProjectId = gitProjectInfo.id
+            )
+        }
     }
 
     override fun getGitFileTree(projectId: String, userId: String, record: TRepositoryRecord): List<GitFileInfo> {
