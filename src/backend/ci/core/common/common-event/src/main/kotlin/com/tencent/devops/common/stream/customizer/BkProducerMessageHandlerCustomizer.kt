5c45223d345eb7d54c067334d83c4618b0c66f09
stubenhuang <stubenhuang@tencent.com>
2024-10-30 09:53:42 +0800

    --story=120425729 使用ProducerMessageHandlerCustomizer来初始化生产者的线程池

--- src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/stream/customizer/BkProducerMessageHandlerCustomizer.kt
+++ src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/stream/customizer/BkProducerMessageHandlerCustomizer.kt
@@ -7,7 +7,7 @@ import org.springframework.integration.amqp.outbound.AmqpOutboundEndpoint
 class BkProducerMessageHandlerCustomizer : ProducerMessageHandlerCustomizer<AmqpOutboundEndpoint> {
     override fun configure(handler: AmqpOutboundEndpoint, destinationName: String) {
         logger.info("handler customizer , destinationName: $destinationName , handler: ${handler.beanName}")
-        handler.rabbitTemplate?.isUsePublisherConnection = false
+        handler.rabbitTemplate?.isUsePublisherConnection = false // 生产者和消费者共享连接池, 提高性能
     }
 
     companion object {
