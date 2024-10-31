7676cf840146390d55e7cd0f871bb3fcd6dcaa56
mingshewhe <mingshewhe@tencent.com>
2024-10-31 11:12:37 +0800

    bug: 修复代码库开启PAC时,git_project_id字段为空 #11167

--- src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/code/CodeGitRepositoryService.kt
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/code/CodeGitRepositoryService.kt
@@ -175,7 +175,7 @@ class CodeGitRepositoryService @Autowired constructor(
         ).url
         var gitProjectId: Long? = null
         // 需要更新gitProjectId
-        if (sourceUrl != repository.url) {
+        if (sourceUrl != repository.url || repository.gitProjectId == null || repository.gitProjectId == 0L) {
             logger.info(
                 "repository url unMatch,need change gitProjectId,sourceUrl=[$sourceUrl] targetUrl=[${repository.url}]"
             )
