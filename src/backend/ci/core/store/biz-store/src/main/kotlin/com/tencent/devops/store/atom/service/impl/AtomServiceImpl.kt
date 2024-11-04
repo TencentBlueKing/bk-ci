c98dba5d72cf5349e2aec436e38ce91af391f22b
yjieliang <yundantengteng@163.com>
2024-11-04 14:27:04 +0800

    bug：优化项目下安装插件分页数据查询，排除与插件关联的内置流水线 #11210

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/AtomServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/AtomServiceImpl.kt
@@ -1100,7 +1100,8 @@ abstract class AtomServiceImpl @Autowired constructor() : AtomService {
             } else {
                 limit = page * pageSize - projectAtomCount
             }
-            logger.info("getDefaultAtoms projectCode:$projectCode |page:$page|pageSize:$pageSize offset:$offset|limit:$limit")
+            logger.info("getDefaultAtoms projectCode:$projectCode |page:$page|pageSize:$pageSize" +
+                    "projectAtomCount:$projectAtomCount|offset:$offset|limit:$limit")
             defaultAtoms = atomDao.getDefaultAtoms(
                 dslContext = dslContext,
                 classifyCode = classifyCode,
