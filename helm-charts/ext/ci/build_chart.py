e71f33a13f7f3593fc27e04a27ed2b6803334859
stubenhuang <stubenhuang@tencent.com>
2024-11-18 14:46:30 +0800

    修复chart部署问题

--- helm-charts/ext/ci/build_chart.py
+++ helm-charts/ext/ci/build_chart.py
@@ -57,7 +57,8 @@ gateway_tpl.flush()
 gateway_tpl.close()
 
 # 更新依赖
-os.system("/bin/bash -c dependencyBuild.sh")
+os.system("chmod +x ./dependencyBuild.sh")
+os.system("/bin/bash -c ./dependencyBuild.sh")
 
 # 打包
 os.system("helm package . --version " + os.environ.get("chart_version") + " --app-version "+os.environ.get("version"))
