25e9094aeea308d72cd9b45c98bffd91f151ce73
v_zkggzhou <2198773846@qq.com>
2024-11-18 15:57:55 +0800

    pref:流水线相关的文件操作人调整为流水线的权限代持人 #11016

--- src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/PipelineService.kt
+++ src/backend/ci/ext/tencent/artifactory/biz-artifactory-tencent/src/main/kotlin/com/tencent/devops/artifactory/service/PipelineService.kt
@@ -149,8 +149,8 @@ abstract class PipelineService @Autowired constructor(
                 resourceType = AuthResourceType.PIPELINE_DEFAULT.value,
                 resourceCode = pipelineId
             ).data
-        } catch (e: Exception) {
-            logger.error("Failed to get pipeline oauth user, projectId: $projectId, pipelineId: $pipelineId, error: $e")
+        } catch (ignored: Exception) {
+            logger.info("get pipeline oauth user fail", ignored)
             null
         }?.handoverFrom
     }
