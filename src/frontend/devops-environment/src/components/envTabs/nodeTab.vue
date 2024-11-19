2025264bc2be22e71fb6d85d48d0b772d9b37ae4
v_hwweng <v_hwweng@tencent.com>
2024-11-19 20:05:27 +0800

    --story=119279187 【云桌面】支持项目内云桌面公共环境 # Reviewed, transaction id: 24238

--- src/frontend/devops-environment/src/components/envTabs/nodeTab.vue
+++ src/frontend/devops-environment/src/components/envTabs/nodeTab.vue
@@ -665,7 +665,7 @@
             },
             handleToNodeDetailPage (row) {
                 if (row.nodeType === 'CMDB') return
-                if (row.nodeType === 'DEVX') {
+                if (this.curEnvDetail.envType === 'DEVX') {
                     window.open(`${location.origin}/console/devx/${this.projectId}/instance-manage?active=${row.displayName}&tab=baseInfo`, '_blank')
                 } else {
                     window.open(`${location.origin}/console/environment/${this.projectId}/nodeDetail/${row.nodeHashId}`, '_blank')
