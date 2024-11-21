00b353a3e54d68bc1f218ba698d3f18d73ea7c15
v_hwweng <v_hwweng@tencent.com>
2024-11-21 19:47:51 +0800

    feat：创建流水线时支持设置标签 #11055 # Reviewed, transaction id: 24508

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/RemoteAtom.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/RemoteAtom.vue
@@ -72,7 +72,6 @@
                 const currentUserInfo = this.$userInfo || {}
                 const atomDisabled = this.disabled || false
                 const envConf = await this.getEnvConf()
-                const query = this.$$route.query || {}
                 this.loading = false
                 const iframe = document.getElementById('atom-iframe').contentWindow
                 iframe.postMessage({
@@ -85,8 +84,7 @@
                     atomDisabled,
                     hostInfo: {
                         ...this.$route.params
-                    },
-                    query
+                    }
                 }, '*')
             },
             receiveMsgFromIframe (e) {
