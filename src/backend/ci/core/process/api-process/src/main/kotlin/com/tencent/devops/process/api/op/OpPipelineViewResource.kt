eb547b418a170f8defaa79b1b8aad34fefc67fba
mingshewhe <mingshewhe@tencent.com>
2024-11-28 20:00:21 +0800

    bug: [PAC].ci下的目录已经删除,但是关联的流水线组没有删除，也无法手工删除 #11254
    增加op接口,删除PAC流水线组

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/op/OpPipelineViewResource.kt
@@ -7,10 +7,13 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 
 @Tag(name = "USER_PIPELINE_VIEW", description = "用户-流水线视图")
@@ -25,5 +28,20 @@ interface OpPipelineViewResource {
         @Parameter(description = "用户ID", required = true, example = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String
+    ): Result<Boolean>
+
+    @Operation(summary = "删除yaml流水线组")
+    @DELETE
+    @Path("{projectId}/{repoHashId}/deleteYamlView")
+    fun deleteYamlView(
+        @Parameter(description = "项目ID", required = true)
+        @PathParam("projectId")
+        projectId: String,
+        @Parameter(description = "代码库hashId", required = true)
+        @PathParam("repoHashId")
+        repoHashId: String,
+        @Parameter(description = "yaml文件目录", required = true)
+        @QueryParam("directory")
+        directory: String
     ): Result<Boolean>
 }
