cf06ec1020b609774dd3538736897291cc79078f
mingshewhe <mingshewhe@tencent.com>
2025-01-03 11:06:10 +0800

    feat：导出/导出流水线功能优化 #11304 # Reviewed, transaction id: 28075

--- src/frontend/common-lib/docs.js
+++ src/frontend/common-lib/docs.js
@@ -15,7 +15,7 @@ const createDocs = (lang, version) => {
         SPAWN_NODES_DOC: `${IWIKI_DOCS_URL}/p/1083624634`, // 跨项目调用文档
         BKAPP_NAV_OPEN_SOURCE_URL: 'https://github.com/TencentBlueKing/bk-ci', // 开源社区
         FEED_BACK_URL: 'https://mk.woa.com/q/new?coterie=346', // 问题反馈
-        PAC_GUIDE_DOC: `${commonPrefix}/Services/Pipeline-as-Code/01-quick-start/01-quict-start.md` // PAC快速上手文档
+        PAC_GUIDE_DOC: `${IWIKI_DOCS_URL}/Services/Pipeline-as-Code/01-quick-start/01-quict-start.md` // PAC快速上手文档
     }
     const pipelineDocs = {
         ALIAS_BUILD_NO_DOC: 'https://docs.bkci.net/services/pipelines/pipeline-edit/alias-buildno', // 构建号别名文档
