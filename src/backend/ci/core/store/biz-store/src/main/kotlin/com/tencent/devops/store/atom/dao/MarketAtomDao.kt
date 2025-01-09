43776d749785c9380f5956d1a0290e916c9e980b
yjieliang <yundantengteng@163.com>
2025-01-09 20:28:01 +0800

    bug:组件latestFlag参数更新时组件所有版本修改者会全部更新为同一人 #11400

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/dao/MarketAtomDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/dao/MarketAtomDao.kt
@@ -39,6 +39,7 @@ import com.tencent.devops.model.store.tables.TLabel
 import com.tencent.devops.model.store.tables.TStoreMember
 import com.tencent.devops.model.store.tables.TStoreStatisticsTotal
 import com.tencent.devops.model.store.tables.records.TAtomRecord
+import com.tencent.devops.store.common.utils.VersionUtils
 import com.tencent.devops.store.pojo.atom.ApproveReq
 import com.tencent.devops.store.pojo.atom.MarketAtomCreateRequest
 import com.tencent.devops.store.pojo.atom.MarketAtomUpdateRequest
@@ -47,7 +48,6 @@ import com.tencent.devops.store.pojo.atom.enums.AtomStatusEnum
 import com.tencent.devops.store.pojo.atom.enums.AtomTypeEnum
 import com.tencent.devops.store.pojo.atom.enums.MarketAtomSortTypeEnum
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
-import com.tencent.devops.store.common.utils.VersionUtils
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import org.jooq.Condition
@@ -739,7 +739,7 @@ class MarketAtomDao : AtomBaseDao() {
                     LATEST_TEST_FLAG,
                     DSL.case_().`when`(ID.eq(atomId), true).otherwise(false)
                 )
-                .set(MODIFIER, userId)
+                .set(MODIFIER, DSL.case_().`when`(ID.eq(atomId), userId))
                 .where(ATOM_CODE.eq(atomCode))
                 .execute()
         }
