44042a3b63281964a8baaa6571d56bc2a6add537
yjieliang <yundantengteng@163.com>
2025-01-14 18:49:26 +0800

    feat：流水线查看和构建详情查看配置界面敏感字段展示优化 #11019

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/OpAtomServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/OpAtomServiceImpl.kt
@@ -681,7 +681,7 @@ class OpAtomServiceImpl @Autowired constructor(
             )
             val tAtom = TAtom.T_ATOM
             result.forEach {
-                val latestFlag = it[KEY_LATEST_FLAG]  as Boolean
+                val latestFlag = it[tAtom.LATEST_FLAG]  as Boolean
                 marketAtomService.updateAtomSensitiveCacheConfig(
                     atomCode = it[tAtom.ATOM_CODE],
                     atomVersion = it[tAtom.VERSION],
