c3965740d155ed7c883c42801b72708eab78399e
greysonfang <greysonfang@tencent.com>
2025-02-13 17:26:14 +0800

    --story=118709485 【后台】成员管理加上默认组：云研发管理员

--- src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/provider/rbac/service/TxMigrateService.kt
+++ src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/provider/rbac/service/TxMigrateService.kt
@@ -9,12 +9,15 @@ import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import com.tencent.devops.common.auth.api.pojo.ProjectConditionDTO
 import com.tencent.devops.common.client.Client
 import com.tencent.devops.project.api.service.ServiceProjectResource
+import com.tencent.devops.project.pojo.ProjectByConditionDTO
 import org.slf4j.LoggerFactory
 
 class TxMigrateService(
     private val client: Client,
     private val permissionResourceGroupService: PermissionResourceGroupService,
-    private val permissionResourceMemberService: PermissionResourceMemberService
+    private val permissionResourceMemberService: PermissionResourceMemberService,
+    private val permissionGradeManagerService: PermissionGradeManagerService,
+    private val resourceService: AuthResourceService
 ) {
 
     fun migrateRemoteDevManager(projectConditionDTO: ProjectConditionDTO) {
@@ -28,32 +31,46 @@ class TxMigrateService(
                 offset = offset
             ).data ?: break
             migrateProjects.forEach { projectInfo ->
-                logger.info("migrate remote dev manager ${projectInfo.englishName}|${projectInfo.remotedevManager}")
-                try {
-                    val groupId = permissionResourceGroupService.createGroupAndPermissionsByGroupCode(
-                        projectId = projectInfo.englishName,
-                        resourceType = ResourceTypeId.PROJECT,
-                        resourceCode = projectInfo.englishName,
-                        groupCode = BkAuthGroup.CGS_MANAGER.value
-                    )
-                    projectInfo.remotedevManager?.let { managers ->
-                        val managerList = managers.split(";")
-                        managerList.forEach {
-                            permissionResourceMemberService.addGroupMember(
-                                projectCode = projectInfo.englishName,
-                                memberId = it,
-                                memberType = MemberType.USER.type,
-                                expiredAt = 4102444800,
-                                iamGroupId = groupId
-                            )
-                        }
-                    }
-                } catch (ex: Exception) {
-                    logger.warn("migrate remote dev manager failed |${projectInfo.englishName}| $ex")
-                }
+                migrateRemoteDevManager(projectInfo)
             }
             offset += limit
         } while (migrateProjects.size == limit)
+    }
+
+    fun migrateRemoteDevManager(projectInfo: ProjectByConditionDTO) {
+        logger.info("migrate remote dev manager ${projectInfo.englishName}|${projectInfo.remotedevManager}")
+        try {
+            val gradeManagerId = resourceService.get(
+                projectCode = projectInfo.englishName,
+                resourceType = ResourceTypeId.PROJECT,
+                resourceCode = projectInfo.englishName
+            ).relationId
+            permissionGradeManagerService.modifyGradeManager(
+                gradeManagerId = gradeManagerId,
+                projectCode = projectInfo.englishName,
+                projectName = projectInfo.projectName
+            )
+            val groupId = permissionResourceGroupService.createGroupAndPermissionsByGroupCode(
+                projectId = projectInfo.englishName,
+                resourceType = ResourceTypeId.PROJECT,
+                resourceCode = projectInfo.englishName,
+                groupCode = BkAuthGroup.CGS_MANAGER.value
+            )
+            projectInfo.remotedevManager?.let { managers ->
+                val managerList = managers.split(";")
+                managerList.forEach {
+                    permissionResourceMemberService.addGroupMember(
+                        projectCode = projectInfo.englishName,
+                        memberId = it,
+                        memberType = MemberType.USER.type,
+                        expiredAt = 4102444800,
+                        iamGroupId = groupId
+                    )
+                }
+            }
+        } catch (ex: Exception) {
+            logger.warn("migrate remote dev manager failed |${projectInfo.englishName}| $ex")
+        }
     }
 
     companion object {
