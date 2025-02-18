75e945afabf9d24a58bbb73a65064d44302cde34
greysonfang <greysonfang@tencent.com>
2025-02-18 14:51:20 +0800

    --story=118709485 【后台】成员管理加上默认组：云研发管理员

--- src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/provider/other/service/OtherMigrateService.kt
+++ src/backend/ci/ext/tencent/auth/biz-auth-tencent/src/main/kotlin/com/tencent/devops/auth/provider/other/service/OtherMigrateService.kt
@@ -4,7 +4,9 @@ import com.tencent.devops.auth.service.TxMigrateService
 import com.tencent.devops.common.auth.api.pojo.ProjectConditionDTO
 
 class OtherMigrateService : TxMigrateService {
-    override fun migrateRemoteDevManager(projectConditionDTO: ProjectConditionDTO) {}
+    override fun migrateRemoteDevManager(projectConditionDTO: ProjectConditionDTO) {
+        return
+    }
 
     override fun migrateRemoteDevManager(projectCode: String): Int {
         return 0
