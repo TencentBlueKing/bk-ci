b5c4235af30fbe9044fe4f40c4fc02821d3343ea
royalhuang <royalhuang@tencent.com>
2025-02-24 15:57:58 +0800

    bug: 通过api保存的新版本可能覆盖旧版本 #11418 解决更新时间和格式问题

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/webhook/PipelineBuildWebhookService.kt
@@ -84,7 +84,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import javax.ws.rs.core.Response
 
 @Suppress("ALL")
 @Service
