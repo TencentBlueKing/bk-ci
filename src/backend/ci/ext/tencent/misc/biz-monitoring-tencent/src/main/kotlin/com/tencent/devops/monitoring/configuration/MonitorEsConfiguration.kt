19d88b450292bda8bc3b41e3689736b84299ca4a
stubenhuang <stubenhuang@tencent.com>
2025-03-13 16:21:33 +0800

    feat: 升级JDK17 #10593 修复质量红线

--- src/backend/ci/ext/tencent/misc/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/configuration/MonitorEsConfiguration.kt
+++ src/backend/ci/ext/tencent/misc/biz-monitoring-tencent/src/main/kotlin/com/tencent/devops/monitoring/configuration/MonitorEsConfiguration.kt
@@ -33,7 +33,6 @@ import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.client.CredentialsProvider
 import org.apache.http.impl.client.BasicCredentialsProvider
 import org.elasticsearch.client.RestClient
-import org.elasticsearch.client.RestClientBuilder
 import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
