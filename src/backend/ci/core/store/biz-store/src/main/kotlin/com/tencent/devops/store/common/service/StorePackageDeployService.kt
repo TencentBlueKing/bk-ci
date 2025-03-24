ddbc2b7d2fe3dbd89080470dc286c482829d0ab7
yjieliang <yundantengteng@163.com>
2025-03-24 19:29:21 +0800

    feat:研发商店组件支持一键发布 #11543

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/StorePackageDeployService.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/StorePackageDeployService.kt
@@ -69,7 +69,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.util.FileSystemUtils
 
-@Service
 abstract class StorePackageDeployService {
 
     @Autowired
