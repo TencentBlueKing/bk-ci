1770f5eb96af6254733179e83437f49e94cee154
stuben <380996643@qq.com>
2025-03-28 14:43:46 +0800

    Revert "feat: 让mq在程序启动的时候正常初始化 #11584"

--- src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/stream/customizer/BkProducerMessageHandlerCustomizer.kt
+++ src/backend/ci/core/common/common-event/src/main/kotlin/com/tencent/devops/common/stream/customizer/BkProducerMessageHandlerCustomizer.kt
@@ -3,16 +3,11 @@ package com.tencent.devops.common.stream.customizer
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.config.ProducerMessageHandlerCustomizer
 import org.springframework.integration.amqp.outbound.AmqpOutboundEndpoint
-import org.springframework.messaging.MessageHandler
 
-class BkProducerMessageHandlerCustomizer : ProducerMessageHandlerCustomizer<MessageHandler> {
-    override fun configure(handler: MessageHandler, destinationName: String) {
-        if (handler is AmqpOutboundEndpoint) {
-            logger.info("handler customizer success , destinationName: $destinationName , handler: ${handler.beanName}")
-            handler.rabbitTemplate?.isUsePublisherConnection = false // 生产者和消费者共享连接池, 提高性能
-        } else {
-            logger.info("handler customizer failed , destinationName: $destinationName , handler: ${handler.javaClass}")
-        }
+class BkProducerMessageHandlerCustomizer : ProducerMessageHandlerCustomizer<AmqpOutboundEndpoint> {
+    override fun configure(handler: AmqpOutboundEndpoint, destinationName: String) {
+        logger.info("handler customizer , destinationName: $destinationName , handler: ${handler.beanName}")
+        handler.rabbitTemplate?.isUsePublisherConnection = false // 生产者和消费者共享连接池, 提高性能
     }
 
     companion object {
