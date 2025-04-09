40573d900c4d72b8a220856fde8eef32c1151884
carlyin <carlyin@tencent.com>
2025-04-09 12:03:50 +0800

    pref:研发商店组件包签名流程优化 #11572

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/resources/ServiceStoreComponentResourceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/resources/ServiceStoreComponentResourceImpl.kt
@@ -43,6 +43,7 @@ import com.tencent.devops.store.pojo.common.UnInstallReq
 import com.tencent.devops.store.pojo.common.enums.RdTypeEnum
 import com.tencent.devops.store.pojo.common.enums.StoreSortTypeEnum
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
+import com.tencent.devops.store.pojo.common.version.VersionInfo
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
@@ -72,6 +73,24 @@ class ServiceStoreComponentResourceImpl @Autowired constructor(
             storeType = storeType,
             storeCode = storeCode,
             unInstallReq = unInstallReq
+        )
+    }
+
+    override fun getStoreUpgradeVersionInfo(
+        userId: String,
+        storeType: String,
+        storeCode: String,
+        projectCode: String?,
+        instanceId: String?
+    ): Result<VersionInfo?> {
+        return Result(
+            storeComponentQueryService.getComponentUpgradeVersionInfo(
+                userId = userId,
+                storeCode = storeCode,
+                storeType = storeType,
+                projectCode = projectCode ?: "",
+                instanceId = instanceId
+            )
         )
     }
 
