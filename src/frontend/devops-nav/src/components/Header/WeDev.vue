882dbe0232d600aaceaaea72f0aeadb79003fe58
lockiechen <lockiechen@tencent.com>
2025-04-09 15:11:54 +0800

    --story=122918567 feat：研效品牌融合-logo规范

---
+++ src/frontend/devops-nav/src/components/Header/WeDev.vue
@@ -0,0 +1,69 @@
+<template>
+    <div
+        class="we-dev-logo-area"
+    >
+        <Icon
+            name="we"
+            :size="26"
+        />
+        <Icon
+            class="we-dev-logo"
+            :name="weName"
+            :size="size"
+        />
+    </div>
+</template>
+
+<script>
+    import Icon from '@/components/Icon/index.vue'
+    import { defineComponent, ref } from 'vue'
+    export default defineComponent({
+        components: {
+            Icon
+        },
+        setup () {
+            const weName = ref('wedev')
+            const size = ref(90)
+            
+            return {
+                weName,
+                size
+            }
+        }
+    })
+</script>
+
+<style lang="scss">
+    .we-dev-logo-area {
+        position: relative;
+        transition: all .3s ease;
+        background-color: #30304E;
+        display: flex;
+        height: 100%;
+        align-items: center;
+        padding: 0 12px;
+        overflow: hidden;
+        width: 50px;
+        
+        &:hover {
+            .we-dev-logo {
+                transform: scaleX(1);
+                opacity: 1;
+            }
+            width: 114px;
+        }
+        > svg {
+            transition: all .3s ease;
+            display: block;
+            overflow: visible;
+        }
+        .we-dev-logo {
+            position: absolute;
+            overflow: hidden;
+            left: 12px;
+            opacity: 0;
+            transform: scaleX(0);
+            transform-origin: left center;
+        }
+    }
+</style>
