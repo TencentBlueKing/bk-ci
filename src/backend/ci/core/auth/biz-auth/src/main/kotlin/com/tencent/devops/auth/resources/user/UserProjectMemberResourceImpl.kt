e05969d0f9cb93ac1160a977a003a2863a9657a2
v_zkggzhou <2198773846@qq.com>
2025-04-14 18:31:46 +0800

    pref:metrics服务部分接口完善权限校验 #11620

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/user/UserProjectMemberResourceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/user/UserProjectMemberResourceImpl.kt
@@ -38,15 +38,10 @@ import org.springframework.beans.factory.annotation.Autowired
 @RestResource
 class UserProjectMemberResourceImpl @Autowired constructor(
     val permissionProjectService: PermissionProjectService,
-    val userProjectPermissionService: UserProjectPermissionService
 ) : UserProjectMemberResource {
     override fun checkManager(userId: String, projectId: String): Result<Boolean> {
         val result = permissionProjectService.checkProjectManager(userId, projectId) ||
             permissionProjectService.isProjectUser(userId, projectId, BkAuthGroup.CI_MANAGER)
         return Result(result)
-    }
-
-    override fun checkMember(userId: String, projectId: String): Result<Boolean> {
-       return Result(userProjectPermissionService.checkMember(userId, projectId))
     }
 }
