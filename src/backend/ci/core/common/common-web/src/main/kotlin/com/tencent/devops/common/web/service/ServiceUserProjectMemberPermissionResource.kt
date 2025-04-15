6be7b92618f22d5425ba9a72248c6e841f18425d
v_zkggzhou <2198773846@qq.com>
2025-04-15 14:34:48 +0800

    pref:metrics服务部分接口完善权限校验 #11620

--- src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/service/ServiceUserProjectMemberPermissionResource.kt
+++ src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/service/ServiceUserProjectMemberPermissionResource.kt
@@ -1,5 +1,6 @@
 package com.tencent.devops.common.web.service
 
+import com.tencent.devops.common.api.annotation.ServiceInterface
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.pojo.Result
 import io.swagger.v3.oas.annotations.Operation
@@ -18,6 +19,7 @@ import jakarta.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/service/user/project/member")
+@ServiceInterface("auth")
 interface ServiceUserProjectMemberPermissionResource {
 
     @GET
