9dc59729807a9f714e06b21b64d3c301877a18f0
v_zkggzhou <2198773846@qq.com>
2025-04-15 14:55:05 +0800

    pref:metrics服务部分接口完善权限校验 #11620

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserProjectMemberPermissionResourceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserProjectMemberPermissionResourceImpl.kt
@@ -2,9 +2,12 @@ package com.tencent.devops.auth.resources.service
 
 import com.tencent.devops.auth.service.UserProjectPermissionService
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.common.web.service.ServiceUserProjectMemberPermissionResource
 import org.springframework.beans.factory.annotation.Autowired
 
+
+@RestResource
 class ServiceUserProjectMemberPermissionResourceImpl @Autowired constructor(
     private val userProjectPermissionService: UserProjectPermissionService
 ) : ServiceUserProjectMemberPermissionResource {
