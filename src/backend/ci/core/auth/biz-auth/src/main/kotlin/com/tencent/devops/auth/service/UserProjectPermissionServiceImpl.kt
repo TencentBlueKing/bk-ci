bf01fe0d582a71d6ebc91ce76dc66fd0b0857102
v_zkggzhou <2198773846@qq.com>
2025-04-15 15:11:12 +0800

    pref:metrics服务部分接口完善权限校验 #11620

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/UserProjectPermissionServiceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/UserProjectPermissionServiceImpl.kt
@@ -4,13 +4,12 @@ import com.tencent.devops.auth.dao.AuthResourceGroupMemberDao
 import org.jooq.DSLContext
 import org.springframework.stereotype.Service
 
-
 @Service
 class UserProjectPermissionServiceImpl(
     private val authResourceGroupMemberDao: AuthResourceGroupMemberDao,
     private val dslContext: DSLContext
 ) : UserProjectPermissionService {
     override fun checkMember(projectCode: String, userId: String): Boolean {
-        return authResourceGroupMemberDao.isProjectMember(dslContext,projectCode, userId)
+        return authResourceGroupMemberDao.isProjectMember(dslContext, projectCode, userId)
     }
 }
