8d568d2ef1ebb280401a9b3290308626ec251e4f
v_yjjiaoyu <1981190393@qq.com>
2025-04-21 19:57:55 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414 # Reviewed, transaction id: 39694

--- src/frontend/devops-pipeline/src/components/PipelineEditTabs/AuthorityTab.vue
+++ src/frontend/devops-pipeline/src/components/PipelineEditTabs/AuthorityTab.vue
@@ -11,26 +11,25 @@
 
 <script>
     import pipelineOperateMixin from '@/mixins/pipeline-operate-mixin'
-    import { mapState } from 'vuex'
+    import { mapState, mapGetters } from 'vuex'
 
     export default {
         name: 'auth-tab',
         mixins: [pipelineOperateMixin],
-        data () {
-            return {
-                resourceType: 'pipeline'
-            }
-        },
         computed: {
             ...mapState('atom', ['pipelineInfo']),
+            ...mapGetters('atom', ['isTemplate']),
             projectCode () {
                 return this.$route.params.projectId
             },
             resourceCode () {
-                return this.$route.params.pipelineId
+                return this.isTemplate ? this.$route.params.templateId : this.$route.params.pipelineId
             },
             pipelineName () {
-                return this.pipelineInfo?.pipelineName ?? ''
+                return (this.isTemplate ? this.pipelineInfo?.name : this.pipelineInfo?.pipelineName) ?? ''
+            },
+            resourceType () {
+                return this.isTemplate ? 'template' : 'pipeline'
             }
         }
     }
