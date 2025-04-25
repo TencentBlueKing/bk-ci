1c4cbf2811b51045bfa96b2b507d78052e0ca882
randychen <randychen@tencent.com>
2025-04-25 11:12:19 +0800

    --story=122371173 【云桌面】云桌面内增加假锁屏登录方式

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/dao/ProjectStrategyDao.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/dao/ProjectStrategyDao.kt
@@ -6,6 +6,7 @@ import com.tencent.devops.remotedev.pojo.WindowsResourceZoneConfigType
 import com.tencent.devops.remotedev.pojo.strategy.StrategyType
 import org.jooq.DSLContext
 import org.jooq.JSON
+import org.jooq.impl.DSL
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -18,25 +19,35 @@ class ProjectStrategyDao {
         content: String
     ) {
         with(TRemotedevProjectStrategy.T_REMOTEDEV_PROJECT_STRATEGY) {
-            val record = dslContext.selectFrom(this).where(PROJECT_ID.eq(projectId))
-                .and(ZONE_TYPE.eq(zoneType.name)).and(STRATEGY_TYPE.eq(strategyType.name))
-                .fetchAny()
-            if (record != null) {
-                dslContext.update(this).set(STRATEGY_CONTENT, JSON.json(content)).where(ID.eq(record.id)).execute()
-                return
+            val condition = PROJECT_ID.eq(projectId)
+                .and(ZONE_TYPE.eq(zoneType.name))
+                .and(STRATEGY_TYPE.eq(strategyType.name))
+
+            dslContext.transaction { config ->
+                val ctx = DSL.using(config)
+
+                val updated = ctx.update(this)
+                    .set(STRATEGY_CONTENT, JSON.json(content))
+                    .where(condition)
+                    .execute()
+
+                if (updated == 0) {
+                    ctx.insertInto(
+                        this,
+                        PROJECT_ID,
+                        ZONE_TYPE,
+                        STRATEGY_TYPE,
+                        STRATEGY_CONTENT
+                    )
+                        .values(
+                            projectId,
+                            zoneType.name,
+                            strategyType.name,
+                            JSON.json(content)
+                        )
+                        .execute()
+                }
             }
-            dslContext.insertInto(
-                this,
-                PROJECT_ID,
-                ZONE_TYPE,
-                STRATEGY_TYPE,
-                STRATEGY_CONTENT
-            ).values(
-                projectId,
-                zoneType.name,
-                strategyType.name,
-                JSON.json(content)
-            ).execute()
         }
     }
 
