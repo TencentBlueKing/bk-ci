84ac9c8bfdfc561be5c0b6a87591396d7e507020
lockiechen <lockiechen@tencent.com>
2025-05-06 09:49:07 +0800

    feat:【PAC模板】流水线模板支持PAC特性 issue #11414

--- src/frontend/devops-atomstore/src/views/manage/setting/publish-strategy.vue
+++ src/frontend/devops-atomstore/src/views/manage/setting/publish-strategy.vue
@@ -2,10 +2,15 @@
     <div class="publish-strategy">
         <section class="publish-strategy-row">
             <label>
-                {{ $t('publishStrategy') }}
+                {{ $t('store.publishStrategy') }}
             </label>
-
-            <bk-select v-if="editing">
+            
+            <bk-select
+                v-if="editing"
+                class="publish-strategy-select"
+                :value="strategy"
+                @change="handleStrategyChange"
+            >
                 <bk-option
                     v-for="strategy in strategyOptions"
                     :key="strategy.id"
@@ -18,7 +23,10 @@
             >
                 <span>{{ strategyLabel }}</span>
                 <span class="publish-strategy-desc">{{ strategyDesc }}</span>
-                <i class="devops-icon icon-edit2" />
+                <i
+                    class="devops-icon icon-edit2"
+                    @click="editStrategy"
+                />
             </p>
         </section>
     </div>
@@ -26,23 +34,46 @@
 
 <script>
     import { PUBLISH_STRATEGY } from '@/utils/constants'
-    import { defineComponent, getCurrentInstance, ref } from 'vue'
+    import { computed, defineComponent, getCurrentInstance, ref } from 'vue'
 
     export default defineComponent({
-        setup () {
+        props: {
+            strategy: {
+                type: String,
+                required: true
+            }
+        },
+        setup (props) {
             const vm = getCurrentInstance()
             const editing = ref(false)
-            const strategyLabel = ref('自动发布')
-            const strategyDesc = ref(vm.proxy.$t('（当源模版有新版本时，新版本自动发布到研发商店）'))
+            const strategyLabel = computed(() => vm.proxy.$t(`store.${props.strategy}`))
+            const strategyDesc = computed(() => vm.proxy.$t(`store.${props.strategy}-upgradeStrategyDesc`))
             const strategyOptions = Object.keys(PUBLISH_STRATEGY).map(key => ({
                 id: key,
-                name: vm.proxy.$t(`store.${key}`)
+                name: `${vm.proxy.$t(`store.${key}`)} (${vm.proxy.$t(`store.${key}-upgradeStrategyDesc`)})`
             }))
+
+            function editStrategy () {
+                editing.value = true
+            }
+
+            function handleStrategyChange (newVal) {
+                // TODO:
+                if (!newVal || newVal === props.strategy) {
+                    editing.value = false
+                    return
+                }
+                vm.emit('update:strategy', newVal)
+                editing.value = false
+            }
             return {
                 editing,
                 strategyOptions,
                 strategyLabel,
-                strategyDesc
+                strategyDesc,
+                editStrategy,
+                handleStrategyChange,
+                strategy: props.strategy
             }
         }
     })
@@ -58,11 +89,16 @@
             display: flex;
             grid-gap: 8px;
             font-size: 12px;
+            align-items: center;
+            .publish-strategy-select {
+                width: 222px;
+            }
             
             .publish-strategy-detail {
                 display: flex;
                 flex: 1;
                 align-items: center;
+                grid-gap: 6px;
                 .publish-strategy-desc {
                     color: #979BA5;
                 }
