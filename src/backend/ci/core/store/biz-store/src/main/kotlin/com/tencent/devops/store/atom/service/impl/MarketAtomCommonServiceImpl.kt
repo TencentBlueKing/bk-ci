777362a0368428470d8250627d03023a6bbed511
v_zkggzhou <2198773846@qq.com>
2025-05-06 18:09:07 +0800

    pref:完善研发商店组件配置文件参数校验 #11269 #11287

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/MarketAtomCommonServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/atom/service/impl/MarketAtomCommonServiceImpl.kt
@@ -114,7 +114,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import javax.ws.rs.core.Response
 import kotlin.reflect.KClass
 
 @Suppress("ALL")
