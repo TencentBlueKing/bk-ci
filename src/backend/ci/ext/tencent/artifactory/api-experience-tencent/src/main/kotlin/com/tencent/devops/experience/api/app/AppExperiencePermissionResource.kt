49966e3fa01cc1f492c4eb0e83aca0c172af255e
greysonfang <greysonfang@tencent.com>
2025-05-26 11:27:57 +0800

    pref：无权限跳转申请优化 #11769

--- src/backend/ci/ext/tencent/artifactory/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/api/app/AppExperiencePermissionResource.kt
+++ src/backend/ci/ext/tencent/artifactory/api-experience-tencent/src/main/kotlin/com/tencent/devops/experience/api/app/AppExperiencePermissionResource.kt
@@ -4,6 +4,8 @@ import com.tencent.devops.auth.pojo.ApplyJoinGroupInfo
 import com.tencent.devops.auth.pojo.vo.AuthApplyRedirectInfoVo
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -16,7 +18,7 @@ import jakarta.ws.rs.Produces
 import jakarta.ws.rs.QueryParam
 import jakarta.ws.rs.core.MediaType
 
-@Tag(name = "APP_EXPERIENCE_SECURITY", description = "版本体验-权限相关")
+@Tag(name = "APP_EXPERIENCE_PERMISSION", description = "版本体验-权限相关")
 @Path("/app/experiences/permission/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -52,4 +54,23 @@ interface AppExperiencePermissionResource {
         @QueryParam("action")
         action: String?
     ): Result<AuthApplyRedirectInfoVo>
+
+    @Path("/getResourceGroupUsers")
+    @Operation(summary = "获取特定资源下用户组成员")
+    fun getResourceGroupUsers(
+        @Parameter(description = "用户名", required = true)
+        @HeaderParam(AUTH_HEADER_USER_ID)
+        userId: String,
+        @Parameter(description = "项目Code", required = true)
+        projectId: String,
+        @QueryParam("resourceType")
+        @Parameter(description = "资源类型", required = false)
+        resourceType: AuthResourceType,
+        @QueryParam("resourceCode")
+        @Parameter(description = "资源code", required = false)
+        resourceCode: String,
+        @QueryParam("group")
+        @Parameter(description = "资源用户组类型", required = false)
+        group: BkAuthGroup? = null
+    ): Result<List<String>>
 }
--- src/backend/ci/ext/tencent/artifactory/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/resources/app/AppExperiencePermissionResourceImpl.kt
+++ src/backend/ci/ext/tencent/artifactory/biz-experience-tencent/src/main/kotlin/com/tencent/devops/experience/resources/app/AppExperiencePermissionResourceImpl.kt
@@ -1,16 +1,22 @@
 package com.tencent.devops.experience.resources.app
 
 import com.tencent.devops.auth.api.service.ServiceAuthApplyResource
+import com.tencent.devops.auth.api.service.ServiceProjectAuthResource
+import com.tencent.devops.auth.api.service.ServiceResourceMemberResource
 import com.tencent.devops.auth.pojo.ApplyJoinGroupInfo
 import com.tencent.devops.auth.pojo.vo.AuthApplyRedirectInfoVo
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.api.pojo.BkAuthGroup
 import com.tencent.devops.common.client.Client
+import com.tencent.devops.common.client.ClientTokenService
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.experience.api.app.AppExperiencePermissionResource
 
 @RestResource
 class AppExperiencePermissionResourceImpl(
-    val client: Client
+    val client: Client,
+    val tokenService: ClientTokenService,
 ) : AppExperiencePermissionResource {
     override fun applyToJoinGroup(
         userId: String,
@@ -32,6 +38,31 @@ class AppExperiencePermissionResourceImpl(
             resourceType = resourceType,
             resourceCode = resourceCode,
             action = action
+        )
+    }
+
+    override fun getResourceGroupUsers(
+        userId: String,
+        projectId: String,
+        resourceType: AuthResourceType,
+        resourceCode: String,
+        group: BkAuthGroup?
+    ): Result<List<String>> {
+        val isProjectUser = client.get(ServiceProjectAuthResource::class).isProjectUser(
+            token = tokenService.getSystemToken(),
+            userId = userId,
+            projectCode = projectId,
+            group = group
+        ).data ?: false
+        if (!isProjectUser) {
+            return Result(emptyList())
+        }
+        return client.get(ServiceResourceMemberResource::class).getResourceGroupMembers(
+            token = tokenService.getSystemToken(),
+            projectCode = projectId,
+            resourceType = resourceType.value,
+            resourceCode = resourceCode,
+            group = group
         )
     }
 }
