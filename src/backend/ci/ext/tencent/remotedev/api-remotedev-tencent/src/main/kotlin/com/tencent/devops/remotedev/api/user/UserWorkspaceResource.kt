d7c4dfab965c45fdd9de17763b754446596f1ada
randychen <randychen@tencent.com>
2025-05-29 12:14:24 +0800

    --task=75721220 【后台】bkw上向管理员发起审批增加审计上报

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserWorkspaceResource.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/api/user/UserWorkspaceResource.kt
@@ -316,7 +316,7 @@ interface UserWorkspaceResource {
         @Parameter(description = "用户ID", required = true, example = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @HeaderParam(AUTH_HEADER_USER_ID)
         userId: String,
-        @Parameter(description = "实例ID", required = true)
+        @Parameter(description = "实例ID", required = false)
         @QueryParam("workspaceName")
         workspaceName: String?,
         @Parameter(description = "发起的验证数据", required = true)
