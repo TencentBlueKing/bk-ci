7bd79dfec05f7dd31ce24cc71f9cf411682abe1a
hejieehe <904696180@qq.com>
2025-06-13 17:15:23 +0800

    feat：新版定时触发器兼容旧版模版参数 #11803 yaml转model时设置插件版本号

--- src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/transfer/TriggerTransfer.kt
+++ src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/transfer/TriggerTransfer.kt
@@ -747,13 +747,14 @@ class TriggerTransfer @Autowired(required = false) constructor(
                             }
                             JsonUtil.toJson(params, false)
                         },
-                        version = if ((timer.newExpression?.size ?: 0) > 1 ||
-                                (timer.advanceExpression?.size ?: 0) > 1
+                        version = if (timer.newExpression.filterNonEmpty().isEmpty() &&
+                                (timer.advanceExpression?.size ?: 0) == 1
                         ) {
-                            // 1.* 插件支持支持多个触发规则,2.*仅支持一个触发规则
-                            "1.*"
-                        } else {
+                            // 2.*仅支持一个触发规则, 且没有newExpression入参
                             "2.*"
+                        } else {
+                            // 1.* 插件支持支持多个触发规则
+                            "1.*"
                         }
                     ).checkTriggerElementEnable(timer.enable)
                 )
@@ -866,6 +867,8 @@ class TriggerTransfer @Autowired(required = false) constructor(
     private fun String.disjoin() = this.split(",")
 
     private fun List<String>?.nonEmptyOrNull() = this?.ifEmpty { null }
+
+    private fun List<String>?.filterNonEmpty() = this?.filter { it.isNotBlank() } ?: listOf()
 
     private fun buildP4TriggerElement(
         rule: PushRule?,
