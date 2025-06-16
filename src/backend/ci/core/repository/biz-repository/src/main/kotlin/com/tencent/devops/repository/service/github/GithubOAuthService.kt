bf2c331383da7a68ddfbf8c3c21343fb730aec31
hejieehe <904696180@qq.com>
2025-06-16 16:43:39 +0800

    feat：关联代码库时，若授权方式为 OAUTH，支持按照授权账号过滤代码库 #11483

--- src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/github/GithubOAuthService.kt
+++ src/backend/ci/core/repository/biz-repository/src/main/kotlin/com/tencent/devops/repository/service/github/GithubOAuthService.kt
@@ -159,9 +159,8 @@ class GithubOAuthService @Autowired constructor(
         val specRedirectUrl = arrays.getOrNull(6) ?: ""
         // 重定向类型
         val redirectUrlTypeEnum = RedirectUrlTypeEnum.getRedirectUrlType(arrays.getOrNull(7) ?: "")
-        // 操作人
-        val operator = (arrays.getOrNull(8) ?: "").ifBlank { userId }// 获取授权server端用户信息（蓝盾平台用户名可能跟github用户名不一致）
-        val userResponse = getUser(githubToken.accessToken)
+        // 操作人, 获取授权server端用户信息（蓝盾平台用户名可能跟github用户名不一致）
+        val operator = (arrays.getOrNull(8) ?: "").ifBlank { userId }
         githubTokenService.createAccessToken(
             userId = userId,
             accessToken = githubToken.accessToken,
