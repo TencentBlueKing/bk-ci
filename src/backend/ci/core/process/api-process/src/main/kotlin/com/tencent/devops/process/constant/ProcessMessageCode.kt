2d894bdccd6fe99c99aa034275321e426eff5a9f
mingshewhe <mingshewhe@tencent.com>
2025-06-25 14:42:54 +0800

    feat: 希望openapi提供批量查询流水线构建任务的接口 #11889
    优化当构建备注太长时，sql报错

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/constant/ProcessMessageCode.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/constant/ProcessMessageCode.kt
@@ -382,6 +382,7 @@ object ProcessMessageCode {
     const val ERROR_RUNNING_PIPELINE_ARCHIVE_INVALID = "2101276" // 流水线[{0}]正在运行中，无法归档
     const val ERROR_RETRY_TASK_NOT_FAILED = "2101277" // 重试的插件不是失败状态，不能进行插件级重试
     const val ERROR_USER_NO_PUSH_PERMISSION = "2101280" // 用户[{0}]没有仓库[{1}]的推送权限, 请联系仓库管理员添加权限
+    const val ERROR_BUILD_REMARK_MAX_LENGTH = "2101281" // 构建备注最多只能输入{0}个字符
 
     const val ERROR_TASK_REVIEW_NOT_FOUND_OR_NOT_RUNNING = "2101279" // 插件【ID:{0}】不存在或者不是可审核状态
     const val BK_SUCCESSFULLY_DISTRIBUTED = "bkSuccessfullyDistributed" // 跨项目构件分发成功，共分发了{0}个文件
