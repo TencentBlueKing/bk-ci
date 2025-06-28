bdf13795845b9b6db38005bc9dc3473e9433a5ec
greysonfang <greysonfang@tencent.com>
2025-06-28 16:17:59 +0800

    --story=125347744 pref：王者接入电子签优化

--- src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/ProjectSignatureManageService.kt
+++ src/backend/ci/ext/tencent/project/biz-project-tencent/src/main/kotlin/com/tencent/devops/project/service/ProjectSignatureManageService.kt
@@ -27,7 +27,6 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import kotlin.math.log
 
 @Service
 class ProjectSignatureManageService(
