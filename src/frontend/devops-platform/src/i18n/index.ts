93ef0c68a22433d43e5144a980fc55ac4edd0c37
lockiechen <lockiechen@tencent.com>
2025-06-30 14:12:11 +0800

    feat: 蓝盾国际化支持日语版 #11877

--- src/frontend/devops-platform/src/i18n/index.ts
+++ src/frontend/devops-platform/src/i18n/index.ts
@@ -2,10 +2,12 @@ import { getCookie } from '@/common/util';
 import { createI18n } from 'vue-i18n';
 import ZhCN from '../../../locale/platform/zh-CN.json';
 import EnUS from '../../../locale/platform/en-US.json';
+import JaJP from '../../../locale/platform/ja-JP.json';
 
 const LS_KEY = 'blueking_language';
 export const ZH_LOCALE = 'zh-CN';
 export const EN_LOCALE = 'en-US';
+export const JA_LOCALE = 'ja-JP';
 const DEFAULT_LANG = ZH_LOCALE;
 const localeAliasMap = {
     'zh-cn': 'zh-CN',
@@ -15,6 +17,10 @@ const localeAliasMap = {
     en: 'en-US',
     us: 'en-US',
     en_us: 'en-US',
+    'ja-jp': 'ja-JP',
+    ja: 'ja-JP',
+    jp: 'ja-JP',
+    ja_jp: 'ja-JP'
 };
 function getLsLocale() {
     try {
@@ -32,5 +38,6 @@ export default createI18n({
     messages: {
         'zh-CN': ZhCN,
         'en-US': EnUS,
+        'ja-JP': JaJP
     }
 });
