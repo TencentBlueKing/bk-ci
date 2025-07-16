2911f059de1e127558b10ad85443cb2790d42357
v_yjjiaoyu <1981190393@qq.com>
2025-07-16 11:31:26 +0800

    feat：第三方构建机节点支持标签管理 #11881 # Reviewed, transaction id: 50521

--- src/frontend/devops-environment/src/views/node/list_table.vue
+++ src/frontend/devops-environment/src/views/node/list_table.vue
@@ -146,7 +146,6 @@
                                     v-for="(item, index) in labelGroups[props.$index].hiddenLabels"
                                     class="group-tag"
                                     :key="index"
-                                    v-bk-overflow-tips
                                 >
                                     <bk-tag class="key">
                                         {{ item.tagKeyName }}: {{ item.tagValues[0].tagValueName }}
@@ -826,7 +825,14 @@
                 })
             },
             deleteRow (index) {
+                const cachedValues = this.setTagForm.map(item => item.tagValueId)
                 this.setTagForm.splice(index, 1)
+
+                this.$nextTick(() => {
+                    this.setTagForm.forEach((item, i) => {
+                        item.tagValueId = cachedValues[i < index ? i : i + 1]
+                    })
+                })
             },
             async handleSetConfirm () {
                 try {
@@ -951,6 +957,15 @@
           width: 100%;
           flex-wrap: wrap;
           margin: 3px 0;
+
+          .group-tag {
+            margin-bottom: 4px;
+          }
+
+          .key {
+            height: 100%;
+            margin-bottom: 2px;
+          }
        }
 
       .node-item-row {
