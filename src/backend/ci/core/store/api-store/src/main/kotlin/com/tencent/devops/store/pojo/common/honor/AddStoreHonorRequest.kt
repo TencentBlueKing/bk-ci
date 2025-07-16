69f1e1cf4e490bfe3ba1a4535fb9f6656c68e7e3
v_zkggzhou <2198773846@qq.com>
2025-07-16 09:54:23 +0800

    feat: 蓝盾国际化支持日语版 #11877

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/pojo/common/honor/AddStoreHonorRequest.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/pojo/common/honor/AddStoreHonorRequest.kt
@@ -27,15 +27,13 @@
 
 package com.tencent.devops.store.pojo.common.honor
 
-import com.tencent.devops.common.web.annotation.BkField
-import com.tencent.devops.store.pojo.common.enums.BkLanguageEnum
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(title = "添加组件荣誉信息请求报文")
 data class AddStoreHonorRequest(
     @get:Schema(title = "国家化荣誉信息", required = true)
-    val i18nHonorInfoList:List<i18nHonorInfoDTO>,
+    val i18nHonorInfoList: List<i18nHonorInfoDTO>,
     @get:Schema(title = "组件范畴", required = true)
     val storeType: StoreTypeEnum,
     @get:Schema(title = "组件代码列表", required = true)
