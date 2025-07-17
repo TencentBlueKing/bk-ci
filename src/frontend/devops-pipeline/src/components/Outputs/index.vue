142b50a12cc5f6779c74b19e1f53a7120e5914f0
v_yjjiaoyu <1981190393@qq.com>
2025-07-17 12:05:11 +0800

    feat：支持展示制品自动化测试结果 #11462 # Reviewed, transaction id: 50747

--- src/frontend/devops-pipeline/src/components/Outputs/index.vue
+++ src/frontend/devops-pipeline/src/components/Outputs/index.vue
@@ -25,6 +25,7 @@
                             class="select-search"
                             :show-condition="false"
                             :data="artifactFilterData"
+                            :placeholder="$t('settings.itemPlaceholder')"
                             v-model="artifactValue"
                             @change="updateSearchKey"
                         />
@@ -931,7 +932,7 @@
                 margin-right: 4px;
                 flex: 0 0 60%;
                 p {
-                    min-width: 50px;
+                    min-width: 62px;
                     text-align: center;
                     height: 32px;
                     line-height: 32px;
