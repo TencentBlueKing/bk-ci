9d160decf328f67b1465814d50938c8a37e1f27d
v_yjjiaoyu <1981190393@qq.com>
2025-07-17 19:26:43 +0800

    feat：支持展示制品自动化测试结果 #11462 # Reviewed, transaction id: 50854

--- src/frontend/devops-manage/src/components/project-settings.vue
+++ src/frontend/devops-manage/src/components/project-settings.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { BaseInfoContent, PermissionContent, PipelineContent, ArtifactoryContent } from "@/components/project-form-item/";
 import { RESOURCE_ACTION, RESOURCE_TYPE} from '@/utils/permission.js';
