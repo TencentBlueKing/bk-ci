cb77ae5d66a54a21b699e38bdc2382dc35d8d957
v_yjjiaoyu <1981190393@qq.com>
2025-07-18 14:39:04 +0800

    feat：支持展示制品自动化测试结果 #11462 # Reviewed, transaction id: 50953

--- src/frontend/devops-manage/src/components/project-form-item/ArtifactoryContent.vue
+++ src/frontend/devops-manage/src/components/project-form-item/ArtifactoryContent.vue
@@ -90,7 +90,7 @@
               v-model="row.display"
               theme="primary"
               :disabled="type === 'show'"
-              @change="handleChangeEnable"
+              @change="updateMetadata"
             />
           </template>
         </bk-table-column>
@@ -304,6 +304,11 @@ defineExpose({
   resetData,
 });
 
+function updateMetadata() {
+  emits('updateMetadata', metadataList.value)
+  emits('handleChangeForm')
+}
+
 async function fetchMetadataList () {
   isLoading.value = true;
   try {
@@ -415,10 +420,6 @@ function showMetadataAdd () {
   isShowMetadataForm.value = true;
 }
 
-function handleChangeEnable() {
-  emits('updateMetadata', metadataList.value)
-}
-
 function colorMapObject(form) {
   const labelColorMap = form.labelColorMap.reduce((accumulator, item) => {
     accumulator[item.value] = item.color;
@@ -447,8 +448,7 @@ async function handleMetadataSubmit () {
           metadataList.value.splice(index, 1, data);
         }
       }
-      emits('updateMetadata', metadataList.value)
-      emits('handleChangeForm')
+      updateMetadata();
       isShowMetadataForm.value = false;
       handleMetadataCancel();
     }
@@ -495,8 +495,7 @@ async function handleDelete (row) {
       const index = metadataList.value.findIndex(item => item.id === row.id);
       if (index !== -1) {
         metadataList.value.splice(index, 1);
-        emits('updateMetadata', metadataList.value);
-        emits('handleChangeForm')
+        updateMetadata();
       }
     },
     onClosed: () => true,
