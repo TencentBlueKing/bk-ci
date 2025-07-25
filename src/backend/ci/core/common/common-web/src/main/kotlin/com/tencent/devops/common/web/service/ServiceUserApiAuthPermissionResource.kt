50cb467431877d8c19e3b7b0f67eb9b6b4d4ce72
mingshewhe <mingshewhe@tencent.com>
2025-07-25 20:30:37 +0800

    bug: 用户态接口增加项目访问权限校验 #11971

--- src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/service/ServiceUserApiAuthPermissionResource.kt
+++ src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/service/ServiceUserApiAuthPermissionResource.kt
@@ -4,13 +4,13 @@ import com.tencent.devops.common.api.annotation.ServiceInterface
 import com.tencent.devops.common.api.auth.AUTH_HEADER_DEVOPS_USER_ID
 import com.tencent.devops.common.api.pojo.Result
 import io.swagger.v3.oas.annotations.Parameter
-import javax.ws.rs.Consumes
-import javax.ws.rs.GET
-import javax.ws.rs.HeaderParam
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-import javax.ws.rs.QueryParam
-import javax.ws.rs.core.MediaType
+import jakarta.ws.rs.Consumes
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.HeaderParam
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.QueryParam
+import jakarta.ws.rs.core.MediaType
 
 /**
  * 验证用户态接口调用,在auth服务实现
