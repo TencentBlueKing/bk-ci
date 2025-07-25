d37509771382964265a5160114b3c60a1177e77b
mingshewhe <mingshewhe@tencent.com>
2025-07-25 23:49:33 +0800

    --story=126144391 拦截非法的用户名

---
+++ src/backend/ci/core/common/common-web/src/main/kotlin/com/tencent/devops/common/web/filter/BlackUserFilter.kt
@@ -0,0 +1,34 @@
+package com.tencent.devops.common.web.filter
+
+import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
+import com.tencent.devops.common.api.constant.CommonMessageCode
+import com.tencent.devops.common.api.exception.ErrorCodeException
+import com.tencent.devops.common.web.RequestFilter
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.container.ContainerRequestFilter
+import jakarta.ws.rs.ext.Provider
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+
+@Provider
+@RequestFilter
+class BlackUserFilter : ContainerRequestFilter {
+
+    @Value("\${black.users:admin}")
+    private val blackUsers: List<String> = listOf("admin")
+
+    override fun filter(requestContext: ContainerRequestContext) {
+        val userId = requestContext.getHeaderString(AUTH_HEADER_USER_ID)
+        if (!userId.isNullOrBlank() && blackUsers.contains(userId)) {
+            logger.warn("User $userId is not allowed to access")
+            throw ErrorCodeException(
+                errorCode = CommonMessageCode.ERROR_INVALID_USER_ID,
+                params = arrayOf(userId)
+            )
+        }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(BlackUserFilter::class.java)
+    }
+}
