a8c009bcd742e16e5579861a56b5585c0272b87c
v_zkggzhou <2198773846@qq.com>
2025-07-28 17:29:48 +0800

    feat: 蓝盾国际化支持日语版 #11877

--- src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/pojo/common/honor/I18nHonorInfoDTO.kt
+++ src/backend/ci/core/store/api-store/src/main/kotlin/com/tencent/devops/store/pojo/common/honor/I18nHonorInfoDTO.kt
@@ -11,9 +11,9 @@ class I18nHonorInfoDTO(
     @get:Schema(title = "荣誉名称", required = true)
     @BkField(maxLength = 40)
     val honorName: String,
-    @get:Schema(title = "荣誉头衔国际化信息", required = true)
+    @get:Schema(title = "荣誉头衔国际化信息", required = false)
     val honorTitleI18n: String? = null,
-    @get:Schema(title = "荣誉名称国际化信息", required = true)
+    @get:Schema(title = "荣誉名称国际化信息", required = false)
     val honorNameI18n: String? = null,
     @get:Schema(title = "对应语言", required = true)
     val language: String
