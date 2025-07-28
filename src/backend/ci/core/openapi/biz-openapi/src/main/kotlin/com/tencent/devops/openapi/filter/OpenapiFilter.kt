04166a7ca702abe91b767f6abd04fd23b632cacb
mingshewhe <mingshewhe@tencent.com>
2025-07-28 15:14:41 +0800

    --story=126144391 拦截非法访问的用户

--- src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/filter/OpenapiFilter.kt
+++ src/backend/ci/core/openapi/biz-openapi/src/main/kotlin/com/tencent/devops/openapi/filter/OpenapiFilter.kt
@@ -2,7 +2,6 @@ package com.tencent.devops.openapi.filter
 
 import com.tencent.devops.common.web.RequestFilter
 import com.tencent.devops.openapi.filter.manager.ApiFilterManagerChain
-import jakarta.annotation.Priority
 import jakarta.ws.rs.container.ContainerRequestContext
 import jakarta.ws.rs.container.ContainerRequestFilter
 import jakarta.ws.rs.container.PreMatching
@@ -10,8 +9,6 @@ import jakarta.ws.rs.ext.Provider
 
 @Provider
 @PreMatching
-// 这个优先级要放在com.tencent.devops.common.web.filter.BlackUserFilter之后
-@Priority(2)
 @RequestFilter
 class OpenapiFilter constructor(
     private val apiFilter: ApiFilterManagerChain
