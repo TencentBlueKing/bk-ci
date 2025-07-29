4ec9bc8ec9a23c26bed72d34251e5020b9e031fd
yaoxuwan <yaoxuwan@tencent.com>
2025-07-29 15:39:18 +0800

    feat: 报告支持压缩预览 #11923

--- src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/config/ReportArchiveTaskConfig.kt
+++ src/backend/ci/core/artifactory/biz-artifactory/src/main/kotlin/com/tencent/devops/artifactory/config/ReportArchiveTaskConfig.kt
@@ -1,7 +1,6 @@
 package com.tencent.devops.artifactory.config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
 
 @Component
