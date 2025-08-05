c3e122626399975c13a1d3c01b2dcea6625da93f
stubenhuang <stubenhuang@tencent.com>
2025-08-05 15:36:29 +0800

    feat: 支持服务间jwt验证 #12067

--- src/gateway/core/lua/util/jwt_key_util.lua
+++ src/gateway/core/lua/util/jwt_key_util.lua
@@ -1,5 +1,8 @@
 -- 将jwtPrivateKey转换为PEM格式
 local single_line_b64_private_key = config.jwtPrivateKey
+if single_line_b64_private_key == '' or single_line_b64_private_key == nil then
+    return
+end
 -- 组装 PEM 格式的私钥
 local private_key_pem_parts = {}
 table.insert(private_key_pem_parts, "-----BEGIN RSA PRIVATE KEY-----")
@@ -7,7 +10,7 @@ table.insert(private_key_pem_parts, "-----BEGIN RSA PRIVATE KEY-----")
 local len = #single_line_b64_private_key
 local chunk_size = 64
 for i = 1, len, chunk_size do
-table.insert(private_key_pem_parts, string.sub(single_line_b64_private_key, i, i + chunk_size - 1))
+    table.insert(private_key_pem_parts, string.sub(single_line_b64_private_key, i, i + chunk_size - 1))
 end
 table.insert(private_key_pem_parts, "-----END RSA PRIVATE KEY-----")
 config.jwtPrivateKey = table.concat(private_key_pem_parts, "\n")
