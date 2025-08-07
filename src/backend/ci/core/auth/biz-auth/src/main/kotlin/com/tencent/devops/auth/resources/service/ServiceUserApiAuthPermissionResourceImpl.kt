70f676f4816d6dc13ed014c9edfde1c5d762640a
mingshewhe <mingshewhe@tencent.com>
2025-08-07 15:48:40 +0800

    bug: 修复用户态接口鉴权时偶现卡顿 #12111

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserApiAuthPermissionResourceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserApiAuthPermissionResourceImpl.kt
@@ -1,30 +1,29 @@
 package com.tencent.devops.auth.resources.service
 
+import com.tencent.devops.auth.service.iam.PermissionService
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.common.auth.api.AuthPermission
-import com.tencent.devops.common.auth.api.AuthPermissionApi
 import com.tencent.devops.common.auth.api.AuthResourceType
-import com.tencent.devops.common.auth.code.ProjectAuthServiceCode
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.common.web.service.ServiceUserApiAuthPermissionResource
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
 class ServiceUserApiAuthPermissionResourceImpl @Autowired constructor(
-    private val authPermissionApi: AuthPermissionApi,
-    private val projectAuthServiceCode: ProjectAuthServiceCode
+    val permissionService: PermissionService
 ) : ServiceUserApiAuthPermissionResource {
     override fun checkVisitPermission(
         userId: String,
         projectId: String
     ): Result<Boolean> {
         return Result(
-            authPermissionApi.validateUserResourcePermission(
-                user = userId,
-                serviceCode = projectAuthServiceCode,
-                resourceType = AuthResourceType.PROJECT,
+            permissionService.validateUserResourcePermissionByRelation(
+                userId = userId,
+                action = AuthPermission.VISIT.value,
                 projectCode = projectId,
-                permission = AuthPermission.VISIT
+                resourceType = AuthResourceType.PROJECT.value,
+                resourceCode = projectId,
+                relationResourceType = null
             )
         )
     }
