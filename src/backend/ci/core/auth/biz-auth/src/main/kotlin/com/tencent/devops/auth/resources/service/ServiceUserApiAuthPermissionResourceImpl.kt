4a17554654b721597480952b7cda01f4de56476c
mingshewhe <mingshewhe@tencent.com>
2025-08-07 11:47:30 +0800

    bug: 修复用户态接口鉴权时偶现卡顿 #12111

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserApiAuthPermissionResourceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/resources/service/ServiceUserApiAuthPermissionResourceImpl.kt
@@ -1,23 +1,30 @@
 package com.tencent.devops.auth.resources.service
 
-import com.tencent.devops.auth.service.iam.PermissionProjectService
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.common.auth.api.AuthPermission
+import com.tencent.devops.common.auth.api.AuthPermissionApi
+import com.tencent.devops.common.auth.api.AuthResourceType
+import com.tencent.devops.common.auth.code.ProjectAuthServiceCode
 import com.tencent.devops.common.web.RestResource
 import com.tencent.devops.common.web.service.ServiceUserApiAuthPermissionResource
 import org.springframework.beans.factory.annotation.Autowired
 
 @RestResource
 class ServiceUserApiAuthPermissionResourceImpl @Autowired constructor(
-    private val permissionProjectService: PermissionProjectService
+    private val authPermissionApi: AuthPermissionApi,
+    private val projectAuthServiceCode: ProjectAuthServiceCode
 ) : ServiceUserApiAuthPermissionResource {
     override fun checkVisitPermission(
         userId: String,
         projectId: String
     ): Result<Boolean> {
         return Result(
-            permissionProjectService.isProjectMember(
-                userId = userId,
+            authPermissionApi.validateUserResourcePermission(
+                user = userId,
+                serviceCode = projectAuthServiceCode,
+                resourceType = AuthResourceType.PROJECT,
                 projectCode = projectId,
+                permission = AuthPermission.VISIT
             )
         )
     }
