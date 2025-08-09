b68bb39a161c09602b33223ea53f4981fc3905ad
greysonfang <greysonfang@tencent.com>
2025-08-09 14:11:07 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414 迁移相关逻辑优化

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/PipelineTemplateSettingService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/PipelineTemplateSettingService.kt
@@ -2,7 +2,7 @@ package com.tencent.devops.process.service.template.v2
 
 import com.tencent.devops.common.api.exception.ErrorCodeException
 import com.tencent.devops.common.pipeline.pojo.setting.PipelineSetting
-import com.tencent.devops.process.constant.ProcessMessageCode
+import com.tencent.devops.process.constant.ProcessTemplateMessageCode.ERROR_TEMPLATE_SETTING_NOT_EXISTS
 import com.tencent.devops.process.dao.template.PipelineTemplateSettingDao
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateSettingCommonCondition
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateSettingUpdateInfo
@@ -22,9 +22,7 @@ class PipelineTemplateSettingService @Autowired constructor(
         return pipelineTemplateSettingDao.get(
             commonCondition = commonCondition,
             dslContext = dslContext
-        ) ?: throw ErrorCodeException(
-            errorCode = ProcessMessageCode.ERROR_TEMPLATE_NOT_EXISTS
-        )
+        ) ?: throw ErrorCodeException(errorCode = ERROR_TEMPLATE_SETTING_NOT_EXISTS)
     }
 
     fun get(
@@ -39,9 +37,7 @@ class PipelineTemplateSettingService @Autowired constructor(
                 settingVersion = settingVersion
             ),
             dslContext = dslContext
-        ) ?: throw ErrorCodeException(
-            errorCode = ProcessMessageCode.ERROR_TEMPLATE_NOT_EXISTS
-        )
+        ) ?: throw ErrorCodeException(errorCode = ERROR_TEMPLATE_SETTING_NOT_EXISTS)
     }
 
     fun count(commonCondition: PipelineTemplateSettingCommonCondition): Int {
@@ -63,9 +59,7 @@ class PipelineTemplateSettingService @Autowired constructor(
                 settingVersion = settingVersion
             ),
             dslContext = dslContext
-        ) ?: throw ErrorCodeException(
-            errorCode = ProcessMessageCode.ERROR_TEMPLATE_NOT_EXISTS
-        )
+        ) ?: throw ErrorCodeException(errorCode = ERROR_TEMPLATE_SETTING_NOT_EXISTS)
     }
 
     fun delete(
