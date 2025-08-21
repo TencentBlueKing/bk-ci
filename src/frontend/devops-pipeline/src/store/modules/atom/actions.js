ccc92a61fcae6bfb76cdedce7acc74b0bf8040c2
lockiechen <lockiechen@tencent.com>
2025-08-21 16:58:21 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414

--- src/frontend/devops-pipeline/src/store/modules/atom/actions.js
+++ src/frontend/devops-pipeline/src/store/modules/atom/actions.js
@@ -302,18 +302,22 @@ export default {
             }
         }
     },
-    async transfer ({ getters, state }, { projectId, pipelineId, actionType, ...params }) {
+    async transfer ({ getters, state }, { projectId, actionType, ...params }) {
+        const { isTemplate } = getters
+        const atomPropUrl = isTemplate ? `/${PROCESS_API_URL_PREFIX}/user/template/v2/atoms/projects/${projectId}/templates/${params.templateId}/atom/prop/list` : `/${PROCESS_API_URL_PREFIX}/user/pipeline/projects/${projectId}/pipelines/${params.pipelineId}/atom/prop/list`
+        const idKey = isTemplate ? 'templateId' : 'pipelineId'
+
         const apis = [
             request.post(`${PROCESS_API_URL_PREFIX}/user/transfer/projects/${projectId}`, params, {
                 params: {
-                    pipelineId,
+                    [idKey]: params[idKey],
                     actionType
                 }
             })
         ]
         if (actionType === 'FULL_YAML2MODEL' && !state.editfromImport) {
             apis.push(
-                request.get(`/${PROCESS_API_URL_PREFIX}/user/pipeline/projects/${projectId}/pipelines/${pipelineId}/atom/prop/list`, {
+                request.get(atomPropUrl, {
                     params: params.version ? { version: params.version } : {}
                 })
             )
