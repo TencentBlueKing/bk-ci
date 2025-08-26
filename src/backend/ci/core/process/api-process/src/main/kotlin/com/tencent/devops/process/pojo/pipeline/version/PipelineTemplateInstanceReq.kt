ba86132d431a6c82cebe7318efd7c52394545147
greysonfang <greysonfang@tencent.com>
2025-08-26 10:56:37 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414 记录模板和流水线版本关联关系

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/pipeline/version/PipelineTemplateInstanceReq.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/pojo/pipeline/version/PipelineTemplateInstanceReq.kt
@@ -27,10 +27,10 @@
 
 package com.tencent.devops.process.pojo.pipeline.version
 
-import com.tencent.devops.common.pipeline.pojo.TemplateInstanceField
 import com.tencent.devops.common.pipeline.enums.CodeTargetAction
 import com.tencent.devops.common.pipeline.pojo.BuildFormProperty
 import com.tencent.devops.common.pipeline.pojo.BuildNo
+import com.tencent.devops.common.pipeline.pojo.TemplateInstanceField
 import com.tencent.devops.common.pipeline.pojo.TemplateInstanceTriggerConfig
 import com.tencent.devops.process.pojo.template.TemplateRefType
 import io.swagger.v3.oas.annotations.media.Schema
@@ -44,7 +44,7 @@ data class PipelineTemplateInstanceReq(
     @get:Schema(title = "模版版本", required = false)
     val templateVersion: Long,
     @get:Schema(title = "模版实例化引用类型", required = false)
-    val templateRefType: TemplateRefType ?= TemplateRefType.ID,
+    val templateRefType: TemplateRefType? = TemplateRefType.ID,
     @get:Schema(title = "模版路径引用,分支/tag/commit", required = false)
     val templateRef: String?,
     @get:Schema(title = "流水线名称", required = false)
