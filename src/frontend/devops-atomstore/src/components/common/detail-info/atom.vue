5cf5e910079a8b796a12d7ef589ecb7ae14d223a
v_yjjiaoyu <1981190393@qq.com>
2025-08-28 18:57:53 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414

--- src/frontend/devops-atomstore/src/components/common/detail-info/atom.vue
+++ src/frontend/devops-atomstore/src/components/common/detail-info/atom.vue
@@ -415,6 +415,7 @@
                 this.$router.push({
                     name: 'install',
                     query: {
+                        name: this.detail.name,
                         code: this.detail.atomCode,
                         type: 'atom',
                         from: 'details'
--- src/frontend/devops-atomstore/src/components/common/detail-info/template.vue
+++ src/frontend/devops-atomstore/src/components/common/detail-info/template.vue
@@ -153,6 +153,7 @@
                 this.$router.push({
                     name: 'install',
                     query: {
+                        name: this.detail.name,
                         code: this.detail.templateCode,
                         type: 'template',
                         from: 'details'
