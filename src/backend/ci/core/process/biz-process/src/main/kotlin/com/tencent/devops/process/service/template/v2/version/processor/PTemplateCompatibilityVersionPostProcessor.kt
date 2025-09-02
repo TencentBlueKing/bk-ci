f914faf0517938899225908c537a4d6ab68e6713
greysonfang <greysonfang@tencent.com>
2025-09-02 17:53:21 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414 优化

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/version/processor/PTemplateCompatibilityVersionPostProcessor.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/version/processor/PTemplateCompatibilityVersionPostProcessor.kt
@@ -7,13 +7,10 @@ import com.tencent.devops.process.engine.dao.template.TemplateDao
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateResource
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateResourceCommonCondition
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateResourceUpdateInfo
-import com.tencent.devops.process.service.template.v2.PipelineTemplateInfoService
 import com.tencent.devops.process.service.template.v2.PipelineTemplateResourceService
-import com.tencent.devops.process.service.template.v2.PipelineTemplateSettingService
 import com.tencent.devops.process.service.template.v2.version.PipelineTemplateVersionCreateContext
 import com.tencent.devops.store.pojo.template.enums.TemplateStatusEnum
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -22,15 +19,14 @@ import org.springframework.stereotype.Service
  */
 @Service
 class PTemplateCompatibilityVersionPostProcessor(
-    val v2TemplateInfoService: PipelineTemplateInfoService,
     val v2TemplateResourceService: PipelineTemplateResourceService,
-    val v2TemplateSettingService: PipelineTemplateSettingService,
     val v1TemplateDao: TemplateDao,
     val v1TemplateSettingService: PipelineSettingDao,
     val dslContext: DSLContext
 ) : PTemplateVersionCreatePostProcessor {
 
-    override fun postProcessAfterVersionCreate(
+    override fun postProcessInTransactionVersionCreate(
+        transactionContext: DSLContext,
         context: PipelineTemplateVersionCreateContext,
         pipelineTemplateResource: PipelineTemplateResource,
         pipelineTemplateSetting: PipelineSetting
@@ -41,69 +37,53 @@ class PTemplateCompatibilityVersionPostProcessor(
             }
             logger.info("template compatibility version post processor :$context")
             val version = pipelineTemplateResource.version
-            val v2TemplateInfo = v2TemplateInfoService.get(
-                projectId = projectId,
-                templateId = templateId
-            )
-            val v2TemplateResource = v2TemplateResourceService.get(
-                projectId = projectId,
-                templateId = templateId,
-                version = version
-            )
-            val v2TemplateSetting = v2TemplateSettingService.get(
-                projectId = projectId,
-                templateId = templateId,
-                settingVersion = v2TemplateResource.settingVersion
-            )
-            val v2VersionName = v2TemplateResource.versionName!!
-            dslContext.transaction { configuration ->
-                val transactionContext = DSL.using(configuration)
-                // 假设新表一开始已经有v1名称版本了，此时老接口，旧逻辑再创建一个同名称v1，
-                // 那么此时应该将之前新表中的v1版本改为v1-1，并且新版本为v1-2.
-                if (!v1VersionName.isNullOrBlank()) {
-                    val existingCount = v1TemplateDao.countTemplateVersions(
-                        dslContext = transactionContext,
-                        projectId = projectId,
-                        templateId = templateId,
-                        versionName = v1VersionName
-                    )
-                    if (existingCount == 1) {
-                        v2TemplateResourceService.update(
-                            transactionContext = transactionContext,
-                            PipelineTemplateResourceUpdateInfo(
-                                versionName = "$v1VersionName-1"
-                            ),
-                            PipelineTemplateResourceCommonCondition(
-                                projectId = projectId,
-                                templateId = templateId,
-                                versionName = v1VersionName
-                            )
-                        )
-                    }
-                }
-
-                v1TemplateDao.createTemplate(
+            val v2TemplateInfo = context.pipelineTemplateInfo
+            val v2VersionName = pipelineTemplateResource.versionName
+            // 假设新表一开始已经有v1名称版本了，此时老接口，旧逻辑再创建一个同名称v1，
+            // 那么此时应该将之前新表中的v1版本改为v1-1，并且新版本为v1-2.
+            if (!v1VersionName.isNullOrBlank()) {
+                val existingCount = v1TemplateDao.countTemplateVersions(
                     dslContext = transactionContext,
                     projectId = projectId,
                     templateId = templateId,
-                    templateName = v2TemplateInfo.name,
-                    versionName = v1VersionName ?: v2VersionName,
-                    userId = userId,
-                    template = JsonUtil.toJson(v2TemplateResource.model),
-                    type = v2TemplateInfo.mode.name,
-                    category = v2TemplateInfo.category,
-                    logoUrl = v2TemplateInfo.logoUrl,
-                    srcTemplateId = v2TemplateResource.srcTemplateId,
-                    storeFlag = v2TemplateInfo.storeStatus != TemplateStatusEnum.NEVER_PUBLISHED,
-                    weight = 0,
-                    version = version,
-                    desc = v2TemplateInfo.desc
+                    versionName = v1VersionName
                 )
-                v1TemplateSettingService.saveSetting(
-                    dslContext = transactionContext,
-                    setting = v2TemplateSetting
-                )
+                if (existingCount == 1) {
+                    v2TemplateResourceService.update(
+                        transactionContext = transactionContext,
+                        PipelineTemplateResourceUpdateInfo(
+                            versionName = "$v1VersionName-1"
+                        ),
+                        PipelineTemplateResourceCommonCondition(
+                            projectId = projectId,
+                            templateId = templateId,
+                            versionName = v1VersionName
+                        )
+                    )
+                }
             }
+
+            v1TemplateDao.createTemplate(
+                dslContext = transactionContext,
+                projectId = projectId,
+                templateId = templateId,
+                templateName = pipelineTemplateSetting.pipelineName,
+                versionName = v1VersionName ?: v2VersionName!!,
+                userId = userId,
+                template = JsonUtil.toJson(pipelineTemplateResource.model),
+                type = v2TemplateInfo.mode.name,
+                category = v2TemplateInfo.category,
+                logoUrl = v2TemplateInfo.logoUrl,
+                srcTemplateId = pipelineTemplateResource.srcTemplateId,
+                storeFlag = v2TemplateInfo.storeStatus != TemplateStatusEnum.NEVER_PUBLISHED,
+                weight = 0,
+                version = version,
+                desc = v2TemplateInfo.desc
+            )
+            v1TemplateSettingService.saveSetting(
+                dslContext = transactionContext,
+                setting = pipelineTemplateSetting
+            )
         }
     }
 
