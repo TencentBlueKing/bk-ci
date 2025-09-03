4430a6deea085d967ca80425acc86bb4ce3d0c4d
v_yjjiaoyu <1981190393@qq.com>
2025-09-03 16:12:57 +0800

    feat:插件发布时发布描述内容支持读取插件代码库最近提交信息 #12080 # Reviewed, transaction id: 56511

--- src/frontend/devops-atomstore/src/store/atom.js
+++ src/frontend/devops-atomstore/src/store/atom.js
@@ -202,7 +202,7 @@ export const actions = {
      */
     requestAtomLog ({ commit }, params) {
         const query = new URLSearchParams(params).toString()
-        return vue.$ajax.get(`${prefix}/user/store/components/commitMessages/get?${query}`)
+        return vue.$ajax.get(`${repositoryPrefix}/service/git/commitMessages/get?${query}`)
     },
     /**
      * 流水线插件详情
