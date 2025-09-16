2e0b1d64119062519a19599cb24d4917a90dd5c4
ruotiantang <ruotiantang@tencent.com>
2025-09-16 10:00:36 +0800

    fix：第三方构建机 CPU 统计有误问题修复 #12104

--- src/agent/agent/src/pkg/collector/telegrafConf/telegrafConf_win.go
+++ src/agent/agent/src/pkg/collector/telegrafConf/telegrafConf_win.go
@@ -165,12 +165,20 @@ const TelegrafConf = `
     X-DEVOPS-AGENT-ID = "{{.AgentId}}"
     X-DEVOPS-AGENT-SECRET-KEY = "{{.AgentSecret}}"
 
-[[inputs.cpu]]
-  percpu = true
-  totalcpu = true
-  collect_cpu_time = false
-  report_active = false
 [[inputs.win_perf_counters]]
+  [[inputs.win_perf_counters.object]]
+    ObjectName = "Processor"
+    Instances = ["*"]
+    Counters = [
+      "% Idle Time",
+      "% Interrupt Time",
+      "% Privileged Time",
+      "% User Time",
+      "% Processor Time",
+      "% DPC Time",
+    ]
+    Measurement = "cpu"
+    IncludeTotal = true
   [[inputs.win_perf_counters.object]]
     ObjectName = "PhysicalDisk"
     Instances = ["*"]
@@ -210,17 +218,15 @@ const TelegrafConf = `
     measurement = "cpu"
     dest = "cpu_detail"
   [[processors.rename.replace]]
-    field = "usage_user"
+    field = "Percent_User_Time"
     dest = "user"
   [[processors.rename.replace]]
-    field = "usage_system"
+    field = "Percent_Privileged_Time"
     dest = "system"
   [[processors.rename.replace]]
-    field = "usage_idle"
+    field = "Percent_Idle_Time"
     dest = "idle"
-  [[processors.rename.replace]]
-    field = "usage_iowait"
-    dest = "iowait"
+
   # net
   [[processors.rename.replace]]
     field = "Bytes_Received_persec"
@@ -292,7 +298,13 @@ const TelegrafConf = `
   namepass = ["disk"]
   [[processors.rename.replace]]
     field = "used_percent"
-    dest = "in_use"  
+    dest = "in_use"
+
+[[processors.rename]]
+  namepass = ["cpu"]
+  [[processors.rename.replace]]
+    tag = "instance"
+    dest = "cpu"  
 
 {{ end }}
 `
