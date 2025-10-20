d96c250f15fee1f0c5149d1bac39d94d288a8d21
ruotiantang <ruotiantang@tencent.com>
2025-10-20 10:45:10 +0800

    --story=126335848 【API】绑定工蜂repo

--- src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/gitproxy/TGitBindRemotedevData.kt
+++ src/backend/ci/ext/tencent/remotedev/api-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/gitproxy/TGitBindRemotedevData.kt
@@ -1,7 +1,13 @@
 package com.tencent.devops.remotedev.pojo.gitproxy
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class TGitBindRemotedevData(
+    @JsonProperty("tGitId")
     val tGitId: Long,
+    @JsonProperty("tGitUrl")
     val tGitUrl: String,
     val projectIds: List<String>
 )
