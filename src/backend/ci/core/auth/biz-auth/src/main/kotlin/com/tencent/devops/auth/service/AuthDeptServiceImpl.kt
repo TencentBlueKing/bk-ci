a0994203d6e46aa37667ea734501431e2e89d017
greysonfang <greysonfang@tencent.com>
2025-10-20 15:29:42 +0800

    feat：用户管理相关优化 #12329

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/AuthDeptServiceImpl.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/service/AuthDeptServiceImpl.kt
@@ -341,7 +341,8 @@ class AuthDeptServiceImpl(
         return if (departedMembersCache.getIfPresent(userId) == true) {
             true
         } else {
-            getUserInfo(userId) == null
+            val userInfo = getUserInfo(userId)
+            userInfo == null || userInfo.departed == true
         }.also {
             if (it) {
                 departedMembersCache.put(userId, true)
@@ -466,7 +467,8 @@ class AuthDeptServiceImpl(
                 name = it.userId,
                 displayName = it.userName,
                 type = ManagerScopesEnum.USER,
-                deptInfo = it.departments
+                deptInfo = it.departments,
+                departed = it.departed
             )
         } ?: getUserInfoFromExternal(userId).also {
             if (it != null) userInfoCache.put(userId, it)
