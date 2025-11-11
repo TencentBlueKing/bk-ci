d5e60bff27869dca8907eafea844684233db7995
v_zkggzhou <2198773846@qq.com>
2025-11-11 17:23:37 +0800

    --story=128421954 存量云研发包增加存储包文件的sha256摘要值

--- src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/common/service/TxStorePkgService.kt
+++ src/backend/ci/ext/tencent/store/biz-store-tencent/src/main/kotlin/com/tencent/devops/store/common/service/TxStorePkgService.kt
@@ -1,7 +1,6 @@
 package com.tencent.devops.store.common.service
 
 import com.tencent.devops.store.pojo.common.enums.StoreTypeEnum
-import com.tencent.devops.common.api.pojo.Result
 
 interface TxStorePkgService {
 
