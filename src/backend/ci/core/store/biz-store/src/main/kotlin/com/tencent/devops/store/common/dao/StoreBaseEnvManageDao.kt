e4000d338fb6376e4a5e715ffefc04b7c69fd068
v_zkggzhou <2198773846@qq.com>
2025-11-12 11:38:36 +0800

    feat：存量云研发包增加存储包文件的sha256摘要值 #12398

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/dao/StoreBaseEnvManageDao.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/dao/StoreBaseEnvManageDao.kt
@@ -28,6 +28,7 @@
 package com.tencent.devops.store.common.dao
 
 import com.tencent.devops.model.store.tables.TStoreBaseEnv
+import com.tencent.devops.model.store.tables.records.TStoreBaseEnvRecord
 import com.tencent.devops.store.pojo.common.publication.StoreBaseEnvDataPO
 import java.time.LocalDateTime
 import org.jooq.DSLContext
