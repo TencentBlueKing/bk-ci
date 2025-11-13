725434764d54296184ce460fdee0a611961f5bf5
hejieehe <904696180@qq.com>
2025-11-13 16:35:28 +0800

    feat：平台管理-注册事件 #12379

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/TriggerEventStoreReleaseSpecBusService.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/common/service/impl/TriggerEventStoreReleaseSpecBusService.kt
@@ -1,5 +1,10 @@
 package com.tencent.devops.store.common.service.impl
 
+import com.tencent.devops.common.api.constant.BEGIN
+import com.tencent.devops.common.api.constant.BUILD
+import com.tencent.devops.common.api.constant.COMMIT
+import com.tencent.devops.common.api.constant.TEST
+import com.tencent.devops.common.web.utils.I18nUtil
 import com.tencent.devops.store.common.service.StoreReleaseSpecBusService
 import com.tencent.devops.store.pojo.common.QueryComponentPkgEnvInfoParam
 import com.tencent.devops.store.pojo.common.enums.ReleaseTypeEnum
@@ -74,7 +79,58 @@ class TriggerEventStoreReleaseSpecBusService : StoreReleaseSpecBusService {
         isNormalUpgrade: Boolean,
         status: StoreStatusEnum
     ): List<ReleaseProcessItem> {
-        logger.info("getReleaseProcessItems")
+        // TODO: 定义发布流程
+//        val processInfo = mutableListOf<ReleaseProcessItem>()
+//        processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = BEGIN), BEGIN, NUM_ONE, SUCCESS))
+//        processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = COMMIT), COMMIT, NUM_TWO, UNDO))
+//        processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = BUILD), BUILD, NUM_THREE, UNDO))
+//        processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = TEST), TEST, NUM_FOUR, UNDO))
+//        processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = EDIT), EDIT, NUM_FIVE, UNDO))
+//        if (isNormalUpgrade) {
+//            processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = END), END, NUM_SIX, UNDO))
+//        } else {
+//            processInfo.add(
+//                ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = APPROVE), APPROVE, NUM_SIX, UNDO)
+//            )
+//            processInfo.add(ReleaseProcessItem(I18nUtil.getCodeLanMessage(messageCode = END), END, NUM_SEVEN, UNDO))
+//        }
+//        val totalStep = if (isNormalUpgrade) NUM_SIX else NUM_SEVEN
+//        when (status) {
+//            StoreStatusEnum.INIT, StoreStatusEnum.COMMITTING -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_TWO, DOING)
+//            }
+//
+//            StoreStatusEnum.BUILDING -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_THREE, DOING)
+//            }
+//
+//            StoreStatusEnum.BUILD_FAIL -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_THREE, FAIL)
+//            }
+//
+//            StoreStatusEnum.TESTING -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_FOUR, DOING)
+//            }
+//
+//            StoreStatusEnum.EDITING -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_FIVE, DOING)
+//            }
+//
+//            StoreStatusEnum.AUDITING -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_SIX, DOING)
+//            }
+//
+//            StoreStatusEnum.AUDIT_REJECT -> {
+//                storeCommonService.setProcessInfo(processInfo, totalStep, NUM_SIX, FAIL)
+//            }
+//
+//            StoreStatusEnum.RELEASED -> {
+//                val currStep = if (isNormalUpgrade) NUM_SIX else NUM_SEVEN
+//                storeCommonService.setProcessInfo(processInfo, totalStep, currStep, SUCCESS)
+//            }
+//
+//            else -> {}
+//        }
         return listOf()
     }
 
@@ -88,8 +144,8 @@ class TriggerEventStoreReleaseSpecBusService : StoreReleaseSpecBusService {
         logger.info("doStoreEnvBus")
     }
 
-    override fun doStorePostCreateBus(userId: String, storeCode: String, storeType: StoreTypeEnum) {
-        logger.info("doStorePostCreateBus")
+    override fun doStoreCreatePostBus(userId: String, storeCode: String, storeType: StoreTypeEnum) {
+        logger.info("doStoreCreatePostBus")
     }
 
     companion object {
