13928aceb5e8cf211b156882c9a9f3e3120dbe51
ruotiantang <ruotiantang@tencent.com>
2025-12-19 16:43:07 +0800

    feat: 创作流-第三方机 #12354

---
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/MyCreateEnv.kt
@@ -0,0 +1,17 @@
+﻿package com.tencent.devops.environment.pojo
+
+/**
+ * 创作环境相关内置环境
+ */
+
+// 我的创作环境
+object MyCreateEnv {
+    const val ENV_ID = -1L
+    const val ENV_NAME_KEY = "myCreateEnv"
+}
+
+// 所有创作节点环境
+object AllCreateNodeEnv {
+    const val ENV_ID = -2L
+    const val ENV_NAME_KEY = "allNodeCreateEnv"
+}
