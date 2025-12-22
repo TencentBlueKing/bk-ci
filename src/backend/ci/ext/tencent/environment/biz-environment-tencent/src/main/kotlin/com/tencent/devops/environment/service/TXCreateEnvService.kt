34e4674e2852659b6ca94f1b277412909c9df9d6
ruotiantang <ruotiantang@tencent.com>
2025-12-22 20:31:07 +0800

    feat: 创作流-第三方机 #12354

---
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/TXCreateEnvService.kt
@@ -0,0 +1,17 @@
+﻿package com.tencent.devops.environment.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Service
+
+@Service
+@Primary
+class TXCreateEnvService @Autowired constructor() : CreateEnvService() {
+    override fun fetchUserWorkspaceId(projectId: String, userId: String): List<String> {
+        return emptyList()
+    }
+
+    override fun fetchAllWorkspaceId(projectId: String): List<String> {
+        return emptyList()
+    }
+}
