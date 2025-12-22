a3ad67edbf27dc7cbd64d9328302da3ad4173286
ruotiantang <ruotiantang@tencent.com>
2025-12-22 20:43:15 +0800

    feat: 创作流-第三方机 #12354

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/TXCreateEnvService.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/TXCreateEnvService.kt
@@ -1,17 +1,24 @@
 ﻿package com.tencent.devops.environment.service
 
+import com.tencent.devops.common.client.Client
+import com.tencent.devops.remotedev.api.service.ServiceRemoteDevResource
+import com.tencent.devops.remotedev.pojo.WorkspaceSearch
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
 @Service
 @Primary
-class TXCreateEnvService @Autowired constructor() : CreateEnvService() {
+class TXCreateEnvService @Autowired constructor(
+    private val client: Client
+) : CreateEnvService() {
     override fun fetchUserWorkspaceId(projectId: String, userId: String): List<String> {
-        return emptyList()
-    }
-
-    override fun fetchAllWorkspaceId(projectId: String): List<String> {
-        return emptyList()
+        return client.get(ServiceRemoteDevResource::class).getWorkspaceListNew(
+            userId = userId,
+            projectId = projectId,
+            page = null,
+            pageSize = null,
+            search = WorkspaceSearch()
+        ).data?.records?.map { it.workspaceName } ?: emptyList()
     }
 }
