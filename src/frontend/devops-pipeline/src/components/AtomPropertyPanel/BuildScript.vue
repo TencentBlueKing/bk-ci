a14451aaa308e93069a63691b5bd81e1617d83b6
lockiechen <lockiechen@tencent.com>
2026-01-15 11:25:05 +0800

    --bug=153145428 [蓝盾]流水线使用Batch Script插件保存失败

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/BuildScript.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/BuildScript.vue
@@ -56,6 +56,7 @@
 </template>
 
 <script>
+    import { getAtomDefaultValue } from '@/store/modules/atom/atomUtil'
     import { mapActions, mapGetters, mapState } from 'vuex'
     import validMixins from '../validMixins'
     import atomMixin from './atomMixin'
