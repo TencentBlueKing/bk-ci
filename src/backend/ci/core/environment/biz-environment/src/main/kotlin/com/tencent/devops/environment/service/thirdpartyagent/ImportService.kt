692d598579039a1307f115aa9cd1f6bc1f447106
ruotiantang <ruotiantang@tencent.com>
2026-01-30 15:36:43 +0800

    feat: 创作流-第三方机 #12354

--- src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/thirdpartyagent/ImportService.kt
+++ src/backend/ci/core/environment/biz-environment/src/main/kotlin/com/tencent/devops/environment/service/thirdpartyagent/ImportService.kt
@@ -208,7 +208,7 @@ class ImportService @Autowired constructor(
             )
 
             val nodeStringId = if (agentRecord.agentType == AgentType.CREATE.name) {
-                createEnvService.getWorkspaceDisplayName(projectId, agentRecord.createWorkspaceName)
+                createEnvService.getWorkspaceDisplayName(userId, projectId, agentRecord.createWorkspaceName)
                     ?: agentRecord.createWorkspaceName ?: "CREATE_${agentRecord.ip}"
             } else {
                 "BUILD_${HashUtil.encodeLongId(nodeId)}_${agentRecord.ip}"
