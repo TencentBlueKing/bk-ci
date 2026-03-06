a5c6fd188568b83134c1ac862a0b65e25cb49aed
greysonfang <greysonfang@tencent.com>
2026-03-06 17:57:11 +0800

    feat：模板Apigw接口从v1版本升级到v2版本 #12686

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/version/convert/PipelineTemplateReleaseCreateReqConverter.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/version/convert/PipelineTemplateReleaseCreateReqConverter.kt
@@ -34,7 +34,6 @@ import com.tencent.devops.common.pipeline.template.PipelineTemplateType
 import com.tencent.devops.process.constant.PipelineTemplateConstant
 import com.tencent.devops.process.pojo.template.TemplateType
 import com.tencent.devops.process.pojo.template.v2.PTemplateResourceWithoutVersion
-import com.tencent.devops.process.pojo.template.v2.PipelineTemplateCompatibilityCreateReq
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateInfoV2
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateReleaseCreateReq
 import com.tencent.devops.process.pojo.template.v2.PipelineTemplateVersionReq
