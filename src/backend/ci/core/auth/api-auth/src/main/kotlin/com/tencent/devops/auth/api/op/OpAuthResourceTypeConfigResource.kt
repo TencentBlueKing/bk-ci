fa59263b16a3b3ec0c77ab60129ca5bd26451980
greysonfang <greysonfang@tencent.com>
2026-03-18 14:17:40 +0800

    feat：创作流接入权限中心 #12493

--- src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/op/OpAuthResourceTypeConfigResource.kt
+++ src/backend/ci/core/auth/api-auth/src/main/kotlin/com/tencent/devops/auth/api/op/OpAuthResourceTypeConfigResource.kt
@@ -24,9 +24,6 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
-
-
 package com.tencent.devops.auth.api.op
 
 import com.tencent.devops.auth.pojo.request.ActionCreateRequest
--- src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/context/ChannelContext.kt
+++ src/backend/ci/core/common/common-api/src/main/kotlin/com/tencent/devops/common/api/context/ChannelContext.kt
@@ -68,6 +68,3 @@ object ChannelContext {
         }
     }
 }
-
-
-
