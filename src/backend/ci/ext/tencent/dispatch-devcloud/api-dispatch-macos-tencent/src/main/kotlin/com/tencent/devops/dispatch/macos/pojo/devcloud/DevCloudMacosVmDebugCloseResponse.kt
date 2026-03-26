d2dc54e4564d0d1824d810d908bb7041424c3ff0
sawyersong <sawyersong@tencent.com>
2026-03-26 17:43:14 +0800

    --story=128261692 feat：MacOS 公共构建机支持登录调试

--- src/backend/ci/ext/tencent/dispatch-devcloud/api-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/pojo/devcloud/DevCloudMacosVmDebugCloseResponse.kt
+++ src/backend/ci/ext/tencent/dispatch-devcloud/api-dispatch-macos-tencent/src/main/kotlin/com/tencent/devops/dispatch/macos/pojo/devcloud/DevCloudMacosVmDebugCloseResponse.kt
@@ -6,5 +6,5 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class DevCloudMacosVmDebugCloseResponse(
     val actionCode: Int,
     val actionMessage: String,
-    val data: DevCloudMacosVmDebugCloseInfo
+    val data: DevCloudMacosVmDebugCloseInfo? = null
 )
