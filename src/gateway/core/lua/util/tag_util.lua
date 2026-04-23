fca8f5a06acc67fc75593e61c7038cc6474fe3dd
stubenhuang <stubenhuang@tencent.com>
2026-04-23 10:53:38 +0800

    --story=133721434 【蓝盾网关】设置默认集群和bkcode的路由

--- src/gateway/core/lua/util/tag_util.lua
+++ src/gateway/core/lua/util/tag_util.lua
@@ -18,7 +18,11 @@
 _M = {}
 -- 判断当前请求属于哪个tag
 function _M:get_tag(ns_config)
-    local gateway_project = ngx.var.project or "bkci"
+    local gateway_project = ngx.var.project
+    if gateway_project == nil or gateway_project == '' then
+        gateway_project = "bkci"
+    end
+
     local devops_service = ngx.var.service
     local devops_project_id = ngx.var.project_id
     local tag = nil
