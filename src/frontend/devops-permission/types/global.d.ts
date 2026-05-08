a9bc46ef807b5289f9c4f751850b4e2c2d436550
lockiechen <lockiechen@tencent.com>
2026-05-08 10:09:35 +0800

    feat: 开源版bk-ci支持域名下的子路径 #12937

--- src/frontend/devops-permission/types/global.d.ts
+++ src/frontend/devops-permission/types/global.d.ts
@@ -2,6 +2,7 @@ declare interface Window {
   SITE_URL: string
   BK_IAM_URL_PREFIX: string
   BK_ITSM_URL_PREFIX: string
+  PUBLIC_URL_PREFIX: string
   $syncUrl: (url: string) => void
   getRoutePrefix: () => string;
 }
