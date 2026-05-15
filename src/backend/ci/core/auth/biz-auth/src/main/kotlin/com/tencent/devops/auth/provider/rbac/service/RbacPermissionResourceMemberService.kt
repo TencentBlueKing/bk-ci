5d0195a1ec0921a4846a4fb7455bd614e84fce80
greysonfang <greysonfang@tencent.com>
2026-05-15 17:26:23 +0800

    feat：支持个人项目 #12852

--- src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/provider/rbac/service/RbacPermissionResourceMemberService.kt
+++ src/backend/ci/core/auth/biz-auth/src/main/kotlin/com/tencent/devops/auth/provider/rbac/service/RbacPermissionResourceMemberService.kt
@@ -39,7 +39,6 @@ import org.apache.commons.lang3.RandomUtils
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
-import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 @Suppress("SpreadOperator", "LongParameterList")
@@ -273,6 +272,7 @@ class RbacPermissionResourceMemberService(
         return true
     }
 
+    @Suppress("NestedBlockDepth")
     override fun batchAddResourceGroupMembers(
         projectCode: String,
         iamGroupId: Int,
