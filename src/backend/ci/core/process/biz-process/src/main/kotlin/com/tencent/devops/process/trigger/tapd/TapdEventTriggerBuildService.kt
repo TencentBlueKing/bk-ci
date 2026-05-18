f6202ca825825ca9dcb1283ffd1f9e989038a949
hejieehe <904696180@qq.com>
2026-05-18 15:22:46 +0800

    feat：支持 TAPD 事件触发一期 #12959 初始化启动变量

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/trigger/tapd/TapdEventTriggerBuildService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/trigger/tapd/TapdEventTriggerBuildService.kt
@@ -37,7 +37,9 @@ import com.tencent.devops.common.pipeline.enums.TapdEventType
 import com.tencent.devops.common.pipeline.pojo.element.trigger.TapdWebHookTriggerElement
 import com.tencent.devops.common.webhook.enums.WebhookI18nConstants.TRIGGER_CONDITION_NOT_MATCH
 import com.tencent.devops.process.constant.ProcessMessageCode
+import com.tencent.devops.process.engine.pojo.PipelineInfo
 import com.tencent.devops.process.engine.service.PipelineRepositoryService
+import com.tencent.devops.process.pojo.pipeline.PipelineResourceVersion
 import com.tencent.devops.process.pojo.trigger.PipelineTriggerFailedMatchElement
 import com.tencent.devops.process.service.CreateStreamTriggerSupportService
 import com.tencent.devops.process.trigger.WebhookTriggerBuildService
@@ -66,131 +68,148 @@ class TapdEventTriggerBuildService @Autowired constructor(
         private val logger = LoggerFactory.getLogger(TapdEventTriggerBuildService::class.java)
     }
 
-    @Suppress("CyclomaticComplexMethod", "NestedBlockDepth")
     fun tapdWebhookTrigger(event: TapdWebhookTriggerEvent) {
         logger.info("Receive tapd webhook trigger event[${JsonUtil.toJson(event, false)}]")
-        with(event) {
-            val context = WebhookTriggerContext(
-                projectId = projectId,
-                pipelineId = pipelineId,
-                eventId = eventId,
-                startType = StartType.WEB_HOOK
+        val context = WebhookTriggerContext(
+            projectId = event.projectId,
+            pipelineId = event.pipelineId,
+            eventId = event.eventId,
+            startType = StartType.WEB_HOOK
+        )
+        try {
+            doTrigger(event, context)
+        } catch (ignored: Exception) {
+            logger.warn(
+                "failed to trigger by tapd webhook|${event.projectId}|${event.pipelineId}|${event.tapdProjectId}",
+                ignored
             )
-            try {
-                val tapdEventType = TapdEventType.parse(eventType) ?: run {
-                    logger.warn("Unsupported tapd event type|$eventType")
+            webhookTriggerManager.fireError(context, ignored)
+        }
+    }
+
+    private fun doTrigger(event: TapdWebhookTriggerEvent, context: WebhookTriggerContext) = with(event) {
+        // 1. 解析事件类型与动作
+        val tapdEventType = TapdEventType.parse(eventType) ?: run {
+            logger.warn("Unsupported tapd event type|$eventType")
+            return@with
+        }
+        val tapdEventAction = TapdEventAction.parse(eventAction) ?: run {
+            logger.warn("Unsupported tapd event action|$eventAction")
+            return@with
+        }
+        // 2. 查询流水线信息，已锁定的流水线跳过
+        val pipelineInfo = pipelineRepositoryService.getPipelineInfo(projectId, pipelineId)
+            ?: throw ErrorCodeException(
+                errorCode = ProcessMessageCode.ERROR_PIPELINE_NOT_EXISTS,
+                params = arrayOf(pipelineId)
+            )
+        if (pipelineInfo.locked == true) return@with
+        context.pipelineInfo = pipelineInfo
+
+        // 3. 创作环境下需要补充节点相关启动参数
+        val externalStartParams = externalStartParams(pipelineInfo)
+
+        // 4. 匹配触发器并启动构建
+        val resource = pipelineRepositoryService.getPipelineResourceVersion(
+            projectId = projectId,
+            pipelineId = pipelineId,
+            version = null
+        ) ?: throw ErrorCodeException(
+            statusCode = Response.Status.NOT_FOUND.statusCode,
+            errorCode = ProcessMessageCode.ERROR_PIPELINE_MODEL_NOT_EXISTS
+        )
+        matchAndStart(
+            event = this,
+            context = context,
+            pipelineInfo = pipelineInfo,
+            resource = resource,
+            tapdEventType = tapdEventType,
+            tapdEventAction = tapdEventAction,
+            externalStartParams = externalStartParams
+        )
+    }
+
+    /**
+     * 补充扩展参数
+     */
+    private fun TapdWebhookTriggerEvent.externalStartParams(
+        pipelineInfo: PipelineInfo
+    ): Map<String, String> {
+        if (pipelineInfo.channelCode != ChannelCode.CREATIVE_STREAM) return mapOf()
+
+        val envHashId = pipelineRepositoryService.getSetting(projectId, pipelineId)?.envHashId ?: ""
+        val oauthUserId = pipelineRepositoryService.getPipelineOauthUser(projectId, pipelineId)
+            ?: pipelineInfo.lastModifyUser
+        val agentHashId = createStreamTriggerSupportService.getEnvNodeList(
+            projectId = projectId,
+            envHashId = envHashId,
+            userId = oauthUserId
+        ).firstOrNull() ?: run {
+            logger.warn("skip trigger $pipelineId|no available node found in env[$envHashId] of project[$projectId]")
+            return mapOf()
+        }
+        return createStreamTriggerSupportService.creativeStreamParams(
+            projectId = projectId,
+            agentHashId = agentHashId,
+            userId = oauthUserId
+        )
+    }
+
+    @Suppress("LongParameterList")
+    private fun matchAndStart(
+        event: TapdWebhookTriggerEvent,
+        context: WebhookTriggerContext,
+        pipelineInfo: PipelineInfo,
+        resource: PipelineResourceVersion,
+        tapdEventType: TapdEventType,
+        tapdEventAction: TapdEventAction,
+        externalStartParams: Map<String, String>
+    ) {
+        val elements = resource.model.getTriggerContainer().elements
+            .filterIsInstance<TapdWebHookTriggerElement>()
+            .filter { it.elementEnabled() }
+        if (elements.isEmpty()) {
+            logger.info("no enabled tapd trigger element|${event.projectId}|${event.pipelineId}")
+            return
+        }
+
+        val failedMatchElements = mutableListOf<PipelineTriggerFailedMatchElement>()
+        for (element in elements) {
+            val atomResponse = tapdEventMatcher.matches(
+                element = element,
+                tapdProjectId = event.tapdProjectId,
+                eventType = tapdEventType,
+                eventAction = tapdEventAction,
+                triggerUser = event.triggerUser
+            )
+            when (atomResponse.matchStatus) {
+                MatchStatus.SUCCESS -> {
+                    webhookTriggerBuildService.startPipeline(
+                        context = context,
+                        pipelineInfo = pipelineInfo,
+                        resource = resource,
+                        startParams = event.startParams.plus(externalStartParams)
+                    )
+                    logger.info(
+                        "tapd webhook trigger success|${event.projectId}|${event.pipelineId}|element=${element.id}"
+                    )
                     return
                 }
-                val tapdEventAction = TapdEventAction.parse(eventAction) ?: run {
-                    logger.warn("Unsupported tapd event action|$eventAction")
-                    return
-                }
-                val pipelineInfo = pipelineRepositoryService.getPipelineInfo(
-                    projectId = projectId,
-                    pipelineId = pipelineId
-                ) ?: throw ErrorCodeException(
-                    errorCode = ProcessMessageCode.ERROR_PIPELINE_NOT_EXISTS,
-                    params = arrayOf(pipelineId)
+                MatchStatus.CONDITION_NOT_MATCH -> failedMatchElements.add(
+                    PipelineTriggerFailedMatchElement(
+                        elementId = element.id,
+                        elementName = element.name,
+                        elementAtomCode = element.getAtomCode(),
+                        reasonMsg = atomResponse.failedReason
+                            ?: I18Variable(code = TRIGGER_CONDITION_NOT_MATCH).toJsonStr()
+                    )
                 )
-                if (pipelineInfo.locked == true) return
-                context.pipelineInfo = pipelineInfo
-                val externalStartParams = mutableMapOf<String, String>()
-                if (pipelineInfo.channelCode == ChannelCode.CREATIVE_STREAM) {
-                    // 对创作环境下所有的创作节点，逐一启动
-                    val envHashId = pipelineRepositoryService.getSetting(
-                        projectId = projectId,
-                        pipelineId = pipelineId
-                    )?.envHashId ?: ""
-                    val oauthUserId = pipelineRepositoryService.getPipelineOauthUser(
-                        projectId = projectId,
-                        pipelineId = pipelineId
-                    ) ?: pipelineInfo.lastModifyUser
-                    val creativeStreamParams = createStreamTriggerSupportService.getEnvNodeList(
-                        projectId = projectId,
-                        envHashId = envHashId,
-                        userId = oauthUserId
-                    ).takeIf { it.isNotEmpty() }?.firstOrNull()?.let { agentHashId ->
-                        createStreamTriggerSupportService.creativeStreamParams(
-                            projectId = projectId,
-                            agentHashId = agentHashId,
-                            userId = oauthUserId
-                        )
-                    } ?: run {
-                        logger.warn(
-                            "skip trigger $pipelineId|no available node found in " +
-                                    "env[$envHashId] of project[$projectId]"
-                        )
-                        mapOf()
-                    }
-                    externalStartParams.putAll(creativeStreamParams)
-                }
-                val resource = pipelineRepositoryService.getPipelineResourceVersion(
-                    projectId = projectId,
-                    pipelineId = pipelineId,
-                    version = null
-                ) ?: throw ErrorCodeException(
-                    statusCode = Response.Status.NOT_FOUND.statusCode,
-                    errorCode = ProcessMessageCode.ERROR_PIPELINE_MODEL_NOT_EXISTS
-                )
-                val triggerContainer = resource.model.getTriggerContainer()
-                val failedMatchElements = mutableListOf<PipelineTriggerFailedMatchElement>()
-                triggerContainer.elements.filterIsInstance<TapdWebHookTriggerElement>()
-                    .forEach elements@{ element ->
-                        if (!element.elementEnabled()) {
-                            return@elements
-                        }
-                        val atomResponse = tapdEventMatcher.matches(
-                            element = element,
-                            tapdProjectId = tapdProjectId,
-                            eventType = tapdEventType,
-                            eventAction = tapdEventAction,
-                            triggerUser = triggerUser
-                        )
-                        when (atomResponse.matchStatus) {
-                            MatchStatus.SUCCESS -> {
-                                webhookTriggerBuildService.startPipeline(
-                                    context = context,
-                                    pipelineInfo = pipelineInfo,
-                                    resource = resource,
-                                    startParams = startParams.plus(externalStartParams)
-                                )
-                                logger.info(
-                                    "tapd webhook trigger success|$projectId|$pipelineId|element=${element.id}"
-                                )
-                                return
-                            }
-
-                            MatchStatus.CONDITION_NOT_MATCH -> {
-                                failedMatchElements.add(
-                                    PipelineTriggerFailedMatchElement(
-                                        elementId = element.id,
-                                        elementName = element.name,
-                                        elementAtomCode = element.getAtomCode(),
-                                        reasonMsg = atomResponse.failedReason ?: I18Variable(
-                                            code = TRIGGER_CONDITION_NOT_MATCH
-                                        ).toJsonStr()
-                                    )
-                                )
-                            }
-
-                            else -> {
-                                return@elements
-                            }
-                        }
-                    }
-                if (failedMatchElements.isNotEmpty()) {
-                    context.failedMatchElements = failedMatchElements
-                    webhookTriggerManager.fireMatchFailed(context)
-                } else {
-                    logger.info("no enabled tapd trigger element|$projectId|$pipelineId")
-                }
-            } catch (ignored: Exception) {
-                logger.error(
-                    "Failed to trigger by tapd webhook|$projectId|$pipelineId|$tapdProjectId",
-                    ignored
-                )
-                webhookTriggerManager.fireError(context, ignored)
+                else -> Unit
             }
+        }
+        if (failedMatchElements.isNotEmpty()) {
+            context.failedMatchElements = failedMatchElements
+            webhookTriggerManager.fireMatchFailed(context)
         }
     }
 }
