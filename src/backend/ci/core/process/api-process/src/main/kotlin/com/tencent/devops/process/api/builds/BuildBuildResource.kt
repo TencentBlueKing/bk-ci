bcfca542388253bc2365f5ea9bd1abf90e131556
hejieehe <904696180@qq.com>
2026-05-28 11:36:17 +0800

    feat：支持 TAPD 事件触发一期 #12959

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/builds/BuildBuildResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/builds/BuildBuildResource.kt
@@ -39,6 +39,7 @@ import com.tencent.devops.process.pojo.pipeline.ModelDetail
 import com.tencent.devops.process.pojo.task.PipelineFailTaskDetail
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.HeaderParam
