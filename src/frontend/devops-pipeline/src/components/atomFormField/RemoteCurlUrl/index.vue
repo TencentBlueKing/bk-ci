c34bc84cf0bd0af7228506bd73cf51a207c7dcfb
lockiechen <lockiechen@tencent.com>
2026-06-09 15:16:44 +0800

    --story=128820023 建议提供自定义参数类型及列表参数类型 ，以便于能填写复杂参数

--- src/frontend/devops-pipeline/src/components/atomFormField/RemoteCurlUrl/index.vue
+++ src/frontend/devops-pipeline/src/components/atomFormField/RemoteCurlUrl/index.vue
@@ -15,6 +15,7 @@
 <script>
     import RouteTips from '@/components/atomFormField/RouteTips'
     import { PROCESS_API_URL_PREFIX } from '@/store/constants'
+    import { isObject } from '@/utils/util'
     import { mapActions } from 'vuex'
     import atomFieldMixin from '../atomFieldMixin'
     export default {
@@ -34,7 +35,9 @@
             stringifyParmas () {
                 const { params } = this.container
                 const paramMap = params.filter(param => param.required).reduce((map, param) => {
-                    map[param.id] = param.defaultValue
+                    map[param.id] = Array.isArray(param.defaultValue)
+                        ? param.defaultValue.map(item => isObject(item) ? JSON.stringify(item) : item).join(',')
+                        : param.defaultValue
                     return map
                 }, {})
                 return JSON.stringify(paramMap).replace(/\"/g, '\\"')
