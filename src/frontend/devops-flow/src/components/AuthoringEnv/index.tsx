6dc39d92a98f7bf19c12afc715d3863cbee55fa3
lockiechen <lockiechen@tencent.com>
2026-06-10 17:37:46 +0800

    feat: optimize authoring environment selector #12414

--- src/frontend/devops-flow/src/components/AuthoringEnv/index.tsx
+++ src/frontend/devops-flow/src/components/AuthoringEnv/index.tsx
@@ -36,6 +36,18 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    selectLabel: {
+      type: String,
+      default: '',
+    },
+    selectRequired: {
+      type: Boolean,
+      default: false,
+    },
+    showEnvironmentManagement: {
+      type: Boolean,
+      default: false,
+    },
   },
   emits: ['update:modelValue'],
   setup(props, { emit }) {
@@ -75,50 +87,91 @@ export default defineComponent({
       goEnvironment(envHashId.value)
     }
 
+    function renderEnvironmentManagement() {
+      return (
+        <button type="button" class={styles.envManageEntry} onClick={goToEnvironment}>
+          <SvgIcon name="jump" size={12} class={styles.envManageIcon} />
+          <span>{t('flow.content.environmentManagement')}</span>
+        </button>
+      )
+    }
+
+    function renderEnvSelect() {
+      const selectProps = {
+        class: styles.envSelect,
+        modelValue: envHashId.value,
+        'onUpdate:modelValue': (value: string) => {
+          envHashId.value = value
+        },
+        filterable: true,
+        list: props.envList,
+        loading: props.envLoading,
+        placeholder: t('flow.content.creationEnvironment'),
+        searchPlaceholder: t('flow.content.searchEnvironment'),
+        onChange: handleChange,
+      }
+      const select = props.showEnvironmentManagement ? (
+        <Select {...selectProps}>
+          {{
+            extension: renderEnvironmentManagement,
+          }}
+        </Select>
+      ) : (
+        <Select
+          {...selectProps}
+        ></Select>
+      )
+
+      return (
+        <div class={[styles.envSelectLine, !props.selectLabel && styles.envSelectLinePlain]}>
+          {props.selectLabel ? (
+            <label class={styles.envSelectLabel}>
+              {props.selectLabel}
+              {props.selectRequired ? <span class={styles.requiredMark}>*</span> : null}
+            </label>
+          ) : null}
+          <div class={styles.envSelectControl}>{select}</div>
+        </div>
+      )
+    }
+
     return () => (
-      <div class={styles.authoringContent}>
-        <p class={styles.authoringHeader}>
-          {props.isEdit ? (
-            <Select
-              class={styles.envSelect}
-              v-model={envHashId.value}
-              filterable
-              list={props.envList}
-              loading={props.envLoading}
-              searchPlaceholder={t('flow.content.searchEnvironment')}
-              onChange={handleChange}
-            ></Select>
-          ) : (
-            <span class={styles.headerText}>{envName.value}</span>
-          )}
-        </p>
-        {envHashId.value ? (
-          <Loading loading={props.nodeLoading} size="small" class="p-lg">
-            <div class={styles.envItem}>
-              <p class={styles.envItemTit}>
-                {t('flow.content.creationNode')}
-                {props.isEdit ? (
-                  <span onClick={goToEnvironment}>
-                    <SvgIcon name="set-line" size={12} class={`cursor-pointer ${styles.setLine}`} />
-                  </span>
-                ) : null}
-              </p>
-              <div class={styles.nodeTag}>
-                {props.nodeList.length > 0
-                  ? props.nodeList.map((node: any) => (
-                      <Tag key={node.nodeId}>{getAuthoringNodeDisplayText(node)}</Tag>
-                    ))
-                  : '--'}
+      <div class={styles.authoringRoot}>
+        {props.isEdit ? renderEnvSelect() : null}
+        <div class={styles.authoringContent}>
+          {!props.isEdit ? (
+            <p class={styles.authoringHeader}>
+              <span class={styles.headerText}>{envName.value}</span>
+            </p>
+          ) : null}
+          {envHashId.value ? (
+            <Loading loading={props.nodeLoading} size="small" class="p-lg">
+              <div class={styles.envItem}>
+                <p class={styles.envItemTit}>
+                  {t('flow.content.creationNode')}
+                  {props.isEdit ? (
+                    <span onClick={goToEnvironment}>
+                      <SvgIcon name="set-line" size={12} class={`cursor-pointer ${styles.setLine}`} />
+                    </span>
+                  ) : null}
+                </p>
+                <div class={styles.nodeTag}>
+                  {props.nodeList.length > 0
+                    ? props.nodeList.map((node: any) => (
+                        <Tag key={node.nodeId}>{getAuthoringNodeDisplayText(node)}</Tag>
+                      ))
+                    : '--'}
+                </div>
               </div>
-            </div>
-            <div class={styles.envItem}>
-              <p class={styles.envItemTit}>{t('flow.content.workspace')}</p>
-              <div>{t('flow.content.workSpaceDesc', [envHashId.value])}</div>
-            </div>
-          </Loading>
-        ) : (
-          <p class={styles.noData}>{t('flow.content.previewDetailsAfterEnvironmentSelection')}</p>
-        )}
+              <div class={styles.envItem}>
+                <p class={styles.envItemTit}>{t('flow.content.workspace')}</p>
+                <div>{t('flow.content.workSpaceDesc', [envHashId.value])}</div>
+              </div>
+            </Loading>
+          ) : (
+            <p class={styles.noData}>{t('flow.content.previewDetailsAfterEnvironmentSelection')}</p>
+          )}
+        </div>
       </div>
     )
   },
