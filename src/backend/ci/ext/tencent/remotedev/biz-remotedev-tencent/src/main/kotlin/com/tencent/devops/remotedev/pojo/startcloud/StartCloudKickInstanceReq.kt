8f9d54fe5591238da27b04271c337c9dc356fc10
randychen <randychen@tencent.com>
2026-06-11 14:45:50 +0800

    --story=133877589 【云桌面】支持当前登录用户切换设备登录

--- src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/startcloud/StartCloudKickInstanceReq.kt
+++ src/backend/ci/ext/tencent/remotedev/biz-remotedev-tencent/src/main/kotlin/com/tencent/devops/remotedev/pojo/startcloud/StartCloudKickInstanceReq.kt
@@ -1,11 +1,6 @@
 package com.tencent.devops.remotedev.pojo.startcloud
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class StartCloudKickInstanceReq(
-    @JsonProperty("userId")
     val userId: String,
-
-    @JsonProperty("envId")
     val envId: String
 )
