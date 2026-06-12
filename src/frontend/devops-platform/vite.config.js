d917bed526617843f61428246f31fbb5754f0848
lockiechen <lockiechen@tencent.com>
2026-06-12 16:43:21 +0800

    feat: 开源版bk-ci支持域名下的子路径 #12937

--- src/frontend/devops-platform/vite.config.js
+++ src/frontend/devops-platform/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
@@ -6,27 +6,31 @@ import postcssImport from 'postcss-import';
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
 
-export default defineConfig({
-  base: '/platform',
-  css: {
-    postcss: {
-      plugins: [
-        postcssImport,
-        autoprefixer,
-        tailwindcss
-      ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_PUBLIC_PATH,
+    publicPath: env.VITE_PUBLIC_PATH,
+    css: {
+      postcss: {
+        plugins: [
+          postcssImport,
+          autoprefixer,
+          tailwindcss
+        ],
+      },
     },
-  },
-  server: {
-    port: 8010
-  },
-  plugins: [vue(), vueJsx()],
-  build: {
-    outDir: '../frontend/platform'
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    server: {
+      port: 8010
     },
-  },
+    plugins: [vue(), vueJsx()],
+    build: {
+      outDir: '../frontend/platform',
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 })
