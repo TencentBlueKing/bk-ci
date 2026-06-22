62e6323e58f9e92a2729317ff3a5209097ee7d67
ruotiantang <ruotiantang@tencent.com>
2026-06-22 11:20:31 +0800

    --story=134226214 feat：创作流支持团队共享的数字同事

--- src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/api/thirdpartyagent/ExternalThirdPartyAgentResource.kt
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/api/thirdpartyagent/ExternalThirdPartyAgentResource.kt
@@ -38,7 +38,6 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.HeaderParam
-import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.Produces
