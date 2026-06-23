1794743a234086d1e6bdbe6cbcdfc298678ee1d4
ruotiantang <ruotiantang@tencent.com>
2026-06-23 15:00:15 +0800

    --story=134226214 feat：创作流支持团队共享的数字同事

--- src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/AsyncExecuteListener.kt
+++ src/backend/ci/ext/tencent/environment/biz-environment-tencent/src/main/kotlin/com/tencent/devops/environment/service/AsyncExecuteListener.kt
@@ -1,14 +1,18 @@
 ﻿package com.tencent.devops.environment.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.tencent.devops.common.api.util.JsonUtil
 import com.tencent.devops.environment.config.async.AsyncExecuteEvent
 import com.tencent.devops.environment.config.async.AsyncExecuteEventType
+import com.tencent.devops.environment.pojo.AsyncInstallImateData
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class AsyncExecuteListener @Autowired constructor(
+    private val objectMapper: ObjectMapper,
     private val tencentNodeService: TencentNodeService
 ) {
 
@@ -24,7 +28,8 @@ class AsyncExecuteListener @Autowired constructor(
     private fun doExecute(event: AsyncExecuteEvent) {
         when (event.type) {
             AsyncExecuteEventType.ASYNC_INSTALL_IMATE -> {
-                tencentNodeService.importImateCallBack(JsonUtil.to(event.eventStr))
+                val data = objectMapper.readValue<AsyncInstallImateData>(event.eventStr)
+                tencentNodeService.importImateCallBack(data)
             }
         }
     }
