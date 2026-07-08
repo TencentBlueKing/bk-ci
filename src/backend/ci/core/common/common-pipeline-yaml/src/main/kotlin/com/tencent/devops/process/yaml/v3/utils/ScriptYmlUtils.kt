05d848b8d206ea6b381114ab7343451b954d585c
mingshewhe <mingshewhe@tencent.com>
2026-07-08 10:14:05 +0800

    bug: 定时触发 YAML 配置中 trigger-conf.variables 参数格式校验错误 #13192

--- src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/v3/utils/ScriptYmlUtils.kt
+++ src/backend/ci/core/common/common-pipeline-yaml/src/main/kotlin/com/tencent/devops/process/yaml/v3/utils/ScriptYmlUtils.kt
@@ -427,7 +427,7 @@ object ScriptYmlUtils {
                     val variable =
                         YamlObjects.transValue<Map<String, Any?>>("extends.template.variables", it.key, it.value)
                     PreTemplateVariable(
-                        value = YamlObjects.getNotNullValueAny("value", it.key, variable),
+                        value = YamlObjects.getNullValue("value", variable),
                         allowModifyAtStartup = YamlObjects.getNullValue("allow-modify-at-startup", variable)
                             ?.toBoolean() ?: true
                     )
