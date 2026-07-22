96b8339354d9f141d110533ac7f38c05090f8d41
ruotiantang <ruotiantang@tencent.com>
2026-07-22 15:25:04 +0800

    feat：搜索节点支持用户输入批量 IP #13343

--- src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/NodeFetchReq.kt
+++ src/backend/ci/core/environment/api-environment/src/main/kotlin/com/tencent/devops/environment/pojo/NodeFetchReq.kt
@@ -8,5 +8,5 @@ data class NodeFetchReq(
     @get:Schema(title = "查询标签列表")
     val tags: List<NodeTagFetchReq>?,
     @get:Schema(title = "大IP查询 （空格、中英文逗号/分号、换行、| 分隔）时，支持解析出 IP 列表")
-    val ipListSearch: String?
+    val ipListSearch: String? = null
 )
