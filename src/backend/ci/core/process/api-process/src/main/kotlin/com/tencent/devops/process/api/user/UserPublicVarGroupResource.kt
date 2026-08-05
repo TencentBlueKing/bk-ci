08e3eefb6cdac4dde87f46cdbf441c7ba92a0eca
yjieliang <v_yjieliang@tencent.com>
2026-08-05 15:33:28 +0800

    feat:流水线公共变量管理 #12010

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPublicVarGroupResource.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/api/user/UserPublicVarGroupResource.kt
@@ -45,7 +45,6 @@ import jakarta.ws.rs.DELETE
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.HeaderParam
 import jakarta.ws.rs.POST
-import jakarta.ws.rs.PUT
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.Produces
