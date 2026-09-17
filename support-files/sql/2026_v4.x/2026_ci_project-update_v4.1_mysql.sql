69e723f3efea24a824111edc2d4790bfed3a0e4b
greysonfang <greysonfang@tencent.com>
2026-09-17 14:42:55 +0800

    feat: 项目管理支持标签能力 #13620

--- support-files/sql/2026_v4.x/2026_ci_project-update_v4.1_mysql.sql
+++ support-files/sql/2026_v4.x/2026_ci_project-update_v4.1_mysql.sql
@@ -12,25 +12,36 @@ BEGIN
     SELECT DATABASE() INTO db;
 
     IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_PROJECT'
+                    AND COLUMN_NAME = 'project_scope') THEN
+        ALTER TABLE T_PROJECT
+            ADD COLUMN `project_scope` int(10) NOT NULL DEFAULT '0'
+                COMMENT '项目组织形态：0-团队项目，1-个人项目';
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
+                  FROM information_schema.COLUMNS
+                  WHERE TABLE_SCHEMA = db
+                    AND TABLE_NAME = 'T_PROJECT_APPROVAL'
+                    AND COLUMN_NAME = 'PROJECT_SCOPE') THEN
+    ALTER TABLE T_PROJECT_APPROVAL
+        ADD COLUMN `PROJECT_SCOPE` int(10) NOT NULL DEFAULT '0'
+                    COMMENT '项目组织形态：0-团队项目，1-个人项目';
+    END IF;
+
+    IF NOT EXISTS(SELECT 1
                   FROM information_schema.STATISTICS
                   WHERE TABLE_SCHEMA = db
                     AND TABLE_NAME = 'T_PROJECT_LABEL_REL'
                     AND INDEX_NAME = 'UNI_LABEL_ID_PROJECT_ID') THEN
-        DELETE t1
-        FROM T_PROJECT_LABEL_REL t1
-                 INNER JOIN T_PROJECT_LABEL_REL t2
-                            ON t1.LABEL_ID = t2.LABEL_ID
-                                AND t1.PROJECT_ID = t2.PROJECT_ID
-                                AND t1.ID > t2.ID;
-        ALTER TABLE `T_PROJECT_LABEL_REL`
-            ADD UNIQUE KEY `UNI_LABEL_ID_PROJECT_ID` (`LABEL_ID`, `PROJECT_ID`);
+    ALTER TABLE `T_PROJECT_LABEL_REL`
+        ADD UNIQUE KEY `UNI_LABEL_ID_PROJECT_ID` (`LABEL_ID`, `PROJECT_ID`);
     END IF;
-
+    
     COMMIT;
 END <CI_UBF>
 DELIMITER ;
 CALL ci_project_schema_update();
 DROP PROCEDURE IF EXISTS ci_project_schema_update;
-
-INSERT IGNORE INTO `T_PROJECT_LABEL` (`ID`, `LABEL_NAME`)
-VALUES ('6f1e2d3c4b5a49788e9d0c1b2a3f4e5d', 'COMMON_IMATE');
